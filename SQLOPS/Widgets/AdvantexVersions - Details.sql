0f8fb966bd9a997e8153323422c873b74b27b1fa
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-02-01 09:39:36 +0100

    Refined output of OnlyAufruf

--- SQLOPS/Widgets/AdvantexVersions - Details.sql
+++ SQLOPS/Widgets/AdvantexVersions - Details.sql
@@ -1,4 +1,4 @@
-SELECT s.Mandant, FORMAT(s.Startzeit, N'G', N'de-AT') AS Startzeit, s.DBLoginName AS DomainUser, m.MitarbeiUser AS AdvanTexUser, alm.ComputerName, s.[Version] AS AdvanTexVersion, FORMAT(s.LastHeartBeat, N'G', N'de-AT') AS LastHeartbeat, s.OnlyAufruf
+SELECT s.Mandant, FORMAT(s.Startzeit, N'G', N'de-AT') AS Startzeit, s.DBLoginName AS DomainUser, m.MitarbeiUser AS AdvanTexUser, alm.ComputerName, s.[Version] AS AdvanTexVersion, FORMAT(s.LastHeartBeat, N'G', N'de-AT') AS LastHeartbeat, ISNULL(s.OnlyAufruf, N'') AS OnlyAufruf
 FROM dbSystem.dbo.sessions s 
 LEFT OUTER JOIN Mitarbei m ON m.ID = s.MitarbeiID 
 LEFT OUTER JOIN ArbPlatz alm ON alm.ID = s.ArbPlatzID 
