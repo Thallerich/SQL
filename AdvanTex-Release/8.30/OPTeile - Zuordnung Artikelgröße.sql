ed8bf207e217328d14c411f3998d8b76abf8d29c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-02-22 15:36:35 +0100

    Artikelzuordnung - Skript neu erstellt

--- AdvanTex-Release/8.30/OPTeile - Zuordnung Artikelgröße.sql
+++ AdvanTex-Release/8.30/OPTeile - Zuordnung Artikelgröße.sql
@@ -1,33 +1,54 @@
-SELECT OPTeile.ID, OPTeile.Status, OPTeile.Code, Teile.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse, OPTeile.ArtGroeID, Teile.ArtGroeID AS TArtGroeID, OPTeile.LastScanTime, OPTeile.LastScanToKunde, ZielNr.ZielNrBez AS [letzter Ort], OPTeile.Anlage_
+DECLARE @ArtNr nchar(15);
+SET @ArtNr = N'209900010386';
+
+SELECT OPTeile.Code, OPTeile.ArtGroeID, Teile.Barcode, Teile.ArtGroeID
 FROM OPTeile
 JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
-JOIN ZielNr ON OPTeile.ZielNrID = ZielNr.ID
-LEFT OUTER JOIN Teile ON OPTeile.Code = Teile.Barcode AND Teile.ArtikelID = OPTeile.ArtikelID
-LEFT OUTER JOIN ArtGroe ON Teile.ArtGroeID = ArtGroe.ID
-WHERE Artikel.ArtikelNr = N'203258100201'
+JOIN Teile ON Teile.OPTeileID = OPTeile.ID
+WHERE Artikel.ArtikelNr = @ArtNr
+  AND OPTeile.ArtGroeID < 0;
+
+/*
+UPDATE OPTeile SET ArtGroeID = Teile.ArtGroeID
+FROM OPTeile
+JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+JOIN Teile ON Teile.OPTeileID = OPTeile.ID
+WHERE Artikel.ArtikelNr = N'209900010386'
   AND OPTeile.ArtGroeID < 0
-ORDER BY OPTeile.LastScanTime DESC;
+  AND Teile.ArtGroeID > 0;
+*/
 
-UPDATE OPTeile SET OPTeile.ArtGroeID = x.TArtGroeID
+SELECT OPTeile.Code, OPTeile.ArtGroeID, OPTeile.LastScanTime
 FROM OPTeile
-JOIN (
-  SELECT OPTeile.ID, OPTeile.Status, OPTeile.Code, Teile.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse, OPTeile.ArtGroeID, Teile.ArtGroeID AS TArtGroeID, OPTeile.LastScanTime, OPTeile.LastScanToKunde, ZielNr.ZielNrBez AS [letzter Ort], OPTeile.Anlage_
-  FROM OPTeile
-  JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
-  JOIN ZielNr ON OPTeile.ZielNrID = ZielNr.ID
-  LEFT OUTER JOIN Teile ON OPTeile.Code = Teile.Barcode AND Teile.ArtikelID = OPTeile.ArtikelID
-  LEFT OUTER JOIN ArtGroe ON Teile.ArtGroeID = ArtGroe.ID
-  WHERE Artikel.ArtikelNr = N'203258100201'
-    AND OPTeile.ArtGroeID < 0
-    AND Teile.ID IS NOT NULL
-) AS x ON x.ID = OPTeile.ID;
+JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+WHERE Artikel.ArtikelNr = @ArtNr
+  AND OPTeile.ArtGroeID < 0;
 
-UPDATE OPTeile SET OPTeile.ArtGroeID = 10297420
-WHERE OPTeile.ArtikelID = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'203258100201')
-  AND OPTeile.ArtGroeID = -1;
-
-SELECT OPTeile.ID, OPTeile.Code, Teile.Barcode, OPTeile.ArtGroeID, Teile.ArtGroeID, OPTeile.ArtikelID, Teile.ArtikelID
+SELECT OPTeile.Code, OPTeile.ArtGroeID, Teile.Barcode, Teile.ArtGroeID, OPTeile.ArtikelID, Teile.ArtikelID
 FROM OPTeile
-LEFT OUTER JOIN Teile ON Teile.OPTeileID = OPTeile.ID
-WHERE OPTeile.ArtikelID = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'203258100201')
-  AND (Teile.ArtGroeID <> OPTeile.ArtGroeID OR OPTeile.ArtGroeID < 0)
+JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+JOIN Teile ON Teile.Barcode = OPTeile.Code
+WHERE Artikel.ArtikelNr = @ArtNr
+  AND OPTeile.ArtGroeID < 0
+  AND OPTeile.ArtikelID = Teile.ArtikelID;
+
+/*
+UPDATE OPTeile SET ArtGroeID = Teile.ArtGroeID
+FROM OPTeile
+JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+JOIN Teile ON Teile.Barcode = OPTeile.Code
+WHERE Artikel.ArtikelNr = N'209900010386'
+  AND OPTeile.ArtGroeID < 0
+  AND OPTeile.ArtikelID = Artikel.ID;
+*/
+
+SELECT ArtGroe.*
+FROM ArtGroe
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+WHERE Artikel.ArtikelNr = @ArtNr;
+
+/*
+UPDATE OPTeile SET ArtGroeID = 10297346
+WHERE OPTeile.ArtikelID = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'209900010386')
+  AND OPTeile.ArtGroeID < 0;
+*/
