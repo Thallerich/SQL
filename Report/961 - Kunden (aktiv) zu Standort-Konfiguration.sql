09cf5803e8452f8d240e3d8a2d8f28d0b9a0475f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-03-30 10:20:42 +0200

    Report 961 zur Auswertung der Kunden / VSAs einer bestimmten Standort-Konfiguration erstellt

---
+++ Report/961 - Kunden (aktiv) zu Standort-Konfiguration.sql
@@ -0,0 +1,11 @@
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenstatus.StatusBez$LAN$ AS Kundenstatus, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS VSA, VsaStatus.StatusBez$LAN$ AS [VSA-Status], StandKon.Bez AS [Standort-Konfiguration]
+FROM Vsa
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN StandKon ON Vsa.StandKonID = StandKon.ID
+JOIN [Status] AS Kundenstatus ON Kunden.[Status] = Kundenstatus.[Status] AND Kundenstatus.Tabelle = N'KUNDEN'
+JOIN [Status] AS VsaStatus ON Vsa.[Status] = VsaStatus.[Status] AND VsaStatus.Tabelle = N'VSA'
+WHERE StandKon.ID IN ($1$)
+  AND ((Kunden.Status = N'A' AND $2$ = 1) OR ($2$ = 0))
+  AND ((Vsa.Status = N'A' AND $3$ = 1) OR ($3$ = 0))
+  AND Kunden.AdrArtID = 1
+ORDER BY Kunden.KdNr ASC, Vsa.VsaNr ASC;
