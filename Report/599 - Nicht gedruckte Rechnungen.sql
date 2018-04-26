dbc48363db3fd785f1904b0b5f95d6aa8b7cfe9e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-04-26 13:38:22 +0200

    Kundenservice-Mitarbeiter hinzugefügt

--- Report/599 - Nicht gedruckte Rechnungen.sql
+++ Report/599 - Nicht gedruckte Rechnungen.sql
@@ -1,17 +1,31 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, KdGf.KurzBez AS SGF, RechKo.RechNr, Status.StatusBez$LAN$ AS Rechnungsstatus, RechKo.Art, RechKo.NettoWert, (
-  SELECT TOP 1 NettoWert
-  FROM RechKo FE
-  WHERE FE.KundenID = RechKo.KundenID
-    AND FE.AbteilID = RechKo.AbteilID
-    AND FE.FibuExpID > 0
-    AND FE.Art = 'R'
-  ORDER BY FE.ID DESC
-) AS [Letzter Fibu-Export]
-FROM RechKo, Kunden, KdGf, Status
-WHERE RechKo.KundenID = Kunden.ID
-  AND Kunden.KdGfID = KdGf.ID
-  AND RechKo.Status = Status.Status
-  AND Status.Tabelle = 'RECHKO'
-  AND KdGf.ID IN ($1$)
-  AND RechKo.Status < 'F'
+SELECT Kunden.KdNr, 
+  Kunden.SuchCode AS Kunde,
+  Kundenservice = (
+    SELECT TOP 1 Mitarbei.Name
+    FROM Mitarbei
+    JOIN KdBer ON KdBer.ServiceID = Mitarbei.ID
+    WHERE KdBer.KundenID = Kunden.ID
+    GROUP BY Mitarbei.Name
+    ORDER BY COUNT(Mitarbei.ID) DESC
+  ),
+  KdGf.KurzBez AS SGF, 
+  RechKo.RechNr, 
+  [Status].StatusBez$LAN$ AS Rechnungsstatus,
+  RechKo.Art,
+  RechKo.NettoWert,
+  [Letzter Fibu-Export] = (
+    SELECT TOP 1 NettoWert
+    FROM RechKo FE
+    WHERE FE.KundenID = RechKo.KundenID
+      AND FE.AbteilID = RechKo.AbteilID
+      AND FE.FibuExpID > 0
+      AND FE.Art = N'R'
+    ORDER BY FE.ID DESC
+  )
+FROM RechKo
+JOIN Kunden ON RechKo.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN [Status] ON RechKo.Status = [Status].[Status] AND [Status].Tabelle = N'RECHKO'
+WHERE KdGf.ID IN ($1$)
+  AND RechKo.[Status] < N'F'
 ORDER BY SGF, Kunden.KdNr, RechKo.RechNr;
