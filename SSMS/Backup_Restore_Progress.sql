1c82c97293613c5cf986c8fbd92631bfb4055de5
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-05-16 12:41:39 +0200

    Named statement text column

--- SSMS/Backup_Restore_Progress.sql
+++ SSMS/Backup_Restore_Progress.sql
@@ -5,5 +5,5 @@ CONVERT(NUMERIC(10,2),r.estimated_completion_time/1000.0/60.0) AS [ETA Min],
 CONVERT(NUMERIC(10,2),r.estimated_completion_time/1000.0/60.0/60.0) AS [ETA Hours],
 CONVERT(VARCHAR(1000),(SELECT SUBSTRING(text,r.statement_start_offset/2,
 CASE WHEN r.statement_end_offset = -1 THEN 1000 ELSE (r.statement_end_offset-r.statement_start_offset)/2 END)
-FROM sys.dm_exec_sql_text(sql_handle)))
+FROM sys.dm_exec_sql_text(sql_handle))) AS [Statement Text]
 FROM sys.dm_exec_requests r WHERE command IN ('RESTORE DATABASE','BACKUP DATABASE')
