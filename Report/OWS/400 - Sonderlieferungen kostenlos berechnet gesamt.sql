fbfa45352665c8b4a0dd2b699d361f2ba6246905
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-06-05 13:53:07 +0200

    Fehler korrigiert, durch den eine Lieferung kostenlos und berechnet gleichzeitig sein konnte

--- Report/OWS/400 - Sonderlieferungen kostenlos berechnet gesamt.sql
+++ Report/OWS/400 - Sonderlieferungen kostenlos berechnet gesamt.sql
@@ -5,60 +5,57 @@ SELECT $1$ AS DatumVon, $2$ AS DatumBis, Vsa.VsaNr, Vsa.Bez AS Vsa, COUNT(LsKo.I
 INTO #TmpTblAnzLs400
 FROM AnfKo, LsKo, Vsa
 WHERE AnfKo.LsKoID = LsKo.ID
-	AND LsKo.VsaID = Vsa.ID
-	AND LsKo.Datum BETWEEN $1$ AND $2$
-	AND AnfKo.Sonderfahrt = $TRUE$
+  AND LsKo.VsaID = Vsa.ID
+  AND LsKo.Datum BETWEEN $1$ AND $2$
+  AND AnfKo.Sonderfahrt = $TRUE$
 GROUP BY Vsa.VsaNr, Vsa.Bez;
 
 /* ### Sonderlieferungen zählen nur als berechnet, wenn die Anfahrpauschale manuell erfasst wurde ###
    ### AnfKo.LiefBerechArt muss gleich A sein und Anfahrpauschal-Artikel A00001 muss existieren   ### */
-	 
+   
 UPDATE AnzLs SET AnzLs.LiefKostenlos = a.Kostenlos
 FROM #TmpTblAnzLs400 AnzLs, (
-	SELECT Vsa.VsaNr, COUNT(DISTINCT LsKo.ID) AS Kostenlos
-	FROM AnfKo, LsKo, LsPo, Vsa
-	WHERE AnfKo.LsKoID = LsKo.ID
-		AND LsPo.LsKoID = LsKo.ID
-		AND LsKo.VsaID = Vsa.ID
-		AND LsKo.Datum BETWEEN $1$ AND $2$
-		AND AnfKo.Sonderfahrt = $TRUE$
-		AND (
-			(
-				NOT EXISTS (
-					SELECT Pos.*
-					FROM LsPo AS Pos
-					JOIN KdArti ON Pos.KdArtiID = KdArti.ID
-					JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-					WHERE Pos.LsKoID = LsKo.ID
-						AND Artikel.ArtikelNr = N'A00001'
-				)
-				AND AnfKo.LiefBerechArt <> N'A'
-			)
-			OR AnfKo.LiefBerechArt IS NULL
-		)
-	GROUP BY Vsa.VsaNr
+  SELECT Vsa.VsaNr, COUNT(DISTINCT LsKo.ID) AS Kostenlos
+  FROM AnfKo, LsKo, LsPo, Vsa
+  WHERE AnfKo.LsKoID = LsKo.ID
+    AND LsPo.LsKoID = LsKo.ID
+    AND LsKo.VsaID = Vsa.ID
+    AND LsKo.Datum BETWEEN $1$ AND $2$
+    AND AnfKo.Sonderfahrt = $TRUE$
+    AND NOT EXISTS (
+      SELECT Pos.*
+      FROM LsPo AS Pos
+      JOIN KdArti ON Pos.KdArtiID = KdArti.ID
+      JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+      WHERE Pos.LsKoID = LsKo.ID
+        AND Artikel.ArtikelNr = N'A00001'
+    )
+    AND ISNULL(AnfKo.LiefBerechArt, N'') <> N'A'
+  GROUP BY Vsa.VsaNr
 ) a
 WHERE AnzLs.VsaNr = a.VsaNr;
   
 UPDATE AnzLs SET AnzLs.LiefBerechnet = a.Berechnet
 FROM #TmpTblAnzLs400 AnzLs, (
-	SELECT Vsa.VsaNr, COUNT(DISTINCT LsKo.ID) AS Berechnet
-	FROM AnfKo, LsKo, LsPo, Vsa
-	WHERE AnfKo.LsKoID = LsKo.ID
-		AND LsPo.LsKoID = LsKo.ID
-		AND LsKo.VsaID = Vsa.ID
-		AND LsKo.Datum BETWEEN $1$ AND $2$
-		AND AnfKo.Sonderfahrt = $TRUE$
-		AND EXISTS (
-			SELECT Pos.*
-			FROM LsPo AS Pos
-			JOIN KdArti ON Pos.KdArtiID = KdArti.ID
-			JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-			WHERE Pos.LsKoID = LsKo.ID
-				AND Artikel.ArtikelNr = N'A00001'
-		)
-		AND AnfKo.LiefBerechArt = N'A'
-	GROUP BY Vsa.VsaNr
+  SELECT Vsa.VsaNr, COUNT(DISTINCT LsKo.ID) AS Berechnet
+  FROM AnfKo, LsKo, LsPo, Vsa
+  WHERE AnfKo.LsKoID = LsKo.ID
+    AND LsPo.LsKoID = LsKo.ID
+    AND LsKo.VsaID = Vsa.ID
+    AND LsKo.Datum BETWEEN $1$ AND $2$
+    AND AnfKo.Sonderfahrt = $TRUE$
+    AND (
+      EXISTS (
+        SELECT Pos.*
+        FROM LsPo AS Pos
+        JOIN KdArti ON Pos.KdArtiID = KdArti.ID
+        JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+        WHERE Pos.LsKoID = LsKo.ID
+          AND Artikel.ArtikelNr = N'A00001'
+      )
+      OR AnfKo.LiefBerechArt = N'A'
+    )
+  GROUP BY Vsa.VsaNr
 ) a
 WHERE AnzLs.VsaNr = a.VsaNr;
   
--- Report/OWS/400a - Sonderlieferungen je Kostenstelle und Artikel.sql
+++ Report/OWS/400a - Sonderlieferungen je Kostenstelle und Artikel.sql
@@ -5,59 +5,56 @@ SELECT $1$ AS [Datum von], $2$ AS [Datum bis], Abteil.Abteilung AS KsSt, Abteil.
 INTO #TmpTblAnzLs400a
 FROM AnfKo, LsKo, LsPo, KdArti, Artikel, Abteil
 WHERE AnfKo.LsKoID = LsKo.ID
-	AND LsPo.LsKoID = LsKo.ID
+  AND LsPo.LsKoID = LsKo.ID
   AND LsPo.KdArtiID = KdArti.ID
   AND KdArti.ArtikelID = Artikel.ID
   AND LsPo.AbteilID = Abteil.ID
-	AND LsKo.Datum BETWEEN $1$ AND $2$
-	AND AnfKo.Sonderfahrt = 1
+  AND LsKo.Datum BETWEEN $1$ AND $2$
+  AND AnfKo.Sonderfahrt = 1
 GROUP BY Abteil.Abteilung, Abteil.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez, Abteil.ID, LsPo.KdArtiID, Artikel.ID, AnfKo.VsaID;
 
 UPDATE AnzLs SET AnzLs.[Kostenlose Lieferungen] = a.Kostenlos
 FROM #TmpTblAnzLs400a AnzLs, (
-	SELECT LsPo.AbteilID, LsPo.KdArtiID, COUNT(DISTINCT LsKo.ID) AS Kostenlos
-	FROM AnfKo, LsKo, LsPo
-	WHERE AnfKo.LsKoID = LsKo.ID
-		AND LsPo.LsKoID = LsKo.ID
-		AND LsKo.Datum BETWEEN $1$ AND $2$
-		AND AnfKo.Sonderfahrt = 1
-		AND (
-			(
-				NOT EXISTS (
-					SELECT Pos.*
-					FROM LsPo AS Pos
-					JOIN KdArti ON Pos.KdArtiID = KdArti.ID
-					JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-					WHERE Pos.LsKoID = LsKo.ID
-						AND Artikel.ArtikelNr = N'A00001'
-				)
-				AND AnfKo.LiefBerechArt <> N'A'
-			)
-			OR AnfKo.LiefBerechArt IS NULL
-		)
-	GROUP BY LsPo.AbteilID, LsPo.KdArtiID
+  SELECT LsPo.AbteilID, LsPo.KdArtiID, COUNT(DISTINCT LsKo.ID) AS Kostenlos
+  FROM AnfKo, LsKo, LsPo
+  WHERE AnfKo.LsKoID = LsKo.ID
+    AND LsPo.LsKoID = LsKo.ID
+    AND LsKo.Datum BETWEEN $1$ AND $2$
+    AND AnfKo.Sonderfahrt = 1
+    AND NOT EXISTS (
+      SELECT Pos.*
+      FROM LsPo AS Pos
+      JOIN KdArti ON Pos.KdArtiID = KdArti.ID
+      JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+      WHERE Pos.LsKoID = LsKo.ID
+        AND Artikel.ArtikelNr = N'A00001'
+    )
+    AND ISNULL(AnfKo.LiefBerechArt, N'') <> N'A'
+  GROUP BY LsPo.AbteilID, LsPo.KdArtiID
 ) a
 WHERE AnzLs.AbteilID = a.AbteilID
   AND AnzLs.KdArtiID = a.KdArtiID;
   
 UPDATE AnzLs SET AnzLs.[Berechnete Lieferungen] = a.Berechnet
 FROM #TmpTblAnzLs400a AnzLs, (
-	SELECT LsPo.AbteilID, LsPo.KdArtiID, COUNT(DISTINCT LsKo.ID) AS Berechnet
-	FROM AnfKo, LsKo, LsPo
-	WHERE AnfKo.LsKoID = LsKo.ID
-		AND LsPo.LsKoID = LsKo.ID
-		AND LsKo.Datum BETWEEN $1$ AND $2$
-		AND AnfKo.Sonderfahrt = 1
-		AND EXISTS (
-			SELECT Pos.*
-			FROM LsPo AS Pos
-			JOIN KdArti ON Pos.KdArtiID = KdArti.ID
-			JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-			WHERE Pos.LsKoID = LsKo.ID
-				AND Artikel.ArtikelNr = N'A00001'
-		)
-		AND AnfKo.LiefBerechArt = N'A'
-	GROUP BY LsPo.AbteilID, LsPo.KdArtiID
+  SELECT LsPo.AbteilID, LsPo.KdArtiID, COUNT(DISTINCT LsKo.ID) AS Berechnet
+  FROM AnfKo, LsKo, LsPo
+  WHERE AnfKo.LsKoID = LsKo.ID
+    AND LsPo.LsKoID = LsKo.ID
+    AND LsKo.Datum BETWEEN $1$ AND $2$
+    AND AnfKo.Sonderfahrt = 1
+     AND (
+      EXISTS (
+        SELECT Pos.*
+        FROM LsPo AS Pos
+        JOIN KdArti ON Pos.KdArtiID = KdArti.ID
+        JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+        WHERE Pos.LsKoID = LsKo.ID
+          AND Artikel.ArtikelNr = N'A00001'
+      )
+      OR AnfKo.LiefBerechArt = N'A'
+    )
+  GROUP BY LsPo.AbteilID, LsPo.KdArtiID
 ) a
 WHERE AnzLs.AbteilID = a.AbteilID
   AND AnzLs.KdArtiID = a.KdArtiID;
@@ -67,7 +64,7 @@ FROM #TmpTblAnzLs400a AnzLs, (
   SELECT OPTeile.ArtikelID, OPTeile.VsaID, COUNT(OPTeile.ID) AS Anz
   FROM OPTeile
   WHERE OPTeile.Status IN (N'A', N'Q')
-		AND OPTeile.LastActionsID = 102
+    AND OPTeile.LastActionsID = 102
     AND OPTeile.VsaID IN (SELECT VsaID FROM #TmpTblAnzLs400a)
   GROUP BY OPTeile.ArtikelID, OPTeile.VsaID
 ) x
