bb132416fd0da0488249a1b50d89e227db12a43a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-06-14 09:48:27 +0200

    Teile-Barcodes verfälschen

--- Diverse/SALI - Barcodes im Nummernkreis 2000000000.sql
+++ Diverse/SALI - Barcodes im Nummernkreis 2000000000.sql
@@ -1,4 +1,5 @@
 SELECT Teile.Barcode, Status.StatusBez AS Teilestatus, Kunden.KdNr, Kunden.SuchCode, Standort.Bez AS Lagerstandort, Teile.PatchDatum, Teile.Eingang1, Teile.Ausgang1
+--UPDATE Teile SET Barcode = RTRIM(Barcode) + N'*SAL'
 FROM Teile
 JOIN Vsa ON Teile.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -6,7 +7,9 @@ JOIN LagerArt ON Teile.LagerArtID = LagerArt.ID
 JOIN Standort ON LagerArt.LagerID = Standort.ID
 JOIN Status ON Teile.Status = Status.Status AND Status.Tabelle = N'TEILE'
 WHERE Teile.Status IN (N'L', N'LM', N'M', N'N', N'O', N'Q', N'S', N'T', N'U', N'W')
-  AND Teile.Barcode LIKE N'2_________';
+  AND Teile.Barcode LIKE N'2_________'
+  AND Teile.PatchDatum <= N'2014-12-31'
+  AND ISNULL(Teile.Eingang1, N'1980-01-01') <= N'2016-12-31';
 
 SELECT Teile.Barcode, Status.StatusBez AS Teilestatus, Kunden.KdNr, Kunden.SuchCode, Standort.Bez AS Lagerstandort, Teile.PatchDatum, Teile.Eingang1, Teile.Ausgang1
 FROM Teile
@@ -15,7 +18,7 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN LagerArt ON Teile.LagerArtID = LagerArt.ID
 JOIN Standort ON LagerArt.LagerID = Standort.ID
 JOIN Status ON Teile.Status = Status.Status AND Status.Tabelle = N'TEILE'
-WHERE Teile.Status NOT IN (N'L', N'LM', N'M', N'N', N'O', N'Q', N'S', N'T', N'U', N'W')
+WHERE Teile.Status NOT IN (N'L', N'LM', N'M', N'N', N'O', N'Q', N'S', N'T', N'U', N'W', N'X')
   AND Teile.Barcode LIKE N'2_________';
 
 /* #### erledigt 08.06.2018 ####
@@ -26,8 +29,18 @@ WHERE Teile.Status NOT IN (N'L', N'LM', N'M', N'N', N'O', N'Q', N'S', N'T', N'U'
 */
 
 -- TODO: Teile mit Status X extra behandeln - Lagerteile!
+DECLARE @Lagerteile TABLE (
+  TeileLagID int,
+  TeileID int
+);
 
-SELECT TeileLag.*
+INSERT INTO @Lagerteile
+SELECT TeileLag.ID AS TeileLagID, Teile.ID AS TeileID --, TeileLag.Barcode, TeileLag.Status, TeileLag.ErstWoche, TeileLag.Ausdienst, Teile.PatchDatum, Teile.Eingang1, Teile.Ausgang1
 FROM TeileLag
---UPDATE TeileLag SET Barcode = RTRIM(Barcode) + N'*SAL'
-WHERE TeileLag.Barcode LIKE N'2_________';
+LEFT OUTER JOIN Teile ON Teile.Barcode = Teilelag.Barcode AND Teile.Status = N'X'
+WHERE TeileLag.Barcode LIKE N'2_________'
+  AND (Teile.PatchDatum < N'2018-04-01' OR Teile.PatchDatum IS NULL)
+  AND TeileLag.Status <> N'R';
+
+UPDATE TeileLag SET Barcode = RTRIM(Barcode) + N'*SAL' WHERE ID IN (SELECT DISTINCT TeileLagID FROM @Lagerteile WHERE TeileLagID IS NOT NULL);
+UPDATE Teile SET Barcode = RTRIM(Barcode) + N'*SAL' WHERE ID IN (SELECT DISTINCT TeileID FROM @Lagerteile WHERE TeileID IS NOT NULL);
