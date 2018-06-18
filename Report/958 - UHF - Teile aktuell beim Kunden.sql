074eff9e129c77e56f7266ab5198dfea287d130c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-06-18 07:49:00 +0200

    - Holding-Auswahl hinzugefügt - Schwundteile-Auswahl hinzugefügt - Kennzeichen hinzugefügt, ob Schwundteil bereits dem Kunden verrechnet wurde

--- Report/958 - UHF - Teile aktuell beim Kunden.sql
+++ Report/958 - UHF - Teile aktuell beim Kunden.sql
@@ -1,12 +1,19 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS Vsa, OPTeile.Code AS Chipcode, REPLACE(Actions.ActionsBez$LAN$, N'OP ', N'') AS [Letzte Aktion], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Bereich.BereichBez$LAN$ AS Produktbereich, OPTeile.LastScanTime AS [Letzter Scan-Zeitpunkt], OPTeile.LastScanToKunde AS [Letzter Auslese-Zeitpunkt]
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS Vsa, OPTeile.Code AS Chipcode, [Status].StatusBez$LAN$ AS [Status des Teils], REPLACE(Actions.ActionsBez$LAN$, N'OP ', N'') AS [Letzte Aktion], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Bereich.BereichBez$LAN$ AS Produktbereich, OPTeile.LastScanTime AS [Letzter Scan-Zeitpunkt], OPTeile.LastScanToKunde AS [Letzter Auslese-Zeitpunkt], CAST(IIF(OPTeile.RechPoID > 0 AND [Status].[Status] = N'W', 1, 0) AS bit) AS [Teil bereits Schwundverrechnet?]
 FROM OPTeile
 JOIN Vsa ON OPTeile.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
 JOIN Bereich ON Artikel.BereichID = Bereich.ID
 JOIN Actions ON OPTeile.LastActionsID = Actions.ID
-WHERE Kunden.ID = $ID$
-  AND Bereich.ID = $2$
-  AND OPTeile.Status = N'Q' -- nur aktive Teile
-  AND OPTeile.LastActionsID = 102 -- nur Teile, die zuletzt ausgelesen und somit beim Kunden sind
-;
+JOIN [Status] ON OPTeile.[Status] = [Status].[Status] AND Tabelle = N'OPTEILE'
+WHERE (
+    (Kunden.ID = $ID$ AND $1$ < 0)
+    OR
+    (Kunden.HoldingID = $1$ AND $1$ <> 0)
+  )
+  AND Bereich.ID IN ($2$)
+  AND (
+    (OPTeile.Status IN (N'Q', N'W') AND OPTeile.LastActionsID IN (102, 116) AND $3$ = 1)
+    OR
+    (OPTeile.Status = N'Q' AND OPTeile.LastActionsID = 102 AND $3$ = 0)
+  );
