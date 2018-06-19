6ca7a2fbd0ef0e4c160556dd1deef61074a2d75b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-06-19 11:54:44 +0200

    - Fehler korrigiert, durch den alle Träger angezeigt wurden, die irgendein Teil haben, das vor dem eingegenen Datum nicht mehr gescannt wurde. Es werden jetzt nur noch Träger angezeigt, bei denen ALLE Teile zuletzt vor dem Datum gescannt wurden. - Traeger.ID hinzugefügt, damit direkter Sprung zum Träger aus der Auswertung heraus möglich ist.

--- Report/955 - Träger ohne Drehung seit Datum.sql
+++ Report/955 - Träger ohne Drehung seit Datum.sql
@@ -1,13 +1,19 @@
-DECLARE @beforDate date = $2$;
+DECLARE @beforeDate date = $2$;
 
 WITH CTE_AltTeile AS (
   SELECT Teile.*
   FROM Teile
   WHERE Teile.Status IN (N'Q', N'S', N'T', N'U', N'W')
-    AND (Teile.Ausgang1 < @beforDate OR Teile.Ausgang1 IS NULL)
-    AND (Teile.Eingang1 < @beforDate OR Teile.Eingang1 IS NULL)
+    AND (Teile.Ausgang1 < @beforeDate OR Teile.Ausgang1 IS NULL)
+    AND (Teile.Eingang1 < @beforeDate OR Teile.Eingang1 IS NULL)
+    AND NOT EXISTS (
+      SELECT x.*
+      FROM Teile AS x
+      WHERE x.TraegerID = Teile.TraegerID
+        AND (x.Eingang1 >= @beforeDate OR x.Ausgang1 >= @beforeDate)
+    )
 )
-SELECT Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.Bez AS Vsa, Traeger.Traeger, [Status].StatusBez AS TraegerStatus, Traeger.Nachname, Traeger.Vorname, ISNULL(Traeger.Indienst, N'') AS [Zeitraum von], ISNULL(Traeger.Ausdienst, N'') AS [Zeitraum bis], MAX(IIF(ISNULL(CTE_AltTeile.Eingang1, N'1980-01-01') > ISNULL(CTE_AltTeile.Ausgang1, N'1980-01-01'), CTE_AltTeile.Eingang1, CTE_AltTeile.Ausgang1)) AS [letzte Teile-Bewegung]
+SELECT Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.Bez AS Vsa, Traeger.ID AS TraegerID, Traeger.Traeger, [Status].StatusBez AS TraegerStatus, Traeger.Nachname, Traeger.Vorname, ISNULL(Traeger.Indienst, N'') AS [Zeitraum von], ISNULL(Traeger.Ausdienst, N'') AS [Zeitraum bis], MAX(IIF(ISNULL(CTE_AltTeile.Eingang1, N'1980-01-01') > ISNULL(CTE_AltTeile.Ausgang1, N'1980-01-01'), CTE_AltTeile.Eingang1, CTE_AltTeile.Ausgang1)) AS [letzte Teile-Bewegung]
 FROM Traeger
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -15,5 +21,5 @@ JOIN [Status] ON Traeger.[Status] = [Status].[Status] AND [Status].Tabelle = N'T
 JOIN CTE_AltTeile ON CTE_AltTeile.TraegerID = Traeger.ID
 WHERE Kunden.ID = $1$
   AND Traeger.Status <> N'I'
-GROUP BY Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.Bez, Traeger.Traeger, [Status].StatusBez, Traeger.Nachname, Traeger.Vorname, Traeger.Indienst, Traeger.Ausdienst
+GROUP BY Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.Bez, Traeger.ID, Traeger.Traeger, [Status].StatusBez, Traeger.Nachname, Traeger.Vorname, Traeger.Indienst, Traeger.Ausdienst
 ORDER BY [letzte Teile-Bewegung] ASC;
