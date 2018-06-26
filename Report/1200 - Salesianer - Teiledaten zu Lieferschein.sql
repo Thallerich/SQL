eae67e6cc16588c283907b7ec284acd3c7bcddb3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-06-26 13:05:33 +0200

    documentation

--- Report/1200 - Salesianer - Teiledaten zu Lieferschein.sql
+++ Report/1200 - Salesianer - Teiledaten zu Lieferschein.sql
@@ -1,3 +1,9 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Pipeline: LSTeile                                                                                                         ++ */
+/* ++                                                                                                                           ++ */
+/* ++ Author: Stefan Thaller - 2018-06-26                                                                                       ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 SELECT Traeger.Traeger, IIF(Traeger.Vorname IS NULL, N'', Traeger.Vorname + N' ') + ISNULL(Traeger.Nachname, N'') AS TraegerName, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, Teile.Barcode, CAST(Scans.[DateTime] AS date) AS Scandatum, Traeger.SchrankInfo
 FROM Scans
 JOIN Teile ON Scans.TeileID = Teile.ID
