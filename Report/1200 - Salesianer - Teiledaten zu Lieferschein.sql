784e3730c3f600966f3cbec452f5487ffba68ed1
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-06-26 12:47:28 +0200

    Anlage zu Salesianer-LS für Bekleidung erstellt

---
+++ Report/1200 - Salesianer - Teiledaten zu Lieferschein.sql
@@ -0,0 +1,10 @@
+SELECT Traeger.Traeger, IIF(Traeger.Vorname IS NULL, N'', Traeger.Vorname + N' ') + ISNULL(Traeger.Nachname, N'') AS TraegerName, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, Teile.Barcode, CAST(Scans.[DateTime] AS date) AS Scandatum, Traeger.SchrankInfo
+FROM Scans
+JOIN Teile ON Scans.TeileID = Teile.ID
+JOIN ArtGroe ON Teile.ArtGroeID = ArtGroe.ID
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+JOIN Traeger ON Teile.TraegerID = Traeger.ID
+JOIN LsPo ON Scans.LsPoID = LsPo.ID
+JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+WHERE LsKo.LsNr = $1$
+ORDER BY Traeger, TraegerName, ArtikelNr, Groesse, Barcode;
