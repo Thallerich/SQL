f7f7da99577c61e701728288606d21130e65b6ac
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-07-05 13:41:16 +0200

    - reporting for june 2018 done

--- Diverse/Controlling - Waschlohn-Berechnung.sql
+++ Diverse/Controlling - Waschlohn-Berechnung.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS #ResultWLohnStueck;
 GO
 
 DECLARE @FirmaID int = (SELECT Firma.ID FROM Firma WHERE Firma.SuchCode = N'WM');
-DECLARE @DatumVon date = CAST(N'2018-05-01' AS date);
-DECLARE @DatumBis date = CAST(N'2018-05-31' AS date);
+DECLARE @DatumVon date = CAST(N'2018-06-01' AS date);
+DECLARE @DatumBis date = CAST(N'2018-06-30' AS date);
 DECLARE @BerufsgruppeID int = (SELECT CAST(Settings.ValueMemo AS int) FROM Settings WHERE Settings.Parameter = N'ID_ARTIKEL_BERUFSGRUPPE');
 
 --* Debug: Netto-Summe der Rechnungen
