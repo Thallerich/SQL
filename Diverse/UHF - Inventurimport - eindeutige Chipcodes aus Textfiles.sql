bb145857ded892d146ed5aebdd38c321241d7b36
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-07-30 14:01:44 +0200

    changed to mass process all files at the location

--- Diverse/UHF - Inventurimport - eindeutige Chipcodes aus Textfiles.sql
+++ Diverse/UHF - Inventurimport - eindeutige Chipcodes aus Textfiles.sql
@@ -1,14 +1,43 @@
+DECLARE @Path nvarchar(100) = N'D:\AdvanTex\Temp\uhf\';
 DECLARE @Filename nvarchar(100);
+DECLARE @Command nvarchar(150);
 DECLARE @BulkSQL nvarchar(max);
 
-SET @Filename = N'all-2301_2018-04-11-999999.txt';
+DECLARE @Files TABLE (
+  [Filename] nvarchar(100)
+);
+
+DROP TABLE IF EXISTS #Inventur;
 
 CREATE TABLE #Inventur (
   Chipcode nvarchar(100) COLLATE Latin1_General_CS_AS
 );
 
-SET @BulkSQL = N'BULK INSERT #Inventur FROM N''D:\AdvanTex\Temp\uhf\' + @Filename + '''WITH (FIELDTERMINATOR = N''\r'', ROWTERMINATOR = N''\n'');';
-EXEC(@BulkSQL);
+SET @Command = 'dir ' + @Path + '*.txt /b';
+
+INSERT INTO @Files
+EXEC master..xp_cmdShell @Command;
+
+DELETE FROM @Files WHERE [Filename] IS NULL;
+
+DECLARE Files CURSOR FOR
+  SELECT [Filename]
+  FROM @Files;
+
+OPEN Files;
+
+FETCH NEXT FROM Files INTO @Filename;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+  SET @BulkSQL = N'BULK INSERT #Inventur FROM ''' + @Path + @Filename + '''WITH (FIELDTERMINATOR = N''\r'', ROWTERMINATOR = N''\n'');';
+  EXEC(@BulkSQL);
+
+  FETCH NEXT FROM Files INTO @Filename;
+END;
+
+CLOSE Files;
+DEALLOCATE Files;
 
 SELECT DISTINCT Chipcode FROM #Inventur;
 
