4170cc22d64d558013623649a7443b51782a7b1f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-08-07 10:32:45 +0200

    - refactored query - added fields for IT-17603

--- Rechnungsanlagen/BewohnerHeimanteil_Netto.sql
+++ Rechnungsanlagen/BewohnerHeimanteil_Netto.sql
@@ -1,30 +1,77 @@
-BEGIN TRY
-  DROP TABLE #TmpLsPoSum;
-END TRY
-BEGIN CATCH
-END CATCH;
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Rechnungsanlage Bewohner / Heimanteile netto                                                                              ++ */
+/* ++ Version 2.0                                                                                                               ++ */
+/* ++                                                                                                                           ++ */
+/* ++ Author: Stefan Thaller - 2018-08-07                                                                                       ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-SELECT Abteil.Abteilung, Abteil.Bez AS KoStBez, Abteil.ID AS KoStID,
-Traeger.Nachname, Traeger.Vorname, Traeger.Traeger, Traeger.PersNr AS ZimmerNr,
-BewAbr.ServiceProz, KdBer.RabattWasch, BewKdAr.ProzTraeger,
-LsPo.ID, LsPo.Menge * LsPo.EPreis * (1 - (KdBer.RabattWasch/100)) AS PosSumme, LsPo.Menge * LsPo.EPreis * (1 - (KdBer.RabattWasch/100)) * ProzTraeger/100 AS AntBew, LsPo.Menge * LsPo.EPreis * (1 - (KdBer.RabattWasch/100)) * (1 - (ProzTraeger/100)) AS AntHeim,
-KdBer.BereichID
-INTO #TmpLsPoSum
-FROM LsPo, LsKo, RechPo, RechKo, Abteil, Traeger, BewAbr, KdBer, KdArti
-LEFT OUTER JOIN BewKdAr ON (BewKdAr.BewAbrID = BewAbrID AND BewKdAr.KdArtiID = KdArti.ID)
-WHERE LsPo.LsKoID = LsKo.ID
-	AND LsPo.RechPoID = RechPo.ID
-	AND RechPo.RechKoID = RechKo.ID
-	AND RechKo.ID = $RECHKOID$
-	AND LsPo.AbteilID = Abteil.ID
-	AND LsKo.TraegerID = Traeger.ID
-	AND Traeger.BewAbrID = BewAbr.ID
-	AND LsPo.KdArtiID = KdArti.ID
-	AND KdArti.KdBerID = KdBer.ID;
+DECLARE @LsPoSumme TABLE (
+  Kostenstelle nchar(15),
+  KostenstelleBez nvarchar(80),
+  KostenstelleID int,
+  Nachname nchar(25),
+  Vorname nchar(20),
+  TraegerNr nchar(8),
+  ZimmerNr nchar(10),
+  ServiceProzent numeric(7,4),
+  RabattBearbeitung numeric(7,4),
+  ProzentTraeger numeric(7,4),
+  LsPoID int,
+  LsPoSumme smallmoney,
+  AnteilTraeger smallmoney,
+  AnteilHeim smallmoney,
+  Jahr nchar(4),
+  Monat nchar(2),
+  RechNr int
+);
 
-SELECT LPS.Abteilung, LPS.KoStBez, LPS.KoStID, LPS.Nachname, LPS.Vorname, LPS.ZimmerNr, LPS.Traeger, LPS.RabattWasch, LPS.ServiceProz, SUM(LPS.PosSumme) AS NettoPreis, SUM(LPS.PosSumme) * 1.2 AS BruttoPreis, SUM(LPS.AntBew) AS NettoTraeger, SUM(LPS.AntBew) * 1.2 AS BruttoTraeger, SUM(LPS.AntHeim) AS NettoHeim, SUM(LPS.AntHeim) * 1.2 AS BruttoHeim
-FROM #TmpLsPoSum LPS, Bereich
-WHERE LPS.BereichID = Bereich.ID
-	AND Bereich.Bereich = 'CT'
-GROUP BY LPS.Abteilung, LPS.KoStBez, LPS.KoStID, LPS.Nachname, LPS.Vorname, LPS.ZimmerNr, LPS.Traeger, LPS.RabattWasch, LPS.ServiceProz
+INSERT INTO @LsPoSumme
+SELECT Abteil.Abteilung AS Kostenstelle,
+  Abteil.Bez AS KostenstelleBez,
+  Abteil.ID AS KostenstelleID,
+  Traeger.Nachname,
+  Traeger.Vorname,
+  Traeger.Traeger AS TraegerNr,
+  Traeger.PersNr AS ZimmerNr,
+  BewAbr.ServiceProz AS ServiceProzent,
+  KdBer.RabattWasch AS RabattBearbeitung,
+  BewKdAr.ProzTraeger AS ProzentTraeger,
+  LsPo.ID AS LsPoID,
+  LsPo.Menge * LsPo.EPreis * (1 - (KdBer.RabattWasch/100)) AS LsPoSumme,
+  LsPo.Menge * LsPo.EPreis * (1 - (KdBer.RabattWasch/100)) * ProzTraeger/100 AS AnteilTraeger,
+  LsPo.Menge * LsPo.EPreis * (1 - (KdBer.RabattWasch/100)) * (1 - (ProzTraeger/100)) AS AnteilHeim,
+  FORMAT(RechKo.RechDat, N'yyyy') AS Jahr,
+  FORMAT(RechKo.RechDat, N'MM') AS Monat,
+  RechKo.RechNr
+FROM LsPo
+JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+JOIN RechPo ON LsPo.RechPoID = RechPo.ID
+JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+JOIN Abteil ON LsPo.AbteilID = Abteil.ID
+JOIN Traeger ON LsKo.TraegerID = Traeger.ID
+JOIN BewAbr ON Traeger.BewAbrID = BewAbr.ID
+JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
+JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+JOIN Bereich ON KdBer.BereichID = Bereich.ID
+LEFT OUTER JOIN BewKdAr ON BewKdAr.BewAbrID = BewAbr.ID AND BewKdAr.KdArtiID = KdArti.ID
+WHERE RechKo.ID = $RECHKOID$
+  AND Bereich.Altenheim = 1;
+
+SELECT LPS.Jahr,
+  LPS.Monat,
+  LPS.RechNr,
+  LPS.Kostenstelle,
+  LPS.KostenstelleBez,
+  LPS.Nachname,
+  LPS.Vorname,
+  LPS.ZimmerNr,
+  LPS.TraegerNr,
+  SUM(LPS.LsPoSumme) AS NettoPreis,
+  SUM(LPS.LsPoSumme) * 1.2 AS BruttoPreis,
+  SUM(LPS.AnteilTraeger) AS NettoTraeger,
+  SUM(LPS.AnteilTraeger) * 1.2 AS BruttoTraeger,
+  SUM(LPS.AnteilHeim) AS NettoHeim,
+  SUM(LPS.AnteilHeim) * 1.2 AS BruttoHeim
+FROM @LsPoSumme AS LPS
+GROUP BY LPS.Jahr, LPS.Monat, LPS.RechNr, LPS.Kostenstelle, LPS.KostenstelleBez, LPS.KostenstelleID, LPS.Nachname, LPS.Vorname, LPS.ZimmerNr, LPS.TraegerNr, LPS.RabattBearbeitung, LPS.ServiceProzent
 ORDER BY Nachname, Vorname;
