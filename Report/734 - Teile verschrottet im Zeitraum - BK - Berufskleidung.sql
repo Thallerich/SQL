aa752648885229f781c2c48b8b016cb23788abcc
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-08-09 14:19:48 +0200

    - optimizations - added customer service location and production location

--- Report/734 - Teile verschrottet im Zeitraum - BK - Berufskleidung.sql
+++ Report/734 - Teile verschrottet im Zeitraum - BK - Berufskleidung.sql
@@ -1,50 +1,50 @@
-BEGIN TRY
-  DROP TABLE #TmpErgebnis;
-END TRY
-BEGIN CATCH
-END CATCH;
+DROP TABLE IF EXISTS #TmpErgebnis734;
 
-SELECT WegGrund.WegGrundBez$LAN$ AS Grund, Teile.Barcode, Status.StatusBez AS Status, Teile.ErstWoche, Teile.ErstDatum, Teile.PatchDatum, Teile.Ausdienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Kunden.KdNr, Kunden.SuchCode AS Kunde, KdGf.KurzBez AS SGF, Teile.AusdRestw AS RestWert
-INTO #TmpErgebnis
-FROM Teile, Vsa, Kunden, Status, Artikel, WegGrund, KdGf, Bereich
-WHERE Teile.VsaID = Vsa.ID
-	AND Vsa.KundenID = Kunden.ID
-	AND Kunden.KdGfID = KdGf.ID
-	AND Kunden.KdGfID IN ($4$)
-	AND Teile.Status = Status.Status
-	AND Status.Tabelle = 'TEILE'
-	AND Teile.ArtikelID = Artikel.ID
-	AND Teile.Status = 'Y'
-	AND Teile.WegGrundID = WegGrund.ID
+SELECT WegGrund.WegGrundBez$LAN$ AS Grund, Teile.Barcode, [Status].StatusBez AS [Status], Teile.ErstWoche, Teile.ErstDatum, Teile.PatchDatum, Teile.Ausdienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS SGF, Teile.AusdRestw AS RestWert
+INTO #TmpErgebnis734
+FROM Teile
+JOIN Vsa ON Teile.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN [Status] ON  Teile.[Status] = [Status].[Status] AND [Status].Tabelle = 'TEILE'
+JOIN Artikel ON Teile.ArtikelID = Artikel.ID
+JOIN WegGrund ON Teile.WegGrundID = WegGrund.ID
+JOIN Bereich ON Artikel.BereichID = Bereich.ID
+JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND Bereich.ID = StandBer.BereichID
+JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
+JOIN Standort AS Kundenservice ON Kunden.StandortID = Kundenservice.ID
+  AND Kunden.KdGfID IN ($4$)
+  AND Teile.[Status] = 'Y'
   AND WegGrund.ID IN ($3$)
-	AND Teile.AusDienstDat BETWEEN $1$ AND $2$
-	AND Artikel.BereichID = Bereich.ID
-	AND Bereich.ID IN ($5$);
+  AND Teile.AusDienstDat BETWEEN $1$ AND $2$
+  AND Bereich.ID IN ($5$);
 
-INSERT INTO #TmpErgebnis
-SELECT WegGrund.WegGrundBez$LAN$ AS Grund, TeileLag.Barcode, Status.StatusBez AS Status, TeileLag.ErstWoche, TeileLag.ErstDatum, CONVERT(date, NULL) AS PatchDatum, TeileLag.AusDienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS ArtikelBez, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Kunden.KdNr, Kunden.SuchCode AS Kunde, KdGf.KurzBez AS SGF, TeileLag.Restwert AS RestWert
-FROM TeileLag, Status, ArtGroe, Artikel, WegGrund, Traeger, Vsa, Kunden, KdGf, Bereich
-WHERE TeileLag.Status = Status.Status
-	AND Status.Tabelle = 'TEILELAG'
-	AND TeileLag.Status = 'Y'
-	AND TeileLag.ArtGroeID = ArtGroe.ID
-	AND ArtGroe.ArtikelID = Artikel.ID
-	AND Artikel.BereichID = Bereich.ID
-	AND Bereich.ID IN ($5$)
-	AND TeileLag.WegGrundID = WegGrund.ID
+INSERT INTO #TmpErgebnis734
+SELECT WegGrund.WegGrundBez$LAN$ AS Grund, TeileLag.Barcode, [Status].StatusBez AS [Status], TeileLag.ErstWoche, TeileLag.ErstDatum, CONVERT(date, NULL) AS PatchDatum, TeileLag.AusDienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS ArtikelBez, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS SGF, TeileLag.Restwert AS RestWert
+FROM TeileLag
+JOIN [Status] ON TeileLag.[Status] = [Status].[Status] AND [Status].Tabelle = 'TEILELAG'
+JOIN ArtGroe ON TeileLag.ArtGroeID = ArtGroe.ID
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+JOIN Bereich ON Artikel.BereichID = Bereich.ID
+JOIN WegGrund ON TeileLag.WegGrundID = WegGrund.ID
+JOIN Traeger ON TeileLag.TraegerID = Traeger.ID
+JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND Bereich.ID = StandBer.BereichID
+JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
+JOIN Standort AS Kundenservice ON Kunden.StandortID = Kundenservice.ID
+WHERE TeileLag.[Status] = 'Y'
+  AND Kunden.KdGfID IN ($4$)
+  AND Bereich.ID IN ($5$)
   AND WegGrund.ID IN ($3$)
-	AND TeileLag.AusDienstDat BETWEEN $1$ AND $2$
-	AND TeileLag.TraegerID = Traeger.ID
-	AND Traeger.VsaID = Vsa.ID
-	AND Vsa.KundenID = Kunden.ID
-	AND Kunden.KdGfID = KdGf.ID
-	AND Kunden.KdGfID IN ($4$)
+  AND TeileLag.AusDienstDat BETWEEN $1$ AND $2$
   AND NOT EXISTS (
     SELECT Ergebnis.Barcode
-    FROM #TmpErgebnis Ergebnis
+    FROM #TmpErgebnis734 Ergebnis
     WHERE Ergebnis.Barcode = TeileLag.Barcode
   );
 
 SELECT *
-FROM #TmpErgebnis
+FROM #TmpErgebnis734
 ORDER BY Kdnr, VsaNr, ArtikelNr;
--- Report/734a - BK-Teile verschrottet - Standort.sql
+++ Report/734a - BK-Teile verschrottet - Standort.sql
@@ -1,47 +1,49 @@
-DROP TABLE IF EXISTS #TmpErgebnis;
+DROP TABLE IF EXISTS #TmpErgebnis734a;
 
-SELECT WegGrund.WegGrundBez$LAN$ AS Schrottgrund, Teile.Barcode, Status.StatusBez AS Status, Teile.ErstWoche, Teile.ErstDatum, Teile.PatchDatum, Teile.Ausdienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Kunden.KdNr, Kunden.SuchCode AS Kunde, Standort.Bez AS Standort, Teile.AusdRestw AS RestWert
-INTO #TmpErgebnis
-FROM Teile, Vsa, Kunden, Status, Artikel, WegGrund, Standort, Bereich
-WHERE Teile.VsaID = Vsa.ID
-	AND Vsa.KundenID = Kunden.ID
-	AND Kunden.StandortID = Standort.ID
-	AND Standort.ID IN ($4$)
-	AND Teile.Status = Status.Status
-	AND Status.Tabelle = 'TEILE'
-	AND Teile.ArtikelID = Artikel.ID
-	AND Teile.Status = 'Y'
-	AND Teile.WegGrundID = WegGrund.ID
+SELECT WegGrund.WegGrundBez$LAN$ AS Schrottgrund, Teile.Barcode, [Status].StatusBez AS [Status], Teile.ErstWoche, Teile.ErstDatum, Teile.PatchDatum, Teile.Ausdienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], Teile.AusdRestw AS RestWert
+INTO #TmpErgebnis734a
+FROM Teile
+JOIN Vsa ON Teile.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN [Status] ON Teile.[Status] = [Status].[Status] AND [Status].Tabelle = 'TEILE'
+JOIN Artikel ON Teile.ArtikelID = Artikel.ID
+JOIN WegGrund ON Teile.WegGrundID = WegGrund.ID
+JOIN Bereich ON Artikel.BereichID = Bereich.ID
+JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND Bereich.ID = StandBer.BereichID
+JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
+JOIN Standort AS Kundenservice ON Kunden.StandortID = Kundenservice.ID
+WHERE Produktion.ID IN ($4$)
+  AND Teile.[Status] = 'Y'
   AND WegGrund.ID IN ($3$)
-	AND Teile.AusDienstDat BETWEEN $1$ AND $2$
-	AND Artikel.BereichID = Bereich.ID
-	AND Bereich.ID IN ($5$);
+  AND Teile.AusDienstDat BETWEEN $1$ AND $2$
+  AND Bereich.ID IN ($5$);
 
-INSERT INTO #TmpErgebnis
-SELECT WegGrund.WegGrundBez$LAN$ AS Schrottgrund, TeileLag.Barcode, Status.StatusBez AS Status, TeileLag.ErstWoche, TeileLag.ErstDatum, CONVERT(date, NULL) AS PatchDatum, TeileLag.AusDienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Kunden.KdNr, Kunden.SuchCode AS Kunde, Standort.Bez AS Standort, TeileLag.Restwert AS RestWert
-FROM TeileLag, Status, ArtGroe, Artikel, WegGrund, Traeger, Vsa, Kunden, Standort, Bereich
-WHERE TeileLag.Status = Status.Status
-	AND Status.Tabelle = 'TEILELAG'
-	AND TeileLag.Status = 'Y'
-	AND TeileLag.ArtGroeID = ArtGroe.ID
-	AND ArtGroe.ArtikelID = Artikel.ID
-	AND Artikel.BereichID = Bereich.ID
-	AND Bereich.ID IN ($5$)
-	AND TeileLag.WegGrundID = WegGrund.ID
+INSERT INTO #TmpErgebnis734a
+SELECT WegGrund.WegGrundBez$LAN$ AS Schrottgrund, TeileLag.Barcode, [Status].StatusBez AS [Status], TeileLag.ErstWoche, TeileLag.ErstDatum, CONVERT(date, NULL) AS PatchDatum, TeileLag.AusDienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], TeileLag.Restwert AS RestWert
+FROM TeileLag
+JOIN [Status] ON TeileLag.[Status] = [Status].[Status] AND [Status].Tabelle = 'TEILELAG'
+JOIN ArtGroe ON TeileLag.ArtGroeID = ArtGroe.ID
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+JOIN Bereich ON Artikel.BereichID = Bereich.ID
+JOIN WegGrund ON TeileLag.WegGrundID = WegGrund.ID
+JOIN Traeger ON TeileLag.TraegerID = Traeger.ID
+JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND Bereich.ID = StandBer.BereichID
+JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
+JOIN Standort AS Kundenservice ON Kunden.StandortID = Kundenservice.ID
+WHERE TeileLag.[Status] = 'Y'
+  AND Bereich.ID IN ($5$)
   AND WegGrund.ID IN ($3$)
-	AND TeileLag.AusDienstDat BETWEEN $1$ AND $2$
-	AND TeileLag.TraegerID = Traeger.ID
-	AND Traeger.VsaID = Vsa.ID
-	AND Vsa.KundenID = Kunden.ID
-	AND Kunden.StandortID = Standort.ID
-	AND Standort.ID IN ($4$)
+  AND TeileLag.AusDienstDat BETWEEN $1$ AND $2$
+  AND Produktion.ID IN ($4$)
   AND NOT EXISTS (
     SELECT Ergebnis.Barcode
-    FROM #TmpErgebnis Ergebnis
+    FROM #TmpErgebnis734a Ergebnis
     WHERE Ergebnis.Barcode = TeileLag.Barcode
   );
 
-SELECT Standort, ArtikelNr, ArtikelNr2 AS [BMD-ArtikelNr], Artikelbezeichnung, EKPreis, Schrottgrund, COUNT(Barcode) AS Menge
-FROM #TmpErgebnis
-GROUP BY Standort, ArtikelNr, ArtikelNr2, Artikelbezeichnung, EKPreis, Schrottgrund
-ORDER BY Standort, ArtikelNr;
+SELECT Produktionsstandort, ArtikelNr, ArtikelNr2 AS [BMD-ArtikelNr], Artikelbezeichnung, EKPreis, Schrottgrund, COUNT(Barcode) AS Menge
+FROM #TmpErgebnis734a
+GROUP BY Produktionsstandort, ArtikelNr, ArtikelNr2, Artikelbezeichnung, EKPreis, Schrottgrund
+ORDER BY Produktionsstandort, ArtikelNr;
