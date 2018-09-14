959525cae463ef5246d0f49e866757933d187336
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-09-14 11:26:30 +0200

    change: if barcode already exists, deal with it in a sensible way

--- Diverse/Kundenverlagerung/SALI - Kundenübernahme - Teile-Import.sql
+++ Diverse/Kundenverlagerung/SALI - Kundenübernahme - Teile-Import.sql
@@ -5,7 +5,7 @@
 /* ++ Author: Stefan Thaller - 2018-08-29                                                                                       ++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-DECLARE @ImportFile nvarchar(200) = N'\\atenadvantex01\AdvanTex\Temp\2018-08-29_10001002_St Anna Service.xlsx';  -- Pfad zum Excel-File mit den Teile-Daten. Muss für den SQL-Server-Prozess zugreifbar sein, daher am Besten unter \\atenadvantex01\advantex\temp\ ablegen.
+DECLARE @ImportFile nvarchar(200) = N'\\atenadvantex01\AdvanTex\Temp\2018-09-12_31172_Hubers Landhendl_VSA Produktion.xlsx';  -- Pfad zum Excel-File mit den Teile-Daten. Muss für den SQL-Server-Prozess zugreifbar sein, daher am Besten unter \\atenadvantex01\advantex\temp\ ablegen.
 DECLARE @XLSXImportSQL nvarchar(max);
 
 DECLARE @ImportTable TABLE (
@@ -27,6 +27,12 @@ DECLARE @ImportTable TABLE (
   IndienstDat date,
   Waschzyklen int,
   AlterMonate int
+);
+
+DECLARE @DoubleParts TABLE (
+  TeileID int,
+  Teilstatus nchar(2) COLLATE Latin1_General_CS_AS,
+  Barcode nvarchar(33) COLLATE Latin1_General_CS_AS
 );
 
 SET @XLSXImportSQL = N'SELECT CAST(KDNR as int) AS KdNr, ' +
@@ -51,6 +57,33 @@ SET @XLSXImportSQL = N'SELECT CAST(KDNR as int) AS KdNr, ' +
 
 INSERT INTO @ImportTable
 EXEC sp_executesql @XLSXImportSQL;
+
+-- Prüfung auf bereits vorhandene Barcode - Ausgabe der bereits im AdvanTex vorhandenen Teile
+INSERT INTO @DoubleParts
+SELECT Teile.ID AS TeileID, Teile.Status AS Teilstatus, Teile.Barcode
+FROM Teile
+WHERE EXISTS (
+  SELECT *
+  FROM @ImportTable AS Import
+  WHERE Teile.Barcode = Import.Barcode
+);
+
+-- Teile mit Status Schrott, Lager, Lager verbraucht - Barcode mit *SAL erweitern, damit Übernahme-Teil importiert werden kann
+UPDATE Teile SET Teile.Barcode = RTRIM(Teile.Barcode) + N'*SAL'
+WHERE Teile.ID IN (
+  SELECT DoubleParts.TeileID
+  FROM @DoubleParts AS DoubleParts
+  WHERE DoubleParts.Teilstatus IN (N'Y', N'L', N'LM')
+);
+
+-- Wenn Teil bereits im AdvanTex vorhanden ist (und nicht den Status Schrott, Lager, Lager verbraucht aufweist), dann das Übernahme-Teil mit *SAL erweitern um es importieren zu können
+-- Nach dem Import wird dann ein Hinweis beim bestehenden Teil und beim Übernahme-Teil angelegt
+UPDATE @ImportTable SET Barcode = RTRIM(Barcode) + N'*SAL'
+WHERE Barcode IN (
+  SELECT DoubleParts.Barcode
+  FROM @DoubleParts AS DoubleParts
+  WHERE DoubleParts.Teilstatus NOT IN (N'Y', N'L', N'LM')
+);
 
 INSERT INTO Teile (Barcode, [Status], VsaID, TraegerID, TraeArtiID, KdArtiID, ArtikelID, ArtGroeID, Entnommen, EinsatzGrund, PatchDatum, Erstwoche, ErstDatum, Indienst, IndienstDat, RuecklaufG, Kostenlos, AlterInfo, AltenheimModus, AnlageUserID_, UserID_)
 SELECT ImportTable.Barcode,
@@ -80,3 +113,16 @@ JOIN Traeger ON Traeger.VsaID = Vsa.ID AND CAST(Traeger.Traeger AS int) = Import
 JOIN TraeArti ON TraeArti.TraegerID = Traeger.ID
 JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID AND ArtGroe.Groesse = ImportTable.Groesse
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID AND Artikel.ArtikelNr = ImportTable.ArtikelNr;
+
+-- Umpatch-Hinweis bei bestehenden Teilen anlegen
+INSERT INTO Hinweis (TeileID, Aktiv, StatusSDC, Hinweis, BisWoche, Anzahl, EingabeDatum, EingabeMitarbeiID, Wichtig)
+SELECT DoubleParts.TeileID, 1 AS Aktiv, N'A' AS StatusSDC, N'<o> ACHTUNG:Neuen Barcode patchen! Auf korrekten Kunden achten! Kundenübernahme von Salesianer!' AS Hinweis, N'2099/52' AS BisWoche, 1 AS Anzahl, CAST(GETDATE() AS date) AS Eingabedatum, (SELECT Mitarbei.ID FROM Mitarbei WHERE Mitarbei.UserName = N'STHA') AS EingabeMitarbeiID, 1 AS Wichtig
+FROM @DoubleParts AS Doubleparts
+WHERE DoubleParts.Teilstatus NOT IN (N'Y', N'L', N'LM');
+
+-- Umpatch-Hinweis bei importierten Teilen anlegen
+INSERT INTO Hinweis (TeileID, Aktiv, StatusSDC, Hinweis, BisWoche, Anzahl, EingabeDatum, EingabeMitarbeiID, Wichtig)
+SELECT Teile.ID, 1 AS Aktiv, N'A' AS StatusSDC, N'<o> ACHTUNG:Neuen Barcode patchen! Auf korrekten Kunden achten! Kundenübernahme von Salesianer!' AS Hinweis, N'2099/52' AS BisWoche, 1 AS Anzahl, CAST(GETDATE() AS date) AS Eingabedatum, (SELECT Mitarbei.ID FROM Mitarbei WHERE Mitarbei.UserName = N'STHA') AS EingabeMitarbeiID, 1 AS Wichtig
+FROM @DoubleParts AS Doubleparts
+JOIN Teile ON Teile.Barcode = RTRIM(Doubleparts.Barcode) + N'*SAL'
+WHERE DoubleParts.Teilstatus NOT IN (N'Y', N'L', N'LM');
