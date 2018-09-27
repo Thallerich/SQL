863b64410290c7176c00ecc1be874715941bb36f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-09-27 10:14:04 +0200

    Refactored to import articles from excel list

--- Diverse/Kundenverlagerung/Artikelimport.sql
+++ Diverse/Kundenverlagerung/Artikelimport.sql
@@ -1,200 +1,41 @@
+DECLARE @ImportFile nvarchar(200) = N'\\atenadvantex01\AdvanTex\Temp\SAL_Artikel.xlsx';  -- Pfad zum Excel-File mit den Teile-Daten. Muss für den SQL-Server-Prozess zugreifbar sein, daher am Besten unter \\atenadvantex01\advantex\temp\ ablegen.
+DECLARE @XLSXImportSQL nvarchar(max);
+
 DECLARE @SalArtikel TABLE (
-  ArtikelID int,
+  Bereich nchar(2) COLLATE Latin1_General_CS_AS,
+  Artikelgruppe nchar(4) COLLATE Latin1_General_CS_AS,
   ArtikelNr nvarchar(15) COLLATE Latin1_General_CS_AS,
-  ArtikelBez nvarchar(60) COLLATE Latin1_General_CS_AS
+  ArtikelBez nvarchar(60) COLLATE Latin1_General_CS_AS,
+  Groesse nvarchar(15) COLLATE Latin1_General_CS_AS
 );
 
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'81T100', N'Geschirrtuch blau kar.,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7199T0', N'SPLT weiß, lila Naht,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U9N', N'Kleid weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U12', N'Mantel weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P55T', N'Pyjamajacke,blau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U7B', N'OP-Hose blau NÖ-Holding,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P82T', N'Pat.mantel,blau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7199T2', N'Säugling-Sp.Lt 35x65,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3109T0', N'HT Frottee taubenblau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7503', N'Decke');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'UJ7', N'LPH Jeans unisex blau UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7808T1', N'PB 40x60,flieder,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6469', N'Wärmedecke Universal 2');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7701T3', N'DB 140x200 Verdi weiß KV,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7199T7', N'Geb.Bettbez.klein/blaue N.,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7201', N'Steppdecke einzel');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'8676T0', N'Vorbinder,weinrot,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U8B', N'Polo bordeaux UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'87A1T1', N'Trägerschürze weiß,Bänder,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SV7521', N'Bettauflage per Stk.');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6461', N'Wärmedecke Erwachsene 1');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7206', N'Babydecke(n)');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U88', N'Polo lime UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7101T1', N'Leintuch 150x270 UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7627', N'Polster Stepp 60x80');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P5DCT2', N'Inko-Unterlage 85x195,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'32GRT0', N'Fr.DT,mintgrün,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U39', N'LPH Überschürze türkis UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'PH00T1', N'Pat.hemd,geschlossen,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3107T0', N'HT Frottee pfirsich,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7708T0', N'DB 140x200,flieder,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'38DTL', N'Bademantel,weiß,L,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7705T0', N'DB 140x200,gelb,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3207T0', N'DT Frottee pfirsich,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P583T', N'DRYCARE Matr.schoner UHF-Chip');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW8707', N'Stillkissen');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7302', N'Federpolster');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U86', N'LPH Polo gelb,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7806T2', N'PB 40x40,mint,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6473', N'Heizdecke');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U37', N'Unisex-Weste dkl.blau UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U71', N'Schlupfhemd weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6460', N'Wärmedecke Universal 3');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U32', N'LPH Sweaterjacke,blau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U4N', N'Hrn.Hose graphit UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'GH1T01', N'Steppdecke 100x140,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P703K', N'AT 160x180 kleb.');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7901T0', N'PB 40x60 mint,Kind UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'G2T100', N'*DS weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U41', N'LPH BH unisex weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U7G', N'OP-Hose grün NÖ-Holding,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7101T0', N'*LT 160x290 weiß,N.grün,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'860000', N'*Halbschürze weiss mit Bändern');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW3603', N'Frottee Liegetuch,weiß,120x240');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7805T1', N'PB 40x60,gelb,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'89MFT6', N'SM-Microfasertuch grün, UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'KD4T', N'Ki.Pyjamahose,gelb,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SV7326', N'Stillpolster');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'R04A', N'RR-Hose Light Tech,hellblau');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'THT4T0', N'Therapie-LT blau,grüne N.,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'74MGT0', N'DLT weiß rote Naht,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P275T1', N'Waschlappen weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'31GET0', N'Fr.HT,gelb,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3208T0', N'DT Frottee lila,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'38DTS', N'Bademantel,weiß,S,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P105T0', N'Durchzug 80x180,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'1060', N'Kochhose schwarz.');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7808T0', N'PB 60x80,flieder,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'G3T119', N'*TT 130x190 weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'PHT505', N'Ki.PH,offen,grün,5-9,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7307', N'Polster klein');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'TESTPT', N'*TEST-PB 60x80,weiß,Duo,4xUHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'PH50T9', N'Pat.hemd,offen ÜG,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'89MFT2', N'SM-Microfasertuch weiß, UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'38DTXL', N'Bademantel,weiß,XL,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P85T', N'Pyjamahose,blau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P578T0', N'Inko-Brustlatz,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7334', N'Lagerungskissen');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U31', N'Da.Kasack weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U4G', N'OP-Kasack grün NÖ-Holding,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P5DCT3', N'Inko-Unterlage 40x60,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'86A1T0', N'Halbschürze weiss,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7805T2', N'PB 40x40,gelb,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'PHT510', N'Ki.PH,offen,blau,10-12,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U5N', N'Jacke grau/blau HAUSARB. UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3209T0', N'DT Frottee taubenblau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6468', N'Wärmedecke Ganzkörper');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'PH00T9', N'Pat.hemd,geschl. ÜG,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'KD6T', N'Babyjacke,weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7806T1', N'PB 40x60,mint,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'PH50T1', N'Pat.hemd,offen,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6466', N'Wärmedecke Unterkörper');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3103T0', N'HT Frottee silber,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'31GRT0', N'Fr.HT,mintgrün,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U4B', N'OP-Kasack blau NÖ-Holding,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7807T1', N'PB 40x60,apricot,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'UH1', N'LPH Panamahemd weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U42', N'Unisex-Hose weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'38DTXX', N'Bademantel,weiß,XXL,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'542T', N'Übermantel blau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'KD2T', N'Ki.Pyjamajacke,mint,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7806T0', N'PB 60x80,mint,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'G1T100', N'*MS weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'THT1T0', N'Therapie-LT 150x270,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7808T2', N'PB 40x40,flieder,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3203T0', N'DT Frottee silber,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U11', N'LPH Ärztemantel weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'541T', N'Übermantel weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7807T0', N'PB 60x80,apricot,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'TLY1', N'T-Shirt,rnd.Aus,lgA weiss');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'8001T1', N'Säugl.DB 80x60 weiß,Kind UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'89MFT4', N'SM-Microfasertuch rot, UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3237T0', N'*DT Frottee weiß');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'GH0100', N'Steppdecke 140x200cm');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7801T3', N'PB 60x80 Verdi1 weiß KV,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7432', N'Steppdecke 1200g, 140x200');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'F11', N'F11 Mantel weiß');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6464', N'Wärmedecke Baby/Kleinkind');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7706T0', N'DB 140x200,mint,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3137T0', N'*HT Frottee weiß');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7707T0', N'DB 140x200,apricot,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'PHT502', N'Ki.PH,offen,gelb,2-4,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U5K', N'Kochjacke weiß/blau UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'89MFT5', N'SM-Microfasertuch gelb, UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'KD3T', N'Babystrampler,rosa,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'GH1T00', N'Steppdecke 140x200,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW9961', N'Wolldecke(n)');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U84', N'LPH Polo weinrot,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U34', N'LPH Da.Kasack weinrot,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P811T', N'OP-Kälte-Rolli,erbsgr.,UHF-Tr.');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7709T0', N'DB 140x200,hellblau,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'UH2', N'Panama-Hemd weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6462', N'Wärmedecke Erwachsene 2');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U4K', N'LPH Kochhose pepita UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'KD1T', N'Babybody,weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'8001T0', N'DB 100x140 mint,Kind UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'GH2T00', N'Stepppolster 60x80,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'MOP1', N'SM-MOP Standard');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'89MFT3', N'SM-Microfasertuch blau, UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7809T1', N'PB 40x60,hellblau,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7199T6', N'Geb.Bettbez. groß/rote N.,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW5301', N'Fell per Stück');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW6467', N'Wärmedecke Oberkörper');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P128MT', N'Kappen-LT weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U8R', N'Polo blau UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3108T0', N'HT Frottee lila,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7199T1', N'SPLT 70x140 weiß,grüne N.,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U91', N'Kleid Küche weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'KD8T00', N'Babydecke,Flanell,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U10', N'Kochhose schwarz pepita UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'CONT01', N'Container Standard           *');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'GH2T02', N'Stepppolster 40x40,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3301T0', N'BT Frottee weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U82', N'LPH Polo weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7807T2', N'PB 40x40,apricot,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U33', N'LPH Da.Kasack royal,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U83', N'LPH Polo royal,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U35', N'Kälteschutzjacke blau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'GH2T01', N'Stepppolster 40x60,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U8W', N'Polo weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'KD9T', N'Babystrampler,h.blau,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'P5DCT1', N'Inko-Unterlage 75x85,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U51', N'LPH Kochjacke weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'G3T113', N'*TT 130x130 weiß,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'32GET0', N'Fr.DT,gelb,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'SW7304', N'Nackenrolle(n)');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7805T0', N'PB 60x80,gelb,Deluxe UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'3440T0', N'*BV Frottee weiß UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'GH0200', N'Stepppolster 60x80cm Std.');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'U36', N'Kälteschutzjacke grün,UHF');
-INSERT INTO @SalArtikel (ArtikelNr, ArtikelBez) VALUES (N'7809T0', N'PB 60x80,hellblau,Deluxe UHF');
+SET @XLSXImportSQL = N'SELECT CAST(AKTIVITÄT AS nchar(2)) AS Bereich, ' +
+  N'CAST(PRODUKTGRUPPE AS nchar(4)) AS Artikelgruppe, ' +
+  N'CAST(ArtikelNr AS nvarchar(15)) AS ArtikelNr, ' +
+  N'CAST(ArtikelBez AS nvarchar(60)) AS Artikelbezeichnung, ' +
+  N'CAST(GroesseKorrigiert AS nvarchar(15)) AS Groesse ' +
+  N'FROM OPENROWSET(N''Microsoft.ACE.OLEDB.12.0'', N''Excel 12.0 Xml;HDR=YES;Database='+@ImportFile+''', [Artikel$]);';
 
-INSERT INTO Artikel (ArtiTypeID, [Status], ArtikelNr, ArtikelBez, ArtikelBez1, ArtikelBez2, SuchCode, BereichID, LiefID, LiefTageID, ArtGruID, MeID, LiefArtID, GroeKoID, PackMenge)
-SELECT 1 AS ArtiTypeID, N'C' AS [Status], SalArtikel.ArtikelNr, SalArtikel.ArtikelBez, SalArtikel.ArtikelBez AS ArtikelBez1, SalArtikel.ArtikelBez AS ArtikelBez2, LEFT(SalArtikel.ArtikelBez, 20) AS SuchCode, 102 AS BereichID, 26716 AS LiefID, 250 AS LiefTageID, 26008 AS ArtGruID, 1 AS MeID, 4 AS LiefArtID, 1 AS GroeKoID, 1 AS Packmenge
+INSERT INTO @SalArtikel
+EXEC sp_executesql @XLSXImportSQL;
+
+INSERT INTO Artikel (ArtiTypeID, [Status], ArtikelNr, ArtikelBez, ArtikelBez1, ArtikelBez2, SuchCode, BereichID, LiefID, LiefTageID, ArtGruID, MeID, LiefArtID, PackMenge, SichtbarID, KontenID)
+SELECT DISTINCT 1 AS ArtiTypeID, N'A' AS [Status], SalArtikel.ArtikelNr, SalArtikel.ArtikelBez, SalArtikel.ArtikelBez AS ArtikelBez1, SalArtikel.ArtikelBez AS ArtikelBez2, LEFT(SalArtikel.ArtikelBez, 20) AS SuchCode, Bereich.ID AS BereichID, 190 AS LiefID, 190 AS LiefTageID, ArtGru.ID AS ArtGruID, 1 AS MeID, 4 AS LiefArtID, 1 AS Packmenge, -2 AS SichtbarID, 564 AS KontenID
 FROM @SalArtikel AS SalArtikel
+JOIN Bereich ON SalArtikel.Bereich = Bereich.Bereich
+JOIN ArtGru ON SalArtikel.Artikelgruppe = ArtGru.Gruppe AND ArtGru.BereichID = Bereich.ID
 WHERE NOT EXISTS (
   SELECT Artikel.ID
   FROM Artikel
-  WHERE Artikel.ArtikelNr = SalArtikel.ArtikelNr);
-
-UPDATE @SalArtikel SET ArtikelID = Artikel.ID
-FROM @SalArtikel AS SalArtikel
-JOIN Artikel ON SalArtikel.ArtikelNr = Artikel.ArtikelNr;
+  WHERE Artikel.ArtikelNr = SalArtikel.ArtikelNr
+);
 
 INSERT INTO ArtGroe (ArtikelID, Groesse, [Status])
-SELECT SalArtikel.ArtikelID, N'-' AS Groesse, N'A' AS [Status]
+SELECT Artikel.ID AS ArtikelID, SalArtikel.Groesse, N'A' AS [Status]
 FROM @SalArtikel AS SalArtikel
+JOIN Artikel ON SalArtikel.ArtikelNr = Artikel.ArtikelNr
 WHERE NOT EXISTS (
   SELECT ArtGroe.ID
   FROM ArtGroe
-  WHERE ArtGroe.ArtikelID = SalArtikel.ArtikelID
+  WHERE ArtGroe.ArtikelID = Artikel.ID AND ArtGroe.Groesse = SalArtikel.Groesse
 );
-
-SELECT ArtikelNr, ArtikelBez FROM @SalArtikel;
