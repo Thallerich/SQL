3bdf3d6643e807324a41fe6cd3fc29e9c8831a4d
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-10-01 13:32:54 +0200

    added KundenIDNr for specific customers

--- Rechnungsanlagen/BewohnerHeimanteil_Netto.sql
+++ Rechnungsanlagen/BewohnerHeimanteil_Netto.sql
@@ -1,8 +1,8 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* ++ Rechnungsanlage Bewohner / Heimanteile netto                                                                              ++ */
-/* ++ Version 2.0                                                                                                               ++ */
+/* ++ Version 2.1                                                                                                               ++ */
 /* ++                                                                                                                           ++ */
-/* ++ Author: Stefan Thaller - 2018-08-07                                                                                       ++ */
+/* ++ Author: Stefan Thaller - 2018-10-01                                                                                       ++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 DECLARE @LsPoSumme TABLE (
@@ -22,7 +22,8 @@ DECLARE @LsPoSumme TABLE (
   AnteilHeim smallmoney,
   Jahr nchar(4),
   Monat nchar(2),
-  RechNr int
+  RechNr int,
+  KundenIDNr int
 );
 
 INSERT INTO @LsPoSumme
@@ -42,7 +43,18 @@ SELECT Abteil.Abteilung AS Kostenstelle,
   LsPo.Menge * LsPo.EPreis * (1 - (KdBer.RabattWasch/100)) * (1 - (ProzTraeger/100)) AS AnteilHeim,
   FORMAT(RechKo.RechDat, N'yyyy') AS Jahr,
   FORMAT(RechKo.RechDat, N'MM') AS Monat,
-  RechKo.RechNr
+  RechKo.RechNr,
+  KundenIDNr =
+    CASE Kunden.KdNr
+      WHEN 7425 THEN 689926
+      WHEN 30061 THEN 621346
+      WHEN 2529403 THEN 402524
+      WHEN 25033 THEN 159771
+      WHEN 19016 THEN 716221
+      WHEN 2024 THEN 499302
+      WHEN 23010 THEN 518885
+      ELSE NULL
+    END
 FROM LsPo
 JOIN LsKo ON LsPo.LsKoID = LsKo.ID
 JOIN RechPo ON LsPo.RechPoID = RechPo.ID
@@ -53,6 +65,7 @@ JOIN BewAbr ON Traeger.BewAbrID = BewAbr.ID
 JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
 JOIN KdBer ON KdArti.KdBerID = KdBer.ID
 JOIN Bereich ON KdBer.BereichID = Bereich.ID
+JOIN Kunden ON RechKo.KundenID = Kunden.ID
 LEFT OUTER JOIN BewKdAr ON BewKdAr.BewAbrID = BewAbr.ID AND BewKdAr.KdArtiID = KdArti.ID
 WHERE RechKo.ID = $RECHKOID$
   AND Bereich.Bereich = N'CT';
@@ -71,7 +84,8 @@ SELECT LPS.Jahr,
   SUM(LPS.AnteilTraeger) AS NettoTraeger,
   SUM(LPS.AnteilTraeger) * 1.2 AS BruttoTraeger,
   SUM(LPS.AnteilHeim) AS NettoHeim,
-  SUM(LPS.AnteilHeim) * 1.2 AS BruttoHeim
+  SUM(LPS.AnteilHeim) * 1.2 AS BruttoHeim,
+  LPS.KundenIDNr
 FROM @LsPoSumme AS LPS
-GROUP BY LPS.Jahr, LPS.Monat, LPS.RechNr, LPS.Kostenstelle, LPS.KostenstelleBez, LPS.KostenstelleID, LPS.Nachname, LPS.Vorname, LPS.ZimmerNr, LPS.TraegerNr, LPS.RabattBearbeitung, LPS.ServiceProzent
+GROUP BY LPS.Jahr, LPS.Monat, LPS.RechNr, LPS.Kostenstelle, LPS.KostenstelleBez, LPS.KostenstelleID, LPS.Nachname, LPS.Vorname, LPS.ZimmerNr, LPS.TraegerNr, LPS.RabattBearbeitung, LPS.ServiceProzent, LPS.KundenIDNr
 ORDER BY Nachname, Vorname;
