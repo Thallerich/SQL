e58b490d4685b6ad13874a76f3f76847d678f75f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-11-19 07:28:26 +0100

    use correct date range

--- Diverse/Pacala - Auswertung Artikel Kostenträger 932800.sql
+++ Diverse/Pacala - Auswertung Artikel Kostenträger 932800.sql
@@ -1,4 +1,4 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Debitor, RechKo.RechNr, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeicnung, SUM(RechPo.Menge) AS [verrehcnete Menge], SUM(RechPo.GPreis) AS Umsatz
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Debitor, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, SUM(RechPo.Menge) AS [verrechnete Menge], SUM(RechPo.GPreis) AS Umsatz
 FROM RechPo
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
 JOIN Kunden ON RechKo.KundenID = Kunden.ID
@@ -6,6 +6,6 @@ JOIN KdArti ON RechPo.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 WHERE RechPo.KsSt = N'2800'
   AND RechKo.FirmaID = (SELECT Firma.ID FROM Firma WHERE Firma.SuchCode = N'UKLU')
-  AND RechKo.RechDat BETWEEN N'2018-04-01' AND N'2018-06-30'
+  AND RechKo.RechDat BETWEEN N'2018-04-01' AND N'2018-09-30'
   AND RechKo.FibuExpID > 0
-GROUP BY Kunden.KdNr, Kunden.SuchCode, Kunden.Debitor, RechKo.RechNr, Artikel.ArtikelNr, Artikel.ArtikelBez;
+GROUP BY Kunden.KdNr, Kunden.SuchCode, Kunden.Debitor, Artikel.ArtikelNr, Artikel.ArtikelBez;
