9f6a48aa07081f9b88319154096d0c83eb1cc7a8
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-11-20 13:03:57 +0100

    created report

---
+++ Report/847 - OP - Nachwäschen.sql
@@ -0,0 +1,12 @@
+DECLARE @from datetime2 = CAST($1$ AS datetime);
+DECLARE @to datetime2 = CAST(DATEADD(day, 1, $2$) AS datetime);
+
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, CAST(OPScans.Zeitpunkt AS date) AS Tag, ZielNr.ZielNrBez AS [Nachwäsche-Grund], COUNT(DISTINCT OPTeile.ID) AS [Anzahl Nachwäsche-Teile], COUNT(OPScans.ID) AS [Anzahl Nachwäschen]
+FROM OPScans
+JOIN OPTeile ON OPScans.OPTeileID = OPTeile.ID
+JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+JOIN ZielNr ON OPScans.ZielNrID = ZielNr.ID
+WHERE OPScans.Zeitpunkt BETWEEN @from AND @to
+  AND OPScans.ZielNrID IN (10000020, 10000021, 10000022, 10000019, 10000031)
+GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez, CAST(OPScans.Zeitpunkt AS date), ZielNr.ZielNrBez
+ORDER BY Tag, [Nachwäsche-Grund], Artikelbezeichnung;
