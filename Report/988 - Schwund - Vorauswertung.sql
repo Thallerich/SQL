ba7ac35230861236e9e808d912f87a78a6c85aa0
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-11-28 07:43:40 +0100

    - refactored for change to ArtGroeID in OPTeile table

--- Report/988 - Schwund - Vorauswertung.sql
+++ Report/988 - Schwund - Vorauswertung.sql
@@ -6,17 +6,20 @@ SET @bis = $2$;
 
 DROP TABLE IF EXISTS #TmpSchwund;
 
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Status.Bez AS VsaStatus, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS Vsa, Bereich.BereichBez$LAN$ AS Produktbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, 0 AS Vertragsbestand, 0 AS SchwundZeitraum, 0 AS SchwundAlt, 0 AS BereitsSchwundmarkiertZeitraum, 0 AS BereitsSchwundmarkiertAlt, 0 AS BereitsSchwundmarkiertNeu, 0 AS SchwundGesperrt, 0 AS Durchschnittsliefermenge, Artikel.EKPreis, Vsa.ID AS VsaID, Artikel.ID AS ArtikelID
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Status.Bez AS VsaStatus, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS Vsa, Bereich.BereichBez$LAN$ AS Produktbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, 0 AS Vertragsbestand, 0 AS SchwundZeitraum, 0 AS SchwundAlt, 0 AS BereitsSchwundmarkiertZeitraum, 0 AS BereitsSchwundmarkiertAlt, 0 AS BereitsSchwundmarkiertNeu, 0 AS SchwundGesperrt, 0 AS Durchschnittsliefermenge, Artikel.EKPreis, Vsa.ID AS VsaID, Artikel.ID AS ArtikelID, ArtGroe.ID AS ArtGroeID
 INTO #TmpSchwund
-FROM OPTeile, Vsa, Kunden, Artikel, Bereich, (SELECT Status.Status, Status.StatusBez$LAN$ AS Bez FROM Status WHERE Status.Tabelle = 'VSA') AS Status
+FROM OPTeile, Vsa, Kunden, ArtGroe, KdArti, Artikel, Bereich, (SELECT Status.Status, Status.StatusBez$LAN$ AS Bez FROM Status WHERE Status.Tabelle = 'VSA') AS Status
 WHERE OPTeile.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
-  AND OPTeile.ArtikelID = Artikel.ID
+  AND OPTeile.ArtGroeID = ArtGroe.ID
+  AND ArtGroe.ArtikelID = Artikel.ID
+  AND KdArti.ArtikelID = Artikel.ID
+  AND KdArti.KundenID = Kunden.ID
   AND Artikel.BereichID = Bereich.ID
   AND Vsa.Status = Status.Status
   AND Kunden.ID = $ID$
   AND Artikel.EAN IS NOT NULL
-GROUP BY Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Status.Bez, Vsa.SuchCode, Vsa.Bez, Bereich.BereichBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Artikel.EKPreis, Vsa.ID, Artikel.ID;
+GROUP BY Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Status.Bez, Vsa.SuchCode, Vsa.Bez, Bereich.BereichBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Artikel.EKPreis, Vsa.ID, Artikel.ID, ArtGroe.ID;
 
 UPDATE Schwund SET Schwund.Vertragsbestand = x.Bestand, Schwund.Durchschnittsliefermenge = x.Durchschnitt
 FROM #TmpSchwund AS Schwund, (
@@ -30,80 +33,80 @@ WHERE x.VsaID = Schwund.VsaID
 
 UPDATE Schwund SET Schwund.SchwundZeitraum = x.Anzahl
 FROM #TmpSchwund AS Schwund, (
-  SELECT OPTeile.VsaID, OPTeile.ArtikelID, COUNT(OPTeile.ID) AS Anzahl
+  SELECT OPTeile.VsaID, OPTeile.ArtGroeID, COUNT(OPTeile.ID) AS Anzahl
   FROM OPTeile
   WHERE OPTeile.VsaID IN (SELECT VsaID FROM #TmpSchwund)
     AND OPTeile.LastScanTime BETWEEN @von AND @bis
     AND OPTeile.Status IN (N'A', N'Q')
     AND OPTeile.LastActionsID = 102  -- Teile beim Kunden
-  GROUP BY OPTeile.VsaID, OPTeile.ArtikelID
+  GROUP BY OPTeile.VsaID, OPTeile.ArtGroeID
 ) AS x
 WHERE x.VsaID = Schwund.VsaID
-  AND x.ArtikelID = Schwund.ArtikelID;
+  AND x.ArtGroeID = Schwund.ArtGroeID;
 
 UPDATE Schwund SET Schwund.SchwundAlt = x.Anzahl
 FROM #TmpSchwund AS Schwund, (
-  SELECT OPTeile.VsaID, OPTeile.ArtikelID, COUNT(OPTeile.ID) AS Anzahl
+  SELECT OPTeile.VsaID, OPTeile.ArtGroeID, COUNT(OPTeile.ID) AS Anzahl
   FROM OPTeile
   WHERE OPTeile.VsaID IN (SELECT VsaID FROM #TmpSchwund)
     AND OPTeile.LastScanTime < @von
     AND OPTeile.Status IN (N'A', N'Q')
     AND OPTeile.LastActionsID = 102  -- Teile beim Kunden
-  GROUP BY OPTeile.VsaID, OPTeile.ArtikelID
+  GROUP BY OPTeile.VsaID, OPTeile.ArtGroeID
 ) AS x
 WHERE x.VsaID = Schwund.VsaID
-  AND x.ArtikelID = Schwund.ArtikelID;
+  AND x.ArtGroeID = Schwund.ArtGroeID;
 
 UPDATE Schwund SET Schwund.BereitsSchwundmarkiertZeitraum = x.Anzahl
 FROM #TmpSchwund AS Schwund, (
-  SELECT OPTeile.VsaID, OPTeile.ArtikelID, COUNT(OPTeile.ID) AS Anzahl
+  SELECT OPTeile.VsaID, OPTeile.ArtGroeID, COUNT(OPTeile.ID) AS Anzahl
   FROM OPTeile
   WHERE OPTeile.VsaID IN (SELECT VsaID FROM #TmpSchwund)
     AND OPTeile.Status = 'W'
     AND OPTeile.RechPoID = -1
     AND OPTeile.LastScanTime BETWEEN @von AND @bis
-  GROUP BY OPTeile.VsaID, OPTeile.ArtikelID
+  GROUP BY OPTeile.VsaID, OPTeile.ArtGroeID
 ) AS x
 WHERE x.VsaID = Schwund.VsaID
-  AND x.ArtikelID = Schwund.ArtikelID;
+  AND x.ArtGroeID = Schwund.ArtGroeID;
 
 UPDATE Schwund SET Schwund.BereitsSchwundmarkiertAlt = x.Anzahl
 FROM #TmpSchwund AS Schwund, (
-  SELECT OPTeile.VsaID, OPTeile.ArtikelID, COUNT(OPTeile.ID) AS Anzahl
+  SELECT OPTeile.VsaID, OPTeile.ArtGroeID, COUNT(OPTeile.ID) AS Anzahl
   FROM OPTeile
   WHERE OPTeile.VsaID IN (SELECT VsaID FROM #TmpSchwund)
     AND OPTeile.Status = 'W'
     AND OPTeile.RechPoID = -1
     AND OPTeile.LastScanTime < @von
-  GROUP BY OPTeile.VsaID, OPTeile.ArtikelID
+  GROUP BY OPTeile.VsaID, OPTeile.ArtGroeID
 ) AS x
 WHERE x.VsaID = Schwund.VsaID
-  AND x.ArtikelID = Schwund.ArtikelID;
+  AND x.ArtGroeID = Schwund.ArtGroeID;
 
 UPDATE Schwund SET Schwund.BereitsSchwundmarkiertNeu = x.Anzahl
 FROM #TmpSchwund AS Schwund, (
-  SELECT OPTeile.VsaID, OPTeile.ArtikelID, COUNT(OPTeile.ID) AS Anzahl
+  SELECT OPTeile.VsaID, OPTeile.ArtGroeID, COUNT(OPTeile.ID) AS Anzahl
   FROM OPTeile
   WHERE OPTeile.VsaID IN (SELECT VsaID FROM #TmpSchwund)
     AND OPTeile.Status = 'W'
     AND OPTeile.RechPoID = -1
     AND OPTeile.LastScanTime > @bis
-  GROUP BY OPTeile.VsaID, OPTeile.ArtikelID
+  GROUP BY OPTeile.VsaID, OPTeile.ArtGroeID
 ) AS x
 WHERE x.VsaID = Schwund.VsaID
-  AND x.ArtikelID = Schwund.ArtikelID;
+  AND x.ArtGroeID = Schwund.ArtGroeID;
 
 UPDATE Schwund SET Schwund.SchwundGesperrt = x.Anzahl
 FROM #TmpSchwund AS Schwund, (
-  SELECT OPTeile.VsaID, OPTeile.ArtikelID, COUNT(OPTeile.ID) AS Anzahl
+  SELECT OPTeile.VsaID, OPTeile.ArtGroeID, COUNT(OPTeile.ID) AS Anzahl
   FROM OPTeile
   WHERE OPTeile.VsaID IN (SELECT VsaID FROM #TmpSchwund)
     AND OPTeile.Status = 'W'
     AND OPTeile.RechPoID = -2
-  GROUP BY OPTeile.VsaID, OPTeile.ArtikelID
+  GROUP BY OPTeile.VsaID, OPTeile.ArtGroeID
 ) AS x
 WHERE x.VsaID = Schwund.VsaID
-  AND x.ArtikelID = Schwund.ArtikelID;
+  AND x.ArtGroeID = Schwund.ArtGroeID;
 
 SELECT KdNr, Kunde, VsaNr, VsaStatus, VsaStichwort, Vsa, Produktbereich, ArtikelNr, Artikelbezeichnung, Durchschnittsliefermenge, Vertragsbestand, SchwundZeitraum, SchwundAlt, BereitsSchwundmarkiertZeitraum, BereitsSchwundmarkiertAlt, BereitsSchwundmarkiertNeu, SchwundGesperrt AS SchwundVerrechnungGesperrt, SchwundZeitraum + BereitsSchwundmarkiertZeitraum AS SchwundVerrechnen, EKPreis AS [EK aktuell], EKPreis * SchwundZeitraum AS Wiederbeschaffungswert
 FROM #TmpSchwund
