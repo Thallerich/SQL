bd492f5f569edc0d7b25d165fd438923ff7dd86f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-12-19 10:59:48 +0100

    added VsaID column to allow printed report to page-break if it changes

--- Report/830 - CareTex Hängende Auslieferung - Liste.sql
+++ Report/830 - CareTex Hängende Auslieferung - Liste.sql
@@ -1,25 +1,18 @@
-DECLARE @von datetime;
-DECLARE @bis datetime;
+DECLARE @von datetime = $2$;
+DECLARE @bis datetime = DATEADD(day, 1, $2$);
 
-SET @von = $2$;
-SET @bis = DATEADD(day, 1, $2$);
-
-BEGIN TRY
-  DROP TABLE #TmpScans;
-END TRY
-BEGIN CATCH
-END CATCH;
+DROP TABLE IF EXISTS #TmpScans_Rep830;
 
 SELECT Scans.TeileID, Scans.DateTime AS Zeitpunkt
-INTO #TmpScans
+INTO #TmpScans_Rep830
 FROM Scans
 WHERE Scans.DateTime BETWEEN @von AND @bis
   AND Scans.ZielNrID = 2;
 
-SELECT L.KdNr, L.SuchCode, L.VsaNr, L.Vsa, L.Nachname, L.Vorname, L.ZimmerNr, L.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS ArtikelBez
+SELECT L.KdNr, L.SuchCode, L.VsaID, L.VsaNr, L.Vsa, L.Nachname, L.Vorname, L.ZimmerNr, L.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS ArtikelBez
 FROM (
-  SELECT Kunden.KdNr, Kunden.SuchCode, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Traeger.Nachname, Traeger.Vorname, Traeger.PersNr AS ZimmerNr, Teile.Barcode, Teile.ID AS TeileID, Teile.KdArtiID, Scans.Zeitpunkt
-  FROM #TmpScans Scans, Teile, Traeger, Vsa, Kunden
+  SELECT Kunden.KdNr, Kunden.SuchCode, Vsa.ID AS VsaID, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Traeger.Nachname, Traeger.Vorname, Traeger.PersNr AS ZimmerNr, Teile.Barcode, Teile.ID AS TeileID, Teile.KdArtiID, Scans.Zeitpunkt
+  FROM #TmpScans_Rep830 Scans, Teile, Traeger, Vsa, Kunden
   WHERE Scans.TeileID = Teile.ID
     AND Teile.TraegerID = Traeger.ID
     AND Traeger.VsaID = Vsa.ID
@@ -30,4 +23,4 @@ WHERE L.KdArtiID = KdArti.ID
   AND KdArti.ArtikelID = Artikel.ID
   AND KdArti.LiefArtID = LiefArt.ID
   AND LiefArt.LiefArt = 'H'
-ORDER BY L.KdNr, L.VsaNr, L.ZimmerNr, L.Nachname, L.Vorname , Artikel.ArtikelNr, L.TeileID;
+ORDER BY L.KdNr, L.VsaID, L.VsaNr, L.ZimmerNr, L.Nachname, L.Vorname , Artikel.ArtikelNr, L.TeileID;
