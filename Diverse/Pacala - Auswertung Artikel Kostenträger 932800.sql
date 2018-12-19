a5382a0a96442bcf4b7e0af363ce3dd0685a6d01
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-12-19 13:14:49 +0100

    if artikelbez is null output emtpy string

--- Diverse/Pacala - Auswertung Artikel Kostenträger 932800.sql
+++ Diverse/Pacala - Auswertung Artikel Kostenträger 932800.sql
@@ -1,11 +1,11 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Debitor, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, SUM(RechPo.Menge) AS [verrechnete Menge], SUM(RechPo.GPreis) AS Umsatz
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Debitor, Artikel.ArtikelNr, ISNULL(Artikel.ArtikelBez, N'') AS Artikelbezeichnung, SUM(RechPo.Menge) AS [verrechnete Menge], SUM(RechPo.GPreis) AS Umsatz
 FROM RechPo
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
 JOIN Kunden ON RechKo.KundenID = Kunden.ID
 JOIN KdArti ON RechPo.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 WHERE RechPo.KsSt = N'2800'
-  AND RechKo.FirmaID = (SELECT Firma.ID FROM Firma WHERE Firma.SuchCode = N'UKLU')
-  AND RechKo.RechDat BETWEEN N'2018-04-01' AND N'2018-09-30'
+  AND RechKo.FirmaID = (SELECT Firma.ID FROM Firma WHERE Firma.SuchCode = N'WOMI')
+  AND RechKo.RechDat BETWEEN N'2018-04-01' AND N'2018-10-31'
   AND RechKo.FibuExpID > 0
 GROUP BY Kunden.KdNr, Kunden.SuchCode, Kunden.Debitor, Artikel.ArtikelNr, Artikel.ArtikelBez;
