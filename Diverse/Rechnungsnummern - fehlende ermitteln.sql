523f6a792948771842f19b1e7ff9e29d47f448b7
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-12-20 07:49:14 +0100

    added invoice date and export date

--- Diverse/Rechnungsnummern - fehlende ermitteln.sql
+++ Diverse/Rechnungsnummern - fehlende ermitteln.sql
@@ -1,5 +1,5 @@
 DECLARE RechNrKr CURSOR FOR
-SELECT DISTINCT RechNrKr.FirmaID, CAST(sequences.start_value AS int) AS startnum, CAST(sequences.current_value AS int) AS endnum FROM sys.sequences JOIN RechNrKr ON N'NextID_RECHNRKR_' + RechNrKr.NextIDCode = sequences.name COLLATE Latin1_General_CI_AS WHERE sequences.name LIKE N'NextID_RECHNRKR_SALI_%_8ST';
+SELECT DISTINCT RechNrKr.FirmaID, CAST(sequences.start_value AS int) AS startnum, CAST(sequences.current_value AS int) AS endnum FROM sys.sequences JOIN Wozabal.dbo.RechNrKr ON N'NextID_RECHNRKR_' + RechNrKr.NextIDCode = sequences.name COLLATE Latin1_General_CI_AS WHERE sequences.name LIKE N'NextID_RECHNRKR_SALI_%_8ST';
 
 DECLARE @FirmaID int;
 DECLARE @startnum int;
@@ -8,6 +8,8 @@ DECLARE @endnum int;
 DECLARE @RechKo TABLE (
   FirmaID int,
   RechNr int,
+  RechDat date,
+  ExportDate date,
   used bit DEFAULT 0,
   exported bit DEFAULT 0
 );
@@ -32,15 +34,16 @@ END;
 CLOSE RechNrKr;
 DEALLOCATE RechNrKr;
 
-UPDATE r SET r.used = 1
+UPDATE r SET r.used = 1, r.RechDat = Rechko.RechDat
 FROM @RechKo AS r
 JOIN Wozabal.dbo.RechKo ON r.RechNr = RechKo.RechNr;
 
-UPDATE r SET r.exported = 1
+UPDATE r SET r.exported = 1, r.ExportDate = CAST(FibuExp.Zeitpunkt AS date)
 FROM @RechKo AS r
 JOIN Wozabal.dbo.RechKo ON r.RechNr = RechKo.RechNr
+JOIN Wozabal.dbo.FibuExp ON RechKo.FibuExpID = FibuExp.ID
 WHERE RechKo.FibuExpID > 0;
 
-SELECT Firma.SuchCode AS Firma, r.RechNr, r.used, r.exported
+SELECT Firma.SuchCode AS Firma, r.RechNr, r.RechDat, r.ExportDate, r.used, r.exported
 FROM @RechKo AS r
 JOIN Firma ON r.FirmaID = Firma.ID;
