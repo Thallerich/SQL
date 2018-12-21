26b50f2ef58e879e5e57226e2bd1b7d31096660d
Thallerich <20878157+Thallerich@users.noreply.github.com>
2018-12-21 07:45:56 +0100

    slight changes - tourid and folge never changes

--- Diverse/CareTex - Lieferscheine Datumswechsel.sql
+++ Diverse/CareTex - Lieferscheine Datumswechsel.sql
@@ -1,8 +1,8 @@
-DECLARE @KdNr int = 31205;
-DECLARE @DateCorrect date = CAST(N'2018-05-21' AS date);
-DECLARE @DateWrong date = CAST(N'2018-05-15' AS date);
+DECLARE @KdNr int = 30092;
+DECLARE @DateCorrect date = CAST(N'2018-12-25' AS date);
+DECLARE @DateWrong date = CAST(N'2018-12-21' AS date);
 
-UPDATE LsKo SET Datum = @DateCorrect, FahrtID = 717038, TourenID = 57351, Folge = 10, MemoIntern = IIF(MemoIntern IS NULL, N'Lieferdatum geändert auf ' + CAST(@DateCorrect AS nchar(10)) + ' laut Ticket tick.IT:23600  -- STHA', MemoIntern + char(13) + char(10) + N'Lieferdatum geändert auf ' + CAST(@DateCorrect AS nchar(10)) + ' laut Ticket tick.IT:23600  -- STHA')
+UPDATE LsKo SET Datum = @DateCorrect, FahrtID = 750823, MemoIntern = IIF(MemoIntern IS NULL, N'Lieferdatum geändert auf ' + CAST(@DateCorrect AS nchar(10)) + ' laut Ticket IT-21205  -- STHA', MemoIntern + char(13) + char(10) + N'Lieferdatum geändert auf ' + CAST(@DateCorrect AS nchar(10)) + ' laut Ticket IT-21205  -- STHA')
 WHERE LsKo.ID IN (
   SELECT DISTINCT LsKo.ID
   FROM LsKo, Vsa, Kunden, LsKoArt, LsPo, Scans
@@ -20,9 +20,9 @@ WHERE LsKo.ID IN (
 
 -- To check for values above.
 /*
-DECLARE @KdNr int = 31205;
-DECLARE @DateCorrect date = CAST(N'2018-05-17' AS date);
-DECLARE @DateWrong date = CAST(N'2018-05-15' AS date);
+DECLARE @KdNr int = 30092;
+DECLARE @DateCorrect date = CAST(N'2018-12-25' AS date);
+DECLARE @DateWrong date = CAST(N'2018-12-21' AS date);
 
 SELECT DISTINCT LsKo.LsNr, LsKo.Datum, LsKo.MemoIntern, LsKo.FahrtID, LsKo.TourenID, LsKo.Folge
 FROM LsKo, Vsa, Kunden, LsKoArt, LsPo, Scans
@@ -38,12 +38,12 @@ WHERE LsKo.VsaID = Vsa.ID
   --AND Scans.DateTime BETWEEN CAST(N'2018-05-15 00:00:00' AS datetime) AND CAST(N'2018-05-15 23:59:00' AS datetime)
 ;
 
-SELECT N'Wrong' AS Type, LsKo.ID, LsKo.LsNr, LsKo.Status, LsKo.VsaID, LsKo.TraegerID, LsKo.Datum, LsKo.UrDatum, LsKo.FahrtID, LsKo.TourenID, LsKo.Folge, LsKo.MemoIntern
+SELECT N'Wrong' AS Type, LsKo.ID, LsKo.LsNr, LsKo.Status, LsKo.VsaID, LsKo.TraegerID, LsKo.Datum, LsKo.UrDatum, LsKo.FahrtID, LsKo.TourenID, LsKo.Folge, LsKo.MemoIntern, LsKo.UrDatum
 FROM LsKo
-WHERE LsKo.LsNr = 25946474
+WHERE LsKo.LsNr = 27193848
 UNION
-SELECT N'Right' AS Type, LsKo.ID, LsKo.LsNr, LsKo.Status, LsKo.VsaID, LsKo.TraegerID, LsKo.Datum, LsKo.UrDatum, LsKo.FahrtID, LsKo.TourenID, LsKo.Folge, LsKo.MemoIntern
+SELECT N'Right' AS Type, LsKo.ID, LsKo.LsNr, LsKo.Status, LsKo.VsaID, LsKo.TraegerID, LsKo.Datum, LsKo.UrDatum, LsKo.FahrtID, LsKo.TourenID, LsKo.Folge, LsKo.MemoIntern, LsKo.UrDatum
 FROM LsKo
-WHERE LsKo.LsNr = 25946473;
+WHERE LsKo.LsNr = 27193850;
 
 */
