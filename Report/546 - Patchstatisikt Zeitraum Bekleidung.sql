64a549937f04bddf1e90d400a244ec193128ea4b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-01-23 15:09:16 +0100

    made several changes to display results better (removing multiple lines for the same combination of customer, vsa and article) and added a criteria for what constitutes a new item

--- Report/546 - Patchstatisikt Zeitraum Bekleidung.sql
+++ Report/546 - Patchstatisikt Zeitraum Bekleidung.sql
@@ -1,26 +1,29 @@
-SELECT Standort.Bez AS Lagerstandort, Firma.Bez AS Firma, KdGF.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, y.SuchCode AS VsaStichwort, y.VsaBez AS VsaBezeichnung, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, AnzGebr, AnzNeu, WertNeu, AnzNeuNeukunde, WertNeuNeukunde, Mitarbei.Nachname AS Betreuer
-FROM KdGF, Kunden, Artikel, KdBer, Mitarbei, Firma, LagerArt, Standort, (
-  SELECT Vsa.KundenID, ArtGroe.ArtikelID, Vsa.SuchCode, Vsa.Bez AS VsaBez, x.LagerArtID,
-    SUM(IIF(Neukunde = 1, IIF(Neu = 1, Anzahl, 0), 0)) AnzNeuNeukunde,
-    SUM(IIF(Neukunde = 0, IIF(Neu = 1, Anzahl, 0), 0)) AnzNeu,
+WITH PatchTeile AS (
+  SELECT Teile.VsaID, Teile.ArtGroeID, LagerArt.ID AS LagerArtID, LagerArt.LagerID,
+    IIF(LagerArt.Neuwertig = 1 OR Teile.Erstwoche = Teile.Indienst, 1, 0) AS Neu,
+    IIF(Teile.EinsatzGrund IN ('1', '2'), 1, 0) AS Neukunde,
+    COUNT(*) Anzahl
+  FROM Teile
+  JOIN LagerArt ON Teile.LagerArtID = LagerArt.ID
+  WHERE Teile.Patchdatum BETWEEN $1$ AND $2$
+    AND Teile.LagerArtID > 0
+    AND Teile.KaufwareModus <> 1
+    AND Teile.KaufwareModus <> 2
+  GROUP BY Teile.VsaID, Teile.ArtGroeID, LagerArt.ID, LagerArt.LagerID, IIF(LagerArt.Neuwertig = 1 OR Teile.ErstWoche = Teile.Indienst, 1, 0), IIF(Teile.EinsatzGrund IN ('1', '2'), 1, 0)
+)
+SELECT y.Lagerstandort, Firma.Bez AS Firma, KdGF.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, y.SuchCode AS VsaStichwort, y.VsaBez AS VsaBezeichnung, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, y.AnzGebr, y.AnzNeu, y.WertNeu, y.AnzNeuNeukunde, y.WertNeuNeukunde, Mitarbei.Nachname AS Betreuer
+FROM KdGF, Kunden, Artikel, KdBer, Mitarbei, Firma, (
+  SELECT Vsa.KundenID, ArtGroe.ArtikelID, Vsa.SuchCode, Vsa.Bez AS VsaBez, Standort.Bez AS Lagerstandort,
+    SUM(IIF(Neukunde = 1 AND Neu = 1, Anzahl, 0)) AnzNeuNeukunde,
+    SUM(IIF(Neukunde = 0 AND Neu = 1, Anzahl, 0)) AnzNeu,
     SUM(IIF(Neu = 0, Anzahl, 0)) AnzGebr,
-    SUM(CONVERT(money, IIF(Neukunde = 0, IIF(Neu = 1, Anzahl * ArtGroe.EkPreis, 0), 0))) WertNeu,
-    SUM(CONVERT(money, IIF(Neukunde = 1, IIF(Neu = 1, Anzahl * ArtGroe.EkPreis, 0), 0))) WertNeuNeukunde
-  FROM Vsa, ArtGroe, (
-    SELECT Teile.VsaID, Teile.ArtGroeID, Teile.LagerArtID,
-      IIF(Teile.LagerartID IN (SELECT ID FROM LagerArt WHERE Neuwertig = $TRUE$), 1, 0) AS Neu,
-      IIF(Teile.EinsatzGrund IN ('1', '2'), 1, 0) AS Neukunde,
-      COUNT(*) Anzahl
-    FROM Teile
-    WHERE Teile.Patchdatum BETWEEN $1$ AND $2$
-      AND Teile.LagerArtID > 0
-      AND Teile.KaufwareModus <> 1
-      AND Teile.KaufwareModus <> 2
-    GROUP BY Teile.VsaID, Teile.ArtGroeID, Teile.LagerArtID, Teile.EinsatzGrund
-  ) AS x
-  WHERE x.ArtGroeID = ArtGroe.ID
-    AND x.VsaID = Vsa.ID
-  GROUP BY Vsa.KundenID, ArtGroe.ArtikelID, Vsa.SuchCode, Vsa.Bez, x.LagerArtID
+    SUM(CONVERT(money, IIF(Neukunde = 0 AND Neu = 1, Anzahl * ArtGroe.EkPreis, 0))) WertNeu,
+    SUM(CONVERT(money, IIF(Neukunde = 1 AND Neu = 1, Anzahl * ArtGroe.EkPreis, 0))) WertNeuNeukunde
+  FROM Vsa, ArtGroe, PatchTeile, Standort
+  WHERE PatchTeile.ArtGroeID = ArtGroe.ID
+    AND PatchTeile.VsaID = Vsa.ID
+    AND PatchTeile.LagerID = Standort.ID
+  GROUP BY Vsa.KundenID, ArtGroe.ArtikelID, Vsa.SuchCode, Vsa.Bez, Standort.Bez
 ) AS y
 WHERE y.KundenID = Kunden.ID
   AND y.ArtikelID = Artikel.ID
@@ -30,6 +33,4 @@ WHERE y.KundenID = Kunden.ID
   AND KdBer.KundenID = Kunden.ID
   AND KdBer.BereichID = 100
   AND Kunden.FirmaID = Firma.ID
-  AND y.LagerArtID = LagerArt.ID
-  AND LagerArt.LagerID = Standort.ID
 ORDER BY Firma, SGF, Kunden.KdNr;
