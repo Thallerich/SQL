2535f5f57e504f06a92125cafb1739c8a6e3e1fd
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-01-30 10:35:40 +0100

    modified according to ticket IT-22169

--- Diverse/Artikelstamm-Auswertung.sql
+++ Diverse/Artikelstamm-Auswertung.sql
@@ -1,3 +1,15 @@
+WITH HadBestand AS (
+  SELECT DISTINCT LagerBew.BestandID, CAST(1 AS bit) AS Bestand12Monate
+  FROM LagerBew
+  WHERE LagerBew.Differenz <> 0
+    AND DATEDIFF(month, LagerBew.Zeitpunkt, GETDATE()) <= 12
+),
+HadKundenbestand AS (
+  SELECT DISTINCT Teile.ArtGroeID, LagerArt.LagerID, CAST(1 AS bit) AS Kunde12Monate
+  FROM Teile
+  JOIN LagerArt ON Teile.LagerArtID = LagerArt.ID
+  WHERE DATEDIFF(month, Teile.AusdienstDat, GETDATE()) <= 12
+)
 SELECT Artikel.ArtikelNr,
   Artikel.ArtikelBez AS Artikelbezeichnung,
   Artikel.SuchCode2 AS [Artikelbezeichnung 2],
@@ -23,13 +35,15 @@ SELECT Artikel.ArtikelNr,
   [Qualität] = 
     CASE LagerArt.Neuwertig
       WHEN 1 THEN N'Neuware'
-      WHEN 2 THEN N'Gebrauchtware'
+      WHEN 0 THEN N'Gebrauchtware'
       ELSE NULL
     END,
   Standort.Bez AS [Lager-Standort],
   SUM(ISNULL(Bestand.Bestand, 0)) AS Lagerbestand,
   SUM(ISNULL(Bestand.Umlauf, 0)) AS Kundenstand,
-  SUM(ISNULL(Bestand.EntnahmeJahr, 0)) AS [Lagerabgang letzte 12 Monate]
+  SUM(ISNULL(Bestand.EntnahmeJahr, 0)) AS [Lagerabgang letzte 12 Monate],
+  ISNULL(HadBestand.Bestand12Monate, CAST(0 AS bit)) AS [War Bestand in den letzten 12 Monaten vorhanden?],
+  ISNULL(HadKundenbestand.Kunde12Monate, CAST(0 AS bit)) AS [War beim Kunden im Bestand in den letzten 12 Monaten?]
 FROM Artikel
 LEFT OUTER JOIN ArtGroe ON ArtGroe.ArtikelID = Artikel.ID
 JOIN Farbe ON Artikel.FarbeID = Farbe.ID
@@ -51,6 +65,10 @@ LEFT OUTER JOIN LiefTage AS LiefTageGroe ON ArtGroe.LiefTageID = LiefTageGroe.ID
 LEFT OUTER JOIN Bestand ON Bestand.ArtGroeID = ArtGroe.ID
 LEFT OUTER JOIN LagerArt ON Bestand.LagerArtID = LagerArt.ID
 LEFT OUTER JOIN Standort ON LagerArt.LagerID = Standort.ID
+LEFT OUTER JOIN HadBestand ON HadBestand.BestandID = Bestand.ID
+LEFT OUTER JOIN HadKundenbestand ON HadKundenbestand.ArtGroeID = Bestand.ArtGroeID AND HadKundenbestand.LagerID = Standort.ID
 WHERE Artikel.ID > 0
   AND Artikel.ArtiTypeID = 1
-GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez, Artikel.SuchCode2, Artikel.ArtikelNr2, Farbe.FarbeBez, Farbe2.FarbeBez, ArtMust.ArtMustBez, ArtKoll.ArtKollBez, ArtMisch.ArtMischBez, Bereich.BereichBez, ArtGru.ArtGruBez, ProdGru.ProdGruBez, WaschPrg.Bez, LiefArt.LiefArtBez, Abc.ABC, Artikel.StueckGewicht, ProdHier.ProdHierBez, IIF(LiefGroe.ID < 0 OR LiefGroe.ID IS NULL, LiefArtikel.SuchCode, LiefGroe.SuchCode), IIF(LiefTageGroe.ID < 0 OR LiefTageGroe.ID IS NULL, LiefTageArtikel.LiefTageBez, LiefTageGroe.LiefTageBez), Artikel.BestNr, Artikel.EKPreis, ArtGroe.Groesse, CASE LagerArt.Neuwertig WHEN 1 THEN N'Neuware' WHEN 2 THEN N'Gebrauchtware' ELSE NULL END, Standort.Bez;
+  AND Bestand.ID IS NOT NULL
+  AND (Bestand.Bestand <> 0 OR Bestand.Umlauf <> 0 OR Bestand.EntnahmeJahr <> 0 OR HadBestand.Bestand12Monate = 1 OR HadKundenbestand.Kunde12Monate = 1)
+GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez, Artikel.SuchCode2, Artikel.ArtikelNr2, Farbe.FarbeBez, Farbe2.FarbeBez, ArtMust.ArtMustBez, ArtKoll.ArtKollBez, ArtMisch.ArtMischBez, Bereich.BereichBez, ArtGru.ArtGruBez, ProdGru.ProdGruBez, WaschPrg.Bez, LiefArt.LiefArtBez, Abc.ABC, Artikel.StueckGewicht, ProdHier.ProdHierBez, IIF(LiefGroe.ID < 0 OR LiefGroe.ID IS NULL, LiefArtikel.SuchCode, LiefGroe.SuchCode), IIF(LiefTageGroe.ID < 0 OR LiefTageGroe.ID IS NULL, LiefTageArtikel.LiefTageBez, LiefTageGroe.LiefTageBez), Artikel.BestNr, Artikel.EKPreis, ArtGroe.Groesse, CASE LagerArt.Neuwertig WHEN 1 THEN N'Neuware' WHEN 0 THEN N'Gebrauchtware' ELSE NULL END, Standort.Bez, ISNULL(HadBestand.Bestand12Monate, CAST(0 AS bit)), ISNULL(HadKundenbestand.Kunde12Monate, CAST(0 AS bit));
