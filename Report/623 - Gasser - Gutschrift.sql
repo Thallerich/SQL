40975ac55f40345a18f83396a1dc6c675d4cc65a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-02-06 11:31:50 +0100

    changed percentages for customer 30974

--- Report/623 - Gasser - Gutschrift.sql
+++ Report/623 - Gasser - Gutschrift.sql
@@ -1,7 +1,4 @@
-IF object_id('tempdb..#TmpGasser') IS NOT NULL
-BEGIN
-  DROP TABLE #TmpGasser;
-END
+DROP TABLE IF EXISTS #TmpGasser;
 
 SELECT LsDaten.KdNr, LsDaten.SuchCode, Bereich.Bereich, IIF(Kdarti.Variante = 'G', CONCAT(RTRIM(Bereich.BereichBez$LAN$),' 15%'), IIF(KdArti.Variante = 'Z', CONCAT(RTRIM(Bereich.BereichBez$LAN$), ' 100%'), RTRIM(Bereich.BereichBez$LAN$))) AS BereichBez, SUM(LsDaten.Menge) AS LsMenge, LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2 AS RabattSkonto, CONVERT(money, SUM(LsDaten.Menge * LsDaten.EPreis)) AS RechSum, CONVERT(money, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2)) AS RabattSum, CONVERT(money, IIF(Kdarti.Variante = 'G', SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 85, IIF(KdArti.Variante = 'Z', SUM(LsDaten.Menge * LsDaten.EPreis), SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 70)) - (IIF(Kdarti.Variante = 'G', SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 85, IIF(KdArti.Variante = 'Z', SUM(LsDaten.Menge * LsDaten.EPreis), SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 70)) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2))) AS Gasser, CONVERT(money, IIF(Kdarti.Variante = 'G', SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 15, IIF(KdArti.Variante = 'Z', 0, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 30)) - (IIF(Kdarti.Variante = 'G', SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 15, IIF(KdArti.Variante = 'Z', 0, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 30)) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2))) AS Wozabal
 INTO #TmpGasser
@@ -13,6 +10,7 @@ FROM KdArti, Artikel, KdBer, Bereich, ZahlZiel, (
     AND Vsa.KundenID = Kunden.ID
     AND Kunden.SichtbarID = 51
     AND LsKo.Datum BETWEEN $1$ AND $2$
+    AND Kunden.KdNr NOT IN (30974)
 ) AS LsDaten
 WHERE LsDaten.KdArtiID = KdArti.ID
   AND KdArti.ArtikelID = Artikel.ID
@@ -23,7 +21,29 @@ WHERE LsDaten.KdArtiID = KdArti.ID
   AND LsDaten.Kostenlos = 0
 GROUP BY LsDaten.KdNr, LsDaten.SuchCode, Bereich.Bereich, IIF(Kdarti.Variante = 'G', CONCAT(RTRIM(Bereich.BereichBez$LAN$),' 15%'), IIF(KdArti.Variante = 'Z', CONCAT(RTRIM(Bereich.BereichBez$LAN$), ' 100%'), RTRIM(Bereich.BereichBez$LAN$))), Kdarti.Variante, LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2
 
-UNION
+UNION ALL
+
+SELECT LsDaten.KdNr, LsDaten.SuchCode, Bereich.Bereich, IIF(Kdarti.Variante = 'G', CONCAT(RTRIM(Bereich.BereichBez$LAN$),' 15%'), IIF(KdArti.Variante = 'Z', CONCAT(RTRIM(Bereich.BereichBez$LAN$), ' 100%'), RTRIM(Bereich.BereichBez$LAN$))) AS BereichBez, SUM(LsDaten.Menge) AS LsMenge, LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2 AS RabattSkonto, CONVERT(money, SUM(LsDaten.Menge * LsDaten.EPreis)) AS RechSum, CONVERT(money, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2)) AS RabattSum, CONVERT(money, IIF(Kdarti.Variante = 'G', SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 85, IIF(KdArti.Variante = 'Z', SUM(LsDaten.Menge * LsDaten.EPreis), SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 75)) - (IIF(Kdarti.Variante = 'G', SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 85, IIF(KdArti.Variante = 'Z', SUM(LsDaten.Menge * LsDaten.EPreis), SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 75)) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2))) AS Gasser, CONVERT(money, IIF(Kdarti.Variante = 'G', SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 15, IIF(KdArti.Variante = 'Z', 0, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 25)) - (IIF(Kdarti.Variante = 'G', SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 15, IIF(KdArti.Variante = 'Z', 0, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 25)) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2))) AS Wozabal
+FROM KdArti, Artikel, KdBer, Bereich, ZahlZiel, (
+  SELECT LsKo.VsaID, LsPo.KdArtiID, LsPo.Kostenlos, LsPo.Menge, LsPo.EPreis, Kunden.ZahlZielID, Kunden.KdNr, Kunden.SuchCode, Kunden._ManRabatt
+  FROM LsPo, LsKo, Vsa, Kunden
+  WHERE LsPo.LsKoID = LsKo.ID
+    AND LsKo.VsaID = Vsa.ID
+    AND Vsa.KundenID = Kunden.ID
+    AND Kunden.SichtbarID = 51
+    AND LsKo.Datum BETWEEN $1$ AND $2$
+    AND Kunden.KdNr IN (30974)
+) AS LsDaten
+WHERE LsDaten.KdArtiID = KdArti.ID
+  AND KdArti.ArtikelID = Artikel.ID
+  AND KdArti.KdBerID = KdBer.ID
+  AND KdBer.BereichID = Bereich.ID
+  AND LsDaten.ZahlZielID = ZahlZiel.ID
+  AND Bereich.Bereich IN ('SH', 'IK', 'TW', 'EW', 'EWB')
+  AND LsDaten.Kostenlos = 0
+GROUP BY LsDaten.KdNr, LsDaten.SuchCode, Bereich.Bereich, IIF(Kdarti.Variante = 'G', CONCAT(RTRIM(Bereich.BereichBez$LAN$),' 15%'), IIF(KdArti.Variante = 'Z', CONCAT(RTRIM(Bereich.BereichBez$LAN$), ' 100%'), RTRIM(Bereich.BereichBez$LAN$))), Kdarti.Variante, LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2
+
+UNION ALL
 
 SELECT LsDaten.KdNr, LsDaten.SuchCode, Bereich.Bereich, RTRIM(Bereich.BereichBez$LAN$) + ' 14,28%' AS BereichBez, SUM(LsDaten.Menge) AS LsMenge, LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2 AS RabattSkonto, CONVERT(money, SUM(LsDaten.Menge * LsDaten.EPreis)) AS RechSum, CONVERT(money, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2)) AS RabattSum, CONVERT(money, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 85.72 - (SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 85.72) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2)) AS Gasser, CONVERT(money, SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 14.28 - (SUM(LsDaten.Menge * LsDaten.EPreis) / 100 * 14.28) / 100 * (LsDaten._ManRabatt + KdBer.RabattWasch + ZahlZiel.Skonto * 1.2)) AS Wozabal
 FROM KdArti, Artikel, KdBer, Bereich, ZahlZiel, (
--- Report/624 - Gasser - Gutschrift Artikel.sql
+++ Report/624 - Gasser - Gutschrift Artikel.sql
@@ -1,7 +1,4 @@
-IF object_id('tempdb..#TmpGasser') IS NOT NULL
-BEGIN
-  DROP TABLE #TmpGasser;
-END
+DROP TABLE IF EXISTS #TmpGasser;
 
 SELECT Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Bereich.Bereich, IIF(Kdarti.Variante = 'G', CONCAT(RTRIM(Bereich.BereichBez$LAN$),' 15%'), IIF(KdArti.Variante = 'Z', CONCAT(RTRIM(Bereich.BereichBez$LAN$), ' 100%'), RTRIM(Bereich.BereichBez$LAN$))) AS BereichBez, SUM(LsPo.Menge) AS LsMenge, CONVERT(money, SUM(LsPo.Menge * LsPo.EPreis)) AS RechSum, CONVERT(money, IIF(Kdarti.Variante = 'G', SUM(LsPo.Menge * LsPo.EPreis) / 100 * 85, IIF(KdArti.Variante = 'Z', SUM(LsPo.Menge * LsPo.EPreis), SUM(LsPo.Menge * LsPo.EPreis) / 100 * 70))) AS Gasser, CONVERT(money, IIF(Kdarti.Variante = 'G', SUM(LsPo.Menge * LsPo.EPreis) / 100 * 15, IIF(KdArti.Variante = 'Z', 0, SUM(LsPo.Menge * LsPo.EPreis) / 100 * 30))) AS Wozabal
 INTO #TmpGasser
@@ -17,9 +14,28 @@ WHERE LsPo.LsKoID = LsKo.ID
   AND LsKo.Datum BETWEEN $1$ AND $2$ -- Datumsbereich, welche Lieferscheine berücksichtigt werden
   AND Bereich.Bereich IN ('SH', 'IK', 'TW', 'EW', 'EWB')
   AND LsPo.Kostenlos = 0
+  AND Kunden.KdNr NOT IN (30974)
 GROUP BY Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Bereich.Bereich, Bereich.BereichBez$LAN$, Kdarti.Variante
 
-UNION
+UNION ALL
+
+SELECT Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Bereich.Bereich, IIF(Kdarti.Variante = 'G', CONCAT(RTRIM(Bereich.BereichBez$LAN$),' 15%'), IIF(KdArti.Variante = 'Z', CONCAT(RTRIM(Bereich.BereichBez$LAN$), ' 100%'), RTRIM(Bereich.BereichBez$LAN$))) AS BereichBez, SUM(LsPo.Menge) AS LsMenge, CONVERT(money, SUM(LsPo.Menge * LsPo.EPreis)) AS RechSum, CONVERT(money, IIF(Kdarti.Variante = 'G', SUM(LsPo.Menge * LsPo.EPreis) / 100 * 85, IIF(KdArti.Variante = 'Z', SUM(LsPo.Menge * LsPo.EPreis), SUM(LsPo.Menge * LsPo.EPreis) / 100 * 75))) AS Gasser, CONVERT(money, IIF(Kdarti.Variante = 'G', SUM(LsPo.Menge * LsPo.EPreis) / 100 * 15, IIF(KdArti.Variante = 'Z', 0, SUM(LsPo.Menge * LsPo.EPreis) / 100 * 25))) AS Wozabal
+FROM LsPo, LsKo, Vsa, Kunden, KdArti, Artikel, KdBer, Bereich
+WHERE LsPo.LsKoID = LsKo.ID
+  AND LsKo.VsaID = Vsa.ID
+  AND Vsa.KundenID = Kunden.ID
+  AND LsPo.KdArtiID = KdArti.ID
+  AND KdArti.ArtikelID = Artikel.ID
+  AND KdArti.KdBerID = KdBer.ID
+  AND KdBer.BereichID = Bereich.ID
+  AND Kunden.SichtbarID = 51
+  AND LsKo.Datum BETWEEN $1$ AND $2$ -- Datumsbereich, welche Lieferscheine berücksichtigt werden
+  AND Bereich.Bereich IN ('SH', 'IK', 'TW', 'EW', 'EWB')
+  AND LsPo.Kostenlos = 0
+  AND Kunden.KdNr IN (30974)
+GROUP BY Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Bereich.Bereich, Bereich.BereichBez$LAN$, Kdarti.Variante
+
+UNION ALL
 
 SELECT Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Bereich.Bereich, RTRIM(Bereich.BereichBez$LAN$) + ' 14,28%' AS BereichBez, SUM(LsPo.Menge) AS LsMenge, CONVERT(money, SUM(LsPo.Menge * LsPo.EPreis)) AS RechSum, CONVERT(money, SUM(LsPo.Menge * LsPo.EPreis) / 100 * 85.72) AS Gasser, CONVERT(money, SUM(LsPo.Menge * LsPo.EPreis) / 100 * 14.28) AS Wozabal
 FROM LsPo, LsKo, Vsa, Kunden, KdArti, Artikel, KdBer, Bereich
