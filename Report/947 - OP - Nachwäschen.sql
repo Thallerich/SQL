83185220f9091255c0b3f97d31b8d96db4298895
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-05-06 14:37:58 +0200

    added customer and delivery point information

--- Report/947 - OP - Nachwäschen.sql
+++ Report/947 - OP - Nachwäschen.sql
@@ -1,14 +1,16 @@
 DECLARE @from datetime2 = CAST($1$ AS datetime);
 DECLARE @to datetime2 = CAST(DATEADD(day, 1, $2$) AS datetime);
 
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, CAST(OPScans.Zeitpunkt AS date) AS Tag, ZielNr.ZielNrBez AS [Nachwäsche-Grund], COUNT(DISTINCT OPTeile.ID) AS [Anzahl Nachwäsche-Teile], COUNT(OPScans.ID) AS [Anzahl Nachwäschen]
+SELECT Kunden.KdNr, IIF(Kunden.ID < 0, N'(unbekannt)', Kunden.SuchCode) AS Kunde, Vsa.VsaNr, IIF(Vsa.ID < 0, N'(unbekannt)', Vsa.Bez) AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, CAST(OPScans.Zeitpunkt AS date) AS Tag, ZielNr.ZielNrBez AS [Nachwäsche-Grund], COUNT(DISTINCT OPTeile.ID) AS [Anzahl Nachwäsche-Teile], COUNT(OPScans.ID) AS [Anzahl Nachwäschen]
 FROM OPScans
 JOIN OPTeile ON OPScans.OPTeileID = OPTeile.ID
 JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
 JOIN ZielNr ON OPScans.ZielNrID = ZielNr.ID
 JOIN Mitarbei ON OPScans.AnlageUserID_ = Mitarbei.ID
+JOIN Vsa ON OPTeile.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
 WHERE OPScans.Zeitpunkt BETWEEN @from AND @to
   AND OPScans.ZielNrID IN (10000020, 10000021, 10000022, 10000019, 10000031)
   AND Mitarbei.StandortID IN ($3$)
-GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez, CAST(OPScans.Zeitpunkt AS date), ZielNr.ZielNrBez
+GROUP BY Kunden.KdNr, IIF(Kunden.ID < 0, N'(unbekannt)', Kunden.SuchCode), Vsa.VsaNr, IIF(Vsa.ID < 0, N'(unbekannt)', Vsa.Bez), Artikel.ArtikelNr, Artikel.ArtikelBez, CAST(OPScans.Zeitpunkt AS date), ZielNr.ZielNrBez
 ORDER BY Tag, [Nachwäsche-Grund], Artikelbezeichnung;
