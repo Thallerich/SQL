70bf2b02df2d8e7c58ab69115a6f76bf2ff0faeb
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-05-10 11:39:09 +0200

    only list active users

--- Report/1050 - Mitarbeiter-Barcodeliste.sql
+++ Report/1050 - Mitarbeiter-Barcodeliste.sql
@@ -2,4 +2,5 @@ SELECT Mitarbei.Name, Mitarbei.Barcode AS MABarcode
 FROM Mitarbei
 WHERE Mitarbei.Funktion = 'Containerverheiratung'
   AND Mitarbei.FirmaID IN ($1$)
+  AND Mitarbei.Status = N'A'
 ORDER BY Mitarbei.Name ASC;
