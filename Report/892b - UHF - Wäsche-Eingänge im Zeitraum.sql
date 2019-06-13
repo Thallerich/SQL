3b87ad8051fb7ad4166525a78cb83e9242c174bc
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-06-13 13:15:02 +0200

    initial version

---
+++ Report/892b - UHF - Wäsche-Eingänge im Zeitraum.sql
@@ -0,0 +1,31 @@
+DECLARE @KundenID int = $ID$;
+DECLARE @EingelesenVon datetime = CAST($1$ AS datetime);
+DECLARE @EingelesenBis datetime = CAST(DATEADD(day, 1, $2$) AS datetime);
+
+DROP TABLE IF EXISTS #Eingangsscans;
+
+SELECT MAX(OPScans.ID) AS OPScansID_Out, OPScans.OPTeileID, Eingangsscans.OPScansID AS OPScansID_In
+INTO #Eingangsscans
+FROM OPScans
+JOIN (
+  SELECT OPScans.ID AS OPScansID, OPScans.OPTeileID, OPScans.Zeitpunkt
+  FROM OPScans
+  WHERE OPScans.Zeitpunkt BETWEEN @EingelesenVon AND @EingelesenBis
+    AND OPScans.Menge = 1
+) AS Eingangsscans ON OPScans.OPTeileID = Eingangsscans.OPTeileID AND OPScans.Zeitpunkt < Eingangsscans.Zeitpunkt
+WHERE OPScans.Menge = -1
+GROUP BY OPScans.OPTeileID, Eingangsscans.OPScansID;
+
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, OPScans_In.Zeitpunkt AS [Einlesezeitpunkt], ZielNr.ZielNrBez AS [Einlese-Ort], LsKo.LsNr AS [geliefert mit LsNr], LsKo.Datum AS [geliefert am], OPTeile.Code AS Chipcode
+FROM #Eingangsscans AS Eingangsscans
+JOIN OPTeile ON Eingangsscans.OPTeileID = OPTeile.ID
+JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+JOIN OPScans AS OPScans_Out ON Eingangsscans.OPScansID_Out = OPScans_Out.ID
+JOIN OPScans AS OPScans_In ON Eingangsscans.OPScansID_In = OPScans_In.ID
+JOIN ZielNr ON OPScans_In.ZielNrID = ZielNr.ID
+JOIN AnfPo ON OPScans_Out.AnfPoID = AnfPo.ID
+JOIN AnfKo ON AnfPo.AnfKoID = AnfKo.ID
+JOIN LsKo ON AnfKo.LsKoID = LsKo.ID
+JOIN Vsa ON LsKo.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+WHERE Kunden.ID = @KundenID;
