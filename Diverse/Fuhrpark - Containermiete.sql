d802053144caa1b38428c19b95a3bbbcb0d401e6
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-07-12 09:42:12 +0200

    - list all containers currently at customer - dynamically calcualte date to show if fee is applied

--- Diverse/Fuhrpark - Containermiete.sql
+++ Diverse/Fuhrpark - Containermiete.sql
@@ -1,10 +1,9 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS VSA, Abteil.Abteilung AS Kostenstelle, Abteil.Bez AS Kostenstellenbezeichnung, Contain.Barcode, Artikel.ArtikelBez AS Container, ContHist.Zeitpunkt AS [Abgeladen bei VSA], CAST(IIF(CAST(ContHist.Zeitpunkt AS DATE) < CAST('2019-06-16' AS DATE), 1, 0) AS bit) AS [berechnen]
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS VSA, Abteil.Abteilung AS Kostenstelle, Abteil.Bez AS Kostenstellenbezeichnung, Contain.Barcode, Artikel.ArtikelBez AS Container, ContHist.Zeitpunkt AS [Abgeladen bei VSA], CAST(IIF(CAST(ContHist.Zeitpunkt AS DATE) < DATEADD(dd, -28, GETDATE()), 1, 0) AS bit) AS [berechnen]
 FROM (
   SELECT ID, KundenID, Ausgang
   FROM CONTAIN
-  WHERE CAST(contain.Ausgang AS DATE) < CAST('2019-06-16' AS DATE)
-    AND KundenID IN (
-      SELECT ID FROM Kunden WHERE KdNr IN (25007, 3113, 19100, 30354, 30512)
+  WHERE KundenID IN (
+      SELECT DISTINCT KundenID FROM KdArti WHERE KdArti.ArtikelID = (SELECT ID FROM Artikel WHERE ArtikelNr = N'CONTMIET')
     )
   ) CT
 JOIN ContHist ON CT.KundenID = ContHist.KundenID AND CT.ID = ContHist.ContainID
@@ -14,6 +13,6 @@ LEFT JOIN Vsa ON Vsa.ID = ContHist.VsaID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Abteil ON Vsa.AbteilID = Abteil.ID
 WHERE ContHist.Zeitpunkt > CT.Ausgang
-  --AND CAST(ContHist.Zeitpunkt AS DATE) < CAST('2019-06-16' AS DATE)
+  --AND CAST(ContHist.Zeitpunkt AS DATE) < CAST('2019-04-01' AS DATE)
   AND ContHist.STATUS = 'e'
 ORDER BY KdNr, VsaNr, Kostenstelle, Container, [Abgeladen bei VSA];
