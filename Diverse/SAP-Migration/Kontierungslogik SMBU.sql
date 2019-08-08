05a9b9103b95ba8c55a6369ba5ff074f2eba03b0
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-08-08 16:09:39 +0200

    prepare for SMKR

--- Diverse/SAP-Migration/Kontierungslogik SMBU.sql
+++ Diverse/SAP-Migration/Kontierungslogik SMBU.sql
@@ -14,25 +14,26 @@ DECLARE @KontologikSMBU TABLE (
 );
 
 INSERT INTO @KontologikSMBU
-SELECT DISTINCT RPoKonto.BereichID, RPoKonto.ArtGruID, -1 AS BrancheID, RPoKonto.RPoTypeID, 5256 AS FirmaID, RPoKonto.KdGfID, RPoKonto.MwStID, RPoKonto.Art, N'automatisch angelegt - STHA 28.09.2018' AS Bez, RPoKonto.KontenID, RPoKonto.RKoTypeID, RPoKonto.AbwKostenstelle
+SELECT DISTINCT RPoKonto.BereichID, RPoKonto.ArtGruID, RPoKonto.BrancheID, RPoKonto.RPoTypeID, 5269 AS FirmaID, RPoKonto.KdGfID, 11 AS MwStID, RPoKonto.Art, N'automatisch angelegt - STHA 08.08.2019' AS Bez, RPoKonto.KontenID, RPoKonto.RKoTypeID, RPoKonto.AbwKostenstelle
 FROM RPoKonto
-WHERE RPoKonto.FirmaID = 5001;
+WHERE RPoKonto.FirmaID = 5260
+  AND RPoKonto.MWStID = 1;
 
-UPDATE @KontologikSMBU SET AbwKostenstelle = N'1310' WHERE AbwKostenstelle = N'1100';
+/* UPDATE @KontologikSMBU SET AbwKostenstelle = N'1310' WHERE AbwKostenstelle = N'1100';
 UPDATE @KontologikSMBU SET AbwKostenstelle = N'1310' WHERE AbwKostenstelle = N'1200';
 UPDATE @KontologikSMBU SET AbwKostenstelle = N'1310' WHERE AbwKostenstelle = N'1300';
 UPDATE @KontologikSMBU SET AbwKostenstelle = N'1338' WHERE AbwKostenstelle = N'1330';
 UPDATE @KontologikSMBU SET AbwKostenstelle = N'1338' WHERE AbwKostenstelle = N'1331';
 UPDATE @KontologikSMBU SET AbwKostenstelle = N'1338' WHERE AbwKostenstelle = N'1333';
 UPDATE @KontologikSMBU SET AbwKostenstelle = N'1338' WHERE AbwKostenstelle = N'1334';
-UPDATE @KontologikSMBU SET AbwKostenstelle = N'1310' WHERE AbwKostenstelle = N'1340';
+UPDATE @KontologikSMBU SET AbwKostenstelle = N'1310' WHERE AbwKostenstelle = N'1340'; */
 
-SELECT RPoKonto.*
+/* SELECT RPoKonto.*
 INTO __RPoKonto_SMBU_BMD
 FROM RPoKonto
-WHERE RPoKonto.FirmaID = 5256;
+WHERE RPoKonto.FirmaID = 5256; */
 
-DELETE FROM RPoKonto WHERE FirmaID = 5256;
+DELETE FROM RPoKonto WHERE FirmaID = 5269;
 
 INSERT INTO RPoKonto (BereichID, ArtGruID, BrancheID, RPoTypeID, FirmaID, KdGfID, MWStID, Art, Bez, KontenID, RKoTypeID, AbwKostenstelle)
 SELECT * FROM @KontologikSMBU;
