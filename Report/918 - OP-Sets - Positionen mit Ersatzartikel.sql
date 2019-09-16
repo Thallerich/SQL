11e7fcc1c5901b00f2e5fd32785bb2f40bed2b08
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-09-16 13:06:46 +0200

    added filter for article status

--- Report/918 - OP-Sets - Positionen mit Ersatzartikel.sql
+++ Report/918 - OP-Sets - Positionen mit Ersatzartikel.sql
@@ -1,10 +1,21 @@
-SELECT Artikel.ArtikelNr AS SetArtikelNr, Artikel.ArtikelBez$LAN$ AS SetArtikelBez, OPSets.Position, OPSets.Menge, Inhalt.ArtikelNr AS InhaltArtikelNr, Inhalt.ArtikelBez$LAN$ AS InhaltArtikelBez, Ersatz1.ArtikelNr AS Ersatz1ArtikelNr, Ersatz1.ArtikelBez$LAN$ AS Ersatz1ArtikelBez, Ersatz2.ArtikelNr AS Ersatz2ArtikelNr, Ersatz2.ArtikelBez$LAN$ AS Ersatz2ArtikelBez, Ersatz3.ArtikelNr AS Ersatz3ArtikelNr, Ersatz3.ArtikelBez$LAN$ AS Ersatz3ArtikelBez, Ersatz4.ArtikelNr AS Ersatz4ArtikelNr, Ersatz4.ArtikelBez$LAN$ AS Ersatz4ArtikelBez
-FROM OPSets, Artikel, Artikel AS Inhalt, Artikel AS Ersatz1, Artikel AS Ersatz2, Artikel AS Ersatz3, Artikel AS Ersatz4
+WITH Artikelstatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'ARTIKEL')
+)
+SELECT Artikel.ArtikelNr AS SetArtikelNr, Artikel.ArtikelBez$LAN$ AS SetArtikelBez, Artikelstatus.StatusBez AS SetArtikelStatus, OPSets.Position, OPSets.Menge, Inhalt.ArtikelNr AS InhaltArtikelNr, Inhalt.ArtikelBez$LAN$ AS InhaltArtikelBez, Ersatz1.ArtikelNr AS Ersatz1ArtikelNr, Ersatz1.ArtikelBez$LAN$ AS Ersatz1ArtikelBez, Ersatz2.ArtikelNr AS Ersatz2ArtikelNr, Ersatz2.ArtikelBez$LAN$ AS Ersatz2ArtikelBez, Ersatz3.ArtikelNr AS Ersatz3ArtikelNr, Ersatz3.ArtikelBez$LAN$ AS Ersatz3ArtikelBez, Ersatz4.ArtikelNr AS Ersatz4ArtikelNr, Ersatz4.ArtikelBez$LAN$ AS Ersatz4ArtikelBez
+FROM OPSets, Artikel, Artikel AS Inhalt, Artikel AS Ersatz1, Artikel AS Ersatz2, Artikel AS Ersatz3, Artikel AS Ersatz4, Artikelstatus
 WHERE OPSets.ArtikelID = Artikel.ID
   AND OPSets.Artikel1ID = Inhalt.ID
   AND OPSets.Artikel2ID = Ersatz1.ID
   AND OPSets.Artikel3ID = Ersatz2.ID
   AND OPSets.Artikel4ID = Ersatz3.ID
   AND OPSets.Artikel5ID = Ersatz4.ID
+  AND Artikel.Status = Artikelstatus.Status
   AND (OPSets.Artikel2ID > 0 OR OPSets.Artikel3ID > 0 OR OPSets.Artikel4ID > 0 OR OPSets.Artikel5ID > 0)
+  AND (
+    ($1$ = 1 AND Artikel.Status < N'E')
+    OR
+    ($1$ = 0)
+  )
 ORDER BY SetArtikelNr, OPSets.Position;
