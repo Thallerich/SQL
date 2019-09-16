d66683b28cc48dea97cf768aa265df60420555da
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-09-16 10:30:26 +0200

    separate inko-versorgung from flachwäsche

--- Rechnungsanlagen/Dienstleistungsübersicht_neu.sql
+++ Rechnungsanlagen/Dienstleistungsübersicht_neu.sql
@@ -1,5 +1,6 @@
-SELECT RechPo.Bez AS Positionsbezeichnung, RechKo.RechNr, RechKo.Debitor, RechKo.EffektivBis, RechKo.RechDat, SUM(RechPo.GPreis) AS Positionssumme
-FROM RechPo, RechKo
-WHERE RechPo.RechKoID = RechKo.ID
-  AND RechKo.ID = $RECHKOID$
-GROUP BY RechPo.Bez, RechKo.RechNr, RechKo.Debitor, RechKo.EffektivBis, RechKo.RechDat;
+SELECT IIF(ArtGru.Gruppe IN (N'IK1', N'IK2', N'SH12', N'SH13', N'DRY'), N'Inko-Versorgung', RechPo.Bez) AS Positionsbezeichnung, RechKo.RechNr, RechKo.Debitor, RechKo.EffektivBis, RechKo.RechDat, SUM(RechPo.GPreis) AS Positionssumme
+FROM RechPo
+JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+LEFT OUTER JOIN ArtGru ON RechPo.ArtGruID = ArtGru.ID
+WHERE RechKo.ID = $RECHKOID$
+GROUP BY IIF(ArtGru.Gruppe IN (N'IK1', N'IK2', N'SH12', N'SH13', N'DRY'), N'Inko-Versorgung', RechPo.Bez), RechKo.RechNr, RechKo.Debitor, RechKo.EffektivBis, RechKo.RechDat;
