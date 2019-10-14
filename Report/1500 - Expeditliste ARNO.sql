bb16ee5744b2c389079656f7a1fe9ee57d708069
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-10-14 14:29:01 +0200

    initial version

---
+++ Report/1500 - Expeditliste ARNO.sql
@@ -0,0 +1,12 @@
+DECLARE @Wochentag int = (SELECT IIF(DATEPART(dw, $2$) > 1, DATEPART(dw, $2$) - 1, 7));
+
+SELECT DISTINCT Touren.Tour, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.Bez AS VsaBez
+FROM VsaTour
+JOIN Vsa ON VsaTour.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN Touren ON VsaTour.TourenID = Touren.ID
+JOIN KdBer ON VsaTour.KdBerID = KdBer.ID
+JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND KdBer.BereichID = StandBer.BereichID
+WHERE StandBer.ExpeditionID = $1$
+  AND Touren.Wochentag = @Wochentag
+ORDER BY Touren.Tour, Kunden.KdNr, VsaBez;
