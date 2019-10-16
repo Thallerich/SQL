bce14e29b68e4515324a69f10e5fd1bf086e06bf
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-10-16 10:09:06 +0200

    initial version

---
+++ SAL-MIG/Benutzer von Live auf Mig kopieren.sql
@@ -0,0 +1,43 @@
+INSERT INTO [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.Mitarbei (SichtbarID, StandortID, MitarAbtID, Status, MaNr, Name, Nachname, Vorname, Initialen, Strasse, Land, PLZ, Ort, GeoX, GeoY, GeoScore, GeoNode, GeoRefAID, Telefon, Mobil, Memo, KdGFID, Betreuer, Vertreter, Kundendienst, Fahrer, TeleMarketing, KartenFarbe, Provision, Rolle, Artikel, EintrittAm, AustrittAm, SollWoPa, Position, eMail, FTE, ProvisionFaktor, FahrzeugID, Barcode, PdaUser, PdaPassword, FahrtenStart, FahrtenEnd, Frei1, Frei2, Frei3, Frei4, Frei5, Frei6, Frei7, ChefMitarbeiID, FreigabeGS, Statistik1, Statistik2, Statistik3, NotInHistory, Kostenstelle, FahrerPKW, FahrerC1, FahrerC1E, FahrerC, FahrerCE, FahrerC1bis, FahrerC1Ebis, FahrerCbis, FahrerCEbis, FahrerB, FahrerBE, FahrerS, FahrerQuali, FahrerQualiBis, FahrerkartenNr, Fuehrerscheinnummer, FahrerkarteGueltigBis, FahrerkarteGueltigVon, Modul1, Modul2, Modul3, Modul4, Modul5, FirmaID, MitarbeiUser, Password, UserName, LastLogin, LastLogout, LoginCount, AutoLogin, BdeUser, ClickLogin, LanguageID, LastNewsID, ShowNews, Telefax, SmtpHost, SmtpUser, SMTPPassword, SmtpPass, CheckWinUser, ExportAllowed, ReklamMess, NoWorkflow, ReklamMail, ServicemailMess, Autorespond, AutorespMemo, CheckForUpdates, PrintPreviewDefault, AdvUserLevel, SqlDebugLevel, OnlyModule, ConfigureMode, SpaeterBuchen, IsAdvanTexUser, PdaTraining, PatchUpdateAllowed, PrintName, PrintTelefon, PrintEMail, PrintMobil, PrintMemo, DistributionMode, MitarTypID, KzDashKoID, EditHelp, WelcomeStartUp, WelcomeMsg, PreviewTranslation, GoCalGuid, GKalenderRefToken, TapiOpenSalesMod, TranslationMode, MsgForwardAnEMail, UseDiag, AutoStartPatchScript, OpenDashboardOnLogin, Funktionszusatz, MaxAnzMandantCon, InProjectPlan, Geburtstag, Team, Funktion, Anrede, Titel, Geschlecht, DoNotCreateMgrEvent, Image32, Image64, Image256, Image256B, ZeigeSeparateFenster, SID, AuftragCloseZusammenfass, RechKoService)
+SELECT MITARBEI.SichtbarID, MITARBEI.StandortID, MITARBEI.MitarAbtID, MITARBEI.Status, MITARBEI.MaNr, MITARBEI.Name, MITARBEI.Nachname, MITARBEI.Vorname, MITARBEI.Initialen, MITARBEI.Strasse, MITARBEI.Land, MITARBEI.PLZ, MITARBEI.Ort, MITARBEI.GeoX, MITARBEI.GeoY, MITARBEI.GeoScore, MITARBEI.GeoNode, MITARBEI.GeoRefAID, MITARBEI.Telefon, MITARBEI.Mobil, MITARBEI.Memo, MITARBEI.KdGFID, MITARBEI.Betreuer, MITARBEI.Vertreter, MITARBEI.Kundendienst, MITARBEI.Fahrer, MITARBEI.TeleMarketing, MITARBEI.KartenFarbe, MITARBEI.Provision, MITARBEI.Rolle, MITARBEI.Artikel, MITARBEI.EintrittAm, MITARBEI.AustrittAm, MITARBEI.SollWoPa, MITARBEI.Position, MITARBEI.eMail, MITARBEI.FTE, MITARBEI.ProvisionFaktor, MITARBEI.FahrzeugID, MITARBEI.Barcode, MITARBEI.PdaUser, MITARBEI.PdaPassword, MITARBEI.FahrtenStart, MITARBEI.FahrtenEnd, MITARBEI.Frei1, MITARBEI.Frei2, MITARBEI.Frei3, MITARBEI.Frei4, MITARBEI.Frei5, MITARBEI.Frei6, MITARBEI.Frei7, MITARBEI.ChefMitarbeiID, MITARBEI.FreigabeGS, MITARBEI.Statistik1, MITARBEI.Statistik2, MITARBEI.Statistik3, MITARBEI.NotInHistory, MITARBEI.Kostenstelle, MITARBEI.FahrerPKW, MITARBEI.FahrerC1, MITARBEI.FahrerC1E, MITARBEI.FahrerC, MITARBEI.FahrerCE, MITARBEI.FahrerC1bis, MITARBEI.FahrerC1Ebis, MITARBEI.FahrerCbis, MITARBEI.FahrerCEbis, MITARBEI.FahrerB, MITARBEI.FahrerBE, MITARBEI.FahrerS, MITARBEI.FahrerQuali, MITARBEI.FahrerQualiBis, MITARBEI.FahrerkartenNr, MITARBEI.Fuehrerscheinnummer, MITARBEI.FahrerkarteGueltigBis, MITARBEI.FahrerkarteGueltigVon, MITARBEI.Modul1, MITARBEI.Modul2, MITARBEI.Modul3, MITARBEI.Modul4, MITARBEI.Modul5, MITARBEI.FirmaID, MITARBEI.MitarbeiUser, MITARBEI.Password, MITARBEI.UserName, MITARBEI.LastLogin, MITARBEI.LastLogout, MITARBEI.LoginCount, MITARBEI.AutoLogin, MITARBEI.BdeUser, MITARBEI.ClickLogin, MITARBEI.LanguageID, MITARBEI.LastNewsID, MITARBEI.ShowNews, MITARBEI.Telefax, MITARBEI.SmtpHost, MITARBEI.SmtpUser, MITARBEI.SMTPPassword, MITARBEI.SmtpPass, MITARBEI.CheckWinUser, MITARBEI.ExportAllowed, MITARBEI.ReklamMess, MITARBEI.NoWorkflow, MITARBEI.ReklamMail, MITARBEI.ServicemailMess, MITARBEI.Autorespond, MITARBEI.AutorespMemo, MITARBEI.CheckForUpdates, MITARBEI.PrintPreviewDefault, MITARBEI.AdvUserLevel, MITARBEI.SqlDebugLevel, MITARBEI.OnlyModule, MITARBEI.ConfigureMode, MITARBEI.SpaeterBuchen, MITARBEI.IsAdvanTexUser, MITARBEI.PdaTraining, MITARBEI.PatchUpdateAllowed, MITARBEI.PrintName, MITARBEI.PrintTelefon, MITARBEI.PrintEMail, MITARBEI.PrintMobil, MITARBEI.PrintMemo, MITARBEI.DistributionMode, MITARBEI.MitarTypID, MITARBEI.KzDashKoID, MITARBEI.EditHelp, MITARBEI.WelcomeStartUp, MITARBEI.WelcomeMsg, MITARBEI.PreviewTranslation, MITARBEI.GoCalGuid, MITARBEI.GKalenderRefToken, MITARBEI.TapiOpenSalesMod, MITARBEI.TranslationMode, MITARBEI.MsgForwardAnEMail, MITARBEI.UseDiag, MITARBEI.AutoStartPatchScript, MITARBEI.OpenDashboardOnLogin, MITARBEI.Funktionszusatz, MITARBEI.MaxAnzMandantCon, MITARBEI.InProjectPlan, MITARBEI.Geburtstag, MITARBEI.Team, MITARBEI.Funktion, MITARBEI.Anrede, MITARBEI.Titel, MITARBEI.Geschlecht, MITARBEI.DoNotCreateMgrEvent, MITARBEI.Image32, MITARBEI.Image64, MITARBEI.Image256, MITARBEI.Image256B, MITARBEI.ZeigeSeparateFenster, MITARBEI.SID, MITARBEI.AuftragCloseZusammenfass, MITARBEI.RechKoService
+FROM Mitarbei
+WHERE Mitarbei.MaNr LIKE N'SMS%'
+  AND NOT EXISTS (
+    SELECT MA.*
+    FROM [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.Mitarbei AS MA
+    WHERE MA.UserName = Mitarbei.UserName
+  );
+
+GO
+
+INSERT INTO [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.SichtUsr (SichtbarID, Sehen, UserID, Special0, Special1, Special2, Special3, Special4, Special5, Special6, Special7, Special8, Special9)
+SELECT SICHTUSR.SichtbarID, SICHTUSR.Sehen, TMA.ID AS UserID, SICHTUSR.Special0, SICHTUSR.Special1, SICHTUSR.Special2, SICHTUSR.Special3, SICHTUSR.Special4, SICHTUSR.Special5, SICHTUSR.Special6, SICHTUSR.Special7, SICHTUSR.Special8, SICHTUSR.Special9
+FROM SichtUsr
+JOIN Mitarbei ON SichtUsr.UserID = Mitarbei.ID
+JOIN [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.Mitarbei AS TMA ON TMA.UserName = Mitarbei.UserName
+WHERE Mitarbei.MaNr LIKE N'SMS%'
+  AND NOT EXISTS (
+    SELECT SU.*
+    FROM [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.SichtUsr AS SU
+    JOIN [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.Mitarbei AS MA ON SU.UserID = MA.ID
+    WHERE MA.MaNr LIKE N'SMS%'
+      AND SU.UserID = SichtUsr.UserID
+      AND SU.SichtbarID = SichtUsr.SichtbarID
+  );
+
+GO
+
+INSERT INTO [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.UsrInGrp (MitarbeiID, UserGrpID)
+SELECT TMA.ID AS MitarbeiID, USRINGRP.UserGrpID
+FROM UsrInGrp
+JOIN Mitarbei ON UsrInGrp.MitarbeiID = Mitarbei.ID
+JOIN [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.Mitarbei AS TMA ON TMA.UserName = Mitarbei.UserName
+WHERE Mitarbei.MaNr LIKE N'SMS%'
+  AND NOT EXISTS (
+    SELECT UIG.*
+    FROM [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.UsrInGrp AS UIG
+    JOIN [SVATWMENADVMIG1.SAL.CO.AT\ADVANTEX].Wozabal_ARNOMATT.dbo.Mitarbei AS MA ON UIG.MitarbeiID = MA.ID
+    WHERE MA.MaNr LIKE N'SMS%'
+  );
+
+GO
