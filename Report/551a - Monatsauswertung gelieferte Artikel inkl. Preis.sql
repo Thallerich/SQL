a4af3ac8a0c4e3b06c4e7a9c6d2b700ec55940b2
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-10-17 10:09:00 +0200

    initial version - copied from 551 and added price columns

---
+++ Report/551a - Monatsauswertung gelieferte Artikel inkl. Preis.sql
@@ -0,0 +1,12 @@
+SELECT Kunden.SuchCode, Vsa.Bez AS VsaBezeichnung, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, SUM(LsPo.Menge) AS Menge, LsPo.EPreis AS Einzelpreis, CAST(SUM(LsPo.Menge * LsPo.EPreis) AS money) AS Preis
+FROM LsPo, LsKo, Vsa, Kunden, KdArti, Artikel
+WHERE LsPo.LsKoID = LsKo.ID
+  AND LsKo.VsaID = Vsa.ID
+  AND Vsa.KundenID = Kunden.ID
+  AND LsPo.KdArtiID = KdArti.ID
+  AND KdArti.ArtikelID = Artikel.ID
+  AND Kunden.ID IN ($3$)
+  AND LsKo.Datum BETWEEN $1$ AND $2$
+  AND Kunden.SichtbarID IN ($SICHTBARIDS$)
+GROUP BY Kunden.SuchCode, Vsa.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, LsPo.EPreis
+ORDER BY VsaBezeichnung ASC, ArtikelNr ASC
