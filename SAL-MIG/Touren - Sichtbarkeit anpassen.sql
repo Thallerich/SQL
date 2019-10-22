2bf2bf301f0fe2430f1d295f30e38abd223ec8bd
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-10-22 08:50:27 +0200

    also update expeditionid

--- SAL-MIG/Touren - Sichtbarkeit anpassen.sql
+++ SAL-MIG/Touren - Sichtbarkeit anpassen.sql
@@ -1,5 +1,12 @@
-UPDATE Touren SET Touren.SichtbarID = (SELECT ID FROM Sichtbar WHERE Bez = N'Grödig')
+UPDATE Touren SET Touren.SichtbarID = (SELECT ID FROM Sichtbar WHERE Bez = N'Grödig'), Touren.ExpeditionID = (SELECT ID FROM Standort WHERE Bez LIKE N'BU SMS:%')
 FROM Touren
 JOIN Sichtbar ON Touren.SichtbarID = Sichtbar.ID
+WHERE Touren.Tour LIKE N'_-32___'
+  AND SUBSTRING(Touren.Tour, 6, 1) <> N'-';
+
+SELECT Touren.Tour, Touren.Bez AS TourBez, Sichtbar.Bez AS Sichtbarkeit, Standort.SuchCode AS StandortKurz, Standort.Bez AS Standort
+FROM Touren
+JOIN Sichtbar ON Touren.SichtbarID = Sichtbar.ID
+JOIN Standort ON Touren.ExpeditionID = Standort.ID
 WHERE Touren.Tour LIKE N'_-32___'
   AND SUBSTRING(Touren.Tour, 6, 1) <> N'-';
