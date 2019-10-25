8c6a850b76ecdac31d7ec2fc11651af53f1fc4e3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-10-25 08:13:21 +0200

    initial version

---
+++ SAL-MIG/Kunden - Patch-Regel eintragen.sql
@@ -0,0 +1,22 @@
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, PatchArt.PatchArtBez AS PatchSchnittstelle
+FROM Kunden
+JOIN PatchArt ON Kunden.PatchArtID = PatchArt.ID
+WHERE Kunden.StandortID = (SELECT Standort.ID FROM Standort WHERE Standort.SuchCode = N'SMS')
+  AND EXISTS (
+    SELECT Teile.*
+    FROM Teile
+    JOIN Vsa ON Teile.VsaID = Vsa.ID
+    WHERE Vsa.KundenID = Kunden.ID
+  );
+
+UPDATE Kunden SET PatchArtID = (SELECT PatchArt.ID FROM PatchArt WHERE PatchArt.PatchArtBez = N'TT4 KR20 (Salesianer)')
+FROM Kunden
+JOIN PatchArt ON Kunden.PatchArtID = PatchArt.ID
+WHERE Kunden.StandortID = (SELECT Standort.ID FROM Standort WHERE Standort.SuchCode = N'SMS')
+  AND Kunden.PatchArtID < 0
+  AND EXISTS (
+    SELECT Teile.*
+    FROM Teile
+    JOIN Vsa ON Teile.VsaID = Vsa.ID
+    WHERE Vsa.KundenID = Kunden.ID
+  );
