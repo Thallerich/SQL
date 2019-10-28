843da551702b9c390a96aa1cadaf865c912d10b0
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-10-28 07:27:48 +0100

    update patchart on more customers

--- SAL-MIG/Kunden - Patch-Regel eintragen.sql
+++ SAL-MIG/Kunden - Patch-Regel eintragen.sql
@@ -20,3 +20,20 @@ WHERE Kunden.StandortID IN (SELECT Standort.ID FROM Standort WHERE Standort.Such
     JOIN Vsa ON Teile.VsaID = Vsa.ID
     WHERE Vsa.KundenID = Kunden.ID
   );
+
+  --SELECT Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.SuchCode, Vsa.Bez
+  UPDATE Kunden SET PatchArtID = (SELECT ID FROM PatchArt WHERE PatchArtBez = N'TT4 KR20 (Salesianer)')
+  FROM Kunden
+  JOIN Vsa ON Vsa.KundenID = Kunden.ID
+  JOIN PatchArt ON Kunden.PatchArtID = PatchArt.ID
+  JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND StandBer.BereichID = 100
+  JOIN Standort ON StandBer.ProduktionID = Standort.ID
+  WHERE Standort.SuchCode = N'SMS'
+    AND EXISTS (
+      SELECT Traeger.*
+      FROM Traeger
+      WHERE Traeger.Status <> N'I'
+        AND Traeger.VsaID = Vsa.ID
+        AND Traeger.Altenheim = 0
+    )
+    AND PatchArt.PatchArtBez <> N'TT4 KR20 (Salesianer)';
