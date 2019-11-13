ba4b6675db765347c66c6de98d74782e812ae398
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-11-13 09:03:41 +0100

    fill empty counts with zero value

--- Diverse/RepQueue-Auswertung.sql
+++ Diverse/RepQueue-Auswertung.sql
@@ -1,7 +1,7 @@
-SELECT TableName, Priority, [11] AS Enns, [3] AS [Enns 2], [1] AS [Lenzing 1], [2] AS [Lenzing 2], [21] AS Klagenfurt 
+SELECT TableName, Priority, ISNULL([11], 0) AS Enns, ISNULL([3], 0) AS [Enns 2], ISNULL([1], 0) AS [Lenzing 1], ISNULL([2], 0) AS [Lenzing 2], ISNULL([21], 0) AS Klagenfurt 
 FROM (
   SELECT SdcDev.ID AS SdcDevID, RepQueue.TableName, RepQueue.Priority, COUNT(DISTINCT RepQueue.TableID) AS AnzahlDS
-  FROM RepQueue
+  FROM RepQueue WITH (NOLOCK)
   JOIN SdcDev ON RepQueue.SdcDevID = SdcDev.ID
   GROUP BY SdcDev.ID, RepQueue.TableName, RepQueue.Priority
 ) AS RepQueueData
