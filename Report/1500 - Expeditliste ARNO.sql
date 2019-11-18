d92140923de76866d3700da327c6a054ff2e3f19
Thallerich <s.thaller@outlook.com>
2019-11-18 16:19:00 +0100

    Update 1500 - Expeditliste ARNO.sql
    only show VSAs that have vsaanf-rows
    additionally show VSAs that have a row in anfko but do not actually have a regular tour for this day

--- Report/1500 - Expeditliste ARNO.sql
+++ Report/1500 - Expeditliste ARNO.sql
@@ -17,4 +17,28 @@ WHERE StandBer.ExpeditionID = $1$
       AND AnfKo.Lieferdatum = $2$
       AND AnfKo.LsKoID < 0            -- VSAs, für die der Packzettel bereits abgeschlossen wurde, ausblenden
   )
+  AND EXISTS (
+    SELECT VsaAnf.*
+    FROM VsaAnf
+    WHERE VsaAnf.VsaID = Vsa.ID
+  )
+  
+UNION ALL
+
+SELECT DISTINCT Touren.Tour, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.Bez AS VsaBez
+FROM AnfKo
+JOIN Vsa ON AnfKo.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN Touren ON AnfKo.TourenID = Touren.ID
+JOIN VsaBer ON VsaBer.VsaID = Vsa.ID
+JOIN KdBer ON VsaBer.KdBerID = KdBer.ID
+JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND KdBer.BereichID = StandBer.BereichID
+WHERE StandBer.ExpeditionID = $1$
+  AND AnfKo.Lieferdatum = $2$
+  AND NOT EXISTS (
+    SELECT VsaTour.*
+    FROM VsaTour
+    WHERE VsaTour.VsaID = Vsa.ID
+      AND VsaTour.TourenID = Touren.ID
+  )
 ORDER BY Touren.Tour, Kunden.KdNr, VsaBez;
