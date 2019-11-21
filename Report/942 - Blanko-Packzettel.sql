f1fb505bcbe214b804d63e422f9b53fc1c802979
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-11-21 13:50:06 +0100

    updated version for SMS

--- Report/942 - Blanko-Packzettel.sql
+++ Report/942 - Blanko-Packzettel.sql
@@ -1,14 +1,44 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS Variantenbezeichnung, ArtGroe.Groesse, VsaAnf.Durchschnitt AS [durchschnittliche Liefermenge], CAST(NULL AS int) AS [Menge]
-FROM VsaAnf
-JOIN Vsa ON VsaAnf.VsaID = Vsa.ID
-JOIN Kunden ON Vsa.KundenID = Kunden.ID
-JOIN KdArti ON VsaAnf.KdArtiID = KdArti.ID
-JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-JOIN KdBer ON KdArti.KdBerID = KdBer.ID
-JOIN Bereich ON KdBer.BereichID = Bereich.ID
-JOIN VsaBer ON VsaBer.KdBerID = KdArti.KdBerID AND VsaBer.VsaID = Vsa.ID
-LEFT OUTER JOIN ArtGroe ON VsaAnf.ArtGroeID = ArtGroe.ID
-WHERE Vsa.ID = $ID$
-  AND VsaAnf.[Status] < N'E'
-  AND VsaBer.[Status] = N'A'
-ORDER BY Bereich.Bereich, Artikel.ArtikelNr, KdArti.Variante;
+SELECT Menge, Artikelbezeichnung, ArtikelNr, VPE, [definierter Bestand], [durchschn. Liefermenge]
+FROM
+(
+  SELECT N'Kunde/Station: ' + CAST(Kunden.KdNr AS nvarchar(20)) + N'/' + Vsa.SuchCode AS Menge, NULL AS Artikelbezeichnung, NULL AS ArtikelNr, NULL AS VPE, NULL AS [definierter Bestand], NULL AS [durchschn. Liefermenge], 1 AS OrderNum
+  FROM Vsa
+  JOIN Kunden ON Vsa.KundenID = Kunden.ID
+  WHERE Vsa.ID = $ID$
+
+  UNION ALL
+
+  SELECT Vsa.Bez AS Menge, NULL AS Artikelbezeichnung, NULL AS ArtikelNr, NULL AS VPE, NULL AS [definierter Bestand], NULL AS [durchschn. Liefermenge], 2 AS OrderNum
+  FROM Vsa
+  WHERE Vsa.ID = $ID$
+
+  UNION ALL
+
+  SELECT N'Anforderung für __.__.____' AS Menge, NULL AS Artikelbezeichnung, NULL AS ArtikelNr, NULL AS VPE, NULL AS [definierter Bestand], NULL AS [durchschn. Liefermenge], 3 AS OrderNum
+
+  UNION ALL
+
+  SELECT N'Bestellt am __.__.____ durch _____________' AS Menge, NULL AS Artikelbezeichnung, NULL AS ArtikelNr, NULL AS VPE, NULL AS [definierter Bestand], NULL AS [durchschn. Liefermenge], 4 AS OrderNum
+
+  UNION ALL
+
+  SELECT N'' AS Menge, NULL AS Artikelbezeichnung, NULL AS ArtikelNr, NULL AS VPE, NULL AS [definierter Bestand], NULL AS [durchschn. Liefermenge], 5 AS OrderNum
+
+  UNION ALL
+
+  SELECT N'Menge' AS Menge, N'Artikelbezeichnung' AS Artikelbezeichnung, N'ArtikelNr' AS ArtikelNr, N'VPE' AS VPE, N'definierter Bestand' AS [definierter Bestand], N'durchschn. Liefermenge' AS [durchschn. Liefermenge], 6 AS OrderNum
+
+  UNION ALL
+
+  SELECT NULL AS Menge, Artikel.ArtikelBez AS Artikelbezeichnung, Artikel.ArtikelNr, CAST(Artikel.PackMenge AS nvarchar(4)) AS VPE, CAST(VsaAnf.Bestand AS nvarchar(10)) AS [definierter Bestand], CAST(VsaAnf.Durchschnitt AS nvarchar(10)) AS [durchschn. Liefermenge], ROW_NUMBER() OVER (ORDER BY Bereich.Bereich, Artikel.ArtikelNr, KdArti.Variante) + 10 AS OrderNum
+  FROM VsaAnf
+  JOIN KdArti ON VsaAnf.KdArtiID = KdArti.ID
+  JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+  JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+  JOIN VsaBer ON VsaBer.KdBerID = KdBer.ID AND VsaBer.VsaID = VsaAnf.VsaID
+  JOIN Bereich ON KdBer.BereichID = Bereich.ID
+  WHERE VsaAnf.VsaID = $ID$
+    AND VsaBer.Status = 'A'
+    AND VsaAnf.Status < 'E'
+) AS x
+ORDER BY OrderNum;
