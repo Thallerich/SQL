2fd879d92a0cf945b68e3927e00cbf306788a170
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-11-25 12:37:22 +0100

    initial version

---
+++ Report/934 - Kunden mit Webportal-Benutzern.sql
@@ -0,0 +1,14 @@
+SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Standort.Bez AS Hauptstandort, COUNT(WebUser.ID) AS [Anzahl Webportal-Benutzer]
+FROM WebUser
+JOIN Kunden ON WebUser.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN Firma ON Kunden.FirmaID = Firma.ID
+JOIN Standort ON Kunden.StandortID = Standort.ID
+WHERE Firma.ID IN ($1$)
+  AND KdGf.ID IN ($2$)
+  AND WebUser.Status = N'A'
+  AND Kunden.Status = N'A'
+  AND Kunden.AdrArtID = 1
+  AND Kunden.SichtbarID IN ($SICHTBARIDS$)
+GROUP BY Firma.SuchCode, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, Standort.Bez
+ORDER BY Firma, Geschäftsbereich, KdNr;
