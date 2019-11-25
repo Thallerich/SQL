a8fdee18f6f3f9111849ef4905508d95cacf81c9
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-11-25 14:23:10 +0100

    added selection by betreuer

--- Report/934 - Kunden mit Webportal-Benutzern.sql
+++ Report/934 - Kunden mit Webportal-Benutzern.sql
@@ -10,5 +10,11 @@ WHERE Firma.ID IN ($1$)
   AND Kunden.Status = N'A'
   AND Kunden.AdrArtID = 1
   AND Kunden.SichtbarID IN ($SICHTBARIDS$)
+  AND EXISTS (
+    SELECT KdBer.*
+    FROM KdBer
+    WHERE KdBer.BetreuerID IN ($3$)
+      AND KdBer.KundenID = Kunden.ID
+  )
 GROUP BY Firma.SuchCode, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, Standort.Bez
 ORDER BY Firma, Geschäftsbereich, KdNr;
