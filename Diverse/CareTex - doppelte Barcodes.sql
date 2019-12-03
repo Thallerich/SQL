9d55e2934aed4d81ae24db0d70c911ba42faeb31
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-12-03 08:29:55 +0100

    extended skript and added update statement

--- Diverse/CareTex - doppelte Barcodes.sql
+++ Diverse/CareTex - doppelte Barcodes.sql
@@ -1,4 +1,5 @@
-SELECT Firma.SuchCode AS Firma, Standort.Bez AS Hauptstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS Vsa, Traeger.Traeger, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, Teile.Barcode, Teile.Eingang1 AS [letzter Eingang], Teile.Ausgang1 AS [letzter Ausgang]
+SELECT Firma.SuchCode AS Firma, Standort.Bez AS Hauptstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Traeger.Traeger, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, Teile.Barcode, Teile.Eingang1 AS [letzter Eingang], Teile.Ausgang1 AS [letzter Ausgang]
+--UPDATE Teile SET Barcode = RTRIM(Barcode) + N'MATT'
 FROM Teile
 JOIN TraeArti ON Teile.TraeArtiID = TraeArti.ID
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
@@ -8,10 +9,20 @@ JOIN Standort ON Kunden.StandortID = Standort.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
 JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+JOIN StandBer ON StandBer.StandKonID = Vsa.StandKonID AND StandBer.BereichID = KdBer.BereichID
+JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
 WHERE Traeger.Altenheim = 1
   AND EXISTS (
     SELECT T.*
     FROM Teile AS T
     WHERE REPLACE(T.Barcode, N'*SAL', N'') = REPLACE(Teile.Barcode, N'*SAL', N'')
       AND T.ID <> Teile.ID
+  )
+  AND Teile.Barcode NOT LIKE N'%*SAL'
+  AND Produktion.Suchcode = N'MATT'
+  AND NOT EXISTS (
+    SELECT Prod.*
+    FROM Prod
+    WHERE Prod.TeileID = Teile.ID
   );
