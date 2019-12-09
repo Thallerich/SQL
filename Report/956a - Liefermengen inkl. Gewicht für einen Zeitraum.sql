9df9c29c0d2de4d14270ae34cc237dc5f2571a5e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-12-09 12:58:38 +0100

    added distinction by customer

--- Report/956a - Liefermengen inkl. Gewicht für einen Zeitraum.sql
+++ Report/956a - Liefermengen inkl. Gewicht für einen Zeitraum.sql
@@ -9,20 +9,12 @@ WHERE LsPo.LsKoID = LsKo.ID
   AND LsPo.ProduktionID IN ($3$)
 GROUP BY LsPo.ProduktionID, LsPo.KdArtiID;
 
-SELECT Standort.Bez AS Produktion, Bereich.BereichBez$LAN$ AS Produktbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Liefermenge.Menge AS Liefermenge, Artikel.StueckGewicht AS [Stückgewicht in kg], Liefermenge.Menge * Artikel.StueckGewicht AS [Liefergewicht in kg]
-INTO #TmpFinal
-FROM #TmpLiefermenge AS Liefermenge, Standort, KdArti, Artikel, Bereich
+SELECT Standort.Bez AS Produktion, Bereich.BereichBez$LAN$ AS Produktbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Liefermenge.Menge AS Liefermenge, Artikel.StueckGewicht AS [Stückgewicht in kg], Liefermenge.Menge * Artikel.StueckGewicht AS [Liefergewicht in kg]
+FROM #TmpLiefermenge AS Liefermenge, Standort, KdArti, Artikel, Bereich, Kunden
 WHERE LieferMenge.ProduktionID = Standort.ID
   AND LieferMenge.KdArtiID = KdArti.ID
   AND KdArti.ArtikelID = Artikel.ID
   AND Artikel.BereichID = Bereich.ID
-  AND Bereich.ID IN ($4$);
-
-SELECT Produktion, Produktbereich, ArtikelNr, Artikelbezeichnung, Liefermenge, [Stückgewicht in kg], [Liefergewicht in kg]
-FROM #TmpFinal
-
-UNION ALL
-
-SELECT 'ZSumme' AS Produktion, '' AS Produktbereich, '' AS ArtikelNr, '' AS Artikelbezeichnung, SUM(Liefermenge) AS Liefermenge, 0 AS [Stückgewicht in kg], SUM([Liefergewicht in kg]) AS [Liefergewicht in kg]
-FROM #TmpFinal
-ORDER BY Produktion, Produktbereich, ArtikelNr;
+  AND KdArti.KundenID = Kunden.ID
+  AND Bereich.ID IN ($4$)
+ORDER BY Produktion, KdNr, Produktbereich, ArtikelNr;
