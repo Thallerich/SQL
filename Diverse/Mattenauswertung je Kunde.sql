3ea9766cea995ed83ebf15f916256d1f1ca8fa4b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-12-11 15:57:15 +0100

    changed to expedition

--- Diverse/Mattenauswertung je Kunde.sql
+++ Diverse/Mattenauswertung je Kunde.sql
@@ -1,6 +1,6 @@
 DECLARE @Week nchar(7) = (SELECT Week.Woche FROM Week WHERE CAST(GETDATE() AS date) BETWEEN Week.VonDat AND Week.BisDat);
 
-SELECT Standort.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez, SUM(VsaLeas.Menge) AS Menge
+SELECT Expedition.Bez AS Expeditionsstandort, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez, SUM(VsaLeas.Menge) AS Menge
 FROM VsaLeas
 JOIN Vsa ON VsaLeas.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -8,8 +8,9 @@ JOIN KdArti ON VsaLeas.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN KdBer ON KdArti.KdBerID = KdBer.ID
 JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND StandBer.BereichID = KdBer.BereichID
-JOIN Standort ON StandBer.ProduktionID = Standort.ID
-WHERE StandBer.ProduktionID IN (SELECT ID FROM Standort WHERE SuchCode IN (N'SMS', N'WOBH', N'WOL1'))
+JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
+JOIN Standort AS Expedition ON StandBer.ExpeditionID = Expedition.ID
+WHERE StandBer.ExpeditionID IN (SELECT ID FROM Standort WHERE SuchCode IN (N'SMS', N'WOBH', N'WOL1'))
   AND KdBer.BereichID = (SELECT ID FROM Bereich WHERE Bereich = N'MA')
   AND @Week BETWEEN VsaLeas.InDienst AND ISNULL(VsaLeas.AusDienst, N'2099/52')
-GROUP BY Standort.Bez, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez;
+GROUP BY Expedition.Bez, Produktion.Bez, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez;
