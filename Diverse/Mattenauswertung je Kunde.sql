40ac7d2f4288d3ab0c3d4a7338eb3a7e5a8a1005
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-12-13 08:52:14 +0100

    added criteria for lenzing hotel

--- Diverse/Mattenauswertung je Kunde.sql
+++ Diverse/Mattenauswertung je Kunde.sql
@@ -10,7 +10,7 @@ JOIN KdBer ON KdArti.KdBerID = KdBer.ID
 JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND StandBer.BereichID = KdBer.BereichID
 JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
 JOIN Standort AS Expedition ON StandBer.ExpeditionID = Expedition.ID
-WHERE StandBer.ExpeditionID IN (SELECT ID FROM Standort WHERE SuchCode IN (N'SMS', N'WOBH', N'WOL1'))
+WHERE (StandBer.ExpeditionID IN (SELECT ID FROM Standort WHERE SuchCode IN (N'SMS', N'WOBH', N'WOL1')) OR Vsa.StandKonID = (SELECT ID FROM StandKon WHERE StandKon.StandKonBez = N'FW: Lenzing Hotel'))
   AND KdBer.BereichID = (SELECT ID FROM Bereich WHERE Bereich = N'MA')
   AND @Week BETWEEN VsaLeas.InDienst AND ISNULL(VsaLeas.AusDienst, N'2099/52')
 GROUP BY Expedition.Bez, Produktion.Bez, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez;
