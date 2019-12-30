121cca975f5ff92cd95c81c0fe379ae79c5b4cc1
Thallerich <20878157+Thallerich@users.noreply.github.com>
2019-12-30 08:10:28 +0100

    also select future breaks

--- Diverse/Lieferpause - falsche Pausen.sql
+++ Diverse/Lieferpause - falsche Pausen.sql
@@ -1,14 +1,15 @@
 DECLARE @Woche nchar(7) = (SELECT Week.Woche FROM Week WHERE CAST(GETDATE() AS date) BETWEEN Week.VonDat AND Week.BisDat);
+DECLARE @Datum date = CAST(GETDATE() AS date);
 
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], VsaPause.VonDatum, VsaPause.VonWoche, VsaPause.BisDatum, VsaPause.BisWoche, VsaPause.IsLieferpause, VsaPause.LeasRabatt, VsaPause.UseSonderpreis, Mitarbei.UserName AS [Anlage-User], VsaPause.Anlage_ AS [Anlage-Zeitpunkt]
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], VsaPause.VonDatum, VsaPause.VonWoche, VsaPause.BisDatum, VsaPause.BisWoche, VsaPause.IsLieferpause, VsaPause.LeasRabatt, VsaPause.UseSonderpreis, Mitarbei.UserName AS [Anlage-User], VsaPause.Anlage_ AS [Anlage-Zeitpunkt], Mitarbei.Name
 FROM VsaPause
 JOIN Vsa ON VsaPause.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Mitarbei ON VsaPause.AnlageUserID_ = Mitarbei.ID
 WHERE (
-  (CAST(GETDATE() AS date) BETWEEN VsaPause.VonDatum AND VsaPause.BisDatum AND VsaPause.VonDatum IS NOT NULL)
+  (VsaPause.BisDatum > @Datum AND VsaPause.VonDatum IS NOT NULL)
   OR
-  (@Woche BETWEEN VsaPause.VonWoche AND VsaPause.BisWoche AND VsaPause.VonWoche IS NOT NULL)
+  (VsaPause.BisWoche > @Woche AND VsaPause.VonWoche IS NOT NULL)
 )
 AND VsaPause.IsLieferpause = 0
 AND VsaPause.LeasRabatt = 0
