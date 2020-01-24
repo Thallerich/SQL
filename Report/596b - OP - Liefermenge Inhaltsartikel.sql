637712329644690c018182f8b33e330cbb544144
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-01-24 10:11:32 +0100

    initial version as per ticket IT-32123

---
+++ Report/596b - OP - Liefermenge Inhaltsartikel.sql
@@ -0,0 +1,26 @@
+DECLARE @von date = $1$;
+DECLARE @bis date = $2$;
+
+WITH OPSchrott AS (
+  SELECT OPTeile.ArtikelID, StandBer.ProduktionID, COUNT(OPTeile.ID) AS SchrottMenge
+  FROM OPTeile
+  JOIN Vsa ON OPTeile.VsaID = Vsa.ID
+  JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+  JOIN StandBer ON StandBer.StandKonID = Vsa.StandKonID AND StandBer.BereichID = Artikel.BereichID
+  WHERE OPTeile.WegDatum BETWEEN @von AND @bis
+    AND StandBer.ProduktionID IN ($3$)
+    AND OPTeile.Status = N'Z'
+  GROUP BY OPTeile.ArtikelID, StandBer.ProduktionID
+)
+SELECT Standort.Bez AS Produktion, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGru.ArtGruBez$LAN$ AS Artikelgruppe, SUM(CAST(LsPo.Menge AS int) * OPSets.Menge) AS Liefermenge, OPSchrott.SchrottMenge AS Schrottmenge
+FROM LsPo
+JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
+JOIN OPSets ON OPSets.ArtikelID = KdArti.ArtikelID
+JOIN Artikel ON OPSets.Artikel1ID = Artikel.ID
+JOIN ArtGru ON Artikel.ArtGruID = ArtGru.ID
+JOIN Standort ON LsPo.ProduktionID = Standort.ID
+LEFT OUTER JOIN OPSchrott ON OPSchrott.ArtikelID = Artikel.ID AND OPSchrott.ProduktionID = Standort.ID
+WHERE LsKo.Datum BETWEEN @von AND @bis
+  AND LsPo.ProduktionID IN ($3$)
+GROUP BY Standort.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, ArtGru.ArtGruBez$LAN$, OPSchrott.SchrottMenge;
