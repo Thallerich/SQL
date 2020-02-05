901c93501f2fdce1865c9b2ca5f355989445c7c1
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-02-05 09:57:05 +0100

    added column Berufsgruppe

--- Report/740 - Rückgabeliste (SGF).sql
+++ Report/740 - Rückgabeliste (SGF).sql
@@ -13,8 +13,8 @@ Kundenstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'KUNDEN')
 )
-SELECT KdGf.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenstatus.StatusBez AS Kundenstatus, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], VsaStatus.StatusBez AS [VSA-Status], Traeger.Nachname, Traeger.Vorname, Traeger.Titel, Traegerstatus.StatusBez AS Trägerstatus, Traeger.SchrankInfo, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Teile.Barcode, Teile.Abmeldung, IIF(Teile.AusDienst='' OR Teile.AusDienst IS NULL, Teile.RestWertInfo, Teile.AusDRestW) AS Restwert
-FROM Teile, Traeger, ArtGroe, Artikel, Vsa, Kunden, KdGf, Traegerstatus, VsaStatus, Kundenstatus
+SELECT KdGf.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenstatus.StatusBez AS Kundenstatus, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], VsaStatus.StatusBez AS [VSA-Status], Traeger.Nachname, Traeger.Vorname, Traeger.Titel, Traegerstatus.StatusBez AS Trägerstatus, IIF(Traeger.BerufsgrKdArtiID < 0, NULL, Berufsgruppe.VariantBez) AS Berufsgruppe, Traeger.SchrankInfo, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Teile.Barcode, Teile.Abmeldung, IIF(Teile.AusDienst='' OR Teile.AusDienst IS NULL, Teile.RestWertInfo, Teile.AusDRestW) AS Restwert
+FROM Teile, Traeger, ArtGroe, Artikel, Vsa, Kunden, KdGf, Traegerstatus, VsaStatus, Kundenstatus, KdArti AS Berufsgruppe
 WHERE Teile.TraegerID = Traeger.ID
   AND Teile.ArtGroeID = ArtGroe.ID
   AND Teile.ArtikelID = Artikel.ID
@@ -24,6 +24,7 @@ WHERE Teile.TraegerID = Traeger.ID
   AND Traeger.Status = Traegerstatus.Status
   AND Vsa.Status = VsaStatus.Status
   AND Kunden.Status = Kundenstatus.Status
+  AND Traeger.BerufsgrKdArtiID = Berufsgruppe.ID
   AND KdGf.ID IN ($1$)  -- Geschäftsfeld
   AND Kunden.ID IN ($2$) -- Kunden abhängig von Geschäftsfeld
   AND Teile.Status = 'W'  -- Rückgabe-Teile
