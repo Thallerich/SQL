79ba5438ea8f83cea2002cc95c16d1e0d6d2c50a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-02-10 14:28:59 +0100

    several additions due to complications

--- Diverse/DCS - Nachfolgeartikel.sql
+++ Diverse/DCS - Nachfolgeartikel.sql
@@ -11,7 +11,7 @@ DECLARE @KdMap TABLE (
   RentomatID int
 );
 
-DECLARE @KdNr int = 23044;
+DECLARE @KdNr int = 31065;
 
 INSERT INTO @ErsatzMap VALUES (N'2501037012', N'25010370121'), (N'2503003011', N'25030030111'), (N'2505000304', N'25050003041'), (N'2505000305', N'25050003051'), (N'2505004418', N'25050044181'), (N'2505004419', N'25050044191'), (N'2505004420', N'25050044201'), (N'2505069505', N'25050695051'), (N'2505501012', N'25055010121'), (N'2505512010', N'25055120101'), (N'2505512220', N'25055122201'), (N'3003448102', N'30034481021'), (N'3060605005', N'30606050051'), (N'3080101005', N'30801010051'), (N'3258100802', N'32581008021'), (N'3258101322', N'32581013221'), (N'3030101001', N'30301010011'), (N'3080101001', N'30801010011'), (N'3080101002', N'30801010021'), (N'2507026270', N'25070262701'), (N'3140103005', N'31401030051'), (N'2501037010', N'25010370101');
 
@@ -32,7 +32,6 @@ JOIN @ErsatzMap AS ErsatzMap ON ErsatzMap.Alt = Artikel.ArtikelNr
 WHERE Rentomat.Interface = N'DCSvoll'
   AND Kunden.KdNr = @KdNr;
 
-
 /* SELECT Kunden.KdNr, Kunden.SuchCode, ArtikelAlt.ArtikelNr, ArtikelAlt.ArtikelBez, ArtikelNeu.ArtikelNr, ArtikelNeu.ArtikelBez, KdArti.Umlauf
 FROM @KdMap AS KdMap
 JOIN Kunden ON KdMap.KundenID = Kunden.ID
@@ -43,7 +42,7 @@ WHERE KdArti.Umlauf > 0; */
 
 
 --SELECT TraeArti.VsaID, TraeArti.TraegerID, NeuArtGroe.ID AS ArtGroeID, KdMap.NeuKdArtiID, 0 AS Menge
-UPDATE TraeArti SET TraeArti.KdArtiID = KdMap.NeuKdArtiID, TraeArti.ArtGroeID = NeuArtGroe.ID
+/* UPDATE TraeArti SET TraeArti.KdArtiID = KdMap.NeuKdArtiID, TraeArti.ArtGroeID = NeuArtGroe.ID
 FROM TraeArti
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
@@ -57,7 +56,7 @@ WHERE Traeger.RentoArtID > 0
     WHERE TA.KdArtiID = KdMap.NeuKdArtiID
       AND TA.ArtGroeID = NeuArtGroe.ID
       AND TA.TraegerID = TraeArti.TraegerID
-  );
+  ); */
 
 
 --SELECT DISTINCT KdAusArt.KdAusstaID, KdMap.NeuKdArtiID, Pos = (SELECT MAX(KAA.Pos) FROM KdAusArt AS KAA WHERE KAA.KdAusstaID = KdAussta.ID) + 10 * DENSE_RANK() OVER (PARTITION BY KdAusArt.KdAusstaID ORDER BY KdAusArt.Pos), KdAusArt.Menge
@@ -71,3 +70,36 @@ WHERE NOT EXISTS (
   WHERE KA.KdAusstaID = KdAusArt.KdAusstaID
     AND KA.KdArtiID = KdMap.NeuKdArtiID
 );
+
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez
+FROM KdArti
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN Kunden ON KdArti.KundenID = Kunden.ID
+WHERE Kunden.KdNr = 31065
+  AND EXISTS (
+    SELECT Teile.*
+    FROM Teile
+    JOIN Vsa ON Teile.VsaID = Vsa.ID
+    JOIN Traeger ON Teile.TraegerID = Traeger.ID
+    WHERE Vsa.RentomatID = 36
+      AND Teile.Status = N'Q'
+      AND Traeger.RentoArtID = 3
+      AND Teile.KdArtiID = KdArti.ID
+  )
+  AND NOT EXISTS (
+    SELECT KdAusArt.*
+    FROM KdAusArt
+    WHERE KdAusArt.KdArtiID = KdArti.ID
+  );
+
+SELECT KdAussta.Bez, KdAusArt.KdAusstaID, KdAusArt.KdArtiID, KdAusArt.Pos, KdAusArt.Menge
+FROM KdAusArt
+JOIN KdAussta ON KdAusArt.KdAusstaID = KdAussta.ID
+JOIN RentoCod ON RentoCod.KdAusstaID = KdAussta.ID
+JOIN KdArti ON KdAusArt.KdArtiID = KdArti.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+WHERE RentoCod.RentomatID = 35
+  AND Artikel.ArtikelNr = N'25050044191';
+
+INSERT INTO KdAusArt (KdAusstaID, KdArtiID, Pos, Menge)
+VALUES (5363, 34794211, 320, 2), (5364, 34794211, 140, 2), (5365, 34794211, 120, 2);
