ad64d2563ef43c1c0af018751750575a9d954a3d
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-02-10 12:22:04 +0100

    fixed incorrect sums

--- Report/921 - Zustellgebühr je Wäscher.sql
+++ Report/921 - Zustellgebühr je Wäscher.sql
@@ -1,4 +1,7 @@
-SELECT Produktion.Bez AS Wäscher, Produktion.SuchCode AS WäscherKurz, Artikel.ArtikelNr, Artikel.ArtikelBez, SUM(RechPo.Menge) AS Menge, SUM(RechPo.GPreis) AS Umsatz
+DROP TABLE IF EXISTS #TmpLsZustell;
+
+SELECT Produktion.Bez AS Wäscher, Produktion.SuchCode AS WäscherKurz, Artikel.ArtikelNr, Artikel.ArtikelBez, LsPo.RechPoID
+INTO #TmpLsZustell
 FROM LsPo
 JOIN RechPo ON LsPo.RechPoID = RechPo.ID
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
@@ -8,5 +11,9 @@ JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 WHERE (UPPER(Artikel.ArtikelBez) LIKE N'%ZUSTELL%' OR UPPER(Artikel.ArtikelBez) LIKE N'%ANFAHR%')
   AND LsPo.RechPoID > 0
   AND RechKo.RechDat BETWEEN $1$ AND $2$
-GROUP BY Produktion.Bez, Produktion.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez
-ORDER BY Wäscher, ArtikelNr;
+GROUP BY Produktion.Bez, Produktion.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez, LsPo.RechPoID;
+
+SELECT LsZustell.Wäscher, LsZustell.WäscherKurz, LsZustell.ArtikelNr, LsZustell.ArtikelBez, SUM(RechPo.Menge) AS Menge, SUM(RechPo.GPreis) AS Umsatz
+FROM #TmpLsZustell AS LsZustell
+JOIN RechPo ON LsZustell.RechPoID = RechPo.ID
+GROUP BY LsZustell.Wäscher, LsZustell.WäscherKurz, LsZustell.ArtikelNr, LsZustell.ArtikelBez;
