d20a7620b862132a7bf535cf55f770f281cba056
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-02-19 14:36:21 +0100

    added yearly sales figure

--- Report/914 - Saisonkunden-Auswertung.sql
+++ Report/914 - Saisonkunden-Auswertung.sql
@@ -15,14 +15,16 @@ WITH AnzLSKunde AS (
     AND LsKo.Datum BETWEEN @von AND @bis
   GROUP BY Vsa.KundenID
 )
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, KdGf.KdGfBez AS Geschäftsbereich, Branche.BrancheBez AS Branche, Firma.SuchCode AS Firma, Standort.Bez AS Haupstandort, ISNULL(AnzLSKunde.AnzLs, 0) AS [Anzahl Lieferscheine]
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, KdGf.KdGfBez AS Geschäftsbereich, Branche.BrancheBez AS Branche, Firma.SuchCode AS Firma, Standort.Bez AS Haupstandort, ISNULL(AnzLSKunde.AnzLs, 0) AS [Anzahl Lieferscheine], SUM(RechKo.NettoWert) AS [Netto-Umsatz 12 Monate]
 FROM Kunden
 JOIN KdGf ON Kunden.KdGFID = Kdgf.ID
 JOIN Branche ON Kunden.BrancheID = Branche.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
 JOIN Standort ON Kunden.StandortID = Standort.ID
+LEFT OUTER JOIN RechKo ON RechKo.KundenID = Kunden.ID AND RechKo.RechDat >= DATEADD(month, -12, GETDATE())
 LEFT OUTER JOIN AnzLSKunde ON AnzLSKunde.KundenID = Kunden.ID
 WHERE ISNULL(AnzLSKunde.AnzLs, 0) <= @MinLS
   AND Standort.ID IN ($3$)
   AND Kunden.Status = N'A'
-  AND Kunden.AdrArtID = 1;
+  AND Kunden.AdrArtID = 1
+GROUP BY Kunden.KdNr, Kunden.SuchCode, KdGf.KdGfBez, Branche.BrancheBez, Firma.SuchCode, Standort.Bez, ISNULL(AnzLSKunde.AnzLs, 0);
