a2303186d34f6a05edd6b778f863942624c0853f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-03-03 13:29:08 +0100

    ...

--- Diverse/BKS - Zusätzlicher Trägerartikel.sql
+++ Diverse/BKS - Zusätzlicher Trägerartikel.sql
@@ -2,14 +2,15 @@ DECLARE @ArtiIDAlt integer;
 DECLARE @ArtiIDNeu integer;
 DECLARE @KundenID integer;
 
-SET @ArtiIDAlt = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'203630010144');
-SET @ArtiIDNeu = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'202504000026');
-SET @KundenID = (SELECT Kunden.ID FROM Kunden WHERE Kunden.Kdnr = 11804);
+SET @ArtiIDAlt = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'3084043303');
+SET @ArtiIDNeu = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'3084043305');
+SET @KundenID = (SELECT Kunden.ID FROM Kunden WHERE Kunden.Kdnr = 31066);
 
--- (VsaID, TraegerID, ArtGroeID, KdArtiID)
+INSERT INTO TraeArti (VsaID, TraegerID, ArtGroeID, KdArtiID, MengeKredit)
 SELECT Vsa.ID AS VsaID, Traeger.ID AS TraegerID, 
   (SELECT Groesse.ID FROM ArtGroe AS Groesse WHERE Groesse.ArtikelID = @ArtiIDNeu AND Groesse.Groesse = ArtGroe.Groesse) AS ArtGroeID,
-  (SELECT KdArti.ID FROM KdArti WHERE KdArti.KundenID = @KundenID AND KdArti.ArtikelID = @ArtiIDNeu) AS KdArtiID
+  (SELECT KdArti.ID FROM KdArti WHERE KdArti.KundenID = @KundenID AND KdArti.ArtikelID = @ArtiIDNeu) AS KdArtiID,
+  TraeArti.MengeKredit
 FROM TraeArti, Traeger, Vsa, KdArti, ArtGroe
 WHERE TraeArti.TraegerID = Traeger.ID
   AND Traeger.VsaID = Vsa.ID
@@ -19,6 +20,7 @@ WHERE TraeArti.TraegerID = Traeger.ID
   AND Vsa.KundenID = @KundenID
   AND Vsa.RentomatID > 0
   AND Traeger.Status = 'A'
+  AND Traeger.RentoArtID = 2
   AND NOT EXISTS (
     SELECT TraeArti.*
     FROM TraeArti, KdArti
