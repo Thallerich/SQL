3cc7ae6969234a3b2218a2c578e484cf77d242c8
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-03-10 10:23:37 +0100

    rewrote to select by production location

--- Report/982 - Memotexte für Kunden eines SGFs - aktive oder zukünftige.sql
+++ Report/982 - Memotexte für Kunden eines SGFs - aktive oder zukünftige.sql
@@ -1,8 +1,23 @@
-SELECT KdGf.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, Status.StatusBez AS Kundenstatus, VsaTexte.VonDatum, VsaTexte.BisDatum, TextArt.TextArtBez$LAN$ AS Textart, VsaTexte.Memo AS Memotext
-FROM VsaTexte, Kunden, KdGf, TextArt, (SELECT Status.Status, Status.StatusBez$LAN$ AS StatusBez FROM Status WHERE Status.Tabelle = 'KUNDEN') AS Status
-WHERE VsaTexte.KundenID = Kunden.ID
-  AND Kunden.KdGfID = KdGf.ID
-  AND Kunden.Status = Status.Status
-  AND VsaTexte.TextArtID = TextArt.ID
-  AND KdGf.ID IN ($1$)
-  AND VsaTexte.BisDatum >= CONVERT(date, GETDATE());
+WITH Kundenstatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'KUNDEN')
+)
+SELECT KdGf.KurzBez AS Geschäftsbereich, Holding.Holding, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenstatus.StatusBez AS Kundenstatus, VsaTexte.VonDatum, VsaTexte.BisDatum, TextArt.TextArtBez$LAN$ AS Textart, VsaTexte.Memo AS Memotext
+FROM VsaTexte
+JOIN Kunden ON VsaTexte.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGFID = KdGf.ID
+JOIN Holding ON Kunden.HoldingID = Holding.ID
+JOIN Kundenstatus ON Kunden.Status = Kundenstatus.Status
+JOIN TextArt ON VsaTexte.TextArtID = TextArt.ID
+WHERE VsaTexte.BisDatum >= CAST(GETDATE() AS date)
+  AND EXISTS (
+    SELECT Vsa.*
+    FROM Vsa
+    JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID
+    WHERE StandBer.ProduktionID = $1$
+      AND Vsa.Status = N'A'
+      AND Vsa.KundenID = Kunden.ID
+  )
+  AND Kunden.Status = N'A'
+  AND Kunden.AdrArtID = 1;
