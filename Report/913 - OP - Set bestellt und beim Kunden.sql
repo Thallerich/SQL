05c8196850916e06eba7cdc272d3650b8efdfae9
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-03-16 08:24:56 +0100

    initial version

---
+++ Report/913 - OP - Set bestellt und beim Kunden.sql
@@ -0,0 +1,22 @@
+WITH OPSetKunde AS (
+  SELECT OPEtiKo.ArtikelID, OPEtiKo.VsaID, COUNT(OPEtiKo.ID) AS Anzahl
+  FROM OPEtiKo
+  WHERE OPEtiKo.[Status] = N'R'
+    AND DATEDIFF(day, OPEtiKo.AusleseZeitpunkt, GETDATE()) < 180
+  GROUP BY OPEtiKo.ArtikelID, OPEtiKo.VsaID
+)
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, AnfPo.Angefordert, VsaAnf.Durchschnitt, ISNULL(OPSetKunde.Anzahl, 0) AS [Sets beim Kunden < 180 Tage]
+FROM AnfPo
+JOIN AnfKo ON AnfPo.AnfKoID = AnfKo.ID
+JOIN Vsa ON AnfKo.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdArti ON AnfPo.KdArtiID = KdArti.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+JOIN VsaAnf ON VsaAnf.KdArtiID = AnfPo.KdArtiID AND VsaAnf.VsaID = AnfKo.VsaID AND VsaAnf.ArtGroeID = AnfPo.ArtGroeID
+LEFT OUTER JOIN OPSetKunde ON OPSetKunde.ArtikelID = Artikel.ID AND OPSetKunde.VsaID = Vsa.ID
+WHERE Vsa.StandKonID IN ($1$)
+  AND AnfKo.LieferDatum = $2$
+  AND (($3$ = 1 AND AnfPo.Angefordert != 0) OR ($3$ = 0))
+  AND AnfKo.[Status] IN (N'F', N'I')
+  AND KdBer.BereichID = (SELECT Bereich.ID FROM Bereich WHERE Bereich.Bereich = N'ST')
