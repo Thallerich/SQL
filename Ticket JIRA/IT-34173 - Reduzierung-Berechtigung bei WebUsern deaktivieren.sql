a13e8a0988b9b90c4f6723aa90d27f8f3093bc34
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-03-30 13:00:40 +0200

    also deactivate "deactivate wearer"

--- Ticket JIRA/IT-34173 - Reduzierung-Berechtigung bei WebUsern deaktivieren.sql
+++ Ticket JIRA/IT-34173 - Reduzierung-Berechtigung bei WebUsern deaktivieren.sql
@@ -1,13 +1,13 @@
-/* CREATE TABLE __WebUserFuncDecAmount (
+CREATE TABLE __WebUserFuncDeactivateW (
   ID int,
   UserName nvarchar(20) COLLATE Latin1_General_CS_AS,
-  FuncDecAmount bit
-); */
+  FuncDeactivateW bit
+);
 
 --SELECT WebUser.ID, WebUser.UserName, WebUser.FuncDecAmount
-UPDATE WebUser SET FuncDecAmount = 0
+UPDATE WebUser SET /*FuncDecAmount = 0,*/ FuncDeactivateW = 0
 OUTPUT inserted.ID, inserted.UserName, deleted.FuncDecAmount
-INTO __WebUserFuncDecAmount
+INTO __WebUserFuncDeactivateW
 FROM WebUser
 JOIN Kunden ON WebUser.KundenID = Kunden.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
@@ -15,4 +15,4 @@ JOIN KdGf ON Kunden.KdGFID = KdGf.ID
 WHERE Firma.Land = N'AT'
   AND KdGf.KurzBez = N'JOB'
   AND WebUser.Status = N'A'
-  AND WebUser.FuncDecAmount = 1;
+  AND WebUser.FuncDeactivateW = 1;
