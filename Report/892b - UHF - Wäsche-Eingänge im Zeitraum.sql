d213f0ed05279fd752e3609a10814bd3dc19e73b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-04-09 12:42:01 +0200

    show last date a part was delivered to a customer, independet of selected date range

--- Report/892b - UHF - Wäsche-Eingänge im Zeitraum.sql
+++ Report/892b - UHF - Wäsche-Eingänge im Zeitraum.sql
@@ -3,16 +3,17 @@ DECLARE @EingelesenVon datetime = CAST($1$ AS datetime);
 DECLARE @EingelesenBis datetime = CAST(DATEADD(day, 1, $2$) AS datetime);
 
 WITH Eingangsscans AS (
-  SELECT MAX(OPScans.ID) AS OPScansID_Out, OPScans.OPTeileID, Eingangsscans.OPScansID AS OPScansID_In
-  FROM OPScans
+  SELECT MAX(Ausgangsscan.ID) AS OPScansID_Out, Ausgangsscan.OPTeileID, Eingangsscan.OPScansID AS OPScansID_In
+  FROM OPScans AS Ausgangsscan
   JOIN (
     SELECT OPScans.ID AS OPScansID, OPScans.OPTeileID, OPScans.Zeitpunkt
     FROM OPScans
     WHERE OPScans.Zeitpunkt BETWEEN @EingelesenVon AND @EingelesenBis
       AND OPScans.Menge = 1
-  ) AS Eingangsscans ON OPScans.OPTeileID = Eingangsscans.OPTeileID AND OPScans.Zeitpunkt < Eingangsscans.Zeitpunkt
-  WHERE OPScans.Menge = -1
-  GROUP BY OPScans.OPTeileID, Eingangsscans.OPScansID
+  ) AS Eingangsscan ON Ausgangsscan.OPTeileID = Eingangsscan.OPTeileID
+  WHERE Ausgangsscan.Menge = -1
+    AND Ausgangsscan.Zeitpunkt > Eingangsscan.Zeitpunkt
+  GROUP BY Ausgangsscan.OPTeileID, Eingangsscan.OPScansID
 )
 SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, OPScans_In.Zeitpunkt AS [Einlesezeitpunkt], ZielNr.ZielNrBez AS [Einlese-Ort], LsKo.LsNr AS [geliefert mit LsNr], LsKo.Datum AS [geliefert am], OPTeile.Code AS Chipcode, CAST(IIF(OPTeile.VsaOwnerID > 0, 1, 0) AS bit) AS [Eigenwäsche?]
 FROM Eingangsscans
