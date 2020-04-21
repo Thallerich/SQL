4e147f42afd040943e0069d0411898be1fa4bc7c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-04-21 10:09:00 +0200

    added more cases to clear up

--- SAL-MIG/VSATexte - leere löschen.sql
+++ SAL-MIG/VSATexte - leere löschen.sql
@@ -1,1 +1,9 @@
-DELETE FROM VsaTexte WHERE REPLACE(REPLACE(Memo, CHAR(10), ''), CHAR(13), '') = '';
+DELETE FROM VsaTexte 
+WHERE REPLACE(REPLACE(Memo, CHAR(10), ''), CHAR(13), '') = ''
+  OR Memo = '.';
+
+UPDATE VsaTexte SET Memo = RIGHT(VsaTexte.Memo, LEN(VsaTexte.Memo) - 2)
+--SELECT VsaTexte.Memo, REPLACE(REPLACE(Memo, CHAR(10), ''), CHAR(13), '<Enter>') AS MemoAlt, RIGHT(VsaTexte.Memo, LEN(VsaTexte.Memo) - 2) AS MemoNeu
+--FROM VsaTexte
+WHERE LEFT(VsaTexte.Memo, 2) = CHAR(13) + CHAR(10)
+  AND VsaTexte.TextArtID = 12;
