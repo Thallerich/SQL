d8d994e0ec4aa5bf4a4cc9c43e0c29d21dd3e7cb
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-05-15 07:18:33 +0200

    select by print date select lists that belong to another warehouse

--- Ticket JIRA/IT-35372 - Entnahmelisten - Anzahl je Tag.sql
+++ Ticket JIRA/IT-35372 - Entnahmelisten - Anzahl je Tag.sql
@@ -1,12 +1,22 @@
-SELECT CAST(EntnKo.PatchDatum AS date) AS Patchdatum, Lager.SuchCode AS LagKurz, Lager.Bez AS Lagerstandort, COUNT(EntnKo.ID) AS [Anzahl Entnahmelisten]
+SELECT CAST(EntnKo.DruckDatum AS date) AS DruckDatum, Lager.SuchCode AS LagKurz, Lager.Bez AS Lagerstandort, COUNT(DISTINCT EntnKo.ID) AS [Anzahl Entnahmelisten]
 FROM EntnKo
+JOIN EntnPo ON EntnPo.EntnKoID = EntnKo.ID
 JOIN Standort AS Lager ON EntnKo.LagerID = Lager.ID
-WHERE EntnKo.PatchDatum > N'2020-03-01'
-  AND EntnKo.PatchMitarbeiID IN (
-    SELECT Mitarbei.ID
-    FROM Mitarbei
-    WHERE Mitarbei.MitarAbtID = 5111024  -- Mitarbeiter-Abteilung "Bekleidungsservice Lenzing"
-      AND Mitarbei.Status = N'A'
+WHERE EntnKo.DruckDatum > N'2020-03-01'
+  AND (
+    EntnKo.PatchMitarbeiID IN (
+      SELECT Mitarbei.ID
+      FROM Mitarbei
+      WHERE Mitarbei.MitarAbtID = 5111024  -- Mitarbeiter-Abteilung "Bekleidungsservice Lenzing"
+        AND Mitarbei.Status = N'A'
+    )
+    OR
+    EntnPo.LagerArtID IN (
+      SELECT Lagerart.ID
+      FROM Lagerart
+      JOIN Standort ON Lagerart.LagerID = Standort.ID
+      WHERE Standort.SuchCode IN (N'WOL3', N'WOLX')
+    )
   )
-GROUP BY CAST(EntnKo.PatchDatum AS date), Lager.SuchCode, Lager.Bez
-ORDER BY Patchdatum, LagKurz;
+GROUP BY CAST(EntnKo.DruckDatum AS date), Lager.SuchCode, Lager.Bez
+ORDER BY DruckDatum, LagKurz;
