5f009bb2ed792e47d0ba1211596d0f0e3f11a41c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-05-18 12:03:43 +0200

    prepared for migration of GRAZ

---
+++ SAL-MIG/Reduzierung-Berechtigung bei WebUsern deaktivieren.sql
@@ -0,0 +1,20 @@
+CREATE TABLE __WebUserGraz (
+  ID int,
+  UserName nvarchar(20) COLLATE Latin1_General_CS_AS,
+  FuncDeactivateW bit,
+  FuncDecAmount bit
+);
+
+--SELECT WebUser.ID, WebUser.UserName, WebUser.FuncDecAmount
+UPDATE WebUser SET FuncDecAmount = 0, FuncDeactivateW = 0
+OUTPUT inserted.ID, inserted.UserName, deleted.FuncDecAmount
+INTO __WebUserGraz
+FROM WebUser
+JOIN Kunden ON WebUser.KundenID = Kunden.ID
+JOIN Firma ON Kunden.FirmaID = Firma.ID
+JOIN KdGf ON Kunden.KdGFID = KdGf.ID
+WHERE Firma.Land = N'AT'
+  AND KdGf.KurzBez = N'JOB'
+  AND WebUser.Status = N'A'
+  AND (WebUser.FuncDeactivateW = 1 OR WebUser.FuncDecAmount = 1)
+  AND WebUser.Anlage_ > N'2020-05-22 00:00:00';
