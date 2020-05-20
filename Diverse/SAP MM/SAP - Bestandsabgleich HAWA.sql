a499703cb9671430123578d4b9de04925bd1a9b2
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-05-20 09:13:26 +0200

    prepared for 2020-05-23

---
+++ Diverse/SAP MM/SAP - Bestandsabgleich HAWA.sql
@@ -0,0 +1,15 @@
+SELECT N'|' + x.ArtikelNr + IIF(x.Groesse = N'-', N'', N'-' + x.Groesse) + N'|' + Lagerstandort + N'|' + CAST(Bestand AS nvarchar(10)) + N'|' AS DataRow
+FROM (
+  SELECT Bereich.Bereich, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, Standort.SuchCode AS Lagerstandort, SUM(Bestand.Bestand) AS Bestand
+  FROM Bestand
+  JOIN LagerArt ON Bestand.LagerArtID = LagerArt.ID
+  JOIN Standort ON LagerArt.LagerID = Standort.ID
+  JOIN ArtGroe ON Bestand.ArtGroeID = ArtGroe.ID
+  JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+  JOIN Bereich ON Artikel.BereichID = Bereich.ID
+  WHERE LagerArt.Neuwertig = 1
+    AND Bestand.Bestand != 0
+    AND Artikel.ArtikelNr IN (SELECT ArtikelNr FROM __SAPHAWA)
+    AND Standort.SuchCode IN ('MATT','LEOG','SMS','ARNO','SCHI','GRAZ')
+  GROUP BY Bereich.Bereich, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse, Standort.SuchCode
+) AS x;
