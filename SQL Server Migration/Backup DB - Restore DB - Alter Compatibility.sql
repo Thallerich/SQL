5206d563928d2f8b4011b407b22debad0113b33a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-07-14 14:35:56 +0200

    rename logical filenames

--- SQL Server Migration/Backup DB - Restore DB - Alter Compatibility.sql
+++ SQL Server Migration/Backup DB - Restore DB - Alter Compatibility.sql
@@ -1,29 +1,41 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Backup Database                                                                                                           ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 USE master;
 GO
 
-BACKUP DATABASE Salesianer_SAWR
-TO DISK = N'\\ATENVCENTER01.wozabal.int\advbackup\Salesianer_SAWR.bak'
+BACKUP DATABASE Salesianer_SMSK_SDC
+TO DISK = N'\\ATENVCENTER01.wozabal.int\advbackup\Salesianer_SMSK.bak'
 WITH STATS = 5, COPY_ONLY, COMPRESSION, INIT, SKIP, FORMAT, BUFFERCOUNT = 47, MAXTRANSFERSIZE = 4194304, MEDIANAME = N'AdvanTex-Backup', NAME = N'Full Backup of the AdvanTex-Database';
 
 GO
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-/* ++                                                                                                                           ++ */
-/* ++                                                                                                                           ++ */
-/* ++ Author: Stefan Thaller - 2020-07-14                                                                                       ++ */
+/* ++ Restore from Backup                                                                                                       ++ */
+/* ++ Upgrade Compatibility Level to SQL Server 2019                                                                            ++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 USE master;
 GO
 
-RESTORE DATABASE Salesianer_SAWR
-FROM DISK = N'\\ATENVCENTER01.wozabal.int\advbackup\Salesianer_SAWR.bak'
+RESTORE DATABASE Salesianer_SMSK
+FROM DISK = N'\\ATENVCENTER01.wozabal.int\advbackup\Salesianer_SMSK.bak'
 WITH STATS = 5, RECOVERY, REPLACE,
-  MOVE N'Salesianer_SAWR' TO N'M:\DATA01\Salesianer_SAWR.mdf',
-  MOVE N'Salesianer_SAWR_log' TO N'M:\LOG01\Salesianer_SAWR_log.ldf';
+  MOVE N'Wozabal_Lenzing_2' TO N'M:\DATA01\Salesianer_SMSK.mdf',
+  MOVE N'Wozabal_Lenzing_2_log' TO N'M:\LOG01\Salesianer_SMSK_log.ldf';
 
 GO
 
-ALTER DATABASE Salesianer_SAWR SET COMPATIBILITY_LEVEL = 150;
+ALTER DATABASE Salesianer_SMSK SET COMPATIBILITY_LEVEL = 150;
+
+GO
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Rename logical file names                                                                                                 ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+ALTER DATABASE Salesianer_SMSK MODIFY FILE (NAME = Wozabal_Lenzing_2, NEWNAME = Salesianer_SMSK);
+ALTER DATABASE Salesianer_SMSK MODIFY FILE (NAME = Wozabal_Lenzing_2_log, NEWNAME = Salesianer_SMSK_log);
 
 GO
