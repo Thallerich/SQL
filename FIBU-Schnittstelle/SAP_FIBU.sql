3bbbe125c5679e6837d723b272dc092640a0eaea
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-07-16 14:38:10 +0200

    mwst-code for SMSK -> 4V

--- FIBU-Schnittstelle/SAP_FIBU.sql
+++ FIBU-Schnittstelle/SAP_FIBU.sql
@@ -49,6 +49,7 @@ DECLARE fibuexp CURSOR LOCAL FAST_FORWARD FOR
       CASE
         WHEN MwSt.SteuerSchl = N'6Z' AND Export.Art = N'G' AND Firma.SuchCode = N'SMP' THEN N'6O'
         WHEN MwSt.Steuerschl = N'A6' AND Firma.SuchCode = N'SMP' THEN N'33'
+        WHEN MwSt.SteuerSchl = N'A2' AND Firma.SuchCode = N'SMSK' THEN N'4V'
         ELSE MwSt.Steuerschl
       END,
     Export.Debitor, Export.Gegenkonto, 
