6a83f5007fadd37d355451ad9edac41cb093512d
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-07-17 09:33:49 +0200

    use CTE for status of set

--- Report/860 - Micronclean - Details zu Charge.sql
+++ Report/860 - Micronclean - Details zu Charge.sql
@@ -1,9 +1,14 @@
-SELECT OPEtiKo.EtiNr, Status.StatusBez AS SetStatus, ArtikelSet.ArtikelNr AS SetArtikelNr, ArtikelSet.ArtikelBez$LAN$ AS SetArtikelBez, Teile.Barcode, ArtikelTeil.ArtikelNr AS TeilArtikelNr, ArtikelTeil.ArtikelBez$LAN$ AS TeilArtikelBez, OPEtiKo.VerfallDatum, OPCharge.ChargeDatum, OPCharge.ChargeNr, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.SuchCode AS VsaSuchCode, Vsa.Bez AS Vsa
-FROM OPEtiPo, OPEtiKo, OPCharge, Artikel AS ArtikelSet, OPTeile, Teile, Artikel AS ArtikelTeil, Vsa, Kunden, (SELECT Status.Status, Status.StatusBez$LAN$ AS StatusBez FROM Status WHERE Status.Tabelle = 'OPETIKO') AS Status
+WITH SetStatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'OPETIKO')
+)
+SELECT OPEtiKo.EtiNr, SetStatus.StatusBez AS SetStatus, ArtikelSet.ArtikelNr AS SetArtikelNr, ArtikelSet.ArtikelBez$LAN$ AS SetArtikelBez, Teile.Barcode, ArtikelTeil.ArtikelNr AS TeilArtikelNr, ArtikelTeil.ArtikelBez$LAN$ AS TeilArtikelBez, OPEtiKo.VerfallDatum, OPCharge.ChargeDatum, OPCharge.ChargeNr, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.SuchCode AS VsaSuchCode, Vsa.Bez AS Vsa
+FROM OPEtiPo, OPEtiKo, OPCharge, Artikel AS ArtikelSet, OPTeile, Teile, Artikel AS ArtikelTeil, Vsa, Kunden, SetStatus
 WHERE OPEtiPo.OPEtiKoID = OPEtiKo.ID
   AND OPEtiKo.OPChargeID = OPCharge.ID
   AND OPEtiKo.ArtikelID = ArtikelSet.ID
-  AND OPEtiKo.Status = Status.Status
+  AND OPEtiKo.Status = SetStatus.Status
   AND OPEtiPo.OPTeileID = OPTeile.ID
   AND Teile.OPTeileID = OPTeile.ID
   AND Teile.ArtikelID = ArtikelTeil.ID
