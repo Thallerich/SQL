dfe93a2a2a9fef817eea9aeca197b683cb90cd39
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-07-23 15:59:46 +0200

    udpate

--- Ticket JIRA/IT-00000 - Restwerte mit Abnützung.sql
+++ Ticket JIRA/IT-00000 - Restwerte mit Abnützung.sql
@@ -1,4 +1,6 @@
-DECLARE @Woche nchar(7) = N'2020/27';
+DECLARE @Woche nchar(7) = (SELECT Week.Woche FROM Week WHERE N'2021-06-30' BETWEEN Week.VonDat AND Week.BisDat);
+
+PRINT @Woche;
 
 SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS VsaBezeichnung, Traeger.Traeger AS TraegerNr, Traeger.PersNr, Traeger.Nachname, Traeger.Vorname, Teile.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, KdArti.Variante, KdArti.VariantBez, Teile.Erstwoche AS [Ersteinsatz-Woche], Teile.Indienst AS [Indienststellung aktueller Träger], IIF(ISNULL(Teile.Eingang1, N'1980-01-01') > ISNULL(Teile.Ausgang1, N'2099-12-31'), Teile.Eingang1, Teile.Ausgang1) AS LetzterScan, Restwert.AlterInfo AS [Alter in Wochen], Restwert.BasisAfa AS Wiederbeschaffungswert, IIF(Teile.Status IN (N'Z', N'V', N'X', N'Y') OR (Teile.Einzug < CONVERT(DATE, GETDATE())), 0, IIF((Teile.AusDienst = N'' OR Teile.AusDienst IS NULL), Restwert.RestwertInfo, IIF(@Woche < Teile.AusDienst, Restwert.RestWertInfo, Teile.AusDRestW))) AS RestWert, IIF(Restwert.AlterInfo > KdArti.AfaWochen, 100, ROUND(100 / CAST(KdArti.AfaWochen AS float) * CAST(Restwert.AlterInfo AS float), 2)) AS [Abnützung in %]
 FROM Teile
@@ -10,5 +12,7 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
-WHERE Kunden.KdNr IN (218645, 219733, 299156, 10002344)
-  AND Teile.Status != N'5';
+WHERE Kunden.KdNr IN (10003817, 10003818, 10003819, 10003845, 10003848, 2529602)
+  AND Teile.Status BETWEEN N'Q' AND N'W'
+  AND Teile.Einzug IS NULL
+  AND Teile.AltenheimModus = 0;
