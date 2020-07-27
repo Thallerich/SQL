87ded06bd15b3ddd78ede3e6eb09ceca7e65c25c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-07-27 08:58:27 +0200

    added entnko.id

--- Report/804 - offene Aufträge Lager je SGF.sql
+++ Report/804 - offene Aufträge Lager je SGF.sql
@@ -8,7 +8,7 @@ Traegerstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'TRAEGER')
 )
-SELECT Kunden.Kdnr, Kunden.Suchcode as Kunde, Holding.Holding, Traeger.Nachname, Traeger.Vorname, Traegerstatus.StatusBez AS Trägerstatus, Teile.Barcode, Teilestatus.StatusBez AS Teilestatus, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Teile.Anlage_ AS [Teil angelegt am], EntnKo.Anlage_ AS [Entnahmeliste angelegt am], EntnKo.DruckDatum AS [Druckdatum Entnahmeliste], [Entnahme-Datum] = (
+SELECT Kunden.Kdnr, Kunden.Suchcode as Kunde, Holding.Holding, Traeger.Nachname, Traeger.Vorname, Traegerstatus.StatusBez AS Trägerstatus, Teile.Barcode, Teilestatus.StatusBez AS Teilestatus, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Teile.Anlage_ AS [Teil angelegt am], EntnKo.ID AS EntnahmelistenNr, EntnKo.Anlage_ AS [Entnahmeliste angelegt am], EntnKo.DruckDatum AS [Druckdatum Entnahmeliste], [Entnahme-Datum] = (
   SELECT MAX(Scans.[DateTime])
   FROM Scans
   WHERE Scans.TeileID = Teile.ID
@@ -23,8 +23,8 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Holding ON Kunden.HoldingID = Holding.ID
 JOIN Teilestatus ON Teile.[Status] = Teilestatus.[Status]
 JOIN Traegerstatus ON Traeger.[Status] = Traegerstatus.[Status]
-LEFT OUTER JOIN EntnPo ON Teile.EntnPoID = EntnPo.ID AND Teile.EntnPoID > 0
-LEFT OUTER JOIN EntnKo ON EntnPo.EntnKoID = EntnKo.ID
+LEFT JOIN EntnPo ON Teile.EntnPoID = EntnPo.ID AND Teile.EntnPoID > 0
+LEFT JOIN EntnKo ON EntnPo.EntnKoID = EntnKo.ID
 WHERE Teile.Anlage_ > N'2019-04-01 00:00:00'
   AND Artikel.BereichID = 100
   AND Kunden.Status = N'A'
