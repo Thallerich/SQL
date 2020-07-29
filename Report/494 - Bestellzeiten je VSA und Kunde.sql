1ce98ad937355724d42a33e72073e130097a8c8c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-07-29 10:13:25 +0200

    refactored   - added period selection   - added servtype   - respect visibility setting

--- Report/494 - Bestellzeiten je VSA und Kunde.sql
+++ Report/494 - Bestellzeiten je VSA und Kunde.sql
@@ -1,9 +1,13 @@
-SELECT KdGf.KurzBez AS SGF, Firma.Bez AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, CONVERT(date, AnfKo.Anlage_) AS Bestelldatum, IIF(DATEPART(hour, AnfKo.Anlage_) < 10, '0' + CONVERT(char(1), DATEPART(hour, AnfKo.Anlage_)), CONVERT(char(2), DATEPART(hour, AnfKo.Anlage_))) + ':' + IIF(DATEPART(minute, AnfKo.Anlage_) < 10, '0' + CONVERT(char(1), DATEPART(minute, AnfKo.Anlage_)), CONVERT(char(2), DATEPART(minute, AnfKo.Anlage_))) AS Bestellzeit, AnfKo.Lieferdatum
-FROM AnfKo, Vsa, Kunden, KdGf, Firma
-WHERE AnfKo.VsaID = Vsa.ID
-  AND Vsa.KundenID = Kunden.ID
-  AND Kunden.KdGfID = KdGf.ID
-  AND Kunden.FirmaID = Firma.ID
-  AND KdGf.ID IN ($1$)
-  AND AnfKo.Lieferdatum BETWEEN  $2$ AND $3$
-ORDER BY Lieferdatum, SGF, Firma, Kunden.KdNr, VsaNr;
+SELECT KdGf.KurzBez AS Geschäftsbereich, Firma.Bez AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], ServType.ServTypeBez$LAN$ AS Serviceart, CAST(AnfKo.Anlage_ AS date) AS Bestelldatum, FORMAT(AnfKo.Anlage_, N'HH:mm', N'de-AT') AS Bestellzeit, AnfKo.LieferDatum, AnfKo.AuftragsNr AS PackzettelNr
+FROM AnfKo
+JOIN Vsa ON AnfKo.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN Firma ON Kunden.FirmaID = Firma.ID
+JOIN KdGf ON Kunden.KdGFID = KdGf.ID
+JOIN ServType ON Vsa.ServTypeID = ServType.ID
+WHERE KdGf.ID IN ($3$)
+  AND Firma.ID IN ($2$)
+  AND ServType.ID IN ($4$)
+  AND AnfKo.Lieferdatum BETWEEN $STARTDATE$ AND $ENDDATE$
+  AND Vsa.SichtbarID IN ($SICHTBARIDS$)
+ORDER BY Lieferdatum, KdNr, VsaNr;
