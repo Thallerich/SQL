108713e4c6310729639bbf34388d04f2c95397af
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-07-29 10:46:24 +0200

    IT-35225   - added column MemoIntern

--- Report/292 - Bestellungen.sql
+++ Report/292 - Bestellungen.sql
@@ -3,7 +3,7 @@ WITH Bestellstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'BKO')
 )
-SELECT BKo.Datum, BKo.BestNr AS Bestellnummer, Bestellstatus.StatusBez AS [Status der Bestellung], Lief.LiefNr AS Lieferantennummer, Lief.Name1 AS Lieferant, Lagerstandort.Bez AS Lagerstandort, LagerArt.LagerArtBez$LAN$ AS Lagerart, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Bereich.BereichBez$LAN$ AS Produktbereich, SUM(BPo.BestMenge) AS Bestellmenge, SUM(BPo.LiefMenge) AS [geliefert], BPo.Einzelpreis, SUM(BPo.BestMenge * BPo.Einzelpreis) AS [Summe der Kosten]
+SELECT BKo.Datum, BKo.BestNr AS Bestellnummer, Bestellstatus.StatusBez AS [Status der Bestellung], Lief.LiefNr AS Lieferantennummer, Lief.Name1 AS Lieferant, Lagerstandort.Bez AS Lagerstandort, LagerArt.LagerArtBez$LAN$ AS Lagerart, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Bereich.BereichBez$LAN$ AS Produktbereich, SUM(BPo.BestMenge) AS Bestellmenge, SUM(BPo.LiefMenge) AS [geliefert], BPo.Einzelpreis, SUM(BPo.BestMenge * BPo.Einzelpreis) AS [Summe der Kosten], BKo.MemoIntern
 FROM BPo
 JOIN BKo ON BPo.BKoID = BKo.ID
 JOIN Lief ON BKo.LiefID = Lief.ID
@@ -15,5 +15,5 @@ JOIN LagerArt ON BKo.LagerArtID = LagerArt.ID
 JOIN Standort AS Lagerstandort ON BKo.LagerID = Lagerstandort.ID
 WHERE BKo.Datum BETWEEN $1$ AND $2$
   AND LagerArt.SichtbarID IN ($SICHTBARIDS$)
-GROUP BY BKo.Datum, BKo.BestNr, Bestellstatus.StatusBez, Lief.LiefNr, Lief.Name1, Lagerstandort.Bez, LagerArt.LagerartBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Bereich.BereichBez$LAN$, BPo.Einzelpreis
+GROUP BY BKo.Datum, BKo.BestNr, Bestellstatus.StatusBez, Lief.LiefNr, Lief.Name1, Lagerstandort.Bez, LagerArt.LagerartBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Bereich.BereichBez$LAN$, BPo.Einzelpreis, BKo.MemoIntern
 ORDER BY BKo.Datum, Lieferant, Artikelbezeichnung;
