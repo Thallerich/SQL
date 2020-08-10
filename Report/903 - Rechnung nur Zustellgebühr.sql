f2720f2513b949ddad558e97273b1cf3efd8d4a4
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-08-10 15:30:21 +0200

    IT-37894

---
+++ Report/903 - Rechnung nur Zustellgebühr.sql
@@ -0,0 +1,24 @@
+SELECT Firma.SuchCode AS Firma, RechKo.RechNr, RechKo.RechDat, Kunden.KdNr, Kunden.SuchCode AS Kunde, RechKo.NettoWert, RechKo.BruttoWert
+FROM RechKo
+JOIN Kunden ON RechKo.KundenID = Kunden.ID
+JOIN Firma ON RechKo.FirmaID = Firma.ID
+WHERE RechKo.Status = N'A'
+  AND RechKo.ID > 0
+  AND RechKo.FirmaID IN ($1$)
+  AND EXISTS (
+    SELECT RechPo.*
+    FROM RechPo
+    JOIN KdArti ON RechPo.KdArtiID = KdArti.ID
+    JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+    WHERE RechPo.RechKoID = RechKo.ID
+      AND Artikel.ArtikelNr = N'ZUS'
+  )
+  AND NOT EXISTS (
+    SELECT RechPo.*
+    FROM RechPo
+    JOIN KdArti ON RechPo.KdArtiID = KdArti.ID
+    JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+    WHERE RechPo.RechKoID = RechKo.ID
+      AND Artikel.ArtikelNr != N'ZUS'
+  )
+ORDER BY RechKo.RechNr DESC;
