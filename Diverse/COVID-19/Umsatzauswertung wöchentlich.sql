0453fe726c9dcdb1f0fbce382c01233970a1e586
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-08-19 13:44:04 +0200

    modifications with PoesHa

--- Diverse/COVID-19/Umsatzauswertung wöchentlich.sql
+++ Diverse/COVID-19/Umsatzauswertung wöchentlich.sql
@@ -1,47 +1,50 @@
 DECLARE @SalesWeekly TABLE (
   CalendarWeek nchar(7) COLLATE Latin1_General_CS_AS,
+  CustomerID int,
+  InvoiceID int,
   SalesLeasing money DEFAULT 0,
   SalesProcessing money DEFAULT 0,
   SalesOther money DEFAULT 0
 );
 
-INSERT INTO @SalesWeekly (CalendarWeek, SalesLeasing)
-SELECT Wochen.Woche, SUM(AbtKdArW.EPreis * AbtKdArW.Menge) AS UmsatzLeasing
+INSERT INTO @SalesWeekly (CalendarWeek, CustomerID, InvoiceID, SalesLeasing)
+SELECT Wochen.Woche, Kunden.ID AS KundenID, RechKo.ID AS RechKoID, SUM((AbtKdArW.EPreis * AbtKdArW.Menge) * (1 - RechPo.RabattProz / 100)) AS UmsatzLeasing
 FROM AbtKdArW
 JOIN Wochen ON AbtKdArW.WochenID = Wochen.ID
 JOIN RechPo ON AbtKdArW.RechPoID = RechPo.ID
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
 JOIN Kunden ON RechKo.KundenID = Kunden.ID
+JOIN Week ON RechKo.RechDat BETWEEN Week.VonDat AND Week.BisDat
 WHERE RechKo.FirmaID IN (SELECT Firma.ID FROM Firma WHERE SuchCode IN (N'FA14', N'WOMI', N'UKLU'))
   AND Kunden.HoldingID != (SELECT Holding.ID FROM Holding WHERE Holding.Holding = N'SAL')
-  AND Wochen.Woche >= N'2019/01'
+  AND Week.Woche IN (SELECT Wochen.Woche FROM Wochen WHERE Wochen.Monat1 = N'2020-04')
   AND RechKo.Status < N'X'
-GROUP BY Wochen.Woche;
+GROUP BY Wochen.Woche, Kunden.ID, RechKo.ID;
 
 MERGE INTO @SalesWeekly AS SalesWeekly
 USING (
-  SELECT Week.Woche, SUM(LsPo.EPreis * LsPo.Menge) AS UmsatzBearbeitung
+  SELECT Week.Woche, Kunden.ID AS KundenID, RechKo.ID AS RechKoID, SUM(LsPo.EPreis * LsPo.Menge * (1 - RechPo.RabattProz / 100)) AS UmsatzBearbeitung
   FROM LsPo
   JOIN LsKo ON LsPo.LsKoID = LsKo.ID
   JOIN RechPo ON LsPo.RechPoID = RechPo.ID
   JOIN RechKo ON RechPo.RechKoID = RechKo.ID
   JOIN Kunden ON RechKo.KundenID = Kunden.ID
-  JOIN Week ON LsKo.Datum BETWEEN Week.VonDat AND Week.BisDat
+  JOIN Week ON RechKo.RechDat BETWEEN Week.VonDat AND Week.BisDat
   WHERE RechKo.FirmaID IN (SELECT Firma.ID FROM Firma WHERE SuchCode IN (N'FA14', N'WOMI', N'UKLU'))
     AND Kunden.HoldingID != (SELECT Holding.ID FROM Holding WHERE Holding.Holding = N'SAL')
     AND RechKo.Status < N'X'
-    AND Week.Woche >= N'2019/01'
-  GROUP BY Week.Woche    
-) AS SP (Woche, UmsatzBearbeitung)
-ON SalesWeekly.CalendarWeek = SP.Woche
+    AND Week.Woche IN (SELECT Wochen.Woche FROM Wochen WHERE Wochen.Monat1 = N'2020-04')
+  GROUP BY Week.Woche, Kunden.ID, RechKo.ID
+) AS SP (Woche, KundenID, RechKoID, UmsatzBearbeitung)
+ON SalesWeekly.CalendarWeek = SP.Woche AND SalesWeekly.CustomerID = SP.KundenID AND SalesWeekly.InvoiceID = SP.RechKoID
 WHEN MATCHED THEN
   UPDATE SET SalesProcessing = SP.UmsatzBearbeitung
 WHEN NOT MATCHED THEN
-  INSERT (CalendarWeek, SalesProcessing) VALUES (SP.Woche, SP.UmsatzBearbeitung);
+  INSERT (CalendarWeek, CustomerID, InvoiceID, SalesProcessing) VALUES (SP.Woche, SP.KundenID, SP.RechKoID, SP.UmsatzBearbeitung);
 
 MERGE INTO @SalesWeekly AS SalesWeekly
 USING (
-  SELECT Week.Woche, SUM(RechPo.GPreis) AS UmsatzAndere
+  SELECT Week.Woche, Kunden.ID AS KundenID, RechKo.ID AS RechKoID, SUM(RechPo.GPreis) AS UmsatzAndere
   FROM RechPo
   JOIN RechKo ON RechPo.RechKoID = RechKo.ID
   JOIN Kunden ON RechKo.KundenID = Kunden.ID
@@ -49,15 +52,16 @@ USING (
   WHERE RechKo.FirmaID IN (SELECT Firma.ID FROM Firma WHERE SuchCode IN (N'FA14', N'WOMI', N'UKLU'))
     AND Kunden.HoldingID != (SELECT Holding.ID FROM Holding WHERE Holding.Holding = N'SAL')
     AND RechKo.Status < N'X'
-    AND Week.Woche >= N'2019/01'
+    AND Week.Woche IN (SELECT Wochen.Woche FROM Wochen WHERE Wochen.Monat1 = N'2020-04')
     AND RechPo.ID NOT IN (
       SELECT RechPo.ID
       FROM AbtKdArW
       JOIN Wochen ON AbtKdArW.WochenID = Wochen.ID
       JOIN RechPo ON AbtKdArW.RechPoID = RechPo.ID
       JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+      JOIN Week ON RechKo.RechDat BETWEEN Week.VonDat AND Week.BisDat
       WHERE RechKo.FirmaID IN (SELECT Firma.ID FROM Firma WHERE SuchCode IN (N'FA14', N'WOMI', N'UKLU'))
-        AND Wochen.Woche >= N'2019/01'
+        AND Week.Woche IN (SELECT Wochen.Woche FROM Wochen WHERE Wochen.Monat1 = N'2020-04')
         AND RechKo.Status < N'X'
     )
     AND RechPo.ID NOT IN (
@@ -66,10 +70,10 @@ USING (
       JOIN LsKo ON LsPo.LsKoID = LsKo.ID
       JOIN RechPo ON LsPo.RechPoID = RechPo.ID
       JOIN RechKo ON RechPo.RechKoID = RechKo.ID
-      JOIN Week ON LsKo.Datum BETWEEN Week.VonDat AND Week.BisDat
+      JOIN Week ON RechKo.RechDat BETWEEN Week.VonDat AND Week.BisDat
       WHERE RechKo.FirmaID IN (SELECT Firma.ID FROM Firma WHERE SuchCode IN (N'FA14', N'WOMI', N'UKLU'))
         AND RechKo.Status < N'X'
-        AND Week.Woche >= N'2019/01'
+        AND Week.Woche IN (SELECT Wochen.Woche FROM Wochen WHERE Wochen.Monat1 = N'2020-04')
     )
     AND NOT EXISTS (
       SELECT LsPo.*
@@ -81,15 +85,17 @@ USING (
       FROM AbtKdArW
       WHERE AbtKdArW.RechPoID = RechPo.ID
     )
-  GROUP BY Week.Woche
-) AS SO (Woche, UmsatzAndere)
-ON SalesWeekly.CalendarWeek = SO.Woche
+  GROUP BY Week.Woche, Kunden.ID, RechKo.ID
+) AS SO (Woche, KundenID, RechKoID, UmsatzAndere)
+ON SalesWeekly.CalendarWeek = SO.Woche AND SalesWeekly.CustomerID = SO.KundenID AND SalesWeekly.InvoiceID = SO.RechKoID
 WHEN MATCHED THEN
   UPDATE SET SalesOther = SO.UmsatzAndere
 WHEN NOT MATCHED THEN
-  INSERT (CalendarWeek, SalesOther) VALUES (SO.Woche, SO.UmsatzAndere);
+  INSERT (CalendarWeek, CustomerID, InvoiceID, SalesOther) VALUES (SO.Woche, SO.KundenID, SO.RechKoID, SO.UmsatzAndere);
 
-SELECT CalendarWeek AS Woche, SalesLeasing AS Miete, SalesProcessing AS Bearbeitung, SalesOther AS Andere, SalesLeasing + SalesProcessing + SalesOther AS Wochensumme
-FROM @SalesWeekly
-WHERE CalendarWeek < (SELECT Week.Woche FROM Week WHERE CAST(GETDATE() AS date) BETWEEN Week.VonDat AND Week.BisDat)
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, RechKo.RechNr, RechKo.RechDat AS Rechnungsdatum, RechKo.Art, RechKo.NettoWert, RechKo.BruttoWert, CalendarWeek AS Woche, SalesLeasing AS Miete, SalesProcessing AS Bearbeitung, SalesOther AS Andere, SalesLeasing + SalesProcessing + SalesOther AS Wochensumme
+FROM @SalesWeekly AS SW
+JOIN RechKo ON SW.InvoiceID = RechKo.ID
+JOIN Kunden ON SW.CustomerID = Kunden.ID
+--WHERE CalendarWeek < (SELECT Week.Woche FROM Week WHERE CAST(GETDATE() AS date) BETWEEN Week.VonDat AND Week.BisDat)
 ORDER BY Woche ASC;
