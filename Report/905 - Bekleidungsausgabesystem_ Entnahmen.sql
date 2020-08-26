b48c238287e74b38903e387bebc6e570bb6bdc71
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-08-26 12:51:03 +0200

    IT-37060   - only show parts that where last issued to a wearer and have not yet been returned

--- Report/905 - Bekleidungsausgabesystem_ Entnahmen.sql
+++ Report/905 - Bekleidungsausgabesystem_ Entnahmen.sql
@@ -5,6 +5,11 @@ WITH Teilestatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'TEILE')
+), 
+LastScan AS (
+  SELECT Scans.TeileID, MAX(Scans.ID) AS ScansID
+  FROM Scans
+  GROUP BY Scans.TeileID
 )
 SELECT Scans.DateTime AS [Datum der Entnahme], EntnahmeTraeger.PersNr, EntnahmeTraeger.Traeger AS TraegerNr, EntnahmeTraeger.Nachname, EntnahmeTraeger.Vorname, Teile.Barcode, Teile.RentomatChip AS Chipcode, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Kunden.KdNr, Kunden.SuchCode AS Kunde, IIF(Teile.Ausdienst IS NULL, Teile.RestwertInfo, Teile.AusdRestw) AS Restwert, Teilestatus.StatusBez AS [aktueller Status des Teils], Actions.ActionsBez AS [letzte Aktion des Teils]
 FROM Teile
@@ -15,11 +20,12 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
-JOIN Scans ON Scans.TeileID = Teile.ID
-JOIN Traeger AS EntnahmeTraeger ON Scans.LastPoolTraegerID = EntnahmeTraeger.ID
 JOIN Actions ON Teile.LastActionsID = Actions.ID
 JOIN Teilestatus ON Teile.Status = Teilestatus.Status
+JOIN LastScan ON LastScan.TeileID = Teile.ID
+JOIN Scans ON LastScan.ScansID = Scans.ID
+JOIN Traeger AS EntnahmeTraeger ON Scans.LastPoolTraegerID = EntnahmeTraeger.ID
 WHERE Vsa.RentomatID = $2$
   AND Traeger.RentoArtID = (SELECT RentoArt.ID FROM RentoArt WHERE RentoArt.Code = N'S')
   AND Scans.DateTime BETWEEN @von AND @bis
-  AND Scans.ActionsID = 65;   -- Aktion "Bekleidungssystem: Entnahme"
+  AND Actions.ID = 65;   -- Aktion "Bekleidungssystem: Entnahme"
