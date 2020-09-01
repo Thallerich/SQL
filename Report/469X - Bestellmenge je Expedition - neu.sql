750513fcff67a5a3a9b3cd26582883a46f962c3e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-09-01 12:49:01 +0200

    IT-37796   - bugfix - results multiplied by VsaTour

--- Report/469X - Bestellmenge je Expedition - neu.sql
+++ Report/469X - Bestellmenge je Expedition - neu.sql
@@ -1,5 +1,10 @@
 SELECT Expedition.SuchCode AS Expedition, AnfKo.Lieferdatum, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, IIF($5$ = 1, Kunden.KdNr, NULL) AS KdNr, IIF($5$ = 1, Kunden.SuchCode, NULL) AS Kunde, IIF($5$ = 1, ABC.ABC, NULL) AS [ABC-Klasse Kunde], IIF($6$ = 1, Vsa.Bez, NULL) AS VSA, SUM(AnfPo.Angefordert) AS Bestellt, ROUND(SUM(VsaAnf.Durchschnitt) / COUNT(DISTINCT Vsa.ID), 0) AS Durchschnitt, SUM(AnfPo.Geliefert) AS Geliefert, SUM(AnfPo.Angefordert - AnfPo.Geliefert) AS [noch Offen], Artikel.Stueckgewicht, SUM(AnfPo.Angefordert - AnfPo.Geliefert) * Artikel.StueckGewicht AS [noch Offen kg]
-FROM AnfPo, AnfKo, Vsa, Kunden, ABC, KdArti, Artikel, VsaAnf, VsaTour, Touren, Standort AS Expedition
+FROM AnfPo, AnfKo, Vsa, Kunden, ABC, KdArti, Artikel, VsaAnf, Standort AS Expedition, (
+    SELECT DISTINCT VsaTour.VsaID, VsaTour.KdBerID, Touren.ExpeditionID
+    FROM VsaTour
+    JOIN Touren ON VsaTour.TourenID = Touren.ID
+    WHERE CAST(GETDATE() AS date) BETWEEN VsaTour.VonDatum AND VsaTour.BisDatum
+  ) AS TourenExpedition
 WHERE AnfPo.AnfKoID = AnfKo.ID
   AND AnfKo.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
@@ -9,10 +14,9 @@ WHERE AnfPo.AnfKoID = AnfKo.ID
   AND VsaAnf.VsaID = Vsa.ID
   AND VsaAnf.KdArtiID = AnfPo.KdArtiID
   AND VsaAnf.ArtGroeID = AnfPo.ArtGroeID
-  AND VsaTour.VsaID = Vsa.ID
-  AND VsaTour.KdBerID = KdArti.KdBerID
-  AND VsaTour.TourenID = Touren.ID
-  AND Touren.ExpeditionID = Expedition.ID
+  AND TourenExpedition.VsaID = Vsa.ID
+  AND TourenExpedition.KdBerID = KdArti.KdBerID
+  AND TourenExpedition.ExpeditionID = Expedition.ID
   AND Expedition.ID IN ($2$)
   AND AnfKo.Lieferdatum BETWEEN $3$ AND $4$
 GROUP BY Expedition.SuchCode, AnfKo.Lieferdatum, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, IIF($5$ = 1, Kunden.KdNr, NULL), IIF($5$ = 1, Kunden.SuchCode, NULL), IIF($5$ = 1, ABC.ABC, NULL), IIF($6$ = 1, Vsa.Bez, NULL), Artikel.StueckGewicht
