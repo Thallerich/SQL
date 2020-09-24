0fef4ed897fb84b2735e931834bf123a7b1d0d59
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-09-24 12:30:54 +0200

    IT-39109   - respect customer visibility setting   - added customer location filter   - use modern time range selection   - output last customer / VSA in result set

--- Report/736 - Teile verschrottet im Zeitraum - UHF-Artikel.sql
+++ Report/736 - Teile verschrottet im Zeitraum - UHF-Artikel.sql
@@ -1,12 +1,14 @@
-SELECT KdGf.KurzBez AS SGF, OpTeile.Code AS Barcode, Artikel.ArtikelNr, Artikel.ArtikelNr2 AS [BMD-ArtikelNr], Artikel.ArtikelBez$LAN$ AS ArtikelBez, Artikel.EKPreis, OpTeile.ErstWoche, OpTeile.WegDatum, WegGrund.WegGrundBez$LAN$ AS WegGrund, OpTeile.AnzWasch
+SELECT KdGf.KurzBez AS SGF, OpTeile.Code AS Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, OpTeile.ErstWoche, OpTeile.WegDatum, WegGrund.WegGrundBez$LAN$ AS WegGrund, OpTeile.AnzWasch AS [Anzahl Wäschen], Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS [Vsa-Bezeichnung]
 FROM OpTeile, Vsa, Kunden, KdGf, Artikel, WegGrund
 WHERE OpTeile.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
   AND Kunden.KdGfID = KdGf.ID
-  AND Kunden.KdGfID IN ($4$)
+  AND Kunden.KdGfID IN ($2$)
+  AND Kunden.StandortID IN ($1$)
   AND OpTeile.ArtikelID = Artikel.ID
   AND Artikel.EAN IS NOT NULL --nur UHF-Artikel
   AND OpTeile.WegGrundID = WegGrund.ID
   AND OpTeile.WegGrundID IN ($3$)
-  AND OpTeile.WegDatum BETWEEN $1$ AND $2$
+  AND OpTeile.WegDatum BETWEEN $STARTDATE$ AND $ENDDATE$
+  AND Kunden.SichtbarID IN ($SICHTBARIDS$)
 ORDER BY Artikel.ArtikelBez, OpTeile.WegDatum ASC;
