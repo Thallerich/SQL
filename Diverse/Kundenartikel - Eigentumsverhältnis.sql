fc50d4ebd4f0733e1d914f4660c69a072854127e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-10-15 11:07:45 +0200

    added bereich and artikelgruppe

--- Diverse/Kundenartikel - Eigentumsverhältnis.sql
+++ Diverse/Kundenartikel - Eigentumsverhältnis.sql
@@ -3,13 +3,16 @@ WITH KdArtiStatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'KDARTI')
 )
-SELECT Firma.SuchCode AS Firma, Standort.Bez AS Hauptstandort, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez, KdArti.Variante, KdArtiStatus.StatusBez AS Kundenartikelstatus, Eigentum.EigentumBez AS Eigentumsverhältnis
+SELECT Firma.SuchCode AS Firma, Standort.Bez AS Hauptstandort, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez, KdArti.Variante, KdArtiStatus.StatusBez AS Kundenartikelstatus, Bereich.BereichBez AS Produktbereich, ArtGru.ArtGruBez AS Artikelgruppe, Eigentum.EigentumBez AS Eigentumsverhältnis
 FROM KdArti
 JOIN Kunden ON KdArti.KundenID = Kunden.ID
 JOIN Standort ON Kunden.StandortID = Standort.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN Eigentum ON KdArti.EigentumID = Eigentum.ID
+JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+JOIN Bereich ON KdBer.BereichID = Bereich.ID
+JOIN ArtGru ON Artikel.ArtGruID = ArtGru.ID
 JOIN KdArtiStatus ON KdArti.[Status] = KdArtiStatus.[Status]
 WHERE Kunden.AdrArtID = 1
   AND Kunden.Status = N'A'
