53419519f727e7f945286524958ad8ee5fccb529
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-10-19 08:24:21 +0200

    added Geschäftsbereich

--- Report/950 - Kundenumsätze der letzten 12 Monate.sql
+++ Report/950 - Kundenumsätze der letzten 12 Monate.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS #KundenUmsatz;
 
 CREATE TABLE #KundenUmsatz (
   Firma nvarchar(40),
+  Geschäftsbereich nchar(5),
   Kundenstatus nchar(12),
   KdNr int,
   Kunde nchar(20),
@@ -13,15 +14,16 @@ CREATE TABLE #KundenUmsatz (
 DECLARE @Spalten nvarchar(max);
 DECLARE @PivotSQL nvarchar(max);
 
-INSERT INTO #KundenUmsatz
-SELECT Firma.Bez AS Firma, [Status].StatusBez$LAN$ AS Kundenstatus, Kunden.KdNr, Kunden.SuchCode AS Kunde, Standort.Bez AS [Kundenservice-Standort], FORMAT(RechKo.RechDat, N'yyyy-MM') AS Monat, SUM(RechKo.BruttoWert) AS Umsatz
+INSERT INTO #KundenUmsatz (Firma, Geschäftsbereich, Kundenstatus, KdNr, Kunde, [Kundenservice-Standort], Monat, Umsatz)
+SELECT Firma.Bez AS Firma, KdGf.KurzBez AS Geschäftsbereich, [Status].StatusBez$LAN$ AS Kundenstatus, Kunden.KdNr, Kunden.SuchCode AS Kunde, Standort.Bez AS [Kundenservice-Standort], FORMAT(RechKo.RechDat, N'yyyy-MM') AS Monat, SUM(RechKo.BruttoWert) AS Umsatz
 FROM Kunden
 JOIN Standort ON Kunden.StandortID = Standort.ID
 JOIN [Status] ON Kunden.[Status] = [Status].[Status] AND [Status].Tabelle = N'KUNDEN'
 JOIN Firma ON Kunden.FirmaID = Firma.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 LEFT OUTER JOIN RechKo ON RechKo.KundenID = Kunden.ID AND RechKo.FibuExpID > 0 AND RechKo.RechDat BETWEEN DATEADD(month, -12, DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), 1)) AND DATEADD(day, -1, DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), 1))
 WHERE Kunden.FirmaID = $1$
-GROUP BY Firma.Bez, [Status].StatusBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Standort.Bez, FORMAT(RechKo.RechDat, N'yyyy-MM');
+GROUP BY Firma.Bez, KdGf.KurzBez, [Status].StatusBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Standort.Bez, FORMAT(RechKo.RechDat, N'yyyy-MM');
 
 SELECT @Spalten = COALESCE(@Spalten + ', [' + Monat + ']', '[' + Monat + ']') FROM (SELECT DISTINCT Monat FROM #KundenUmsatz WHERE Monat IS NOT NULL) AS K ORDER BY Monat ASC;
 
