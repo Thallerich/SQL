8fb54aef0e018c2b3371836c5dd03fa275653768
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-10-23 09:04:51 +0200

    switched to release test system

--- Diverse/SalesApp - MDE-Device von Test auf Live.sql
+++ Diverse/SalesApp - MDE-Device von Test auf Live.sql
@@ -1,4 +1,4 @@
 INSERT INTO Wozabal.dbo.MdeDev (Status, Bez, SerienNr, IMEI, eMail, Art, LetzterZugriff, LastMitarbeiID, StatusInfo, Passwort)
 SELECT MdeDev.Status, MdeDev.Bez, MdeDev.SerienNr, MdeDev.IMEI, MdeDev.eMail, MdeDev.Art, MdeDev.LetzterZugriff, MdeDev.LastMitarbeiID, MdeDev.StatusInfo, MdeDev.Passwort
-FROM Wozabal_Test.dbo.MdeDev
-WHERE MdeDev.ID = 4728;
+FROM [SRVATENADVTEST.WOZABAL.INT\ADVANTEX].Wozabal.dbo.MdeDev
+WHERE MdeDev.ID = 4730;
