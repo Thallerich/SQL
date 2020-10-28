53704394c6167d0b0846f1d174748bd7a21b5c99
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-10-28 14:48:39 +0100

    two bugfixes:  - only use scans with action "auslesen  - use CTE instead of temp-table to improve performance

--- Report/830 - CareTex Hängende Auslieferung - Liste.sql
+++ Report/830 - CareTex Hängende Auslieferung - Liste.sql
@@ -1,18 +1,17 @@
 DECLARE @von datetime = $2$;
 DECLARE @bis datetime = DATEADD(day, 1, $2$);
 
-DROP TABLE IF EXISTS #TmpScans_Rep830;
-
-SELECT Scans.TeileID, Scans.DateTime AS Zeitpunkt
-INTO #TmpScans_Rep830
-FROM Scans
-WHERE Scans.DateTime BETWEEN @von AND @bis
-  AND Scans.ZielNrID = 2;
-
+WITH ExpScan AS (
+  SELECT Scans.TeileID, Scans.DateTime AS Zeitpunkt
+  FROM Scans
+  WHERE Scans.DateTime BETWEEN @von AND @bis
+    AND Scans.ZielNrID = 2
+    AND Scans.ActionsID = 2
+)
 SELECT L.KdNr, L.SuchCode, L.VsaID, L.VsaNr, L.Vsa, L.Nachname, L.Vorname, L.ZimmerNr, L.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS ArtikelBez
 FROM (
   SELECT Kunden.KdNr, Kunden.SuchCode, Vsa.ID AS VsaID, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Traeger.Nachname, Traeger.Vorname, Traeger.PersNr AS ZimmerNr, Teile.Barcode, Teile.ID AS TeileID, Teile.KdArtiID, Scans.Zeitpunkt
-  FROM #TmpScans_Rep830 Scans, Teile, Traeger, Vsa, Kunden
+  FROM ExpScan AS Scans, Teile, Traeger, Vsa, Kunden
   WHERE Scans.TeileID = Teile.ID
     AND Teile.TraegerID = Traeger.ID
     AND Traeger.VsaID = Vsa.ID
