5f427fb24496968c760bb71a3e9f897a9e93545e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-10-29 12:27:48 +0100

    added:   - column "Lieferpausen-Grund"   - filter for break reason

--- Report/926 - Kunden mit Lieferpause.sql
+++ Report/926 - Kunden mit Lieferpause.sql
@@ -1,8 +1,9 @@
-SELECT Standort.Bez AS Haupstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.ID AS VsaID, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], VsaPause.VonDatum AS [Lieferpause von], VsaPause.BisDatum AS [Lieferpause bis]
+SELECT Standort.Bez AS Haupstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.ID AS VsaID, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], WegGrund.WeggrundBez$LAN$ AS [Lieferpausen-Grund], VsaPause.VonDatum AS [Lieferpause von], VsaPause.BisDatum AS [Lieferpause bis]
 FROM Vsa
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Standort ON Kunden.StandortID = Standort.ID
 JOIN VsaPause ON VsaPause.VsaID = Vsa.ID
+JOIN WegGrund ON VsaPause.PauseGrundID = WegGrund.ID
 WHERE VsaPause.KdArtiID < 0
   AND VsaPause.TraegerID < 0
   AND VsaPause.BisDatum >= CAST(GETDATE() AS date)
@@ -10,5 +11,6 @@ WHERE VsaPause.KdArtiID < 0
   AND Kunden.Status = N'A'
   AND Kunden.AdrArtID = 1
   AND Standort.ID IN ($1$)
+  AND VsaPause.PauseGrundID IN ($2$)
   AND Kunden.SichtbarID IN ($SICHTBARIDS$)
 ORDER BY Haupstandort, KdNr, VsaNr;
