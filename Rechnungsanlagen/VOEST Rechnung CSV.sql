296620fc6b78498fc068448e8478cf40df27c56a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-11-17 14:28:55 +0100

    changed column name to SGTXT

--- Rechnungsanlagen/VOEST Rechnung CSV.sql
+++ Rechnungsanlagen/VOEST Rechnung CSV.sql
@@ -1,4 +1,4 @@
-SELECT N'' AS BELNR_D, Kunden._KdBuKr AS BUKRS, RechKo.RechDat AS BLDAT, N'' AS BUDAT, 99 AS BLART, RechKo.RechNr AS XBLNR, Wae.IsoCode AS WAERS, 40 AS BSCHL, 721000 AS HKONT, RechPo.GPreis AS WRBTR, N'V0' AS MWSKZ, Abteil.Bez AS KOSTL, N'' AS AUFNR, RechPo.Menge AS MENGE_D, ME.IsoCode AS MEINS, IIF(Artikel.ArtikelBez IS NOT NULL, Artikel.ArtikelBez, RechPo.Bez) AS SGTEXT
+SELECT N'' AS BELNR_D, Kunden._KdBuKr AS BUKRS, RechKo.RechDat AS BLDAT, N'' AS BUDAT, 99 AS BLART, RechKo.RechNr AS XBLNR, Wae.IsoCode AS WAERS, 40 AS BSCHL, 721000 AS HKONT, RechPo.GPreis AS WRBTR, N'V0' AS MWSKZ, Abteil.Bez AS KOSTL, N'' AS AUFNR, RechPo.Menge AS MENGE_D, ME.IsoCode AS MEINS, IIF(Artikel.ArtikelBez IS NOT NULL, Artikel.ArtikelBez, RechPo.Bez) AS SGTXT
 FROM RechKo, RechPo, Abteil, VSA, Artikel, KdArti, Wae, ME, Kunden
 WHERE RechKo.ID = $RECHKOID$
 AND RechPo.RechKoID = RechKo.ID
