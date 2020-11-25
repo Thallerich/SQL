aec29dc3b3964186518d5a743cbb02477d953204
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-11-25 15:05:13 +0100

    added supplier information

--- Report/546 - Patchstatisikt Zeitraum Bekleidung.sql
+++ Report/546 - Patchstatisikt Zeitraum Bekleidung.sql
@@ -11,8 +11,8 @@ WITH PatchTeile AS (
     AND Teile.KaufwareModus <> 2
   GROUP BY Teile.VsaID, Teile.ArtGroeID, LagerArt.ID, LagerArt.LagerID, IIF(LagerArt.Neuwertig = 1 OR Teile.ErstWoche = Teile.Indienst, 1, 0), IIF(Teile.EinsatzGrund IN ('1', '2'), 1, 0)
 )
-SELECT y.Lagerstandort, Firma.Bez AS Firma, KdGF.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, y.SuchCode AS VsaStichwort, y.VsaBez AS VsaBezeichnung, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, y.AnzGebr, y.AnzNeu, y.WertNeu, y.AnzNeuNeukunde, y.WertNeuNeukunde, Mitarbei.Nachname AS Betreuer, Produktion.Bez AS Produktionsstandort
-FROM KdGF, Kunden, Artikel, KdBer, Mitarbei, Firma, StandBer, Standort AS Produktion, (
+SELECT y.Lagerstandort, Firma.Bez AS Firma, KdGF.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, y.SuchCode AS VsaStichwort, y.VsaBez AS VsaBezeichnung, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, y.AnzGebr, y.AnzNeu, y.WertNeu, y.AnzNeuNeukunde, y.WertNeuNeukunde, Mitarbei.Nachname AS Betreuer, Produktion.Bez AS Produktionsstandort, Lief.LiefNr AS Lieferantennummer, Lief.SuchCode AS Lieferant
+FROM KdGF, Kunden, Artikel, KdBer, Mitarbei, Firma, StandBer, Standort AS Produktion, Lief, (
   SELECT Vsa.StandKonID, Vsa.KundenID, ArtGroe.ArtikelID, Vsa.SuchCode, Vsa.Bez AS VsaBez, Standort.Bez AS Lagerstandort,
     SUM(IIF(Neukunde = 1 AND Neu = 1, Anzahl, 0)) AnzNeuNeukunde,
     SUM(IIF(Neukunde = 0 AND Neu = 1, Anzahl, 0)) AnzNeu,
@@ -36,4 +36,5 @@ WHERE y.KundenID = Kunden.ID
   AND y.StandKonID = StandBer.StandKonID
   AND StandBer.BereichID = 100
   AND StandBer.ProduktionID = Produktion.ID
+  AND Artikel.LiefID = Lief.ID
 ORDER BY Firma, SGF, Kunden.KdNr;
