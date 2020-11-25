60f7edae6a8962eab5939efcb76390acbb80f49c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-11-25 08:08:07 +0100

    added select by windows username

--- Diverse/Arbeitsplatz - Loginlog mit Startparameter.sql
+++ Diverse/Arbeitsplatz - Loginlog mit Startparameter.sql
@@ -1,8 +1,16 @@
-DECLARE @Computer nvarchar(50) = N'LEMICRORR06';
+DECLARE @Computer nvarchar(50) = N'UHF-EN-2114009';
+DECLARE @WinUser nvarchar(50) = N'TL_Metrik-FWGP';
 
-SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, LoginLog.StartParameter
+SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, LoginLog.StartParameter, LoginLog.WindowsUserName
 FROM LoginLog
 JOIN ArbPlatz ON LoginLog.ArbPlatzID = ArbPlatz.ID
 JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
-WHERE ArbPlatz.ComputerName = @Computer
+WHERE UPPER(ArbPlatz.ComputerName) = UPPER(@Computer)
+ORDER BY LoginLog.LoginZeit DESC;
+
+SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, LoginLog.StartParameter, LoginLog.WindowsUserName
+FROM LoginLog
+JOIN ArbPlatz ON LoginLog.ArbPlatzID = ArbPlatz.ID
+JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
+WHERE LOWER(LoginLog.WindowsUserName) = LOWER(@WinUser)
 ORDER BY LoginLog.LoginZeit DESC;
