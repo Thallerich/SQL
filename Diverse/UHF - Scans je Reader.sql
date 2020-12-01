399251b5b6f5c880cb0e09f8b1516657a4e8ffd7
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-12-01 06:09:18 +0100

    updated to inclued new, non-wozi readers

--- Diverse/UHF - Scans je Reader.sql
+++ Diverse/UHF - Scans je Reader.sql
@@ -1,16 +1,16 @@
-USE Wozabal;
+DECLARE @ZielGrpID int = (SELECT ID FROM ZielGrp WHERE ZielGrp.ZielGrpBez = N'Beladeband SAWR');
 
-DECLARE @von datetime;
-DECLARE @bis datetime;
+DECLARE @von datetime2;
+DECLARE @bis datetime2;
 
-SET @von = CONVERT(datetime, CONVERT(char(10), GETDATE(), 120) + ' 00:00:00');
-SET @bis = CONVERT(datetime, CONVERT(char(10), GETDATE(), 120) + ' 23:59:59');
+SET @von = CONVERT(datetime2, CONVERT(char(10), GETDATE(), 120) + ' 00:00:00');
+SET @bis = CONVERT(datetime2, CONVERT(char(10), GETDATE(), 120) + ' 23:59:59');
 
-SELECT ZielNr.ID AS ZielNr, Standort.Bez AS Standort, ZielNr.ZielNrBez AS Scanort, ZielNr.GeraeteNr, COUNT(DISTINCT OPScans.OPTeileID) AS [Teile gescannt], COUNT(OPScans.ID) AS [Anzahl Scans]
+SELECT ZielNr.ID AS ZielNr, Standort.Bez AS Standort, ZielNr.ZielNrBez AS Scanort, FORMAT(OPScans.Zeitpunkt, N'HH') AS Einlesestunde, COUNT(DISTINCT OPScans.OPTeileID) AS [Teile gescannt], COUNT(OPScans.ID) AS [Anzahl Scans]
 FROM OPScans, ZielNr, Standort
 WHERE OPScans.ZielNrID = ZielNr.ID
   AND ZielNr.ProduktionsID = Standort.ID
-  AND ZielNr.GeraeteNr IS NOT NULL
   AND OPScans.Zeitpunkt BETWEEN @von AND @bis
-GROUP BY ZielNr.ID, Standort.Bez, ZielNr.ZielNrBez, ZielNr.GeraeteNr
-ORDER BY ZielNr;
+  AND ZielNr.ProduktionsID = (SELECT ID FROM Standort WHERE SuchCode = N'SAWR')
+GROUP BY ZielNr.ID, Standort.Bez, ZielNr.ZielNrBez, FORMAT(OPScans.Zeitpunkt, N'HH')
+ORDER BY ZielNr, Einlesestunde;
