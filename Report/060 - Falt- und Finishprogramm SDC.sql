28d9c205b88ba8282c921238b66acd3252a3c4f3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-12-03 07:27:12 +0100

    added process

--- Report/060 - Falt- und Finishprogramm SDC.sql
+++ Report/060 - Falt- und Finishprogramm SDC.sql
@@ -5,15 +5,16 @@ WITH Artikelstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'ARTIKEL')
 )
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikelstatus.StatusBez AS Artikelstatus, Standort.SuchCode AS Produktionsstandort, FaltProg.Programm AS Faltprogramm, FaltProg.FaltProgBez AS Faltprogrammbezeichnung, FinishPr.Programm AS Finishprogramm, FinishPrBez AS Finishprogrammbezeichnung
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikelstatus.StatusBez AS Artikelstatus, Standort.SuchCode AS Produktionsstandort, FaltProg.Programm AS Faltprogramm, FaltProg.FaltProgBez$LAN$ AS Faltprogrammbezeichnung, FinishPr.Programm AS Finishprogramm, FinishPr.FinishPrBez$LAN$ AS Finishprogrammbezeichnung, Prozess.Prozess AS Bearbeitungsprozess, Prozess.ProzessBez$LAN$ AS Bearbeitungsprozessbezeichnung
 FROM ArtiStan
 JOIN FaltProg ON ArtiStan.FaltProgID = FaltProg.ID
 JOIN FinishPr ON ArtiStan.FinishPrID = FinishPr.ID
+JOIN Prozess ON ArtiStan.BearbProzessID = Prozess.ID
 JOIN Standort ON ArtiStan.StandortID = Standort.ID
 JOIN Artikel ON ArtiStan.ArtikelID = Artikel.ID
 JOIN Artikelstatus ON Artikel.Status = Artikelstatus.Status
 WHERE Standort.ID = @Produktion
-  AND (($2$ = 1 AND (FaltProg.ID < 0 OR FinishPr.ID < 0)) OR ($2$ = 0))
+  AND (($2$ = 1 AND (FaltProg.ID < 0 OR FinishPr.ID < 0 OR Prozess.ID < 0)) OR ($2$ = 0))
   AND Artikel.ID > 0
   AND Artikel.ArtiTypeID = 1
   AND EXISTS (
