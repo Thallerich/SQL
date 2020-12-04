af3bdeb4ee2a0006e72514bc8122cf182a6a9a0b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-12-04 07:30:53 +0100

    added selection by week

--- Report/926 - Kunden mit Lieferpause.sql
+++ Report/926 - Kunden mit Lieferpause.sql
@@ -1,4 +1,12 @@
-SELECT Standort.Bez AS Haupstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.ID AS VsaID, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], WegGrund.WeggrundBez$LAN$ AS [Lieferpausen-Grund], VsaPause.VonDatum AS [Lieferpause von], VsaPause.BisDatum AS [Lieferpause bis]
+DECLARE @CurrentWeek nchar(7);
+
+SET @CurrentWeek = (
+  SELECT Week.Woche
+  FROM Week
+  WHERE CAST(GETDATE() AS date) BETWEEN Week.VonDat AND Week.BisDat
+);
+
+SELECT Standort.Bez AS Haupstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.ID AS VsaID, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], WegGrund.WeggrundBez$LAN$ AS [Lieferpausen-Grund], VsaPause.VonDatum AS [Lieferpause von], VsaPause.BisDatum AS [Lieferpause bis], VsaPause.VonWoche AS [Lieferpause ab KW], VsaPause.BisWoche AS [Lieferpause bis KW]
 FROM Vsa
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Standort ON Kunden.StandortID = Standort.ID
@@ -6,7 +14,7 @@ JOIN VsaPause ON VsaPause.VsaID = Vsa.ID
 JOIN WegGrund ON VsaPause.PauseGrundID = WegGrund.ID
 WHERE VsaPause.KdArtiID < 0
   AND VsaPause.TraegerID < 0
-  AND VsaPause.BisDatum >= CAST(GETDATE() AS date)
+  AND (VsaPause.BisDatum >= CAST(GETDATE() AS date) OR VsaPause.BisWoche >= @CurrentWeek)
   AND VsaPause.IsLieferpause = 1
   AND Kunden.Status = N'A'
   AND Kunden.AdrArtID = 1
