eecf2c687ab22aeddc621895219c7de73bf6bde4
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-12-09 06:57:24 +0100

    optimizations:  - strip start parameters from default in ini-file

--- Diverse/Arbeitsplatz - Loginlog mit Startparameter.sql
+++ Diverse/Arbeitsplatz - Loginlog mit Startparameter.sql
@@ -1,14 +1,14 @@
-DECLARE @Computer nvarchar(50) = N'UHF-EN-2114009';
+DECLARE @Computer nvarchar(50) = N'sawrpc126';
 DECLARE @WinUser nvarchar(50) = N'TL_Metrik-FWGP';
 
-SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, LoginLog.StartParameter, LoginLog.WindowsUserName
+SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
 FROM LoginLog
 JOIN ArbPlatz ON LoginLog.ArbPlatzID = ArbPlatz.ID
 JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
 WHERE UPPER(ArbPlatz.ComputerName) = UPPER(@Computer)
 ORDER BY LoginLog.LoginZeit DESC;
 
-SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, LoginLog.StartParameter, LoginLog.WindowsUserName
+SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
 FROM LoginLog
 JOIN ArbPlatz ON LoginLog.ArbPlatzID = ArbPlatz.ID
 JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
