9d57329d31de6cbaa64fb8100917c92e3c7464d0
Thallerich <20878157+Thallerich@users.noreply.github.com>
2020-12-11 09:39:46 +0100

    initial version

---
+++ Report/1240 - Teile verkauft.sql
@@ -0,0 +1,16 @@
+SELECT KdGf.KurzBez AS Geschäftsbereich, [Zone].ZonenCode AS Vertriebszone, Holding.Holding, Kunden.KdNr, Kunden.SuchCode AS Kunde, RechKo.RechNr AS Rechnungsnummer, RechKo.RechDat AS Rechnungsdatum, Bereich.Bereich AS Produktbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Teile.Barcode, Teile.AusdienstDat AS Außerdienststellungsdatum, Einsatz.EinsatzBez$LAN$ AS Ausscheidungsgrund, RechPo.EPreis AS [fakt. Wert]
+FROM Teile
+JOIN RechPo ON Teile.RechPoID = RechPo.ID
+JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+JOIN Kunden ON RechKo.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN [Zone] ON Kunden.ZoneID = [Zone].ID
+JOIN Holding ON Kunden.HoldingID = Holding.ID
+JOIN KdArti ON RechPo.KdArtiID = KdArti.ID
+JOIN Artikel ON Teile.ArtikelID = Artikel.ID
+JOIN Bereich ON Artikel.BereichID = Bereich.ID
+JOIN Einsatz ON Teile.AusdienstGrund = Einsatz.EinsatzGrund
+WHERE Kunden.FirmaID = (SELECT ID FROM Firma WHERE SuchCode = N'FA14')
+  AND Teile.AusdienstDat BETWEEN $STARTDATE$ AND $ENDDATE$
+  AND Teile.RechPoID > 0
+  AND Teile.KaufwareModus = 0;
