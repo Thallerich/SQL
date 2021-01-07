abd3e7bae24dff1c5b0fdfd1b108126be5caf8bf
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-01-07 15:48:25 +0100

    insert in batches, delete once at the end

--- Datenbereinigung/OPScans - move back.sql
+++ Datenbereinigung/OPScans - move back.sql
@@ -1,64 +1,40 @@
 SET NOCOUNT ON;
 
 DECLARE @MaxRuns int = 100;
-DECLARE @RowsPerBatch int = 1000000;
-DECLARE @Cutoff datetime2 = N'2017-01-01 00:00:00';
+DECLARE @RowsPerBatch int = 1000;
 
 DECLARE @RunNumber int = 1;
-DECLARE @RowsDeleted int = 1;
-DECLARE @RowsDeletedAllRuns int = 0;
-DECLARE @MaxRows int = 0;
+DECLARE @RowsInserted int = 1;
+DECLARE @RowsInsertedAllRuns int = 0;
+DECLARE @RowsDeleted int = 0;
 DECLARE @Message nvarchar(100);
 
-WHILE ((@RowsDeleted > 0) OR DATEPART(hour, GETDATE()) != 6)
+WHILE @RowsInserted > 0
 BEGIN
   INSERT INTO OPScans (ID, Zeitpunkt, OpTeileID, ZielNrID, ActionsID, OpGrundID, AnfPoID, ArbPlatzID, VPSPoID, EingAnfPoID, Menge, OpEtiKoID, VonLagerBewID, InvPoID, NachLagerBewID, TraegerID, ContainID, LsPoID, Anlage_, Update_, AnlageUserID_, UserID_)
   SELECT TOP (@RowsPerBatch) ID, Zeitpunkt, OpTeileID, ZielNrID, ActionsID, OpGrundID, AnfPoID, ArbPlatzID, VPSPoID, EingAnfPoID, Menge, OpEtiKoID, VonLagerBewID, InvPoID, NachLagerBewID, TraegerID, ContainID, LsPoID, Anlage_, Update_, AnlageUserID_, UserID_
   FROM ___OPSCANS
-  WHERE (LsPoID > 0 OR EingAnfPoID > 0 OR AnfPoID > 0 OR InvPoID > 0 OR OPEtiKoID > 0 OR Menge != 0)
-    AND NOT EXISTS (
+  WHERE NOT EXISTS (
       SELECT OPScans.*
       FROM OPScans
       WHERE OPScans.ID = ___OPSCANS.ID
-    )
-    AND EXISTS (
-      SELECT OPEtiKo.*
-    FROM OPEtiKo
-    WHERE OPEtiKo.ID = ___OPSCANS.OPEtiKoID
-    )
-    AND EXISTS (
-      SELECT LsPo.*
-    FROM LsPo
-    WHERE LsPo.ID = ___OPSCANS.LsPoID
-    )
-    AND EXISTS (
-      SELECT AnfPo.*
-    FROM AnfPo
-    WHERE AnfPo.ID = ___OPSCANS.EingAnfPoID
-    )
-    AND EXISTS (
-      SELECT AnfPo.*
-    FROM AnfPo
-    WHERE AnfPo.ID = ___OPSCANS.AnfPoID
-    )
-    AND EXISTS (
-      SELECT InvPo.*
-    FROM InvPo
-    WHERE InvPo.ID = ___OPSCANS.InvPoID
-    )
-  ORDER BY ID DESC;
-
-  DELETE FROM ___OPSCANS
-  WHERE ID IN (
-    SELECT ID FROM OPScans
   );
 
-  SET @RowsDeleted = @@ROWCOUNT;
+  SET @RowsInserted = @@ROWCOUNT;
 
-  SET @RowsDeletedAllRuns = @RowsDeletedAllRuns + @RowsDeleted;
-  SET @Message = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss', N'de-AT') + N' - Deleted ' + FORMAT(@RowsDeletedAllRuns, N'##,#', N'de-AT') + N' of ' + FORMAT(@MaxRows, N'##,#', N'de-AT') + ' rows!  -> Run number ' + CAST(@RunNumber AS nvarchar) + N' / ' + CAST(@MaxRuns AS nvarchar);
+  SET @RowsInsertedAllRuns = @RowsInsertedAllRuns + @RowsInserted;
+  SET @Message = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss', N'de-AT') + N' - Inserted ' + FORMAT(@RowsInsertedAllRuns, N'##,#', N'de-AT') + ' rows!  -> Run number ' + CAST(@RunNumber AS nvarchar) + N' / ' + CAST(@MaxRuns AS nvarchar);
   SET @RunNumber = @RunNumber + 1;
   RAISERROR(@Message, 0, 1) WITH NOWAIT;
   WAITFOR DELAY '00:00:02';
-
 END;
+
+DELETE FROM ___OPSCANS
+WHERE ID IN (
+  SELECT ID FROM OPScans
+);
+
+SET @RowsDeleted = @@ROWCOUNT;
+
+SET @Message = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss', N'de-AT') + N' - Deleted ' + FORMAT(@RowsDeleted, N'##,#', N'de-AT') + ' rows!';
+RAISERROR(@Message, 0, 1);
