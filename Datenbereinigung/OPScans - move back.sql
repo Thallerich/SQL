bd864d5c57027e22dfb55446c8c14838d304d16a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-01-07 15:56:49 +0100

    actually limit to defined maxruns

--- Datenbereinigung/OPScans - move back.sql
+++ Datenbereinigung/OPScans - move back.sql
@@ -1,7 +1,7 @@
 SET NOCOUNT ON;
 
 DECLARE @MaxRuns int = 100;
-DECLARE @RowsPerBatch int = 1000;
+DECLARE @RowsPerBatch int = 10000;
 
 DECLARE @RunNumber int = 1;
 DECLARE @RowsInserted int = 1;
@@ -9,7 +9,7 @@ DECLARE @RowsInsertedAllRuns int = 0;
 DECLARE @RowsDeleted int = 0;
 DECLARE @Message nvarchar(100);
 
-WHILE @RowsInserted > 0
+WHILE (@RowsInserted > 0 AND @RunNumber <= @MaxRuns)
 BEGIN
   INSERT INTO OPScans (ID, Zeitpunkt, OpTeileID, ZielNrID, ActionsID, OpGrundID, AnfPoID, ArbPlatzID, VPSPoID, EingAnfPoID, Menge, OpEtiKoID, VonLagerBewID, InvPoID, NachLagerBewID, TraegerID, ContainID, LsPoID, Anlage_, Update_, AnlageUserID_, UserID_)
   SELECT TOP (@RowsPerBatch) ID, Zeitpunkt, OpTeileID, ZielNrID, ActionsID, OpGrundID, AnfPoID, ArbPlatzID, VPSPoID, EingAnfPoID, Menge, OpEtiKoID, VonLagerBewID, InvPoID, NachLagerBewID, TraegerID, ContainID, LsPoID, Anlage_, Update_, AnlageUserID_, UserID_
@@ -26,7 +26,6 @@ BEGIN
   SET @Message = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss', N'de-AT') + N' - Inserted ' + FORMAT(@RowsInsertedAllRuns, N'##,#', N'de-AT') + ' rows!  -> Run number ' + CAST(@RunNumber AS nvarchar) + N' / ' + CAST(@MaxRuns AS nvarchar);
   SET @RunNumber = @RunNumber + 1;
   RAISERROR(@Message, 0, 1) WITH NOWAIT;
-  WAITFOR DELAY '00:00:02';
 END;
 
 DELETE FROM ___OPSCANS
