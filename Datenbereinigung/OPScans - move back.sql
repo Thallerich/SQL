77e68c0c1fa8a4ca569d67c0e0ae978f141c6425
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-01-08 08:15:19 +0100

    move back all scans except where foreign key is missing

--- Datenbereinigung/OPScans - move back.sql
+++ Datenbereinigung/OPScans - move back.sql
@@ -1,7 +1,8 @@
 SET NOCOUNT ON;
 
-DECLARE @MaxRuns int = 100;
-DECLARE @RowsPerBatch int = 10000;
+DECLARE @MaxRuns int = 1000;
+DECLARE @RowsPerBatch int = 3000;
+DECLARE @MaxTemp int = @MaxRuns * @RowsPerBatch;
 
 DECLARE @RunNumber int = 1;
 DECLARE @RowsInserted int = 1;
@@ -9,16 +10,48 @@ DECLARE @RowsInsertedAllRuns int = 0;
 DECLARE @RowsDeleted int = 0;
 DECLARE @Message nvarchar(100);
 
+DROP TABLE IF EXISTS #TmpRestoreOPScans;
+
+SELECT TOP (@MaxTemp) ID, Zeitpunkt, OpTeileID, ZielNrID, ActionsID, OpGrundID, AnfPoID, ArbPlatzID, VPSPoID, EingAnfPoID, Menge, OpEtiKoID, VonLagerBewID, InvPoID, NachLagerBewID, TraegerID, ContainID, LsPoID, Anlage_, Update_, AnlageUserID_, UserID_
+INTO #TmpRestoreOPScans
+FROM Salesianer.dbo.___OPSCANS
+WHERE NOT EXISTS (
+    SELECT OPScans.*
+    FROM Salesianer.dbo.OPScans
+    WHERE OPScans.ID = ___OPSCANS.ID
+  )
+  AND EXISTS (
+    SELECT OPEtiKo.*
+    FROM Salesianer.dbo.OPEtiKo
+    WHERE OPEtiKo.ID = ___OPSCANS.OPEtiKoID
+  )
+  AND EXISTS (
+    SELECT LsPo.*
+    FROM Salesianer.dbo.LsPo
+    WHERE LsPo.ID = ___OPSCANS.LsPoID
+  )
+  AND EXISTS (
+    SELECT AnfPo.*
+    FROM Salesianer.dbo.AnfPo
+    WHERE AnfPo.ID = ___OPSCANS.EingAnfPoID
+  )
+  AND EXISTS (
+    SELECT AnfPo.*
+    FROM Salesianer.dbo.AnfPo
+    WHERE AnfPo.ID = ___OPSCANS.AnfPoID
+  )
+  AND EXISTS (
+    SELECT InvPo.*
+    FROM Salesianer.dbo.InvPo
+    WHERE InvPo.ID = ___OPSCANS.InvPoID
+  )
+ORDER BY ID DESC;
+
 WHILE (@RowsInserted > 0 AND @RunNumber <= @MaxRuns)
 BEGIN
   INSERT INTO Salesianer.dbo.OPScans (ID, Zeitpunkt, OpTeileID, ZielNrID, ActionsID, OpGrundID, AnfPoID, ArbPlatzID, VPSPoID, EingAnfPoID, Menge, OpEtiKoID, VonLagerBewID, InvPoID, NachLagerBewID, TraegerID, ContainID, LsPoID, Anlage_, Update_, AnlageUserID_, UserID_)
   SELECT TOP (@RowsPerBatch) ID, Zeitpunkt, OpTeileID, ZielNrID, ActionsID, OpGrundID, AnfPoID, ArbPlatzID, VPSPoID, EingAnfPoID, Menge, OpEtiKoID, VonLagerBewID, InvPoID, NachLagerBewID, TraegerID, ContainID, LsPoID, Anlage_, Update_, AnlageUserID_, UserID_
-  FROM Salesianer.dbo.___OPSCANS
-  WHERE NOT EXISTS (
-      SELECT OPScans.*
-      FROM Salesianer.dbo.OPScans
-      WHERE OPScans.ID = ___OPSCANS.ID
-  );
+  FROM #TmpRestoreOPScans;
 
   SET @RowsInserted = @@ROWCOUNT;
 
@@ -26,6 +59,11 @@ BEGIN
   SET @Message = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss', N'de-AT') + N' - Inserted ' + FORMAT(@RowsInsertedAllRuns, N'##,#', N'de-AT') + ' rows!  -> Run number ' + CAST(@RunNumber AS nvarchar) + N' / ' + CAST(@MaxRuns AS nvarchar);
   SET @RunNumber = @RunNumber + 1;
   RAISERROR(@Message, 0, 1) WITH NOWAIT;
+
+  DELETE FROM #TmpRestoreOPScans
+  WHERE ID IN (
+    SELECT ID FROM Salesianer.dbo.OPScans
+  );
 END;
 
 DELETE FROM Salesianer.dbo.___OPSCANS
