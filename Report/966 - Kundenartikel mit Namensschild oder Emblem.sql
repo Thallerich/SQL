5f163636daf9f61fc4393f6c191c854ee5d145a8
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-01-08 11:41:52 +0100

    labels / emblems moved to own table structure

--- Report/966 - Kundenartikel mit Namensschild oder Emblem.sql
+++ Report/966 - Kundenartikel mit Namensschild oder Emblem.sql
@@ -1,18 +1,25 @@
+WITH KdArtiStatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'KDARTI')
+)
 SELECT 
   Kunden.KdNr, 
   Kunden.SuchCode AS Kunde, 
   Artikel.ArtikelNr, 
   Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, 
-  Status.StatusBez$LAN$ AS [Status Kundenartikel], 
+  KdArtiStatus.StatusBez AS [Status Kundenartikel], 
   KdArti.Variante, 
   KdArti.VariantBez AS Variantenbezeichnung, 
-  CAST(IIF(KdArti.NsKdArtiID > 0, 1, 0) AS bit) AS [Namenschild?], 
-  CAST(IIF(KdArti.EmbKdArtiID > 0, 1, 0) AS bit) AS [Emblem?],
-  KdArti.ID AS KdArtiID               -- um im AdvanTex direkt zum Kundenartikel springen zu können; wird nicht angezeigt / exportiert
+  CAST(IIF(NsAppl.ID IS NOT NULL, 1, 0) AS bit) AS [Namenschild?], 
+  CAST(IIF(EmblAppl.ID IS NOT NULL, 1, 0) AS bit) AS [Emblem?],
+  KdArti.ID AS KdArtiID               -- um im AdvanTex direkt zum Kundenartikel springen zu kÃ¶nnen; wird nicht angezeigt / exportiert
 FROM KdArti
 JOIN Kunden ON KdArti.KundenID = Kunden.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-JOIN Status ON KdArti.Status = Status.Status AND Status.Tabelle = N'KDARTI'
-WHERE Kunden.AdrArtID = 1             -- nur tatsächliche Kunden
+JOIN KdArtiStatus ON KdArti.Status = KdArtiStatus.Status
+LEFT JOIN KdArAppl AS NsAppl ON NsAppl.KdArtiID = KdArti.ID AND NsAppl.ArtiTypeID = 2
+LEFT JOIN KdArAppl AS EmblAppl ON EmblAppl.KdArtiID = KdArti.ID AND EmblAppl.ArtiTypeID = 3
+WHERE Kunden.AdrArtID = 1             -- nur tatsÃ¤chliche Kunden
   AND Kunden.Status = N'A'            -- nur aktive Kunden
-  AND (KdArti.NsKdArtiID > 0 OR KdArti.EmbKdArtiID > 0)
+  AND (NsAppl.ID IS NOT NULL OR EmblAppl.ID IS NOT NULL);
