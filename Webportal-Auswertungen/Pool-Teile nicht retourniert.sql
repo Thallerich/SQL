9fae2b22a77a37197b98c53bc7fcba0eac5ec97f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-01-20 12:57:15 +0100

    initial version

---
+++ Webportal-Auswertungen/Pool-Teile nicht retourniert.sql
@@ -0,0 +1,23 @@
+WITH LastScan AS (
+  SELECT Scans.TeileID, MAX(Scans.ID) AS ScanID
+  FROM Scans
+  WHERE Scans.ActionsID = 135  -- Action "Ausgabe an Pool-Träger"
+  GROUP BY Scans.TeileID
+)
+SELECT Teile.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, CAST(Scans.[DateTime] AS date) AS Ausgabedatum, Scans.Info AS [Träger laut Webportal]
+FROM Teile
+JOIN Artikel ON Teile.ArtikelID = Artikel.ID
+JOIN ArtGroe ON Teile.ArtGroeID = ArtGroe.ID
+JOIN LastScan ON LastScan.TeileID = Teile.ID
+JOIN Scans ON LastScan.ScanID = Scans.ID
+JOIN Traeger ON Teile.TraegerID = Traeger.ID
+JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+WHERE Teile.LastActionsID = 135  -- Action "Ausgabe an Pool-Träger"
+  AND Kunden.ID = " . $kundenid . "
+  AND Vsa.ID IN ($vsaids)
+  AND Traeger.AbteilID IN (
+    SELECT WebUAbt.AbteilID
+      FROM WebUAbt
+      WHERE WebUAbt.WebUserID = " . $webuserID . "
+  );
