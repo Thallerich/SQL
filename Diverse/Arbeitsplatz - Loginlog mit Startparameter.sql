76a8f415db25e46cee41073ff6158d9dd83e79d3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-02-01 16:06:04 +0100

    small change to structure

--- Diverse/Arbeitsplatz - Loginlog mit Startparameter.sql
+++ Diverse/Arbeitsplatz - Loginlog mit Startparameter.sql
@@ -1,5 +1,4 @@
-DECLARE @Computer nvarchar(50) = N'sawrpc126';
-DECLARE @WinUser nvarchar(50) = N'TL_Metrik-FWGP';
+DECLARE @Computer nvarchar(50) = N'THSK201002';
 
 SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
 FROM LoginLog
@@ -8,9 +7,15 @@ JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
 WHERE UPPER(ArbPlatz.ComputerName) = UPPER(@Computer)
 ORDER BY LoginLog.LoginZeit DESC;
 
+GO
+
+DECLARE @WinUser nvarchar(50) = N'';
+
 SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
 FROM LoginLog
 JOIN ArbPlatz ON LoginLog.ArbPlatzID = ArbPlatz.ID
 JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
 WHERE LOWER(LoginLog.WindowsUserName) = LOWER(@WinUser)
 ORDER BY LoginLog.LoginZeit DESC;
+
+GO
