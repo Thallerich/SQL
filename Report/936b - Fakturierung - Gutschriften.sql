e0c1a732e3f76eecc51a465cbe7cb3991c4a2b80
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-02-08 10:27:46 +0100

    added columns for customer industrial sector

--- Report/936b - Fakturierung - Gutschriften.sql
+++ Report/936b - Fakturierung - Gutschriften.sql
@@ -1,4 +1,4 @@
-SELECT Holding.Holding AS Kette, Kunden.Debitor, Kunden.KdNr, Kunden.Name1 AS [Name], Hauptstandort.SuchCode AS [Standard BT], Expedition.SuchCode AS [prod. BT], Produktion.SuchCode AS [intern prod. BT], RechKo.RechNr, RechKo.RechDat, RechKo.Memo AS Bemerkung, IIF(RechKo.BasisRechKoID < 0, NULL, BasisRechKo.RechNr) AS [GU für], RechKo.Art AS RechArt, Artikel.ArtikelNr, IIF(Artikel.ID < 0, RechPo.Bez, Artikel.ArtikelBez$LAN$) AS Bezeichnung, IIF(KdArti.ID < 0, NULL, KdArti.Variante) AS Variante, RechPo.EPreis AS Preis, RechPo.RabattProz AS [RabattProzent], RechPo.GPreis AS Betrag
+SELECT Holding.Holding AS Kette, Branche.Branche AS BrancheCode, Branche.BrancheBez$LAN$ AS Branche, Kunden.Debitor, Kunden.KdNr, Kunden.Name1 AS [Name], Hauptstandort.SuchCode AS [Standard BT], Expedition.SuchCode AS [prod. BT], Produktion.SuchCode AS [intern prod. BT], RechKo.RechNr, RechKo.RechDat, RechKo.Memo AS Bemerkung, IIF(RechKo.BasisRechKoID < 0, NULL, BasisRechKo.RechNr) AS [GU für], RechKo.Art AS RechArt, Artikel.ArtikelNr, IIF(Artikel.ID < 0, RechPo.Bez, Artikel.ArtikelBez$LAN$) AS Bezeichnung, IIF(KdArti.ID < 0, NULL, KdArti.Variante) AS Variante, RechPo.EPreis AS Preis, RechPo.RabattProz AS [RabattProzent], RechPo.GPreis AS Betrag
 FROM RechPo
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
 JOIN Kunden ON RechKo.KundenID = Kunden.ID
@@ -12,6 +12,7 @@ JOIN Vsa ON RechPo.VsaID = Vsa.ID
 JOIN StandBer ON StandBer.StandKonID = Vsa.StandKonID AND StandBer.BereichID = KdBer.BereichID
 JOIN Standort AS Expedition ON StandBer.ExpeditionID = Expedition.ID
 JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
+JOIN Branche ON Kunden.BrancheID = Branche.ID
 WHERE RechKo.RechDat BETWEEN $2$ AND $3$
   AND RechKo.FirmaID IN ($1$)
   AND RechKo.Art = N'G';
