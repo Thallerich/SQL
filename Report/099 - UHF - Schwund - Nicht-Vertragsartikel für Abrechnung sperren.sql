66dcc7d28ba93efac3972ebbf7d4823af7f71927
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-02-09 09:56:23 +0100

    optimization pass   - do not lock parts that where delievered as replacement   - output amount of parts affected

--- Report/099 - UHF - Schwund - Nicht-Vertragsartikel für Abrechnung sperren.sql
+++ Report/099 - UHF - Schwund - Nicht-Vertragsartikel für Abrechnung sperren.sql
@@ -1,27 +1,51 @@
-DECLARE @cutoff datetime;
-SET @cutoff = $1$;
+DECLARE @cutoff datetime = $1$;
+DECLARE @CustomerID int = $ID$;
+DECLARE @LockedNonContract int;
+DECLARE @NotLockedReplacement int;
+DECLARE @LockedOld int;
 
 UPDATE OPTeile SET RechPoID = -2
 WHERE OPTeile.Status = 'W'
+  AND OPTeile.LastActionsID IN (102, 116, 120)
   AND OPTeile.RechPoID < 0
-  AND OPTeile.VsaID IN (SELECT Vsa.ID FROM Vsa WHERE Vsa.KundenID = $ID$)
-  AND OPTeile.ArtikelID IN (SELECT KdArti.ArtikelID FROM KdArti WHERE KdArti.KundenID = $ID$ AND KdArti.Vertragsartikel = $FALSE$);
+  AND OPTeile.RechPoID != -2
+  AND OPTeile.VsaID IN (SELECT Vsa.ID FROM Vsa WHERE Vsa.KundenID = @CustomerID)
+  AND OPTeile.ArtikelID IN (SELECT KdArti.ArtikelID FROM KdArti WHERE KdArti.KundenID = @CustomerID AND KdArti.Vertragsartikel = 0 AND KdArti.ErsatzFuerKdArtiID < 0);
+
+SET @LockedNonContract = @@ROWCOUNT;
+
+SET @NotLockedReplacement = (
+  SELECT COUNT(OPTeile.ID)
+  FROM OPTeile
+  WHERE OPTeile.Status = 'W'
+    AND OPTeile.LastActionsID IN (102, 116, 120)
+    AND OPTeile.RechPoID < 0
+    AND OPTeile.RechPoID != -2
+    AND OPTeile.VsaID IN (SELECT Vsa.ID FROM Vsa WHERE Vsa.KundenID = @CustomerID)
+    AND OPTeile.ArtikelID IN (SELECT KdArti.ArtikelID FROM KdArti WHERE KdArti.KundenID = @CustomerID AND KdArti.Vertragsartikel = 0 AND KdArti.ErsatzFuerKdArtiID > 0)
+);
 
 UPDATE OPTeile SET RechPoID = -2
 WHERE OPTeile.Status = 'W'
+  AND OPTeile.LastActionsID IN (102, 116, 120)
   AND OPTeile.RechPoID < 0
-  AND OPTeile.VsaID IN (SELECT Vsa.ID FROM Vsa WHERE Vsa.KundenID = $ID$)
+  AND OPTeile.RechPoID != -2
+  AND OPTeile.VsaID IN (SELECT Vsa.ID FROM Vsa WHERE Vsa.KundenID = @CustomerID)
   AND OPTeile.LastScanTime < @cutoff;
 
-SELECT 'Schwundteile, die kein Vertragsartikel sind, für Verrechnung gesperrt!' AS Message
+SET @LockedOld = @@ROWCOUNT;
+
+SELECT CAST(@LockedNonContract AS nvarchar) + ' Schwundteile, die kein Vertragsartikel sind, für Verrechnung gesperrt!' AS Message
 UNION ALL
-SELECT 'Schwundteile mit letztem Scan vor ' + CONVERT(char(10), FORMAT($1$, 'dd.MM.yyyy', 'de-AT')) + ' für die Verrechnung gesperrt!' AS Message
+SELECT CAST(@NotLockedReplacement AS nvarchar) + ' Schwundteile wurden nicht gesperrt, da als Ersatz geliefert!' AS Message
 UNION ALL
-SELECT TRIM(CONVERT(char, x.Anz)) + ' Schwundteile verbleiben zur Verrechnung.' AS Message
+SELECT CAST(@LockedOld AS nvarchar) + ' Schwundteile mit letztem Scan vor ' + CONVERT(char(10), FORMAT(@cutoff, 'dd.MM.yyyy', 'de-AT')) + ' für die Verrechnung gesperrt!' AS Message
+UNION ALL
+SELECT CAST(x.Anz AS nvarchar) + ' Schwundteile verbleiben zur Verrechnung.' AS Message
 FROM (
   SELECT COUNT(*) AS Anz
   FROM OPTeile
   WHERE OPTeile.Status = 'W'
     AND OPTeile.RechPoID = -1
-    AND OPTeile.VsaID IN (SELECT Vsa.ID FROM Vsa WHERE Vsa.KundenID = $ID$)
+    AND OPTeile.VsaID IN (SELECT Vsa.ID FROM Vsa WHERE Vsa.KundenID = @CustomerID)
 ) x;
