c93e7423390c22c6c9d596b3b32b6ecbb7348999
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-02-09 15:33:38 +0100

    added  - column "zone"  - column "main location"

--- Report/446 - Kundenartikel ohne Preis.sql
+++ Report/446 - Kundenartikel ohne Preis.sql
@@ -3,13 +3,15 @@ WITH KdArtiStatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'KDARTI')
 )
-SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Name1, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS Variantenbezeichnung, KdArti.Umlauf, KdArtiStatus.StatusBez AS KundenartikelStatus, KdArti.Vorlaeufig
-FROM Kunden, KdArti, Artikel, KdGf, Firma, KdArtiStatus
+SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS SGF, [Zone].ZonenCode AS Vertriebszone, Standort.SuchCode AS Hauptstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Name1, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS Variantenbezeichnung, KdArti.Umlauf, KdArtiStatus.StatusBez AS KundenartikelStatus, KdArti.Vorlaeufig
+FROM Kunden, KdArti, Artikel, KdGf, Firma, KdArtiStatus, [Zone], Standort
 WHERE KdArti.KundenID = Kunden.ID
   AND Kunden.KdgfID = Kdgf.ID
   AND Kunden.FirmaID = Firma.ID
   AND KdArti.ArtikelID = Artikel.ID
   AND KdArtiStatus.Status = KdArti.Status
+  AND Kunden.ZoneID = [Zone].ID
+  AND Kunden.StandortID = Standort.ID
   AND KdGf.ID IN ($2$)
   AND KdArti.LeasingPreis = 0  
   AND KdArti.WaschPreis = 0 
