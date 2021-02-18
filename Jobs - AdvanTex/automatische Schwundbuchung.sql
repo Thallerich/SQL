0661b34a6165ae154d594421570b748ee61ef4b1
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-02-18 12:32:42 +0100

    included customer number 19080

--- Jobs - AdvanTex/automatische Schwundbuchung.sql
+++ Jobs - AdvanTex/automatische Schwundbuchung.sql
@@ -1,9 +1,8 @@
 DECLARE @KdNr AS TABLE (KdNr int);
 
-INSERT INTO @KdNr VALUES (24045), (11050), (20000), (6071), (7240), (9013), (23041), (23042), (23032), (23037), (10001756), (242013), (2710499), (2710498), (18029), (245347), (248564), (246805), (10003247);
+INSERT INTO @KdNr VALUES (24045), (11050), (20000), (6071), (7240), (9013), (23041), (23042), (23032), (23037), (10001756), (242013), (2710499), (2710498), (18029), (245347), (248564), (246805), (10003247), (19080);
 
 DROP TABLE IF EXISTS #TmpSchwundAuto;
-DROP TABLE IF EXISTS #TmpVsaAnf;
 
 SELECT OPTeile.ID AS OPTeileID, IIF(OPTeile.LastErsatzFuerKdArtiID < 0, OPTeile.ArtikelID, KdArti.ArtikelID) AS ArtikelID, OPTeile.VsaID AS VsaID
 INTO #TmpSchwundAuto
@@ -21,26 +20,4 @@ WHERE ID IN (
   SELECT OPTeileID FROM #TmpSchwundAuto
 );
 
-/*
-SELECT VsaAnf.ID, VsaAnf.Status, (VsaAnf.Bestand - Schwund.Schwundmenge) - ((VsaAnf.Bestand - Schwund.Schwundmenge) % Artikel.Packmenge) + IIF((VsaAnf.Bestand - Schwund.Schwundmenge) % Artikel.Packmenge = 0, 0, Artikel.Packmenge) AS Bestand, VsaAnf.BestandIst - Schwund.Schwundmenge AS BestandIst
-INTO #TmpVsaAnf
-FROM VsaAnf, KdArti, Artikel, Vsa, Kunden, (
-  SELECT SA.VsaID, SA.ArtikelID, COUNT(SA.OPTeileID) AS Schwundmenge
-  FROM #TmpSchwundAuto AS SA
-  GROUP BY SA.VsaID, SA.ArtikelID
-) AS Schwund
-WHERE VsaAnf.KdArtiID = KdArti.ID
-  AND KdArti.ArtikelID = Artikel.ID
-  AND VsaAnf.VsaID = Vsa.ID
-  AND Vsa.KundenID = Kunden.ID
-  AND Schwund.VsaID = Vsa.ID
-  AND Schwund.ArtikelID = Artikel.ID
-  AND Kunden.KdNr IN (SELECT KdNr FROM @KdNr);
-
-UPDATE VsaAnf SET VsaAnf.BestandIst = VA.BestandIst
-FROM VsaAnf, #TmpVsaAnf AS VA
-WHERE VsaAnf.ID = VA.ID;
-*/
-
 DROP TABLE IF EXISTS #TmpSchwundAuto;
-DROP TABLE IF EXISTS #TmpVsaAnf;
