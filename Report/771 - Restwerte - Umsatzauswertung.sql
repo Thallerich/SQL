35c25bb1083b9328577aab5f6f384a170b8658ee
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-02-19 09:34:43 +0100

    initial version

---
+++ Report/771 - Restwerte - Umsatzauswertung.sql
@@ -0,0 +1,13 @@
+SELECT KdGf.KurzBez AS Geschäftsbereich, Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, RechKo.RechNr, RechKo.RechDat AS Rechnungsdatum, SUM(RechPo.GPreis) AS [Umsatz netto]
+FROM RechPo
+JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+JOIN Kunden ON RechKo.KundenID = Kunden.ID
+JOIN Firma ON RechKo.FirmaID = Firma.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN RPoType ON RechPo.RPoTypeID = RPoType.ID
+WHERE Firma.ID IN ($1$)
+  AND RechKo.RechDat BETWEEN $STARTDATE$ AND $ENDDATE$
+  AND KdGf.ID IN ($2$)
+  AND RPoType.StatistikGruppe = N'Restwerte'
+GROUP BY KdGf.KurzBez, Firma.SuchCode, Kunden.KdNr, Kunden.SuchCode, RechKo.Rechnr, RechKo.RechDat
+ORDER BY Firma, KdNr, Rechnungsdatum;
