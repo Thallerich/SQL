9799c526e40710624bae1685fd5e75acc0ff64ff
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-02-25 08:33:12 +0100

    added "Vertragsbestand"

--- Report/953 - UHF - Drehungsliste.sql
+++ Report/953 - UHF - Drehungsliste.sql
@@ -1,9 +1,12 @@
 SELECT KdGf.KurzBez AS SGF,
   Bereich.BereichBez$LAN$ AS Produktbereich,
+  Kunden.KdNr,
   Kunden.SuchCode AS  [Kunde SuchCode],
+  Vsa.ID AS VsaID,
   Vsa.Bez AS Vsa,
   Artikel.ArtikelNr,
   Artikel.ArtikelBez$LAN$ AS Artikel,
+  ISNULL(VsaAnf.Bestand, 0) AS Vertragsbestand,
   SUM(IIF(OPTeile.Status = N'Q', 1, 0)) AS [Teile beim Kunden],
   SUM(IIF(OPTeile.Status = N'W' AND OPTeile.RechPoID > 0, 1, 0)) AS [Schwundmarkiert (verrechnet)],
   SUM(IIF(OPTeile.Status = N'W' AND OPTeile.RechPoID < 0, 1, 0)) AS [Schwundmarkiert (nicht verrechnet)],
@@ -17,7 +20,9 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
 JOIN Bereich ON Artikel.BereichID = Bereich.ID
+JOIN KdArti ON KdArti.ArtikelID = Artikel.ID AND KdArti.KundenID = Kunden.ID
+LEFT JOIN VsaAnf ON VsaAnf.VsaID = Vsa.ID AND VsaAnf.KdArtiID = KdArti.ID
 WHERE Kunden.ID = $1$
   AND OPTeile.Status IN (N'Q', N'W')
   AND OPTeile.LastActionsID IN (102, 116)
-GROUP BY KdGf.KurzBez, Bereich.BereichBez$LAN$, Kunden.SuchCode, Vsa.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$;
+GROUP BY KdGf.KurzBez, Bereich.BereichBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Vsa.ID, Vsa.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, VsaAnf.Bestand;
