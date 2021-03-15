d1ea68f300aa6ef9546e5ebc7319950d4691d279
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-03-15 16:34:05 +0100

    IT-44861   - 601a: added column "Größe"   - 340b: refactored and added column "Größe"

---
+++ Report/340b - Liefermenge Kunde pro Monat.sql
@@ -0,0 +1,66 @@
+DECLARE @vonDatum date = $STARTDATE$;
+DECLARE @bisDatum date = $ENDDATE$;
+DECLARE @vonWoche nchar(7) = (SELECT Week.Woche FROM Week WHERE @vonDatum BETWEEN Week.VonDat AND Week.BisDat);
+DECLARE @bisWoche nchar(7) = (SELECT Week.Woche FROM Week WHERE @bisDatum BETWEEN Week.VonDat AND Week.BisDat);
+
+SELECT FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT') AS Monat,
+  Kunden.KdNr,
+  Kunden.SuchCode AS Kunde,
+  Vsa.VsaNr AS [VSA-Nr],
+  Vsa.SuchCode AS [VSA-Stichwort],
+  Vsa.Bez AS [VSA-Bezeichnung],
+  Vsa.Name1 AS [VSA-Adresszeile 1],
+  Vsa.Name2 AS [VSA-Adresszeile 2],
+  Vsa.GebaeudeBez AS Gebäude,
+  Abteil.Abteilung AS Kostenstelle,
+  Abteil.Bez AS Kostenstellenbezeichnung,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung,
+  ArtGroe.Groesse AS [Größe],
+  LsPo.EPreis AS Preis,
+  SUM(LsPo.Menge) AS Menge,
+  SUM(LsPo.Menge * LsPo.EPreis) AS [Summe netto]
+FROM LsPo
+JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+JOIN Vsa ON LsKo.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN ArtGroe ON LsPo.ArtGroeID = ArtGroe.ID
+JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN Abteil ON LsPo.AbteilID = Abteil.ID
+WHERE Kunden.ID IN ($3$)
+  AND LsKo.Datum BETWEEN @vonDatum AND @bisDatum
+  AND Vsa.SichtbarID IN ($SICHTBARIDS$)
+GROUP BY FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT'), Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.SuchCode, Vsa.Bez, Vsa.Name1, Vsa.Name2, Vsa.GebaeudeBez, Abteil.Abteilung, Abteil.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, ArtGroe.Groesse, LsPo.EPreis
+
+UNION ALL
+
+SELECT AbtKdArW.Monat,
+  Kunden.KdNr,
+  Kunden.SuchCode AS Kunde,
+  Vsa.VsaNr AS [VSA-Nr],
+  Vsa.SuchCode AS [VSA-Stichwort],
+  Vsa.Bez AS [VSA-Bezeichnung],
+  Vsa.Name1 AS [VSA-Adresszeile 1],
+  Vsa.Name2 AS [VSA-Adresszeile 2],
+  Vsa.GebaeudeBez AS Gebäude,
+  Abteil.Abteilung AS Kostenstelle,
+  Abteil.Bez AS Kostenstellenbezeichnung,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung,
+  NULL AS [Größe],
+  AbtKdArW.EPreis AS Preis,
+  SUM(AbtKdArW.Menge) AS Menge,
+  SUM(AbtKdArW.Menge * AbtKdArW.EPreis) AS [Summe netto]
+FROM AbtKdArW
+JOIN Vsa ON AbtKdArW.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdArti ON AbtKdArW.KdArtiID = KdArti.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN Abteil ON AbtKdArW.AbteilID = Abteil.ID
+JOIN Wochen ON AbtKdArW.WochenID = Wochen.ID
+JOIN Week ON Wochen.Woche = Week.Woche
+WHERE Kunden.ID IN ($3$)
+  AND Wochen.Woche BETWEEN @vonWoche AND @bisWoche
+  AND Vsa.SichtbarID IN ($SICHTBARIDS$)
+GROUP BY AbtKdArW.Monat, Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.SuchCode, Vsa.Bez, Vsa.Name1, Vsa.Name2, Vsa.GebaeudeBez, Abteil.Abteilung, Abteil.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, AbtKdArW.EPreis;
--- Report/601a - Bestellt_Geliefert für Kunde.sql
+++ Report/601a - Bestellt_Geliefert für Kunde.sql
@@ -1,13 +1,14 @@
 DROP TABLE IF EXISTS #TmpAnfData;
 
-SELECT AnfKo.Auftragsdatum AS Bestelldatum, AnfKo.Lieferdatum, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, AnfPo.Angefordert AS Bestellt, 0 AS Geliefert, 0 AS Verrechnet, CONVERT(money, 0) AS Einzelpreis, AnfKo.LsKoID, AnfPo.KdArtiID, AnfPo.VpsKoID, AnfPo.ArtGroeID, AnfPo.LsKoGruID
+SELECT AnfKo.Auftragsdatum AS Bestelldatum, AnfKo.Lieferdatum, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS [Größe], AnfPo.Angefordert AS Bestellt,Anfpo.UrAngefordert AS UrMengeBestellt, 0 AS Geliefert, 0 AS Verrechnet, CONVERT(money, 0) AS Einzelpreis, AnfKo.LsKoID, AnfPo.KdArtiID, AnfPo.VpsKoID, AnfPo.ArtGroeID, AnfPo.LsKoGruID
 INTO #TmpAnfData
-FROM AnfPo, AnfKo, Vsa, Kunden, KdArti, Artikel
+FROM AnfPo, AnfKo, Vsa, Kunden, KdArti, Artikel, ArtGroe
 WHERE AnfPo.AnfKoID = AnfKo.ID
   AND AnfKo.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
   AND AnfPo.KdArtiID = KdArti.ID
   AND KdArti.ArtikelID = Artikel.ID
+  AND AnfPo.ArtGroeID = ArtGroe.ID
   AND LieferDatum BETWEEN $1$ AND $2$
   AND Kunden.ID = $ID$
   AND (AnfPo.Angefordert > 0 OR AnfPo.Geliefert > 0);
@@ -25,7 +26,7 @@ WHERE x.LsKoID = AnfData.LsKoID
   AND x.VpsKoID = AnfData.VpsKoID
   AND x.LsKoGruID = AnfData.LsKoGruID;
 
-SELECT Bestelldatum, Lieferdatum, KdNr, Kunde, VsaNr, Vsa, ArtikelNr, Artikelbezeichnung, SUM(Bestellt) AS Bestellt, SUM(Geliefert) AS Geliefert, SUM(Verrechnet) AS Verrechnet, Einzelpreis
+SELECT Bestelldatum, Lieferdatum, KdNr, Kunde, VsaNr, Vsa, ArtikelNr, Artikelbezeichnung, [Größe], SUM(Bestellt) AS Bestellt, SUM(URMengeBestellt) AS UrBestell, SUM(Geliefert) AS Geliefert, SUM(Verrechnet) AS Verrechnet, Einzelpreis
 FROM #TmpAnfData AnfData
-GROUP BY Bestelldatum, Lieferdatum, KdNr, Kunde, VsaNr, Vsa, ArtikelNr, Artikelbezeichnung, Einzelpreis
+GROUP BY Bestelldatum, Lieferdatum, KdNr, Kunde, VsaNr, Vsa, ArtikelNr, Artikelbezeichnung, [Größe], Einzelpreis
 ORDER BY KdNr, VsaNr, Bestelldatum, ArtikelNr;
