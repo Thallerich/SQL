dc3f00e46ffc5814dc3c3f6824a5c489dc73ab42
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-04-01 08:02:45 +0200

    IT-46278   - added first delivery date to customer

--- Report/804 - offene Aufträge Lager je SGF.sql
+++ Report/804 - offene Aufträge Lager je SGF.sql
@@ -13,7 +13,7 @@ SELECT Kunden.Kdnr, Kunden.Suchcode as Kunde, Holding.Holding, Traeger.Nachname,
   FROM Scans
   WHERE Scans.TeileID = Teile.ID
     AND Scans.ActionsID = 57
-), EntnKo.PatchDatum AS [Patchdatum Entnahmeliste]
+), EntnKo.PatchDatum AS [Patchdatum Entnahmeliste], Teile.IndienstDat AS [Lieferdatum zum Kunden]
 FROM Teile
 JOIN ArtGroe ON Teile.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
@@ -25,8 +25,7 @@ JOIN Teilestatus ON Teile.[Status] = Teilestatus.[Status]
 JOIN Traegerstatus ON Traeger.[Status] = Traegerstatus.[Status]
 LEFT JOIN EntnPo ON Teile.EntnPoID = EntnPo.ID AND Teile.EntnPoID > 0
 LEFT JOIN EntnKo ON EntnPo.EntnKoID = EntnKo.ID
-WHERE Teile.Anlage_ > N'2019-04-01 00:00:00'
-  AND Artikel.BereichID = 100
+WHERE Artikel.BereichID = 100
   AND Kunden.Status = N'A'
   AND Vsa.Status = N'A'
   AND Traeger.Status != N'I'
