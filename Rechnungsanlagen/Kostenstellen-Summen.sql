e57da22a43a834a368f20e9374bb828796240785
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-04-12 10:26:38 +0200

    "fixed" for special customer requirement

--- Rechnungsanlagen/Kostenstellen-Summen.sql
+++ Rechnungsanlagen/Kostenstellen-Summen.sql
@@ -1,9 +1,10 @@
-SELECT RechKo.RechNr, RechKo.RechDat, WocheVon.Woche AS StartWoche, WocheBis.Woche AS EndWoche, Abteil.Abteilung AS KsSt, Abteil.Bez AS KsStBez, SUM(RechPo.GPreis) AS PosSumme
+SELECT RechKo.RechNr, RechKo.RechDat, MIN(Wochen.Woche) AS StartWoche, MAX(Wochen.Woche) AS EndWoche, Traeger.PersNr, SUM(TraeArch.Menge * AbtKdArW.EPreis) AS PosSumme
 FROM RechPo
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
-JOIN Abteil ON RechPo.AbteilID = Abteil.ID
-JOIN Week AS WocheVon ON RechKo.VonDatum BETWEEN WocheVon.VonDat AND WocheVon.BisDat
-JOIN Week AS WocheBis ON RechKo.BisDatum BETWEEN WocheBis.VonDat AND WocheBis.BisDat
+JOIN AbtKdArW ON AbtKdArW.RechPoID = RechPo.ID
+JOIN TraeArch ON TraeArch.AbtKdArWID = AbtKdArW.ID
+JOIN Wochen ON TraeArch.WochenID = Wochen.ID
+JOIN TraeArti ON TraeArch.TraeArtiID = TraeArti.ID
+JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
 WHERE RechKo.ID = $RECHKOID$
-GROUP BY RechKo.RechNr, RechKo.RechDat, WocheVon.Woche, WocheBis.Woche, Abteil.Abteilung, Abteil.Bez
-ORDER BY KsSt ASC;
+GROUP BY RechKo.RechNr, RechKo.RechDat, Traeger.PersNr;
