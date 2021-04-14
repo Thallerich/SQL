5480d6fe7251a86c8bd9028fa8cdfa514f81858b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-04-14 16:41:33 +0200

    added "rechnungspositionstyp"

--- Report/771 - Restwerte - Umsatzauswertung.sql
+++ Report/771 - Restwerte - Umsatzauswertung.sql
@@ -1,4 +1,4 @@
-SELECT KdGf.KurzBez AS Geschäftsbereich, Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, RechKo.RechNr, RechKo.RechDat AS Rechnungsdatum, SUM(RechPo.GPreis) AS [Umsatz netto]
+SELECT KdGf.KurzBez AS Geschäftsbereich, Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, RechKo.RechNr, RechKo.RechDat AS Rechnungsdatum, RPoType.RPoTypeBez$LAN$ AS [Rechnungstyp], SUM(RechPo.GPreis) AS [Umsatz netto]
 FROM RechPo
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
 JOIN Kunden ON RechKo.KundenID = Kunden.ID
@@ -9,5 +9,5 @@ WHERE Firma.ID IN ($1$)
   AND RechKo.RechDat BETWEEN $STARTDATE$ AND $ENDDATE$
   AND KdGf.ID IN ($2$)
   AND RPoType.StatistikGruppe = N'Restwerte'
-GROUP BY KdGf.KurzBez, Firma.SuchCode, Kunden.KdNr, Kunden.SuchCode, RechKo.Rechnr, RechKo.RechDat
+GROUP BY KdGf.KurzBez, Firma.SuchCode, Kunden.KdNr, Kunden.SuchCode, RechKo.Rechnr, RechKo.RechDat, RPoType.RPoTypeBez$LAN$
 ORDER BY Firma, KdNr, Rechnungsdatum;
