6c44e149d87105714c8e5052b14eadb7ab4aea62
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-04-19 14:47:57 +0200

    IT-42652   - added columns VPE, VPE-Menge, Gewicht pro Stück   - exclude negative delivery amounts (credit)

--- Report/361 - Liefermengen Produktion.sql
+++ Report/361 - Liefermengen Produktion.sql
@@ -1,5 +1,5 @@
-SELECT Firma.Bez AS Firma, Standort.Bez AS Produzent, StandKon.StandKonBez$LAN$ AS [Standort-Konfiguration], KdGf.KurzBez AS SGF, KdGf.KdGfBez$LAN$ AS [Geschäftsfeld], Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT') AS Monat, SUM(LsPo.Menge) AS Menge, ROUND(SUM(LsPo.Menge * Artikel.StueckGewicht), 2) AS Liefergewicht
-FROM LsPo, LsKo, Vsa, Kunden, Firma, Standort, StandKon, KdGf, KdArti, Artikel, KdBer, Bereich
+SELECT Firma.Bez AS Firma, Standort.Bez AS Produzent, StandKon.StandKonBez$LAN$ AS [Standort-Konfiguration], KdGf.KurzBez AS SGF, KdGf.KdGfBez$LAN$ AS [Geschäftsfeld], Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT') AS Monat, SUM(LsPo.Menge) AS Menge, Artikel.StueckGewicht AS [Gewicht pro Stück], Artikel.Packmenge AS [VPE-Menge], ME.MeBez$LAN$ AS [VPE], ROUND(SUM(LsPo.Menge * Artikel.StueckGewicht), 2) AS Liefergewicht
+FROM LsPo, LsKo, Vsa, Kunden, Firma, Standort, StandKon, KdGf, KdArti, Artikel, KdBer, Bereich, ME
 WHERE LsPo.LsKoID = LsKo.ID
   AND LsKo.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
@@ -11,8 +11,10 @@ WHERE LsPo.LsKoID = LsKo.ID
   AND KdArti.ArtikelID = Artikel.ID
   AND KdArti.KdBerID = KdBer.ID
   AND KdBer.BereichID = Bereich.ID
+  AND Artikel.MEID = ME.ID
   AND Kunden.KdGfID IN ($2$)
   AND Standort.ID IN ($3$)
   AND LsKo.Datum BETWEEN $STARTDATE$ AND $ENDDATE$
   AND Artikel.ID > 0
-GROUP BY Firma.Bez, Standort.Bez, StandKon.StandKonBez$LAN$, KdGf.KurzBez, KdGf.KdGfBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Bereich.BereichBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT');
+  AND LsPo.Menge > 0
+GROUP BY Firma.Bez, Standort.Bez, StandKon.StandKonBez$LAN$, KdGf.KurzBez, KdGf.KdGfBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Bereich.BereichBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT'), Artikel.StueckGewicht, Artikel.Packmenge, ME.MeBez$LAN$;
