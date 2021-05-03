abfbbf5188ea1fa8aa610cbd0583032656d6556b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-05-03 13:10:59 +0200

    changes:   - show all times a part was taken out of the system   - added "kostenstelle"

--- Report/905 - Bekleidungsausgabesystem_ Entnahmen.sql
+++ Report/905 - Bekleidungsausgabesystem_ Entnahmen.sql
@@ -6,12 +6,13 @@ WITH Teilestatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'TEILE')
 ), 
-LastScan AS (
-  SELECT Scans.TeileID, MAX(Scans.DateTime) AS MaxTimestamp
+Entnahmen AS (
+  SELECT Scans.TeileID, Scans.[DateTime], Scans.LastPoolTraegerID
   FROM Scans
-  GROUP BY Scans.TeileID
+  WHERE Scans.[DateTime] BETWEEN @von AND @bis
+    AND Scans.ActionsID = 65
 )
-SELECT Scans.DateTime AS [Datum der Entnahme], EntnahmeTraeger.PersNr, EntnahmeTraeger.Traeger AS TraegerNr, EntnahmeTraeger.Nachname, EntnahmeTraeger.Vorname, Teile.Barcode, Teile.RentomatChip AS Chipcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Kunden.KdNr, Kunden.SuchCode AS Kunde, IIF(Teile.Ausdienst IS NULL, Teile.RestwertInfo, Teile.AusdRestw) AS Restwert, Teilestatus.StatusBez AS [aktueller Status des Teils], Actions.ActionsBez$LAN$ AS [letzte Aktion des Teils]
+SELECT Entnahmen.DateTime AS [Datum der Entnahme], EntnahmeTraeger.PersNr, EntnahmeTraeger.Traeger AS TraegerNr, EntnahmeTraeger.Nachname, EntnahmeTraeger.Vorname, Abteil.Abteilung AS KsSt, Abteil.Bez AS Kostenstelle, Teile.Barcode, Teile.RentomatChip AS Chipcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Kunden.KdNr, Kunden.SuchCode AS Kunde, IIF(Teile.Ausdienst IS NULL, Teile.RestwertInfo, Teile.AusdRestw) AS Restwert, Teilestatus.StatusBez AS [aktueller Status des Teils], Actions.ActionsBez$LAN$ AS [letzte Aktion des Teils]
 FROM Teile
 JOIN TraeArti ON Teile.TraeArtiID = TraeArti.ID
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
@@ -21,11 +22,10 @@ JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
 JOIN Teilestatus ON Teile.Status = Teilestatus.Status
-JOIN LastScan ON LastScan.TeileID = Teile.ID
-JOIN Scans ON LastScan.MaxTimestamp = Scans.Datetime
-JOIN Actions ON Scans.ActionsID = Actions.ID
-JOIN Traeger AS EntnahmeTraeger ON Scans.LastPoolTraegerID = EntnahmeTraeger.ID
+JOIN Entnahmen ON Entnahmen.TeileID = Teile.ID
+JOIN Traeger AS EntnahmeTraeger ON Entnahmen.LastPoolTraegerID = EntnahmeTraeger.ID
+JOIN Abteil ON EntnahmeTraeger.AbteilID = Abteil.ID
+JOIN Actions ON Teile.LastActionsID = Actions.ID
 WHERE Vsa.RentomatID = $2$
   AND Traeger.RentoArtID IN (SELECT RentoArt.ID FROM RentoArt WHERE RentoArt.Code IN (N'S', N'T'))
-  AND Scans.DateTime BETWEEN @von AND @bis
-  AND Actions.ID = 65;   -- Aktion "Bekleidungssystem: Entnahme"
+ORDER BY Barcode ASC;
