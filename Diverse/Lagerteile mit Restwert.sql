3437482fde19eaefdbabe21135de2c9eebdabc57
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-05-20 14:52:00 +0200

    IT-48283 preparation work

--- Diverse/Lagerteile mit Restwert.sql
+++ Diverse/Lagerteile mit Restwert.sql
@@ -1,15 +1,22 @@
-USE Wozabal
+USE Salesianer
 GO
 
-SELECT RTRIM(Standort.Bez) AS Lagerstandort, RTRIM(LagerArt.LagerartBez) AS Lagerart, RTRIM(TeileLag.Barcode) AS Barcode, RTRIM(Artikel.ArtikelNr) AS ArtikelNr, RTRIM(Artikel.ArtikelBez) AS Artikelbezeichnung, RTRIM(ArtGroe.Groesse) AS [Größe], RTRIM(Status.StatusBez) AS Teilestatus, FORMAT(TeileLag.Restwert, 'C', 'de-AT') AS [Restwert bei Einlagerung], Teile.AlterInfo AS [Alter des Teils in Wochen], TeileLag.ErstWoche, TeileLag.AnzWaschen AS [Anzahl Wäschen], FORMAT(TeileLag.ErstDatum, 'd', 'de-at') AS [Erste Auslieferung]
+WITH LagerteilStatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'TeileLag')
+)
+SELECT Standort.Bez AS Lagerstandort, LagerArt.LagerartBez AS Lagerart, TeileLag.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS [GrÃ¶ÃŸe], LagerteilStatus.StatusBez AS [Status Lager-Teil], FORMAT(TeileLag.Restwert, 'C', 'de-AT') AS [Restwert bei Einlagerung], TeileLag.AnzWaschen AS [Anzahl WÃ¤schen], FORMAT(TeileLag.ErstDatum, 'd', 'de-at') AS [Erste Auslieferung zu erstem Kunden], TeileLag.AusdienstDat, Kunden.KdNr AS [KdNr letzter Kunde], Kunden.SuchCode AS [letzter Kunde]
 FROM TeileLag
-LEFT OUTER JOIN Teile ON TeileLag.Barcode = Teile.Barcode
 JOIN ArtGroe ON TeileLag.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
-JOIN Status ON TeileLag.Status = Status.Status AND Status.Tabelle = N'TEILELAG'
+JOIN LagerteilStatus ON TeileLag.[Status] = LagerteilStatus.[Status]
 JOIN LagerArt ON TeileLag.LagerArtID = LagerArt.ID
+JOIN Firma ON Lagerart.FirmaID = Firma.ID
 JOIN Standort ON LagerArt.LagerID = Standort.ID
-WHERE TeileLag.Status < N'X'
-AND TeileLag.ArtGroeID > 0
+JOIN Kunden ON TeileLag.KundenID = Kunden.ID
+WHERE TeileLag.Status < N'Y'
+  AND TeileLag.ArtGroeID > 0
+  AND Artikel.ArtikelNr IN (N'01AU', N'01AV', N'01S9', N'01SO', N'01V1', N'01V3', N'01VN', N'04AU', N'04S9', N'04SO', N'04V2', N'04VN', N'04VW', N'05AU', N'05AV', N'3522003823', N'05S9', N'05SO', N'05V1', N'05V2', N'05V3', N'05VN', N'05VW', N'06AU', N'06AV', N'3522003824', N'06S9', N'06SO', N'06V1', N'06V3', N'06VN', N'06VW', N'24V3', N'06PS', N'05PP', N'06PO', N'05PO', N'04PS', N'05SD', N'06SD', N'70P6', N'03PO');
 
 GO
