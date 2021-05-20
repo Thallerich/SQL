8bb89b38080d98258256727982d45804790cd290
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-05-20 09:17:58 +0200

    optimization pass   - set variables directly on declare, instead of separatly   - fill UserID for creation and update of row

--- Diverse/BKS - Zusätzlicher Trägerartikel.sql
+++ Diverse/BKS - Zusätzlicher Trägerartikel.sql
@@ -1,16 +1,13 @@
-DECLARE @ArtiIDAlt integer;
-DECLARE @ArtiIDNeu integer;
-DECLARE @KundenID integer;
+DECLARE @ArtiIDAlt int = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'2504000002');
+DECLARE @ArtiIDNeu int = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'2504000001');
+DECLARE @KundenID int = (SELECT Kunden.ID FROM Kunden WHERE Kunden.Kdnr = 31063);
+DECLARE @UserID int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST');
 
-SET @ArtiIDAlt = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'3084043303');
-SET @ArtiIDNeu = (SELECT Artikel.ID FROM Artikel WHERE Artikel.ArtikelNr = N'3084043305');
-SET @KundenID = (SELECT Kunden.ID FROM Kunden WHERE Kunden.Kdnr = 31066);
-
-INSERT INTO TraeArti (VsaID, TraegerID, ArtGroeID, KdArtiID, MengeKredit)
+INSERT INTO TraeArti (VsaID, TraegerID, ArtGroeID, KdArtiID, MengeKredit, AnlageUserID_, UserID_)
 SELECT Vsa.ID AS VsaID, Traeger.ID AS TraegerID, 
   (SELECT Groesse.ID FROM ArtGroe AS Groesse WHERE Groesse.ArtikelID = @ArtiIDNeu AND Groesse.Groesse = ArtGroe.Groesse) AS ArtGroeID,
   (SELECT KdArti.ID FROM KdArti WHERE KdArti.KundenID = @KundenID AND KdArti.ArtikelID = @ArtiIDNeu) AS KdArtiID,
-  TraeArti.MengeKredit
+  TraeArti.MengeKredit, @UserID AS AnlageUserID_, @UserID AS UserID_
 FROM TraeArti, Traeger, Vsa, KdArti, ArtGroe
 WHERE TraeArti.TraegerID = Traeger.ID
   AND Traeger.VsaID = Vsa.ID
