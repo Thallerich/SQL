1b8678035680bd3949a0ae7d162ec2e0838e79fe
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-05-26 09:00:17 +0200

    replace column "erstwoche" with calculated week

--- Report/546 - Patchstatisikt Zeitraum Bekleidung.sql
+++ Report/546 - Patchstatisikt Zeitraum Bekleidung.sql
@@ -1,15 +1,16 @@
 WITH PatchTeile AS (
   SELECT Teile.VsaID, Teile.ArtGroeID, LagerArt.ID AS LagerArtID, LagerArt.LagerID,
-    IIF(LagerArt.Neuwertig = 1 OR Teile.Erstwoche = Teile.Indienst, 1, 0) AS Neu,
+    IIF(LagerArt.Neuwertig = 1 OR Week.Woche = Teile.Indienst, 1, 0) AS Neu,
     IIF(Teile.EinsatzGrund IN ('1', '2'), 1, 0) AS Neukunde,
     COUNT(*) Anzahl
   FROM Teile
+  JOIN Week ON DATEADD(day, Teile.AnzTageImLager, Teile.ErstDatum) BETWEEN Week.VonDat AND Week.BisDat
   JOIN LagerArt ON Teile.LagerArtID = LagerArt.ID
   WHERE Teile.Patchdatum BETWEEN $1$ AND $2$
     AND Teile.LagerArtID > 0
     AND Teile.KaufwareModus <> 1
     AND Teile.KaufwareModus <> 2
-  GROUP BY Teile.VsaID, Teile.ArtGroeID, LagerArt.ID, LagerArt.LagerID, IIF(LagerArt.Neuwertig = 1 OR Teile.ErstWoche = Teile.Indienst, 1, 0), IIF(Teile.EinsatzGrund IN ('1', '2'), 1, 0)
+  GROUP BY Teile.VsaID, Teile.ArtGroeID, LagerArt.ID, LagerArt.LagerID, IIF(LagerArt.Neuwertig = 1 OR Week.Woche = Teile.Indienst, 1, 0), IIF(Teile.EinsatzGrund IN ('1', '2'), 1, 0)
 )
 SELECT y.Lagerstandort, Firma.Bez AS Firma, KdGF.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, y.SuchCode AS VsaStichwort, y.VsaBez AS VsaBezeichnung, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, y.AnzGebr, y.AnzNeu, y.WertNeu, y.AnzNeuNeukunde, y.WertNeuNeukunde, Mitarbei.Nachname AS Betreuer, Produktion.Bez AS Produktionsstandort, Lief.LiefNr AS Lieferantennummer, Lief.SuchCode AS Lieferant
 FROM KdGF, Kunden, Artikel, KdBer, Mitarbei, Firma, StandBer, Standort AS Produktion, Lief, (
