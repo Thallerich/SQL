14555994246ff12746777092449bb90cb16540c3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-06-07 16:30:48 +0200

    added serveral columns requests by SaffLa

--- Report/404 - Preisliste.sql
+++ Report/404 - Preisliste.sql
@@ -1,9 +1,18 @@
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.WaschPreis, IIF(FakFreq.FakPerID <> -1, KdArti.PeriodenPreis, KdArti.LeasingPreis) AS LeasingPreis, KdArti.KundenID
+WITH KdArtiStatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'KdArti')
+)
+SELECT Holding.Holding, Holding.Bez AS Holdingbezeichnung, Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS [Varianten-Bezeichnung], Bereich.Bereich AS Produktbereich, KdArti.WaschPreis AS [Bearbeitungs-Preis], IIF(FakFreq.FakPerID <> -1, KdArti.LeasingPreis, LeasPreisWoche.LeasPreisProWo) AS [Leasing-Preis], FakFreq.FakFreqBez AS Fakturafrequenz, KdArti.KundenID, KdArti.ID AS KdArtiID
 FROM KdArti
+CROSS APPLY dbo.advFunc_GetLeasPreisProWo(KdArti.ID) AS LeasPreisWoche
+JOIN Kunden ON KdArti.KundenID = Kunden.ID
+JOIN Holding ON Kunden.HoldingID = Holding.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+JOIN Bereich ON KdBer.BereichID = Bereich.ID
 JOIN FakFreq ON KdBer.FakFreqID = FakFreq.ID
-JOIN [Status] ON KdArti.[Status] = [Status].[Status] AND [Status].Tabelle = N'KDARTI'
-WHERE KdArti.KundenID = $1$
-  AND Status.ID IN ($2$)
-ORDER BY Artikelbezeichnung ASC;
+JOIN KdArtiStatus ON KdArti.[Status] = KdArtiStatus.[Status]
+WHERE KdArti.KundenID IN ($2$)
+  AND KdArtiStatus.ID IN ($3$)
+ORDER BY Holding, KdNr, Artikelbezeichnung;
