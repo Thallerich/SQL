03b71c48269b58788adb1aff27a746f063eac0b2
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-06-11 08:44:17 +0200

    fixed due to changed structure for mwst

--- Report/949 - Preisliste Bewohner-Artikel.sql
+++ Report/949 - Preisliste Bewohner-Artikel.sql
@@ -16,7 +16,7 @@ WHERE Kunden.ID = $ID$;
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 DECLARE @NK int = (SELECT Wae.NK FROM Wae JOIN Kunden ON Kunden.VertragWaeID = Wae.ID WHERE Kunden.ID = $ID$);
 
-SELECT DISTINCT Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.ID AS KundenID, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, KdArti.WaschPreis AS NettoPreis, MwSt.MWStFaktor, KdBer.RabattWasch, ROUND((KdArti.WaschPreis - (KdArti.WaschPreis / 100 * KdBer.RabattWasch)) * (1 + MwSt.MWStFaktor), @NK) AS BruttoPreisRabattiert, @NK AS Nachkomma
+SELECT DISTINCT Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.ID AS KundenID, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, KdArti.WaschPreis AS NettoPreis, MwStZeit.MWStFaktor, KdBer.RabattWasch, ROUND((KdArti.WaschPreis - (KdArti.WaschPreis / 100 * KdBer.RabattWasch)) * (1 + MwStZeit.MWStFaktor), @NK) AS BruttoPreisRabattiert, @NK AS Nachkomma
 FROM Traeger
 JOIN BewKdAr ON Traeger.BewAbrID = BewKdAr.BewAbrID
 JOIN KdArti ON BewKdAr.KdArtiID = KdArti.ID
@@ -24,6 +24,7 @@ JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN MwSt ON Kunden.MWStID = MwSt.ID
+JOIN MwStZeit ON MwStZeit.MwStID = MwSt.ID AND CAST(GETDATE() AS date) BETWEEN MwStZeit.VonDatum AND MwStZeit.BisDatum
 JOIN KdBer ON KdArti.KdBerID = KdBer.ID
 WHERE BewKdAr.ProzTraeger = 100
   AND BewKdAr.Vorlaeufig = 0
