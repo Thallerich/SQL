009d6316fab70b401605a9981bc72516621210fb
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-06-15 09:49:36 +0200

    added selection for orders

--- Diverse/Restwert - Artikelliste mit EK-Preis-Änderungen.sql
+++ Diverse/Restwert - Artikelliste mit EK-Preis-Änderungen.sql
@@ -14,6 +14,8 @@ WHERE Artikel.ArtiTypeID = 1
   AND Artikel.ID > 0
 ORDER BY ArtikelNr, Änderungszeitpunkt DESC;
 
+GO
+
 WITH Artikelstatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
   FROM [Status]
@@ -38,3 +40,27 @@ LEFT JOIN ArtiVerwend ON ArtiVerwend.ArtikelID = Artikel.ID
 WHERE Artikel.ArtiTypeID = 1
   AND Artikel.ID > 0
 ORDER BY ArtikelNr, Firma DESC;
+
+GO
+
+WITH Artikelstatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'Artikel')
+)
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, Artikelstatus.StatusBez AS [Status Artikel], Bereich.BereichBez AS Produktbereich, ArtGru.ArtGruBez AS Artikelgruppe, Firma.SuchCode AS Firma, Lief.LiefNr, COALESCE(Lief.SuchCode, Lief.Name1) AS Lieferant, MAX(BKo.BestDat) AS [letzte Bestellung], SUM(BPo.BestMenge) AS [bestellte Menge]
+FROM BPo
+JOIN BKo ON BPo.BKoID = BKo.ID
+JOIN ArtGroe ON BPo.ArtGroeID = ArtGroe.ID
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+JOIN Bereich ON Artikel.BereichID = Bereich.ID
+JOIN ArtGru ON Artikel.ArtGruID = ArtGru.ID
+JOIN Artikelstatus ON Artikelstatus.[Status] = Artikel.[Status]
+JOIN Lief ON BKo.LiefID = Lief.ID
+JOIN Lagerart ON BKo.LagerArtID = Lagerart.ID
+JOIN Firma ON Lagerart.FirmaID = Firma.ID
+WHERE BKo.BestDat >= DATEADD(year, -1, GETDATE())
+  AND BKo.Status BETWEEN N'D' AND N'M'
+GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez, Artikelstatus.StatusBez, Bereich.BereichBez, ArtGru.ArtGruBez, Firma.SuchCode, Lief.LiefNr, COALESCE(Lief.SuchCode, Lief.Name1);
+
+GO
