78f903e19580d8b029b7742c7dd2af94ea0c2df8
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-06-16 08:01:14 +0200

    IT-49073 added date of final warehouse check

--- Report/883 - BK-Teile - angelegt im Zeitraum.sql
+++ Report/883 - BK-Teile - angelegt im Zeitraum.sql
@@ -2,8 +2,14 @@ WITH Teilestatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'TEILE')
+),
+Lagerendkontrolle AS (
+  SELECT MAX(Scans.[DateTime]) AS Zeitpunkt, Scans.TeileID
+  FROM Scans
+  WHERE Scans.ActionsID = 49
+  GROUP BY Scans.TeileID
 )
-SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr AS [VSA-Nummer], Vsa.Bez AS [VSA-Bezeichnung], Traeger.Traeger AS [Trägernummer], Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS [Größe], Teilestatus.StatusBez AS [aktueller Teile-Status], Teile.Barcode, Teile.Anlage_ AS [Anlage-Zeitpunkt], Teile.PatchDatum, Teile.IndienstDat AS [Indienststellungs-Datum]
+SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr AS [VSA-Nummer], Vsa.Bez AS [VSA-Bezeichnung], Traeger.Traeger AS [Trägernummer], Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS [Größe], Teilestatus.StatusBez AS [aktueller Teile-Status], Teile.Barcode, Teile.Anlage_ AS [Anlage-Zeitpunkt], Teile.PatchDatum, CAST(Lagerendkontrolle.Zeitpunkt AS date) AS Lagerendkontrolle, Teile.IndienstDat AS [Indienststellungs-Datum]
 FROM Teile
 JOIN TraeArti ON Teile.TraeArtiID = TraeArti.ID
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
@@ -15,6 +21,7 @@ JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
 JOIN Teilestatus ON Teile.Status = Teilestatus.Status
 JOIN GroePo ON GroePo.Groesse = ArtGroe.Groesse AND GroePo.GroeKoID = Artikel.GroeKoID
+LEFT JOIN Lagerendkontrolle ON Lagerendkontrolle.TeileID = Teile.ID
 WHERE Teile.Anlage_ BETWEEN $STARTDATE$ AND $ENDDATE$
   AND Firma.ID IN ($1$)
   AND KdGf.ID IN ($2$)
