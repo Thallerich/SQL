40b17dd8b12035351279cd484872d4bf29841111
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-06-17 13:28:51 +0200

    IT-49266   - merged report 526 into 526a/526b   - added ABC class of customer   - check customer visibility   - general optimization

--- Report/526a - Kunden nach SGF mit Ansprechpartnern.sql
+++ Report/526a - Kunden nach SGF mit Ansprechpartnern.sql
@@ -4,29 +4,36 @@ WITH Kundenstatus AS (
   WHERE [Status].Tabelle = UPPER(N'KUNDEN')
 ),
 Ansprechpartner AS (
-  SELECT Sachbear.TableID AS KundenID, Sachbear.Anrede, Sachbear.Titel, Sachbear.Vorname, Sachbear.Name, Sachbear.SerienAnrede, Sachbear.eMail, Sachbear.Telefon
+  SELECT Sachbear.TableID AS KundenID, Sachbear.Anrede, Sachbear.Titel, Sachbear.Vorname, Sachbear.Name, Sachbear.SerienAnrede, Sachbear.eMail, Sachbear.Telefon, Sachbear.Abteilung, Sachbear.Position, Rollen.RollenBez$LAN$ AS Rolle
   FROM Sachbear
-  WHERE Sachbear.TableName = N'KUNDEN'
-    AND Sachbear.Status = N'A'
+  LEFT JOIN SachRoll ON SachRoll.SachBearID = SachBear.ID
+  LEFT JOIN Rollen ON SachRoll.RollenID = Rollen.ID
+  WHERE Sachbear.Status = N'A'
+    AND Sachbear.TableName = N'KUNDEN'
 
   UNION ALL
 
-  SELECT Vsa.KundenID, Sachbear.Anrede, Sachbear.Titel, Sachbear.Vorname, Sachbear.Name, Sachbear.SerienAnrede, Sachbear.eMail, Sachbear.Telefon
+  SELECT Vsa.KundenID AS KundenID, Sachbear.Anrede, Sachbear.Titel, Sachbear.Vorname, Sachbear.Name, Sachbear.SerienAnrede, Sachbear.eMail, Sachbear.Telefon, Sachbear.Abteilung, Sachbear.Position, Rollen.RollenBez$LAN$ AS Rolle
   FROM Sachbear
   JOIN Vsa ON Sachbear.TableID = Vsa.ID
-  WHERE Sachbear.TableName = N'VSA'
-    AND Sachbear.Status = N'A'
+  LEFT JOIN SachRoll ON SachRoll.SachBearID = SachBear.ID
+  LEFT JOIN Rollen ON SachRoll.RollenID = Rollen.ID
+  WHERE Sachbear.Status = N'A'
+    AND Sachbear.TableName = N'VSA'
 )
-SELECT DISTINCT KdGf.KurzBez AS SGF, Firma.Bez AS Firma, Standort.Bez AS Hauptstandort, Kundenstatus.StatusBez AS Kundenstatus, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Name1 AS Adresszeile1, Kunden.Name2 AS Adresszeile2, Kunden.Name3 AS Adresszeile3, Kunden.Strasse, Kunden.Land, Kunden.PLZ, Kunden.Ort, Ansprechpartner.Anrede, Ansprechpartner.Titel, Ansprechpartner.Vorname, Ansprechpartner.Name, Ansprechpartner.SerienAnrede, Ansprechpartner.eMail, Ansprechpartner.Telefon
+SELECT DISTINCT KdGf.KurzBez AS Geschäftsbereich, Firma.Bez AS Firma, Standort.Bez AS Hauptstandort, Kundenstatus.StatusBez AS Kundenstatus, ABC.ABCBez AS Kundenklasse, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Name1 AS Adresszeile1, Kunden.Name2 AS Adresszeile2, Kunden.Name3 AS Adresszeile3, Kunden.Strasse, Kunden.Land, Kunden.PLZ, Kunden.Ort, Kunden.KundeSeit AS [Kunde seit], ZahlZiel.ZahlZielBez$LAN$ AS Zahlungsziel, Ansprechpartner.Anrede, Ansprechpartner.Titel, Ansprechpartner.Vorname, Ansprechpartner.Name, Ansprechpartner.SerienAnrede, Ansprechpartner.eMail, Ansprechpartner.Telefon, Ansprechpartner.Abteilung, Ansprechpartner.Position, Ansprechpartner.Rolle
 FROM Vsa
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
 JOIN Standort ON Kunden.StandortID = Standort.ID
 JOIN Kundenstatus ON Kunden.Status = Kundenstatus.Status
-JOIN Ansprechpartner ON Ansprechpartner.KundenID = Kunden.ID
+JOIN ABC ON Kunden.ABCID = ABC.ID
+JOIN ZahlZiel ON Kunden.ZahlZielID = ZahlZiel.ID
+LEFT JOIN Ansprechpartner ON Ansprechpartner.KundenID = Kunden.ID
 WHERE Kunden.AdrArtID = 1
   AND Kundenstatus.ID IN ($3$)
   AND KdGf.ID IN ($1$)
   AND Kunden.StandortID IN ($2$)
-ORDER BY Kunden.KdNr
+  AND Kunden.SichtbarID IN ($SICHTBARIDS$)
+ORDER BY Kunden.KdNr, Ansprechpartner.Name;
--- Report/526b - Kunden nach SGF mit Kundenbetreuer.sql
+++ Report/526b - Kunden nach SGF mit Kundenbetreuer.sql
@@ -3,14 +3,17 @@ WITH Kundenstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'KUNDEN')
 )
-SELECT DISTINCT KdGf.KurzBez AS Geschäftsbereich, Firma.Bez AS Firma, Kundenstatus.StatusBez AS Kundenstatus, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Name1 AS Adresszeile1, Kunden.Name2 AS Adresszeile2, Kunden.Name3 AS Adresszeile3, Kunden.Strasse, Kunden.Land, Kunden.PLZ, Kunden.Ort, Mitarbei.Name, Mitarbei.Initialen
+SELECT DISTINCT KdGf.KurzBez AS Geschäftsbereich, Firma.Bez AS Firma, Kundenstatus.StatusBez AS Kundenstatus, ABC.ABCBez$LAN$ AS Kundenklasse, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kunden.Name1 AS Adresszeile1, Kunden.Name2 AS Adresszeile2, Kunden.Name3 AS Adresszeile3, Kunden.Strasse, Kunden.Land, Kunden.PLZ, Kunden.Ort, Kunden.KundeSeit AS [Kunde seit], ZahlZiel.ZahlZielBez$LAN$ AS Zahlungsziel, Mitarbei.Name, Mitarbei.Initialen AS Kundenbetreuer
 FROM Kunden
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
 JOIN Kundenstatus ON Kunden.Status = Kundenstatus.Status
+JOIN ABC ON Kunden.ABCID = ABC.ID
 JOIN KdBer ON KdBer.KundenID = Kunden.ID
 JOIN Mitarbei ON KdBer.BetreuerID = Mitarbei.ID
+JOIN ZahlZiel ON Kunden.ZahlZielID = ZahlZiel.ID
 WHERE Kunden.AdrArtID = 1
   AND Kundenstatus.ID IN ($2$)
   AND KdGf.ID IN ($1$)
+  AND Kunden.SichtbarID IN ($SICHTBARIDS$)
 ORDER BY Kunden.KdNr;
