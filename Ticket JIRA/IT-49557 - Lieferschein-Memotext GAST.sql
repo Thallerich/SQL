744b8675032755b395627cd45c706994ac430e20
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-07-05 09:29:21 +0200

    IT-49557   - insert memo for LS

--- Ticket JIRA/IT-49557 - Lieferschein-Memotext GAST.sql
+++ Ticket JIRA/IT-49557 - Lieferschein-Memotext GAST.sql
@@ -1,4 +1,12 @@
-SELECT DISTINCT Kunden.KdNr, Kunden.SuchCode AS Kunde, KdGf.KurzBez AS Geschäftsbereich, Vsa.VsaNr, Vsa.Bez AS VsaBezeichnung, Expedition.SuchCode AS Expedition
+DECLARE @UserID int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST');
+
+DECLARE @Vsa TABLE (
+  KundenID int,
+  VsaID int
+);
+
+INSERT INTO @Vsa (KundenID, VsaID)
+SELECT DISTINCT Kunden.ID, Vsa.ID
 FROM Kunden
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 JOIN Vsa ON Vsa.KundenID = Kunden.ID
@@ -10,4 +18,12 @@ JOIN Standort AS Expedition ON Touren.ExpeditionID = Expedition.ID
 WHERE Kunden.AdrArtID = 1
   AND Kunden.Status = N'A'
   AND (Expedition.SuchCode LIKE N'UKL_' OR Expedition.SuchCode = N'GRAZ')
-  AND Bereich.Bereich = N'FW';
+  AND KdGf.KurzBez = N'GAST'
+  AND Bereich.Bereich = N'FW'
+  AND Vsa.ID != 6112460;
+
+INSERT INTO VsaTexte (KundenID, VsaID, TextArtID, Memo, VonDatum, BisDatum, Anlage_, Update_, AnlageUserID_, UserID_)
+SELECT v.KundenID, v.VsaID, VsaTexte.TextArtID, VsaTexte.Memo, VsaTexte.VonDatum, VsaTexte.BisDatum, GETDATE(), GETDATE(), @UserID, @UserID
+FROM VsaTexte
+CROSS JOIN @Vsa v
+WHERE VsaTexte.ID = 178138;
