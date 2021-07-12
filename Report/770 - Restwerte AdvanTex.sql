0831be1d78d865c9aaf2203f5f3cbb47b41a4ebd
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-07-12 14:58:32 +0200

    bugfix: calculate ErstWoche - table field no longer exists

--- Report/770 - Restwerte AdvanTex.sql
+++ Report/770 - Restwerte AdvanTex.sql
@@ -2,16 +2,17 @@ DECLARE @Woche char(7);
 
 SET @Woche = (SELECT Week.Woche FROM Week WHERE Week.vonDat < CONVERT(date, GETDATE()) AND Week.bisDat > CONVERT(date, GETDATE()));
 
-SELECT Traeger.Vorname, Traeger.Nachname, Abteil.Abteilung, Vsa.VsaNr, Vsa.Bez AS VSA, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, Artikel.EKPreis, Teile.Barcode, Teile.Eingang1, Teile.Ausgang1, Teile.Indienst, Teile.Erstwoche, IIF(Teile.Status IN ('Z', 'V', 'X', 'Y', 'U') OR (Teile.Einzug < CONVERT(date, GETDATE())), 0, IIF((Teile.AusDienst = '' OR Teile.AusDienst IS NULL), Teile.RestWertInfo, IIF(@Woche < Teile.AusDienst, Teile.RestWertInfo, Teile.AusDRestW))) AS RestWert, DATEDIFF(day, ISNULL(Teile.Ausgang1, CONVERT(date, GETDATE())), CONVERT(date, GETDATE())) AS BeimKundeSeitTagen
-FROM Teile, Traeger, Vsa, Kunden, Abteil, Artikel, ArtGroe
+SELECT Traeger.Vorname, Traeger.Nachname, Abteil.Abteilung, Vsa.VsaNr, Vsa.Bez AS VSA, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, Artikel.EKPreis, Teile.Barcode, Teile.Eingang1, Teile.Ausgang1, Teile.Indienst, Week.Woche AS Erstwoche, IIF(Teile.Status IN ('Z', 'V', 'X', 'Y', 'U') OR (Teile.Einzug < CONVERT(date, GETDATE())), 0, IIF((Teile.AusDienst = '' OR Teile.AusDienst IS NULL), Teile.RestWertInfo, IIF(@Woche < Teile.AusDienst, Teile.RestWertInfo, Teile.AusDRestW))) AS RestWert, DATEDIFF(day, ISNULL(Teile.Ausgang1, CONVERT(date, GETDATE())), CONVERT(date, GETDATE())) AS BeimKundeSeitTagen
+FROM Teile, Traeger, Vsa, Kunden, Abteil, Artikel, ArtGroe, Week
 WHERE Teile.TraegerID = Traeger.ID
   AND Traeger.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
   AND Traeger.AbteilID = Abteil.ID
   AND Teile.ArtikelID = Artikel.ID
   AND Teile.ArtGroeID = ArtGroe.ID
+  AND DATEADD(day, Teile.AnzTageImLager, Teile.ErstDatum) BETWEEN Week.VonDat AND Week.BisDat
   AND Kunden.ID = $ID$
   AND Artikel.Status <> 'B'
   AND (Teile.Ausdienst = '' OR Teile.Ausdienst IS NULL)
   AND Teile.Status <> '5'
-  AND Traeger.Altenheim = $FALSE$;
+  AND Traeger.Altenheim = 0;
--- Report/770a - Restwerte AdvanTex inkl. Einzug.sql
+++ Report/770a - Restwerte AdvanTex inkl. Einzug.sql
@@ -1,5 +1,3 @@
-DECLARE @Woche CHAR(7) = $1$;
-
 WITH Teilestatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
@@ -20,11 +18,10 @@ SELECT Traeger.Traeger AS TrägerNr,
   Teile.Eingang1,
   Teile.Ausgang1,
   Teile.Indienst,
-  Teile.Erstwoche,
-  IIF(Teile.Status IN ('Z', 'V', 'X', 'Y') OR (Teile.Einzug < CONVERT(DATE, GETDATE())), 0, IIF((Teile.AusDienst = '' OR Teile.AusDienst IS NULL), RWCalc.RestwertInfo, IIF(@Woche < Teile.AusDienst, RWCalc.RestWertInfo, Teile.AusDRestW))) AS RestWert,
-  DATEDIFF(day, ISNULL(Teile.Ausgang1, CONVERT(DATE, GETDATE())), CONVERT(DATE, GETDATE())) AS BeimKundeSeitTagen
+  Week.Woche AS Erstwoche,
+  IIF(Teile.Status IN (N'Z', N'V', N'X', N'Y') OR (Teile.Einzug < CONVERT(DATE, GETDATE())), 0, IIF((Teile.AusDienst = '' OR Teile.AusDienst IS NULL), RWCalc.RestwertInfo, IIF($1$ < Teile.AusDienst, RWCalc.RestWertInfo, Teile.AusDRestW))) AS RestWert, DATEDIFF(day, ISNULL(Teile.Ausgang1, CONVERT(DATE, GETDATE())), CONVERT(DATE, GETDATE())) AS BeimKundeSeitTagen
 FROM Teile
-CROSS APPLY funcGetRestwert(Teile.ID, @Woche, 1) AS RWCalc
+CROSS APPLY funcGetRestwert(Teile.ID, $1$, 1) AS RWCalc
 JOIN Traeger ON Teile.TraegerID = Traeger.ID
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -32,9 +29,10 @@ JOIN Abteil ON Traeger.AbteilID = Abteil.ID
 JOIN Artikel ON Teile.ArtikelID = Artikel.ID
 JOIN ArtGroe ON Teile.ArtGroeID = ArtGroe.ID
 JOIN Teilestatus ON Teile.Status = Teilestatus.Status
+JOIN Week ON DATEADD(day, Teile.AnzTageImLager, Teile.ErstDatum) BETWEEN Week.VonDat AND Week.BisDat
 WHERE Kunden.ID = $ID$
-  AND Artikel.Status <> 'B'
-  AND (Teile.Ausdienst = '' OR Teile.Ausdienst IS NULL)
-  AND Teile.Status <> '5'
-  AND Traeger.Altenheim = $FALSE$
+  AND Artikel.Status != N'B'
+  AND (Teile.Ausdienst = N'' OR Teile.Ausdienst IS NULL)
+  AND Teile.Status != N'5'
+  AND Traeger.Altenheim = 0
 ORDER BY TrägerNr, Nachname, ArtikelNr, Größe;
