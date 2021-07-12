63cf52cd21f980065bb0a1a5fdea3fd203794a88
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-07-12 14:45:59 +0200

    bugfix: temp table was auto-created with not null this error out the second part (insert into) changed to creating the temp table separtly

--- Report/734 - Teile verschrottet im Zeitraum - BK - Berufskleidung.sql
+++ Report/734 - Teile verschrottet im Zeitraum - BK - Berufskleidung.sql
@@ -1,7 +1,37 @@
+-- Pipeline 1
 DROP TABLE IF EXISTS #TmpErgebnis734;
 
-SELECT  WegGrund.WegGrundBez$LAN$ AS Grund, Teile.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, Teile.ErstDatum, Teile.PatchDatum, Teile.Ausdienst, Teile.AusdienstDat, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS Geschäftsbereich, Teile.AusdRestw AS Restwert, Teile.RuecklaufG AS [Anzahl Wäschen gesamt], Teile.RuecklaufK AS [Anzahl Wäschen Kunde], Teile.Kostenlos, Mitarbei.Name AS Mitarbeiter
-INTO #TmpErgebnis734
+--Pipeline 2
+CREATE TABLE #TmpErgebnis734 (
+  Grund nvarchar(60) COLLATE Latin1_General_CS_AS,
+  Barcode nvarchar(33) COLLATE Latin1_General_CS_AS,
+  [Status] nvarchar(40) COLLATE Latin1_General_CS_AS,
+  ErstWoche nchar(7) COLLATE Latin1_General_CS_AS,
+  ErstDatum date,
+  PatchDatum date,
+  Ausdienst nchar(7) COLLATE Latin1_General_CS_AS,
+  AusdienstDat date,
+  ArtikelNr nvarchar(15) COLLATE Latin1_General_CS_AS,
+  ArtikelNr2 nvarchar(15) COLLATE Latin1_General_CS_AS,
+  Artikelbezeichnung nvarchar(60) COLLATE Latin1_General_CS_AS,
+  EKPreis money,
+  VsaNr int,
+  [VSA-Stichwort] nvarchar(40) COLLATE Latin1_General_CS_AS,
+  [VSA-Bezeichnung] nvarchar(40) COLLATE Latin1_General_CS_AS,
+  Produktionsstandort nvarchar(40) COLLATE Latin1_General_CS_AS,
+  KdNr int,
+  Kunde nvarchar(20) COLLATE Latin1_General_CS_AS,
+  [Kundenservice-Standort] nvarchar(40) COLLATE Latin1_General_CS_AS,
+  Geschäftsbereich nvarchar(5) COLLATE Latin1_General_CS_AS,
+  Restwert money,
+  [Anzahl Wäschen gesamt] int,
+  [Anzahl Wäschen Kunde] int,
+  Kostenlos bit,
+  Mitarbeiter nvarchar(40) COLLATE Latin1_General_CS_AS
+);
+
+INSERT INTO #TmpErgebnis734 (Grund, Barcode, [Status], ErstWoche, ErstDatum, PatchDatum, Ausdienst, AusdienstDat, ArtikelNr, ArtikelNr2, Artikelbezeichnung, EKPreis, VsaNr, [VSA-Stichwort], [VSA-Bezeichnung], Produktionsstandort, KdNr, Kunde, [Kundenservice-Standort], Geschäftsbereich, Restwert, [Anzahl Wäschen gesamt], [Anzahl Wäschen Kunde], Kostenlos, Mitarbeiter)
+SELECT  WegGrund.WegGrundBez$LAN$ AS Grund, Teile.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, Teile.ErstDatum, Teile.PatchDatum, Teile.Ausdienst, Teile.AusdienstDat, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS Geschäftsbereich, Teile.AusdRestw AS Restwert, Teile.RuecklaufG AS [Anzahl Wäschen gesamt], Teile.RuecklaufK AS [Anzahl Wäschen Kunde], Teile.Kostenlos, Mitarbei.Name AS Mitarbeiter
 FROM Teile
 JOIN Vsa ON Teile.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -22,8 +52,8 @@ WHERE Kunden.KdGfID IN ($4$)
   AND Teile.AusDienstDat BETWEEN $STARTDATE$ AND $ENDDATE$
   AND Bereich.ID IN ($5$);
 
-INSERT INTO #TmpErgebnis734
-SELECT WegGrund.WegGrundBez$LAN$ AS Grund, TeileLag.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, TeileLag.ErstDatum, CONVERT(date, NULL) AS PatchDatum, TeileLag.AusDienst, TeileLag.AusdienstDat, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS Geschäftsbereich, TeileLag.Restwert AS Restwert, TeileLag.AnzWaschen AS [Anzahl Wäschen gesamt], NULL AS [Anzahl Wäschen Kunde], NULL AS Kostenlos, NULL AS Mitarbeiter
+INSERT INTO #TmpErgebnis734 (Grund, Barcode, [Status], ErstWoche, ErstDatum, PatchDatum, Ausdienst, AusdienstDat, ArtikelNr, ArtikelNr2, Artikelbezeichnung, EKPreis, VsaNr, [VSA-Stichwort], [VSA-Bezeichnung], Produktionsstandort, KdNr, Kunde, [Kundenservice-Standort], Geschäftsbereich, Restwert, [Anzahl Wäschen gesamt], [Anzahl Wäschen Kunde], Kostenlos, Mitarbeiter)
+SELECT WegGrund.WegGrundBez$LAN$ AS Grund, TeileLag.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, TeileLag.ErstDatum, CONVERT(date, NULL) AS PatchDatum, TeileLag.AusDienst, TeileLag.AusdienstDat, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS Geschäftsbereich, TeileLag.Restwert AS Restwert, TeileLag.AnzWaschen AS [Anzahl Wäschen gesamt], NULL AS [Anzahl Wäschen Kunde], NULL AS Kostenlos, NULL AS Mitarbeiter
 FROM TeileLag
 JOIN [Status] ON TeileLag.[Status] = [Status].[Status] AND [Status].Tabelle = 'TEILELAG'
 JOIN ArtGroe ON TeileLag.ArtGroeID = ArtGroe.ID
@@ -49,6 +79,7 @@ WHERE TeileLag.[Status] = N'Y'
     WHERE Ergebnis.Barcode = TeileLag.Barcode
   );
 
+-- Pipeline 3
 SELECT *
 FROM #TmpErgebnis734
 ORDER BY Kdnr, VsaNr, ArtikelNr;
