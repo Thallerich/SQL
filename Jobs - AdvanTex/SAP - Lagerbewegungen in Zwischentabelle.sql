b515afe701893e6b373f874cb900e29517174466
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-07-20 08:39:56 +0200

    latest version from advantex

--- Jobs - AdvanTex/SAP - Lagerbewegungen in Zwischentabelle.sql
+++ Jobs - AdvanTex/SAP - Lagerbewegungen in Zwischentabelle.sql
@@ -1,21 +1,16 @@
-DECLARE @MinLagerBewID INTEGER;
-DECLARE @MaxLagerBewID INTEGER;
-
-SET @MinLagerBewID = (SELECT CAST(ValueMemo AS INTEGER) + 1 FROM Settings WHERE Parameter = 'LAST_LAGERBEWID_TO_SAP');
-SET @MaxLagerBewID = (SELECT MAX(ID) FROM LagerBew);
+DECLARE @MinLagerBewID int = (SELECT CAST(ValueMemo AS int) FROM Settings WHERE Parameter = 'LAST_LAGERBEWID_TO_SAP');
 
 TRUNCATE TABLE __LagerBewHW;
 
-IF (@MaxLagerBewID >= @MinLagerBewID) BEGIN 
-  INSERT INTO __LagerBewHW (ID, Zeitpunkt, ArtikelNr, ArtikelBez, SuchCode, Differenz, Groesse, BestandID, LgBewCodBez)
-  SELECT LagerBew.ID, LagerBew.Zeitpunkt, Artikel.ArtikelNr, Artikel.ArtikelBez, Standort.SuchCode, LagerBew.Differenz, ArtGroe.Groesse, Bestand.ID BestandID, LgBewCodBez
-  FROM LagerBew, Bestand, ArtGroe, Artikel, LagerArt, Standort, LgBewCod
-  WHERE LagerBew.BestandID = Bestand.ID
-    AND Bestand.ArtGroeID = ArtGroe.ID
-    AND ArtGroe.ArtikelID = Artikel.ID
-    AND Bestand.LagerArtID = LagerArt.ID
-    AND LagerArt.LagerID = Standort.ID
-    AND LagerBew.LgBewCodID = LgBewCod.ID
-    AND Standort.SuchCode IN ('MATT','LEOG','SMS','ARNO','SCHI','GRAZ')
-    AND LagerBew.ID BETWEEN @MinLagerBewID AND @MaxLagerBewID;
-END;
+INSERT INTO __LagerBewHW (ID, Zeitpunkt, ArtikelNr, ArtikelBez, SuchCode, Differenz, Groesse, BestandID, LgBewCodBez)
+SELECT LagerBew.ID, LagerBew.Zeitpunkt, Artikel.ArtikelNr, Artikel.ArtikelBez, Standort.SuchCode, LagerBew.Differenz, ArtGroe.Groesse, Bestand.ID BestandID, LgBewCodBez
+FROM LagerBew, Bestand, ArtGroe, Artikel, LagerArt, Standort, LgBewCod
+WHERE LagerBew.BestandID = Bestand.ID
+  AND Bestand.ArtGroeID = ArtGroe.ID
+  AND ArtGroe.ArtikelID = Artikel.ID
+  AND Bestand.LagerArtID = LagerArt.ID
+  AND LagerArt.LagerID = Standort.ID
+  AND LagerBew.LgBewCodID = LgBewCod.ID
+  AND Standort.FirmaID = 5260
+  AND Standort.SuchCode != N'SMZL'
+  AND LagerBew.ID > @MinLagerBewID;
