5c92e046b8f3cddb790cde83e0d21b868b201129
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-07-30 08:19:12 +0200

    initial version

---
+++ Diverse/Preiserhöhung Juli 2021 - Preislisten.sql
@@ -0,0 +1,36 @@
+WITH PrListPE AS (
+  SELECT DISTINCT Kunden.PrListKundenID
+  FROM Kunden
+  WHERE Kunden.FirmaID = 5260
+    AND Kunden.PrListKundenID > 0
+    AND EXISTS (
+      SELECT PePo.*
+      FROM PePo
+      JOIN PeKo ON PePo.PeKoID = PeKo.ID
+      JOIN Vertrag ON PePo.VertragID = Vertrag.ID
+      WHERE Vertrag.KundenID = Kunden.ID
+        AND PeKo.Status = N'C'
+    )
+),
+PrListABCKunden AS (
+  SELECT Kunden.PrListKundenID, ABC.ABC, COUNT(Kunden.ID) AS Anzahl
+  FROM Kunden
+  JOIN ABC ON Kunden.AbcID = ABC.ID
+  WHERE Kunden.PrListKundenID > 0
+    AND Kunden.Status = N'A'
+  GROUP BY Kunden.PrListKundenID, ABC.ABC
+)
+SELECT [Preislisten-Nummer], Preisliste, [Preislisten-Bezeichnung], [1] AS [A-Kunden], [2] AS [B-Kunden], [3] AS [C-Kunden]
+FROM (
+  SELECT Kunden.KdNr AS [Preislisten-Nummer], Kunden.Name1 AS [Preisliste], Kunden.Name2 AS [Preislisten-Bezeichnung], PrListABCKunden.ABC, PrListABCKunden.Anzahl
+  FROM Kunden
+  JOIN PrListABCKunden ON PrListABCKunden.PrListKundenID = Kunden.ID
+  WHERE Kunden.ID IN (
+    SELECT PrListPe.PrListKundenID
+    FROM PrListPe
+  )
+) PivotData
+PIVOT (
+  SUM(Anzahl) FOR ABC IN ([1], [2], [3])
+) AS PivotResult
+ORDER BY [Preislisten-Nummer] ASC;
