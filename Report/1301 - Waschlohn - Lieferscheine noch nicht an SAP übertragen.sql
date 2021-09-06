5a6f2662f4b2faa8a8b10f329fb4ed852fb9af5b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-09-06 11:06:58 +0200

    refined

--- Report/1301 - Waschlohn - Lieferscheine noch nicht an SAP übertragen.sql
+++ Report/1301 - Waschlohn - Lieferscheine noch nicht an SAP übertragen.sql
@@ -1,11 +1,12 @@
-SELECT Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, LsKo.Datum AS Lieferdatum, LsKo.LsNr, LsKo.InternKalkFix AS [Waschlohn fixiert?], LsKo.SentToSAP AS [an SAP gesendet?], LsKo.ID AS LsKoID
+SELECT Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, LsKo.Datum AS Lieferdatum, LsKo.LsNr, LsKo.InternKalkFix AS [Waschlohn fixiert?], LsKo.SentToSAP AS [an SAP gesendet?], LsKo.ID AS LsKoID, LsKo.Anlage_, Mitarbei.Name AS AnlageUser_
 FROM LsKo
 JOIN Vsa ON LsKo.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
+JOIN Mitarbei ON LsKo.AnlageUserID_ = Mitarbei.ID
 WHERE LsKo.Status >= N'Q'
-  AND KdGf.KurzBez IN (N'MED', N'GAST', N'JOB', N'SAEU')
+  AND ((Firma.SuchCode = N'FA14' AND KdGf.KurzBez IN (N'MED', N'GAST', N'JOB', N'SAEU')) OR (Firma.SuchCode != N'FA14'))
   AND Kunden.FirmaID IN ($2$)
   AND LsKo.SentToSAP = 0
   AND LsKo.Datum BETWEEN $STARTDATE$ AND $ENDDATE$
