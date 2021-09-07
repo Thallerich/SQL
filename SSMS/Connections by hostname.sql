712e7f9e30abf8e7189fadd84e69337a0773cf11
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-09-07 14:31:48 +0200

    initial version

---
+++ SSMS/Connections by hostname.sql
@@ -0,0 +1,5 @@
+SELECT conn.connect_time, conn.client_net_address, sess.host_name, sess.login_name, sess.[status], sess.cpu_time, sqltext.text AS [last_sql]
+FROM sys.dm_exec_connections AS conn
+JOIN sys.dm_exec_sessions AS sess ON conn.session_id = sess.session_id
+CROSS APPLY sys.dm_exec_sql_text(conn.most_recent_sql_handle) AS sqltext
+WHERE sess.host_name LIKE N'sns%'
