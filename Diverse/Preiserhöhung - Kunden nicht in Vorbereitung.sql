7962387bd2db224001755173fe36b1283509f899
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-09-17 09:38:54 +0200

    stuff for current PE

---
+++ Diverse/Preiserhöhung - Kunden nicht in Vorbereitung.sql
@@ -0,0 +1,17 @@
+SELECT DISTINCT Kunden.KdNr, Kunden.SuchCode AS Kunde, ABC.ABCBez AS [ABC-Klasse], [Zone].ZonenCode AS Vertriebszone, KdGf.KurzBez AS [Geschäftsbereich], PrLauf.PrLaufBez AS Preiserhöhungslauf, CAST(IIF(Kunden.PrListKundenID > 0, 1, 0) AS bit) AS [über Preisliste?]
+FROM Kunden
+JOIN ABC ON Kunden.ABCID = ABC.ID
+JOIN [Zone] ON Kunden.ZoneID = [Zone].ID
+JOIN KdGf ON Kunden.KdGFID = KdGf.ID
+JOIN Vertrag ON Vertrag.KundenID = Kunden.ID
+JOIN PrLauf ON Vertrag.PrLaufID = PrLauf.ID
+WHERE Kunden.FirmaID = 5260
+  AND Kunden.Status = N'A'
+  AND Kunden.AdrArtID = 1
+  AND Kunden.ID NOT IN (
+    SELECT Vertrag.KundenID
+    FROM Vertrag
+    JOIN PePo ON PePo.VertragID = Vertrag.ID
+    JOIN PeKo ON PePo.PeKoID = PeKo.ID
+    WHERE PeKo.Status = N'C'
+  );
--- Diverse/Preiserhöhung Juli 2021 - Preislisten.sql
+++ Diverse/Preiserhöhung Juli 2021 - Preislisten.sql
@@ -20,11 +20,13 @@ PrListABCKunden AS (
     AND Kunden.Status = N'A'
   GROUP BY Kunden.PrListKundenID, ABC.ABC
 )
-SELECT [Preislisten-Nummer], Preisliste, [Preislisten-Bezeichnung], [1] AS [A-Kunden], [2] AS [B-Kunden], [3] AS [C-Kunden]
+SELECT [Preislisten-Nummer], Preisliste, [Preislisten-Bezeichnung], Preiserhöhungslauf, [Letzte Preiserhöhung], [1] AS [A-Kunden], [2] AS [B-Kunden], [3] AS [C-Kunden]
 FROM (
-  SELECT Kunden.KdNr AS [Preislisten-Nummer], Kunden.Name1 AS [Preisliste], Kunden.Name2 AS [Preislisten-Bezeichnung], PrListABCKunden.ABC, PrListABCKunden.Anzahl
+  SELECT DISTINCT Kunden.KdNr AS [Preislisten-Nummer], Kunden.Name1 AS [Preisliste], Kunden.Name2 AS [Preislisten-Bezeichnung], PrLauf.PrLaufBez AS [Preiserhöhungslauf], Vertrag.LetztePeDatum AS [Letzte Preiserhöhung], PrListABCKunden.ABC, PrListABCKunden.Anzahl
   FROM Kunden
   JOIN PrListABCKunden ON PrListABCKunden.PrListKundenID = Kunden.ID
+  JOIN Vertrag ON Vertrag.KundenID = Kunden.ID
+  JOIN PrLauf ON Vertrag.PrLaufID = PrLauf.ID
   WHERE Kunden.ID IN (
     SELECT PrListPe.PrListKundenID
     FROM PrListPe
@@ -34,3 +36,34 @@ PIVOT (
   SUM(Anzahl) FOR ABC IN ([1], [2], [3])
 ) AS PivotResult
 ORDER BY [Preislisten-Nummer] ASC;
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+WITH PrListPE AS (
+  SELECT DISTINCT Kunden.PrListKundenID
+  FROM Kunden
+  WHERE Kunden.FirmaID = 5260
+    AND Kunden.PrListKundenID > 0
+    AND EXISTS (
+      SELECT PePo.*
+      FROM PePo
+      JOIN PeKo ON PePo.PeKoID = PeKo.ID
+      JOIN Vertrag ON PePo.VertragID = Vertrag.ID
+      WHERE Vertrag.KundenID = Kunden.ID
+        AND PeKo.Status = N'C'
+    )
+)
+SELECT DISTINCT Kunden.KdNr AS [Preislisten-Nummer], Kunden.Name1 AS [Preisliste], Kunden.Name2 AS [Preislisten-Bezeichnung], PrLauf.PrLaufBez AS [Preiserhöhungslauf Preisliste], KundePrList.KdNr, KundePrList.SuchCode AS Kunde, ABC.ABCBez AS [ABC-Klasse], KdPrLauf.PrLaufBez AS [Preiserhöhungslauf Kunde]
+FROM Kunden
+JOIN Vertrag ON Vertrag.KundenID = Kunden.ID
+JOIN PrLauf ON Vertrag.PrLaufID = PrLauf.ID
+JOIN Kunden AS KundePrList ON KundePrList.PrListKundenID = Kunden.ID
+JOIN ABC ON KundePrList.AbcID = ABC.ID
+JOIN Vertrag AS VertragPrList ON VertragPrList.KundenID = KundePrList.ID
+JOIN PrLauf AS KdPrLauf ON VertragPrList.PrLaufID = KdPrLauf.ID
+WHERE Kunden.ID IN (
+  SELECT PrListPe.PrListKundenID
+  FROM PrListPe
+)
+ORDER BY [Preislisten-Nummer], KdNr ASC;
