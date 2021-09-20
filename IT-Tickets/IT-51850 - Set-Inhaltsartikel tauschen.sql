c40fa33b388d63cf6ed364331d0e60958b656648
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-09-20 15:11:32 +0200

    IT-51850   - articles changed as per excel list

---
+++ IT-Tickets/IT-51850 - Set-Inhaltsartikel tauschen.sql
@@ -0,0 +1,30 @@
+WITH SetArtiChange AS (
+  SELECT DISTINCT Setartikel.ID AS SetartikelID, InhaltAktuell.ID AS InhaltAktuellID, InhaltNeu.ID AS InhaltNeuID, ErsatzNeu.ID AS ErsatzNeuID
+  FROM Salesianer.dbo.__OPSetArtiChange
+  JOIN Artikel AS Setartikel ON __OPSetArtiChange.Setartikel = Setartikel.ArtikelNr
+  JOIN Artikel AS InhaltAktuell ON __OPSetArtiChange.InhaltAktuell = InhaltAktuell.ArtikelNr
+  JOIN Artikel AS InhaltNeu ON __OPSetArtiChange.InhaltNeu = InhaltNeu.ArtikelNr
+  JOIN Artikel AS ErsatzNeu ON __OPSetArtiChange.ErsatzNeu = ErsatzNeu.ArtikelNr
+)
+UPDATE OPSets SET Artikel1ID = SetArtiChange.InhaltNeuID, Artikel3ID = SetArtiChange.ErsatzNeuID
+FROM OPSets
+JOIN SetArtiChange ON OPSets.ArtikelID = SetArtiChange.SetartikelID AND OPSets.Artikel1ID = SetArtiChange.InhaltAktuellID
+WHERE OPSets.Artikel2ID > 0
+  AND OPSets.Artikel2ID != SetArtiChange.ErsatzNeuID;
+
+GO
+
+WITH SetArtiChange AS (
+  SELECT DISTINCT Setartikel.ID AS SetartikelID, InhaltAktuell.ID AS InhaltAktuellID, InhaltNeu.ID AS InhaltNeuID, ErsatzNeu.ID AS ErsatzNeuID
+  FROM Salesianer.dbo.__OPSetArtiChange
+  JOIN Artikel AS Setartikel ON __OPSetArtiChange.Setartikel = Setartikel.ArtikelNr
+  JOIN Artikel AS InhaltAktuell ON __OPSetArtiChange.InhaltAktuell = InhaltAktuell.ArtikelNr
+  JOIN Artikel AS InhaltNeu ON __OPSetArtiChange.InhaltNeu = InhaltNeu.ArtikelNr
+  JOIN Artikel AS ErsatzNeu ON __OPSetArtiChange.ErsatzNeu = ErsatzNeu.ArtikelNr
+)
+UPDATE OPSets SET Artikel1ID = SetArtiChange.InhaltNeuID, Artikel2ID = SetArtiChange.ErsatzNeuID
+FROM OPSets
+JOIN SetArtiChange ON OPSets.ArtikelID = SetArtiChange.SetartikelID AND OPSets.Artikel1ID = SetArtiChange.InhaltAktuellID
+WHERE OPSets.Artikel2ID < 0;
+
+GO
