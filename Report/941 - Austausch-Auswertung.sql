2387016c4d1c60235a6530d3e105442d6cdf361b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-09-29 09:49:45 +0200

    IT-47756 - bugfix + change   - fixed wrong assignment of weggrund   - added column for amount repaired

--- Report/941 - Austausch-Auswertung.sql
+++ Report/941 - Austausch-Auswertung.sql
@@ -26,18 +26,28 @@ WegTeileSumme AS (
   WHERE Teile.AusdienstGrund IN (N'A', N'a', N'B', N'b', N'C', N'c')
     AND Teile.AusdienstDat BETWEEN @DatumVon AND @DatumBis
   GROUP BY Teile.ArtikelID, Vsa.KundenID, FORMAT(Teile.AusdienstDat, N'yyyy-MM', N'de-AT')
+),
+TeileRep AS (
+  SELECT Teile.ArtikelID, Vsa.KundenID, CAST(SUM(TeilSoFa.Menge) AS int) AS AnzReparatur, FORMAT(TeilSoFa.Zeitpunkt, N'yyyy-MM', N'de-AT') AS Monat
+  FROM TeilSoFa
+  JOIN Artikel ON TeilSoFa.ArtikelID = Artikel.ID
+  JOIN Teile ON TeilSoFa.TeileID = Teile.ID
+  JOIN Vsa ON Teile.VsaID = Vsa.ID
+  WHERE CAST(TeilSoFa.Zeitpunkt AS date) BETWEEN @DatumVon AND @DatumBis
+    AND Artikel.ArtiTypeID = 5  --Reparatur-Artikel
+  GROUP BY Teile.ArtikelID, Vsa.KundenID, FORMAT(TeilSoFa.Zeitpunkt, N'yyyy-MM', N'de-AT')
 )
-SELECT [Monat], [KdNr], [Kunde], [Haupstandort Kunde], [ArtikelNr], [Artikelbezeichnung], [Umlaufmenge aktuell], [Liefermenge im Zeitraum], [AORW] AS [Austausch ohne RW-Berechnung], [AMRW] AS [Austausch mit RW-Berechnung], [BORW] AS [Größentausch ohne RW-Berechnung], [BMRW] AS [Größentausch mit RW-Berechnung], [CORW] AS [Artikeltausch ohne RW-Berechnung], [CMRW] AS [Artikeltausch mit RW-Berechnung], [MengeGesamt] AS [Anzahl gesamt / Kunde], [Restwert] AS [Restwert Austausch], [RestwertFakt] AS [Restwert verrechnet], [Differenz]
+SELECT [Monat], [KdNr], [Kunde], [Haupstandort Kunde], [ArtikelNr], [Artikelbezeichnung], [Umlaufmenge aktuell], [Liefermenge im Zeitraum], [AORW] AS [Austausch ohne RW-Berechnung], [AMRW] AS [Austausch mit RW-Berechnung], [BORW] AS [Größentausch ohne RW-Berechnung], [BMRW] AS [Größentausch mit RW-Berechnung], [CORW] AS [Artikeltausch ohne RW-Berechnung], [CMRW] AS [Artikeltausch mit RW-Berechnung], [MengeGesamt] AS [Anzahl gesamt / Kunde], [Restwert] AS [Restwert Austausch], [RestwertFakt] AS [Restwert verrechnet], [Differenz], [AnzReparatur]
 FROM (
   SELECT COALESCE(Liefermenge.Monat, WegTeile.Monat, WegTeileSumme.Monat) AS Monat, Kunden.KdNr, Kunden.SuchCode AS Kunde, Standort.SuchCode AS [Haupstandort Kunde], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArtiSum.Umlauf AS [Umlaufmenge aktuell], SUM(ISNULL(Liefermenge.Menge, 0)) AS [Liefermenge im Zeitraum], SUM(ISNULL(WegTeile.Menge, 0)) AS MengeWeg, GrundKurz = 
     CASE WegTeile.GrundKurz
-      WHEN N'A' THEN N'AORW'
-      WHEN N'a' THEN N'AMRW'
-      WHEN N'B' THEN N'BORW'
-      WHEN N'b' THEN N'BMRW'
-      WHEN N'C' THEN N'CORW'
-      WHEN N'c' THEN N'CMRW'
-    END, WegTeileSumme.Menge AS MengeGesamt, WegTeileSumme.Restwert, WegTeileSumme.RestwertFakt, WegTeileSumme.Restwert - WegTeileSumme.RestwertFakt AS Differenz
+      WHEN N'A' THEN N'AMRW'
+      WHEN N'a' THEN N'AORW'
+      WHEN N'B' THEN N'BMRW'
+      WHEN N'b' THEN N'BORW'
+      WHEN N'C' THEN N'CMRW'
+      WHEN N'c' THEN N'CORW'
+    END, WegTeileSumme.Menge AS MengeGesamt, WegTeileSumme.Restwert, WegTeileSumme.RestwertFakt, WegTeileSumme.Restwert - WegTeileSumme.RestwertFakt AS Differenz, SUM(ISNULL(TeileRep.AnzReparatur, 0)) AS AnzReparatur
   FROM WegTeile
   JOIN (
     SELECT KdArti.ArtikelID, KdArti.KundenID, SUM(KdArti.Umlauf) AS Umlauf
@@ -49,6 +59,7 @@ FROM (
   JOIN Standort ON Kunden.StandortID = Standort.ID
   JOIN WegTeileSumme ON WegTeileSumme.ArtikelID = KdArtiSum.ArtikelID AND WegTeileSumme.KundenID = KdArtiSum.KundenID AND WegTeileSumme.Monat = WegTeile.Monat
   LEFT JOIN Liefermenge ON Liefermenge.ArtikelID = KdArtiSum.ArtikelID AND Liefermenge.KundenID = KdArtiSum.KundenID AND Liefermenge.Monat = WegTeile.Monat
+  LEFT JOIN TeileRep ON WegTeile.ArtikelID = TeileRep.ArtikelID AND WegTeile.KundenID = TeileRep.KundenID AND WegTeile.Monat = TeileRep.Monat
   WHERE Kunden.KdGFID IN ($3$)
     AND Kunden.FirmaID IN ($2$)
     AND Kunden.StandortID IN ($4$)
