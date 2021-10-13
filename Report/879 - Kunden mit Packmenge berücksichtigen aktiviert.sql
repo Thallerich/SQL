6e06987fc4571a303f772dc4f49c2ea6b9c2c4d9
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-10-13 14:38:59 +0200

    IT-52609   - added column "Anforderungsart"

--- Report/879 - Kunden mit Packmenge berücksichtigen aktiviert.sql
+++ Report/879 - Kunden mit Packmenge berücksichtigen aktiviert.sql
@@ -1,9 +1,11 @@
-SELECT Firma.SuchCode AS Firma, Holding.Bez AS Holding, Branche.BrancheBez$LAN$ AS Branche, [Zone].ZonenCode AS Vertriebszone, Kunden.KdNr, Kunden.SuchCode AS Kunde
-FROM Kunden
+SELECT DISTINCT Firma.SuchCode AS Firma, Holding.Bez AS Holding, Branche.BrancheBez$LAN$ AS Branche, [Zone].ZonenCode AS Vertriebszone, Kunden.KdNr, Kunden.SuchCode AS Kunde, AnfArt.AnfArtBez AS Anforderungsart
+FROM Vsa
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
 JOIN Holding ON Kunden.HoldingID = Holding.ID
 JOIN Branche ON Kunden.BrancheID = Branche.ID
 JOIN [Zone] ON Kunden.ZoneID = [Zone].ID
+JOIN AnfArt ON Vsa.AnfArtID = AnfArt.ID
 WHERE Kunden.Status = N'A'
   AND Kunden.AdrArtID = 1
   AND Kunden.FirmaID IN ($1$)
