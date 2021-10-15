f83384dbfa25c9b56811f78dc64409dd0938791d
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-10-15 10:49:40 +0200

    Release 9.30 prep

--- AdvanTex-Release/dbsystem-Mandant übernehmen.sql
+++ AdvanTex-Release/dbsystem-Mandant übernehmen.sql
@@ -1,4 +1,5 @@
 INSERT INTO dbSystem.dbo.Mandant (ID, Bez, ConnectPath, TestMandant, Verfuegbar, Anlage_, FertigEingespielt, DataPath, SQLConvertDateFormat, SQLConvertTimeFormat, AnlageUser_, User_)
-SELECT ID, Bez, ConnectPath, TestMandant, Verfuegbar, Anlage_, FertigEingespielt, DataPath, SQLConvertDateFormat, SQLConvertTimeFormat, N'THALST' AS AnlageUser_, N'THALST' AS User_
-FROM dbsystem_orig.dbo.Mandant;
+SELECT ID, Bez, ConnectPath, TestMandant, Verfuegbar, Anlage_, FertigEingespielt, DataPath, SQLConvertDateFormat, SQLConvertTimeFormat, AnlageUser_, User_
+FROM dbSystem_old.dbo.Mandant;
+
 GO
