f21132305193892007702650b86f19db4fcfd8f6
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-10-21 15:14:34 +0200

    IT-53069   - KdArti.LeasingPreis -> KdArti.LeasPreis

--- Report/891 - Vorläufige Kundenartikel.sql
+++ Report/891 - Vorläufige Kundenartikel.sql
@@ -1,4 +1,4 @@
-SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.Suchcode AS Kunde, Kunden.Name1 AS Adresszeile1, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS Variantenbezeichnu7ng, KdArti.WaschPreis AS Bearbeitungspreis, KdArti.LeasingPreis AS Mietpreis, Kundenservice.Name AS [Kundenservice-Mitarbeiter], KdArti.ID AS KdArtiID
+SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.Suchcode AS Kunde, Kunden.Name1 AS Adresszeile1, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS Variantenbezeichnung, KdArti.WaschPreis AS Bearbeitungspreis, KdArti.LeasPreis AS Mietpreis, Kundenservice.Name AS [Kundenservice-Mitarbeiter], KdArti.ID AS KdArtiID
 FROM KdArti
 JOIN Kunden ON KdArti.KundenID = Kunden.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
@@ -12,4 +12,5 @@ WHERE KdArti.Vorlaeufig = 1
   AND Kunden.AdrArtID = 1
   AND KdArti.Status != N'I'
   AND Kunden.Status != N'I'
+  AND Kunden.SichtbarID IN ($SICHTBARIDS$)
 ORDER BY Kunden.KdNr, Artikel.ArtikelNr, KdArti.Variante;
--- Report/891a - Vorläufige Bewohner-Anteile.sql
+++ Report/891a - Vorläufige Bewohner-Anteile.sql
@@ -1,4 +1,4 @@
-SELECT Kunden.KdNr, Kunden.Suchcode, Kunden.Name1, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ Artikelbezeichnung, KdArti.Variante, KdArti.WaschPreis, KdArti.LeasingPreis, KdArti.Vorlaeufig AS [Kundenartikel vorläufig?], BewAbr.Bez AS [Bewohner-Abrechnungsschema], ServiceMa.Name AS [Kundenservice-Mitarbeiter], KdArti.ID AS KdArtiID
+SELECT Kunden.KdNr, Kunden.Suchcode, Kunden.Name1, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ Artikelbezeichnung, KdArti.Variante, KdArti.WaschPreis AS Bearbeitungspreis, KdArti.LeasPreis AS Mietpreis, KdArti.Vorlaeufig AS [Kundenartikel vorläufig?], BewAbr.Bez AS [Bewohner-Abrechnungsschema], ServiceMa.Name AS [Kundenservice-Mitarbeiter], KdArti.ID AS KdArtiID
 FROM Kunden, Artikel, KdArti, KdBer, Mitarbei AS ServiceMA, BewKdAr, BewAbr
 WHERE BewKdAr.Vorlaeufig = 1
   AND KdArti.ArtikelID = Artikel.ID 
@@ -8,7 +8,7 @@ WHERE BewKdAr.Vorlaeufig = 1
   AND BewKdAr.KdArtiID = KdArti.ID
   AND BewKdAr.BewAbrID = BewAbr.ID
   AND Kunden.AdrArtID = 1
-  AND KdArti.Status <> N'I'
-  AND Kunden.Status <> N'I'
+  AND KdArti.Status != N'I'
+  AND Kunden.Status != N'I'
   AND Kunden.SichtbarID IN ($SICHTBARIDS$)
 ORDER BY Kunden.KdNr, Artikel.ArtikelNr, KdArti.Variante;
