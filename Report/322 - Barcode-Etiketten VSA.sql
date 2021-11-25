af11b211a845d5511e37cfc974e45e9bf766ab1e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-11-25 15:56:34 +0100

    changes for INZ  - use parameterized query  - build barcode if not present  - check if vsa-barcode is a number

---
+++ Report/322 - Barcode-Etiketten VSA.sql
@@ -0,0 +1,16 @@
+DECLARE @vsaid int = $ID$;
+
+DECLARE @sqltext nvarchar(max);
+
+SET @sqltext = N'SELECT Kunden.KdNr, Vsa.ID AS VsaID, Vsa.SuchCode, Kunden.Name1, Vsa.Bez, IIF(TRY_PARSE(Vsa.BarcodeNr AS bigint) IS NOT NULL, SUBSTRING(Vsa.BarcodeNr, 2, LEN(Vsa.BarcodeNr)), NULL) AS VsaBarcode, ISNULL(Artikel.BarcodeNr, Artikel.ArtikelNr + N''-'' + ArtGroe.Groesse) AS Artikelbarcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbez
+FROM VsaAnf
+JOIN Vsa ON VsaAnf.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdArti ON VsaAnf.KdArtiID = KdArti.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN ArtGroe ON VsaAnf.ArtGroeID = ArtGroe.ID
+WHERE VsaAnf.MitInventur = 1
+  AND Vsa.ID = @vsaid
+ORDER BY VsaID, ArtikelNr;';
+
+EXEC sp_executesql @sqltext, N'@vsaid int', @vsaid;
