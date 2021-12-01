70c4dcc12d847079449707827036693248c00030
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-12-01 16:06:33 +0100

    added size, removed sunday

--- Report/341a - Fixmengen je Kunde.sql
+++ Report/341a - Fixmengen je Kunde.sql
@@ -1,5 +1,6 @@
-SELECT  Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, VsaAnf.Liefern1 AS [Liefermenge Montag], VsaAnf.Liefern2 AS [Liefermenge Dienstag], VsaAnf.Liefern3 AS [Liefermenge Mittwoch], VsaAnf.Liefern4 AS [Liefermenge Donnerstag], VsaAnf.Liefern5 AS [Liefermenge Freitag], VsaAnf.Liefern6 AS [Liefermenge Samstag], VsaAnf.Liefern7 AS [Liefermenge Sonntag], VsaAnf.NormMenge AS [Norm-Liefermenge], VsaAnf.SollPuffer, VsaAnf.Durchschnitt, VsaAnf.IstDatum AS [letzte Inventur], Kunden.ID AS KundenID
+SELECT  Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, VsaAnf.Liefern1 AS [Liefermenge Montag], VsaAnf.Liefern2 AS [Liefermenge Dienstag], VsaAnf.Liefern3 AS [Liefermenge Mittwoch], VsaAnf.Liefern4 AS [Liefermenge Donnerstag], VsaAnf.Liefern5 AS [Liefermenge Freitag], VsaAnf.Liefern6 AS [Liefermenge Samstag], VsaAnf.NormMenge AS [Norm-Liefermenge], VsaAnf.SollPuffer, VsaAnf.Durchschnitt, VsaAnf.IstDatum AS [letzte Inventur], Kunden.ID AS KundenID
 FROM VsaAnf
+JOIN ArtGroe ON VsaAnf.ArtGroeID = ArtGroe.ID
 JOIN KdArti ON VsaAnf.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN Vsa ON VsaAnf.VsaID = Vsa.ID
