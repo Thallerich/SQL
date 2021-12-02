b68ecdb9671dcb9e83a3b5c711b76a040908ef14
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-12-02 15:20:50 +0100

    bugfix removed expedition location- doesn't make sense in this context

--- Report/740 - Rückgabeliste (SGF).sql
+++ Report/740 - Rückgabeliste (SGF).sql
@@ -12,23 +12,61 @@ Kundenstatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'KUNDEN')
+),
+TeileStatus AS ( 
+SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'TEILE')
 )
-SELECT KdGf.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenstatus.StatusBez AS Kundenstatus, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], VsaStatus.StatusBez AS [VSA-Status], Traeger.Nachname, Traeger.Vorname, Traeger.Titel, Traegerstatus.StatusBez AS Trägerstatus, IIF(Traeger.BerufsgrKdArtiID < 0, NULL, Berufsgruppe.VariantBez) AS Berufsgruppe, Traeger.SchrankInfo, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Teile.Barcode, Teile.Abmeldung, IIF(Teile.AusDienst='' OR Teile.AusDienst IS NULL, Teile.RestWertInfo, Teile.AusDRestW) AS Restwert
-FROM Teile, Traeger, ArtGroe, Artikel, Vsa, Kunden, KdGf, Traegerstatus, VsaStatus, Kundenstatus, KdArti AS Berufsgruppe
-WHERE Teile.TraegerID = Traeger.ID
-  AND Teile.ArtGroeID = ArtGroe.ID
-  AND Teile.ArtikelID = Artikel.ID
-  AND Teile.VsaID = Vsa.ID
-  AND Vsa.KundenID = Kunden.ID
-  AND Kunden.KdGfID = KdGf.ID
-  AND Traeger.Status = Traegerstatus.Status
-  AND Vsa.Status = VsaStatus.Status
-  AND Kunden.Status = Kundenstatus.Status
-  AND Traeger.BerufsgrKdArtiID = Berufsgruppe.ID
-  AND KdGf.ID IN ($1$)  -- Geschäftsfeld
-  AND Kunden.ID IN ($2$) -- Kunden abhängig von Geschäftsfeld
-  AND Teile.Status = 'W'  -- Rückgabe-Teile
-  AND Teile.AbmeldDat BETWEEN $4$ AND $5$ -- Datum der Abmeldung
-  AND Teile.Einzug IS NULL -- noch nicht in Produktion eingelesen
-  AND Artikel.BereichID IN ($3$)
-ORDER BY SGF, KdNr, VsaNr, Traeger.Nachname, Artikel.ArtikelNr, Größe;
+SELECT KdGf.KurzBez AS SGF,
+  Kunden.KdNr,
+  Kunden.SuchCode AS Kunde,
+  Kundenstatus.StatusBez AS Kundenstatus,
+  Vsa.VsaNr,
+  Vsa.SuchCode AS [VSA-Stichwort],
+  Vsa.Bez AS [VSA-Bezeichnung],
+  VsaStatus.StatusBez AS [VSA-Status],
+  Vsa.Name1,
+  Vsa.Name2,
+  Vsa.GebaeudeBez,
+  Abteil.Abteilung,
+  Abteil.Bez as Stammkostenstelle,
+  Traeger.Traeger AS TrägerNr,
+  Traeger.Nachname,
+  Traeger.Vorname,
+  Traeger.Titel,
+  Traegerstatus.StatusBez AS Trägerstatus,
+  IIF(Traeger.BerufsgrKdArtiID < 0, NULL, Berufsgruppe.VariantBez) AS Berufsgruppe,
+  Traeger.SchrankInfo,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung,
+  ArtGroe.Groesse AS Größe,
+  Teile.Barcode,
+  TeileStatus.StatusBez AS [Status Teil],
+  Teile.Abmeldung,
+  IIF(Teile.AusDienst = N'' OR Teile.AusDienst IS NULL, Teile.RestWertInfo, Teile.AusDRestW) AS Restwert,
+  Teile.Alterinfo AS [Alter in Wochen],
+  Teile.Indienst,
+  Einsatz.EinsatzBez$LAN$ AS [Außerdienststellungs-Grund],
+  WegGrund.WegGrundBez$LAN$ AS [Schrott-Grund]
+FROM Teile
+JOIN Traeger on Teile.TraegerID = Traeger.ID
+JOIN ArtGroe on Teile.ArtGroeID = ArtGroe.ID
+JOIN Artikel on Teile.ArtikelID = Artikel.ID
+JOIN Vsa on Teile.VsaID = Vsa.ID
+JOIN Kunden on Vsa.KundenID = Kunden.ID
+JOIN KdGf on Kunden.KdGfID = KdGf.ID
+JOIN Traegerstatus on Traeger.Status = Traegerstatus.Status
+JOIN VsaStatus on  Vsa.Status = VsaStatus.Status
+JOIN Kundenstatus on Kunden.Status = Kundenstatus.Status 
+JOIN TeileStatus on Teile.Status = Teilestatus.status
+JOIN Abteil ON Vsa.AbteilID = Abteil.ID
+JOIN KdArti AS Berufsgruppe ON Traeger.BerufsgrKdArtiID = Berufsgruppe.ID
+JOIN WegGrund on Teile.WegGrundID = WegGrund.ID
+LEFT JOIN Einsatz ON  Teile.AusdienstGrund = Einsatz.EinsatzGrund
+WHERE Kunden.ID IN ($4$)
+  AND Teile.Status = N'W'
+  AND Teile.AbmeldDat BETWEEN $STARTDATE$ AND $ENDDATE$
+  AND Teile.Einzug IS NULL
+  AND Artikel.BereichID IN ($5$)
+ORDER BY SGF, KdNr, VsaNr, Traeger.Nachname, Artikel.ArtikelNr, Größe, [Status Teil];
