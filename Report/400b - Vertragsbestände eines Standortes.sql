512c539bbcea21c9f48ea4ca8bd3acacd4912237
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-12-10 07:53:28 +0100

    fixed changes done by julia

--- Report/400b - Vertragsbestände eines Standortes.sql
+++ Report/400b - Vertragsbestände eines Standortes.sql
@@ -1,18 +1,21 @@
 WITH VsaAnfStatus AS (
-  SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'VSAANF')
 )
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, VsaAnfStatus.StatusBez AS [Status anforderbarer Artikel], VsaAnf.Bestand AS Vertragsbestand, VsaAnf.BestandIst AS [Ist-Bestand]
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], Bereich.Bereich AS Kundenbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, VsaAnfStatus.StatusBez AS [Status anforderbarer Artikel], VsaAnf.Bestand AS Vertragsbestand, VsaAnf.BestandIst AS [Ist-Bestand]
 FROM VsaAnf
 JOIN Vsa ON VsaAnf.VsaID = Vsa.ID
-JOIN Kunden ON Vsa.KundenID = Kunden.ID
-JOIN KdArti ON VsaAnf.KdArtiID = KdArti.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID 
+JOIN KdArti ON VsaAnf.KdArtiID = KdArti.ID 
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+JOIN Bereich ON KdBer.BereichID = Bereich.ID
 JOIN ArtGroe ON VsaAnf.ArtGroeID = ArtGroe.ID
 JOIN VsaAnfStatus ON VsaAnf.Status = VsaAnfStatus.Status
 WHERE (($2$ = 1 AND VsaAnf.Bestand != 0) OR ($2$ = 0))
   AND VsaAnf.Status != N'I'
   AND Vsa.Status = N'A'
   AND Kunden.StandortID IN ($1$)
+  AND Bereich.ID IN ($3$)
 ORDER BY Kunden.KdNr, [Vsa-Stichwort], Artikel.ArtikelNr;
