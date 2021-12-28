93a62823e97dd5bfd139565b0c6164c903b101ae
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-12-28 15:00:56 +0100

    added: sorting device Inzing

--- Diverse/RepQueue-Auswertung.sql
+++ Diverse/RepQueue-Auswertung.sql
@@ -1,4 +1,4 @@
-SELECT TableName, ISNULL([11], 0) AS Enns, ISNULL([3], 0) AS [Enns 2], ISNULL([1], 0) AS [Lenzing 1], ISNULL([2], 0) AS [Lenzing 2], ISNULL([21], 0) AS Klagenfurt, ISNULL([31], 0) AS Bratislava, ISNULL([41], 0) AS Graz, ISNULL([51], 0) AS [Wr. Neustadt], ISNULL([61], 0) AS SA22  
+SELECT TableName, ISNULL([11], 0) AS Enns, ISNULL([3], 0) AS [Enns 2], ISNULL([1], 0) AS [Lenzing 1], ISNULL([2], 0) AS [Lenzing 2], ISNULL([21], 0) AS Klagenfurt, ISNULL([31], 0) AS Bratislava, ISNULL([41], 0) AS Graz, ISNULL([51], 0) AS [Wr. Neustadt], ISNULL([61], 0) AS SA22, ISNULL([106], 0) AS Inzing 
 FROM (
   SELECT SdcDev.ID AS SdcDevID, RepQueue.TableName, COUNT(DISTINCT RepQueue.TableID) AS AnzahlDS
   FROM RepQueue WITH (NOLOCK)
@@ -7,6 +7,6 @@ FROM (
 ) AS RepQueueData
 PIVOT (
   SUM(AnzahlDS)
-  FOR SdcDevID IN ([11], [3], [1], [2], [21], [31], [41], [51], [61])
+  FOR SdcDevID IN ([11], [3], [1], [2], [21], [31], [41], [51], [61], [106])
 ) AS SDCPivot
 ORDER BY TableName ASC;
