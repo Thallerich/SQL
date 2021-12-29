52bc5ab898224e26801ad304e4a47183ea582ff8
Thallerich <20878157+Thallerich@users.noreply.github.com>
2021-12-29 09:54:09 +0100

    changed we now select all pool-parts, even if they are not a customer article this is needed specifically for sterile sets

--- Report/030 - UHF-Schwundauswertung.sql
+++ Report/030 - UHF-Schwundauswertung.sql
@@ -7,25 +7,48 @@ WITH Inventurscan AS (
   GROUP BY OPScans.OPTeileID
 ),
 PoolteilStatus AS (
-  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
   FROM [Status]
   WHERE [Status].Tabelle = N'OPTEILE'
 )
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr AS [VSA-Nr], Vsa.Bez AS [VSA-Bezeichnung], OPTeile.Code AS Chipcode, PoolteilStatus.StatusBez AS [aktueller Status des Teils], Bereich.Bereich AS Produktbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS [Größe], KdArti.Vertragsartikel, RWCalc.BasisAfa AS Basisrestwert, RWCalc.RestwertInfo AS Restwert, CAST(OPTeile.LastScanTime AS date) AS [letzter Scan], Actions.ActionsBez AS [letzte Aktion], CAST(Inventurscan.Zeitpunkt AS date) AS [zuletzt inventiert], DATEDIFF(day, OPTeile.LastScanTime, GETDATE()) AS [Tage ohne Bewegung], OPTeile.Erstwoche AS [Erster Einsatz]
-FROM OPTeile
-CROSS APPLY funcGetRestwertOP(OPTeile.ID, @curweek, 1) AS RWCalc
-JOIN Vsa ON OPTeile.VsaID = Vsa.ID
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr AS [VSA-Nr], Vsa.Bez AS [VSA-Bezeichnung], Schwundteile.Code AS Chipcode, PoolteilStatus.StatusBez AS [aktueller Status des Teils], Bereich.Bereich AS Produktbereich, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS [Größe], Schwundteile.Vertragsartikel, Schwundteile.BasisAfa AS Basisrestwert, Schwundteile.RestwertInfo AS Restwert, CAST(Schwundteile.LastScanTime AS date) AS [letzter Scan], Actions.ActionsBez AS [letzte Aktion], CAST(Inventurscan.Zeitpunkt AS date) AS [zuletzt inventiert], DATEDIFF(day, Schwundteile.LastScanTime, GETDATE()) AS [Tage ohne Bewegung], Schwundteile.Erstwoche AS [Erster Einsatz]
+FROM (
+  SELECT OPTeile.ID AS OPTeileID, ArtGroe.ArtikelID, OPTeile.ArtGroeID, OPTeile.Status, OPTeile.RechPoID, OPTeile.VsaID, OPTeile.Code, OPTeile.LastScanTime, OPTeile.Erstwoche, OPTeile.LastErsatzFuerKdArtiID, CAST(MAX(CAST(KdArti.Vertragsartikel AS int)) AS bit) AS Vertragsartikel, OPTeile.LastActionsID, fRW.BasisAfa, fRW.RestwertInfo, CAST(0 AS bit) AS SetTeil
+  FROM Opteile
+  CROSS APPLY dbo.funcGetRestWertOP(OpTeile.ID, @curweek, 1) fRW
+  JOIN ArtGroe ON OPTeile.ArtGroeID = ArtGroe.ID
+  JOIN Vsa ON OPTeile.VsaID = Vsa.ID
+  JOIN KdArti ON ArtGroe.ArtikelID = KdArti.ArtikelID AND KdArti.KundenID = Vsa.KundenID
+  WHERE Vsa.KundenID = $ID$
+  GROUP BY OPTeile.ID, ArtGroe.ArtikelID, OPTeile.ArtGroeID, OPTeile.Status, OPTeile.RechPoID, OPTeile.VsaID, OPTeile.Code, OPTeile.LastScanTime, OPTeile.Erstwoche, OPTeile.LastActionsID, OPTeile.LastErsatzFuerKdArtiID, fRW.BasisAfa, fRW.RestwertInfo
+  
+  UNION
+  
+  SELECT Opteile.ID AS OPTeileID, OPTeile.ArtikelID, OPTeile.ArtGroeID, OPTeile.Status, OPTeile.RechPoID, OPTeile.VsaID, OPTeile.Code, OPTeile.LastScanTime, OPTeile.Erstwoche, OPTeile.LastErsatzFuerKdArtiID, CAST(0 AS bit) AS VertragsArtikel, OPTeile.LastActionsID, CAST(0 AS money) AS BasisAfa, CAST(0 AS money) AS RestwertInfo, CAST(1 AS bit) AS SetTeil
+  FROM OPTeile
+  JOIN ArtGroe ON OPTeile.ArtGroeID = ArtGroe.ID
+  JOIN OPEtiPo ON OPEtiPo.OPTeileID = OPTeile.ID
+  JOIN OPEtiKo ON OPEtiPo.OPEtiKoID = OPEtiKo.ID AND OPEtiKo.VsaID = OPTeile.VsaID
+  JOIN Vsa ON OPTeile.VsaID = Vsa.ID
+  WHERE Vsa.KundenID = $ID$
+    AND OPEtiKo.Status IN (N'R', N'U')
+    AND NOT EXISTS (
+      SELECT KdArti.ID
+      FROM KdArti
+      WHERE KdArti.KundenID = Vsa.KundenID
+        AND KdArti.ArtikelID = ArtGroe.ArtikelID
+    )
+) AS Schwundteile
+JOIN Vsa ON Schwundteile.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
-JOIN ArtGroe ON OPTeile.ArtGroeID = ArtGroe.ID
-JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+JOIN ArtGroe ON Schwundteile.ArtGroeID = ArtGroe.ID
+JOIN Artikel ON Schwundteile.ArtikelID = Artikel.ID
 JOIN Bereich ON Artikel.BereichID = Bereich.ID
-JOIN KdArti ON KdArti.KundenID = Kunden.ID AND KdArti.ArtikelID = Artikel.ID
-JOIN Actions ON OPTeile.LastActionsID = Actions.ID
-LEFT JOIN Inventurscan ON Inventurscan.OPTeileID = OPTeile.ID
-JOIN PoolteilStatus ON PoolteilStatus.Status = OPTeile.Status
-WHERE OPTeile.Status IN (N'Q', N'W')
-  AND OPTeile.LastActionsID IN (102, 120, 136)
-  AND OPTeile.RechPoID < 0
-  AND OPTeile.LastScanTime < $1$
+JOIN Actions ON Schwundteile.LastActionsID = Actions.ID
+LEFT JOIN Inventurscan ON Inventurscan.OPTeileID = Schwundteile.OPTeileID
+JOIN PoolteilStatus ON PoolteilStatus.Status = Schwundteile.Status
+WHERE Schwundteile.LastActionsID IN (102, 120, 136)
+  AND Schwundteile.RechPoID < 0
+  AND Schwundteile.LastScanTime < $1$
   AND Bereich.ID IN ($2$)
   AND Kunden.ID = $ID$;
