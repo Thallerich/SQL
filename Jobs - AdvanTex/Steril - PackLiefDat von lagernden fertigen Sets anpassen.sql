1b810c833e74fc4efca2fd69d363b36c427112cc
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-01-03 15:49:05 +0100

    refinded script now updates date to first packing note available on this VSA

--- Jobs - AdvanTex/Steril - PackLiefDat von lagernden fertigen Sets anpassen.sql
+++ Jobs - AdvanTex/Steril - PackLiefDat von lagernden fertigen Sets anpassen.sql
@@ -4,8 +4,20 @@ DECLARE @date1weekago date = CAST(DATEADD(week, -1, GETDATE()) AS date);
 DECLARE @sqltext nvarchar(max);
 
 SET @sqltext = N'
-UPDATE OPEtiKo SET PackLiefDat = @curdate
+WITH Anf AS (
+  SELECT AnfKo.VsaID, KdArti.ArtikelID, MIN(AnfKo.LieferDatum) AS Lieferdatum
+  FROM AnfPo
+  JOIN AnfKo ON AnfPo.AnfKoID = AnfKo.ID
+  JOIN KdArti ON AnfPo.KdArtiID = KdArti.ID
+  WHERE AnfKo.LieferDatum > @curdate
+    AND AnfPo.Angefordert > 0
+    AND AnfPo.Angefordert != AnfPo.Geliefert
+    AND AnfKo.Status <= N''I''
+  GROUP BY AnfKo.VsaID, KdArti.ArtikelID
+)
+UPDATE OPEtiKo SET PackLiefDat = Anf.Lieferdatum
 FROM OPEtiKo
+JOIN Anf ON Anf.ArtikelID = OPEtiKo.ArtikelID AND Anf.VsaID = OPEtiKo.PackVsaID
 WHERE OPEtiKo.Status IN (N''J'', N''M'', N''P'')
   AND OPEtiKo.PackLiefDat < @date1weekago;
 ';
