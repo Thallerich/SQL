04da612d37fc2f3f12ec37081c5cbf0e21115bb4
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-01-14 09:26:10 +0100

    added - column "Status Teil"

--- Report/286 - Detailinfoliste.sql
+++ Report/286 - Detailinfoliste.sql
@@ -1,11 +1,17 @@
-SELECT Kunden.KdNr, Kunden.ID AS KundenID, Vsa.Bez AS VsaBez, Abteil.Bez AS KsSt, Traeger.ID AS TraegerID, Traeger.PersNr, Traeger.Titel, Traeger.Vorname, Traeger.Nachname, Traeger.SchrankInfo, Teile.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS ArtikelBez, ArtGroe.Groesse, Teile.Indienst, Teile.Ausdienst, Teile.Ausgang1, Teile.Eingang1, Teile.Ausgang2, Teile.Eingang2, Teile.Ausgang3, Teile.Eingang3, DATEDIFF(day, IIF(Teile.Eingang1 <= Teile.Ausgang1, Ausgang1, Eingang1), GETDATE()) AS Tage, Teile.Kostenlos, Teile.RuecklaufK
-FROM Teile, Traeger, Vsa, Kunden, Artikel, ArtGroe, Abteil
+WITH Teilestatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = N'TEILE'
+)
+SELECT Kunden.KdNr, Kunden.ID AS KundenID, Vsa.Bez AS VsaBez, Abteil.Bez AS KsSt, Traeger.ID AS TraegerID, Traeger.PersNr, Traeger.Titel, Traeger.Vorname, Traeger.Nachname, Traeger.SchrankInfo, Teile.Barcode, Teilestatus.StatusBez AS [Status Teil], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS ArtikelBez, ArtGroe.Groesse, Teile.Indienst, Teile.Ausdienst, Teile.Ausgang1, Teile.Eingang1, Teile.Ausgang2, Teile.Eingang2, Teile.Ausgang3, Teile.Eingang3, DATEDIFF(day, IIF(Teile.Eingang1 <= Teile.Ausgang1, Ausgang1, Eingang1), GETDATE()) AS Tage, Teile.Kostenlos, Teile.RuecklaufK
+FROM Teile, Traeger, Vsa, Kunden, Artikel, ArtGroe, Abteil, Teilestatus
 WHERE Teile.TraegerID = Traeger.ID
   AND Traeger.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
   AND Teile.ArtikelID = Artikel.ID
   AND Teile.ArtGroeID = ArtGroe.ID
   AND Traeger.AbteilID = Abteil.ID
+  AND Teile.Status = Teilestatus.Status
   AND Kunden.ID = $ID$
   AND Teile.Indienst IS NOT NULL
   AND Traeger.Status IN (N'A', N'K')
