ce21246f8ca4dcaed394493ec0eb7a9d5a6e27ec
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-02-02 13:11:03 +0100

    IT-55830   - allow parameter input for dynamic columns

--- Report/953 - UHF - Drehungsliste.sql
+++ Report/953 - UHF - Drehungsliste.sql
@@ -1,28 +1,46 @@
-SELECT KdGf.KurzBez AS SGF,
-  Bereich.BereichBez$LAN$ AS Produktbereich,
-  Kunden.KdNr,
-  Kunden.SuchCode AS  [Kunde SuchCode],
-  Vsa.ID AS VsaID,
-  Vsa.Bez AS Vsa,
-  Artikel.ArtikelNr,
-  Artikel.ArtikelBez$LAN$ AS Artikel,
-  ISNULL(VsaAnf.Bestand, 0) AS Vertragsbestand,
-  SUM(IIF(OPTeile.Status = N'Q', 1, 0)) AS [Teile beim Kunden],
-  SUM(IIF(OPTeile.Status = N'W' AND OPTeile.RechPoID > 0, 1, 0)) AS [Schwundmarkiert (verrechnet)],
-  SUM(IIF(OPTeile.Status = N'W' AND OPTeile.RechPoID < 0, 1, 0)) AS [Schwundmarkiert (nicht verrechnet)],
-  SUM(IIF(DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) <= 7 AND OPTeile.Status = N'Q', 1, 0)) AS [stark drehend <= 7],
-  SUM(IIF(DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) > 7 AND DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) <= 30 AND OPTeile.Status = N'Q', 1, 0)) AS [schwach drehend <= 30],
-  SUM(IIF(DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) > 30 AND DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) <= 60 AND OPTeile.Status = N'Q', 1, 0)) AS [kaum drehend <= 60],
-  SUM(IIF(DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) > 60 AND OPTeile.Status = N'Q', 1, 0)) AS [nicht drehend > 60]
-FROM OPTeile
-JOIN Vsa ON OPTeile.VsaID = Vsa.ID
-JOIN Kunden ON Vsa.KundenID = Kunden.ID
-JOIN KdGf ON Kunden.KdGfID = KdGf.ID
-JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
-JOIN Bereich ON Artikel.BereichID = Bereich.ID
-JOIN KdArti ON KdArti.ArtikelID = Artikel.ID AND KdArti.KundenID = Kunden.ID
-LEFT JOIN VsaAnf ON VsaAnf.VsaID = Vsa.ID AND VsaAnf.KdArtiID = KdArti.ID
-WHERE Kunden.ID = $1$
-  AND OPTeile.Status IN (N'Q', N'W')
-  AND OPTeile.LastActionsID IN (102, 116, 120, 136)
-GROUP BY KdGf.KurzBez, Bereich.BereichBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Vsa.ID, Vsa.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, VsaAnf.Bestand;
+DECLARE @sqltext nvarchar(max);
+DECLARE @stark int, @schwach int, @kaum int, @kundenid int;
+
+SET @kundenid = $1$;
+SET @stark = $2$;
+SET @schwach = $3$;
+SET @kaum = $4$;
+
+IF @schwach < @stark
+  RAISERROR(N'Schwach drehend muss größer Stark drehend sein!', 15, 1);
+
+IF @kaum < @schwach
+  RAISERROR(N'Kaum drehend muss größer als Schwach drehend sein!', 15, 2);
+
+SET @sqltext = N'
+  SELECT KdGf.KurzBez AS SGF,
+    Bereich.BereichBez$LAN$ AS Produktbereich,
+    Kunden.KdNr,
+    Kunden.SuchCode AS  [Kunde SuchCode],
+    Vsa.ID AS VsaID,
+    Vsa.Bez AS Vsa,
+    Artikel.ArtikelNr,
+    Artikel.ArtikelBez$LAN$ AS Artikel,
+    ISNULL(VsaAnf.Bestand, 0) AS Vertragsbestand,
+    SUM(IIF(OPTeile.Status = N''Q'', 1, 0)) AS [Teile beim Kunden],
+    SUM(IIF(OPTeile.Status = N''W'' AND OPTeile.RechPoID > 0, 1, 0)) AS [Schwundmarkiert (verrechnet)],
+    SUM(IIF(OPTeile.Status = N''W'' AND OPTeile.RechPoID < 0, 1, 0)) AS [Schwundmarkiert (nicht verrechnet)],
+    SUM(IIF(DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) <= @stark AND OPTeile.Status = N''Q'', 1, 0)) AS [stark drehend <= ' + CAST(@stark AS nvarchar) + '],
+    SUM(IIF(DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) > @stark AND DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) <= @schwach AND OPTeile.Status = N''Q'', 1, 0)) AS [schwach drehend <= ' + CAST(@schwach AS nvarchar) + '],
+    SUM(IIF(DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) > @schwach AND DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) <= @kaum AND OPTeile.Status = N''Q'', 1, 0)) AS [kaum drehend <= ' + CAST(@kaum AS nvarchar) +'],
+    SUM(IIF(DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) > @kaum AND OPTeile.Status = N''Q'', 1, 0)) AS [nicht drehend > ' + CAST(@kaum AS nvarchar) + ']
+  FROM OPTeile
+  JOIN Vsa ON OPTeile.VsaID = Vsa.ID
+  JOIN Kunden ON Vsa.KundenID = Kunden.ID
+  JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+  JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+  JOIN Bereich ON Artikel.BereichID = Bereich.ID
+  JOIN KdArti ON KdArti.ArtikelID = Artikel.ID AND KdArti.KundenID = Kunden.ID
+  LEFT JOIN VsaAnf ON VsaAnf.VsaID = Vsa.ID AND VsaAnf.KdArtiID = KdArti.ID
+  WHERE Kunden.ID = @kundenid
+    AND OPTeile.Status IN (N''Q'', N''W'')
+    AND OPTeile.LastActionsID IN (102, 116, 120, 136)
+  GROUP BY KdGf.KurzBez, Bereich.BereichBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Vsa.ID, Vsa.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, VsaAnf.Bestand;
+';
+
+EXEC sp_executesql @sqltext, N'@kundenid int, @stark int, @schwach int, @kaum int', @kundenid, @stark, @schwach, @kaum;
