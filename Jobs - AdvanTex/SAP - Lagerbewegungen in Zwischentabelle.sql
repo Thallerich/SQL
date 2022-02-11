57eb392e825004e0f800dd0e4deedcf7b4af9fab
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-02-11 09:58:09 +0100

    bugfix should be company, not location

--- Jobs - AdvanTex/SAP - Lagerbewegungen in Zwischentabelle.sql
+++ Jobs - AdvanTex/SAP - Lagerbewegungen in Zwischentabelle.sql
@@ -4,13 +4,14 @@ TRUNCATE TABLE __LagerBewHW;
 
 INSERT INTO __LagerBewHW (ID, Zeitpunkt, ArtikelNr, ArtikelBez, SuchCode, Differenz, Groesse, BestandID, LgBewCodBez)
 SELECT LagerBew.ID, LagerBew.Zeitpunkt, Artikel.ArtikelNr, Artikel.ArtikelBez, Standort.SuchCode, LagerBew.Differenz, ArtGroe.Groesse, Bestand.ID BestandID, LgBewCodBez
-FROM LagerBew, Bestand, ArtGroe, Artikel, LagerArt, Standort, LgBewCod
+FROM LagerBew, Bestand, ArtGroe, Artikel, LagerArt, Standort, LgBewCod, Firma
 WHERE LagerBew.BestandID = Bestand.ID
   AND Bestand.ArtGroeID = ArtGroe.ID
   AND ArtGroe.ArtikelID = Artikel.ID
   AND Bestand.LagerArtID = LagerArt.ID
   AND LagerArt.LagerID = Standort.ID
   AND LagerBew.LgBewCodID = LgBewCod.ID
-  AND (Standort.SuchCode = N'FA14' OR Standort.SuchCode = N'BUDA')
+  AND Lagerart.FirmaID = Firma.ID
+  AND (Firma.SuchCode = N'FA14' OR Firma.SuchCode = N'BUDA')
   AND ((DB_NAME() = N'Salesianer' AND Standort.SuchCode != N'SMZL') OR (DB_NAME() != N'Salesianer'))
   AND LagerBew.ID > @MinLagerBewID;
