54847b05a475ac0be4a1d90d1be39089e0343fb9
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-02-22 13:21:16 +0100

    IT-55386 - changes  - added production location  - added article group  - added last customer info

--- Report/735 - Teile verschrottet im Zeitraum - OP - OP-Textilien.sql
+++ Report/735 - Teile verschrottet im Zeitraum - OP - OP-Textilien.sql
@@ -1,10 +1,12 @@
-SELECT OPTeile.Code AS Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, OPTeile.ErstWoche AS [Erstauslieferungswoche], OPTeile.WegDatum AS [Schrott-Datum], WegGrund.WegGrundBez$LAN$ AS Schrottgrund, OPTeile.AnzSteril AS [Anzahl Steriliationen], OPTeile.AnzWasch AS [Anzahl Wäschen], OPTeile.AnzImpregnier AS [Anzahl Imprägnierungen]
+SELECT Standort.Bez AS Produktion, OPTeile.Code AS Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGru.ArtGruBez$LAN$ AS Artikelgruppe, Artikel.EKPreis, OPTeile.ErstWoche AS [Erstauslieferungswoche], OPTeile.WegDatum AS [Schrott-Datum], WegGrund.WegGrundBez$LAN$ AS Schrottgrund, OPTeile.AnzSteril AS [Anzahl Steriliationen], OPTeile.AnzWasch AS [Anzahl Wäschen], OPTeile.AnzImpregnier AS [Anzahl Imprägnierungen], Kunden.KdNr AS [KdNr letzter Kunde], Kunden.SuchCode AS [letzter Kunde]
 FROM OPTeile
 JOIN Vsa ON OPTeile.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+JOIN ArtGru ON Artikel.ArtGruID = ArtGru.ID
 JOIN WegGrund ON OPTeile.WegGrundID = WegGrund.ID
 JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND Artikel.BereichID = StandBer.BereichID
+JOIN Standort ON StandBer.ProduktionID = Standort.ID
 WHERE Kunden.KdGfID IN ($3$)
   AND Kunden.FirmaID IN ($4$)
   AND OPTeile.WegGrundID IN ($2$)
