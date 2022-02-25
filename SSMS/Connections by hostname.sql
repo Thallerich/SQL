01f5debaf8e7ca59b65e48c57e7bc3b64ba53bc5
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-02-25 08:51:48 +0100

    removed specific hostname

--- SSMS/Connections by hostname.sql
+++ SSMS/Connections by hostname.sql
@@ -2,4 +2,3 @@ SELECT conn.connect_time, conn.client_net_address, sess.host_name, sess.login_na
 FROM sys.dm_exec_connections AS conn
 JOIN sys.dm_exec_sessions AS sess ON conn.session_id = sess.session_id
 CROSS APPLY sys.dm_exec_sql_text(conn.most_recent_sql_handle) AS sqltext
-WHERE sess.host_name LIKE N'sns%'
