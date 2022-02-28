a752d652b555f2cc29384aa74d43eac3ea758716
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-02-28 14:58:44 +0100

    initial version

---
+++ VOEST/VOEST - Vollversorgung.sql
@@ -0,0 +1,113 @@
+DROP TABLE IF EXISTS #TmpVOESTRechnung;
+
+DECLARE @RechKo TABLE (
+  RechKoID int
+);
+
+INSERT INTO @RechKo (RechKoID)
+SELECT RechKo.ID
+FROM RechKo
+JOIN Kunden ON RechKo.KundenID = Kunden.ID
+WHERE RechKo.RechDat BETWEEN N'2021-01-01' AND N'2021-12-31'
+  AND RechKo.Status = N'F'
+  AND Kunden.KdNr = 272295;
+
+SELECT Artikel.ID AS ArtikelID,
+  Traeger.ID AS TraegerID,
+  Kunden.KdNr,
+  Kunden.SuchCode AS Kunde,
+  Vsa.VsaNr,
+  Vsa.SuchCode AS VsaStichwort,
+  Vsa.Bez AS VsaBezeichnung,
+  Vsa.GebaeudeBez AS Abteilung,
+  Vsa.Name2 AS Bereich,
+  Abteil.ID AS AbteilID,
+  Abteil.Abteilung AS Kostenstelle,
+  Abteil.Bez AS Kostenstellenbezeichnung,
+  Traeger.Traeger AS TraegerNr,
+  Traeger.PersNr,
+  Traeger.Nachname,
+  Traeger.Vorname,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelBez AS ArtikelBez,
+  KdArti.VariantBez AS Variante,
+  0 AS Waschzyklen,
+  Teile.RuecklaufG AS WaschzyklenGesamt,
+  SUM(AbtKdArW.EPreis) AS Mietkosten,
+  CAST(0 AS money) AS Waschkosten,
+  CAST(0 AS money) AS Gesamt,
+  Teile.Barcode,
+  Teile.IndienstDat AS Erstausgabedatum
+INTO #TmpVOESTRechnung
+FROM RechPo
+JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+JOIN AbtKdArW ON AbtKdArW.RechPoID = RechPo.ID
+JOIN TraeArch ON TraeArch.AbtKdArWID = AbtKdArW.ID
+JOIN TraeArti ON TraeArch.TraeArtiID = TraeArti.ID
+JOIN Teile ON Teile.TraeArtiID = TraeArti.ID
+JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
+JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN Abteil ON RechPo.AbteilID = Abteil.ID
+JOIN KdArti ON RechPo.KdArtiID = KdArti.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+WHERE RechKo.ID IN (SELECT RechKoID FROM @RechKo)
+  AND Teile.IndienstDat <= RechKo.BisDatum
+  AND ISNULL(Teile.AusdienstDat, N'2099-12-31') >= RechKo.VonDatum
+GROUP BY Artikel.ID,
+  Traeger.ID,
+  Kunden.KdNr,
+  Kunden.SuchCode,
+  Vsa.VsaNr,
+  Vsa.SuchCode,
+  Vsa.Bez,
+  Vsa.GebaeudeBez,
+  Vsa.Name2,
+  Abteil.ID,
+  Abteil.Abteilung,
+  Abteil.Bez,
+  Traeger.Traeger,
+  Traeger.PersNr,
+  Traeger.Nachname,
+  Traeger.Vorname,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelBez,
+  KdArti.VariantBez,
+  Teile.RuecklaufG,
+  Teile.Barcode,
+  Teile.IndienstDat;
+
+MERGE INTO #TmpVOESTRechnung AS VOESTRechnung
+USING (
+  SELECT ArtikelID, TraegerID, Barcode, Erstausgabedatum, KdNr, Kunde, VsaNr, VsaStichwort, VsaBezeichnung, Abteilung, Bereich, AbteilID, Kostenstelle, Kostenstellenbezeichnung, TraegerNr, PersNr, Nachname, Vorname, ArtikelNr, ArtikelBez, Variante, SUM(EPreis * Waschzyklen) AS Bearbeitungspreis, SUM(Waschzyklen) AS Waschzyklen, RuecklaufG
+  FROM (
+    SELECT Teile.ArtikelID, Teile.TraegerID, Teile.Barcode, Teile.IndienstDat AS Erstausgabedatum, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS VsaBezeichnung, Vsa.GebaeudeBez AS Abteilung, Vsa.Name2 AS Bereich, Abteil.ID AS AbteilID, Abteil.Abteilung AS Kostenstelle, Abteil.Bez AS Kostenstellenbezeichnung, Traeger.Traeger AS TraegerNr, Traeger.PersNr, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez AS ArtikelBez, KdArti.VariantBez AS Variante, LsPo.EPreis, COUNT(Scans.ID) AS Waschzyklen, Teile.RuecklaufG
+    FROM Scans
+    JOIN LsPo ON Scans.LsPoID = LsPo.ID
+    JOIN Teile ON Scans.TeileID = Teile.ID
+    JOIN Traeger ON Teile.TraegerID = Traeger.ID
+    JOIN Vsa ON Traeger.VsaID = Vsa.ID
+    JOIN Kunden oN Vsa.KundenID = Kunden.ID
+    JOIN RechPo ON LsPo.RechPoID = RechPo.ID
+    JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+    JOIN Abteil ON LsPo.AbteilID = Abteil.ID
+    JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
+    JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+    WHERE RechKo.ID IN (SELECT RechKoID FROM @RechKo)
+    GROUP BY Teile.ArtikelID, Teile.TraegerID, Teile.Barcode, Teile.IndienstDat, Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.SuchCode, Vsa.Bez, Vsa.GebaeudeBez, Vsa.Name2, Abteil.ID, Abteil.Abteilung, Abteil.Bez, Traeger.Traeger, Traeger.PersNr, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez, KdArti.VariantBez, LsPo.EPreis, Teile.RuecklaufG
+  ) x
+  GROUP BY ArtikelID, TraegerID, Barcode, Erstausgabedatum, KdNr, Kunde, VsaNr, VsaStichwort, VsaBezeichnung, Abteilung, Bereich, AbteilID, Kostenstelle, Kostenstellenbezeichnung, TraegerNr, PersNr, Nachname, Vorname, ArtikelNr, ArtikelBez, Variante, RuecklaufG
+) AS Bearbeitung
+ON Bearbeitung.ArtikelID = VOESTRechnung.ArtikelID AND Bearbeitung.TraegerID = VOESTRechnung.TraegerID AND Bearbeitung.Variante = VOESTRechnung.Variante AND Bearbeitung.Barcode = VOESTRechnung.Barcode AND Bearbeitung.AbteilID = VOESTRechnung.AbteilID
+WHEN MATCHED THEN
+  UPDATE SET Waschkosten = Bearbeitung.Bearbeitungspreis, Waschzyklen = Bearbeitung.Waschzyklen
+WHEN NOT MATCHED THEN
+  INSERT (ArtikelID, TraegerID, Barcode, Erstausgabedatum, KdNr, Kunde, VsaNr, VsaStichwort, VsaBezeichnung, Abteilung, Bereich, AbteilID, Kostenstelle, Kostenstellenbezeichnung, TraegerNr, PersNr, Nachname, Vorname, ArtikelNr, ArtikelBez, Variante, Mietkosten, Waschkosten, Waschzyklen, WaschzyklenGesamt)
+  VALUES (Bearbeitung.ArtikelID, Bearbeitung.TraegerID, Bearbeitung.Barcode, Bearbeitung.Erstausgabedatum, Bearbeitung.KdNr, Bearbeitung.Kunde, Bearbeitung.VsaNr, Bearbeitung.VsaStichwort, Bearbeitung.VsaBezeichnung, Bearbeitung.Abteilung, Bearbeitung.Bereich, Bearbeitung.AbteilID, Bearbeitung.Kostenstelle, Bearbeitung.Kostenstellenbezeichnung, Bearbeitung.TraegerNr, Bearbeitung.PersNr, Bearbeitung.Nachname, Bearbeitung.Vorname, Bearbeitung.ArtikelNr, Bearbeitung.ArtikelBez, Bearbeitung.Variante, 0, Bearbeitung.Bearbeitungspreis, Bearbeitung.Waschzyklen, Bearbeitung.RuecklaufG);
+
+UPDATE #TmpVOESTRechnung SET Gesamt = Waschkosten + Mietkosten;
+
+SELECT VOESTRechnung.KdNr, VOESTRechnung.Kunde, VOESTRechnung.VsaNr, VOESTRechnung.VsaBezeichnung AS [Vsa-Bezeichnung], VOESTRechnung.Abteilung, VOESTRechnung.Bereich, VOESTRechnung.Kostenstelle, VOESTRechnung.Kostenstellenbezeichnung, VOESTRechnung.TraegerNr AS TrägerNr, VOESTRechnung.PersNr AS Personalnummer, VOESTRechnung.Nachname, VOESTRechnung.Vorname, VOESTRechnung.ArtikelNr, VOESTRechnung.ArtikelBez AS Artikelbezeichnung, VOESTRechnung.Variante AS Verrechnungsart, VOESTRechnung.Waschzyklen, VOESTRechnung.WaschzyklenGesamt AS [Waschzyklen Gesamt], VOESTRechnung.Mietkosten, VOESTRechnung.Waschkosten, VOESTRechnung.Gesamt AS Gesamtkosten, VOESTRechnung.Barcode, VOESTRechnung.Erstausgabedatum, VOEST_VVPrList.Vollversorgungspreis
+FROM #TmpVOESTRechnung AS VOESTRechnung
+LEFT JOIN Salesianer_Archive.dbo.VOEST_VVPrList ON VOESTRechnung.ArtikelNr = VOEST_VVPrList.ArtikelNr
+ORDER BY KdNr, VsaNr, TrägerNr, ArtikelNr;
