d4c0bcc303b289ff0bcaa34c090402cf39603f5e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-01 10:31:08 +0200

    prepwork for automatic reporting

--- VOEST/Restwerte zu Barcode-Liste.sql
+++ VOEST/Restwerte zu Barcode-Liste.sql
@@ -3,10 +3,13 @@ WITH Teilestatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = N'TEILE'
 )
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr AS [VSA-Nummer], Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Vsa.Name1 AS [VSA-Adresszeile 1], Vsa.Name2 AS [VSA-Adresszeile 2], Vsa.Name3 AS [VSA-Adresszeile 3], Traeger.Traeger AS Trägernummer, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS Variantenbezeichnung, ArtGroe.Groesse AS Größe, TraeArti.Menge AS [Max. Bestand], Teile.Barcode, CAST(IIF(Teile.Status > N'Q', 1, 0) AS bit) AS Stilllegung, Teilestatus.StatusBez AS [Status Teil], Teile.Eingang1 AS [letzter Eingang], Teile.Ausgang1 AS [letzter Ausgang], Week.Woche AS [Ersteinsatz-Woche], TeileRestwert.BasisAfa AS Basisrestwert, TeileRestwert.AlterInfo AS [Alter in Wochen], TeileRestwert.RestwertInfo AS [Restwert aktuell]
+/* SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr AS [VSA-Nummer], Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Vsa.Name1 AS [VSA-Adresszeile 1], Vsa.Name2 AS [VSA-Adresszeile 2], Vsa.Name3 AS [VSA-Adresszeile 3], Traeger.Traeger AS Trägernummer, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS Variantenbezeichnung, ArtGroe.Groesse AS Größe, TraeArti.Menge AS [Max. Bestand], Teile.Barcode, CAST(IIF(Teile.Status > N'Q', 1, 0) AS bit) AS Stilllegung, Teilestatus.StatusBez AS [Status Teil], Teile.Eingang1 AS [letzter Eingang], Teile.Ausgang1 AS [letzter Ausgang], Week.Woche AS [Ersteinsatz-Woche], TeileRestwert.BasisAfa AS Basisrestwert, TeileRestwert.AlterInfo AS [Alter in Wochen], TeileRestwert.RestwertInfo AS [Restwert aktuell] */
+SELECT Kunden.kdNr, Vsa.VsaNr, Vsa.Bez AS [VSA-Bezeichnung], Schrank.SchrankNr AS Schrank, TraeFach.Fach, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Teile.Barcode, Teilestatus.StatusBez AS Teilestatus, TeileRestwert.RestwertInfo AS Restwert
 FROM Teile
 JOIN TraeArti ON Teile.TraeArtiID = TraeArti.ID
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
+LEFT JOIN TraeFach ON TraeFach.TraegerID = Traeger.ID
+LEFT JOIN Schrank ON TraeFach.SchrankID = Schrank.ID
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
@@ -18,4 +21,5 @@ JOIN Week ON DATEADD(day, Teile.AnzTageImLager, Teile.ErstDatum) BETWEEN Week.Vo
 CROSS APPLY funcGetRestwert(Teile.ID, N'2022/09', 1) AS TeileRestwert
 WHERE Teile.Barcode IN (SELECT Barcode COLLATE Latin1_General_CS_AS FROM __voestpool)
   AND Teile.Status BETWEEN N'Q' AND N'W'
-  AND Teile.Einzug IS NULL;
+  AND Teile.Einzug IS NULL
+  AND Kunden.KdNr = 272295;
