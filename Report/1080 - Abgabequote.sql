ae91e4e7fbd471ebb9e239272e9dce43fd2a85dc
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-04 13:22:06 +0200

    finalized and published to advantex

---
+++ Report/1080 - Abgabequote.sql
@@ -0,0 +1,25 @@
+DECLARE @StartWeek nchar(7) = (SELECT Week.Woche FROM Week WHERE $STARTDATE$ BETWEEN Week.VonDat AND Week.BisDat);
+DECLARE @EndWeek nchar(7) = (SELECT Week.Woche FROM Week WHERE $ENDDATE$ BETWEEN Week.VonDat AND Week.BisDat);
+
+SELECT KdNr, Kunde, Produktbereich, Artikelgruppe, Sortiment, ArtikelNr, Artikelbezeichnung, ROUND(AVG(TraeAnz), 0) AS Trägeranzahl, ROUND(AVG(Menge), 0) AS Durchschnitt, SUM(Menge) AS Umlauf, ROUND(SUM(Ruecklauf), 1) AS Maximum, SUM(Effektiv) AS Effektiv, ROUND(IIF(SUM(Ruecklauf) = 0, 0.0, SUM(Effektiv) * 100 / SUM(Ruecklauf)), 2) AS [Quote %]
+FROM (
+  SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Bereich.BereichBez$LAN$ AS Produktbereich, ArtGru.ArtGruBez$LAN$ AS Artikelgruppe, ProdGru.ProdGruBez$LAN$ AS Sortiment, Wochen.Woche, SUM(TraeArch.Menge) AS Menge, SUM(TraeArch.Effektiv) AS Effektiv, IIF(SUM(TraeArch.Menge) < 3, CAST((SUM(TraeArch.Menge) / 3.0) AS numeric(18, 4)), CAST(((SUM(TraeArch.Menge) - 1) / 2.0) AS numeric(18, 4))) AS Ruecklauf, COUNT(DISTINCT Traeger.ID) AS TraeAnz
+  FROM TraeArch
+  JOIN TraeArti ON TraeArch.TraeArtiID = TraeArti.ID
+  JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
+  JOIN Vsa ON Traeger.VsaID = Vsa.ID
+  JOIN Kunden ON Vsa.KundenID = Kunden.ID
+  JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
+  JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+  JOIN ArtGru ON Artikel.ArtGruID = ArtGru.ID
+  JOIN Prodgru ON Artikel.ProdgruID = Prodgru.ID
+  JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+  JOIN Bereich ON KdBer.BereichID = Bereich.ID
+  JOIN Wochen ON TraeArch.WochenID = Wochen.ID
+  WHERE Kunden.ID IN ($3$)
+    AND Wochen.Woche BETWEEN @StartWeek AND @EndWeek
+    AND TraeArch.Menge > 0
+  GROUP BY Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Bereich.BereichBez$LAN$, ArtGru.ArtGruBez$LAN$, ProdGru.ProdGruBez$LAN$, Wochen.Woche
+) AS Ruecklaufdaten
+GROUP BY KdNr, Kunde, Produktbereich, Artikelgruppe, Sortiment, ArtikelNr, Artikelbezeichnung
+ORDER BY KdNr, ArtikelNr;
