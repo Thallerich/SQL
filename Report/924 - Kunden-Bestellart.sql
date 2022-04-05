edb5e76bf255aed2fc25d7a56721f9401f16b286
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-05 09:20:08 +0200

    IT-57748  - added "bereich" list

--- Report/924 - Kunden-Bestellart.sql
+++ Report/924 - Kunden-Bestellart.sql
@@ -1,10 +1,23 @@
 DECLARE @ProduktionID int = $1$;
 
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], AnfArt.AnfArtBez AS [Anforderungsart VSA], N'Zählkunde' AS Bestellart
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], AnfArt.AnfArtBez AS [Anforderungsart VSA], N'Zählkunde' AS Bestellart, Touren.Tour, WochTag.WochtagBez$LAN$ AS Wochentag,
+  Kundenbereiche = CAST(STUFF((
+    SELECT N', ' + Bereich.Bereich
+    FROM VsaTour AS vt
+    JOIN KdBer ON vt.KdBerID = KdBer.ID
+    JOIN Bereich ON KdBer.BereichID = Bereich.ID
+    WHERE vt.VsaID = Vsa.ID
+      AND vt.TourenID = Touren.ID
+      AND (CAST(GETDATE() AS date) BETWEEN vt.VonDatum AND vt.BisDatum OR CAST(GETDATE() AS date) < vt.VonDatum)
+    ORDER BY Bereich FOR XML PATH(N'')
+  ), 1, 1, N'') AS nchar(100))
 FROM Vsa
 JOIN AnfArt ON Vsa.AnfArtID = AnfArt.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN StandKon ON Vsa.StandKonID = StandKon.ID
+JOIN VsaTour ON VsaTour.VsaID = Vsa.ID
+JOIN Touren ON VsaTour.TourenID = Touren.ID
+JOIN WochTag ON Touren.Wochentag = WochTag.Wochentag
 WHERE EXISTS (
     SELECT StandBer.*
     FROM StandBer
@@ -21,14 +34,29 @@ WHERE EXISTS (
   AND Vsa.Status = N'A'
   AND Kunden.Status = N'A'
   AND Kunden.AdrArtID = 1
+  AND (CAST(GETDATE() AS date) BETWEEN VsaTour.VonDatum AND VsaTour.BisDatum OR CAST(GETDATE() AS date) < VsaTour.VonDatum)
+GROUP BY Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.SuchCode, Vsa.Bez, AnfArt.AnfArtBez, Touren.Tour, WochTag.WochtagBez$LAN$, Vsa.ID, Touren.ID
 
 UNION ALL
 
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], AnfArt.AnfArtBez AS [Anforderungsart VSA], N'Bestellkunde' AS Bestellart
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [Vsa-Stichwort], Vsa.Bez AS [Vsa-Bezeichnung], AnfArt.AnfArtBez AS [Anforderungsart VSA], N'Bestellkunde' AS Bestellart, Touren.Tour, WochTag.WochtagBez$LAN$ AS Wochentag,
+  Kundenbereiche = CAST(STUFF((
+    SELECT N', ' + Bereich.Bereich
+    FROM VsaTour AS vt
+    JOIN KdBer ON vt.KdBerID = KdBer.ID
+    JOIN Bereich ON KdBer.BereichID = Bereich.ID
+    WHERE vt.VsaID = Vsa.ID
+      AND vt.TourenID = Touren.ID
+      AND (CAST(GETDATE() AS date) BETWEEN vt.VonDatum AND vt.BisDatum OR CAST(GETDATE() AS date) < vt.VonDatum)
+    ORDER BY Bereich FOR XML PATH(N'')
+  ), 1, 1, N'') AS nchar(100))
 FROM Vsa
 JOIN AnfArt ON Vsa.AnfArtID = AnfArt.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN StandKon ON Vsa.StandKonID = StandKon.ID
+JOIN VsaTour ON VsaTour.VsaID = Vsa.ID
+JOIN Touren ON VsaTour.TourenID = Touren.ID
+JOIN WochTag ON Touren.Wochentag = WochTag.Wochentag
 WHERE EXISTS (
     SELECT StandBer.*
     FROM StandBer
@@ -45,4 +73,6 @@ WHERE EXISTS (
   )
   AND Vsa.Status = N'A'
   AND Kunden.Status = N'A'
-  AND Kunden.AdrArtID = 1;
+  AND Kunden.AdrArtID = 1
+  AND (CAST(GETDATE() AS date) BETWEEN VsaTour.VonDatum AND VsaTour.BisDatum OR CAST(GETDATE() AS date) < VsaTour.VonDatum)
+GROUP BY Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.SuchCode, Vsa.Bez, AnfArt.AnfArtBez, Touren.Tour, WochTag.WochtagBez$LAN$, Vsa.ID, Touren.ID
