3546d385e1b1cc5d943b6ffab1514588122a1c81
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-08 10:01:48 +0200

    included "geschäftsbereich"

--- Report/055 - Reklamationsquote.sql
+++ Report/055 - Reklamationsquote.sql
@@ -6,13 +6,14 @@ IF OBJECT_ID(N'tempdb..#ReklQuot') IS NOT NULL
 ELSE
   CREATE TABLE #ReklQuot (
     Jahr int,
+    Geschäftsbereich nchar(5) COLLATE Latin1_General_CS_AS,
     Produktion nvarchar(40) COLLATE Latin1_General_CS_AS,
     Liefermenge bigint,
     Reklamationsmenge bigint
   );
 
-INSERT INTO #ReklQuot (Jahr, Produktion, Reklamationsmenge, Liefermenge)
-SELECT YEAR(LsKo.Datum) AS Jahr, Standort.Bez AS Produktion, SUM(IIF(LsPoLsKoGru.Reklamation = 1 OR LsKoLsKoGru.Reklamation = 1, ABS(LsPo.Menge), 0)) AS Reklamationsmenge, SUM(IIF(LsPoLsKoGru.Reklamation = 0 OR LsKoLsKoGru.Reklamation = 0, LsPo.Menge, 0)) AS Liefermenge
+INSERT INTO #ReklQuot (Jahr, Geschäftsbereich, Produktion, Reklamationsmenge, Liefermenge)
+SELECT YEAR(LsKo.Datum) AS Jahr, KdGf.KurzBez AS Geschäftsbereich, Standort.Bez AS Produktion, SUM(IIF(LsPoLsKoGru.Reklamation = 1 OR LsKoLsKoGru.Reklamation = 1, ABS(LsPo.Menge), 0)) AS Reklamationsmenge, SUM(IIF(LsPoLsKoGru.Reklamation = 0 OR LsKoLsKoGru.Reklamation = 0, LsPo.Menge, 0)) AS Liefermenge
 FROM LsPo
 JOIN LsKo ON LsPo.LsKoID = LsKo.ID
 JOIN Standort ON LsPo.ProduktionID = Standort.ID
@@ -21,11 +22,15 @@ JOIN LsKoGru AS LsPoLsKoGru ON LsPo.LsKoGruID = LsPoLsKoGru.ID
 JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN Me ON Artikel.MeID = Me.ID
+JOIN Vsa ON LsKo.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 WHERE LsKo.Datum BETWEEN @from AND @to
-  AND Standort.ID IN ($1$)
+  AND Standort.ID IN ($2$)
+  AND KdGf.ID IN ($1$)
   AND Me.IsoCode = N'ST'
-GROUP BY YEAR(LsKo.Datum), Standort.Bez
+GROUP BY YEAR(LsKo.Datum), KdGf.KurzBez, Standort.Bez
 
-SELECT Jahr, Produktion, Liefermenge, Reklamationsmenge, Reklamationsquote = CAST(ROUND(CAST(Reklamationsmenge AS decimal(15, 3)) / CAST(Liefermenge AS decimal(15, 3)) * 100, 4) AS decimal(7, 4))
+SELECT Jahr, Geschäftsbereich, Produktion, Liefermenge, Reklamationsmenge, Reklamationsquote = CAST(ROUND(CAST(Reklamationsmenge AS decimal(15, 3)) / CAST(Liefermenge AS decimal(15, 3)) * 100, 4) AS decimal(7, 4))
 FROM #ReklQuot
-ORDER BY Produktion, Jahr;
+ORDER BY Produktion, Geschäftsbereich, Jahr;
