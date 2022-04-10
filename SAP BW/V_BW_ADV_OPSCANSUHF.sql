beca089fe951cdace8b7abfca0591625f3da6ef9
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-10 14:50:45 +0200

    switched opscans and opteile to new tables

--- SAP BW/V_BW_ADV_OPSCANSUHF.sql
+++ SAP BW/V_BW_ADV_OPSCANSUHF.sql
@@ -1,5 +1,5 @@
 CREATE OR ALTER VIEW sapbw.V_BW_ADV_OPSCANSUHF AS
-SELECT OPTeile.ID AS Chipcode, --(Code wird als Textfeld gesondert geladen... - WTF?)
+SELECT EinzTeil.ID AS Chipcode, /* (Code wird als Textfeld gesondert geladen... - WTF?) */
   ArtikelNr = UPPER(Artikel.ArtikelNr + IIF(ISNULL(ArtGroe.Groesse, N'-') = N'-', N'', N'-' + ArtGroe.Groesse)),
   Auslesezeitpunkt = Poolscan.Zeitpunkt,
   Auslesedatum = CAST(Poolscan.Zeitpunkt AS date),
@@ -7,18 +7,19 @@ SELECT OPTeile.ID AS Chipcode, --(Code wird als Textfeld gesondert geladen... - 
   Einlesedatum = CAST(Poolscan.NextZeitpunkt AS date),
   Vsa.VsaNr,
   Kunden.KdNr
-FROM Salesianer.dbo.OPTeile
-JOIN Salesianer.dbo.ArtGroe ON OPTeile.ArtGroeID = ArtGroe.ID
+FROM Salesianer.dbo.EinzTeil
+JOIN Salesianer.dbo.ArtGroe ON EinzTeil.ArtGroeID = ArtGroe.ID
 JOIN Salesianer.dbo.Artikel ON ArtGroe.ArtikelID = Artikel.ID
 JOIN (
-  SELECT OPScans.OPTeileID, OPScans.Zeitpunkt, LEAD(OPScans.Zeitpunkt) OVER (PARTITION BY OPScans.OPTeileID ORDER BY OPScans.Zeitpunkt) AS NextZeitpunkt, OPScans.AnfPoID, AnfKo.VsaID
-  FROM Salesianer.dbo.OPScans
-  JOIN Salesianer.dbo.AnfPo ON OPScans.AnfPoID = AnfPo.ID
+  SELECT Scans.EinzTeilID, Scans.[DateTime] AS Zeitpunkt, LEAD(Scans.[DateTime]) OVER (PARTITION BY Scans.EinzTeilID ORDER BY Scans.ID ASC) AS NextZeitpunkt, Scans.AnfPoID, AnfKo.VsaID
+  FROM Salesianer.dbo.Scans
+  JOIN Salesianer.dbo.AnfPo ON Scans.AnfPoID = AnfPo.ID
   JOIN Salesianer.dbo.AnfKo ON AnfPo.AnfKoID = AnfKo.ID
-  WHERE OPScans.Menge != 0
-    AND OPScans.ActionsID IN (1, 100, 2, 102)
-    AND OPScans.Zeitpunkt >= N'2019-01-01 00:00:00'
-) AS Poolscan ON Poolscan.OPTeileID = OPTeile.ID
+  WHERE Scans.EinzTeilID > 0  /* nur Poolteile */
+    AND Scans.Menge != 0
+    AND Scans.ActionsID IN (1, 100, 2, 102)
+    AND Scans.[DateTime] >= N'2019-01-01 00:00:00'
+) AS Poolscan ON Poolscan.EinzTeilID = EinzTeil.ID
 JOIN Salesianer.dbo.Vsa ON Poolscan.VsaID = Vsa.ID
 JOIN Salesianer.dbo.Kunden ON Vsa.KundenID = Kunden.ID
 WHERE Poolscan.AnfPoID > 0;
