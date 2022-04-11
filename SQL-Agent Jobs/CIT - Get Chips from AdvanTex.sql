75174b72e13d8bc75dbfd1e95d492032efe42212
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-11 11:05:41 +0200

    restored job from old server and changed table

--- SQL-Agent Jobs/CIT - Get Chips from AdvanTex.sql
+++ SQL-Agent Jobs/CIT - Get Chips from AdvanTex.sql
@@ -1,9 +1,10 @@
 IF OBJECT_ID(N'tempdb..#AdvUHF') IS NOT NULL
   DROP TABLE #AdvUHF;
 
-GO
+IF OBJECT_ID(N'tempdb..#AdvUHF2') IS NOT NULL
+  DROP TABLE #AdvUHF2;
 
-DECLARE @Anlage datetime = DATEADD(day, -7, GETDATE());
+GO
 
 CREATE TABLE #AdvUHF (
   Code nchar(24),
@@ -11,42 +12,59 @@ CREATE TABLE #AdvUHF (
   Groesse nchar(12)
 );
 
+CREATE TABLE #AdvUHF2 (
+  Code nvarchar(33),
+  ArtikelNr nchar(15),
+  Groesse nchar(12)
+);
+
+INSERT INTO #AdvUHF2 (Code, ArtikelNr, Groesse)
+SELECT EinzTeil.Code COLLATE Latin1_General_CI_AS AS Code, Artikel.ArtikelNr COLLATE Latin1_General_CI_AS AS ArtikelNr, ArtGroe.Groesse
+FROM [SALADVPSQLC1A1.salres.com].Salesianer.dbo.EinzTeil
+JOIN [SALADVPSQLC1A1.salres.com].Salesianer.dbo.ArtGroe ON EinzTeil.ArtGroeID = ArtGroe.ID
+JOIN [SALADVPSQLC1A1.salres.com].Salesianer.dbo.Artikel ON ArtGroe.ArtikelID = Artikel.ID
+WHERE EinzTeil.Update_ > DATEADD(hour, -1, GETDATE())
+  AND EinzTeil.ArtikelID > 0;
+
 INSERT INTO #AdvUHF (Code, ArtikelNr, Groesse)
-SELECT EinzTeil.Code, Artikel.ArtikelNr, ArtGroe.Groesse
-FROM [SALADVPSQLC1A1.SALRES.COM].Salesianer.dbo.EinzTeil
-JOIN [SALADVPSQLC1A1.SALRES.COM].Salesianer.dbo.ArtGroe ON EinzTeil.ArtGroeID = ArtGroe.ID
-JOIN [SALADVPSQLC1A1.SALRES.COM].Salesianer.dbo.Artikel ON ArtGroe.ArtikelID = Artikel.ID
-WHERE LEN(EinzTeil.Code) = 24
-  AND EinzTeil.Anlage_ > @Anlage;
+SELECT AdvTeile.Code, AdvTeile.ArtikelNr, AdvTeile.Groesse
+FROM #AdvUHF2 AdvTeile
+WHERE LEN(AdvTeile.Code) = 24
+  AND NOT EXISTS (
+    SELECT SalesianerChip.Sgtin96HexCode
+    FROM LaundryAutomation.dbo.SalesianerChip
+    JOIN LaundryAutomation.dbo.Article ON SalesianerChip.ArticleID = Article.ArticleID
+    WHERE SalesianerChip.Sgtin96HexCode = AdvTeile.Code
+      AND Article.ArticleNumber = AdvTeile.ArtikelNr
+  )
+  AND NOT EXISTS (
+    SELECT Chip.Sgtin96HexCode
+    FROM LaundryAutomation.dbo.Chip
+	  JOIN LaundryAutomation.dbo.Article ON Chip.ArticleID = Article.ArticleID
+    WHERE Chip.Sgtin96HexCode = AdvTeile.Code
+	    AND Article.ArticleNumber = AdvTeile.ArtikelNr
+  );
 
 UPDATE #AdvUHF SET ArtikelNr = ProductSizeConversion.singlesizeproduct
 FROM LaundryAutomation.dbo.ProductSizeConversion
-INNER JOIN #AdvUHF ON (ProductSizeConversion.multisizeproduct = #AdvUHF.Code AND ProductSizeConversion.sizecode = #AdvUHF.Groesse);
+INNER JOIN #AdvUHF ON (ProductSizeConversion.multisizeproduct = #AdvUHF.ArtikelNr AND ProductSizeConversion.sizecode = #AdvUHF.Groesse);
 
 GO
 
-INSERT INTO LaundryAutomation.dbo.SalesianerChip (ArticleID, Sgtin96HexCode, IsEncoded, Created, LastUpdated)
-SELECT Article.ArticleID, AdvUHF.Code, 0 AS IsEncoded, GETDATE() AS Created, GETDATE() AS LastUpdated
-FROM #AdvUHF AS AdvUHF
-JOIN (
-  SELECT a.ArticleID, a.ArticleNumber
-  FROM LaundryAutomation.dbo.Article AS a
-  WHERE a.LastTransmissionDate >= DATEADD(day, -7, GETDATE())
-) AS Article ON Article.ArticleNumber = AdvUHF.ArtikelNr
-WHERE NOT EXISTS (
-    SELECT SalesianerChip.*
-    FROM LaundryAutomation.dbo.SalesianerChip
-    WHERE SalesianerChip.Sgtin96HexCode = AdvUHF.Code
-  )
-  AND NOT EXISTS (
-    SELECT Chip.*
-    FROM LaundryAutomation.dbo.Chip
-    WHERE Chip.Sgtin96HexCode = AdvUHF.Code
-  )
-  AND NOT EXISTS (
-    SELECT Chip.*
-    FROM AdvanTexSync.dbo.Chip
-    WHERE Chip.Sgtin96HexCode = AdvUHF.Code
-  );
+MERGE INTO LaundryAutomation.dbo.SalesianerChip
+USING (
+  SELECT DISTINCT Article.ArticleID, AdvUHF.Code
+  FROM #AdvUHF AS AdvUHF
+  JOIN (
+    SELECT a.ArticleID, a.ArticleNumber
+    FROM LaundryAutomation.dbo.Article AS a
+    WHERE a.LastTransmissionDate >= DATEADD(day, -7, GETDATE())
+  ) AS Article ON Article.ArticleNumber = AdvUHF.ArtikelNr
+) AS AdvChanged ON AdvChanged.Code = SalesianerChip.Sgtin96HexCode
+WHEN MATCHED THEN
+  UPDATE SET ArticleID = AdvChanged.ArticleID, LastUpdated = GETDATE()
+WHEN NOT MATCHED THEN
+  INSERT (ArticleID, Sgtin96HexCode, IsEncoded, Created, LastUpdated)
+  VALUES (AdvChanged.ArticleID, AdvChanged.Code, 0, GETDATE(), GETDATE());
 
 GO
