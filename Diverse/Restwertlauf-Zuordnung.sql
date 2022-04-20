977ec66bc09123bac788529001fcdd452850aa8c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-20 08:31:57 +0200

    initial version

---
+++ Diverse/Restwertlauf-Zuordnung.sql
@@ -0,0 +1,37 @@
+DECLARE @RwLauf TABLE (
+  RwLaufBez nvarchar(40) COLLATE Latin1_General_CS_AS,
+  RwLaufID int,
+  FirmaID int
+);
+
+DECLARE @UserID int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST');
+
+INSERT INTO @RwLauf (RwLaufBez, FirmaID)
+SELECT DISTINCT N'RW-Lauf Firma: ' + Firma.SuchCode AS RwLaufBez, Firma.ID AS FirmaID
+FROM Kunden
+JOIN Firma ON Kunden.FirmaID = Firma.ID
+WHERE Kunden.RWLaufID = -1
+  AND Kunden.Status = N'A'
+  AND Kunden.AdrArtID = 1
+  AND Kunden.ID > 0;
+
+BEGIN TRANSACTION
+  INSERT INTO RwLauf (RWLaufBez, RWLaufBez1, RWLaufBez2, RWLaufBez3, RWLaufBez4, RWLaufBez5, RWLaufBez6, RWLaufBez7, RWLaufBez8, RWLaufBez9, RWLaufBezA, AnlageUserID_, UserID_)
+  SELECT RwLaufBez, RwLaufBez, RwLaufBez, RwLaufBez, RwLaufBez, RwLaufBez, RwLaufBez, RwLaufBez, RwLaufBez, RwLaufBez, RwLaufBez, @UserID, @UserID
+  FROM @RwLauf;
+
+  UPDATE r SET RwLaufID = RwLauf.ID
+  FROM @RwLauf AS r
+  JOIN RwLauf ON RwLauf.RWLaufBez = r.RwLaufBez;
+
+  UPDATE Kunden SET RwLaufID = r.RwLaufID
+  FROM @RwLauf AS r
+  WHERE r.FirmaID = Kunden.FirmaID
+    AND Kunden.RWLaufID = -1
+    AND Kunden.Status = N'A'
+    AND Kunden.AdrArtID = 1
+    AND Kunden.ID > 0;
+
+COMMIT;
+
+GO
