5a65433fdba211eca7cc7a3add0c473fd6a3289e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-21 14:02:33 +0200

    bugfix  - OPScans -> Scans  - OPTeile -> EinzTeile

--- Report/889 - Lieferkontrolle Eigenwäsche.sql
+++ Report/889 - Lieferkontrolle Eigenwäsche.sql
@@ -1,23 +1,21 @@
-BEGIN TRANSACTION;
-  DROP TABLE IF EXISTS #TmpLKontrolle889;
-COMMIT;
+DROP TABLE IF EXISTS #TmpLKontrolle889;
 
-SELECT LsKo.Datum, AnfKo.AuftragsNr AS Packzettel, LsKo.LsNr AS Lieferschein, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, RTRIM(CONVERT(char(10), BKunde.KdNr)) + ' - ' + RTRIM(BKunde.SuchCode) AS [Besitzer Kunde], RTRIM(BVsa.SuchCode) + ' - ' + RTRIM(BVsa.Bez) AS [Besitzer VSA], RTRIM(CONVERT(char(10), GKunde.KdNr)) + ' - ' + RTRIM(GKunde.Suchcode) AS [Geliefert Kunde], RTRIM(GVsa.SuchCode) + ' - ' + RTRIM(GVsa.Bez) AS [Geliefert VSA], COUNT(DISTINCT OPTeile.ID) AS Liefermenge, LsKo.ID AS LsKoID, AnfPo.KdArtiID, CONVERT(bit, IIF(AnfKo.VsaID <> OPTeile.VsaOwnerID, 1, 0)) AS Falsch
+SELECT LsKo.Datum, AnfKo.AuftragsNr AS Packzettel, LsKo.LsNr AS Lieferschein, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, RTRIM(CONVERT(char(10), BKunde.KdNr)) + ' - ' + RTRIM(BKunde.SuchCode) AS [Besitzer Kunde], RTRIM(BVsa.SuchCode) + ' - ' + RTRIM(BVsa.Bez) AS [Besitzer VSA], RTRIM(CONVERT(char(10), GKunde.KdNr)) + ' - ' + RTRIM(GKunde.Suchcode) AS [Geliefert Kunde], RTRIM(GVsa.SuchCode) + ' - ' + RTRIM(GVsa.Bez) AS [Geliefert VSA], COUNT(DISTINCT EinzTeil.ID) AS Liefermenge, LsKo.ID AS LsKoID, AnfPo.KdArtiID, CONVERT(bit, IIF(AnfKo.VsaID <> EinzTeil.VsaOwnerID, 1, 0)) AS Falsch
 INTO #TmpLKontrolle889
-FROM OPScans, AnfPo, AnfKo, LsKo, OPTeile, Artikel, Vsa AS BVsa, Kunden AS BKunde, Vsa AS GVsa, Kunden AS GKunde
-WHERE OPScans.AnfPoID = AnfPo.ID
+FROM Scans, AnfPo, AnfKo, LsKo, EinzTeil, Artikel, Vsa AS BVsa, Kunden AS BKunde, Vsa AS GVsa, Kunden AS GKunde
+WHERE Scans.AnfPoID = AnfPo.ID
   AND AnfPo.AnfKoID = AnfKo.ID
   AND AnfKo.LsKoID = LsKo.ID
-  AND OPScans.OPTeileID = OPTeile.ID
-  AND OPTeile.ArtikelID = Artikel.ID
-  AND OPTeile.VsaOwnerID = BVsa.ID
+  AND Scans.EinzTeilID = EinzTeil.ID
+  AND EinzTeil.ArtikelID = Artikel.ID
+  AND EinzTeil.VsaOwnerID = BVsa.ID
   AND BVsa.KundenID = BKunde.ID
   AND AnfKo.VsaID = GVsa.ID
   AND GVsa.KundenID = GKunde.ID
   AND LsKo.Datum = $1$
-  AND OPTeile.VsaOwnerID > 0
+  AND EinzTeil.VsaOwnerID > 0
   AND GVsa.StandKonID IN ($2$)
-GROUP BY LsKo.Datum, AnfKo.AuftragsNr, LsKo.LsNr, Artikel.ArtikelNr, Artikel.ArtikelBez, RTRIM(CONVERT(char(10), BKunde.KdNr)) + ' - ' + RTRIM(BKunde.SuchCode), RTRIM(BVsa.SuchCode) + ' - ' + RTRIM(BVsa.Bez), RTRIM(CONVERT(char(10), GKunde.KdNr)) + ' - ' + RTRIM(GKunde.Suchcode), RTRIM(GVsa.SuchCode) + ' - ' + RTRIM(GVsa.Bez), LsKo.ID, AnfPo.KdArtiID, CONVERT(bit, IIF(AnfKo.VsaID <> OPTeile.VsaOwnerID, 1, 0));
+GROUP BY LsKo.Datum, AnfKo.AuftragsNr, LsKo.LsNr, Artikel.ArtikelNr, Artikel.ArtikelBez, RTRIM(CONVERT(char(10), BKunde.KdNr)) + ' - ' + RTRIM(BKunde.SuchCode), RTRIM(BVsa.SuchCode) + ' - ' + RTRIM(BVsa.Bez), RTRIM(CONVERT(char(10), GKunde.KdNr)) + ' - ' + RTRIM(GKunde.Suchcode), RTRIM(GVsa.SuchCode) + ' - ' + RTRIM(GVsa.Bez), LsKo.ID, AnfPo.KdArtiID, CONVERT(bit, IIF(AnfKo.VsaID <> EinzTeil.VsaOwnerID, 1, 0));
 
 SELECT Datum, Packzettel, Lieferschein, ArtikelNr, Artikelbezeichnung, [Besitzer Kunde], [Besitzer VSA], [Geliefert Kunde], [Geliefert VSA], Liefermenge, Falsch AS [Falsch Geliefert]
 FROM #TmpLKontrolle889
