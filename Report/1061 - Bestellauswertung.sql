4883e49b32d31a1ac902672ca4643d90668cf9e0
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-26 11:06:00 +0200

    initial version

---
+++ Report/1061 - Bestellauswertung.sql
@@ -0,0 +1,17 @@
+WITH Bestellstatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = N'BKO'
+)
+SELECT Lagerart.Lagerart, Lagerart.LagerartBez AS Lagerartbezeichnung, BKo.BestNr AS [Bestellung Nr.], BKo.Datum AS Bestelldatum, Bestellstatus.StatusBez AS [Status der Bestellung], BPo.Pos AS Positionsnummer, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, BPo.Menge AS [bestellte Menge], BPo.LiefMenge AS [bereits geliefert], IIF(BPo.LiefMenge > BPo.Menge, 0, BPo.Menge - BPo.LiefMenge) AS [noch offen], LiefAbKo.ABNr, LiefAbKo.Datum AS [AB-Datum], LiefAbPo.Menge AS [bestätigte Menge], LiefAbPo.Termin AS [Liefertermin lt. AB], BKo.ID AS BKoID
+FROM BPo
+JOIN ArtGroe ON BPo.ArtGroeID = ArtGroe.ID
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+LEFT JOIN LiefAbKo ON BPo.LatestLiefABKoID = LiefAbKo.ID
+LEFT JOIN LiefAbPo ON LiefAbPo.LiefABKoID = LiefAbKo.ID AND LiefAbPo.BPoID = BPo.ID
+JOIN BKo ON BPo.BKoID = BKo.ID
+JOIN Bestellstatus ON BKo.[Status] = Bestellstatus.[Status]
+JOIN Lagerart ON BKo.LagerArtID = Lagerart.ID
+WHERE BKo.LagerID IN ($1$)
+  AND Bestellstatus.ID IN ($2$)
+ORDER BY Lagerart, [Bestellung Nr.], Positionsnummer;
