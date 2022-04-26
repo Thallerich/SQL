bb8ccc16f7a523226cea3d87d961d05fd7b6a2d9
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-04-26 08:31:23 +0200

    clarified that the quote is a percent value

--- Report/055 - Reklamationsquote.sql
+++ Report/055 - Reklamationsquote.sql
@@ -33,7 +33,7 @@ WHERE LsKo.Datum BETWEEN @from AND @to
   AND Artikel.ArtiTypeID = 1
 GROUP BY YEAR(LsKo.Datum), KdGf.KurzBez, Standort.Bez, Artikel.ID;
 
-SELECT ReklQuot.Jahr, ReklQuot.Geschäftsbereich, ReklQuot.Produktion, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ReklQuot.Liefermenge, ReklQuot.Reklamationsmenge, Reklamationsquote = CAST(ROUND(CAST(ReklQuot.Reklamationsmenge AS float) / CAST(IIF(ReklQuot.Liefermenge = 0, 1, ReklQuot.Liefermenge) AS float) * 100, 4) AS float)
+SELECT ReklQuot.Jahr, ReklQuot.Geschäftsbereich, ReklQuot.Produktion, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ReklQuot.Liefermenge, ReklQuot.Reklamationsmenge, [Reklamationsquote in %] = CAST(ROUND(CAST(ReklQuot.Reklamationsmenge AS float) / CAST(IIF(ReklQuot.Liefermenge = 0, 1, ReklQuot.Liefermenge) AS float) * 100, 4) AS float)
 FROM #ReklQuot AS ReklQuot
 JOIN Artikel ON ReklQuot.ArtikelID = Artikel.ID
 ORDER BY Produktion, Geschäftsbereich, Jahr;
