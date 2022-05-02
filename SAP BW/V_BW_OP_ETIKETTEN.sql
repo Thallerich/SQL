a9c9f186388679fb0d3afe2cab88e6ba1b03702f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-05-02 11:03:17 +0200

    query tuning

--- SAP BW/V_BW_OP_ETIKETTEN.sql
+++ SAP BW/V_BW_OP_ETIKETTEN.sql
@@ -1,35 +1,34 @@
 ALTER VIEW [sapbw].[V_BW_OP_ETIKETTEN] AS
   WITH SetStatus
   AS (
-    SELECT OPEtiKo.ID AS OPEtiKoID, OPEtiKo.EtiNr, COUNT(OPEtiPo.ID) AS Anz, SUM(IIF(CAST(OPEtiKo.AusleseZeitpunkt AS date) = CAST(EinzTeil.LastScanTime AS date), 0, 1)) AS Retour
+    SELECT OPEtiKo.ID AS OPEtiKoID, COUNT(OPEtiPo.ID) AS Anz, SUM(IIF(CAST(OPEtiKo.AusleseZeitpunkt AS date) = CAST(EinzTeil.LastScanTime AS date), 0, 1)) AS Retour
     FROM Salesianer.dbo.OPEtiKo
     JOIN Salesianer.dbo.OPEtiPo ON OPEtiPo.OPEtiKoID = OPEtiKo.ID
     JOIN Salesianer.dbo.EinzTeil ON OPEtiPo.EinzTeilID = EinzTeil.ID
-    WHERE OPEtiKo.AusleseZeitpunkt >= N'2019-01-01 00:00:00'
+    WHERE OPEtiKo.AusleseZeitpunkt > N'2019-01-01 00:00:00'
+      AND OPEtiKo.AusleseZeitpunkt IS NOT NULL
+      AND OPEtiPo.EinzTeilID > 0
     GROUP BY OPEtiko.ID, OPEtiKo.EtiNr
-  ),
-  OPScans AS (
-    SELECT OPEtiPo.ID AS OPEtiPoID, OPEtiKo.ID AS OPEtiKoID, Scans.EinzTeilID, MIN(Scans.[DateTime]) AS Zeitpunkt, OPEtiKo.AusleseZeitpunkt
-    FROM Salesianer.dbo.Scans
-    JOIN Salesianer.dbo.OPEtiPo ON Scans.EinzTeilID = OPEtiPo.EinzTeilID
-    JOIN Salesianer.dbo.OPEtiKo ON OPEtiPo.OPEtiKoID = OPEtiKo.ID
-    WHERE Scans.[DateTime] >= '2019-01-01 00:00:00'
-      AND Scans.ActionsID IN (100, 1)
-      AND Scans.Menge = 1
-      AND Scans.EinzTeilID != -1
-      AND OPEtiKo.AusleseZeitpunkt >= '2019-01-01 00:00:00'
-      AND Scans.[DateTime] > Opetiko.AusleseZeitpunkt
-    GROUP BY OPEtiPo.ID, OPEtiKo.ID, Scans.EinzTeilID, OPEtiKo.Auslesezeitpunkt
   )
-  SELECT OPScans.OPEtiPoID, --> Schlüssel
-    SetStatus.EtiNr, --> Text
+  SELECT OPEtiPo.ID, --> Schlüssel
+    OPEtiKo.EtiNr, --> Text
     --> Status R & U in SAP einschränken
-    OPScans.EinzTeilID AS Chipcode,
+    EinzTeil.ID AS Chipcode,
     --> in Transformation
     SetStatus.Retour,
     IIF(SetStatus.Anz - SetStatus.Retour = 0, 0, 1) AS Statusoffen, -- 0 = Alle Mehrweg retour 1 = fehlende Teile
-    OPScans.Zeitpunkt
-  FROM SetStatus
-  JOIN OPScans ON OPScans.OPEtiKoID = SetStatus.OPEtiKoID;
+    MIN(Scans.[DateTime]) AS Zeitpunkt  -- erstes Einlesen nach dem Auslesen
+  FROM Salesianer.dbo.OPEtiPo
+  JOIN Salesianer.dbo.OPEtiKo ON OPEtiPo.OPEtiKoID = OPEtiKo.ID
+  JOIN Salesianer.dbo.EinzTeil ON OPEtiPo.EinzTeilID = EinzTeil.ID
+  JOIN SetStatus ON SetStatus.OPEtiKoID = OPEtiKo.ID
+  JOIN Salesianer.dbo.Scans ON Scans.EinzTeilID = EinzTeil.ID
+  WHERE OPEtiPo.EinzTeilID > 0
+    AND OPEtiKo.AusleseZeitpunkt IS NOT NULL
+    AND OPEtiKo.AusleseZeitpunkt > N'2019-01-01 00:00:00'
+    AND Scans.ActionsID IN (100, 1)
+    AND Scans.Menge = 1
+    AND Scans.[DateTime] > OPEtiKo.AusleseZeitpunkt
+  GROUP BY OPEtiPo.ID, OPEtiKo.EtiNr, EinzTeil.iD, SetStatus.Retour, IIF(SetStatus.Anz - SetStatus.Retour = 0, 0, 1);
   
 GO
