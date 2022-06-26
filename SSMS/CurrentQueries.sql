34df4728a2a26a034f3e3137e1f0205a2f17ef88
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-06-26 16:59:55 +0200

    added ETA

--- SSMS/CurrentQueries.sql
+++ SSMS/CurrentQueries.sql
@@ -1,4 +1,4 @@
-SELECT process.session_id, process.blocking_session_id, process.start_time, session.program_name, session.host_name, session.login_name, session.login_time, db.name, process.status, process.percent_complete, process.command, sqltext.text
+SELECT process.session_id, process.blocking_session_id, process.start_time, session.program_name, session.host_name, session.login_name, session.login_time, db.name, process.status, process.percent_complete, process.command, sqltext.text, DATEADD(minute, process.estimated_completion_time / (1000 * 60), GETDATE()) AS ETA
 FROM sys.databases db, sys.dm_exec_sessions session, sys.dm_exec_requests process
 CROSS APPLY sys.dm_exec_sql_text(process.sql_handle) sqltext
 WHERE process.database_id = db.database_id
