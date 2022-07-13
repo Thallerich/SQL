6a69967f862bc342e6d9a04b40caa7be02794bf5
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-07-13 12:48:00 +0200

    IT-60991 initial version

---
+++ Rechnungsanlagen/Bekleidungsautomat - Entnahmen zu Rechnung.sql
@@ -0,0 +1,17 @@
+SELECT Scans.DateTime AS [Datum der Entnahme], EntnahmeTraeger.PersNr, EntnahmeTraeger.Traeger AS TraegerNr, EntnahmeTraeger.Nachname, EntnahmeTraeger.Vorname, Abteil.Abteilung AS KsSt, Abteil.Bez AS Kostenstelle, Teile.Barcode, Teile.RentomatChip AS Chipcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Kunden.KdNr, Kunden.SuchCode AS Kunde, IIF(Teile.Ausdienst IS NULL, Teile.RestwertInfo, Teile.AusdRestw) AS Restwert
+FROM Scans
+JOIN LsPo ON Scans.LsPoID = LsPo.ID
+JOIN RechPo ON LsPo.RechPoID = RechPo.ID
+JOIN Teile ON Scans.TeileID = Teile.ID
+JOIN TraeArti ON Teile.TraeArtiID = TraeArti.ID
+JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
+JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
+JOIN Traeger AS EntnahmeTraeger ON Scans.LastPoolTraegerID = EntnahmeTraeger.ID
+JOIN Abteil ON EntnahmeTraeger.AbteilID = Abteil.ID
+WHERE RechPo.RechKoID = $RECHKOID$
+  AND Traeger.RentoArtID IN (SELECT RentoArt.ID FROM RentoArt WHERE RentoArt.Code IN (N'S', N'T'))
+  AND Scans.ActionsID = 65;
