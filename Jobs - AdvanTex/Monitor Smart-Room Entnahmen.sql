e811e36beef6a28e5630cc8cc52696691073f8e8
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-07-27 08:27:08 +0200

    select from previous day

--- Jobs - AdvanTex/Monitor Smart-Room Entnahmen.sql
+++ Jobs - AdvanTex/Monitor Smart-Room Entnahmen.sql
@@ -1,5 +1,6 @@
-DECLARE @daystart datetime2 = DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), DAY(GETDATE()));
-DECLARE @dayend datetime2 = DATEADD(day, 1, DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), DAY(GETDATE())));
+DECLARE @previousday datetime2 = DATEADD(day, -1, CAST(GETDATE() AS date));
+DECLARE @daystart datetime2 = DATEFROMPARTS(YEAR(@previousday), MONTH(@previousday), DAY(@previousday));
+DECLARE @dayend datetime2 = DATEADD(day, 1, DATEFROMPARTS(YEAR(@previousday), MONTH(@previousday), DAY(@previousday)));
 DECLARE @sqltext nvarchar(max);
 
 SET @sqltext = N'SELECT Teile.Barcode, Teile.RentomatChip, Scans.[DateTime] AS Entnahmezeitpunkt, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, ISNULL(Traeger.Nachname, N'''') + N'' '' + ISNULL(Traeger.Vorname, N'''') + N'' ('' + Traeger.Traeger + N'')'' AS [entnommen von]
