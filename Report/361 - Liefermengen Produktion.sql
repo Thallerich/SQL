890d2b32b8cc1b93e7e0b7588c52da2f92c8c5f4
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-08-10 13:03:40 +0200

    IT-61902  - added column "Expedition"  - added column "Serviceart"

--- Report/361 - Liefermengen Produktion.sql
+++ Report/361 - Liefermengen Produktion.sql
@@ -1,10 +1,10 @@
-SELECT Firma.Bez AS Firma, Standort.Bez AS Produzent, StandKon.StandKonBez$LAN$ AS [Standort-Konfiguration], KdGf.KurzBez AS SGF, KdGf.KdGfBez$LAN$ AS [Geschäftsfeld], Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT') AS Monat, SUM(LsPo.Menge) AS Menge, Artikel.StueckGewicht AS [Gewicht pro Stück], Artikel.Packmenge AS [VPE-Menge], ME.MeBez$LAN$ AS [VPE], ROUND(SUM(LsPo.Menge * Artikel.StueckGewicht), 2) AS Liefergewicht
-FROM LsPo, LsKo, Vsa, Kunden, Firma, Standort, StandKon, KdGf, KdArti, Artikel, KdBer, Bereich, ME
+SELECT Firma.Bez AS Firma, Produktion.Bez AS Produzent, Expedition.Bez AS Expedition, ServType.ServTypeBez$LAN$ AS Serviceart, StandKon.StandKonBez$LAN$ AS [Standort-Konfiguration], KdGf.KurzBez AS SGF, KdGf.KdGfBez$LAN$ AS [Geschäftsfeld], Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT') AS Monat, SUM(LsPo.Menge) AS Menge, Artikel.StueckGewicht AS [Gewicht pro Stück], Artikel.Packmenge AS [VPE-Menge], ME.MeBez$LAN$ AS [VPE], ROUND(SUM(LsPo.Menge * Artikel.StueckGewicht), 2) AS Liefergewicht
+FROM LsPo, LsKo, Vsa, Kunden, Firma, Standort AS Produktion, StandKon, KdGf, KdArti, Artikel, KdBer, Bereich, ME, Fahrt, Touren, Standort AS Expedition, ServType
 WHERE LsPo.LsKoID = LsKo.ID
   AND LsKo.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
   AND Kunden.FirmaID = Firma.ID
-  AND LsPo.ProduktionID = Standort.ID
+  AND LsPo.ProduktionID = Produktion.ID
   AND Vsa.StandKonID = StandKon.ID
   AND Kunden.KdGfID = KdGf.ID
   AND LsPo.KdArtiID = KdArti.ID
@@ -12,10 +12,14 @@ WHERE LsPo.LsKoID = LsKo.ID
   AND KdArti.KdBerID = KdBer.ID
   AND KdBer.BereichID = Bereich.ID
   AND Artikel.MEID = ME.ID
+  AND LsKo.FahrtID = Fahrt.ID
+  AND Fahrt.TourenID = Touren.ID
+  AND Touren.ExpeditionID = Expedition.ID
+  AND Vsa.ServTypeID = ServType.ID
   AND Kunden.KdGfID IN ($2$)
-  AND Standort.ID IN ($3$)
+  AND Produktion.ID IN ($3$)
   AND LsKo.Datum BETWEEN $STARTDATE$ AND $ENDDATE$
   AND Artikel.ID > 0
   AND LsPo.Menge != 0
   AND Artikel.ArtiTypeID = 1
-GROUP BY Firma.Bez, Standort.Bez, StandKon.StandKonBez$LAN$, KdGf.KurzBez, KdGf.KdGfBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Bereich.BereichBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT'), Artikel.StueckGewicht, Artikel.Packmenge, ME.MeBez$LAN$;
+GROUP BY Firma.Bez, Produktion.Bez, Expedition.Bez, ServType.ServTypeBez$LAN$, StandKon.StandKonBez$LAN$, KdGf.KurzBez, KdGf.KdGfBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Bereich.BereichBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT'), Artikel.StueckGewicht, Artikel.Packmenge, ME.MeBez$LAN$;
