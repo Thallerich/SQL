8a9aa03873b32401606a30b977d6e8d5e7e1c1dd
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-09-05 10:08:54 +0200

    changes after "faktura"-workshop  - filter out customers 2300, 6060 (hardcoded)  - added column for creation of delivery not position  - added column for print time of delivery note

--- Report/859 - Lieferschein-Position nicht berechnet.sql
+++ Report/859 - Lieferschein-Position nicht berechnet.sql
@@ -1,12 +1,11 @@
-WITH LsKoStatus AS (
+SELECT DISTINCT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, Zone.ZonenCode AS Vertriebszone, Kunden.KdNr, Kunden.SuchCode, DrLauf.Bez AS Drucklauf, BrLauf.BrLaufBez$LAN$ AS Bearbeitungsrechnungslauf, Kunden.MonatAbgeschl AS [letzter abgeschlossener Monat], LsKo.ID AS LsKoID, LsKo.LsNr, LsKo.Datum, LsKoStatus.StatusBez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, LsPo.Menge, LsPo.EPreis, KdArti.WaschPreis AS [Bearbeitungspreis Kundenartikel], LsPo.Anlage_ AS [Position erstellt], LsKo.DruckZeitpunkt AS [Lieferschein gedruckt]
+FROM LsPo
+JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+JOIN (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'LSKO')
-)
-SELECT DISTINCT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, Zone.ZonenCode AS Vertriebszone, Kunden.KdNr, Kunden.SuchCode, DrLauf.Bez AS Drucklauf, BrLauf.BrLaufBez$LAN$ AS Bearbeitungsrechnungslauf, Kunden.MonatAbgeschl AS [letzter abgeschlossener Monat], LsKo.ID AS LsKoID, LsKo.LsNr, LsKo.Datum, LsKoStatus.StatusBez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, LsPo.Menge, LsPo.EPreis, KdArti.WaschPreis AS [Bearbeitungspreis Kundenartikel]
-FROM LsPo
-JOIN LsKo ON LsPo.LsKoID = LsKo.ID
-JOIN LsKoStatus ON LsKo.[Status] = LsKoStatus.[Status]
+) AS LsKoStatus ON LsKo.[Status] = LsKoStatus.[Status]
 JOIN Vsa ON LsKo.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
@@ -23,4 +22,5 @@ WHERE Firma.ID IN ($2$)
   AND BrLauf.ID IN ($5$)
   AND LsPo.RechPoID = -1
   AND LsPo.Menge * IIF(LsPo.EPreis != 0, LsPo.EPreis, KdArti.WaschPreis) != 0
-  AND LsPo.Kostenlos = 0;
+  AND LsPo.Kostenlos = 0
+  AND Kunden.KdNr NOT IN (2300, 6060) /* Ausnahme lt. SvobKu - Kunden mit Berufsgruppen-Faktura, Rechnung wird hier immer storniert! */;
