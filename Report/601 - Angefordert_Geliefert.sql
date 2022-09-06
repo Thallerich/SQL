442b6b2bc4eb0dc7e695155151af5f8dee308bc0
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-09-06 13:19:02 +0200

    IT-62748   - added "geschäftsbereich", if "show customers" is activated

--- Report/601 - Angefordert_Geliefert.sql
+++ Report/601 - Angefordert_Geliefert.sql
@@ -1,9 +1,10 @@
-SELECT AnfKo.LieferDatum, IIF($2$ = 1, Kunden.KdNr, NULL) AS KdNr, IIF($2$ = 1, Kunden.SuchCode, NULL) AS Kunde, IIF($3$ = 1, Vsa.Bez, NULL) AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, SUM(AnfPo.Angefordert) AS Angefordert, SUM(AnfPo.Geliefert) AS Geliefert, SUM(AnfPo.Angefordert - AnfPo.Geliefert) AS Gestrichen
-FROM AnfPo, AnfKo, VSA, Kunden, KdArti, KdBer, Artikel, StandKon, StandBer
+SELECT AnfKo.LieferDatum, IIF($2$ = 1, KdGf.KurzBez, NULL) AS Geschäftsbereich, IIF($2$ = 1, Kunden.KdNr, NULL) AS KdNr, IIF($2$ = 1, Kunden.SuchCode, NULL) AS Kunde, IIF($3$ = 1, Vsa.Bez, NULL) AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, SUM(AnfPo.Angefordert) AS Angefordert, SUM(AnfPo.Geliefert) AS Geliefert, SUM(AnfPo.Angefordert - AnfPo.Geliefert) AS Gestrichen
+FROM AnfPo, AnfKo, VSA, Kunden, KdGf, KdArti, KdBer, Artikel, StandKon, StandBer
 WHERE (AnfPo.Angefordert > 0 OR AnfPo.Geliefert > 0)
   AND AnfPo.AnfKoID = AnfKo.ID
   AND AnfKo.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
+  AND Kunden.KdGfID = KdGf.ID
   AND AnfPo.KdArtiID = KdArti.ID
   AND KdArti.ArtikelID = Artikel.ID
   AND KdArti.KdBerID = KdBer.ID
@@ -13,5 +14,5 @@ WHERE (AnfPo.Angefordert > 0 OR AnfPo.Geliefert > 0)
   AND StandBer.ProduktionID IN ($5$)
   AND LieferDatum BETWEEN $STARTDATE$ AND $ENDDATE$
   AND Artikel.BereichID IN ($6$)
-GROUP BY AnfKo.LieferDatum, IIF($2$ = 1, Kunden.KdNr, NULL), IIF($2$ = 1, Kunden.SuchCode, NULL), IIF($3$ = 1, Vsa.Bez, NULL), Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$
+GROUP BY AnfKo.LieferDatum, IIF($2$ = 1, KdGf.KurzBez, NULL), IIF($2$ = 1, Kunden.KdNr, NULL), IIF($2$ = 1, Kunden.SuchCode, NULL), IIF($3$ = 1, Vsa.Bez, NULL), Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$
 ORDER BY Artikel.ArtikelNr, AnfKo.LieferDatum;
