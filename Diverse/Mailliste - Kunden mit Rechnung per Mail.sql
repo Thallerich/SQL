5617b28618bd48db300fca8a8299a7065878baf3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-09-07 14:26:17 +0200

    amended for SAP data export

--- Diverse/Mailliste - Kunden mit Rechnung per Mail.sql
+++ Diverse/Mailliste - Kunden mit Rechnung per Mail.sql
@@ -1,29 +1,14 @@
-USE Wozabal
-GO
-
 SELECT 
-  KdGf.KurzBez AS SGF, 
   Kunden.KdNr, 
-  Kunden.SuchCode AS Kunde, 
-  Kunden.Name1, 
-  Kunden.Name2,
-  Kunden.Name3,
-  Kunden.Strasse,
-  Kunden.Land,
-  Kunden.PLZ,
-  Kunden.Ort,
-  Sachbear.Vorname, 
-  Sachbear.Name, 
-  Sachbear.SerienAnrede, 
-  IIF(RKoOut.ID = 5, Kunden.Website, Sachbear.eMail) AS eMail, 
-  RKoOut.RkoOutBez AS Ausgabeart
+  Kunden.Debitor, 
+  Sachbear.eMail
 FROM Kunden
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 JOIN RKoOut ON Kunden.RKoOutID = RKoOut.ID
-LEFT OUTER JOIN RKoMail ON RKoMail.TableID = Kunden.ID AND RKoMail.TableName = N'KUNDEN' AND RKoMail.FieldName = 'AUTOEMAIL'
-LEFT OUTER JOIN Sachbear ON RKoMail.SachbearID = Sachbear.ID
-WHERE (RKoOut.EMailVersand = 1 OR RKoOut.ID = 5)  --ID 5 = digital signierte Rechnung über DIG
+JOIN RKoMail ON RKoMail.TableID = Kunden.ID AND RKoMail.TableName = N'KUNDEN' AND RKoMail.FieldName = 'AUTOEMAIL'
+JOIN Sachbear ON RKoMail.SachbearID = Sachbear.ID
+WHERE RKoOut.EMailVersand = 1
   AND Kunden.Status = N'A'
-  AND KdGf.KurzBez = N'IG'
+  AND Kunden.AdrArtID = 1;
 
 GO
