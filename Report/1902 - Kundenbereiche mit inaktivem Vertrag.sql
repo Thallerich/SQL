317c08d02dd96e6cfe220a537fbe4bb33c27943d
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-09-07 14:56:13 +0200

    Invoicing - reporting: new report as per mail 2022-09-05 (PeisPe)

---
+++ Report/1902 - Kundenbereiche mit inaktivem Vertrag.sql
@@ -0,0 +1,10 @@
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Vertrag.Nr AS [Vertrag Nr.], Vertrag.Bez AS Vertragsbezeichung, Vertrag.Status
+FROM KdBer
+JOIN Bereich ON KdBer.BereichID = Bereich.ID
+JOIN Kunden ON KdBer.KundenID = Kunden.ID
+JOIN Vertrag ON KdBer.VertragID = Vertrag.ID
+WHERE Kunden.Status = N'A'
+  AND Kunden.AdrArtID = 1
+  AND KdBer.Status = N'A'
+  AND ISNULL(Vertrag.Status, N'I') = N'I'
+  AND Kunden.FirmaID = $1$;
