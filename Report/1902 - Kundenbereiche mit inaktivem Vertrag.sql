10218636efa5770ca7b1b43b32f31d1a1730433c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-09-07 15:26:22 +0200

    added feature  - added jump point for customer article and contract

--- Report/1902 - Kundenbereiche mit inaktivem Vertrag.sql
+++ Report/1902 - Kundenbereiche mit inaktivem Vertrag.sql
@@ -1,4 +1,4 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Vertrag.Nr AS [Vertrag Nr.], Vertrag.Bez AS Vertragsbezeichung, Vertrag.Status
+SELECT KdBer.ID AS KdBerID, Vertrag.ID AS VertragID, Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Vertrag.Nr AS [Vertrag Nr.], Vertrag.Bez AS Vertragsbezeichung, Vertrag.Status
 FROM KdBer
 JOIN Bereich ON KdBer.BereichID = Bereich.ID
 JOIN Kunden ON KdBer.KundenID = Kunden.ID
