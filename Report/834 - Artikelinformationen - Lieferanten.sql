278fa797ceba30241b75b89e5da59b99d542efee
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-10-18 09:25:45 +0200

    IT-63953 added filter by customer main location

--- Report/834 - Artikelinformationen - Lieferanten.sql
+++ Report/834 - Artikelinformationen - Lieferanten.sql
@@ -1,16 +1,20 @@
-SELECT [Status].StatusBez$LAN$ AS Artikelstatus, Artikel.ArtikelNr AS [ArtikelNr Wozabal], Artikel.ArtikelBez$LAN$ AS [Artikelbezeichnung Wozabal], IIF(Farbe1.ID < 0, NULL, Farbe1.FarbeBez$LAN$) AS Farbe1, IIF(Farbe2.ID < 0, NULL, Farbe2.FarbeBez$LAN$) AS Farbe2, ISNULL(ArtGroe.BestNr, Artikel.BestNr) AS [ArtikelNr Lieferant], ArtGroe.Groesse, ArtGroe.EKPreis, ME.MEBez$LAN$ AS Mengeneinheit, IIF(Lief.ID > 0, Lief.LiefNr, Lief2.LiefNr) AS Lieferantennummer, IIF(Lief.ID > 0, Lief.Name1, Lief2.Name1) AS Lieferantenname, ArtMisch.ArtMischBez$LAN$ AS Gewebe, Artikel.StueckGewicht AS [Artikelgewicht (kg/Stück)]
-FROM Artikel, ArtGroe, ME, Lief, Lief AS Lief2, ArtMisch, [Status], Farbe AS Farbe1, Farbe AS Farbe2
-WHERE ArtGroe.ArtikelID = Artikel.ID
-  AND ArtGroe.LiefID = Lief.ID
-  AND Artikel.LiefID = Lief2.ID
-  AND Artikel.MEID = ME.ID
-  AND Artikel.ArtMischID = ArtMisch.ID
-  AND Artikel.FarbeID = Farbe1.ID
-  AND Artikel.Farbe2ID = Farbe2.ID
-  AND Artikel.[Status] = [Status].[Status]
-  AND [Status].Tabelle = 'ARTIKEL'
-  AND Artikel.BereichID IN ($1$)
-  AND [Status].ID IN ($2$)
+WITH Artikelstatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = N'ARTIKEL'
+)
+SELECT Artikelstatus.StatusBez AS Artikelstatus, Artikel.ArtikelNr AS ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, IIF(Farbe1.ID < 0, NULL, Farbe1.FarbeBez$LAN$) AS Farbe1, IIF(Farbe2.ID < 0, NULL, Farbe2.FarbeBez$LAN$) AS Farbe2, Coalesce(ArtGroe.BestNr, Artikel.BestNr) AS [ArtikelNr Lieferant], ArtGroe.Groesse AS Größe, ArtGroe.EKPreis, ME.MEBez$LAN$ AS Mengeneinheit, IIF(Lief.ID > 0, Lief.LiefNr, Lief2.LiefNr) AS Lieferantennummer, IIF(Lief.ID > 0, Lief.Name1, Lief2.Name1) AS Lieferantenname, ArtMisch.ArtMischBez$LAN$ AS Gewebe, Artikel.StueckGewicht AS [Artikelgewicht (kg/Stück)]
+FROM Artikel
+JOIN ArtGroe ON ArtGroe.ArtikelID = Artikel.ID
+JOIN ME ON Artikel.MeID = ME.ID
+JOIN Lief ON ArtGroe.LiefID = Lief.ID
+JOIN Lief AS Lief2 ON Artikel.LiefID = Lief2.ID
+JOIN ArtMisch ON Artikel.ArtMischID = ArtMisch.ID
+JOIN Artikelstatus ON Artikel.[Status] = Artikelstatus.[Status]
+JOIN Farbe AS Farbe1 ON Artikel.FarbeID = Farbe1.ID
+JOIN Farbe AS Farbe2 ON Artikel.Farbe2ID = Farbe2.ID
+WHERE Artikel.BereichID IN ($1$)
+  AND Artikelstatus.ID IN ($2$)
   AND EXISTS (
     SELECT KdArti.ID
     FROM KdArti
@@ -18,5 +22,6 @@ WHERE ArtGroe.ArtikelID = Artikel.ID
     JOIN KdGf ON Kunden.KdGfID = KdGf.ID
     WHERE KdArti.ArtikelID = Artikel.ID
       AND KdGf.ID IN ($3$)
+      AND Kunden.StandortID IN ($4$)
   )
 ORDER BY Artikel.ArtikelNr;
