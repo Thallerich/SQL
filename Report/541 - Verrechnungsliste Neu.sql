235c41594a8c0d92ef8f04f5b08e98d78f12b990
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-10-21 10:30:34 +0200

    IT-64367 added "Personalnummer" column

--- Report/541 - Verrechnungsliste Neu.sql
+++ Report/541 - Verrechnungsliste Neu.sql
@@ -1,16 +1,15 @@
-SELECT Traeger.Nachname, Traeger.Vorname, Abteil.Bez AS Kostenstelle, KdArti.VariantBez AS Berufsgruppe, Traeger.Indienst AS [Indienststellungswoche Träger], 1 AS Menge, KdArti.LeasPreis AS Periodenpreis
-FROM KdArti, Abteil, Traeger, Kunden, TraeArti, TraeArch, Wochen
-WHERE TraeArch.WochenID = Wochen.ID
-	AND Wochen.Woche = $2$
-	AND Kunden.ID = $1$
-	AND TraeArch.TraeArtiID = TraeArti.ID
-	AND KdArti.ID = TraeArti.KdArtiID
+SELECT Traeger.PersNr AS Personalnummer, Traeger.Nachname, Traeger.Vorname, Abteil.Bez AS Kostenstelle, KdArti.VariantBez AS Berufsgruppe, Traeger.Indienst AS [Indienststellungswoche Träger], 1 AS Menge, KdArti.LeasPreis AS Periodenpreis
+FROM TraeArch
+JOIN TraeArti ON TraeArch.TraeArtiID = TraeArti.ID
+JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
+JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
+JOIN Abteil ON TraeArch.AbteilID = Abteil.ID
+JOIN Wochen ON TraeArch.WochenID = Wochen.ID
+WHERE Wochen.Woche = $2$
+	AND Abteil.KundenID = $1$
 	AND KdArti.ArtikelID = (
-		SELECT CONVERT(integer, ValueMemo)
+		SELECT CAST(Settings.ValueMemo AS int)
 		FROM Settings
 		WHERE Parameter = N'ID_ARTIKEL_BERUFSGRUPPE'
 	)
-	AND Abteil.ID = TraeArch.AbteilID
-	AND Traeger.ID = TraeArti.TraegerID
-	AND Abteil.KundenID = Kunden.ID
 ORDER BY Kostenstelle, Berufsgruppe;
