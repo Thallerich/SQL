16b47e6fd57f9843fd68c5d71f662cb85d83d8e3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-11-09 09:35:15 +0100

    added script to add missing rows

--- Diverse/Kontierungslogik - Bereinigung SAP-Schnittstelle.sql
+++ Diverse/Kontierungslogik - Bereinigung SAP-Schnittstelle.sql
@@ -38,3 +38,72 @@ WHERE RPoType.ID > 0
   AND Bereich.ID = @BereichID;
 
 GO
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Fehlende Datensätze nachtragen                                                                                            ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+DECLARE @FirmaID int = (SELECT ID FROM Firma WHERE SuchCode = N'FA14');
+DECLARE @BereichID int = (SELECT ID FROM Bereich WHERE Bereich = N'PWS');
+DECLARE @BewohnerRechnungID int = (SELECT ID FROM RKoType WHERE RKoTypeBez = N'Bewohnerwäsche');
+DECLARE @MonatsRechnungID int = (SELECT ID FROM RKoType WHERE RKoTypeBez = N'Monatsrechnung');
+DECLARE @DummyKonto int = (SELECT ID FROM Konten WHERE Konto = N'SAP');
+DECLARE @UserID int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST');
+
+DELETE FROM RPoKonto WHERE KontenID = -1;
+
+WITH Firmen AS (
+  SELECT Firma.ID
+  FROM Firma
+  WHERE Status = N'A'
+    AND EXISTS (
+      SELECT RechNrKr.*
+      FROM RechNrKr
+      WHERE RechNrKr.FirmaID = Firma.ID
+    )
+)
+INSERT INTO RPoKonto (BereichID, ArtGruID, BrancheID, RPoTypeID, FirmaID, KdGfID, MWStID, Art, Bez, KontenID, RKoTypeID, AnlageUserID_, UserID_)
+SELECT Bereich.ID AS BereichID, -1 AS ArtGruID, -1 AS BrancheID, RPoType.ID AS RPoTypeID, Firmen.ID AS FirmaID, -1 AS KdGfID, MwSt.ID AS MwStID, N'B' AS Art, N'Dummy' AS Bez, @DummyKonto AS KontenID, @MonatsRechnungID AS RKoTypeID, @UserID AS AnlageUserID_, @UserID AS UserID_
+FROM RPoType
+CROSS JOIN MwSt
+CROSS JOIN Bereich
+CROSS JOIN Firmen
+WHERE RPoType.ID > 0
+  AND MwSt.ID > 0
+  AND Bereich.ID > 0
+  AND ((Bereich.ID != @BereichID AND Firmen.ID = @FirmaID) OR (Firmen.ID != @FirmaID))
+  AND NOT EXISTS (
+    SELECT r.*
+    FROM RPoKonto r
+    WHERE r.BereichID = Bereich.ID
+      AND r.RPoTypeID = RPoType.ID
+      AND r.BrancheID = -1
+      AND r.FirmaID = Firmen.ID
+      AND r.KdGfID = -1
+      AND r.MWStID = MwSt.ID
+      AND r.Art = N'B'
+      AND r.ArtGruID = -1
+  );
+
+INSERT INTO RPoKonto (BereichID, ArtGruID, BrancheID, RPoTypeID, FirmaID, KdGfID, MWStID, Art, Bez, KontenID, RKoTypeID, AnlageUserID_, UserID_)
+SELECT Bereich.ID AS BereichID, -1 AS ArtGruID, -1 AS BrancheID, RPoType.ID AS RPoTypeID, @FirmaID AS FirmaID, -1 AS KdGfID, MwSt.ID AS MwStID, N'B' AS Art, N'Dummy' AS Bez, @DummyKonto AS KontenID, @BewohnerRechnungID AS RKoTypeID, @UserID AS AnlageUserID_, @UserID AS UserID_
+FROM RPoType
+CROSS JOIN MwSt
+CROSS JOIN Bereich
+WHERE RPoType.ID > 0
+  AND MwSt.ID > 0
+  AND Bereich.ID = @BereichID
+  AND NOT EXISTS (
+    SELECT r.*
+    FROM RPoKonto r
+    WHERE r.BereichID = Bereich.ID
+      AND r.RPoTypeID = RPoType.ID
+      AND r.BrancheID = -1
+      AND r.FirmaID = @FirmaID
+      AND r.KdGfID = -1
+      AND r.MWStID = MwSt.ID
+      AND r.Art = N'B'
+      AND r.ArtGruID = -1
+  );
+
+GO
