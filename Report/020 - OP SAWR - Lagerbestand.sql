5c0eeb3a18631246d4bc3ace9aa5aef5ab33c6b6
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-11-14 16:06:56 +0100

    bugfix [9.50] table structure changed

--- Report/020 - OP SAWR - Lagerbestand.sql
+++ Report/020 - OP SAWR - Lagerbestand.sql
@@ -3,13 +3,13 @@ WITH Artikelstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'Artikel')
 )
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikelstatus.StatusBez AS [Status Artikel], ArtGroe.Groesse AS Größe, COUNT(OPTeile.ID) AS [Bestand gesamt], SUM(IIF(OPTeile.AnzWasch <= 1, 1, 0)) AS [Bestand neu], SUM(IIF(OPTeile.AnzWasch > 1, 1, 0)) AS [Bestand gebraucht]
-FROM OPTeile
-JOIN ArtGroe ON OPTeile.ArtGroeID = ArtGroe.ID
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikelstatus.StatusBez AS [Status Artikel], ArtGroe.Groesse AS Größe, COUNT(EinzTeil.ID) AS [Bestand gesamt], SUM(IIF(EinzTeil.AnzWasch <= 1, 1, 0)) AS [Bestand neu], SUM(IIF(EinzTeil.AnzWasch > 1, 1, 0)) AS [Bestand gebraucht]
+FROM EinzTeil
+JOIN ArtGroe ON EinzTeil.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
 JOIN GroePo ON GroePo.GroeKoID = Artikel.GroeKoID AND GroePo.Groesse = ArtGroe.Groesse
 JOIN Artikelstatus ON Artikelstatus.[Status] = Artikel.[Status]
-WHERE OPTeile.ZielNrID = 100070077
-  AND OPTeile.Status IN (N'A', N'Q')
+WHERE EinzTeil.ZielNrID = 100070077
+  AND EinzTeil.Status IN (N'A', N'Q')
 GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez, Artikelstatus.StatusBez, ArtGroe.Groesse, GroePo.Folge
 ORDER BY ArtikelNr, GroePo.Folge;
