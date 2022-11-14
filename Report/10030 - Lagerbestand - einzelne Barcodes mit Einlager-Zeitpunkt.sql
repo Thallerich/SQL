34fde0a3dda03e7ce4a77d0ca2fc0d5cfa76f311
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-11-14 16:05:08 +0100

    bugfix [9.50] table structure changed

--- Report/10030 - Lagerbestand - einzelne Barcodes mit Einlager-Zeitpunkt.sql
+++ Report/10030 - Lagerbestand - einzelne Barcodes mit Einlager-Zeitpunkt.sql
@@ -1,10 +1,12 @@
-SELECT TeileLag.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse AS Größe, TeileLag.Anlage_ AS [Zeitpunkt Einbuchung], Lagerart.LagerartBez$LAN$ AS Lagerart, Lagerart.Neuwertig, Lager.Bez AS Lagerstandort, Lagerort.Lagerort, Kunden.KdNr AS [KdNr letzter Kunde], Kunden.SuchCode AS [letzter Kunde]
-FROM TeileLag
-JOIN Lagerart ON TeileLag.LagerartID = Lagerart.ID
+SELECT EinzHist.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS [Zeitpunkt Einbuchung], Lagerart.LagerartBez$LAN$ AS Lagerart, Lagerart.Neuwertig, Lager.Bez AS Lagerstandort, Lagerort.Lagerort, Kunden.KdNr AS [KdNr letzter Kunde], Kunden.SuchCode AS [letzter Kunde]
+FROM EinzHist
+JOIN Lagerart ON EinzHist.LagerartID = Lagerart.ID
 JOIN Standort AS Lager ON Lagerart.LagerID = Lager.ID
-JOIN ArtGroe ON TeileLag.ArtGroeID = ArtGroe.ID
+JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
-JOIN Kunden ON TeileLag.KundenID = Kunden.ID
-JOIN Lagerort ON TeileLag.LagerortID = Lagerort.ID
+JOIN Kunden ON EinzHist.KundenID = Kunden.ID
+JOIN Lagerort ON EinzHist.LagerortID = Lagerort.ID
 WHERE Lager.ID IN ($1$)
-  AND TeileLag.Status IN (N'L', N'R');
+  AND EinzHist.Status IN (N'X', N'XE')
+  AND EinzHist.EinzHistTyp = 2
+  AND EinzHist.IsCurrEinzHist = 1;
