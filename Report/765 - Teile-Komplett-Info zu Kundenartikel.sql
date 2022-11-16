c75468c484241c4658e53a3338ceae3a2a188c84
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-11-16 15:19:00 +0100

    bugfix StatusBez - alias should not be language specific :P

--- Report/765 - Teile-Komplett-Info zu Kundenartikel.sql
+++ Report/765 - Teile-Komplett-Info zu Kundenartikel.sql
@@ -23,14 +23,14 @@ END;
 
 SET @SQL = N'
 WITH Teilestatus AS (
-  SELECT [Status].ID, [Status].[Status], [Status].StatusBez AS StatusBez$LAN$
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
-  WHERE [Status].Tabelle = UPPER(N''TEILE'')
+  WHERE [Status].Tabelle = UPPER(N''EINZHIST'')
 )
-SELECT Kunden.ID AS KundenID, Kunden.KdNr, Vsa.ID AS VsaID, Vsa.VsaNr, Traeger.ID AS TraegerID, Traeger.Traeger AS TraegerNr, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.ID AS ArtGroeID, ArtGroe.Groesse, Teile.Barcode, Teilestatus.StatusBez AS Teilestatus, Teile.Indienst, Teile.Ausdienst, Teile.Ausgang1, Teile.RuecklaufK AS WaschzyklenKunde, Teile.TeileSchrankInfo AS SchrankFach, IIF(Teile.TeileSchrankInfo IS NULL, 0, 1) AS HatSchrank, IIF(Teile.Status IN (''Q'', ''S'', ''U'', ''W''), 1, 0) AS Istmenge, IIF(Teile.Status <= ''Q'' AND Teile.Status >= ''A'', 1, 0) AS Sollmenge
-FROM Teile
-JOIN Teilestatus ON Teile.Status = Teilestatus.Status
-JOIN TraeArti ON Teile.TraeArtiID = TraeArti.ID
+SELECT Kunden.ID AS KundenID, Kunden.KdNr, Vsa.ID AS VsaID, Vsa.VsaNr, Traeger.ID AS TraegerID, Traeger.Traeger AS TraegerNr, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.ID AS ArtGroeID, ArtGroe.Groesse, EinzHist.Barcode, Teilestatus.StatusBez AS Teilestatus, EinzHist.Indienst, EinzHist.Ausdienst, EinzHist.Ausgang1, EinzHist.RuecklaufK AS WaschzyklenKunde, EinzHist.TeileSchrankInfo AS SchrankFach, IIF(EinzHist.TeileSchrankInfo IS NULL, 0, 1) AS HatSchrank, IIF(EinzHist.Status IN (''Q'', ''S'', ''U'', ''W''), 1, 0) AS Istmenge, IIF(EinzHist.Status <= ''Q'' AND EinzHist.Status >= ''A'', 1, 0) AS Sollmenge
+FROM EinzHist
+JOIN Teilestatus ON EinzHist.Status = Teilestatus.Status
+JOIN TraeArti ON EinzHist.TraeArtiID = TraeArti.ID
 JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
@@ -38,7 +38,7 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 WHERE KdArti.ID = ' + CAST(@KdArtiID AS nvarchar(max)) + '
-  AND Teile.Status IN (''A'',''E'',''G'',''I'',''K'',''L'',''M'',''O'',''Q'',''S'',''N'')
+  AND EinzHist.Status IN (''A'',''E'',''G'',''I'',''K'',''L'',''M'',''O'',''Q'',''S'',''N'')
 ' + @SelectWearer + CHAR(13) + CHAR(10) + @ListOrder + N';';
 
 EXEC sp_executesql @SQL;
