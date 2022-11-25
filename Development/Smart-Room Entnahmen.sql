b031416f50c854a1a491fcadaf85b439abea314b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-11-25 08:00:41 +0100

    [9.50] Teile -> EinzHist

--- Development/Smart-Room Entnahmen.sql
+++ Development/Smart-Room Entnahmen.sql
@@ -1,21 +1,21 @@
-SELECT Traeger, Nachname, Vorname, ArtikelNr, ArtikelBez, Groesse, COUNT(TeileID) AS [Anzahl Teile], Aktion, Zeitpunkt, COUNT(Retourniert) AS [Anzahl retourniert]
+SELECT Traeger, Nachname, Vorname, ArtikelNr, ArtikelBez, Groesse, COUNT(EinzHistID) AS [Anzahl Teile], Aktion, Zeitpunkt, COUNT(Retourniert) AS [Anzahl retourniert]
 FROM (
-  SELECT Traeger.Traeger, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse, Teile.ID AS TeileID, Actions.ActionsBez AS Aktion, FORMAT(Scans.[DateTime], N'dd.MM.yyyy HH:mm') AS Zeitpunkt, Retourniert = (
-    SELECT DISTINCT s.TeileID
+  SELECT Traeger.Traeger, Traeger.Nachname, Traeger.Vorname, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse, EinzHist.ID AS EinzHistID, Actions.ActionsBez AS Aktion, FORMAT(Scans.[DateTime], N'dd.MM.yyyy HH:mm') AS Zeitpunkt, Retourniert = (
+    SELECT DISTINCT s.EinzHistID
     FROM Scans s
-    WHERE s.TeileID = Teile.ID
+    WHERE s.EinzHistID = EinzHist.ID
       AND s.[DateTime] >= Scans.[DateTime]
       AND s.ActionsID = 66
   )
   FROM Scans
-  JOIN Teile ON Scans.TeileID = Teile.ID
-  JOIN ArtGroe ON Teile.ArtGroeID = ArtGroe.ID
+  JOIN EinzHist ON Scans.EinzHistID = EinzHist.ID
+  JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
   JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
   JOIN Actions ON Scans.ActionsID = Actions.ID
   JOIN Traeger ON Scans.LastPoolTraegerID = Traeger.ID
-  WHERE Teile.VsaID = 6140348
+  WHERE EinzHist.VsaID = 6140348
     AND Scans.[DateTime] > CAST(GETDATE() AS date)
-    AND Scans.LastPoolTraegerID > 0
+    AND Scans.ActionsID = 65
 ) SmartroomDaten
 GROUP BY Traeger, Nachname, Vorname, ArtikelNr, ArtikelBez, Groesse, Aktion, Zeitpunkt
 ORDER BY Zeitpunkt ASC;
