bc3c97b10287d098cca28a5a3764feb461cd0e54
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-11-29 13:44:13 +0100

    Anpassungen für TSA-SQL-Server

--- Diverse/AUVA - Initialdaten-Import.sql
+++ Diverse/AUVA - Initialdaten-Import.sql
@@ -1,12 +1,8 @@
--- DROP TABLE #auvainitial;
+-- DROP TABLE IF EXISTS _auvainitial;
 
-/* ## Import file to table ## */
-DECLARE @Filename nvarchar(100) = N'initub.csv';
-DECLARE @ImportSQL nvarchar(200) = N'BULK INSERT #auvainitial FROM N''D:\AdvanTex\Temp\' + @Filename + '''WITH (CODEPAGE = ''65001'', FIELDTERMINATOR = N'';'', ROWTERMINATOR = N''\n'');';
-
-IF object_id('tempdb..#auvainitial') IS NULL
+/* IF object_id('Salesianer.dbo._auvainitial') IS NULL
 BEGIN
-  CREATE TABLE #auvainitial (
+  CREATE TABLE _auvainitial (
     LfdNr int,
     MifareID nchar(10) COLLATE Latin1_General_CS_AS,
     Kartennummer nchar(8) COLLATE Latin1_General_CS_AS NOT NULL,
@@ -19,21 +15,33 @@ BEGIN
     Standort nchar(2) COLLATE Latin1_General_CS_AS NOT NULL,
     Kostenstelle nchar(6) COLLATE Latin1_General_CS_AS NOT NULL
   );
+END; */
 
-  EXEC(@ImportSQL);
-END;
+ALTER TABLE _auvainitial ALTER COLUMN MifareID nchar(10) COLLATE Latin1_General_CS_AS;
+ALTER TABLE _auvainitial ALTER COLUMN Kartennummer nchar(8) COLLATE Latin1_General_CS_AS NOT NULL;
+ALTER TABLE _auvainitial ALTER COLUMN [Status] nchar(7) COLLATE Latin1_General_CS_AS NOT NULL;
+ALTER TABLE _auvainitial ALTER COLUMN Typ nchar(7) COLLATE Latin1_General_CS_AS NOT NULL;
+ALTER TABLE _auvainitial ALTER COLUMN Vorname nvarchar(30) COLLATE Latin1_General_CS_AS;
+ALTER TABLE _auvainitial ALTER COLUMN Nachname nvarchar(30) COLLATE Latin1_General_CS_AS;
+ALTER TABLE _auvainitial ALTER COLUMN Titel nvarchar(20) COLLATE Latin1_General_CS_AS;
+ALTER TABLE _auvainitial ALTER COLUMN TitelN nvarchar(20) COLLATE Latin1_General_CS_AS;
+ALTER TABLE _auvainitial ALTER COLUMN Standort nchar(2) COLLATE Latin1_General_CS_AS NOT NULL;
+ALTER TABLE _auvainitial ALTER COLUMN Kostenstelle nchar(6) COLLATE Latin1_General_CS_AS NOT NULL;
 
-SELECT * FROM #auvainitial;
--- SELECT ISNULL(LfdNr, '') AS LfdNr, MifareID, Kartennummer, Status, Typ, Vorname, Nachname, Titel, TitelN, ISNULL(Standort, '') AS Standort, ISNULL(Kostenstelle, '') AS Kostenstelle FROM #auvainitial WHERE Standort IS NULL;
+/* Import über SSMS in Tabelle _auvainitial - Struktur siehe oben */
+
+SELECT * FROM _auvainitial;
+-- SELECT ISNULL(LfdNr, '') AS LfdNr, MifareID, Kartennummer, Status, Typ, Vorname, Nachname, Titel, TitelN, ISNULL(Standort, '') AS Standort, ISNULL(Kostenstelle, '') AS Kostenstelle FROM _auvainitial WHERE Standort IS NULL;
+-- SELECT DISTINCT Standort FROM _auvainitial;
 
 DROP TABLE IF EXISTS #TmpImport;
 
 SELECT x.MifareID AS Kartennummer, x.Kartennummer AS PersNr, x.Status, x.Typ AS Kartentyp, x.Vorname, x.Nachname, x.Titel, x.TitelN, x.Standort, x.Kostenstelle, Rentomat.ID AS RentomatID
 INTO #TmpImport
-FROM #auvainitial x
+FROM _auvainitial x
 JOIN Rentomat ON Rentomat.SchrankNr LIKE '%' + x.Standort + '%';
 
-SELECT Traeger.PersNr AS Kartennummer, Traeger.Vorname, Traeger.Nachname, Traeger.RentomatKarte AS [MifareID Wozabal], x.Kartennummer AS [MifareID Initialdaten], x.Kartentyp, x.Standort
+SELECT Traeger.PersNr AS Kartennummer, Traeger.Vorname, Traeger.Nachname, Traeger.RentomatKarte AS [MifareID Salesianer], x.Kartennummer AS [MifareID Initialdaten], x.Kartentyp, x.Standort
 FROM Traeger, Vsa, #TmpImport x
 WHERE Traeger.VsaID = Vsa.ID
   AND Vsa.RentomatID = x.RentomatID
@@ -108,6 +116,20 @@ FROM Traeger, (
 ) AS i
 WHERE i.TraegerID = Traeger.ID;
 
+UPDATE Traeger SET Traeger.AbteilID = i.I_AbteilID, Traeger.DebitorNr = i.I_Kartennummer, Traeger.Status = 'A', Traeger.VormalsNr = i.I_Kartentyp, Traeger.Ausdienst = NULL, Traeger.AusdienstDat = NULL
+FROM Traeger, (
+  SELECT Traeger.ID AS TraegerID, Traeger.PersNr, x.PersNr AS I_PersNr, Traeger.Vorname, x.Vorname AS I_Vorname, Traeger.Nachname, RTRIM(x.Nachname) + ', ' + ISNULL(x.TitelN, '') AS I_Nachname, Traeger.Titel, x.Titel AS I_Titel, Traeger.RentomatKarte AS Kartennummer, x.Kartennummer AS I_Kartennummer, Abteil.ID AS I_AbteilID, x.Kartentyp AS I_Kartentyp
+  FROM Traeger, Vsa, Abteil, #TmpImport x
+  WHERE Traeger.VsaID = Vsa.ID
+    AND Vsa.RentomatID = x.RentomatID
+    AND Traeger.PersNr = x.PersNr
+    AND Traeger.Nachname = x.Nachname + ISNULL(x.TitelN, '')
+    AND Abteil.Abteilung = x.Kostenstelle
+    AND Abteil.KundenID = Vsa.KundenID
+    AND Traeger.AbteilID != Abteil.ID
+) AS i
+WHERE i.TraegerID = Traeger.ID;
+
 UPDATE Traeger SET Traeger.VormalsNr = i.I_Kartentyp, Traeger.DebitorNr = i.I_Kartennummer, Traeger.Status = 'A', Traeger.Ausdienst = NULL, Traeger.AusdienstDat = NULL
 FROM Traeger, (
   SELECT Traeger.ID AS TraegerID, Traeger.PersNr, x.PersNr AS I_PersNr, Traeger.Vorname, x.Vorname AS I_Vorname, Traeger.Nachname, RTRIM(x.Nachname) + ', ' + ISNULL(x.TitelN, '') AS I_Nachname, Traeger.Titel, x.Titel AS I_Titel, Traeger.RentomatKarte AS Kartennummer, x.Kartennummer AS I_Kartennummer, x.Kartentyp AS I_Kartentyp
@@ -130,8 +152,7 @@ WHERE NOT EXISTS (
   WHERE Vsa.RentomatID = ImportData.RentomatID
     AND Traeger.PersNr = ImportData.PersNr 
     AND Abteil.Abteilung = ImportData.Kostenstelle
-)
-  AND Nachname = N'KUNTSCHER';
+);
 
 -- Check ob alle Datensätze verarbeitet wurden
 SELECT Traeger.*
@@ -140,17 +161,19 @@ JOIN Vsa ON Traeger.VsaID = Vsa.ID
 WHERE Vsa.RentomatID IN (SELECT RentomatID FROM #TmpImport)
   AND Traeger.Status = N'A'
   AND Traeger.RentomatKarte IS NOT NULL
-  AND Traeger.Update_ > N'2018-09-13 09:30:00';
+  AND Traeger.Update_ > N'2022-11-29 12:35:00';
 
 SELECT Traeger.RentomatKarte
 FROM Traeger
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
 WHERE Vsa.RentomatID IN (SELECT RentomatID FROM #TmpImport)
   AND Traeger.Status = N'I'
-  AND Traeger.Update_ > N'2018-11-28 09:00:00'
+  AND Traeger.Update_ > N'2022-11-29 09:00:00'
   AND Traeger.UserID_ = (SELECT Mitarbei.ID FROM Mitarbei WHERE UserName = N'STHA');
 
--- Query für Ergebnis-Rückmeldung
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Einzeltests                                                                                                               ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 SELECT Traeger.ID, ISNULL(Traeger.RentomatKarte, N'') AS MifareID, Traeger.PersNr AS Kartennummer, Status.StatusBez AS Status, ISNULL(Traeger.VormalsNr, N'') AS Kartentyp, ISNULL(Traeger.Vorname, N'') AS Vorname, ISNULL(Traeger.Nachname, N'') AS Nachname, ISNULL(Traeger.Titel, N'') AS Titel, ISNULL(Abteil.Abteilung, N'') AS Kostenstelle, Kunden.KdNr, Kunden.SuchCode AS Kunde, Traeger.Update_ AS [letztes Datensatz-Update]
 FROM Traeger, Vsa, Kunden, Abteil, (SELECT Status.Status, Status.StatusBez FROM Status WHERE Status.Tabelle = 'TRAEGER') AS Status
 WHERE Traeger.VsaID = Vsa.ID
@@ -159,7 +182,9 @@ WHERE Traeger.VsaID = Vsa.ID
   AND Traeger.Status = Status.Status
   AND Traeger.PersNr IN (N'00112260');
 
--- Auswertung aktive Träger DCS
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Auswertung zur Datenprüfung                                                                                               ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 SELECT Kunden.KdNr, RTRIM(Kunden.SuchCode) AS Kunde, ISNULL(RTRIM(Traeger.RentomatKarte), N'') AS MifareID, RTRIM(Traeger.PersNr) AS Kartennummer, [Status].StatusBez AS [Status], ISNULL(RTRIM(Traeger.VormalsNr), N'') AS Kartentyp, ISNULL(RTRIM(Traeger.Vorname), N'') AS Vorname, ISNULL(RTRIM(Traeger.Nachname), N'') AS Nachname, ISNULL(RTRIM(Traeger.Titel), N'') AS Titel, ISNULL(RTRIM(Abteil.Abteilung), N'') AS Kostenstelle, IIF(Traeger.RentoArtID < 0, 0, 1) AS Bekleidungsprofil, Traeger.Anlage_
 FROM Traeger
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
@@ -167,6 +192,37 @@ JOIN Kunden oN Vsa.KundenID = Kunden.ID
 JOIN Rentomat ON Vsa.RentomatID = Rentomat.ID
 JOIN [Status] ON Traeger.[Status] = [Status].[Status] AND [Status].[Tabelle] = N'TRAEGER'
 JOIN Abteil ON Traeger.AbteilID = Abteil.ID
-WHERE Rentomat.SchrankNr LIKE N'%UB%'
+WHERE Rentomat.SchrankNr LIKE N'%UM%'
   AND Traeger.Status = N'A'
   AND Traeger.RentomatKarte IS NOT NULL;
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Doppelte Träger finden                                                                                                    ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+WITH doublewearer AS (
+  SELECT Traeger.RentomatKarte, COUNT(Traeger.ID) AS [Anzahl Träger]
+  FROM Traeger
+  JOIN Vsa ON Traeger.VsaID = Vsa.ID
+  JOIN Kunden oN Vsa.KundenID = Kunden.ID
+  JOIN Rentomat ON Vsa.RentomatID = Rentomat.ID
+  JOIN [Status] ON Traeger.[Status] = [Status].[Status] AND [Status].[Tabelle] = N'TRAEGER'
+  JOIN Abteil ON Traeger.AbteilID = Abteil.ID
+  WHERE Rentomat.SchrankNr LIKE N'%UM%'
+    AND Traeger.Status = N'A'
+    AND Traeger.RentomatKarte IS NOT NULL
+  GROUP BY Traeger.RentomatKarte
+  HAVING COUNT(Traeger.ID) > 1
+)
+SELECT Traeger.Traeger, Traeger.PersNr, Traeger.Vorname, Traeger.Nachname, Traeger.Titel
+FROM Traeger
+JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden oN Vsa.KundenID = Kunden.ID
+JOIN Rentomat ON Vsa.RentomatID = Rentomat.ID
+JOIN [Status] ON Traeger.[Status] = [Status].[Status] AND [Status].[Tabelle] = N'TRAEGER'
+JOIN Abteil ON Traeger.AbteilID = Abteil.ID
+WHERE Rentomat.SchrankNr LIKE N'%UM%'
+  AND Traeger.Status = N'A'
+  AND Traeger.RentomatKarte IS NOT NULL
+  AND Traeger.RentomatKarte IN (SELECT RentomatKarte FROM doublewearer)
+ORDER BY PersNr ASC;
