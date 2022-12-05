0806b6c8a68ee6132c1d0fe795498486cce6448b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-12-05 14:29:35 +0100

    IT-65420 show if garment has been (or will be) invoiced

--- Report/734 - Teile verschrottet im Zeitraum - BK - Berufskleidung.sql
+++ Report/734 - Teile verschrottet im Zeitraum - BK - Berufskleidung.sql
@@ -27,12 +27,14 @@ CREATE TABLE #TmpErgebnis734 (
   [Anzahl Wäschen gesamt] int,
   [Anzahl Wäschen Kunde] int,
   Kostenlos bit,
+  [Restwert berechnet?] bit,
   Mitarbeiter nvarchar(40) COLLATE Latin1_General_CS_AS
 );
 
-INSERT INTO #TmpErgebnis734 (Grund, Barcode, [Status], ErstWoche, ErstDatum, PatchDatum, Ausdienst, AusdienstDat, ArtikelNr, ArtikelNr2, Artikelbezeichnung, EKPreis, VsaNr, [VSA-Stichwort], [VSA-Bezeichnung], Produktionsstandort, KdNr, Kunde, [Kundenservice-Standort], Geschäftsbereich, Restwert, [Anzahl Wäschen gesamt], [Anzahl Wäschen Kunde], Kostenlos, Mitarbeiter)
-SELECT  WegGrund.WegGrundBez$LAN$ AS Grund, EinzHist.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, EinzHist.ErstDatum, EinzHist.PatchDatum, EinzHist.Ausdienst, EinzHist.AusdienstDat, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS Geschäftsbereich, EinzHist.AusdRestw AS Restwert, EinzHist.RuecklaufG AS [Anzahl Wäschen gesamt], EinzHist.RuecklaufK AS [Anzahl Wäschen Kunde], EinzHist.Kostenlos, Mitarbei.Name AS Mitarbeiter
+INSERT INTO #TmpErgebnis734 (Grund, Barcode, [Status], ErstWoche, ErstDatum, PatchDatum, Ausdienst, AusdienstDat, ArtikelNr, ArtikelNr2, Artikelbezeichnung, EKPreis, VsaNr, [VSA-Stichwort], [VSA-Bezeichnung], Produktionsstandort, KdNr, Kunde, [Kundenservice-Standort], Geschäftsbereich, Restwert, [Anzahl Wäschen gesamt], [Anzahl Wäschen Kunde], Kostenlos, [Restwert berechnet?], Mitarbeiter)
+SELECT  WegGrund.WegGrundBez$LAN$ AS Grund, EinzHist.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, EinzHist.ErstDatum, EinzHist.PatchDatum, EinzHist.Ausdienst, EinzHist.AusdienstDat, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS Geschäftsbereich, EinzHist.AusdRestw AS Restwert, EinzHist.RuecklaufG AS [Anzahl Wäschen gesamt], EinzHist.RuecklaufK AS [Anzahl Wäschen Kunde], EinzHist.Kostenlos, CAST(IIF(ISNULL(TeilSoFa.RechPoID, -9) < 0, 0, IIF(TeilSoFa.Status IN (N'L', N'P'), 1, 0)) AS bit) AS [Restwert berechnet?], Mitarbei.Name AS Mitarbeiter
 FROM EinzHist
+LEFT JOIN TeilSoFa ON TeilSoFa.EinzHistID = EinzHist.ID AND TeilSoFa.SoFaArt = N'R'
 JOIN Vsa ON EinzHist.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
@@ -43,7 +45,7 @@ JOIN Bereich ON Artikel.BereichID = Bereich.ID
 JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND Bereich.ID = StandBer.BereichID
 JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
 JOIN Standort AS Kundenservice ON Kunden.StandortID = Kundenservice.ID
-JOIN Week ON DATEADD(day, EinzHist.AnzTageImLager, EinzHist.ErstDatum) BETWEEN Week.VonDat AND Week.BisDat
+JOIN [Week] ON DATEADD(day, EinzHist.AnzTageImLager, EinzHist.ErstDatum) BETWEEN [Week].VonDat AND [Week].BisDat
 LEFT JOIN Scans ON Scans.EinzHistID = EinzHist.ID AND Scans.ActionsID = 7
 LEFT JOIN Mitarbei ON Scans.AnlageUserID_ = Mitarbei.ID
 WHERE Kunden.KdGfID IN ($4$)
