7c6aea9d5e0cfb6da8656a89c00b50c8ff47ca30
Thallerich <20878157+Thallerich@users.noreply.github.com>
2022-12-12 11:52:31 +0100

    IT-66006 [9.50] removed dynamic query - parameters are filled differently now

--- Report/3010 - Preiserhöhungen im Zeitraum.sql
+++ Report/3010 - Preiserhöhungen im Zeitraum.sql
@@ -1,39 +1,24 @@
-DECLARE @von date, @bis date;
-DECLARE @LetztesJahr int, @Firma int;
-DECLARE @sqltext nvarchar(max);
-
-SET @von = $STARTDATE$;
-SET @bis = $ENDDATE$;
-SET @LetztesJahr = DATEPART(year, @bis) - 1;
-
-SET @Firma = $1$;
-
-/* CAST(N'' AS nvarchar(max)) prevents implicit conversion to nvarchar(4000) because of string concetanation --> stupid bug! */
-SET @sqltext = CAST(N'' AS nvarchar(max)) + N'
-  WITH Jahresumsatz AS (
-    SELECT RechKo.KundenID, SUM(RechKo.Nettowert) AS Nettoumsatz
-    FROM RechKo
-    WHERE DATEPART(year, RechKo.RechDat) = @LetztesJahr
-      AND RechKo.Status BETWEEN N''F'' AND N''S''
-    GROUP BY RechKo.KundenID
-  )
-  SELECT DISTINCT Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, KdGf.KurzBez AS Geschäftsbereich, [Zone].ZonenCode AS Vertriebszone, ABC.ABCBez AS [ABC-Klasse], PeKo.Bez AS [Bezeichnung Preiserhöhung], PeKo.WirksamDatum AS [Wirksam ab], PeKo.DurchfuehrungsDatum AS [durchgeführt am], PePo.PeProzent AS Prozentsatz, Jahresumsatz.Nettoumsatz AS [Jahresumsatz ' + CAST(@LetztesJahr AS nchar(4)) + N']
-  FROM PePo
-  JOIN PeKo ON PePo.PeKoID = PeKo.ID
-  JOIN Vertrag ON PePo.VertragID = Vertrag.ID
-  JOIN Kunden ON Vertrag.KundenID = Kunden.ID
-  JOIN ABC ON Kunden.ABCID = ABC.ID
-  JOIN [Zone] ON Kunden.ZoneID = [Zone].ID
-  JOIN KdGf ON Kunden.KdGfID = KdGf.ID
-  JOIN Firma ON Kunden.FirmaID = Firma.ID
-  JOIN Holding ON Kunden.HoldingID = Holding.ID
-  JOIN Jahresumsatz ON Jahresumsatz.KundenID = Kunden.ID
-  WHERE PeKo.WirksamDatum BETWEEN @von AND @bis
-    AND PePo.PeProzent != 0
-    AND PeKo.Status = N''N''
-    AND Firma.ID = @Firma
-    AND KdGf.ID IN ($2$)
-    AND Holding.ID IN ($3$);
-';
-
-EXEC sp_executesql @sqltext, N'@LetztesJahr int, @von date, @bis date, @Firma int', @LetztesJahr, @von, @bis, @Firma;
+WITH Jahresumsatz AS (
+  SELECT RechKo.KundenID, SUM(RechKo.Nettowert) AS Nettoumsatz
+  FROM RechKo
+  WHERE DATEPART(year, RechKo.RechDat) = DATEPART(year, $ENDDATE$) - 1  /* Umsatz vom Jahr vor der PE verwenden */
+    AND RechKo.Status BETWEEN N'F' AND N'S'
+  GROUP BY RechKo.KundenID
+)
+SELECT DISTINCT Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, KdGf.KurzBez AS Geschäftsbereich, [Zone].ZonenCode AS Vertriebszone, ABC.ABCBez AS [ABC-Klasse], PeKo.Bez AS [Bezeichnung Preiserhöhung], PeKo.WirksamDatum AS [Wirksam ab], PeKo.DurchfuehrungsDatum AS [durchgeführt am], PePo.PeProzent AS Prozentsatz, Jahresumsatz.Nettoumsatz AS [Jahresumsatz]
+FROM PePo
+JOIN PeKo ON PePo.PeKoID = PeKo.ID
+JOIN Vertrag ON PePo.VertragID = Vertrag.ID
+JOIN Kunden ON Vertrag.KundenID = Kunden.ID
+JOIN ABC ON Kunden.ABCID = ABC.ID
+JOIN [Zone] ON Kunden.ZoneID = [Zone].ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN Firma ON Kunden.FirmaID = Firma.ID
+JOIN Holding ON Kunden.HoldingID = Holding.ID
+JOIN Jahresumsatz ON Jahresumsatz.KundenID = Kunden.ID
+WHERE PeKo.WirksamDatum BETWEEN $STARTDATE$ AND $ENDDATE$
+  AND PePo.PeProzent != 0
+  AND PeKo.Status = N'N'
+  AND Firma.ID = $1$
+  AND KdGf.ID IN ($2$)
+  AND Holding.ID IN ($3$);
