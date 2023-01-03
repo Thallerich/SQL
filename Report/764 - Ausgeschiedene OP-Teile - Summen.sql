f06ddd3ab5eba815f6585ec14089c59913e96af8
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-01-03 09:18:09 +0100

    IT-66762 changed table structure [release 9.50]

--- Report/764 - Ausgeschiedene OP-Teile - Summen.sql
+++ Report/764 - Ausgeschiedene OP-Teile - Summen.sql
@@ -1,12 +1,11 @@
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, WegGrund.WegGrundBez$LAN$ AS WegGrund, COUNT(OPTeile.ID) AS AnzahlAusgeschieden
-FROM OPTeile, WegGrund, Artikel, Vsa, Kunden
-WHERE OPTeile.WegGrundID = WegGrund.ID
-	AND OPTeile.VsaID = Vsa.ID
-	AND Vsa.KundenID = Kunden.ID
-	AND OPTeile.ArtikelID = Artikel.ID
-	AND OPTeile.WegGrundID > 0
-	AND OPTeile.WegDatum BETWEEN $2$ AND $3$
-	AND Vsa.StandKonID IN ($1$)
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, WegGrund.WeggrundBez$LAN$ AS Schrottgrund, COUNT(EinzTeil.ID) AS [Anzahl Ausgeschieden]
+FROM EinzTeil
+JOIN WegGrund ON EinzTeil.WegGrundID = WegGrund.ID
+JOIN Vsa ON EinzTeil.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
+WHERE EinzTeil.WegGrundID > 0
+	AND EinzTeil.WegDatum BETWEEN $STARTDATE$ AND $ENDDATE$
+	AND Vsa.StandKonID IN ($2$)
 	AND Kunden.SichtbarID IN ($SICHTBARIDS$)
-GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, WegGrund.WegGrundBez$LAN$
-ORDER BY Artikel.ArtikelNr, WegGrund;
+GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, WegGrund.WegGrundBez$LAN$;
