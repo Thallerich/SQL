9c7f2b540349677bf5315c19803fb143e28ac199
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-01-03 13:18:13 +0100

    calculation changed

--- Diverse/Rechnungsdruck-Performance.sql
+++ Diverse/Rechnungsdruck-Performance.sql
@@ -3,14 +3,14 @@ WITH Rechnungsanlagen AS (
   FROM KdRKoAnl
   GROUP BY KdRKoAnl.KundenID
 )
-SELECT Drucklauf, DruckMitarbeiter, RechNr, RechDat, NettoWert, BruttoWert, VorherigerDruckzeitpunkt, DruckZeitpunkt, DATEDIFF(second, VorherigerDruckzeitpunkt, DruckZeitpunkt) AS DruckDauer, Rechnungsanlagen.AnzAnlagen
+SELECT Drucklauf, DruckMitarbeiter, RechNr, RechDat, CAST(NettoWert AS float) AS NettoWert, CAST(BruttoWert AS float) AS BruttoWert, DruckZeitpunkt, NächsterDruckzeitpunkt, DATEDIFF(second, DruckZeitpunkt, NächsterDruckzeitpunkt) AS [DruckDauer Sekunden], Rechnungsanlagen.AnzAnlagen AS [Anzahl Rechnungsanlagen]
 FROM (
-  SELECT RechKo.ID AS RechKoID, RechKo.KundenID, DrLauf.Bez AS Drucklauf, Mitarbei.Name AS DruckMitarbeiter, RechKo.RechNr, RechKo.RechDat, RechKo.NettoWert, RechKo.BruttoWert, RechKo.DruckZeitpunkt, LAG(RechKo.DruckZeitpunkt, 1, NULL) OVER (PARTITION BY RechKo.DrLaufID, RechKo.DruckMitarbeiID ORDER BY RechKo.DruckZeitpunkt) AS VorherigerDruckzeitpunkt
+  SELECT RechKo.ID AS RechKoID, RechKo.KundenID, DrLauf.Bez AS Drucklauf, Mitarbei.Name AS DruckMitarbeiter, RechKo.RechNr, RechKo.RechDat, RechKo.NettoWert, RechKo.BruttoWert, RechKo.DruckZeitpunkt, LEAD(RechKo.DruckZeitpunkt, 1, NULL) OVER (PARTITION BY RechKo.DrLaufID, RechKo.DruckMitarbeiID ORDER BY RechKo.DruckZeitpunkt) AS NächsterDruckzeitpunkt
   FROM RechKo
   JOIN DrLauf ON RechKo.DrLaufID = DrLauf.ID
   JOIN Mitarbei ON RechKo.DruckMitarbeiID = Mitarbei.ID
-  WHERE RechKo.FreigabeZeit > N'2021-02-02 00:00:00'
-    AND RechKo.FirmaID = (SELECT Firma.ID FROM Firma WHERE Firma.SuchCode = N'FA14')
+  WHERE RechKo.DruckZeitpunkt > N'2023-01-03 08:38:00'
+    AND RechKo.DrLaufID = (SELECT DrLaufID FROM RechKo WHERE RechNr = 30287942)
 ) AS x
 RIGHT JOIN Rechnungsanlagen ON Rechnungsanlagen.KundenID = x.KundenID
 WHERE x.RechKoID IS NOT NULL
