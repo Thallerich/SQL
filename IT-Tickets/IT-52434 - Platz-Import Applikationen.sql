1ec0ce520a37f41949af88c5879aa4b20dcdbb7b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-01-10 10:54:03 +0100

    changes for new import  - include check for old placing  - include alterations (Art = N)  - make transaction-safe

--- IT-Tickets/IT-52434 - Platz-Import Applikationen.sql
+++ IT-Tickets/IT-52434 - Platz-Import Applikationen.sql
@@ -1,50 +1,67 @@
-BEGIN TRANSACTION;
+BEGIN TRY
 
-  DECLARE @KdArPlatzUpdate TABLE (
-    KdArApplID int,
-    PlatzID int
-  );
+  BEGIN TRANSACTION;
 
-  DECLARE @TraePlatzUpdate TABLE (
-    TraeApplID int,
-    PlatzID int
-  );
+    DECLARE @KdArPlatzUpdate TABLE (
+      KdArApplID int,
+      PlatzID int
+    );
 
-  WITH PlatzImport AS (
-    SELECT KdNr,
-      ArtikelNr COLLATE Latin1_General_CS_AS AS ArtikelNr,
-      Art = CASE NsEmbl
-        WHEN N'E' THEN 3
-        WHEN N'N' THEN 2
-      END,
-      Platz.ID AS PlatzID
-    FROM Salesianer.dbo.__PlatzImport
-    JOIN Platz ON __PlatzImport.PlatzNeu COLLATE Latin1_General_CS_AS = Platz.Code
-  )
-  UPDATE KdArAppl SET PlatzID = PlatzImport.PlatzID
-  OUTPUT inserted.ID, inserted.PlatzID
-  INTO @KdArPlatzUpdate (KdArApplID, PlatzID)
-  --SELECT KdArAppl.ID, KdArAppl.PlatzID, PlatzImport.PlatzID AS PlatzIDNeu
-  FROM KdArAppl
-  JOIN KdArti ON KdArAppl.KdArtiID = KdArti.ID
-  JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-  JOIN Kunden ON KdArti.KundenID = Kunden.ID
-  JOIN KdArti AS Applikation ON KdArAppl.ApplKdArtiID = Applikation.ID
-  JOIN Artikel AS ApplArti ON Applikation.ArtikelID = ApplArti.ID
-  JOIN PlatzImport ON Kunden.KdNr = PlatzImport.KdNr AND Artikel.ArtikelNr = PlatzImport.ArtikelNr AND ApplArti.ArtiTypeID = PlatzImport.Art;
+    DECLARE @TraePlatzUpdate TABLE (
+      TraeApplID int,
+      PlatzID int
+    );
 
-  UPDATE TraeAppl SET PlatzID = PlatzUpdate.PlatzID
-  OUTPUT inserted.ID, inserted.PlatzID
-  INTO @TraePlatzUpdate (TraeApplID, PlatzID)
-  FROM TraeAppl
-  JOIN @KdArPlatzUpdate AS PlatzUpdate ON PlatzUpdate.KdArApplID = TraeAppl.KdArApplID;
+    WITH PlatzImport AS (
+      SELECT KdNr,
+        ArtikelNr COLLATE Latin1_General_CS_AS AS ArtikelNr,
+        Art = CASE NsEmbl
+          WHEN N'E' THEN 3
+          WHEN N'N' THEN 2
+          WHEN N'A' THEN 8
+        END,
+        Platz.ID AS PlatzID,
+        PlatzBezOld
+      FROM Salesianer.dbo.__PlatzImport
+      JOIN Platz ON __PlatzImport.PlatzNeu COLLATE Latin1_General_CS_AS = Platz.Code
+    )
+    UPDATE KdArAppl SET PlatzID = PlatzImport.PlatzID
+    OUTPUT inserted.ID, inserted.PlatzID
+    INTO @KdArPlatzUpdate (KdArApplID, PlatzID)
+    /* SELECT KdArAppl.ID, KdArAppl.PlatzID, PlatzImport.PlatzID AS PlatzIDNeu, Kunden.KdNr, Artikel.ArtikelNr, KdArti.Variante */
+    FROM KdArAppl
+    JOIN KdArti ON KdArAppl.KdArtiID = KdArti.ID
+    JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+    JOIN Kunden ON KdArti.KundenID = Kunden.ID
+    JOIN KdArti AS Applikation ON KdArAppl.ApplKdArtiID = Applikation.ID
+    JOIN Artikel AS ApplArti ON Applikation.ArtikelID = ApplArti.ID
+    JOIN Platz ON KdArAppl.PlatzID = Platz.ID
+    JOIN PlatzImport ON Kunden.KdNr = PlatzImport.KdNr AND Artikel.ArtikelNr = PlatzImport.ArtikelNr AND ApplArti.ArtiTypeID = PlatzImport.Art AND Platz.PlatzBez = PlatzImport.PlatzBezOld
+    WHERE KdArAppl.PlatzID != PlatzImport.PlatzID;
 
-  UPDATE TeilAppl SET PlatzID = PlatzUpdate.PlatzID
-  FROM TeilAppl
-  JOIN @TraePlatzUpdate AS PlatzUpdate ON PlatzUpdate.TraeApplID = TeilAppl.TraeApplID;
+    UPDATE TraeAppl SET PlatzID = PlatzUpdate.PlatzID
+    OUTPUT inserted.ID, inserted.PlatzID
+    INTO @TraePlatzUpdate (TraeApplID, PlatzID)
+    FROM TraeAppl
+    JOIN @KdArPlatzUpdate AS PlatzUpdate ON PlatzUpdate.KdArApplID = TraeAppl.KdArApplID;
 
-  UPDATE RepQueue SET Priority = 90000 + Priority
-  WHERE ApplicationID = N'AdvanTex.exe';
+    UPDATE TeilAppl SET PlatzID = PlatzUpdate.PlatzID
+    FROM TeilAppl
+    JOIN @TraePlatzUpdate AS PlatzUpdate ON PlatzUpdate.TraeApplID = TeilAppl.TraeApplID;
 
---COMMIT
---ROLLBACK
+    UPDATE RepQueue SET Priority = 90000 + Priority
+    WHERE ApplicationID = N'AdvanTex.exe';
+
+  COMMIT;
+
+END TRY
+BEGIN CATCH
+  DECLARE @Message nvarchar(max) = ERROR_MESSAGE();
+  DECLARE @Severity int = ERROR_SEVERITY();
+  DECLARE @State smallint = ERROR_STATE();
+
+  IF XACT_STATE() != 0
+    ROLLBACK;
+
+  RAISERROR(@Message, @Severity, @State);
+END CATCH;
