5f1abb5e2fbc74213011238ac919544ff2945695
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-01-18 08:48:32 +0100

    added counter for NS / Embl

--- Report/966 - Kundenartikel mit Namensschild oder Emblem.sql
+++ Report/966 - Kundenartikel mit Namensschild oder Emblem.sql
@@ -4,6 +4,8 @@ WITH KdArtiStatus AS (
   WHERE [Status].Tabelle = UPPER(N'KDARTI')
 )
 SELECT 
+  Standort.SuchCode AS Hauptstandort,
+  KdGf.KurzBez AS SGF,
   Kunden.KdNr, 
   Kunden.SuchCode AS Kunde, 
   Artikel.ArtikelNr, 
@@ -11,15 +13,22 @@ SELECT
   KdArtiStatus.StatusBez AS [Status Kundenartikel], 
   KdArti.Variante, 
   KdArti.VariantBez AS Variantenbezeichnung, 
-  CAST(IIF(NsAppl.ID IS NOT NULL, 1, 0) AS bit) AS [Namenschild?], 
+  CAST(IIF(NsAppl.ID IS NOT NULL, 1, 0) AS bit) AS [Namenschild?],
+  COUNT(DISTINCT NsAppl.ID) AS [Anzahl Namenschilder],
   CAST(IIF(EmblAppl.ID IS NOT NULL, 1, 0) AS bit) AS [Emblem?],
+  COUNT(DISTINCT EmblAppl.ID) AS [Anzahl Embleme],
   KdArti.ID AS KdArtiID               -- um im AdvanTex direkt zum Kundenartikel springen zu können; wird nicht angezeigt / exportiert
 FROM KdArti
 JOIN Kunden ON KdArti.KundenID = Kunden.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN Standort ON Kunden.StandortID = Standort.ID
 JOIN KdArtiStatus ON KdArti.Status = KdArtiStatus.Status
 LEFT JOIN KdArAppl AS NsAppl ON NsAppl.KdArtiID = KdArti.ID AND NsAppl.ArtiTypeID = 2
 LEFT JOIN KdArAppl AS EmblAppl ON EmblAppl.KdArtiID = KdArti.ID AND EmblAppl.ArtiTypeID = 3
 WHERE Kunden.AdrArtID = 1             -- nur tatsächliche Kunden
   AND Kunden.Status = N'A'            -- nur aktive Kunden
-  AND (NsAppl.ID IS NOT NULL OR EmblAppl.ID IS NOT NULL);
+  AND Standort.ID IN ($1$)
+  AND KdGf.ID IN ($2$)
+  AND (NsAppl.ID IS NOT NULL OR EmblAppl.ID IS NOT NULL)
+GROUP BY Standort.SuchCode, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, KdArtiStatus.StatusBez, KdArti.Variante, KdArti.VariantBez, CAST(IIF(NsAppl.ID IS NOT NULL, 1, 0) AS bit), CAST(IIF(EmblAppl.ID IS NOT NULL, 1, 0) AS bit), KdArti.ID;
