114026dc0d0835b309d46677dde16d9dc38d1d00
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-01-24 11:11:37 +0100

    initial version

---
+++ Development/Anzahl Umlaufteile.sql
@@ -0,0 +1,43 @@
+DROP TABLE IF EXISTS #PartsPerLocation;
+
+GO
+
+CREATE TABLE #PartsPerLocation (
+  LocationID int,
+  SizeID int,
+  AmountOfParts int
+);
+
+GO
+
+INSERT INTO #PartsPerLocation
+SELECT LastScanLocation, ArtGroeID, COUNT(*) AS AmountOfParts
+FROM (
+  SELECT LastScanLocation = (
+    SELECT TOP 1 COALESCE(IIF(ZielNr.ProduktionsID < 0, NULL, ZielNr.ProduktionsID), IIF(ArbPlatz.StandortID < 0, NULL, ArbPlatz.StandortID)) AS StandortID
+    FROM Scans
+    JOIN ZielNr ON Scans.ZielNrID = ZielNr.ID
+    JOIN ArbPlatz ON Scans.ArbPlatzID = ArbPlatz.ID
+    WHERE (ZielNr.ProduktionsID > 0 OR ArbPlatz.StandortID > 0)
+      AND Scans.EinzTeilID = EinzTeil.ID
+    ORDER BY Scans.[DateTime] DESC
+  ), EinzTeil.ArtGroeID
+  FROM EinzTeil
+  JOIN ArtGroe ON EinzTeil.ArtGroeID = ArtGroe.ID
+  JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+  WHERE Artikel.ArtikelNr = N'U41'
+    AND EinzTeil.[Status] <= N'Q'
+    AND ISNULL(EinzTeil.LastScanTime, N'1980-01-01 12:00:00') >= DATEADD(year, -1, GETDATE())
+) x
+WHERE LastScanLocation = (SELECT ID FROM Standort WHERE SuchCode = N'SAWR')
+GROUP BY LastScanLocation, ArtGroeID;
+
+GO
+
+SELECT Standort.SuchCode, Artikel.ArtikelNr, ArtGroe.Groesse, #PartsPerLocation.AmountOfParts
+FROM #PartsPerLocation
+JOIN ArtGroe ON #PartsPerLocation.SizeID = ArtGroe.ID
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+JOIN Standort ON #PartsPerLocation.LocationID = Standort.ID;
+
+GO
