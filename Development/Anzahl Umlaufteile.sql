4ea0a02dfa1f39d103f41e010120274f45c038c2
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-01-24 13:53:56 +0100

    small changes for special reporting

--- Development/Anzahl Umlaufteile.sql
+++ Development/Anzahl Umlaufteile.sql
@@ -25,7 +25,7 @@ FROM (
   FROM EinzTeil
   JOIN ArtGroe ON EinzTeil.ArtGroeID = ArtGroe.ID
   JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
-  WHERE Artikel.ArtikelNr = N'U41'
+  WHERE Artikel.ArtikelNr IN (N'U41', N'U4K', N'U32')
     AND EinzTeil.[Status] <= N'Q'
     AND ISNULL(EinzTeil.LastScanTime, N'1980-01-01 12:00:00') >= DATEADD(year, -1, GETDATE())
 ) x
@@ -34,7 +34,7 @@ GROUP BY LastScanLocation, ArtGroeID;
 
 GO
 
-SELECT Standort.SuchCode, Artikel.ArtikelNr, ArtGroe.Groesse, #PartsPerLocation.AmountOfParts
+SELECT Standort.SuchCode AS Produktion, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, #PartsPerLocation.AmountOfParts AS [Anzahl Teile]
 FROM #PartsPerLocation
 JOIN ArtGroe ON #PartsPerLocation.SizeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
