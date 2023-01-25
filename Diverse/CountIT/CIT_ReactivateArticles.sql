515f35586d6088326b74c518c5fef1c2436f0a78
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-01-25 14:37:29 +0100

    check for last transmission date to use

--- Diverse/CountIT/CIT_ReactivateArticles.sql
+++ Diverse/CountIT/CIT_ReactivateArticles.sql
@@ -1,9 +1,19 @@
+DECLARE @LastSuccessDate date;
+
+SELECT @LastSuccessDate = Datum
+FROM (
+  SELECT TOP 1 CAST(LastTransmissionDate AS date) Datum, COUNT(ArticleID) Anzahl
+  FROM LaundryAutomation.dbo.Article
+  GROUP BY CAST(LastTransmissionDate AS date)
+  ORDER BY Anzahl DESC
+) x;
+
 SELECT LaundryAutomation.dbo.Article.*
 FROM LaundryAutomation.dbo.Article
-WHERE CONVERT(date, LaundryAutomation.dbo.Article.LastTransmissionDate) >= CONVERT(date, DATEADD(day, -1, GETDATE()))
+WHERE CONVERT(date, LaundryAutomation.dbo.Article.LastTransmissionDate) >= @LastSuccessDate
   AND CONVERT(date, LaundryAutomation.dbo.Article.LastTransmissionDate) < CONVERT(date, GETDATE());
 
 /* Reactivate Articles ################ */
 UPDATE LaundryAutomation.dbo.Article SET LastTransmissionDate = GETDATE()
-WHERE CONVERT(date, LaundryAutomation.dbo.Article.LastTransmissionDate) >= CONVERT(date, DATEADD(day, -1, GETDATE()))
+WHERE CONVERT(date, LaundryAutomation.dbo.Article.LastTransmissionDate) >= @LastSuccessDate
   AND CONVERT(date, LaundryAutomation.dbo.Article.LastTransmissionDate) < CONVERT(date, GETDATE());
