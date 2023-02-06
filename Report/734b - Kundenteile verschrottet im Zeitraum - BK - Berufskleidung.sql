cde005803deb27e39734a96f114507cade64f4a9
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-02-06 15:50:28 +0100

    IT-67877  - changed the way we query teilsofa-table  - only use teilsofa-rows where invoicing happened / should happen  - we avoid duplicate rows this way

--- Report/734b - Kundenteile verschrottet im Zeitraum - BK - Berufskleidung.sql
+++ Report/734b - Kundenteile verschrottet im Zeitraum - BK - Berufskleidung.sql
@@ -1,5 +1,6 @@
-SELECT WegGrund.WegGrundBez$LAN$ AS Grund, EinzHist.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, EinzHist.ErstDatum, EinzHist.PatchDatum, EinzHist.Ausdienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS SGF, EinzHist.AusdRestw AS RestWert
+SELECT WegGrund.WegGrundBez$LAN$ AS Grund, EinzHist.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, EinzHist.ErstDatum, EinzHist.PatchDatum, EinzHist.Ausdienst, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$, Artikel.EKPreis, Vsa.SuchCode AS VsaNr, Vsa.Bez AS Vsa, Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS SGF, EinzHist.AusdRestw AS RestWert, EinzHist.RuecklaufG AS [Anzahl Wäschen gesamt], EinzHist.RuecklaufK AS [Anzahl Wäschen Kunde], EinzHist.Kostenlos, CAST(IIF(ISNULL(TeilSoFa.ID, -1) > 0, 1, 0) AS bit) AS [Restwert berechnet?]
 FROM EinzHist
+LEFT JOIN TeilSoFa ON TeilSoFa.EinzHistID = EinzHist.ID AND TeilSoFa.SoFaArt = N'R' AND (TeilSoFa.RechPoID > 0 OR TeilSoFa.Status IN (N'L', N'P'))
 JOIN Vsa ON EinzHist.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
@@ -10,10 +11,10 @@ JOIN Bereich ON Artikel.BereichID = Bereich.ID
 JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND Bereich.ID = StandBer.BereichID
 JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
 JOIN Standort AS Kundenservice ON Kunden.StandortID = Kundenservice.ID
-JOIN Week ON DATEADD(day, EinzHist.AnzTageImLager, EinzHist.ErstDatum) BETWEEN Week.VonDat AND Week.BisDat
+JOIN [Week] ON DATEADD(day, EinzHist.AnzTageImLager, EinzHist.ErstDatum) BETWEEN [Week].VonDat AND [Week].BisDat
   AND Kunden.ID IN ($ID$)
-  AND EinzHist.[Status] = 'Y'
+  AND EinzHist.[Status] = N'Y'
+  AND EinzHist.EinzHistTyp = 1 /* ausgeschieden */
   AND WegGrund.ID IN ($3$)
   AND EinzHist.AusDienstDat BETWEEN $STARTDATE$ AND $ENDDATE$
-  AND Bereich.ID IN ($5$)
-ORDER BY Kdnr, VsaNr, ArtikelNr;
+  AND Bereich.ID IN ($5$);
