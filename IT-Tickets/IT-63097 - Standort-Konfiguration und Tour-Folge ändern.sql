e9ca9f6da1c95e5e6f637941e229e0b225a2bd9f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-02-14 15:04:37 +0100

    prepwork for next ticket

--- IT-Tickets/IT-63097 - Standort-Konfiguration und Tour-Folge ändern.sql
+++ IT-Tickets/IT-63097 - Standort-Konfiguration und Tour-Folge ändern.sql
@@ -1,5 +1,5 @@
-IF OBJECT_ID('_IT67442_Vsa') IS NULL
-  CREATE TABLE _IT67442_Vsa (
+IF OBJECT_ID('_IT68183_Vsa') IS NULL
+  CREATE TABLE _IT68183_Vsa (
     VsaID int PRIMARY KEY,
     StandKonID int,
     ServTypeID int,
@@ -9,8 +9,8 @@ IF OBJECT_ID('_IT67442_Vsa') IS NULL
 
 GO
 
-IF OBJECT_ID('_IT67442_VsaTour') IS NULL
-  CREATE TABLE _IT67442_VsaTour (
+IF OBJECT_ID('_IT68183_VsaTour') IS NULL
+  CREATE TABLE _IT68183_VsaTour (
     VsaTourID int PRIMARY KEY,
     MinBearbTage int,
     Zeitpunkt datetime DEFAULT GETDATE()
@@ -19,34 +19,34 @@ IF OBJECT_ID('_IT67442_VsaTour') IS NULL
 GO
 
 WITH BaseData AS (
-  SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.ID AS VsaID, Vsa.VsaNr, Vsa.Bez, StandKon.ID AS StandKonID, StandKon.StandKonBez, ServType.ID AS ServTypeID, ServType.ServTypeBez, LEFT(_IT67442_StandKon.VsaName3 COLLATE Latin1_General_CS_AS + ISNULL(N' - ' + Vsa.Name3, N''), 40) AS Name3
-  FROM Salesianer.dbo._IT67442_StandKon
-  JOIN Kunden ON _IT67442_StandKon.KdNr = Kunden.KdNr
-  JOIN Vsa ON Vsa.KundenID = Kunden.ID AND Vsa.VsaNr = _IT67442_StandKon.VsaNr
-  JOIN StandKon ON StandKon.StandKonBez = _IT67442_StandKon.StandKon COLLATE Latin1_General_CS_AS
-  JOIN ServType ON ServType.ServTypeBez = _IT67442_StandKon.ServType COLLATE Latin1_General_CS_AS
+  SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.ID AS VsaID, Vsa.VsaNr, Vsa.Bez, StandKon.ID AS StandKonID, StandKon.StandKonBez, ServType.ID AS ServTypeID, ServType.ServTypeBez, LEFT(_IT68183_StandKon.VsaName3 COLLATE Latin1_General_CS_AS + ISNULL(N' - ' + Vsa.Name3, N''), 40) AS Name3
+  FROM Salesianer.dbo._IT68183_StandKon
+  JOIN Kunden ON _IT68183_StandKon.KdNr = Kunden.KdNr
+  JOIN Vsa ON Vsa.KundenID = Kunden.ID AND Vsa.VsaNr = _IT68183_StandKon.VsaNr
+  JOIN StandKon ON StandKon.StandKonBez = _IT68183_StandKon.StandKon COLLATE Latin1_General_CS_AS
+  JOIN ServType ON ServType.ServTypeBez = _IT68183_StandKon.ServType COLLATE Latin1_General_CS_AS
 )
 UPDATE Vsa SET StandKonID = BaseData.StandKonID, ServTypeID = BaseData.ServTypeID, Vsa.Name3 = BaseData.Name3
 OUTPUT deleted.ID, deleted.StandKonID, deleted.ServTypeID, deleted.Name3
-INTO _IT67442_Vsa (VsaID, StandKonID, ServTypeID, Name3)
+INTO _IT68183_Vsa (VsaID, StandKonID, ServTypeID, Name3)
 FROM BaseData
 WHERE BaseData.VsaID = Vsa.ID;
 
 GO
 
 WITH BaseData AS (
-  SELECT VsaTour.ID AS VsaTourID, _IT67442_TourTage.Bearbeitungstage
-  FROM Salesianer.dbo._IT67442_TourTage
-  JOIN Touren ON _IT67442_TourTage.Tour COLLATE Latin1_General_CS_AS = Touren.Tour
-  JOIN Kunden ON _IT67442_TourTage.KdNr = Kunden.KdNr
-  JOIN Vsa ON Vsa.KundenID = Kunden.ID AND Vsa.VsaNr = _IT67442_TourTage.VsaNr
+  SELECT VsaTour.ID AS VsaTourID, _IT68183_TourTage.Bearbeitungstage
+  FROM Salesianer.dbo._IT68183_TourTage
+  JOIN Touren ON _IT68183_TourTage.Tour COLLATE Latin1_General_CS_AS = Touren.Tour
+  JOIN Kunden ON _IT68183_TourTage.KdNr = Kunden.KdNr
+  JOIN Vsa ON Vsa.KundenID = Kunden.ID AND Vsa.VsaNr = _IT68183_TourTage.VsaNr
   JOIN VsaTour ON VsaTour.TourenID = Touren.ID AND VsaTour.VsaID = Vsa.ID AND VsaTour.BisDatum > CAST(GETDATE() AS date)
   JOIN KdBer ON VsaTour.KdBerID = KdBer.ID
-  JOIN Bereich ON KdBer.BereichID = Bereich.ID AND _IT67442_TourTage.Bereich COLLATE Latin1_General_CS_AS = Bereich.BereichBez
+  JOIN Bereich ON KdBer.BereichID = Bereich.ID AND _IT68183_TourTage.Bereich COLLATE Latin1_General_CS_AS = Bereich.BereichBez
 )
 UPDATE VsaTour SET MinBearbTage = BaseData.Bearbeitungstage
 OUTPUT deleted.ID, deleted.MinBearbTage
-INTO _IT67442_VsaTour (VsaTourID, MinBearbTage)
+INTO _IT68183_VsaTour (VsaTourID, MinBearbTage)
 FROM BaseData
 WHERE BaseData.VsaTourID = VsaTour.ID;
 
@@ -58,7 +58,7 @@ FROM (
   FROM Vsa
   JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID
   WHERE StandBer.BereichID = (SELECT Bereich.ID FROM Bereich WHERE Bereich.Bereich = N'FW')
-    AND Vsa.ID IN (SELECT _IT67442_Vsa.VsaID FROM _IT67442_Vsa)
+    AND Vsa.ID IN (SELECT _IT68183_Vsa.VsaID FROM _IT68183_Vsa)
 ) AS x
 WHERE x.VsaID = AnfKo.VsaID
   AND AnfKo.LieferDatum > GETDATE()
