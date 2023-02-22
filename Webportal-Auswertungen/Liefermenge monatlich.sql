20176c9569fa422feb24a50f4a3fa06a0276fb65
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-02-22 15:51:57 +0100

    IT-68344  - removed useless (for customer) columns  - optimized sql after column removal  - report now live

--- Webportal-Auswertungen/Liefermenge monatlich.sql
+++ Webportal-Auswertungen/Liefermenge monatlich.sql
@@ -60,22 +60,13 @@ ORDER BY CalMon.[Month] ASC;
 DROP TABLE IF EXISTS #LiefermengeVSA;
 
 CREATE TABLE #LiefermengeVSA (
-  [produzierender Betrieb] nvarchar(15) COLLATE Latin1_General_CS_AS,
-  [intern produzierender Betrieb] nvarchar(15) COLLATE Latin1_General_CS_AS,
-  Kette nvarchar(10) COLLATE Latin1_General_CS_AS,
-  Kundennummer int,
-  Kundenname nvarchar(20) COLLATE Latin1_General_CS_AS,
   [VSA-Nummer] int,
   [VSA-Bezeichnung] nvarchar(40) COLLATE Latin1_General_CS_AS,
   Kostenstelle nvarchar(20) COLLATE Latin1_General_CS_AS,
   Kostenstellenbezeichnung nvarchar(80) COLLATE Latin1_General_CS_AS,
-  Produktbereich nchar(3) COLLATE Latin1_General_CS_AS,
-  Artikelgruppe nchar(8) COLLATE Latin1_General_CS_AS,
   Artikelnummer nvarchar(15) COLLATE Latin1_General_CS_AS,
   Artikelbezeichnung nvarchar(60) COLLATE Latin1_General_CS_AS,
   Größe nvarchar(12) COLLATE Latin1_General_CS_AS,
-  Stückgewicht numeric(18, 4),
-  Auslieferart nchar(2) COLLATE Latin1_General_CS_AS,
   Monat nchar(7) COLLATE Latin1_General_CS_AS,
   Liefermenge numeric(18, 4)
 );
@@ -146,28 +137,19 @@ WITH LiefermengeMonatlich AS (
     AND ((Vsa.RentomatID > 0 AND Rentomat.LsKoArtScanOutID > 0 AND LsKo.LsKoArtID != Rentomat.LsKoArtScanOutID) OR (Vsa.RentomatID < 0) OR (Rentomat.LsKoArtScanOutID < 0))
   GROUP BY FORMAT(LsKo.Datum, N''yyyy-MM'', N''de-AT''), LsKo.VsaID, LsPo.KdArtiID, LsPo.AbteilID, EinzHist.ArtGroeID
 )
-INSERT INTO #LiefermengeVSA ([produzierender Betrieb], [intern produzierender Betrieb], Kette, Kundennummer, Kundenname, [VSA-Nummer], [VSA-Bezeichnung], Kostenstelle, Kostenstellenbezeichnung, Produktbereich, Artikelgruppe, Artikelnummer, Artikelbezeichnung, Größe, Stückgewicht, Auslieferart, Monat, Liefermenge)
-SELECT ProdBetrieb.SuchCode AS [produzierender Betrieb], IntProdBetrieb.SuchCode AS [intern produzierender Betrieb], Holding.Holding AS Kette, Kunden.KdNr AS Kundennummer, Kunden.SuchCode AS Kundenname, Vsa.VsaNr AS [VSA-Nummer], Vsa.Bez AS [VSA-Bezeichnung], Abteil.Abteilung AS Kostenstelle, Abteil.Bez AS Kostenstellenbezeichnung, Bereich.Bereich AS Produktbereich, ArtGru.Gruppe AS Artikelgruppe, Artikel.ArtikelNr AS Artikelnummer, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Artikel.StueckGewicht AS Stückgewicht, LiefArt.LiefArt AS Auslieferart, LiefermengeMonatlich.Monat, LiefermengeMonatlich.Liefermenge
+INSERT INTO #LiefermengeVSA ([VSA-Nummer], [VSA-Bezeichnung], Kostenstelle, Kostenstellenbezeichnung, Artikelnummer, Artikelbezeichnung, Größe, Monat, Liefermenge)
+SELECT Vsa.VsaNr AS [VSA-Nummer], Vsa.Bez AS [VSA-Bezeichnung], Abteil.Abteilung AS Kostenstelle, Abteil.Bez AS Kostenstellenbezeichnung, Artikel.ArtikelNr AS Artikelnummer, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, LiefermengeMonatlich.Monat, LiefermengeMonatlich.Liefermenge
 FROM (
   SELECT Monat, VsaID, KdArtiID, AbteilID, ArtGroeID, SUM(Liefermenge) AS Liefermenge
   FROM LiefermengeMonatlich
   GROUP BY Monat, VsaID, KdArtiID, AbteilID, ArtGroeID
 ) AS LiefermengeMonatlich
 JOIN Vsa ON LiefermengeMonatlich.VsaID = Vsa.ID
-JOIN Kunden ON Vsa.KundenID = Kunden.ID
-JOIN Holding ON Kunden.HoldingID = Holding.ID
 JOIN KdArti ON LiefermengeMonatlich.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-JOIN KdBer ON KdArti.KdBerID = KdBer.ID
-JOIN Bereich ON KdBer.BereichID = Bereich.ID
-JOIN ArtGru ON Artikel.ArtGruID = ArtGru.ID
-JOIN LiefArt ON KdArti.LiefArtID = LiefArt.ID
 JOIN ArtGroe ON LiefermengeMonatlich.ArtGroeID = ArtGroe.ID
 JOIN Abteil ON LiefermengeMonatlich.AbteilID = Abteil.ID
-JOIN StandBer ON StandBer.StandKonID = Vsa.StandKonID AND StandBer.BereichID = Bereich.ID
-JOIN Standort AS ProdBetrieb ON StandBer.ExpeditionID = ProdBetrieb.ID
-JOIN Standort AS IntProdBetrieb ON StandBer.ProduktionID = IntProdBetrieb.ID
-WHERE Kunden.ID = @customerid
+WHERE Vsa.KundenID = @customerid
   AND Vsa.AbteilID IN (
     SELECT WebUAbt.AbteilID
     FROM WebUAbt
@@ -185,6 +167,6 @@ WHERE Kunden.ID = @customerid
 
 EXEC sp_executesql @sqltext, N'@customerid int, @webuserid int, @basedate date, @maxdate date', @customerid, @webuserid, @basedate, @maxdate;
 
-SET @sqltext = N'SELECT [produzierender Betrieb], [intern produzierender Betrieb], Kette, Kundennummer, Kundenname, [VSA-Nummer], [VSA-Bezeichnung], Kostenstelle, Kostenstellenbezeichnung, Produktbereich, Artikelgruppe, Artikelnummer, Artikelbezeichnung, Größe, Stückgewicht, Auslieferart, ' + @pivotcolumns + ' FROM #LiefermengeVSA AS LiefermengeVSA PIVOT ( SUM(LiefermengeVSA.Liefermenge) FOR LiefermengeVSA.Monat IN (' + @pivotcolumns + ')) AS PivotResult ORDER BY Kundennummer, [VSA-Nummer], Artikelnummer';
+SET @sqltext = N'SELECT [VSA-Nummer], [VSA-Bezeichnung], Kostenstelle, Kostenstellenbezeichnung, Artikelnummer, Artikelbezeichnung, Größe, ' + @pivotcolumns + ' FROM #LiefermengeVSA AS LiefermengeVSA PIVOT ( SUM(LiefermengeVSA.Liefermenge) FOR LiefermengeVSA.Monat IN (' + @pivotcolumns + ')) AS PivotResult ORDER BY [VSA-Nummer], Artikelnummer';
 
 EXEC sp_executesql @sqltext;
