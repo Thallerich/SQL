9e9c104456d59a8d54defcb1902356ffa25e48c1
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-02-27 15:51:47 +0100

    added reporting query

--- IT-Tickets/IT-68606 - Kundenservice in VSA-Bereich über Tour aktualisieren.sql
+++ IT-Tickets/IT-68606 - Kundenservice in VSA-Bereich über Tour aktualisieren.sql
@@ -41,3 +41,28 @@ FROM #KSUpdate KSUpdate
 WHERE KSUpdate.VsaBerID = VsaBer.ID;
 
 GO
+
+SELECT Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.Bez, Bereich.BereichBez
+FROM (
+  SELECT VsaBer.ID AS VsaBerID, COUNT(DISTINCT Mitarbei.ID) AS KundenserviceID
+  FROM VsaBer
+  JOIN VsaTour ON VsaTour.VsaID = VsaBer.VsaID AND VsaTour.KdBerID = VsaBer.KdBerID
+  JOIN Touren ON VsaTour.TourenID = Touren.ID
+  JOIN Vsa ON VsaBer.VsaID = Vsa.ID
+  JOIN Kunden ON Vsa.KundenID = Kunden.ID
+  JOIN Salesianer.dbo._IT68606 ON Touren.Tour = _IT68606.Tour
+  JOIN Mitarbei ON _IT68606.Kundenservice = Mitarbei.Name
+  WHERE VsaTour.BisDatum > CAST(GETDATE() AS date)
+    AND Vsa.[Status] = N'A'
+    AND Kunden.[Status] = N'A'
+    AND Kunden.AdrArtID = 1
+  GROUP BY VsaBer.ID
+  HAVING COUNT(DISTINCT Mitarbei.ID) > 1
+) x
+JOIN VsaBer ON x.VsaBerID = VsaBer.ID
+JOIN KdBer ON VsaBer.KdBerID = KdBer.ID
+JOIN Bereich ON KdBer.BereichID = Bereich.ID
+JOIN Vsa ON VsaBer.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID;
+
+GO
