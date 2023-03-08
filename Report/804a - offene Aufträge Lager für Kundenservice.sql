85cbd41ec2b14b3349369845eb11588c7b8ea5b0
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-03-08 15:06:56 +0100

    rewrote to allow autofilter in advantex to work

--- Report/804a - offene Aufträge Lager für Kundenservice.sql
+++ Report/804a - offene Aufträge Lager für Kundenservice.sql
@@ -1,19 +1,52 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Pipeline: PrepareData                                                                                                     ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+DROP TABLE IF EXISTS #Result804a;
+
+DECLARE @fromdate datetime2 = CAST($STARTDATE$ AS datetime2);
+DECLARE @todate date = DATEADD(day, 1, CAST($ENDDATE$ AS datetime2));
+
+CREATE TABLE #Result804a (
+  ServiceMA nvarchar(40),
+  BetreuerMA nvarchar(40),
+  KdNr int,
+  Kunde nvarchar(20),
+  Holding nvarchar(10),
+  Nachname nvarchar(40),
+  Vorname nvarchar(20),
+  Trägerstatus nvarchar(40),
+  Barcode nvarchar(33),
+  Teilestatus nvarchar(40),
+  ArtikelNr nvarchar(15),
+  Artikelbezeichnung nvarchar(60),
+  Größe nvarchar(12),
+  Anlage datetime2,
+  Entnahmeliste int,
+  EntnahmeAnlage datetime2,
+  EntnahmeDruck datetime2,
+  EntnhameZeit datetime2,
+  EntnahmePatch datetime2,
+  Einsatzgrund nvarchar(60)
+);
+
 WITH Teilestatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
-  WHERE [Status].Tabelle = UPPER(N'EINZHIST')
+  WHERE [Status].Tabelle = N'EINZHIST'
 ),
 Traegerstatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
-  WHERE [Status].Tabelle = UPPER(N'TRAEGER')
+  WHERE [Status].Tabelle = N'TRAEGER'
 )
-SELECT ServiceMA.Name AS [Kundenservice-Mitarbeiter], BetreuerMA.Name AS [Betreuer], Kunden.Kdnr, Kunden.Suchcode as Kunde, Holding.Holding, Traeger.Nachname, Traeger.Vorname, Traegerstatus.StatusBez AS Trägerstatus, EinzHist.Barcode, Teilestatus.StatusBez AS Teilestatus, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS [Teil angelegt am], EntnKo.ID AS EntnahmelistenNr, EntnKo.Anlage_ AS [Entnahmeliste angelegt am], EntnKo.DruckDatum AS [Druckdatum Entnahmeliste], [Entnahme-Datum] = (
+INSERT INTO #Result804a (ServiceMA, BetreuerMA, KdNr, Kunde, Holding, Nachname, Vorname, Trägerstatus, Barcode, Teilestatus, ArtikelNr, Artikelbezeichnung, Größe, Anlage, Entnahmeliste, EntnahmeAnlage, EntnahmeDruck, EntnhameZeit, EntnahmePatch, Einsatzgrund)
+SELECT ServiceMA.Name AS ServiceMA, BetreuerMA.Name AS BetreuerMA, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, Traeger.Nachname, Traeger.Vorname, Traegerstatus.StatusBez AS Trägerstatus, EinzHist.Barcode, Teilestatus.StatusBez AS Teilestatus, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS Anlage, EntnKo.ID AS Entnahmeliste, EntnKo.Anlage_ AS EntnahmeAnlage, EntnKo.DruckDatum AS EntnahmeDruck, EntnahmeZeit = (
   SELECT MAX(Scans.[DateTime])
   FROM Scans
   WHERE Scans.EinzHistID = EinzHist.ID
     AND Scans.ActionsID = 57
-), EntnKo.PatchDatum AS [Patchdatum Entnahmeliste], Einsatz.Einsatzbez$LAN$
+), EntnKo.PatchDatum AS EntnahmePatch, Einsatz.Einsatzbez$LAN$ AS Einsatzgrund
 FROM EinzHist
 JOIN TraeArti ON EinzHist.TraeArtiID = TraeArti.ID
 JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
@@ -31,7 +64,7 @@ JOIN Traegerstatus ON Traeger.[Status] = Traegerstatus.[Status]
 JOIN Einsatz ON EinzHist.EinsatzGrund = Einsatz.EinsatzGrund
 LEFT JOIN EntnPo ON EinzHist.EntnPoID = EntnPo.ID AND EinzHist.EntnPoID > 0
 LEFT JOIN EntnKo ON EntnPo.EntnKoID = EntnKo.ID
-WHERE EinzHist.Anlage_ > N'2019-04-01 00:00:00'
+WHERE EinzHist.Anlage_ > CAST(N'2019-04-01 00:00:00' AS datetime2)
   AND Artikel.BereichID = 100
   AND Kunden.Status = N'A'
   AND Vsa.Status = N'A'
@@ -41,5 +74,13 @@ WHERE EinzHist.Anlage_ > N'2019-04-01 00:00:00'
   AND Kunden.StandortID IN ($3$)
   AND ServiceMA.ID IN ($4$)
   AND BetreuerMA.ID IN ($5$)
-  AND CAST(EinzHist.Anlage_ AS date) BETWEEN $STARTDATE$ AND $ENDDATE$
+  AND EinzHist.Anlage_ BETWEEN @fromdate AND @todate
+  AND EinzHist.IsCurrEinzHist = 1;
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Pipeline: Reportdaten                                                                                                     ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+SELECT ServiceMA AS [Kundenservice-Mitarbeiter], BetreuerMA AS Betreuer, KdNr, Kunde, Holding, Nachname, Vorname, Trägerstatus, Barcode, Teilestatus, ArtikelNr, Artikelbezeichnung, Größe, Anlage AS [Teil angelegt am], Entnahmeliste AS EntnahmelistenNr, EntnahmeAnlage AS [Entnahmeliste angelegt am], EntnahmeDruck AS [Druckdatum Entnahmeliste], EntnhameZeit AS [Entnahme-Datum], EntnahmePatch AS [Patchdatum Entnahmeliste], Einsatzgrund
+FROM #Result804a
 ORDER BY [Entnahmeliste angelegt am];
