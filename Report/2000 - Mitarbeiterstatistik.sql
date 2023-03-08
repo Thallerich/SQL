caceb76bdff58e9c2a5c2103f7453ce1d87cc0ab
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-03-08 09:06:17 +0100

    added wearer article increase / decrease statistic

--- Report/2000 - Mitarbeiterstatistik.sql
+++ Report/2000 - Mitarbeiterstatistik.sql
@@ -1,17 +1,29 @@
 DECLARE @fromdate date = $1$;
 DECLARE @todate date = $2$;
+DECLARE @fromdatetime datetime2 = CAST(@fromdate AS datetime2);
+DECLARE @todatetime datetime2 = DATEADD(day, 1, CAST(@todate AS datetime2));
 DECLARE @userid int = (SELECT MitarbeiID FROM #AdvSession);
 
 DECLARE @sqltext nvarchar(max);
 
 SET @sqltext = N'
-SELECT [Name], [TA] AS [Träger-An-/Abmeldung], [BV] AS [Bestandsveränderung], [MB] AS [manuelle Bestellung]
+SELECT [Name], [TA] AS [Träger-An-/Abmeldung], [TM] AS [Träger-Aufstockung/Reduzierung], [BV] AS [Bestandsveränderung], [MB] AS [manuelle Bestellung]
 FROM (
   SELECT Mitarbei.Name, COUNT(History.ID) AS AnzahlAktion, N''TA'' AS Aktion
   FROM History
   JOIN Mitarbei ON History.MitarbeiID = Mitarbei.ID
-  WHERE History.HistKatID IN (10013, 10014)  --Träger angelegt, Träger abgemeldet
-    AND History.Zeitpunkt BETWEEN @fromdate AND @todate
+  WHERE History.HistKatID IN (10013, 10014)  /* Träger angelegt, Träger abgemeldet */
+    AND History.Zeitpunkt BETWEEN @fromdatetime AND @todatetime
+    AND (Mitarbei.ChefMitarbeiID = @userid OR Mitarbei.ID = @userid)
+  GROUP BY Mitarbei.Name
+
+  UNION ALL
+
+  SELECT Mitarbei.Name, COUNT(History.ID) AS AnzahlAktion, N''TM'' AS Aktion
+  FROM History
+  JOIN Mitarbei ON History.MitarbeiID = Mitarbei.ID
+  WHERE History.HistKatID IN (10046, 10047)  /* Aufstockung, Reduzierung */
+    AND History.Zeitpunkt BETWEEN @fromdatetime AND @todatetime
     AND (Mitarbei.ChefMitarbeiID = @userid OR Mitarbei.ID = @userid)
   GROUP BY Mitarbei.Name
 
@@ -20,7 +32,7 @@ FROM (
   SELECT Mitarbei.Name, COUNT(VsaAnfHi.ID) AS AnzahlAktion, N''BV'' AS Aktion
   FROM VsaAnfHi
   JOIN Mitarbei ON VsaAnfHi.MitarbeiID = Mitarbei.ID
-  WHERE VsaAnfHi.Zeitpunkt BETWEEN @fromdate AND @todate
+  WHERE VsaAnfHi.Zeitpunkt BETWEEN @fromdatetime AND @todatetime
     AND VsaAnfHi.VertragDiff <> 0
     AND (Mitarbei.ChefMitarbeiID = @userid OR Mitarbei.ID = @userid)
   GROUP BY Mitarbei.Name
@@ -35,9 +47,9 @@ FROM (
   GROUP BY Mitarbei.Name
 ) AS MAData
 PIVOT (
-  SUM(AnzahlAktion) FOR Aktion IN ([TA], [BV], [MB])
+  SUM(AnzahlAktion) FOR Aktion IN ([TA], [TM], [BV], [MB])
 ) AS MAPivot
 ORDER BY [Name] ASC;
 ';
 
-EXEC sp_executesql @sqltext, N'@fromdate date, @todate date, @userid int', @fromdate, @todate, @userid;
+EXEC sp_executesql @sqltext, N'@fromdate date, @fromdatetime datetime2, @todate date, @todatetime datetime2, @userid int', @fromdate, @fromdatetime, @todate, @todatetime, @userid;
