7ba65b5e4764cca64cd31459558700a2a3be01b5
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-03-15 07:51:56 +0100

    added  - only textile articles (ArtiTypeID = 1)  - filter on "Bereich"  - exclude short term "warehouse" (Wareneingang / Rückgabe)

--- Report/943 - Lagerreichweite.sql
+++ Report/943 - Lagerreichweite.sql
@@ -5,7 +5,7 @@ WITH Entnahmen AS (
     AND LagerBew.LgBewCodID IN (SELECT LgBewCod.ID FROM LgBewCod WHERE (LgBewCod.IstEntnahme = 1 OR LgBewCod.Code = N'UMZL'))
   GROUP BY LagerBew.BestandID
 )
-SELECT Bereich.BereichBez AS Produktbereich, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, Standort.Bez AS Lagerstandort, LagerArt.Neuwertig AS IstNeuware, SUM(Bestand.Bestand) AS Lagerbestand, ISNULL(SUM(Entnahmen.AnzEntnahmen), 0) AS [Entnahmen letzte 12 Monate], CAST(ROUND(CAST(ISNULL(SUM(Entnahmen.AnzEntnahmen), 0) AS float) / 12, 0) AS int) AS [Durschnittliche monatliche Entnahmen], SUM(Bestand.Umlauf) AS Umlaufmenge
+SELECT Bereich.BereichBez AS Produktbereich, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse, Standort.Bez AS Lagerstandort, LagerArt.Neuwertig AS IstNeuware, SUM(Bestand.Bestand) AS Lagerbestand, Bestand.EntnahmeJahr, CAST(ROUND(CAST(ISNULL(SUM(Bestand.EntnahmeJahr), 0) AS float) / 12, 0) AS int) AS [Durschnittliche monatliche Entnahmen], SUM(Bestand.Umlauf) AS Umlaufmenge, ArtGroe.Umlauf as 'Gesamtumlauf'
 FROM Bestand
 JOIN ArtGroe ON Bestand.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
@@ -16,4 +16,8 @@ LEFT OUTER JOIN Entnahmen ON Entnahmen.BestandID = Bestand.ID
 WHERE Standort.ID IN ($1$)
   AND (Bestand.Bestand <> 0 OR ArtGroe.EntnahmeJahr <> 0)
   AND LagerArt.IstAnfLager = 0
-GROUP BY Bereich.BereichBez, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse, Standort.Bez, LagerArt.Neuwertig;
+  AND LagerArt.WarenEinLager = 0
+  AND LagerArt.Lagerart NOT IN (N'R', N'RB', N'RU', N'SMKRRU', N'WOENR') /* Rückgabe-Zwischenläger */
+  AND Artikel.ArtiTypeID = 1
+  AND Artikel.BereichID IN ($2$)
+GROUP BY Bereich.BereichBez, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse, Standort.Bez, LagerArt.Neuwertig, Bestand.EntnahmeJahr, ArtGroe.Umlauf;
