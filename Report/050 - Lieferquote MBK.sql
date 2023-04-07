09d4c62c967e5e0e185e48a170e5590cd5b71da0
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-04-07 11:12:05 +0200

    bugfix

--- Report/050 - Lieferquote MBK.sql
+++ Report/050 - Lieferquote MBK.sql
@@ -1,8 +1,4 @@
-BEGIN TRY
-  DROP TABLE #TmpFinal;
-END TRY
-BEGIN CATCH
-END CATCH;
+DROP TABLE IF EXISTS #TmpFinal;
 
 SELECT KdGf.KurzBez AS SGF, Kunden.KdNr, Kunden.SuchCode AS Kunde, StandBer.Metrik, 0 AS [Geplanter Ausgang], 0 AS [Tatsächlicher Ausgang], 0 AS Fehlteile, 0 AS [Nachwäsche], 0 AS Magazin, 0 AS [Reparatur Nähen], 0 AS Speicher, 0 AS Austausch, Vsa.ID AS VsaID
 INTO #TmpFinal
@@ -22,20 +18,20 @@ WHERE Vsa.KundenID = Kunden.ID
   AND VsaTour.TourenID = Touren.ID
   AND VsaTour.KdBerID = KdBer.ID
   AND KdBer.BereichID = StandBer.BereichID
-  AND Touren.Wochentag = CONVERT(char(1), IIF(DATEPART(dw, GETDATE()) - 1 = 0, 6, DATEPART(dw, GETDATE()) - 1)) --CONVERT(IIF(dbo.DayOfWeek(GETDATE()) - 1 = 0, 7, dbo.DayOfWeek(GETDATE()) - 1), SQL_CHAR)
+  AND Touren.Wochentag = CONVERT(char(1), IIF(DATEPART(dw, GETDATE()) - 1 = 0, 6, DATEPART(dw, GETDATE()) - 1))
   AND VsaTour.Bringen = 1
 GROUP BY KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, StandBer.Metrik, Vsa.ID;
 
 UPDATE Final SET Fehlteile = AnzTeileRueckstand
 FROM #TmpFinal Final, (
-  SELECT Vsa.ID AS VsaID, COUNT(Prod.TeileID) AS AnzTeileRueckstand
-  FROM Prod, Vsa, Teile, KdArti, KdBer
+  SELECT Vsa.ID AS VsaID, COUNT(Prod.EinzHistID) AS AnzTeileRueckstand
+  FROM Prod, Vsa, EinzHist, KdArti, KdBer
   WHERE Prod.VsaID = Vsa.ID
-    AND Prod.TeileID = Teile.ID
+    AND Prod.EinzHistID = EinzHist.ID
     AND Prod.KdArtiID = KdArti.ID
     AND KdArti.KdBerID = KdBer.ID
     AND KdBer.BereichID IN ($2$)
-    AND Teile.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
+    AND EinzHist.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
     AND Prod.AusDat <= GETDATE()
   GROUP BY Vsa.ID
 ) Rueckstand
@@ -43,14 +39,14 @@ WHERE Rueckstand.VsaID = Final.VsaID;
 
 UPDATE Final SET [Nachwäsche] = AnzNachwaesche
 FROM #TmpFinal Final, (
-  SELECT Vsa.ID AS VsaID, COUNT(Prod.TeileID) AS AnzNachwaesche
-  FROM Prod, Vsa, Teile, KdArti, KdBer
+  SELECT Vsa.ID AS VsaID, COUNT(Prod.EinzHistID) AS AnzNachwaesche
+  FROM Prod, Vsa, EinzHist, KdArti, KdBer
   WHERE Prod.VsaID = Vsa.ID
-    AND Prod.TeileID = Teile.ID
+    AND Prod.EinzHistID = EinzHist.ID
     AND Prod.KdArtiID = KdArti.ID
     AND KdArti.KdBerID = KdBer.ID
     AND KdBer.BereichID IN ($2$)
-    AND Teile.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
+    AND EinzHist.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
     AND Prod.AusDat <= GETDATE()
     AND Prod.ZielNrID IN (
       SELECT ZielNr.ID
@@ -63,14 +59,14 @@ WHERE Nachwaesche.VsaID = Final.VsaID;
 
 UPDATE Final SET Magazin = AnzMagazin
 FROM #TmpFinal Final, (
-  SELECT Vsa.ID AS VsaID, COUNT(Prod.TeileID) AS AnzMagazin
-  FROM Prod, Vsa, Teile, KdArti, KdBer
+  SELECT Vsa.ID AS VsaID, COUNT(Prod.EinzHistID) AS AnzMagazin
+  FROM Prod, Vsa, EinzHist, KdArti, KdBer
   WHERE Prod.VsaID = Vsa.ID
-    AND Prod.TeileID = Teile.ID
+    AND Prod.EinzHistID = EinzHist.ID
     AND Prod.KdArtiID = KdArti.ID
     AND KdArti.KdBerID = KdBer.ID
     AND KdBer.BereichID IN ($2$)
-    AND Teile.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
+    AND EinzHist.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
     AND Prod.AusDat <= GETDATE()
     AND Prod.ZielNrID IN (1109003, 109003, 209003) -- Metrik Enns, Lenzing 1 und 2: Lager Magazin
   GROUP BY Vsa.ID
@@ -79,14 +75,14 @@ WHERE Magazin.VsaID = Final.VsaID;
 
 UPDATE Final SET [Reparatur Nähen] = AnzReparatur
 FROM #TmpFinal Final, (
-  SELECT Vsa.ID AS VsaID, COUNT(Prod.TeileID) AS AnzReparatur
-  FROM Prod, Vsa, Teile, KdArti, KdBer
+  SELECT Vsa.ID AS VsaID, COUNT(Prod.EinzHistID) AS AnzReparatur
+  FROM Prod, Vsa, EinzHist, KdArti, KdBer
   WHERE Prod.VsaID = Vsa.ID
-    AND Prod.TeileID = Teile.ID
+    AND Prod.EinzHistID = EinzHist.ID
     AND Prod.KdArtiID = KdArti.ID
     AND KdArti.KdBerID = KdBer.ID
     AND KdBer.BereichID IN ($2$)
-    AND Teile.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
+    AND EinzHist.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
     AND Prod.AusDat <= GETDATE()
     AND Prod.ZielNrID IN (
       SELECT ZielNr.ID
@@ -99,14 +95,14 @@ WHERE Reparatur.VsaID = Final.VsaID;
 
 UPDATE Final SET Speicher = AnzSpeicher
 FROM #TmpFinal Final, (
-  SELECT Vsa.ID AS VsaID, COUNT(Prod.TeileID) AS AnzSpeicher
-  FROM Prod, Vsa, Teile, KdArti, KdBer
+  SELECT Vsa.ID AS VsaID, COUNT(Prod.EinzHistID) AS AnzSpeicher
+  FROM Prod, Vsa, EinzHist, KdArti, KdBer
   WHERE Prod.VsaID = Vsa.ID
-    AND Prod.TeileID = Teile.ID
+    AND Prod.EinzHistID = EinzHist.ID
     AND Prod.KdArtiID = KdArti.ID
     AND KdArti.KdBerID = KdBer.ID
     AND KdBer.BereichID IN ($2$)
-    AND Teile.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
+    AND EinzHist.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
     AND Prod.AusDat <= GETDATE()
     AND Prod.ZielNrID IN (
       SELECT ZielNr.ID
@@ -119,14 +115,14 @@ WHERE Speicher.VsaID = Final.VsaID;
 
 UPDATE Final SET Austausch = AnzAustausch
 FROM #TmpFinal Final, (
-  SELECT Vsa.ID AS VsaID, COUNT(Prod.TeileID) AS AnzAustausch
-  FROM Prod, Vsa, Teile, KdArti, KdBer
+  SELECT Vsa.ID AS VsaID, COUNT(Prod.EinzHistID) AS AnzAustausch
+  FROM Prod, Vsa, EinzHist, KdArti, KdBer
   WHERE Prod.VsaID = Vsa.ID
-    AND Prod.TeileID = Teile.ID
+    AND Prod.EinzHistID = EinzHist.ID
     AND Prod.KdArtiID = KdArti.ID
     AND KdArti.KdBerID = KdBer.ID
     AND KdBer.BereichID IN ($2$)
-    AND Teile.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
+    AND EinzHist.Status IN ('M', 'N', 'Q') --gepatcht, Dauer-BC, aktiv
     AND Prod.AusDat <= GETDATE()
     AND Prod.ZielNrID IN (109002, 209002, 1109002)  -- Lenzing 1 und 2, Enns - Austausch
   GROUP BY Vsa.ID
@@ -135,14 +131,14 @@ WHERE Austausch.VsaID = Final.VsaID;
 
 UPDATE Final SET Final.[Tatsächlicher Ausgang] = Ausgang.Ausgang
 FROM #TmpFinal Final, (
-  SELECT Final.VsaID, KdBer.BereichID, COUNT(Scans.TeileID) AS Ausgang
+  SELECT Final.VsaID, KdBer.BereichID, COUNT(Scans.EinzHistID) AS Ausgang
   FROM #TmpFinal Final, LsKo, LsPo, Scans, KdArti, KdBer
   WHERE Final.VsaID = LsKo.VsaID
     AND LsPo.LsKoID = LsKo.ID
     AND Scans.LsPoID = LsPo.ID
     AND LsPo.KdArtiID = KdArti.ID
     AND KdArti.KdBerID = KdBer.ID
-    AND LsKo.Datum = GETDATE()
+    AND LsKo.Datum = cast(GETDATE() as Date)
   GROUP BY Final.VsaID, KdBer.BereichID
 ) Ausgang
 WHERE Final.VsaID = Ausgang.VsaID
