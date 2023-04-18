695918b919d6e0ad8d3d40707ef4b0bd53749e8c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-04-18 14:39:21 +0200

    fix for 9.60

--- Report/905 - Bekleidungsausgabesystem_ Entnahmen.sql
+++ Report/905 - Bekleidungsausgabesystem_ Entnahmen.sql
@@ -14,6 +14,7 @@ Entnahmen AS (
 )
 SELECT Entnahmen.DateTime AS [Datum der Entnahme], EntnahmeTraeger.PersNr, EntnahmeTraeger.Traeger AS TraegerNr, EntnahmeTraeger.Nachname, EntnahmeTraeger.Vorname, Abteil.Abteilung AS KsSt, Abteil.Bez AS Kostenstelle, EinzHist.Barcode, EinzHist.RentomatChip AS Chipcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Kunden.KdNr, Kunden.SuchCode AS Kunde, IIF(EinzHist.Ausdienst IS NULL, EinzHist.RestwertInfo, EinzHist.AusdRestw) AS Restwert, Teilestatus.StatusBez AS [aktueller Status des Teils], Actions.ActionsBez$LAN$ AS [letzte Aktion des Teils]
 FROM EinzHist
+JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
 JOIN TraeArti ON EinzHist.TraeArtiID = TraeArti.ID
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
@@ -25,7 +26,7 @@ JOIN Teilestatus ON EinzHist.Status = Teilestatus.Status
 JOIN Entnahmen ON Entnahmen.EinzHistID = EinzHist.ID
 JOIN Traeger AS EntnahmeTraeger ON Entnahmen.LastPoolTraegerID = EntnahmeTraeger.ID
 JOIN Abteil ON EntnahmeTraeger.AbteilID = Abteil.ID
-JOIN Actions ON EinzHist.LastActionsID = Actions.ID
+JOIN Actions ON EinzTeil.LastActionsID = Actions.ID
 WHERE Vsa.RentomatID = $2$
   AND Traeger.RentoArtID IN (SELECT RentoArt.ID FROM RentoArt WHERE RentoArt.Code IN (N'S', N'T'))
 ORDER BY Barcode ASC;
