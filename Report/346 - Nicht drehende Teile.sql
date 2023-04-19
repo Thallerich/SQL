1bbf1e51535932fd88e20300e07d3f7ba59af819
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-04-19 15:38:52 +0200

    fix for 9.60

--- Report/346 - Nicht drehende Teile.sql
+++ Report/346 - Nicht drehende Teile.sql
@@ -1,15 +1,19 @@
 DECLARE @currentweek nchar(7) = (SELECT Week.Woche FROM Week WHERE CAST(GETDATE() AS date) BETWEEN Week.VonDat AND Week.BisDat);
 
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr AS [VSA-Nr.], Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS VSA, Traeger.Traeger, Traeger.Titel, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Teile.Barcode, Teile.Eingang1 AS [letzter Eingang], Teile.Ausgang1 AS [letzter Ausgang], TeileRW.RestwertInfo AS [aktueller Restwert]
-FROM Teile
-CROSS APPLY funcGetRestwert(Teile.ID, @currentweek, 1) AS TeileRW
-JOIN TraeArti ON Teile.TraeArtiID = TraeArti.ID
-JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
-JOIN Vsa ON Traeger.VsaID = Vsa.ID
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr AS [VSA-Nr.], Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS VSA, Traeger.Traeger, Traeger.Titel, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, EinzHist.Barcode, EinzHist.Eingang1 AS [letzter Eingang], EinzHist.Ausgang1 AS [letzter Ausgang], EinzHist.RestwertInfo AS [aktueller Restwert], Lagerart.LagerartBez$LAN$ AS Lagerart
+FROM EinzHist
+JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
+JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
+JOIN Vsa ON EinzHist.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
-JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
+JOIN KdArti ON EinzHist.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
-WHERE Kunden.ID = $ID$
-  AND Teile.Eingang1 < $1$
-  AND Teile.Status = 'Q'
+JOIN Lagerart ON EinzHist.LagerartID = Lagerart.ID
+WHERE Vsa.KundenID = $ID$
+  AND EinzHist.Eingang1 < $1$
+  AND EinzHist.Status = N'Q'
+  AND EinzHist.IsCurrEinzHist = 1
+  AND EinzHist.EinzHistTyp = 1
+  AND EinzTeil.AltenheimModus = 0
+  AND Lagerart.LagerID IN ($3$) 
 ORDER BY [VSA-Nr.], Traeger.Traeger, Artikel.ArtikelNr;
