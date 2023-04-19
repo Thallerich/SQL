d23daf8e5cafbdba53eadea4ef7ae6f9bd4f25fb
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-04-19 09:29:10 +0200

    bugfix to enable sorting / filtering

--- Report/296 - Benutzer Statistik Tag.sql
+++ Report/296 - Benutzer Statistik Tag.sql
@@ -1,9 +1,29 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Prepare data                                                                                                              ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 IF OBJECT_ID(N'tempdb..#TmpSichtbar') IS NULL
   CREATE TABLE #TmpSichtbar (
     SichtbarID int PRIMARY KEY CLUSTERED
   );
 ELSE
   DELETE FROM #TmpSichtbar;
+
+IF OBJECT_ID(N'tempdb..#TmpResult296') IS NULL
+  CREATE TABLE #TmpResult296 (
+    Arbeitsplatz nvarchar(8) COLLATE Latin1_General_CS_AS,
+    Eingelesen int NOT NULL DEFAULT 0,
+    Gepatcht int NOT NULL DEFAULT 0,
+    Ausgelesen int NOT NULL DEFAULT 0,
+    Info int NOT NULL DEFAULT 0,
+    Schrott int NOT NULL DEFAULT 0,
+    Infektion int NOT NULL DEFAULT 0,
+    Falsch int NOT NULL DEFAULT 0,
+    Rückgabe int NOT NULL DEFAULT 0,
+    Gesamt int NOT NULL DEFAULT 0
+  );
+ELSE
+  DELETE FROM #TmpResult296;
 
 INSERT INTO #TmpSichtbar (SichtbarID)
 SELECT x.value AS SichtbarID
@@ -14,15 +34,16 @@ DECLARE @totime datetime = CAST(CAST(CAST(N'2023-04-18' AS date) AS nchar(10)) +
 DECLARE @sqltext nvarchar(max);
 
 SET @sqltext = N'
+INSERT INTO #TmpResult296 (Arbeitsplatz, Eingelesen, Gepatcht, Ausgelesen, Info, Schrott, Infektion, Falsch, Rückgabe, Gesamt)
 SELECT Mitarbei.UserName AS Arbeitsplatz,
-  SUM(CASE Scans.ZielNrID WHEN 1 THEN 1 END) AS [eingelesen],
-  SUM(CASE Scans.ZielNrID WHEN 7 THEN 1 END) AS [neu gepatcht],
-  SUM(CASE Scans.ZielNrID WHEN 2 THEN 1 END) AS [ausgelesen],
-  SUM(CASE Scans.ZielNrID WHEN 36 THEN 1 END) AS [Teile Info],
-  SUM(CASE Scans.ZielNrID WHEN 19 THEN 1 END) AS [verschrottet],
-  SUM(CASE Scans.ZielNrID WHEN 37 THEN 1 END) AS [Infektionswäsche],
-  SUM(CASE Scans.ZielNrID WHEN 38 THEN 1 END) AS [Falschabwurf],
-  SUM(CASE Scans.ZielNrID WHEN 6 THEN 1 END) AS [Rückgabe],
+  ISNULL(SUM(CASE Scans.ZielNrID WHEN 1 THEN 1 END), 0) AS [eingelesen],
+  ISNULL(SUM(CASE Scans.ZielNrID WHEN 7 THEN 1 END), 0) AS [neu gepatcht],
+  ISNULL(SUM(CASE Scans.ZielNrID WHEN 2 THEN 1 END), 0) AS [ausgelesen],
+  ISNULL(SUM(CASE Scans.ZielNrID WHEN 36 THEN 1 END), 0) AS [Teile Info],
+  ISNULL(SUM(CASE Scans.ZielNrID WHEN 19 THEN 1 END), 0) AS [verschrottet],
+  ISNULL(SUM(CASE Scans.ZielNrID WHEN 37 THEN 1 END), 0) AS [Infektionswäsche],
+  ISNULL(SUM(CASE Scans.ZielNrID WHEN 38 THEN 1 END), 0) AS [Falschabwurf],
+  ISNULL(SUM(CASE Scans.ZielNrID WHEN 6 THEN 1 END), 0) AS [Rückgabe],
   COUNT(*) AS Total
 FROM Scans
 JOIN EinzTeil ON Scans.EinzTeilID = EinzTeil.ID
@@ -35,3 +56,10 @@ GROUP BY Mitarbei.UserName;
 ';
 
 EXEC sp_executesql @sqltext, N'@from datetime, @to datetime', @fromtime, @totime;
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Reporting                                                                                                                 ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+SELECT Arbeitsplatz, Eingelesen AS eingelesen, Gepatcht AS [neu gepatcht], Ausgelesen AS ausgelesen, Info AS [Teile Info], Schrott AS [verschrottet], Infektion AS Infektionswäsche, Falsch AS Falschabwurf, Rückgabe, Gesamt AS Total
+FROM #TmpResult296;
