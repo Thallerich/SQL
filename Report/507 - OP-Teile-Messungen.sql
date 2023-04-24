d97b43741fbbdeb70b9143dfd5a2ce78f1d6685f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-04-24 08:25:50 +0200

    bugfix - use correct column

--- Report/507 - OP-Teile-Messungen.sql
+++ Report/507 - OP-Teile-Messungen.sql
@@ -1,4 +1,4 @@
-SELECT EinzTeil.Code AS Barcode, OPTMess.Datum, Standort.Bez AS Produktion, WaschMa.Bez AS Waschmaschine, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, OPTMess.Flaechenmessung AS Fläche, OPTMess.Nahtmessung AS Naht, OPTMess.Info, EinzTeil.AnzWasch AS Wäschen
+SELECT EinzTeil.Code AS Barcode, OPTMess.Datum, Standort.Bez AS Produktion, WaschMa.Bez AS Waschmaschine, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, OPTMess.Flaechenmessung AS Fläche, OPTMess.Nahtmessung AS Naht, OPTMess.Info, EinzTeil.RuecklaufG AS Wäschen
 FROM OPTMess
 JOIN EinzTeil ON OPTMess.EinzTeilID = EinzTeil.ID
 JOIN ArtGroe ON EinzTeil.ArtGroeID = ArtGroe.ID
