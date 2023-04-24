d1c04a4ee7b7e9b326e8c675d176acf519769563
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-04-24 13:12:42 +0200

    initial version

---
+++ Troubleshooting and Maintenance/Check-Constraint not trusted.sql
@@ -0,0 +1,15 @@
+SELECT 'ALTER TABLE [' + s.name + '].[' + o.name + '] WITH CHECK CHECK CONSTRAINT [' + i.name + '];' + CHAR(13) + CHAR(10) + N'GO' AS keyname
+from sys.foreign_keys i
+INNER JOIN sys.objects o ON i.parent_object_id = o.object_id
+INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+WHERE i.is_not_trusted = 1 AND i.is_not_for_replication = 0
+
+UNION
+
+SELECT 'ALTER TABLE [' + s.name + '].[' + o.name + '] WITH CHECK CHECK CONSTRAINT [' + i.name + '];' + CHAR(13) + CHAR(10) + N'GO' AS keyname
+from sys.check_constraints i
+INNER JOIN sys.objects o ON i.parent_object_id = o.object_id
+INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
+WHERE i.is_not_trusted = 1 AND i.is_not_for_replication = 0 AND i.is_disabled = 0;
+
+GO
