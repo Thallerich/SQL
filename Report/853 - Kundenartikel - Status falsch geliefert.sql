f34a99e91bdde9376e194504a45803d2f314c7c2
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-05-02 10:17:37 +0200

    IT-70772  - initial version

---
+++ Report/853 - Kundenartikel - Status falsch geliefert.sql
@@ -0,0 +1,13 @@
+SELECT Kunden.KdNr,
+  Kunden.SuchCode AS Kunde,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelBez AS Artikelbezeichnung,
+  KdArti.Variante,
+  KdArti.WaschPreis AS Bearbeitungspreis,
+  KdArti.LeasPreis AS Leasingpreis,
+  KdArti.ID AS KdArtiID
+FROM KdArti
+JOIN Kunden ON KdArti.KundenID = Kunden.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+WHERE Kunden.ID IN ($10$)
+  AND KdArti.Status = N'F';
