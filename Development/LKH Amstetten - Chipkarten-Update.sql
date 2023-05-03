81b6fc9804a3e4847f698eeb0f482b3c624003ff
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-05-03 15:35:34 +0200

    prepared with current data and table layout

--- Development/LKH Amstetten - Chipkarten-Update.sql
+++ Development/LKH Amstetten - Chipkarten-Update.sql
@@ -4,14 +4,16 @@ SELECT @vsaid = Vsa.ID
 FROM Vsa
 WHERE Vsa.RentomatID = 62;
 
-SELECT Traeger.ID AS TraegerID, Traeger.Nachname, Traeger.Vorname, Traeger.PersNr, _LKHAmstettenBKS.PersNr AS PersNr_Neu, Traeger.ChipSerienNummer, Traeger.RentomatKarte, _LKHAmstettenBKS.KartenID AS KarteNeu
+SELECT Traeger.ID AS TraegerID, Traeger.Nachname, Traeger.Vorname, Traeger.PersNr, _LKAmstettenBKS.PersNr AS PersNr_Neu, Traeger.ChipSerienNummer, Traeger.RentomatKarte, _LKAmstettenBKS.RentomartKarte AS KarteNeu
 FROM Traeger
-JOIN _LKHAmstettenBKS ON SUBSTRING(Traeger.PersNr, PATINDEX('%[^0]%', Traeger.PersNr), 20) = _LKHAmstettenBKS.PersNr COLLATE Latin1_General_CS_AS
+JOIN _LKAmstettenBKS ON SUBSTRING(Traeger.PersNr, PATINDEX('%[^0]%', Traeger.PersNr), 20) = _LKAmstettenBKS.PersNr
 WHERE Traeger.VsaID = @vsaid
   AND Traeger.Status = N'A';
 
-/* UPDATE Traeger SET RentomatKarte = _LKHAmstettenBKS.KartenID COLLATE Latin1_General_CS_AS
+/* 
+UPDATE Traeger SET RentomatKarte = _LKAmstettenBKS.KartenID
 FROM Traeger
-JOIN _LKHAmstettenBKS ON SUBSTRING(Traeger.PersNr, PATINDEX('%[^0]%', Traeger.PersNr), 20) = _LKHAmstettenBKS.PersNr COLLATE Latin1_General_CS_AS
+JOIN _LKAmstettenBKS ON SUBSTRING(Traeger.PersNr, PATINDEX('%[^0]%', Traeger.PersNr), 20) = _LKAmstettenBKS.PersNr 
 WHERE Traeger.VsaID = @vsaid
-  AND Traeger.Status = N'A'; */
+  AND Traeger.Status = N'A';
+*/
