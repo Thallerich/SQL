5130ac07d7210e61f983d1520c17fe50fed802e3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-05-04 08:00:02 +0200

    ran on live

--- Diverse/LKH Amstetten - Chipkarten-Update.sql
+++ Diverse/LKH Amstetten - Chipkarten-Update.sql
@@ -4,16 +4,15 @@ SELECT @vsaid = Vsa.ID
 FROM Vsa
 WHERE Vsa.RentomatID = 62;
 
-SELECT Traeger.ID AS TraegerID, Traeger.Nachname, Traeger.Vorname, Traeger.PersNr, _LKAmstettenBKS.PersNr AS PersNr_Neu, Traeger.ChipSerienNummer, Traeger.RentomatKarte, _LKAmstettenBKS.RentomartKarte AS KarteNeu
+/* SELECT Traeger.ID AS TraegerID, Traeger.Nachname, Traeger.Vorname, Traeger.PersNr, _LKAmstettenBKS.PersNr AS PersNr_Neu, Traeger.ChipSerienNummer, Traeger.RentomatKarte, _LKAmstettenBKS.RentomartKarte AS KarteNeu
 FROM Traeger
 JOIN _LKAmstettenBKS ON SUBSTRING(Traeger.PersNr, PATINDEX('%[^0]%', Traeger.PersNr), 20) = _LKAmstettenBKS.PersNr
 WHERE Traeger.VsaID = @vsaid
-  AND Traeger.Status = N'A';
+  AND Traeger.Status = N'A'; */
 
-/* 
-UPDATE Traeger SET RentomatKarte = _LKAmstettenBKS.KartenID
+
+UPDATE Traeger SET RentomatKarte = _LKAmstettenBKS.RentomartKarte
 FROM Traeger
 JOIN _LKAmstettenBKS ON SUBSTRING(Traeger.PersNr, PATINDEX('%[^0]%', Traeger.PersNr), 20) = _LKAmstettenBKS.PersNr 
 WHERE Traeger.VsaID = @vsaid
   AND Traeger.Status = N'A';
-*/
