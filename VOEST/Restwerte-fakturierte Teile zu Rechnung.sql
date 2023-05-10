0e371c2cbb572c2e48dd7ae6917f8912ff088948
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-05-10 15:15:29 +0200

    invoicing done, added report

--- VOEST/Restwerte-fakturierte Teile zu Rechnung.sql
+++ VOEST/Restwerte-fakturierte Teile zu Rechnung.sql
@@ -1,16 +1,36 @@
-SELECT EinzHist.Barcode, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, Vsa.Bez AS VSA, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Grö0e, EinzHist.AbmeldDat AS [Abmelde-Datum], Einsatz.EinsatzBez AS [Abmelde-Grund], IIF(RPoType.RPoTypeBez != N'Restwerte', LTRIM(REPLACE(RPoType.RPoTypeBez, N'Restwerte', N'')), RPoType.RPoTypeBez) AS Berechnungsgrund, CAST(TeilSoFa.EPreis AS float) AS Restwert
+SELECT EinzHist.Barcode, Kunden.KdNr, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, Vsa.Bez AS VSA, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Grö0e, EinzHist.AbmeldDat AS [Abmelde-Datum], /* Einsatz.EinsatzBez AS [Abmelde-Grund], */ IIF(RPoType.RPoTypeBez != N'Restwerte', LTRIM(REPLACE(RPoType.RPoTypeBez, N'Restwerte', N'')), RPoType.RPoTypeBez) AS Berechnungsgrund, CAST(TeilSoFa.EPreis AS float) AS Restwert
 FROM TeilSoFa
 JOIN RechPo ON TeilSoFa.RechPoID = RechPo.ID
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
 JOIN EinzHist ON TeilSoFa.EinzHistID = EinzHist.ID
 JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
-JOIN Einsatz ON EinzHist.AusdienstGrund = Einsatz.EinsatzGrund
+--JOIN Einsatz ON EinzHist.AusdienstGrund = Einsatz.EinsatzGrund
 JOIN RPoType ON TeilSoFa.RPoTypeID = RPoType.ID
 WHERE RechKo.RechNr = 30303276
   AND EinzHist.Barcode IN (SELECT Barcode COLLATE Latin1_General_CS_AS FROM Salesianer.dbo._VOEST_KVPTeile);
+
+GO
+
+SELECT EinzHist.Barcode, Kunden.KdNr, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, Vsa.Bez AS VSA, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Grö0e, EinzHist.AbmeldDat AS [Abmelde-Datum], /* Einsatz.EinsatzBez AS [Abmelde-Grund], */ IIF(RPoType.RPoTypeBez != N'Restwerte', LTRIM(REPLACE(RPoType.RPoTypeBez, N'Restwerte', N'')), RPoType.RPoTypeBez) AS Berechnungsgrund, CAST(TeilSoFa.EPreis AS float) AS Restwert, GRechKo.RechNr, GRechKo.RechDat
+FROM TeilSoFa
+JOIN RechPo ON TeilSoFa.RechPoID = RechPo.ID
+JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+JOIN RechPo GRechPo ON TeilSoFa.RechPoGutschriftID = GRechPo.ID
+JOIN RechKo GRechKo ON GRechPo.RechKoID = GRechKo.ID
+JOIN EinzHist ON TeilSoFa.EinzHistID = EinzHist.ID
+JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
+JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+--JOIN Einsatz ON EinzHist.AusdienstGrund = Einsatz.EinsatzGrund
+JOIN RPoType ON TeilSoFa.RPoTypeID = RPoType.ID
+WHERE EinzHist.Barcode IN (SELECT Barcode COLLATE Latin1_General_CS_AS FROM Salesianer.dbo._VOEST_KVPTeile)
+  AND RechKo.RechNr = 30303276;
 
 GO
 
