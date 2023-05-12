ad970487ef78522ea93d6b8beb177c5034ffdc50
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-05-12 12:20:24 +0200

    bugfix  teile -> einzhist + einzteil

--- Report/976a - Verlustteile des Kunden.sql
+++ Report/976a - Verlustteile des Kunden.sql
@@ -1,14 +1,18 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.Bez AS Vsa, Teile.Barcode, Status.StatusBez$LAN$ AS Teilestatus, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, Teile.AusdienstDat, Einsatz.EinsatzBez$LAN$ AS AusdienstGrund, WegGrund.WegGrundBez$LAN$ AS Tauschgrund, Teile.AusdRestw AS Restwert
-FROM Teile, Vsa, Kunden, Einsatz, Artikel, ArtGroe, Status, WegGrund
-WHERE Teile.VsaID = Vsa.ID
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.Bez AS Vsa, EinzHist.Barcode, Status.StatusBez$LAN$ AS Teilestatus, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, EinzHist.AusdienstDat, Einsatz.EinsatzBez$LAN$ AS AusdienstGrund, WegGrund.WegGrundBez$LAN$ AS Tauschgrund, EinzHist.AusdRestw AS Restwert
+FROM EinzHist, EinzTeil, Vsa, Kunden, Einsatz, Artikel, ArtGroe, Status, WegGrund
+WHERE EinzHist.EinzTeilID = EinzTeil.ID
+  AND EinzHist.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
-  AND Teile.AusdienstGrund = Einsatz.EinsatzGrund
-  AND Teile.ArtikelID = Artikel.ID
-  AND Teile.ArtGroeID = ArtGroe.ID
-  AND Teile.Status = Status.Status
-  AND Status.Tabelle = 'TEILE'
-  AND Teile.WegGrundID = WegGrund.ID
+  AND EinzHist.AusdienstGrund = Einsatz.EinsatzGrund
+  AND EinzHist.ArtikelID = Artikel.ID
+  AND EinzHist.ArtGroeID = ArtGroe.ID
+  AND EinzHist.Status = Status.Status
+  AND Status.Tabelle = 'EINZHIST'
+  AND EinzHist.WegGrundID = WegGrund.ID
   AND Kunden.ID = $ID$
-  AND Teile.AusdienstGrund IN ('d', 'D')
-  AND Teile.AltenheimModus = 0
-  AND Teile.AusdienstDat BETWEEN $1$ AND $2$;
+  AND EinzHist.AusdienstGrund IN ('d', 'D')
+  AND EinzTeil.AltenheimModus = 0
+  AND EinzHist.AusdienstDat BETWEEN $1$ AND $2$
+  AND EinzHist.IsCurrEinzHist = 1
+  AND EinzHist.PoolFkt = 0
+  AND EinzHist.EinzHistTyp = 1;
