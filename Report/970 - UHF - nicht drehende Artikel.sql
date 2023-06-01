20c838adb60e51582e80fda1132a3669b16eaa28
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-06-01 10:28:40 +0200

    bugfix db structure changes release 9.60

--- Report/970 - UHF - nicht drehende Artikel.sql
+++ Report/970 - UHF - nicht drehende Artikel.sql
@@ -1,16 +1,18 @@
 DECLARE @filter datetime = CAST(CAST($1$ AS nchar(10))+ N' 00:00:00' AS datetime);
 
-SELECT Kunden.ID AS KundenID, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, COUNT(OPTeile.ID) AS AnzahlTeile
-FROM OPTeile, Vsa, Kunden, Artikel
-WHERE OPTeile.VsaID = Vsa.ID
-  AND Vsa.KundenID = Kunden.ID
-  AND OPTeile.ArtikelID = Artikel.ID
-  AND Kunden.ID = $ID$
-  AND DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) > 90
-  AND ((OPTeile.Status = N'Q' AND OPTeile.LastActionsID = 102) OR (OPTeile.Status = 'W' AND OPTeile.RechPoID = -1)) -- bei Schwund-Teilen nur nicht verrechnete und nicht für Verrechnung gesperrte Teile
+SELECT Kunden.ID AS KundenID, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, COUNT(EinzTeil.ID) AS AnzahlTeile
+FROM EinzTeil
+JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
+JOIN Vsa ON EinzTeil.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
+WHERE Kunden.ID = $ID$
+  AND DATEDIFF(day, EinzTeil.LastScanToKunde, GETDATE()) > 90
+  AND ((EinzTeil.[Status] = N'Q' AND EinzTeil.LastActionsID = 102) OR (EinzTeil.[Status] = N'W' AND EinzTeil.RechPoID = -1))  /* bei Schwund-Teilen nur nicht verrechnete und nicht für Verrechnung gesperrte Teile */
   AND Artikel.EAN IS NOT NULL
-  AND LEN(OPTeile.Code) = 24
-  AND Artikel.BereichID <> 104
-  AND OPTeile.LastScanToKunde > @filter
+  AND LEN(EinzTeil.Code) = 24
+  AND Artikel.BereichID != 104
+  AND EinzTeil.LastScanToKunde > @filter
+  AND EinzHist.PoolFkt = 1
 GROUP BY Kunden.ID, Kunden.KdNr, Kunden.SuchCode, Vsa.VsaNr, Vsa.SuchCode, Vsa.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$
 ORDER BY Kunden.KdNr, Vsa.VsaNr, Artikelbezeichnung;
--- Report/970a - UHF - nicht drehende Teile.sql
+++ Report/970a - UHF - nicht drehende Teile.sql
@@ -2,17 +2,19 @@ DECLARE @RwArt integer = 1;
 DECLARE @Woche nchar(7) = (SELECT Week.Woche FROM Week WHERE CAST(GETDATE() AS date) BETWEEN Week.VonDat AND Week.BisDat);
 DECLARE @filter datetime = CAST(CAST($1$ AS nchar(10))+ N' 00:00:00' AS datetime);
 
-SELECT Kunden.ID AS KundenID, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, OPTeile.Code, OPTeile.Code2, OPTeile.LastScanToKunde AS [letzter Ausgangsscan], OPTeile.Erstwoche, OPRW.RestwertInfo AS Restwert
-FROM OPTeile
-CROSS APPLY funcGetRestwertOP(OPTeile.ID, @Woche, @RwArt) AS OPRW
-JOIN Vsa ON OPTeile.VsaID = Vsa.ID
+SELECT Kunden.ID AS KundenID, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS Vsa, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, EinzTeil.Code, EinzTeil.Code2, EinzTeil.LastScanToKunde AS [letzter Ausgangsscan], EinzTeil.Erstwoche, OPRW.RestwertInfo AS Restwert
+FROM EinzTeil
+CROSS APPLY dbo.funcGetRestwertOP(EinzTeil.ID, @Woche, @RwArt) AS OPRW
+JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
+JOIN Vsa ON EinzTeil.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
-JOIN Artikel ON OPTeile.ArtikelID = Artikel.ID
+JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
 WHERE Kunden.ID = $ID$
-  AND DATEDIFF(day, OPTeile.LastScanToKunde, GETDATE()) > 90
-  AND ((OPTeile.Status = 'Q' AND OPTeile.LastActionsID = 102) OR (OPTeile.Status = 'W' AND OPTeile.RechPoID = -1)) -- bei Schwund-Teilen nur nicht verrechnete und nicht für Verrechnung gesperrte Teile
+  AND DATEDIFF(day, EinzTeil.LastScanToKunde, GETDATE()) > 90
+  AND ((EinzTeil.[Status] = N'Q' AND EinzTeil.LastActionsID = 102) OR (EinzTeil.[Status] = N'W' AND EinzTeil.RechPoID = -1))  /* bei Schwund-Teilen nur nicht verrechnete und nicht für Verrechnung gesperrte Teile */
   AND Artikel.EAN IS NOT NULL
-  AND LENGTH(OPTeile.Code) = 24
-  AND Artikel.BereichID <> 104
-  AND OPTeile.LastScanToKunde > @filter
+  AND LEN(EinzTeil.Code) = 24
+  AND Artikel.BereichID != 104
+  AND EinzTeil.LastScanToKunde > @filter
+  AND EinzHist.PoolFkt = 1
 ORDER BY Kunden.KdNr, Vsa.VsaNr, Artikel.ArtikelNr;
