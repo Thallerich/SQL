f0354cf68e89dd6c5c7346c44b51bcf5f3ff4edb
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-06-05 10:41:39 +0200

    bugfix  - db structure changes 9.60

--- Report/935a - Träger-Details eines Kunden.sql
+++ Report/935a - Träger-Details eines Kunden.sql
@@ -1,16 +1,16 @@
 WITH Traegerstatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
-  WHERE [Status].Tabelle = UPPER(N'TRAEGER')
+  WHERE [Status].Tabelle = N'TRAEGER'
 ),
 Teilestatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
-  WHERE [Status].Tabelle = UPPER(N'TEILE')
+  WHERE [Status].Tabelle = N'EINZHIST'
 )
-SELECT DISTINCT Traeger.ID AS TraegerID, Traeger.Traeger, Traeger.Nachname, Traeger.Vorname, Traeger.Titel, Traeger.PersNr, Traegerstatus.StatusBez AS Traegerstatus, CAST(IIF(TraeAppl.ArtiTypeID = 2, 1, 0) AS bit) AS NS, Traeger.Namenschild1, Traeger.Namenschild2, Traeger.Namenschild3, Traeger.SchrankInfo, Abteil.Abteilung AS Kostenstelle, Abteil.Bez AS Kostenstellenbezeichnung, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, KdArti.Variante, TraeArti.Menge, Teile.Barcode, Teilestatus.StatusBez AS Teilestatus, Teile.Eingang1, Teile.Ausgang1
-FROM Teile
-JOIN TraeArti ON Teile.TraeArtiID = TraeArti.ID
+SELECT DISTINCT Traeger.ID AS TraegerID, Traeger.Traeger, Traeger.Nachname, Traeger.Vorname, Traeger.Titel, Traeger.PersNr, Traegerstatus.StatusBez AS Traegerstatus, CAST(IIF(TraeAppl.ArtiTypeID = 2, 1, 0) AS bit) AS NS, Traeger.Namenschild1, Traeger.Namenschild2, Traeger.Namenschild3, Traeger.SchrankInfo, Abteil.Abteilung AS Kostenstelle, Abteil.Bez AS Kostenstellenbezeichnung, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, KdArti.Variante, TraeArti.Menge, EinzHist.Barcode, Teilestatus.StatusBez AS Teilestatus, EinzHist.Eingang1, EinzHist.Ausgang1
+FROM EinzHist
+JOIN TraeArti ON EinzHist.TraeArtiID = TraeArti.ID
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -19,9 +19,12 @@ JOIN Abteil ON Traeger.AbteilID = Abteil.ID
 JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
-JOIN Teilestatus ON Teile.Status = Teilestatus.Status
+JOIN Teilestatus ON EinzHist.Status = Teilestatus.Status
 LEFT JOIN TraeAppl ON TraeAppl.TraeArtiID = TraeArti.ID AND TraeAppl.ArtiTypeID = 2
 WHERE Kunden.ID = $ID$
-  AND Teile.Status IN (N'A', N'E', N'G', N'I', N'K', N'L', N'M', N'O', N'C', N'Q', N'S', N'N')
-  AND Traeger.Status <> N'I'
-ORDER BY Traeger.Nachname, Traeger.Vorname, Artikelbezeichnung, ArtGroe.Groesse, Teile.Ausgang1 DESC;
+  AND EinzHist.Status IN (N'A', N'E', N'G', N'I', N'K', N'L', N'M', N'O', N'C', N'Q', N'S', N'N')
+  AND Traeger.Status != N'I'
+  AND EinzHist.IsCurrEinzHist = 1
+  AND EinzHist.PoolFkt = 0
+  AND EinzHist.EinzHistTyp = 1
+ORDER BY Traeger.Nachname, Traeger.Vorname, Artikelbezeichnung, ArtGroe.Groesse, EinzHist.Ausgang1 DESC;
