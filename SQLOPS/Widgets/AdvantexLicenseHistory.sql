39337ef460aababce900fc0ebce160e978546d38
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-06-19 15:17:18 +0200

    include weekends

--- SQLOPS/Widgets/AdvantexLicenseHistory.sql
+++ SQLOPS/Widgets/AdvantexLicenseHistory.sql
@@ -1,5 +1,5 @@
 SELECT LoginUse.Datum, LoginUse.AnzSessions
 FROM LoginUse
 WHERE LoginUse.Datum BETWEEN CAST(DATEADD(day, -30, GETDATE()) AS date) AND CAST(DATEADD(day, -1, GETDATE()) AS date)
-  AND DATEPART(weekday, LoginUse.Datum) NOT IN (7, 1) -- Samstag / Sonntag
+  /* AND DATEPART(weekday, LoginUse.Datum) NOT IN (7, 1) -- Samstag / Sonntag */
 ORDER BY Datum ASC;
