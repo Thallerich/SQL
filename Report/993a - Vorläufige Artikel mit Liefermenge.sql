f1d06a04b2222013a0238c46729efe01dd3e82aa
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-06-21 07:59:30 +0200

    bugfix  KdArti.LeasingPreis -> KdArti.LeasPreis

--- Report/993a - Vorläufige Artikel mit Liefermenge.sql
+++ Report/993a - Vorläufige Artikel mit Liefermenge.sql
@@ -1,4 +1,4 @@
-SELECT Firma.Bez AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, SUM(LsPo.Menge) AS Liefermenge, KdArti.Vorlaeufig, KdArti.WaschPreis, KdArti.LeasingPreis, KdArti.ID AS KdArtiID
+SELECT Firma.Bez AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, SUM(LsPo.Menge) AS Liefermenge, KdArti.Vorlaeufig, KdArti.WaschPreis, KdArti.LeasPreis AS LeasingPreis, KdArti.ID AS KdArtiID
 FROM LsPo
 JOIN LsKo ON LsPo.LsKoID = LsKo.ID
 JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
@@ -6,9 +6,9 @@ JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN Kunden ON KdArti.KundenID = Kunden.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
 WHERE KdArti.Vorlaeufig = 1
-  AND LsPo.Menge <> 0
+  AND LsPo.Menge != 0
   AND LsKo.Status < N'W'
   AND Artikel.ID > 0
   AND Kunden.FirmaID IN ($1$)
-GROUP BY Firma.Bez, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez, KdArti.Vorlaeufig, KdArti.WaschPreis, KdArti.LeasingPreis, KdArti.ID
+GROUP BY Firma.Bez, Kunden.KdNr, Kunden.SuchCode, Artikel.ArtikelNr, Artikel.ArtikelBez, KdArti.Vorlaeufig, KdArti.WaschPreis, KdArti.LeasPreis, KdArti.ID
 ORDER BY Firma, KdNr;
