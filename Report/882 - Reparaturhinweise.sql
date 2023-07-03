1c0ee22fa64bc9cd0c4fa0e55b99f1895bbcb0ad
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-07-03 12:52:03 +0200

    bugfixing

--- Report/882 - Reparaturhinweise.sql
+++ Report/882 - Reparaturhinweise.sql
@@ -4,14 +4,24 @@ DECLARE @EingabeDatumBis datetime;
 SET @EingabeDatumVon = $1$;
 SET @EingabeDatumBis = DATEADD(day, 1, $2$);
 
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Teile.Barcode, Teile.RestwertInfo AS RestwertAktuell, Hinweis.Hinweis, Hinweis.Aktiv, CONVERT(date, Hinweis.EingabeDatum) AS [Hinweis erfasst am], EingabeMitarbei.Name AS [Erfasst von], CONVERT(date, Hinweis.BestaetDatum) AS [Bestätigt am], BestaetMitarbei.Name AS [Bestätigt von]
-FROM Hinweis, Teile, Vsa, Kunden, KdGf, Artikel, Mitarbei AS EingabeMitarbei, Mitarbei AS BestaetMitarbei
-WHERE Hinweis.TeileID = Teile.ID
-  AND Teile.VsaID = Vsa.ID
-  AND Vsa.KundenID = Kunden.ID
-  AND Teile.ArtikelID = Artikel.ID
-  AND Hinweis.EingabeMitarbeiID = EingabeMitarbei.ID
-  AND Hinweis.BestaetMitarbeiID = BestaetMitarbei.ID
-  AND Hinweis.HinwTextID IN ($3$)
+WITH Teilestatus AS (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = N'EINZHIST'
+)
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, EinzHist.Barcode, EinzHist.RestwertInfo AS RestwertAktuell, Hinweis.Hinweis, Hinweis.Aktiv, CAST(Hinweis.EingabeDatum AS date) AS [Hinweis erfasst am], EingabeMitarbei.Name AS [Erfasst von], CAST(Hinweis.BestaetDatum AS date) AS [Bestätigt am], BestaetMitarbei.Name AS [Bestätigt von], EinzHist.Eingang1 AS [letzter Eingang], EinzHist.Ausgang1 AS [letzter Ausgang], Actions.ActionsBez$LAN$ AS [letzte Aktion]
+FROM Hinweis
+JOIN EinzHist ON Hinweis.EinzHistID = EinzHist.ID
+JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
+JOIN Teilestatus ON EinzHist.[Status] = Teilestatus.[Status]
+JOIN Vsa ON EinzHist.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
+JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+JOIN Mitarbei AS EingabeMitarbei ON Hinweis.EingabeMitarbeiID = EingabeMitarbei.ID
+JOIN Mitarbei AS BestaetMitarbei ON Hinweis.BestaetMitarbeiID = BestaetMitarbei.ID
+JOIN Actions ON EinzTeil.LastActionsID = Actions.ID
+WHERE Hinweis.HinwTextID IN ($3$)
   AND Hinweis.EingabeDatum BETWEEN @EingabeDatumVon AND @EingabeDatumBis
   AND Kunden.SichtbarID IN ($SICHTBARIDS$);
