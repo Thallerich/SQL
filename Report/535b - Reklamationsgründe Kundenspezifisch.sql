a0b140d11cc2b68d9eaf933e411cdca05930f93f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-07-07 11:27:16 +0200

    no comment

---
+++ Report/535b - Reklamationsgründe Kundenspezifisch.sql
@@ -0,0 +1,64 @@
+DECLARE @from date = $STARTDATE$;
+DECLARE @to date = $ENDDATE$;
+
+DROP TABLE IF EXISTS #Reklamation;
+
+CREATE TABLE #Reklamation (
+  Datumsbereich nchar(23),
+  Standort nvarchar(40),
+  Geschäftsbereich nchar(5),
+  KdNr int,
+  Kunde nvarchar(20),
+  ArtikelNr nvarchar(15),
+  Artikelbezeichnung nvarchar(60),
+  Reklamationsgrund nvarchar(40),
+  Reklamationsmenge int
+);
+
+INSERT INTO #Reklamation (Datumsbereich, Standort, Geschäftsbereich, KdNr, Kunde, ArtikelNr, Artikelbezeichnung, Reklamationsgrund, Reklamationsmenge)
+SELECT FORMAT(@from, 'd', 'de-at') + ' - ' + FORMAT(@to, 'd', 'de-at') AS Datumsbereich, Standort.Bez AS Standort, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr AS KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, LsKoGru.LsKoGruBez$LAN$ AS Reklamationsgrund,  SUM(ABS(LsPo.Menge)) AS Reklamationsmenge
+FROM Kunden, Vsa, LsKo, LsKoGru, LsPo, KdArti, Artikel, Standort, KdGf
+WHERE Kunden.ID IN ($2$)
+  AND Kunden.ID = Vsa.KundenID
+  AND Kunden.KdGfID = KdGf.ID
+  AND Vsa.ID = LsKo.VsaID
+  AND LsKo.ID = LsPo.LsKoID
+  AND LsPo.LsKoGruID = LsKoGru.ID
+  AND LsKoGru.ID IN ($3$)
+  AND LsPo.KdArtiID = KdArti.ID
+  AND KdArti.ArtikelID = Artikel.ID
+  AND LsKo.Datum BETWEEN @from AND @to
+  AND LsKo.ProduktionID = Standort.ID
+  AND Standort.ID IN ($4$)
+  AND Artikel.BereichID in ($5$)
+GROUP BY Standort.Bez, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, LsKoGru.LsKoGruBez$lan$, Artikel.ArtikelNr, Artikel.ArtikelBez$lan$
+HAVING SUM(ABS(LsPo.Menge)) > 0;
+
+INSERT INTO #Reklamation (Datumsbereich, Standort, Geschäftsbereich, KdNr, Kunde, ArtikelNr, Artikelbezeichnung, Reklamationsgrund, Reklamationsmenge)
+SELECT FORMAT(@from, 'd', 'de-at') + ' - ' + FORMAT(@to, 'd', 'de-at') AS Datumsbereich, Standort.Bez AS Standort, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr AS Kundennummer, Kunden.SuchCode AS Kundenname, Artikel.ArtikelNr AS Artikelnummer, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, LsKoGru.LsKoGruBez$LAN$ AS Reklamationsgrund, SUM(ABS(LsPo.Menge)) AS Reklamationsmenge
+FROM Kunden, Vsa, LsKo, LsKoGru, LsPo, KdArti, Artikel, Standort, KdGf
+WHERE Kunden.ID IN ($2$)
+  AND Kunden.ID = Vsa.KundenID
+  AND Kunden.KdGfID = KdGf.ID
+  AND Vsa.ID = LsKo.VsaID
+  AND LsKo.ID = LsPo.LsKoID
+  AND LsKo.LsKoGruID = LsKoGru.ID
+  AND LsPo.LsKoGruID NOT IN ($3$)
+  AND LsKoGru.ID IN ($3$)
+  AND LsPo.KdArtiID = KdArti.ID
+  AND KdArti.ArtikelID = Artikel.ID
+  AND LsKo.Datum BETWEEN @from AND @to
+  AND LsKo.ProduktionID = Standort.ID
+  AND Standort.ID IN ($4$)
+  AND Artikel.BereichID in ($5$)
+GROUP BY Standort.Bez, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, LsKoGru.LsKoGruBez$lan$, Artikel.ArtikelNr, Artikel.ArtikelBez$lan$
+HAVING SUM(ABS(LsPo.Menge)) > 0;
+
+SELECT Datumsbereich, Standort, Geschäftsbereich, KdNr, Kunde, ArtikelNr, Artikelbezeichnung, Reklamationsgrund, SUM(Reklamationsmenge) AS Reklamationsmenge 
+FROM #Reklamation
+GROUP BY Datumsbereich, Standort, Geschäftsbereich, KdNr, Kunde, ArtikelNr, Artikelbezeichnung, Reklamationsgrund
+
+UNION ALL
+
+SELECT 'Summe:' AS Datumsbereich, '' AS Standort, NULL AS Geschäftsbereich, NULL AS Kundennummer, '' AS Kundenname, '' AS Artikelnummer, '' AS Artikelbezeichnung, '' AS Reklamationsgrund,  SUM(Reklamationsmenge) AS Reklamationsmenge
+FROM #Reklamation;
--- Report/846a - Reklamationsstatistik.sql
+++ Report/846a - Reklamationsstatistik.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS #TmpResult846a;
 DECLARE @from date = $STARTDATE$;
 DECLARE @to date = $ENDDATE$;
 
-SELECT FORMAT(@from, 'd', 'de-at') + ' - ' + FORMAT(@to, 'd', 'de-at') AS Datumsbereich, Standort.Bez AS Standort, Artikel.ArtikelNr AS Artikelnummer, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, CAST(0 AS numeric(18,4)) AS Reklamationsmenge, SUM(LsPo.Menge) AS Liefermenge, KdArti.WaschPreis AS Stückpreis, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, Branche.BrancheBez$LAN$ AS Branche, KdArti.ID AS KdArtiID
+SELECT FORMAT(@from, 'd', 'de-at') + ' - ' + FORMAT(@to, 'd', 'de-at') AS Datumsbereich, Standort.Bez AS Standort, Artikel.ArtikelNr AS Artikelnummer, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, CAST(0 AS numeric(18,4)) AS Reklamationsmenge, SUM(LsPo.Menge) AS Liefermenge, KdArti.WaschPreis AS Stückpreis, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, Branche.BrancheBez$LAN$ AS Branche, KdArti.ID AS KdArtiID, kunden.id as kundenid, standort.id as standortid
 INTO #TmpResult846a
 FROM Kunden, Vsa, LsKo, LsPo, KdArti, Artikel, Standort, KdGf, Holding, Branche
 WHERE Kunden.ID = Vsa.KundenID
@@ -21,10 +21,10 @@ WHERE Kunden.ID = Vsa.KundenID
   AND Branche.ID IN ($5$)
   AND LsPo.LsKoGruID NOT IN ($2$)
   AND Artikel.ArtGruID NOT IN (26136, 26137, 26154, 26158) --keine Eigenwäsche
-GROUP BY Standort.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, KdArti.Variante, KdArti.WaschPreis, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, Holding.Holding, Branche.BrancheBez$LAN$, KdArti.ID;
+GROUP BY Standort.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, KdArti.Variante, KdArti.WaschPreis, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, Holding.Holding, Branche.BrancheBez$LAN$, KdArti.ID,kunden.id,standort.id;
 
 MERGE INTO #TmpResult846a USING (
-  SELECT FORMAT(@from, 'd', 'de-at') + ' - ' + FORMAT(@to, 'd', 'de-at') AS Datumsbereich, Standort.Bez AS Standort, Artikel.ArtikelNr AS Artikelnummer, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, SUM(ABS(LsPo.Menge)) AS Reklamationsmenge, SUM(LsPo.Menge) AS Liefermenge, KdArti.WaschPreis AS Stückpreis, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, Branche.BrancheBez$LAN$ AS Branche, KdArti.ID AS KdArtiID
+  SELECT FORMAT(@from, 'd', 'de-at') + ' - ' + FORMAT(@to, 'd', 'de-at') AS Datumsbereich, Standort.Bez AS Standort, Artikel.ArtikelNr AS Artikelnummer, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, SUM(ABS(LsPo.Menge)) AS Reklamationsmenge, SUM(LsPo.Menge) AS Liefermenge, KdArti.WaschPreis AS Stückpreis, KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, Branche.BrancheBez$LAN$ AS Branche, KdArti.ID AS KdArtiID, kunden.id as kundenid, standort.id as standortid
   FROM Kunden, Vsa, LsKo, LsPo, KdArti, Artikel, Standort, KdGf, Holding, Branche
   WHERE Kunden.ID = Vsa.KundenID
     AND Kunden.KdGfID = KdGf.ID
@@ -41,14 +41,14 @@ MERGE INTO #TmpResult846a USING (
     AND Branche.ID IN ($5$)
     AND LsPo.LsKoGruID IN ($2$)
     AND Artikel.ArtGruID NOT IN (26136, 26137, 26154, 26158) --keine Eigenwäsche
-  GROUP BY Standort.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, KdArti.Variante, KdArti.WaschPreis, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, Holding.Holding, Branche.BrancheBez$LAN$, KdArti.ID
-) AS Reklamationsmenge (Datumsbereich, Standort, Artikelnummer, Artikelbezeichnung, Variante, Reklamationsmenge, Liefermenge, Stückpreis, Geschäftsbereich, KdNr, Kunde, Holding, Branche, KdArtiID)
-ON #TmpResult846a.KdArtiID = Reklamationsmenge.KdArtiID
+  GROUP BY Standort.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, KdArti.Variante, KdArti.WaschPreis, KdGf.KurzBez, Kunden.KdNr, Kunden.SuchCode, Holding.Holding, Branche.BrancheBez$LAN$, KdArti.ID,kunden.id,standort.id
+) AS Reklamationsmenge (Datumsbereich, Standort, Artikelnummer, Artikelbezeichnung, Variante, Reklamationsmenge, Liefermenge, Stückpreis, Geschäftsbereich, KdNr, Kunde, Holding, Branche, KdArtiID,kundenid, standortid)
+ON #TmpResult846a.KdArtiID = Reklamationsmenge.KdArtiID and #TmpResult846a.Kundenid = Reklamationsmenge.kundenid and #TmpResult846a.standortid = Reklamationsmenge.standortid
 WHEN MATCHED THEN
   UPDATE SET Reklamationsmenge = Reklamationsmenge.Reklamationsmenge, Liefermenge = #TmpResult846a.Liefermenge + Reklamationsmenge.Liefermenge
 WHEN NOT MATCHED THEN
-  INSERT (Datumsbereich, Standort, Artikelnummer, Artikelbezeichnung, Variante, Reklamationsmenge, Liefermenge, Stückpreis, Geschäftsbereich, KdNr, Kunde, Holding, Branche, KdArtiID)
-  VALUES (Reklamationsmenge.Datumsbereich, Reklamationsmenge.Standort, Reklamationsmenge.Artikelnummer, Reklamationsmenge.Artikelbezeichnung, Reklamationsmenge.Variante, Reklamationsmenge.Reklamationsmenge, Reklamationsmenge.Liefermenge, Reklamationsmenge.Stückpreis, Reklamationsmenge.Geschäftsbereich, Reklamationsmenge.KdNr, Reklamationsmenge.Kunde, Reklamationsmenge.Holding, Reklamationsmenge.Branche, Reklamationsmenge.KdArtiID);
+  INSERT (Datumsbereich, Standort, Artikelnummer, Artikelbezeichnung, Variante, Reklamationsmenge, Liefermenge, Stückpreis, Geschäftsbereich, KdNr, Kunde, Holding, Branche, KdArtiID,kundenid, standortid)
+  VALUES (Reklamationsmenge.Datumsbereich, Reklamationsmenge.Standort, Reklamationsmenge.Artikelnummer, Reklamationsmenge.Artikelbezeichnung, Reklamationsmenge.Variante, Reklamationsmenge.Reklamationsmenge, Reklamationsmenge.Liefermenge, Reklamationsmenge.Stückpreis, Reklamationsmenge.Geschäftsbereich, Reklamationsmenge.KdNr, Reklamationsmenge.Kunde, Reklamationsmenge.Holding, Reklamationsmenge.Branche, Reklamationsmenge.KdArtiID, Reklamationsmenge.kundenid, Reklamationsmenge.standortid);
 
 SELECT Datumsbereich, Standort, Artikelnummer, Artikelbezeichnung, Variante, Reklamationsmenge, Liefermenge, Stückpreis, Geschäftsbereich, KdNr, Kunde, Holding, Branche
 FROM #TmpResult846a
