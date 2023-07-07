d458b1b02887a51734f91c3d737b134aa846d2cf
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-07-07 13:08:17 +0200

    pws-parts  - filter out pws-parts, except "sack"

--- Report/740 - Rückgabeliste (SGF).sql
+++ Report/740 - Rückgabeliste (SGF).sql
@@ -16,7 +16,7 @@ Kundenstatus AS (
 TeileStatus AS ( 
 SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
-  WHERE [Status].Tabelle = UPPER(N'TEILE')
+  WHERE [Status].Tabelle = UPPER(N'EINZHIST')
 )
 SELECT KdGf.KurzBez AS SGF,
   Kunden.KdNr,
@@ -41,32 +41,37 @@ SELECT KdGf.KurzBez AS SGF,
   Artikel.ArtikelNr,
   Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung,
   ArtGroe.Groesse AS Größe,
-  Teile.Barcode,
+  EinzHist.Barcode,
   TeileStatus.StatusBez AS [Status Teil],
-  Teile.Abmeldung,
-  IIF(Teile.AusDienst = N'' OR Teile.AusDienst IS NULL, Teile.RestWertInfo, Teile.AusDRestW) AS Restwert,
-  Teile.Alterinfo AS [Alter in Wochen],
-  Teile.Indienst,
+  EinzHist.Abmeldung,
+  IIF(EinzHist.AusDienst = N'' OR EinzHist.AusDienst IS NULL, EinzHist.RestWertInfo, EinzHist.AusDRestW) AS Restwert,
+  EinzTeil.Alterinfo AS [Alter in Wochen],
+  EinzHist.Indienst,
   Einsatz.EinsatzBez$LAN$ AS [Außerdienststellungs-Grund],
   WegGrund.WegGrundBez$LAN$ AS [Schrott-Grund]
-FROM Teile
-JOIN Traeger on Teile.TraegerID = Traeger.ID
-JOIN ArtGroe on Teile.ArtGroeID = ArtGroe.ID
-JOIN Artikel on Teile.ArtikelID = Artikel.ID
-JOIN Vsa on Teile.VsaID = Vsa.ID
+FROM EinzHist
+JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
+JOIN Traeger on EinzHist.TraegerID = Traeger.ID
+JOIN ArtGroe on EinzHist.ArtGroeID = ArtGroe.ID
+JOIN Artikel on EinzHist.ArtikelID = Artikel.ID
+JOIN Vsa on EinzHist.VsaID = Vsa.ID
 JOIN Kunden on Vsa.KundenID = Kunden.ID
 JOIN KdGf on Kunden.KdGfID = KdGf.ID
 JOIN Traegerstatus on Traeger.Status = Traegerstatus.Status
 JOIN VsaStatus on  Vsa.Status = VsaStatus.Status
 JOIN Kundenstatus on Kunden.Status = Kundenstatus.Status 
-JOIN TeileStatus on Teile.Status = Teilestatus.status
+JOIN TeileStatus on EinzHist.Status = Teilestatus.status
 JOIN Abteil ON Vsa.AbteilID = Abteil.ID
 JOIN KdArti AS Berufsgruppe ON Traeger.BerufsgrKdArtiID = Berufsgruppe.ID
-JOIN WegGrund on Teile.WegGrundID = WegGrund.ID
-LEFT JOIN Einsatz ON  Teile.AusdienstGrund = Einsatz.EinsatzGrund
+JOIN WegGrund on EinzHist.WegGrundID = WegGrund.ID
+LEFT JOIN Einsatz ON  EinzHist.AusdienstGrund = Einsatz.EinsatzGrund
 WHERE Kunden.ID IN ($4$)
-  AND Teile.Status BETWEEN N'U' AND N'W'
-  AND Teile.AbmeldDat BETWEEN $STARTDATE$ AND $ENDDATE$
-  AND Teile.Einzug IS NULL
+  AND EinzHist.Status BETWEEN N'U' AND N'W'
+  AND EinzHist.AbmeldDat BETWEEN $STARTDATE$ AND $ENDDATE$
+  AND EinzHist.Einzug IS NULL
+  AND EinzHist.IsCurrEinzHist = 1
+  AND EinzHist.EinzHistTyp = 1
+  AND EinzHist.PoolFkt = 0
+  AND (EinzTeil.AltenheimModus = 0 OR Artikel.ArtGruID IN (SELECT ArtGru.ID FROM ArtGru WHERE ArtGru.Sack = 1))
   AND Artikel.BereichID IN ($5$)
 ORDER BY SGF, KdNr, VsaNr, Traeger.Nachname, Artikel.ArtikelNr, Größe, [Status Teil];
