58bef2bebe60ddb4beb8fdadd5dd8235fec5e9af
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-07-10 15:27:07 +0200

    do not use encryption

--- Powershell/SMBDB1/U4ber_CopyToTest.ps1
+++ Powershell/SMBDB1/U4ber_CopyToTest.ps1
@@ -20,4 +20,4 @@ $key = Get-Content $KeyFile
 $SQLCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (Get-Content $PasswordFile | ConvertTo-SecureString -Key $key)
 
 # Execute Backup command
-Invoke-Sqlcmd -InputFile ".\job_copytotest.sql" -ServerInstance "SMBDB1.sal.co.at\EXPRESS" -Credential $SQLCredential
+Invoke-Sqlcmd -InputFile ".\job_copytotest.sql" -ServerInstance "SMBDB1.sal.co.at\EXPRESS" -Credential $SQLCredential -Encrypt Optional
