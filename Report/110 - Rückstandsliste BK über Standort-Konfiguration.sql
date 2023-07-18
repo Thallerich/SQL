3806c352575e7fff9648945592f2907d8c24e535
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-07-18 10:00:55 +0200

    IT-73062 added column "letzter Scan-Zeitpunkt"

--- Report/110 - Rückstandsliste BK über Standort-Konfiguration.sql
+++ Report/110 - Rückstandsliste BK über Standort-Konfiguration.sql
@@ -1,5 +1,5 @@
 WITH TeileInProd AS (
-  SELECT Prod.AusTourID AS TourenID, Prod.VsaID, Prod.ZielNrID, Prod.EinDat, Prod.AusDat, Prod.ProduktionID AS ProdStandortID, EinzHist.ID AS TeileID
+  SELECT Prod.AusTourID AS TourenID, Prod.VsaID, Prod.ZielNrID, Prod.EinDat, Prod.AusDat, Prod.ProduktionID AS ProdStandortID, EinzHist.ID AS TeileID, Prod.LetzterScan
   FROM Prod
   JOIN EinzHist ON Prod.EinzHistID = EinzHist.ID
   WHERE EinzHist.Status IN (N'N', N'Q')
@@ -21,10 +21,10 @@ SELECT Daten.Tour AS Liefertour, Daten.Bez AS [Liefertour-Bezeichnung], Wochenta
     WHEN 7 THEN N'Sonntag'
     ELSE N'WTF?'
   END
-  , Standort.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Traeger.Traeger AS TrägerNr, Traeger.Nachname, Traeger.Vorname, EinzHist.Barcode, Teilestatus.StatusBez AS [Status Teil], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Daten.EinDat AS [Abhol-Datum beim Kunden], Daten.AusDat AS [geplantes Lieferdatum], ZielNr.ZielNrBez$LAN$ AS [letzter Scan-Ort]
+  , Standort.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Traeger.Traeger AS TrägerNr, Traeger.Nachname, Traeger.Vorname, EinzHist.Barcode, Teilestatus.StatusBez AS [Status Teil], Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Daten.EinDat AS [Abhol-Datum beim Kunden], Daten.AusDat AS [geplantes Lieferdatum], ZielNr.ZielNrBez$LAN$ AS [letzter Scan-Ort], Daten.LetzterScan AS [letzter Scan-Zeitpunkt]
 FROM ZielNr
 JOIN (
-    SELECT DISTINCT Touren.ID, Touren.Tour, Touren.Bez, Touren.TourPrioID, Touren.Wochentag, Vsa.ID AS VsaID, Vsa.Suchcode AS VsaSuchcode, Vsa.Bez AS VsaBez, BkZiele.TeileID, BkZiele.ZielNrID, BkZiele.EinDat, BkZiele.AusDat, VSA.KundenID, Touren.SDCTour, WLot.Bez AS WLotBez, Touren.ExpeditionID AS TourExpeditionID, BKZiele.ProdStandortID, TourPrio.TourPrioBez AS TourPrioBez, Fahrt.PlanDatum AS FahrtPlanDatum
+    SELECT DISTINCT Touren.ID, Touren.Tour, Touren.Bez, Touren.TourPrioID, Touren.Wochentag, Vsa.ID AS VsaID, Vsa.Suchcode AS VsaSuchcode, Vsa.Bez AS VsaBez, BkZiele.TeileID, BkZiele.ZielNrID, BkZiele.EinDat, BkZiele.AusDat, VSA.KundenID, Touren.SDCTour, WLot.Bez AS WLotBez, Touren.ExpeditionID AS TourExpeditionID, BKZiele.ProdStandortID, TourPrio.TourPrioBez AS TourPrioBez, Fahrt.PlanDatum AS FahrtPlanDatum, BKZiele.LetzterScan
     FROM VsaTour, Vsa, TourPrio, TeileInProd AS BkZiele
     LEFT JOIN Touren ON Touren.ID = BkZiele.TourenID
     LEFT JOIN WLot ON WLot.ID = Touren.WLotID
