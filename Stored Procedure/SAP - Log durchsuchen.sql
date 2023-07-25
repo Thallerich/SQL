2ebf8c9ea09e7ff7499cf109f871d179cb09f6a6
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-07-25 14:27:05 +0200

    initial version

---
+++ Stored Procedure/SAP - Log durchsuchen.sql
@@ -0,0 +1,59 @@
+CREATE PROCEDURE _sali_searchsaplog
+  @function nvarchar(40) = N'???', /* Possible functions: StockTransaction, Invoice, AveragePrice, PayedWash, PoLineStat, Supplier, Customer, Article, ExchangeRate, PackingNote, PurchaseOrder, AveragePrice_verbose */
+  @searchstring nvarchar(40) = NULL
+AS
+
+  SET NOCOUNT ON;
+
+  DECLARE @datetimefilter datetime2 = DATEADD(day, -7, DATETIME2FROMPARTS(DATEPART(year, GETDATE()), DATEPART(month, GETDATE()), DATEPART(day, GETDATE()), 0, 0, 0, 0, 0)); /* look at the last 7 days */
+  DECLARE @errorflag tinyint = 0;
+  DECLARE @xmlfiltertext nvarchar(300);
+  DECLARE @sqltext nvarchar(max);
+
+  IF @searchstring IS NULL
+    SET @errorflag = 1;
+  ELSE
+  BEGIN
+    SET @xmlfiltertext = 
+      CASE @function
+        WHEN N'Article'
+        THEN N'SUBSTRING(SalExLog.HTTPRequest, CHARINDEX(N''<ArticleNumber>'', SalExLog.HTTPRequest, 1) + 15, CHARINDEX(N''</ArticleNumber>'', SalExLog.HTTPRequest, 1) - (CHARINDEX(N''<ArticleNumber>'', SalExLog.HTTPRequest, 1) + 15)) = @searchstring'
+        WHEN N'Invoice'
+        THEN N'TRY_CAST(SUBSTRING(SalExLog.HTTPRequest, CHARINDEX(N''<InvoiceNumber>'', SalExLog.HTTPRequest, 1) + 15, CHARINDEX(N''</InvoiceNumber>'', SalExLog.HTTPRequest, 1) - (CHARINDEX(N''<InvoiceNumber>'', SalExLog.HTTPRequest, 1) + 15)) AS int) = TRY_CAST(@searchstring AS int)'
+        WHEN N'Customer'
+        THEN N'TRY_CAST(SUBSTRING(SalExLog.HTTPRequest, CHARINDEX(N''<Number>'', SalExLog.HTTPRequest, 1) + 8, CHARINDEX(N''</Number>'', SalExLog.HTTPRequest, 1) - (CHARINDEX(N''<Number>'', SalExLog.HTTPRequest, 1) + 8)) AS int) = TRY_CAST(@searchstring AS int)'
+        ELSE NULL
+    END;
+
+    IF @xmlfiltertext IS NULL
+      SET @errorflag += 2;
+  END;
+  
+  IF @errorflag = 0
+    BEGIN
+      SET @sqltext = N'
+      SELECT SalExLog.Anlage_ AS Zeitpunkt, SalExLog.HTTPRequest, SalExLog.ResponseSuccessful AS [Succcess?], SalExLog.ResponseReturnDescriptio AS [Rückmeldung SAP]
+      FROM SalExLog
+      WHERE SalExLog.Anlage_ > @filter
+        AND SalExLog.FunctionName = @function
+        AND ' + @xmlfiltertext + '
+      ORDER BY Zeitpunkt DESC;
+      ';
+
+      EXEC sp_executesql @sqltext, N'@filter datetime2, @function nvarchar(40), @searchstring nvarchar(40)', @datetimefilter, @function, @searchstring;
+    END;
+  
+  IF @errorflag = 1
+    SELECT N'FEHLER: Kein Suchtext angegeben!'
+    UNION
+    SELECT N'Aufruf: EXEC _sali_searchsaplog @functionname = N''<<funktion>>'', @searchstring = N''<<suchtext>>''';
+  IF @errorflag = 2
+    SELECT N'FEHLER: Keine gültige Funktion angegeben!'
+    UNION
+    SELECT N'Aufruf: EXEC _sali_searchsaplog @functionname = N''<<funktion>>'', @searchstring = N''<<suchtext>>''';
+  IF @errorflag = 3
+    SELECT N'FEHLER: Keine gültige Funktion angegeben!'
+    UNION
+    SELECT N'FEHLER: Kein Suchtext angegeben!'
+    UNION
+    SELECT N'Aufruf: EXEC _sali_searchsaplog @functionname = N''<<funktion>>'', @searchstring = N''<<suchtext>>''';
