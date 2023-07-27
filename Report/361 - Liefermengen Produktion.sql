6c0053865e964b9b9bbd4604aa2bfa9952f3579d
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-07-27 13:07:11 +0200

    IT-73431 added article filter

--- Report/361 - Liefermengen Produktion.sql
+++ Report/361 - Liefermengen Produktion.sql
@@ -1,5 +1,5 @@
-SELECT Firma.Bez AS Firma, Produktion.Bez AS Produzent, Expedition.Bez AS Expedition, ServType.ServTypeBez$LAN$ AS Serviceart, StandKon.StandKonBez$LAN$ AS [Standort-Konfiguration], KdGf.KurzBez AS SGF, KdGf.KdGfBez$LAN$ AS [Geschäftsfeld], Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT') AS Monat, SUM(LsPo.Menge) AS Menge, Artikel.StueckGewicht AS [Gewicht pro Stück], Artikel.Packmenge AS [VPE-Menge], ME.MeBez$LAN$ AS [VPE], ROUND(SUM(LsPo.Menge * Artikel.StueckGewicht), 2) AS Liefergewicht
-FROM LsPo, LsKo, Vsa, Kunden, Firma, Standort AS Produktion, StandKon, KdGf, KdArti, Artikel, KdBer, Bereich, ME, Fahrt, Touren, Standort AS Expedition, ServType
+SELECT Firma.Bez AS Firma, Produktion.Bez AS Produzent, Expedition.Bez AS Expedition, ServType.ServTypeBez$LAN$ AS Serviceart, StandKon.StandKonBez$LAN$ AS [Standort-Konfiguration], KdGf.KurzBez AS SGF, KdGf.KdGfBez$LAN$ AS [Geschäftsfeld], Kunden.KdNr, Kunden.SuchCode AS Kunde, Bereich.BereichBez$LAN$ AS Kundenbereich, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS Variantenbezeichnung, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT') AS Monat, SUM(LsPo.Menge) AS Menge, Artikel.StueckGewicht AS [Gewicht pro Stück], Artikel.Packmenge AS [VPE-Menge], ME.MeBez$LAN$ AS [VPE], ROUND(SUM(LsPo.Menge * Artikel.StueckGewicht), 2) AS Liefergewicht
+FROM LsPo, LsKo, Vsa, Kunden, Firma, Standort AS Produktion, StandKon, KdGf, KdArti, Artikel, KdBer, Bereich, ME, Fahrt, Touren, Standort AS Expedition, ServType, STRING_SPLIT($4$, N',') AS ArtiList
 WHERE LsPo.LsKoID = LsKo.ID
   AND LsKo.VsaID = Vsa.ID
   AND Vsa.KundenID = Kunden.ID
@@ -22,4 +22,5 @@ WHERE LsPo.LsKoID = LsKo.ID
   AND Artikel.ID > 0
   AND LsPo.Menge != 0
   AND Artikel.ArtiTypeID = 1
-GROUP BY Firma.Bez, Produktion.Bez, Expedition.Bez, ServType.ServTypeBez$LAN$, StandKon.StandKonBez$LAN$, KdGf.KurzBez, KdGf.KdGfBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Bereich.BereichBez$LAN$, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT'), Artikel.StueckGewicht, Artikel.Packmenge, ME.MeBez$LAN$;
+  AND (Artikel.ArtikelNr = LTRIM(ArtiList.[value]) OR LTRIM(ArtiList.value) = N'')
+GROUP BY Firma.Bez, Produktion.Bez, Expedition.Bez, ServType.ServTypeBez$LAN$, StandKon.StandKonBez$LAN$, KdGf.KurzBez, KdGf.KdGfBez$LAN$, Kunden.KdNr, Kunden.SuchCode, Bereich.BereichBez$LAN$, Artikel.ArtikelNr, KdArti.Variante, KdArti.VariantBez, Artikel.ArtikelBez$LAN$, FORMAT(LsKo.Datum, N'yyyy-MM', N'de-AT'), Artikel.StueckGewicht, Artikel.Packmenge, ME.MeBez$LAN$;
