b905ad67087f82a5a26cfc210bd152ff2ebea688
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-10-03 12:57:46 +0200

    bugfix  - behave more like closeweek-module  - check only active wearers  - correctly alias wearer status column

--- Report/040 - Träger aktiv trotz Abmelde-Woche.sql
+++ Report/040 - Träger aktiv trotz Abmelde-Woche.sql
@@ -3,7 +3,7 @@ WITH Trägerstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = N'TRAEGER'
 )
-SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, [Zone].ZonenCode AS Vertriebszone, Standort.SuchCode AS Hauptstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Traeger.Traeger AS TrägerNr, Traeger.PersNr AS Personalnummer, Trägerstatus.StatusBez, Traeger.Vorname, Traeger.Nachname, Traeger.Indienst, Traeger.Ausdienst, Traeger.ID AS TraegerID
+SELECT Firma.SuchCode AS Firma, KdGf.KurzBez AS Geschäftsbereich, [Zone].ZonenCode AS Vertriebszone, Standort.SuchCode AS Hauptstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Traeger.Traeger AS TrägerNr, Traeger.PersNr AS Personalnummer, Trägerstatus.StatusBez AS [Status Träger], Traeger.Vorname, Traeger.Nachname, Traeger.Indienst, Traeger.Ausdienst, Traeger.ID AS TraegerID
 FROM Traeger
 JOIN Vsa ON Traeger.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -12,7 +12,7 @@ JOIN KdGf ON Kunden.KdGfID = KdGf.ID
 JOIN [Zone] ON Kunden.ZoneID = [Zone].ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
 JOIN Trägerstatus ON Traeger.[Status] = Trägerstatus.[Status]
-WHERE Traeger.[Status] != N'I'
+WHERE Traeger.[Status] = N'A'
   AND Traeger.Ausdienst IS NOT NULL
   AND Traeger.Ausdienst < (SELECT [Week].Woche FROM [Week] WHERE CAST(GETDATE() AS date) BETWEEN [Week].VonDat AND [Week].BisDat)
   AND Firma.ID IN ($1$)
