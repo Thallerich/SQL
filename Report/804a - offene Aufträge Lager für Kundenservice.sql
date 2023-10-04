fe0770b6930cbd0bc787d5e20a79c24ea4a22e5c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-10-04 09:17:26 +0200

    new column  - added column "Termin Auftragbestätigung" to result set

--- Report/804a - offene Aufträge Lager für Kundenservice.sql
+++ Report/804a - offene Aufträge Lager für Kundenservice.sql
@@ -22,6 +22,7 @@ CREATE TABLE #Result804a (
   Artikelbezeichnung nvarchar(60),
   Größe nvarchar(12),
   Anlage datetime2,
+  ABTermin date,
   Entnahmeliste int,
   EntnahmeAnlage datetime2,
   EntnahmeDruck datetime2,
@@ -40,14 +41,15 @@ Traegerstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = N'TRAEGER'
 )
-INSERT INTO #Result804a (ServiceMA, BetreuerMA, KdNr, Kunde, Holding, Nachname, Vorname, Trägerstatus, Barcode, Teilestatus, ArtikelNr, Artikelbezeichnung, Größe, Anlage, Entnahmeliste, EntnahmeAnlage, EntnahmeDruck, EntnhameZeit, EntnahmePatch, Einsatzgrund)
-SELECT ServiceMA.Name AS ServiceMA, BetreuerMA.Name AS BetreuerMA, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, Traeger.Nachname, Traeger.Vorname, Traegerstatus.StatusBez AS Trägerstatus, EinzHist.Barcode, Teilestatus.StatusBez AS Teilestatus, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS Anlage, EntnKo.ID AS Entnahmeliste, EntnKo.Anlage_ AS EntnahmeAnlage, EntnKo.DruckDatum AS EntnahmeDruck, EntnahmeZeit = (
+INSERT INTO #Result804a (ServiceMA, BetreuerMA, KdNr, Kunde, Holding, Nachname, Vorname, Trägerstatus, Barcode, Teilestatus, ArtikelNr, Artikelbezeichnung, Größe, Anlage, ABTermin, Entnahmeliste, EntnahmeAnlage, EntnahmeDruck, EntnhameZeit, EntnahmePatch, Einsatzgrund)
+SELECT ServiceMA.Name AS ServiceMA, BetreuerMA.Name AS BetreuerMA, Kunden.KdNr, Kunden.SuchCode AS Kunde, Holding.Holding, Traeger.Nachname, Traeger.Vorname, Traegerstatus.StatusBez AS Trägerstatus, EinzHist.Barcode, Teilestatus.StatusBez AS Teilestatus, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS Anlage, LiefAbPo.Termin AS ABTermin, EntnKo.ID AS Entnahmeliste, EntnKo.Anlage_ AS EntnahmeAnlage, EntnKo.DruckDatum AS EntnahmeDruck, EntnahmeZeit = (
   SELECT MAX(Scans.[DateTime])
   FROM Scans
   WHERE Scans.EinzHistID = EinzHist.ID
     AND Scans.ActionsID = 57
-), EntnKo.PatchDatum AS EntnahmePatch, Einsatz.Einsatzbez$LAN$ AS Einsatzgrund
-FROM EinzHist
+), EntnKo.PatchDatum AS EntnahmePatch, Einsatz.EinsatzBez$LAN$ AS Einsatzgrund
+FROM EinzTeil
+JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
 JOIN TraeArti ON EinzHist.TraeArtiID = TraeArti.ID
 JOIN ArtGroe ON TraeArti.ArtGroeID = ArtGroe.ID
 JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
@@ -64,6 +66,10 @@ JOIN Traegerstatus ON Traeger.[Status] = Traegerstatus.[Status]
 JOIN Einsatz ON EinzHist.EinsatzGrund = Einsatz.EinsatzGrund
 LEFT JOIN EntnPo ON EinzHist.EntnPoID = EntnPo.ID AND EinzHist.EntnPoID > 0
 LEFT JOIN EntnKo ON EntnPo.EntnKoID = EntnKo.ID
+LEFT JOIN TeileBPo ON TeileBPo.EinzHistID = EinzHist.ID AND TeileBPo.Latest = 1
+LEFT JOIN BPo ON TeileBPo.BPoID = BPo.ID
+LEFT JOIN BKo ON BPo.BKoID = BKo.ID
+LEFT JOIN LiefAbPo ON BPo.LatestLiefABKoID = LiefAbPo.LiefABKoID AND BPo.ID = LiefAbPo.BPoID
 WHERE EinzHist.Anlage_ > CAST(N'2019-04-01 00:00:00' AS datetime2)
   AND Artikel.BereichID = 100
   AND Kunden.Status = N'A'
@@ -74,13 +80,12 @@ WHERE EinzHist.Anlage_ > CAST(N'2019-04-01 00:00:00' AS datetime2)
   AND Kunden.StandortID IN ($3$)
   AND ServiceMA.ID IN ($4$)
   AND BetreuerMA.ID IN ($5$)
-  AND EinzHist.Anlage_ BETWEEN @fromdate AND @todate
-  AND EinzHist.IsCurrEinzHist = 1;
+  AND EinzHist.Anlage_ BETWEEN @fromdate AND @todate;
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* ++ Pipeline: Reportdaten                                                                                                     ++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-SELECT ServiceMA AS [Kundenservice-Mitarbeiter], BetreuerMA AS Betreuer, KdNr, Kunde, Holding, Nachname, Vorname, Trägerstatus, Barcode, Teilestatus, ArtikelNr, Artikelbezeichnung, Größe, Anlage AS [Teil angelegt am], Entnahmeliste AS EntnahmelistenNr, EntnahmeAnlage AS [Entnahmeliste angelegt am], EntnahmeDruck AS [Druckdatum Entnahmeliste], EntnhameZeit AS [Entnahme-Datum], EntnahmePatch AS [Patchdatum Entnahmeliste], Einsatzgrund
+SELECT ServiceMA AS [Kundenservice-Mitarbeiter], BetreuerMA AS Betreuer, KdNr, Kunde, Holding, Nachname, Vorname, Trägerstatus, Barcode, Teilestatus, ArtikelNr, Artikelbezeichnung, Größe, Anlage AS [Teil angelegt am], ABTermin AS [Termin Auftragsbestätigung], Entnahmeliste AS EntnahmelistenNr, EntnahmeAnlage AS [Entnahmeliste angelegt am], EntnahmeDruck AS [Druckdatum Entnahmeliste], EntnhameZeit AS [Entnahme-Datum], EntnahmePatch AS [Patchdatum Entnahmeliste], Einsatzgrund
 FROM #Result804a
 ORDER BY [Entnahmeliste angelegt am];
