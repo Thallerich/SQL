97e38d1fa69f67e95e14def4e624c5994c8df0f2
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-10-05 12:39:55 +0200

    optimization and changes for next release  - removed usage of IsCurrEinzHist  - filter on vsanr = 902

--- VOEST/VOEST - Restwerte Pool-Entnahme-Teile.sql
+++ VOEST/VOEST - Restwerte Pool-Entnahme-Teile.sql
@@ -11,8 +11,8 @@ Traegerstatus AS (
   WHERE [Status].Tabelle = N'TRAEGER'
 )
 SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS [Vsa-Bezeichnung], EinzHist.Barcode, Teilestatus.StatusBez AS [Status des Teils], Traeger.Traeger AS TrägerNr, Traeger.Vorname, Traeger.Nachname, Traegerstatus.StatusBez AS [Status des Trägers], Abteil.Abteilung AS Kostenstelle, Abteil.Bez AS Kostenstellenbezeichnung, Vsa.GebaeudeBez AS Abteilung, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, KdArti.Variante, EinzTeil.LastScanTime AS [Datum Ausgabe], LeasProWo.LeasPreisProWo AS [Leasing pro Woche], RwCalc.RestwertInfo AS Restwert
-FROM EinzHist
-JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
+FROM EinzTeil
+JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
 CROSS APPLY advFunc_GetLeasPreisProWo(EinzHist.KdArtiID) AS LeasProWo
 CROSS APPLY funcGetRestwert(EinzHist.ID, @Woche, 1) AS RwCalc
 JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
@@ -27,8 +27,8 @@ JOIN Abteil ON Traeger.AbteilID = Abteil.ID
 WHERE Kunden.HoldingID IN (SELECT ID FROM Holding WHERE Holding LIKE N'VOES%')
   AND EinzHist.Status BETWEEN N'Q' AND N'W'
   AND EinzHist.Einzug IS NULL
-  AND EinzHist.IsCurrEinzHist = 1
   AND EinzHist.PoolFkt = 0
   AND EinzHist.EinzHistTyp = 1
   AND Traeger.ParentTraegerID > 0
+  AND Vsa.VsaNr = 902
   AND EinzTeil.LastScanTime < N'2023-01-01 00:00:00';
