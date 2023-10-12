e0611fa8acc8028b793320b395f5cf236287bb8f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-10-12 16:12:23 +0200

    alias result

--- Jobs - AdvanTex/Monitor - Version Ghost Record Count REPQUEUE.sql
+++ Jobs - AdvanTex/Monitor - Version Ghost Record Count REPQUEUE.sql
@@ -6,5 +6,5 @@ DECLARE @vgr_count bigint;
 SELECT @vgr_count = sum(version_ghost_record_count)
 FROM sys.dm_db_index_physical_stats (@db_id, @object_id, @index_id, NULL, 'detailed');
 
-IF @vgr_count >= 200000
-  SELECT N'Version ghost record count in REPQUEUE is at ' + FORMAT(@vgr_count, N'##,#', N'de-AT') + N'!';
+IF @vgr_count < 200000
+  SELECT N'Version ghost record count in REPQUEUE is at ' + FORMAT(@vgr_count, N'##,#', N'de-AT') + N'!' AS [Alert];
