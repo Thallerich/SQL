f64e4e8500e0404210e5b15c0718c962489fb9f3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-12-14 13:04:00 +0100

    initial version

---
+++ Diverse/Waschlohn - Einträge mit einem Wert Null für eine Art.sql
@@ -0,0 +1,19 @@
+SELECT Firma.Bez AS Firma, Produktion.Bez AS Produktionsstandort, Expedition.Bez AS Expeditionsstandort, Bereich.Bereich, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, Wae.Code AS Währung, InKalk.InKalkWaschPreis AS [Bearbeitung fest], InKalk.InKalkWaschProzent AS [Bearbeitung %], InKalk.InKalkLeasPreis AS [Leasing fest], InKalk.InKalkLeasProzent AS [Leasing %], InKalk.InKalkSplitPreis AS [Split fest], InKalk.InKalkSplitProzent AS [Split %]
+FROM InKalk
+JOIN Firma ON InKalk.FirmaID = Firma.ID
+JOIN Standort AS Produktion ON InKalk.ProduktionID = Produktion.ID
+JOIN Standort AS Expedition ON InKalk.ExpeditionID = Expedition.ID
+JOIN Bereich ON InKalk.BereichID = Bereich.ID
+JOIN Artikel ON InKalk.ArtikelID = Artikel.ID
+JOIN Wae ON InKalk.WaeID = Wae.ID
+WHERE Firma.SuchCode = N'FA14'
+  AND InKalk.KundenID < 0
+  AND ((Artikel.ID > 0 AND Artikel.ArtiTypeID = 1) OR (Artikel.ID < 0))
+  AND (InKalk.ProduktionID > 0 OR InKalk.ExpeditionID > 0 OR InKalk.BereichID > 0 OR InKalk.ArtikelID > 0 OR InKalk.WaeID > 0)
+  AND (
+    (InKalk.InKalkWaschPreis = 0 AND InKalk.InKalkWaschProzent = 0) OR
+    (InKalk.InKalkLeasPreis = 0 AND InKalk.InKalkLeasProzent = 0) OR
+    (InKalk.InKalkSplitPreis = 0 AND InKalk.InKalkSplitProzent = 0)
+  )
+
+GO
