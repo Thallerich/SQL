56506f1f5a3f4905fb946a293dbff291b8fde1b6
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-12-20 10:52:10 +0100

    create table with clustered index

--- Jobs - AdvanTex/SAP - Bar-Rechnungen - Zwischentabelle füllen.sql
+++ Jobs - AdvanTex/SAP - Bar-Rechnungen - Zwischentabelle füllen.sql
@@ -1,7 +1,7 @@
 IF OBJECT_ID(N'dbo.__RechKoSofortSAP') IS NULL
 BEGIN
   CREATE TABLE __RechKoSofortSAP (
-    ID int
+    ID int PRIMARY KEY CLUSTERED
   );
 END ELSE BEGIN
   TRUNCATE TABLE __RechKoSofortSAP;
@@ -14,4 +14,4 @@ JOIN Kunden ON RechKo.KundenID = Kunden.ID
 WHERE RechKo.FibuExpID < 0
   AND Kunden.BarRech = 1
   AND RechKo.Status = N'F'
-  AND RechKo.FirmaID = 5260;
+  AND RechKo.FirmaID IN (5260, 5272);
