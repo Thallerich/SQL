2a0c4d3776c3e9955b77f18995550886f87cd6f6
Thallerich <20878157+Thallerich@users.noreply.github.com>
2023-12-22 11:51:56 +0100

    bugfix  - altenheimmodus can be 1 or 2  - include all SACK-articles

--- IT-Tickets/IT-73669 - PWS-Bewohnerteile - Rückgabe bereinigen.sql
+++ IT-Tickets/IT-73669 - PWS-Bewohnerteile - Rückgabe bereinigen.sql
@@ -1,41 +1,74 @@
-DISABLE TRIGGER ALL ON EinzHist
+SET NOCOUNT ON;
+SET CONTEXT_INFO 0x1; /* AdvanTex-Trigger für RepQueue deaktivieren */
+GO
+
+DROP TABLE IF EXISTS #PWSCleanup;
+GO
+
+CREATE TABLE #PWSCleanup (
+  EinzHistID int PRIMARY KEY CLUSTERED
+);
 
 GO
 
 DECLARE @kdgfid int = (SELECT KdGf.ID FROM KdGf WHERE KdGf.KurzBez = N'MED');
+DECLARE @msg nvarchar(max);
 
 DECLARE @Hauptstandort TABLE (
   StandortID int,
   StandortKuerzel nchar(4) COLLATE Latin1_General_CS_AS
 );
 
-INSERT INTO @Hauptstandort (StandortKuerzel)
-VALUES (N'WOEN'), (N'WOLI');
-
-UPDATE @Hauptstandort SET StandortID = Standort.ID
+INSERT INTO @Hauptstandort (StandortID, StandortKuerzel)
+SELECT Standort.ID, Standort.SuchCode
 FROM Standort
-WHERE Standort.SuchCode = [@Hauptstandort].StandortKuerzel;
+WHERE Standort.SuchCode IN (N'WOEN', N'WOLI');
 
-UPDATE EinzHist SET [Status] = N'Q', Abmeldung = NULL, AbmeldDat = NULL, Ausdienst = NULL, AusdienstDat = NULL, AusdienstGrund = NULL, Einzug = NULL
-WHERE ID IN (
-  SELECT EinzHist.ID
-  FROM EinzTeil
-  JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
-  JOIN Vsa ON EinzHist.VsaID = Vsa.ID
-  JOIN Kunden ON Vsa.KundenID = Kunden.ID
-  JOIN KdArti ON EinzHist.KdArtiID = KdArti.ID
-  JOIN Eigentum ON KdArti.EigentumID = Eigentum.ID
-  WHERE Kunden.KdGFID = @kdgfid
-    AND Kunden.StandortID IN (SELECT StandortID FROM @Hauptstandort)
-    AND EinzTeil.AltenheimModus = 1
-    AND EinzHist.Status = N'W'
-    AND Eigentum.RueckgabeBew = 0
-    AND EinzHist.EinzHistTyp = 1
-    AND EinzHist.TraeArtiID != -1
-)
+SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Starte Ermittlung falscher Rückgabe-Teile';
+RAISERROR(@msg, 0, 1) WITH NOWAIT;
+
+INSERT INTO #PWSCleanup (EinzHistID)
+SELECT EinzHist.ID
+FROM EinzTeil
+JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
+JOIN Vsa ON EinzHist.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdArti ON EinzHist.KdArtiID = KdArti.ID
+JOIN Eigentum ON KdArti.EigentumID = Eigentum.ID
+WHERE Kunden.KdGFID = @kdgfid
+  AND Kunden.StandortID IN (SELECT StandortID FROM @Hauptstandort)
+  AND EinzTeil.AltenheimModus IN (1, 2)
+  AND EinzHist.Status = N'W'
+  AND Eigentum.RueckgabeBew = 0
+  AND EinzHist.EinzHistTyp = 1
+  AND EinzHist.TraeArtiID != -1;
+
+SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Anzahl falscher Rückgabe-Teile = ' + FORMAT(@@ROWCOUNT, N'N0', N'de-AT');
+RAISERROR(@msg, 0, 1) WITH NOWAIT;
+
+BEGIN TRY
+  BEGIN TRANSACTION;
+  
+    UPDATE EinzHist SET [Status] = N'Q', Abmeldung = NULL, AbmeldDat = NULL, Ausdienst = NULL, AusdienstDat = NULL, AusdienstGrund = NULL, Einzug = NULL
+    WHERE ID IN (SELECT EinzHistID FROM #PWSCleanup);
+  
+  COMMIT;
+END TRY
+BEGIN CATCH
+  DECLARE @Message varchar(MAX) = ERROR_MESSAGE();
+  DECLARE @Severity int = ERROR_SEVERITY();
+  DECLARE @State smallint = ERROR_STATE();
+  
+  IF XACT_STATE() != 0
+    ROLLBACK TRANSACTION;
+  
+  RAISERROR(@Message, @Severity, @State) WITH NOWAIT;
+END CATCH;
+
+SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Korrekter falscher Rückgabe-Teile abgeschlossen';
+RAISERROR(@msg, 0, 1) WITH NOWAIT;
 
 GO
 
-ENABLE TRIGGER ALL ON EinzHist
-
+DROP TABLE #PWSCleanup;
 GO
--- IT-Tickets/IT-73669 - PWS-Wäschesäcke - Datenbereinigung.sql
+++ IT-Tickets/IT-73669 - PWS-Wäschesäcke - Datenbereinigung.sql
@@ -1,8 +1,12 @@
-DROP TABLE IF EXISTS #PWSSchrott;
+SET NOCOUNT ON;
+SET CONTEXT_INFO 0x1; /* AdvanTex-Trigger für RepQueue deaktivieren */
+GO
+
+DROP TABLE IF EXISTS #PWSCleanup;
 GO
 
 CREATE TABLE #PWSCleanup (
-  EinzHistID int,
+  EinzHistID int PRIMARY KEY CLUSTERED,
   EinzHistID_Schrott int,
   EinzTeilID int,
   Barcode varchar(33) COLLATE Latin1_General_CS_AS,
@@ -18,6 +22,8 @@ CREATE TABLE #PWSCleanup (
   AusdienstGrund varchar(1)
 );
 
+CREATE NONCLUSTERED INDEX IX_PWSCleanup ON #PWSCleanup (EinzTeilID) WITH (DATA_COMPRESSION = PAGE);
+
 GO
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
@@ -26,6 +32,13 @@ GO
 
 DECLARE @cutoff date = N'2022-12-31';
 DECLARE @kdgfid int = (SELECT KdGf.ID FROM KdGf WHERE KdGf.KurzBez = N'MED');
+DECLARE @weggrundid int = 167; /* IT -> Schrott */
+DECLARE @curdatetime datetime2 = GETDATE();
+DECLARE @returntime datetime2 = DATEADD(millisecond, -10, @curdatetime);
+DECLARE @ausdienstweek varchar(7) = (SELECT [Week].Woche FROM [Week] WHERE CAST(GETDATE() AS date) BETWEEN [Week].VonDat AND [Week].BisDat);
+DECLARE @userid int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST')
+DECLARE @arbplatzid int = (SELECT ID FROM ArbPlatz WHERE ComputerName = HOST_NAME());
+DECLARE @msg nvarchar(max);
 
 DECLARE @Hauptstandort TABLE (
   StandortID int,
@@ -37,6 +50,14 @@ DECLARE @Artikel TABLE (
   ArtikelNr nchar(15) COLLATE Latin1_General_CS_AS
 );
 
+DECLARE @MapTable TABLE (
+  EinzTeilID int,
+  EinzHistID int
+);
+
+SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Alte Säcke laden';
+RAISERROR(@msg, 0, 1) WITH NOWAIT;
+
 INSERT INTO @Hauptstandort (StandortKuerzel)
 VALUES (N'WOEN'), (N'WOLI');
 
@@ -44,12 +65,10 @@ UPDATE @Hauptstandort SET StandortID = Standort.ID
 FROM Standort
 WHERE Standort.SuchCode = [@Hauptstandort].StandortKuerzel;
 
-INSERT INTO @Artikel (ArtikelNr)
-VALUES (N'1800000605_1'), (N'180000000003'), (N'180000000001'), (N'180000000002'), (N'180000000004');
-
-UPDATE @Artikel SET ArtikelID = Artikel.ID
+INSERT INTO @Artikel (ArtikelID, ArtikelNr)
+SELECT Artikel.ID, Artikel.ArtikelNr
 FROM Artikel
-WHERE Artikel.ArtikelNr = [@Artikel].ArtikelNr;
+WHERE Artikel.ArtGruID IN (SELECT ArtGru.ID FROM ArtGru WHERE ArtGru.Sack = 1);
 
 INSERT INTO #PWSCleanup (EinzHistID, EinzTeilID, Barcode, KundenID, VsaID, TraegerID, TraeArtiID, KdArtiID, ArtikelID, ArtGroeID, Ausdienst, AusdienstDat, AusdienstGrund)
 SELECT EinzHist.ID, EinzHist.EinzTeilID, EinzHist.Barcode, EinzHist.KundenID, EinzHist.VsaID, EinzHist.TraegerID, EinzHist.TraeArtiID, EinzHist.KdArtiID, EinzHist.ArtikelID, EinzHist.ArtGroeID, EinzHist.Ausdienst, EinzHist.AusdienstDat, EinzHist.AusdienstGrund
@@ -59,25 +78,14 @@ JOIN Kunden ON EinzHist.KundenID = Kunden.ID
 WHERE Kunden.KdGFID = @kdgfid
   AND Kunden.StandortID IN (SELECT StandortID FROM @Hauptstandort)
   AND EinzHist.ArtikelID IN (SELECT ArtikelID FROM @Artikel)
-  AND EinzTeil.AltenheimModus = 1
+  AND EinzTeil.AltenheimModus IN (1, 2)
   AND EinzHist.EinzHistTyp = 1
   AND EinzHist.[Status] IN (N'U', N'W')
   AND EinzHist.AbmeldDat <= @cutoff
   AND ISNULL(EinzHist.Ausgang1, CAST(N'1980-01-01' AS date)) <= @cutoff;
 
-GO
-
-DECLARE @weggrundid int = 167; /* IT -> Schrott */
-DECLARE @curdatetime datetime2 = GETDATE();
-DECLARE @returntime datetime2 = DATEADD(millisecond, -10, @curdatetime);
-DECLARE @ausdienstweek varchar(7) = (SELECT [Week].Woche FROM [Week] WHERE CAST(GETDATE() AS date) BETWEEN [Week].VonDat AND [Week].BisDat);
-DECLARE @userid int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST')
-DECLARE @arbplatzid int = (SELECT ID FROM ArbPlatz WHERE ComputerName = HOST_NAME());
-
-DECLARE @MapTable TABLE (
-  EinzTeilID int,
-  EinzHistID int
-);
+SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Anzahl alter Säcke = ' + FORMAT(@@ROWCOUNT, N'N0', N'de-AT');
+RAISERROR(@msg, 0, 1) WITH NOWAIT;
 
 BEGIN TRY
   BEGIN TRANSACTION;
@@ -96,6 +104,9 @@ BEGIN TRY
     FROM @MapTable
     WHERE [@MapTable].EinzTeilID = #PWSCleanup.EinzTeilID;
 
+    SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Neuen EinzHist-Eintrag eingefügt';
+    RAISERROR(@msg, 0, 1) WITH NOWAIT;
+
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
     /* ++ Alten Umlauf-Datensatz anpassen                                                                                           ++ */
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
@@ -103,6 +114,9 @@ BEGIN TRY
     UPDATE EinzHist SET [Status] = N'Y', WegGrundID = @weggrundid, Ausdienst = ISNULL(#PWSCleanup.Ausdienst, @ausdienstweek), AusdienstDat = ISNULL(#PWSCleanup.AusdienstDat, CAST(@curdatetime AS date)), AusdienstGrund = ISNULL(#PWSCleanup.AusdienstGrund, N'Z'), Einzug = ISNULL(EinzHist.Einzug, CAST(@curdatetime AS date)), EinzHistBis = @curdatetime, UserID_ = @userid
     FROM #PWSCleanup
     WHERE #PWSCleanup.EinzHistID = EinzHist.ID;
+
+    SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Alten EinzHist-Eintrage aktualisiert';
+    RAISERROR(@msg, 0, 1) WITH NOWAIT;
 
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
     /* ++ EinzTeil-Datensatz anpassen                                                                                               ++ */
@@ -112,6 +126,9 @@ BEGIN TRY
     FROM #PWSCleanup
     JOIN @MapTable ON #PWSCleanup.EinzTeilID = [@MapTable].EinzTeilID
     WHERE #PWSCleanup.EinzTeilID = EinzTeil.ID
+
+    SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': EinzTeil-Datensatz angepasst';
+    RAISERROR(@msg, 0, 1) WITH NOWAIT;
 
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
     /* ++ Scans schreiben (Rückgabe und Schrott - jeweils auf alten Umlauf-Datensatz)                                               ++ */
@@ -125,6 +142,8 @@ BEGIN TRY
     SELECT [#PWSCleanup].EinzHistID, [#PWSCleanup].EinzTeilID, @curdatetime, CAST(7 AS int) AS ActionsID, CAST(19 AS int) AS ZielNrID, @arbplatzid AS ArbPlatzID, #PWSCleanup.TraegerID, #PWSCleanup.VsaID, @userid AS AnlageUserID_, @userid AS UserID_
     FROM #PWSCleanup;
 
+    SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Scans geschrieben';
+    RAISERROR(@msg, 0, 1) WITH NOWAIT;
   
   COMMIT;
 END TRY
@@ -138,7 +157,6 @@ BEGIN CATCH
   
   RAISERROR(@Message, @Severity, @State) WITH NOWAIT;
 END CATCH;
-
 
 GO
 
