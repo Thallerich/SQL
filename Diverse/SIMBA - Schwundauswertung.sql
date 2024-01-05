93c4e99ed0eb50004c7df2fc2363fe065465d875
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-01-05 08:03:41 +0100

    bugfix and optimization  - removed unused IsCurrEinzHist    - use join on EinzTeil.CurrEinzHistID instead   - include info if part is at customer or at laundry

--- Diverse/SIMBA - Schwundauswertung.sql
+++ Diverse/SIMBA - Schwundauswertung.sql
@@ -1,5 +1,5 @@
-DECLARE @kdnr int = 10001922;
-DECLARE @lastscanbefore date = N'2023-01-01';
+DECLARE @kdnr int = 10001933;
+DECLARE @lastscanbefore date = N'2023-07-24';
 
 DECLARE @curweek nchar(7) = (SELECT [Week].Woche FROM [Week] WHERE GETDATE() BETWEEN [Week].VonDat AND [Week].BisDat);
 DECLARE @sqltext nvarchar(max);
@@ -10,10 +10,12 @@ SET @sqltext = N'
     FROM [Status]
     WHERE [Status].Tabelle = N''EINZHIST''
   )
-  SELECT Holding.Holding, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS VsaBez, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, KdArti.Variante, EinzHist.Barcode, Teilestatus.StatusBez AS [aktueller Status], EinzTeil.LastScanTime AS [letzter Scan], DATEDIFF(day, EinzTeil.LastScanTime, GETDATE()) AS [Tage seit letztem Scan], curRW.RestwertInfo AS [Restwert aktuell]
+  SELECT Holding.Holding, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS VsaBez, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, KdArti.Variante, EinzHist.Barcode, Teilestatus.StatusBez AS [aktueller Status], EinzTeil.LastScanTime AS [letzter Scan], DATEDIFF(day, EinzTeil.LastScanTime, GETDATE()) AS [Tage seit letztem Scan],
+    [Teil beim Kunden] = IIF((SELECT TOP 1 Scans.Menge FROM Scans WHERE Scans.EinzHistID = EinzHist.ID AND Scans.Menge != 0 ORDER BY Scans.ID DESC) = 1, CAST(0 AS bit), CAST(1 AS bit)),
+    curRW.RestwertInfo AS [Restwert aktuell]
   FROM EinzHist
   CROSS APPLY funcGetRestwert(EinzHist.ID, @curweek, 1) curRW
-  JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
+  JOIN EinzTeil ON EinzTeil.CurrEinzHistID = EinzHist.ID
   JOIN Teilestatus ON EinzHist.[Status] = Teilestatus.[Status]
   JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
   JOIN Vsa ON Traeger.VsaID = Vsa.ID
@@ -24,7 +26,6 @@ SET @sqltext = N'
   JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
   WHERE Kunden.KdNr = @kdnr
     AND Vsa._IsSIMBAPool = 1
-    AND EinzHist.IsCurrEinzHist = 1
     AND EinzHist.PoolFkt = 0
     AND EinzHist.[Status] BETWEEN N''Q'' AND N''W''
     AND EinzHist.Einzug IS NULL
@@ -37,7 +38,7 @@ SET @sqltext = N'
   SELECT Holding.Holding, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS VsaBez, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, KdArti.Variante, SUM(IIF(EinzHist.Status = N''Q'', 1, 0)) AS [Menge Aufstocken]
   FROM EinzHist
   CROSS APPLY funcGetRestwert(EinzHist.ID, @curweek, 1) curRW
-  JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
+  JOIN EinzTeil ON EinzTeil.CurrEinzHistID = EinzHist.ID
   JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
   JOIN Vsa ON Traeger.VsaID = Vsa.ID
   JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -47,7 +48,6 @@ SET @sqltext = N'
   JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
   WHERE Kunden.KdNr = @kdnr
     AND Vsa._IsSIMBAPool = 1
-    AND EinzHist.IsCurrEinzHist = 1
     AND EinzHist.PoolFkt = 0
     AND EinzHist.[Status] BETWEEN N''Q'' AND N''W''
     AND EinzHist.Einzug IS NULL
