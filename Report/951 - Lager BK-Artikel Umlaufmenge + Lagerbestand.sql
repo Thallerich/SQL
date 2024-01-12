e2b3797811cf6e3069fdbcf579f68ec639f99539
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-01-12 09:01:58 +0100

    added  - columns for "taken from stock" for new and used

--- Report/951 - Lager BK-Artikel Umlaufmenge + Lagerbestand.sql
+++ Report/951 - Lager BK-Artikel Umlaufmenge + Lagerbestand.sql
@@ -2,7 +2,7 @@ DECLARE @currentweek nchar(7) = (SELECT [Week].Woche FROM [Week] WHERE CAST(GETD
 
 DROP TABLE IF EXISTS #TmpResult951;
 
-SELECT Artikel.ArtikelNr, Status.Bez AS Artikelstatus, ArtGru.Gruppe, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ProdGru.ProdGruBez$LAN$ AS Sortiment, ArtGroe.Groesse, GroePo.Folge, 0 AS Umlaufmenge, 0 AS [Bestand Neuware], 0 AS [Bestand Gebraucht], ArtGroe.ID AS ArtGroeID
+SELECT Artikel.ArtikelNr, Status.Bez AS Artikelstatus, ArtGru.Gruppe, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ProdGru.ProdGruBez$LAN$ AS Sortiment, ArtGroe.Groesse, GroePo.Folge, 0 AS Umlaufmenge, 0 AS [Bestand Neuware], 0 AS [Bestand Gebraucht], 0 AS [Entnahme Neuware], 0 AS [Entnahme Gebraucht], ArtGroe.ID AS ArtGroeID
 INTO #TmpResult951
 FROM ArtGroe, Artikel, Bereich, ProdGru, GroePo, GroeKo, ARTGRU, (SELECT Status.Status, Status.StatusBez$LAN$ AS Bez FROM Status WHERE Status.Tabelle = 'ARTIKEL') AS Status
 WHERE ArtGroe.ArtikelID = Artikel.ID
@@ -119,7 +119,29 @@ FROM #TmpResult951 AS R, (
 ) AS x
 WHERE x.ArtGroeID = R.ArtGroeID;
 
-SELECT Gruppe, ArtikelNr, Artikelstatus, Artikelbezeichnung, Sortiment, Groesse, Umlaufmenge, [Bestand Neuware], [Bestand Gebraucht]
+UPDATE R SET [Entnahme Neuware] = x.EntnahmeMenge
+FROM #TmpResult951 AS R, (
+  SELECT Bestand.ArtGroeID, SUM(Bestand.Entnahme01 + Bestand.Entnahme02 + Bestand.Entnahme03 + Bestand.Entnahme04 + Bestand.Entnahme05 + Bestand.Entnahme06 + Bestand.Entnahme07 + Bestand.Entnahme08 + Bestand.Entnahme09 + Bestand.Entnahme10 + Bestand.Entnahme11 + Bestand.Entnahme12) AS EntnahmeMenge
+  FROM Bestand, Lagerart
+  WHERE Bestand.LagerArtID = Lagerart.ID
+    AND Lagerart.LagerID IN ($1$)
+    AND Lagerart.Neuwertig = 1
+  GROUP BY Bestand.ArtGroeID
+) AS x
+WHERE x.ArtGroeID = R.ArtGroeID;
+
+UPDATE R SET [Entnahme Gebraucht] = x.EntnahmeMenge
+FROM #TmpResult951 AS R, (
+  SELECT Bestand.ArtGroeID, SUM(Bestand.Entnahme01 + Bestand.Entnahme02 + Bestand.Entnahme03 + Bestand.Entnahme04 + Bestand.Entnahme05 + Bestand.Entnahme06 + Bestand.Entnahme07 + Bestand.Entnahme08 + Bestand.Entnahme09 + Bestand.Entnahme10 + Bestand.Entnahme11 + Bestand.Entnahme12) AS EntnahmeMenge
+  FROM Bestand, Lagerart
+  WHERE Bestand.LagerArtID = Lagerart.ID
+    AND Lagerart.LagerID IN ($1$)
+    AND Lagerart.Neuwertig = 0
+  GROUP BY Bestand.ArtGroeID
+) AS x
+WHERE x.ArtGroeID = R.ArtGroeID;
+
+SELECT Gruppe, ArtikelNr, Artikelstatus, Artikelbezeichnung, Sortiment, Groesse, Umlaufmenge, [Bestand Neuware], [Bestand Gebraucht], [Entnahme Neuware], [Entnahme Gebraucht]
 FROM #TmpResult951
 WHERE (($2$ = 0) OR ($2$ = 1 AND ([Bestand Neuware] != 0 OR [Bestand Gebraucht] != 0 OR Umlaufmenge != 0)))
 ORDER BY ArtikelNr, Folge;
