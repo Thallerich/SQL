4368cecae6660fd93b42b8101cc4a737a5f8cf4c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-01-22 10:38:00 +0100

    IT-78970

--- Report/1910 - Währungskontrolle Kunden.sql
+++ Report/1910 - Währungskontrolle Kunden.sql
@@ -1,8 +1,15 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, VertragWae.WaeBez$LAN$ + N' (' + VertragWae.Code + N')' AS Vertragswährung, RechWae.WaeBez$LAN$ + N' (' + RechWae.Code + N')' AS Rechnungswährung
+WITH VertragProKunde AS (
+  SELECT DISTINCT Vertrag.KundenID, Vertrag.VertragAbschluss
+  FROM Vertrag
+  WHERE Vertrag.[Status] = N'A'
+)
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, VertragWae.WaeBez$LAN$ + N' (' + VertragWae.Code + N')' AS Vertragswährung, RechWae.WaeBez$LAN$ + N' (' + RechWae.Code + N')' AS Rechnungswährung, CAST(Kunden.Anlage_ AS date) AS [Anlagedatum Kunde], VertragProKunde.VertragAbschluss AS [Vertrag abgeschlossen am]
 FROM Kunden
 JOIN Wae AS VertragWae ON Kunden.VertragWaeID = VertragWae.ID
 JOIN Wae AS RechWae ON Kunden.RechWaeID = RechWae.ID
+LEFT JOIN VertragProKunde ON VertragProKunde.KundenID = Kunden.ID
 WHERE Kunden.FirmaID IN ($1$)
   AND Kunden.Status = N'A'
   AND Kunden.AdrArtID = 1
-  AND Kunden.SichtbarID IN ($SICHTBARIDS$);
+  AND Kunden.SichtbarID IN ($SICHTBARIDS$)
+  AND Kunden.Anlage_ > $2$;
