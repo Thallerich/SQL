61c4829a94f6630e5e8ea98a66f9cce6e36797a6
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-01-31 10:49:46 +0100

    correctly change einzteil, einzhist and write scan

--- Jobs - AdvanTex/UHF - Schwundbuchung automatisch.sql
+++ Jobs - AdvanTex/UHF - Schwundbuchung automatisch.sql
@@ -1,13 +1,38 @@
 DECLARE @HalfYearAgo datetime = DATEADD(day, -180, GETDATE());
-DECLARE @KdNr AS TABLE (KdNr int);
+DECLARE @curdatetime datetime2 = GETDATE();
+DECLARE @returntime datetime2 = DATEADD(millisecond, -10, @curdatetime);
+DECLARE @userid int = (SELECT MitarbeiID FROM #AdvSession);
+DECLARE @arbplatzid int = (SELECT ID FROM ArbPlatz WHERE ComputerName = HOST_NAME());
 
-INSERT INTO @KdNr VALUES (24045), (11050), (20000), (6071), (7240), (9013), (23041), (23042), (23044), (23032), (23037), (10001756), (242013), (2710499), (2710498), (18029), (245347), (248564), (246805), (10003247), (19080), (20156), (25033), (10001810), (10001671), (10001672), (10001770), (10001816);
+DECLARE @KdNr AS TABLE (
+  KdNr int
+);
 
-DROP TABLE IF EXISTS #TmpSchwundAuto;
+DECLARE @MapTable TABLE (
+  EinzTeilID int,
+  EinzHistID int
+);
 
-SELECT EinzTeil.ID AS EinzTeilID, IIF(EinzTeil.LastErsatzFuerKdArtiID < 0, EinzTeil.ArtikelID, KdArti.ArtikelID) AS ArtikelID, EinzTeil.VsaID AS VsaID
-INTO #TmpSchwundAuto
+/* Die folgenden Kundennummern werden von diesem Skript automatisch Schwund-gebucht */
+INSERT INTO @KdNr VALUES (24045), (11050), (20000), (6071), (7240), (9013), (23041), (23042), (23044), (23032), (23037), (10001756), (242013), (2710499), (2710498), (18029), (245347), (248564), (246805), (10003247), (19080), (20156), (10001810), (10001671), (10001672), (10001770), (10001816);
+
+DROP TABLE IF EXISTS #PoolSchwund;
+
+CREATE TABLE #PoolSchwund (
+  EinzHistID int,
+  EinzHistID_Schwund int,
+  EinzTeilID int,
+  Barcode varchar(33) COLLATE Latin1_General_CS_AS,
+  KundenID int,
+  KdArtiID int,
+  ArtikelID int,
+  ArtGroeID int
+);
+
+INSERT INTO #PoolSchwund (EinzHistID, EinzTeilID, Barcode, ArtikelID, ArtGroeID)
+SELECT EinzHist.ID, EinzHist.EinzTeilID, EinzHist.Barcode, EinzHist.ArtikelID, EinzHist.ArtGroeID
 FROM EinzTeil
+JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
 JOIN Vsa ON EinzTeil.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdArti ON EinzTeil.LastErsatzFuerKdArtiID = KdArti.ID
@@ -19,9 +44,60 @@ WHERE Kunden.KdNr IN (SELECT KdNr FROM @KdNr)
   AND EinzTeil.LastActionsID IN (102, 120, 136)
   AND EinzTeil.LastScanTime < @HalfYearAgo;
 
-UPDATE EinzTeil SET [Status] = N'W', LastActionsID = 116
-WHERE ID IN (
-  SELECT EinzTeilID FROM #TmpSchwundAuto
-);
+BEGIN TRY
+  BEGIN TRANSACTION;
+  
+    /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    /* ++ Neuen EinzHist-Eintrag (Typ 3 - ausgeschieden) erstellen und in Temp-Table eintragen                                      ++ */
+    /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-DROP TABLE IF EXISTS #TmpSchwundAuto;
+    INSERT INTO EinzHist (EinzTeilID, Barcode, EinzHistTyp, PoolFkt, [Status], EinzHistVon, ArtikelID, ArtGroeID, UserID_, AnlageUserID_)
+    OUTPUT inserted.EinzTeilID, inserted.ID
+    INTO @MapTable (EinzTeilID, EinzHistID)
+    SELECT EinzTeilID, Barcode, CAST(3 AS int) AS EinzHistTyp, CAST(1 AS bit) AS PoolFkt, CAST('Z' AS varchar(2)) AS [Status], @curdatetime AS EinzHistVon, ArtikelID, ArtGroeID, @userid AS UserID_, @userid AS AnlageUserID_
+    FROM #PoolSchwund;
+
+    UPDATE #PoolSchwund SET EinzHistID_Schwund = [@MapTable].EinzHistID
+    FROM @MapTable
+    WHERE [@MapTable].EinzTeilID = #PoolSchwund.EinzTeilID;
+
+    /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    /* ++ Alten Umlauf-Datensatz anpassen                                                                                           ++ */
+    /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+    UPDATE EinzHist SET EinzHistBis = @curdatetime, UserID_ = @userid
+    FROM #PoolSchwund
+    WHERE #PoolSchwund.EinzHistID = EinzHist.ID;
+
+    /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    /* ++ EinzTeil-Datensatz anpassen                                                                                               ++ */
+    /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+    UPDATE EinzTeil SET [Status] = N'W', CurrEinzHistID = [@MapTable].EinzHistID, LastActionsID = 116, ZielNrID = 10000105, RechPoID = -2, UserID_ = @userid
+    FROM #PoolSchwund
+    JOIN @MapTable ON #PoolSchwund.EinzTeilID = [@MapTable].EinzTeilID
+    WHERE #PoolSchwund.EinzTeilID = EinzTeil.ID
+
+    /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    /* ++ Scans schreiben (Rückgabe und Schrott - jeweils auf alten Umlauf-Datensatz)                                               ++ */
+    /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+    INSERT INTO Scans (EinzHistID, EinzTeilID, [DateTime], ActionsID, ZielNrID, ArbPlatzID, AnlageUserID_, UserID_)
+    SELECT [#PoolSchwund].EinzHistID, [#PoolSchwund].EinzTeilID, @returntime AS [DateTime], CAST(116 AS int) AS ActionsID, CAST(10000105 AS int) AS ZielNrID, @arbplatzid AS ArbPlatzID, @userid AS AnlageUserID_, @userid AS UserID_
+    FROM #PoolSchwund;
+
+  
+  COMMIT;
+END TRY
+BEGIN CATCH
+  DECLARE @Message varchar(MAX) = ERROR_MESSAGE();
+  DECLARE @Severity int = ERROR_SEVERITY();
+  DECLARE @State smallint = ERROR_STATE();
+  
+  IF XACT_STATE() != 0
+    ROLLBACK TRANSACTION;
+  
+  RAISERROR(@Message, @Severity, @State) WITH NOWAIT;
+END CATCH;
+
+DROP TABLE #PoolSchwund;
