6df9e43d34c244fdcb9e16ae10690dbf7fdc392b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-02-05 12:53:56 +0100

    optimization:  - force index usage  - remove useless joins, do subselect instead  - remove case, do iif instead

--- Report/843a - OP-Qualitätskontrolle nutzbare Teile - nur angemeldeter User.sql
+++ Report/843a - OP-Qualitätskontrolle nutzbare Teile - nur angemeldeter User.sql
@@ -3,23 +3,16 @@ DECLARE @bis datetime = DATEADD(day, 1, $ENDDATE$);
 
 WITH QKTeile AS (
   SELECT Scans.EinzTeilID, Scans.AnlageUserID_ AS UserID, CAST(Scans.[DateTime] AS date) AS Datum, Scans.ActionsID, LEAD(Scans.ActionsID) OVER (PARTITION BY Scans.EinzTeilID ORDER BY Scans.[DateTime]) AS NextActionID
-  FROM Scans
-  JOIN ArbPlatz ON Scans.ArbPlatzID = ArbPlatz.ID
-  JOIN Standort ON ArbPlatz.StandortID = Standort.ID
+  FROM Scans WITH (INDEX([DateTime]))
   WHERE Scans.[DateTime] BETWEEN @von AND @bis
-    AND Standort.ID IN ($2$)
+    AND Scans.ArbPlatzID IN (SELECT ArbPlatz.ID FROM ArbPlatz WHERE ArbPlatz.StandortID IN ($2$))
 )
 SELECT QKPivot.Datum, QKPivot.Name AS Mitarbeiter, [109] AS [Qualitätskontrolle OK], [105] AS Nachwäsche, [108] AS Schrott
 FROM (
   SELECT QKTeile.EinzTeilID,
     QKTeile.Datum,
     Mitarbei.Name,
-    ActionID =
-      CASE QKTeile.NextActionID
-        WHEN 105 THEN 105
-        WHEN 108 THEN 108
-        ELSE 109
-      END
+    ActionID = IIF(QKTeile.NextActionID IN (105, 108, 109), QKTeile.NextActionID, 109)
   FROM QKTeile
   JOIN Mitarbei ON QKTeile.UserID = Mitarbei.ID
   WHERE QKTeile.ActionsID = 109
