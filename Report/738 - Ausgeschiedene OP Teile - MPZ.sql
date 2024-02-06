7eb562c067dad864a2e4bcad5d6767e5d933518c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-02-06 14:25:09 +0100

    IT-79577  - added restwert (both variants)

--- Report/738 - Ausgeschiedene OP Teile - MPZ.sql
+++ Report/738 - Ausgeschiedene OP Teile - MPZ.sql
@@ -29,11 +29,12 @@ CREATE TABLE #OPSchrott (
   VsaID int NOT NULL,
   Erstwoche nchar(7) COLLATE Latin1_General_CS_AS,
   RuecklaufG int NOT NULL,
+  Restwert money,
   ProduktionID int NOT NULL
 );
 
-INSERT INTO #OPSchrott (EinzTeilID, [Code], ArtikelID, WegGrundID, WegDatum, VsaID, Erstwoche, RuecklaufG, ProduktionID)
-SELECT EinzTeil.ID AS EinzTeilID, EinzTeil.Code, EinzTeil.ArtikelID, EinzTeil.WegGrundID, EinzTeil.WegDatum, EinzTeil.VsaID, EinzTeil.Erstwoche, EinzTeil.RuecklaufG, ProduktionID = ISNULL((
+INSERT INTO #OPSchrott (EinzTeilID, [Code], ArtikelID, WegGrundID, WegDatum, VsaID, Erstwoche, RuecklaufG, Restwert, ProduktionID)
+SELECT EinzTeil.ID AS EinzTeilID, EinzTeil.Code, EinzTeil.ArtikelID, EinzTeil.WegGrundID, EinzTeil.WegDatum, EinzTeil.VsaID, EinzTeil.Erstwoche, EinzTeil.RuecklaufG, EinzTeil.RestwertInfo, ProduktionID = ISNULL((
     SELECT TOP 1 COALESCE(IIF(ZielNr.ProduktionsID < 0, NULL, ZielNr.ProduktionsID), IIF(ArbPlatz.StandortID < 0, NULL, ArbPlatz.StandortID), IIF(Mitarbei.StandortID < 0, NULl, Mitarbei.StandortID), -1)
     FROM Scans
     JOIN ZielNr ON Scans.ZielNrID = ZielNr.ID
@@ -48,7 +49,7 @@ WHERE EinzTeil.WegDatum BETWEEN $STARTDATE$ AND $ENDDATE$
   AND EinzTeil.WegGrundID IN ($3$)
   AND EinzTeil.Status = N'Z';
 
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, #OPSchrott.Code, WegGrund.WegGrundBez$LAN$ AS Schrottgrund, #OPSchrott.WegDatum AS Schrottdatum, ISNULL(Standort.SuchCode, N'<unbekannt>') AS [verschrottender Produktionsstandort], Kunden.KdNr AS [letzter Kunde], #OPSchrott.Erstwoche AS [Ersteinsatz-Woche], #OPSchrott.RuecklaufG AS [Anzahl Wäschen], Artikel.MaxWaschen AS [maximale Wäschen], Artikel.EKPreis AS Einkaufspreis, (Artikel.EKPreis/IIF(Artikel.MaxWaschen = 0, 1, Artikel.MaxWaschen)) * (Artikel.MaxWaschen - #OPSchrott.RuecklaufG) * IIF(Artikel.MaxWaschen = 0, 0, 1) AS Restwert
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, #OPSchrott.Code, WegGrund.WegGrundBez$LAN$ AS Schrottgrund, #OPSchrott.WegDatum AS Schrottdatum, ISNULL(Standort.SuchCode, N'<unbekannt>') AS [verschrottender Produktionsstandort], Kunden.KdNr AS [letzter Kunde], #OPSchrott.Erstwoche AS [Ersteinsatz-Woche], #OPSchrott.RuecklaufG AS [Anzahl Wäschen], Artikel.MaxWaschen AS [maximale Wäschen], Artikel.EKPreis AS Einkaufspreis, (Artikel.EKPreis/IIF(Artikel.MaxWaschen = 0, 1, Artikel.MaxWaschen)) * (Artikel.MaxWaschen - #OPSchrott.RuecklaufG) * IIF(Artikel.MaxWaschen = 0, 0, 1) AS [Restwert Kunden-unabhängig], #OPSchrott.Restwert AS [Restwert lt. RW-Konfiguration letzter Kunde]
 FROM #OPSchrott
 JOIN Artikel ON #OPSchrott.ArtikelID = Artikel.ID
 JOIN WegGrund ON #OPSchrott.WegGrundID = WegGrund.ID
