4a8497768b13f023cbdfb786f1942acaa840fd79
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-02-07 15:06:15 +0100

    changes as defined in meeting

--- Export-Definition/LST - Kunden.sql
+++ Export-Definition/LST - Kunden.sql
@@ -1,12 +1,13 @@
 DECLARE @customer TABLE (
   customerid int,
+  customername nvarchar(20),
   amountchip int,
   amountnonchip int,
   percentagechipped AS (100 / (amountchip + amountnonchip) * amountchip)
 )
 
-INSERT INTO @customer (customerid, amountchip, amountnonchip)
-SELECT Kunden.ID AS customerid, SUM(IIF(LEN(EinzHist.Barcode) = 24 OR LEN(EinzHist.RentomatChip) = 24, 1, 0)) AS amountchip, SUM(IIF(LEN(EinzHist.Barcode) != 24 AND LEN(EinzHist.RentomatChip) != 24, 1, 0)) AS amountnonchip
+INSERT INTO @customer (customerid, customername, amountchip, amountnonchip)
+SELECT Kunden.KdNr AS customerid, Kunden.SuchCode AS customername, SUM(IIF(LEN(EinzHist.Barcode) = 24 OR LEN(EinzHist.RentomatChip) = 24, 1, 0)) AS amountchip, SUM(IIF(LEN(EinzHist.Barcode) != 24 AND LEN(EinzHist.RentomatChip) != 24, 1, 0)) AS amountnonchip
 FROM EinzTeil
 JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
 JOIN Kunden ON EinzHist.KundenID = Kunden.ID
@@ -14,7 +15,7 @@ WHERE Kunden.[Status] = N'A'
   AND Kunden.AdrArtID = 1
   AND EinzTeil.LastScanTime > DATEADD(year, -1, GETDATE())
   AND EinzHist.EinzHistTyp = 1
-GROUP BY Kunden.ID
+GROUP BY Kunden.KdNr, Kunden.SuchCode
 HAVING SUM(IIF(LEN(EinzHist.Barcode) = 24 OR LEN(EinzHist.RentomatChip) = 24, 1, 0)) > 0 OR SUM(IIF(LEN(EinzHist.Barcode) != 24 AND LEN(EinzHist.RentomatChip) != 24, 1, 0)) > 0;
 
 SELECT customerid, CAST(IIF(percentagechipped > 50, 1, 0) AS bit) AS chipcoded
--- Export-Definition/LST - Kundenartikel.sql
+++ Export-Definition/LST - Kundenartikel.sql
@@ -1,14 +1,16 @@
 DECLARE @customerarticle TABLE (
   customerid int,
-  customername nvarchar(20),
-  articleid int,
-  articlename nvarchar(60),
-  sortingcategory nvarchar(60),
-  washingprogram nvarchar(40)
+  customername nvarchar(20) COLLATE Latin1_General_CS_AS,
+  articleid nvarchar(15) COLLATE Latin1_General_CS_AS,
+  articlename nvarchar(60) COLLATE Latin1_General_CS_AS,
+  sortingcategory int,
+  sortingcategorybez nvarchar(60) COLLATE Latin1_General_CS_AS,
+  washingprogram nchar(8) COLLATE Latin1_General_CS_AS,
+  washingprogrambez nvarchar(40) COLLATE Latin1_General_CS_AS
 );
 
-INSERT INTO @customerarticle (customerid, customername, articleid, articlename, sortingcategory, washingprogram)
-SELECT Kunden.ID AS customerid, Kunden.SuchCode AS customername, Artikel.ID AS articleid, Artikel.ArtikelBez AS articlename, WascSort.WascSortBez AS sortingcategory, IIF(WaschPrg.ID = -1, NULL, WaschPrg.WaschPrgBez) AS washingprogram
+INSERT INTO @customerarticle (customerid, customername, articleid, articlename, sortingcategory, sortingcategorybez, washingprogram, washingprogrambez)
+SELECT Kunden.Kdnr AS customerid, Kunden.SuchCode AS customername, Artikel.ArtikelNr AS articleid, Artikel.ArtikelBez AS articlename, WascSort.Waschsortierung AS sortingcategory, WascSort.WascSortBez AS sortingcategorybez, IIF(WaschPrg.ID = -1, NULL, WaschPrg.WaschPrg) AS washingprogram, IIF(WaschPrg.ID = -1, NULL, WaschPrg.WaschPrgBez) AS washingprogrambez
 FROM WascSoPr
 JOIN WascSort ON WascSoPr.WascSortID = WascSort.ID
 JOIN KdArti ON WascSoPr.KdArtiID = KdArti.ID
@@ -17,8 +19,8 @@ JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN WaschPrg ON KdArti.WaschPrgID = WaschPrg.ID
 WHERE WascSoPr.KdArtiID > 0;
 
-INSERT INTO @customerarticle (customerid, customername, articleid, articlename, sortingcategory, washingprogram)
-SELECT Kunden.ID AS customerid, Kunden.SuchCode AS customername, Artikel.ID AS articleid, Artikel.ArtikelBez AS articlename, WascSort.WascSortBez AS sortingcategory, IIF(WaschPrg.ID = -1, NULL, WaschPrg.WaschPrgBez) AS washingprogram
+INSERT INTO @customerarticle (customerid, customername, articleid, articlename, sortingcategory, sortingcategorybez, washingprogram, washingprogrambez)
+SELECT Kunden.KdNr AS customerid, Kunden.SuchCode AS customername, Artikel.ArtikelNr AS articleid, Artikel.ArtikelBez AS articlename, WascSort.Waschsortierung AS sortingcategory, WascSort.WascSortBez AS sortingcategorybez, IIF(WaschPrg.ID = -1, NULL, WaschPrg.WaschPrg) AS washingprogram, IIF(WaschPrg.ID = -1, NULL, WaschPrg.WaschPrgBez) AS washingprogrambez
 FROM WascSoPr
 JOIN WascSort ON WascSoPr.WascSortID = WascSort.ID
 JOIN KdArti ON WascSoPr.ArtikelID = KdArti.ArtikelID
@@ -31,8 +33,8 @@ WHERE WascSoPr.KdArtiID = -1
   AND NOT EXISTS (
     SELECT customerarticle.*
     FROM @customerarticle AS customerarticle
-    WHERE customerarticle.articleid = Artikel.ID
-      AND customerarticle.customerid = Kunden.ID
+    WHERE customerarticle.articleid = Artikel.ArtikelNr
+      AND customerarticle.customerid = Kunden.KdNr
   );
 
 SELECT * FROM @customerarticle;
