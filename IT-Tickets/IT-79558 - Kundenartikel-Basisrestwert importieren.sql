6a7b5b4efbb44fc1e4a0f162b447abac879232c1
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-02-08 13:25:13 +0100

    IT-79558  - initial version

---
+++ IT-Tickets/IT-79558 - Kundenartikel-Basisrestwert importieren.sql
@@ -0,0 +1,46 @@
+DECLARE @userid int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST');
+
+DECLARE @PreisChanged TABLE (
+  KdArtiID int,
+  LeasPreis money,
+  WaschPreis money,
+  SonderPreis money,
+  VKPreis money,
+  BasisRestwert money,
+  LeasPreisAbwAbWo money,
+  LeasPreisPrListKdArtiID int,
+  WaschPreisPrListKdArtiID int,
+  SondPreisPrListKdArtiID int,
+  VkPreisPrListKdArtiID int,
+  BasisRWPrListKdArtiID int,
+  VkPreisOld money
+);
+
+BEGIN TRY
+  BEGIN TRANSACTION;
+  
+    UPDATE KdArti SET BasisRestwert = _IT79558_BasisRW.Basisrestwert
+    OUTPUT inserted.ID, inserted.LeasPreis, inserted.WaschPreis, inserted.SonderPreis, inserted.VkPreis, inserted.BasisRestwert, inserted.LeasPreisAbwAbWo, inserted.LeasPreisPrListKdArtiID, inserted.WaschPreisPrListKdArtiID, inserted.SondPreisPrListKdArtiID, inserted.VkPreisPrListKdArtiID, inserted.BasisRWPrListKdArtiID, deleted.VkPreis
+    INTO @PreisChanged (KdArtiID, LeasPreis, WaschPreis, SonderPreis, VKPreis, BasisRestwert, LeasPreisAbwAbWo, LeasPreisPrListKdArtiID, WaschPreisPrListKdArtiID, SondPreisPrListKdArtiID, VkPreisPrListKdArtiID, BasisRWPrListKdArtiID, VkPreisOld)
+    FROM KdArti
+    JOIN Kunden ON KdArti.KundenID = Kunden.ID
+    JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+    JOIN Salesianer.dbo._IT79558_BasisRW ON Kunden.KdNr = _IT79558_BasisRW.KdNr AND Artikel.ArtikelNr = _IT79558_BasisRW.ArtikelNr COLLATE Latin1_General_CS_AS
+    WHERE KdArti.BasisRestwert != _IT79558_BasisRW.Basisrestwert;
+
+    INSERT INTO PrArchiv (KdArtiID, Datum, LeasPreis, WaschPreis, SonderPreis, VKPreis, BasisRestwert, LeasPreisAbwAbWo, MitarbeiID, Aktivierungszeitpunkt, LeasPreisPrListKdArtiID, WaschPreisPrListKdArtiID, SondPreisPrListKdArtiID, VkPreisPrListKdArtiID, BasisRWPrListKdArtiID, AnlageUserID_, UserID_)
+    SELECT KdArtiID, CAST(GETDATE() AS date) AS Datum, LeasPreis, WaschPreis, SonderPreis, VKPreis, BasisRestwert, LeasPreisAbwAbWo, @UserID AS MitarbeiID, GETDATE() AS Aktivierungszeitpunkt, LeasPreisPrListKdArtiID, WaschPreisPrListKdArtiID, SondPreisPrListKdArtiID, VkPreisPrListKdArtiID, BasisRWPrListKdArtiID, @UserID AS AnlageUserID_, @UserID AS UserID_
+    FROM @PreisChanged;
+
+  COMMIT;
+END TRY
+BEGIN CATCH
+  DECLARE @Message varchar(MAX) = ERROR_MESSAGE();
+  DECLARE @Severity int = ERROR_SEVERITY();
+  DECLARE @State smallint = ERROR_STATE();
+  
+  IF XACT_STATE() != 0
+    ROLLBACK TRANSACTION;
+  
+  RAISERROR(@Message, @Severity, @State) WITH NOWAIT;
+END CATCH;
