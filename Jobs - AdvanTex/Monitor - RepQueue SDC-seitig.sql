4084ac91d525138bcf8c9eeb00698e5e86cd4e2c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-02-19 15:12:21 +0100

    initial version

---
+++ Jobs - AdvanTex/Monitor - RepQueue SDC-seitig.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS ##SdcQueueLength, #SdcQueueLength;
+
+DECLARE @sqltext nvarchar(max);
+DECLARE @sdcbez nvarchar(40), @linkedserverstring nvarchar(100);
+
+DECLARE SdcDB CURSOR FOR
+SELECT SdcDev.Bez, LinkedServerString = CONCAT(N'[', SdcDev.LinkedServerName, N'].', SdcDev.LinkedServerDBName, N'.dbo.REPQUEUE')
+FROM SdcDev
+WHERE SdcDev.IsTriggerDest = 1;
+
+CREATE TABLE ##SdcQueueLength (
+  SDCBez nvarchar(40),
+  QueueLength bigint
+);
+
+OPEN SdcDB;
+
+FETCH NEXT FROM SdcDB INTO @sdcbez, @linkedserverstring;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+  IF @linkedserverstring IS NOT NULL
+  BEGIN
+    SET @sqltext = N'INSERT INTO ##SdcQueueLength (SDCBez, QueueLength) SELECT ''' + @sdcbez + N''', COUNT(*) FROM ' + @linkedserverstring + N' WHERE ErrorCounter < 100 AND SdcDevID = 100';
+    PRINT @sqltext;
+    EXEC sp_executesql @sqltext;  
+  END;
+
+  FETCH NEXT FROM SdcDB INTO @sdcbez, @linkedserverstring;
+END;
+
+CLOSE SdcDB;
+DEALLOCATE SdcDB;
+
+SELECT *
+INTO #SdcQueueLength
+FROM ##SdcQueueLength;
+
+DROP TABLE ##SdcQueueLength;
+
+SELECT * FROM #SdcQueueLength WHERE QueueLength > 1000;
