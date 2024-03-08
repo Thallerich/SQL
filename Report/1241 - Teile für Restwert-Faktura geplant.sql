93db2ea573477fdcd43d4fc6a16fb367f6142387
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-03-08 08:48:40 +0100

    new feature  - it is now possible to list invoiced parts

--- Report/1241 - Teile für Restwert-Faktura geplant.sql
+++ Report/1241 - Teile für Restwert-Faktura geplant.sql
@@ -1,5 +1,10 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS [Vsa-Bezeichnung], EinzHist.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Einsatz.EinsatzBez$LAN$ AS Außerdienststellungsgrund, WegGrund.WeggrundBez$LAN$ AS [Schrott-Grund], RwArt.RwArtBez$LAN$ AS [Restwert-Art], TeilSoFa.EPreis AS Restwert, Produktion.SuchCode AS Produktion, Betreuer.Name AS Kundenbetreuer, Kundenservice.Name AS Kundenservice
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS [Vsa-Bezeichnung], EinzHist.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Einsatz.EinsatzBez$LAN$ AS Außerdienststellungsgrund, WegGrund.WeggrundBez$LAN$ AS [Schrott-Grund], RwArt.RwArtBez$LAN$ AS [Restwert-Art], TeilSoFa.EPreis AS Restwert, TeilSoFaStatus.StatusBez AS [Status], IIF($3$ = 1, RechKo.RechNr, NULL) AS Rechnungsnummer, IIF($3$ = 1, Rechko.RechDat, NULL) AS Rechnungsdatum, Produktion.SuchCode AS Produktion, Betreuer.Name AS Kundenbetreuer, Kundenservice.Name AS Kundenservice
 FROM TeilSofa
+JOIN (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = N'TEILSOFA'
+) AS TeilSoFaStatus ON TeilSoFa.[Status] = TeilSoFaStatus.[Status]
 JOIN EinzHist ON TeilSoFa.EinzHistID = EinzHist.ID
 JOIN Vsa ON EinzHist.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -15,6 +20,12 @@ JOIN VsaBer ON VsaBer.VsaID = Vsa.ID AND VsaBer.KdBerID = KdBer.ID
 JOIN Mitarbei AS Betreuer ON VsaBer.BetreuerID = Betreuer.ID
 JOIN Mitarbei AS Kundenservice ON VsaBer.ServiceID = Kundenservice.ID
 JOIN WegGrund ON EinzHist.WegGrundID = WegGrund.ID
-WHERE TeilSoFa.SoFaArt = N'R'        /* Restwert-Abrechnung */
-  AND TeilSoFa.[Status] = N'L'       /* Abrechnung geplant  */
+JOIN RechPo ON TeilSoFa.RechPoID = RechPo.ID
+JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+WHERE TeilSoFa.SoFaArt = N'R' /* Restwert-Abrechnung */
+  AND (
+    (TeilSoFa.[Status] = N'L' AND $3$ = 0) /* Abrechnung geplant  */
+    OR
+    (TeilSoFa.[Status] = N'P' AND RechKo.RechDat BETWEEN $STARTDATE$ AND $ENDDATE$ AND $3$ = 1) /* abgerechnet, nicht wieder gutgeschrieben */
+  )
   AND Vsa.StandKonID IN ($1$);
