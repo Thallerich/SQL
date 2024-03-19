4578c77cc2b813b4ce1201c129a664c153970ef3
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-03-19 15:34:11 +0100

    IT-81058  - initial version

---
+++ IT-Tickets/IT-81058 - Echte Berufsgruppe in Sonstige Daten einfügen.sql
@@ -0,0 +1,11 @@
+WITH DataUpdate AS (
+  SELECT Traeger.ID AS TraegerID, /* Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, KdArti.VariantBez AS Berufsgruppe, Traeger.VormalsNr AS Sonstiges, */ _IT81058.BGreal
+  FROM Traeger
+  JOIN Vsa ON Traeger.VsaID = Vsa.ID
+  JOIN KdArti ON Traeger.BerufsgrKdArtiID = KdArti.ID
+  JOIN _IT81058 ON _IT81058.VsaNr = Vsa.VsaNr AND _IT81058.Traeger = Traeger.Traeger AND _IT81058.Vorname = Traeger.Vorname AND _IT81058.Nachname = Traeger.Nachname
+  WHERE Vsa.KundenID = (SELECT ID FROM Kunden WHERE KdNr = 19009)
+)
+UPDATE Traeger SET VormalsNr = DataUpdate.BGreal
+FROM DataUpdate
+WHERE DataUpdate.TraegerID = Traeger.ID;
