b7d6b7e9eab8d9db0579aaf6d6f0d7ea52dce52a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-03-25 12:37:52 +0100

    IT-80833  - copied from "Barcodeliste Bekleidungs-LS"  - amended for excel export

---
+++ Rechnungsanlagen/Barcodeliste Bekleidungs-LS (Excel).sql
@@ -0,0 +1,21 @@
+SELECT Kunden.KdNr, Vsa.VsaNr, Vsa.SuchCode AS VsaSuchCode, Vsa.Name1 AS Adresszeile1, Vsa.Name2 AS Adresszeile2, Vsa.Name3 AS Adresszeile3, Vsa.Strasse AS Straße, Vsa.Land, Vsa.PLZ, Vsa.Ort, Vsa.MemoLS, LsKo.LsNr, LsKo.Datum, Touren.Bez AS Tour, Touren.Tour AS TourKurz, TRIM(Mitarbei.Nachname) + IIF(Mitarbei.Vorname <> '', ', ' + TRIM(Mitarbei.Vorname), '') AS Fahrer, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, ArtGroe.Groesse AS Größe, Abteil.Bez AS Kostenstelle, Traeger.Traeger AS Träger, Traeger.Nachname, Traeger.Vorname, EinzHist.Barcode, Scans.[DateTime] AS [Auslese-Zeitpunkt]
+FROM Scans
+JOIN EinzHist ON Scans.EinzHistID = EinzHist.ID
+JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
+JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
+JOIN LsPo ON Scans.LsPoID = LsPo.ID
+JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+JOIN Vsa ON LsKo.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
+JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+JOIN Fahrt ON LsKo.FahrtID = Fahrt.ID
+JOIN Touren ON Fahrt.TourenID = Touren.ID
+JOIN Mitarbei ON Fahrt.MitarbeiID = Mitarbei.ID
+JOIN RechPo ON LsPo.RechPoID = RechPo.ID
+JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
+JOIN Abteil ON LsPo.AbteilID = Abteil.ID
+WHERE RechPo.RechKoID = $RECHKOID$
+  AND EinzTeil.AltenheimModus = 0 --keine Bewohnerwäsche
+  AND Scans.EinzHistID > 0
+ORDER BY Kunden.KdNr, Vsa.VsaNr, LsKo.LsNr, Traeger.Nachname, Artikel.ArtikelNr;
