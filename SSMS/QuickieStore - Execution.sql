57cac1883a927672821ede9050bc3af90813b165
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-03-27 15:55:31 +0100

    initial version

---
+++ SSMS/QuickieStore - Execution.sql
@@ -0,0 +1,3 @@
+EXEC Salesianer_Archive.dbo.sp_QuickieStore @database_name = N'Salesianer', @sort_order = N'cpu', @execution_count = 5, @workdays = 1, @work_start = N'6am', @work_end = N'10pm'
+GO
+/* EXEC Salesianer_Archive.dbo.sp_QuickieStore @help = 1 */
