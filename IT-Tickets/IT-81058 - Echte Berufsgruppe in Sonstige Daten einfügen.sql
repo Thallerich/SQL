95ae55e2410a393f1dbb23b100d0363f535f105a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-04-11 07:18:56 +0200

    optimization  - added a check if a value is set already  - added a truncate statement (commented out)

--- IT-Tickets/IT-81058 - Echte Berufsgruppe in Sonstige Daten einfügen.sql
+++ IT-Tickets/IT-81058 - Echte Berufsgruppe in Sonstige Daten einfügen.sql
@@ -4,8 +4,13 @@ WITH DataUpdate AS (
   JOIN Vsa ON Traeger.VsaID = Vsa.ID
   JOIN KdArti ON Traeger.BerufsgrKdArtiID = KdArti.ID
   JOIN _IT81058 ON _IT81058.VsaNr = Vsa.VsaNr AND _IT81058.Traeger = Traeger.Traeger AND _IT81058.Vorname = Traeger.Vorname AND _IT81058.Nachname = Traeger.Nachname
-  WHERE Vsa.KundenID = (SELECT ID FROM Kunden WHERE KdNr = 19009)
+  WHERE Vsa.KundenID = (SELECT ID FROM Kunden WHERE KdNr = 2511145)
 )
 UPDATE Traeger SET VormalsNr = DataUpdate.BGreal
 FROM DataUpdate
-WHERE DataUpdate.TraegerID = Traeger.ID;
+WHERE DataUpdate.TraegerID = Traeger.ID
+  AND Traeger.VormalsNr IS NULL;
+
+/*
+TRUNCATE TABLE _IT81058;
+*/
