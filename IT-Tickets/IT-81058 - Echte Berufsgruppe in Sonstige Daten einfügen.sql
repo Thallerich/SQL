1488365cfeb4e94995945dbc58dfa8069d81c923
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-04-15 10:19:09 +0200

    IT-81999  - added check for ksst and persnr

--- IT-Tickets/IT-81058 - Echte Berufsgruppe in Sonstige Daten einfügen.sql
+++ IT-Tickets/IT-81058 - Echte Berufsgruppe in Sonstige Daten einfügen.sql
@@ -1,10 +1,11 @@
 WITH DataUpdate AS (
-  SELECT Traeger.ID AS TraegerID, /* Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, KdArti.VariantBez AS Berufsgruppe, Traeger.VormalsNr AS Sonstiges, */ _IT81058.BGreal
+  SELECT Traeger.ID AS TraegerID, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, KdArti.VariantBez AS Berufsgruppe, Traeger.VormalsNr AS Sonstiges, _IT81058.BGreal
   FROM Traeger
   JOIN Vsa ON Traeger.VsaID = Vsa.ID
   JOIN KdArti ON Traeger.BerufsgrKdArtiID = KdArti.ID
-  JOIN _IT81058 ON _IT81058.VsaNr = Vsa.VsaNr AND _IT81058.Traeger = Traeger.Traeger AND _IT81058.Vorname = Traeger.Vorname AND _IT81058.Nachname = Traeger.Nachname
-  WHERE Vsa.KundenID = (SELECT ID FROM Kunden WHERE KdNr = 2511145)
+  JOIN Abteil ON Traeger.AbteilID = Abteil.ID
+  JOIN _IT81058 ON _IT81058.VsaNr = Vsa.VsaNr AND _IT81058.Traeger = Traeger.Traeger AND _IT81058.Vorname = Traeger.Vorname AND _IT81058.Nachname = Traeger.Nachname AND _IT81058.KsSt = Abteil.Abteilung AND ISNULL(_IT81058.PersNr, N'') = ISNULL(Traeger.PersNr, N'')
+  WHERE Vsa.KundenID = (SELECT ID FROM Kunden WHERE KdNr = 19010)
 )
 UPDATE Traeger SET VormalsNr = DataUpdate.BGreal
 FROM DataUpdate
