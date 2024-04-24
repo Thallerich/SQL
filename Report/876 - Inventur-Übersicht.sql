ab52f44ede075592576e468d77da529775b509f5
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-04-24 10:36:44 +0200

    IT-82315  - added VsaAnf.Statistik1

--- Report/876 - Inventur-Übersicht.sql
+++ Report/876 - Inventur-Übersicht.sql
@@ -1,4 +1,4 @@
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, InvKo.Anlage_ AS [Inventur-Zeitpunkt], InvKo.Datum AS [Inventur für Datum], Vsa.VsaNr, Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, InvPo.HistSollPuffer AS [Soll-Puffer zum Planungszeitpunkt], InvPo.Menge AS [inventierte Menge]
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, InvKo.Anlage_ AS [Inventur-Zeitpunkt], InvKo.Datum AS [Inventur für Datum], Vsa.VsaNr, Vsa.Bez AS [Vsa-Bezeichnung], Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, VsaAnf.Statistik1 AS [anforderbarer Artikel Statistik 1], InvPo.HistSollPuffer AS [Soll-Puffer zum Planungszeitpunkt], InvPo.Menge AS [inventierte Menge]
 FROM InvPo
 JOIN InvKo ON InvPo.InvKoID = InvKo.ID
 JOIN Vsa ON InvPo.VsaID = Vsa.ID
@@ -7,6 +7,7 @@ JOIN KdArti ON InvPo.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN ArtGroe ON InvPo.ArtGroeID = ArtGroe.ID
 JOIN GroePo ON Artikel.GroeKoID = GroePo.GroeKoID AND ArtGroe.Groesse = GroePo.Groesse
+LEFT JOIN VsaAnf ON VsaAnf.VsaID = Vsa.ID AND VsaAnf.KdArtiID = KdArti.ID AND VsaAnf.ArtGroeID = ArtGroe.ID
 WHERE Kunden.ID = $ID$
   AND InvKo.Anlage_ BETWEEN CAST($STARTDATE$ AS datetime2) AND CAST(DATEADD(day, 1, $ENDDATE$) AS datetime2)
 ORDER BY [Inventur-Zeitpunkt], VsaNr, ArtikelNr, GroePo.Folge;
