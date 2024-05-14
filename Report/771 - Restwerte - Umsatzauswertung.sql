5add51e590a82f8d552b0a29778e887d5cad4288
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-05-14 14:00:15 +0200

    IT-82918  - added filter by zone  - added column for type of residual value calc.

--- Report/771 - Restwerte - Umsatzauswertung.sql
+++ Report/771 - Restwerte - Umsatzauswertung.sql
@@ -1,14 +1,17 @@
-SELECT KdGf.KurzBez AS Geschäftsbereich, Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, RechKo.RechNr, RechKo.RechDat AS Rechnungsdatum, RPoType.RPoTypeBez$LAN$ AS [Rechnungstyp], SUM(RechPo.GPreis) AS [Umsatz netto]
+SELECT KdGf.KurzBez AS Geschäftsbereich, Firma.SuchCode AS Firma, [Zone].ZonenCode AS Vertriebszone, Kunden.KdNr, Kunden.SuchCode AS Kunde, RechKo.RechNr, RechKo.RechDat AS Rechnungsdatum, RPoType.RPoTypeBez$LAN$ AS [Rechnungstyp], RwArt.RwArtBez$LAN$ AS [Restwert-Art], SUM(RechPo.GPreis) AS [Umsatz netto]
 FROM RechPo
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
 JOIN Kunden ON RechKo.KundenID = Kunden.ID
 JOIN Firma ON RechKo.FirmaID = Firma.ID
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN [Zone] ON Kunden.ZoneID = [Zone].ID
 JOIN RPoType ON RechPo.RPoTypeID = RPoType.ID
-WHERE Firma.ID IN ($1$)
+LEFT JOIN TeilSoFa ON TeilSoFa.RechPoID = RechPo.ID
+LEFT JOIN RwArt ON TeilSoFa.RwArtID = RwArt.ID
+WHERE Kunden.FirmaID IN ($1$)
+  AND Kunden.KdGfID IN ($2$)
+  AND Kunden.ZoneID IN ($3$)
   AND RechKo.RechDat BETWEEN $STARTDATE$ AND $ENDDATE$
-  AND KdGf.ID IN ($2$)
   AND RPoType.StatistikGruppe = N'Restwerte'
   AND Kunden.SichtbarID IN ($SICHTBARIDS$)
-GROUP BY KdGf.KurzBez, Firma.SuchCode, Kunden.KdNr, Kunden.SuchCode, RechKo.Rechnr, RechKo.RechDat, RPoType.RPoTypeBez$LAN$
-ORDER BY Firma, KdNr, Rechnungsdatum;
+GROUP BY KdGf.KurzBez, Firma.SuchCode, [Zone].ZonenCode, Kunden.KdNr, Kunden.SuchCode, RechKo.Rechnr, RechKo.RechDat, RPoType.RPoTypeBez$LAN$, RwArt.RwArtBez$LAN$;
