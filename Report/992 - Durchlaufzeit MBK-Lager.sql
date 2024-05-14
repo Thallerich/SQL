e199886d286b948189170ffb8b96ab7f6847329f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-05-14 14:03:43 +0200

    IT-82897  - removed cte for "Einlesescan", doing it inline now  - switched to explict joins  - added criteria for which einzhist-rows to show  - added column "Lager-Endkontrolle"

--- Report/992 - Durchlaufzeit MBK-Lager.sql
+++ Report/992 - Durchlaufzeit MBK-Lager.sql
@@ -4,38 +4,59 @@ DECLARE @bis datetime;
 SET @von = $1$;
 SET @bis = DATEADD(day, 1, $2$);
 
-WITH Einlesescan AS (
-  SELECT MIN(Scans.ID) AS FirstScanID, Scans.EinzHistID
-  FROM Scans
-  WHERE Scans.Menge = 1
-    AND Scans.EinzHistID > 0
-  GROUP BY Scans.EinzHistID
-),
-Ausgangsscan AS (
+WITH Ausgangsscan AS (
   SELECT MIN(Scans.ID) AS FirstOutScanID, Scans.EinzHistID
   FROM Scans
   WHERE Scans.Menge = -1
     AND Scans.EinzHistID > 0
   GROUP BY Scans.EinzHistID
 )
-SELECT KdGf.KurzBez AS SGF, EinzHist.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse, Auftrag.AuftragsNr, Auftrag.Zeitpunkt AS [Erstelldatum Auftrag], CONVERT(date, EinzHist.Anlage_) AS Anlage, AnlageUser.Name AS [Benutzer der Bestellung], EntnKo._WorkOrderNumber AS [ABS-Pickliste], EinzHist.Entnommen, EinzHist.PatchDatum, Scans.[DateTime] AS Einlesescan, EinzHist.IndienstDat AS Aktivierung, LsKo.LsNr, LsKo.Datum AS [erstes Lieferscheindatum], DATEDIFF(day, EinzHist.Anlage_, EinzHist.IndienstDat) AS Durchlaufzeit
+SELECT KdGf.KurzBez AS SGF,
+  EinzHist.Barcode,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung,
+  ArtGroe.Groesse AS Größe,
+  Auftrag.AuftragsNr,
+  Auftrag.Zeitpunkt AS [Erstelldatum Auftrag],
+  CAST(EinzHist.Anlage_ AS date) AS Anlage,
+  Mitarbei.Name AS [Benutzer der Bestellung],
+  EinzHist.Entnommen,
+  EinzHist.PatchDatum,
+  [Lager-Endkontrolle] = (
+    SELECT TOP 1 Scans.[DateTime]
+    FROM Scans
+    WHERE Scans.EinzHistID = EinzHist.ID
+      AND Scans.ActionsID = 49
+    ORDER BY Scans.[DateTime] DESC
+  ),
+  Einlesescan = (
+    SELECT TOP 1 Scans.[DateTime]
+    FROM Scans
+    WHERE Scans.EinzHistID = EinzHist.ID
+      AND Scans.Menge = -1
+    ORDER BY Scans.[DateTime] ASC
+  ),
+  EinzHist.IndienstDat AS Aktivierung,
+  LsKo.LsNr,
+  LsKo.Datum AS [erstes Lieferscheindatum],
+  DATEDIFF(day, EinzHist.Anlage_, EinzHist.IndienstDat) AS Durchlaufzeit
 FROM EinzHist
-LEFT JOIN Auftrag ON EinzHist.StartAuftragID = Auftrag.ID
-LEFT JOIN Einlesescan ON EinzHist.ID = Einlesescan.EinzHistID
-LEFT JOIN Scans ON Einlesescan.FirstScanID = Scans.ID
+JOIN Auftrag ON EinzHist.StartAuftragID = Auftrag.ID
+JOIN Vsa ON EinzHist.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+JOIN KdGf ON Kunden.KdGfID = KdGf.ID
+JOIN Artikel ON EinzHist.ArtikelID = Artikel.ID
+JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
+JOIN Lagerart ON EinzHist.LagerArtID = Lagerart.ID
+JOIN Mitarbei ON EinzHist.AnlageUserID_ = Mitarbei.ID
+JOIN EntnPo ON EinzHist.EntnPoID = EntnPo.ID
+JOIN EntnKo ON EntnPo.EntnKoID = EntnKo.ID
 LEFT JOIN Ausgangsscan ON EinzHist.ID = Ausgangsscan.EinzHistID
 LEFT JOIN Scans AS OutScan ON Ausgangsscan.FirstOutScanID = OutScan.ID
 LEFT JOIN LsPo ON OutScan.LsPoID = LsPo.ID
-LEFT JOIN LsKo ON LsPo.LsKoID = LsKo.ID, Vsa, Kunden, KdGf, Artikel, ArtGroe, LagerArt, Mitarbei AS AnlageUser, EntnPo, EntnKo
-WHERE EinzHist.VsaID = Vsa.ID
-  AND Vsa.KundenID = Kunden.ID
-  AND Kunden.KdGfID = KdGf.ID
-  AND EinzHist.ArtikelID = Artikel.ID
-  AND EinzHist.ArtGroeID = ArtGroe.ID
-  AND EinzHist.LagerArtID = LagerArt.ID
-  AND EinzHist.AnlageUserID_ = Anlageuser.ID
-  AND EinzHist.EntnPoID = EntnPo.ID
-  AND EntnPo.EntnKoID = EntnKo.ID
-  AND EinzHist.Status <> '5' --keine stornierten Teile
+LEFT JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+WHERE EinzHist.Status != '5' --keine stornierten Teile
   AND LagerArt.LagerID = $3$ --Lagerstandort
-  AND EinzHist.Anlage_ BETWEEN @von AND @bis;
+  AND EinzHist.Anlage_ BETWEEN @von AND @bis
+  AND EinzHist.EinzHistTyp = 1
+  AND CAST(EinzHist.Anlage_ AS date) <= EinzHist.PatchDatum;
