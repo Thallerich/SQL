f2dd884ab79bf67e138a14cf7bb695d7e12776bd
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-05-17 07:15:32 +0200

    added production info

--- Diverse/UHF - Reader für Pulk-Einlese-Service.sql
+++ Diverse/UHF - Reader für Pulk-Einlese-Service.sql
@@ -1,6 +1,7 @@
-SELECT ZielNr.ZielNrBez, ZielNr.Funktion, CAST(ArbPSetT.Wert AS nchar(15)) AS IP
+SELECT Standort.Bez AS Produktion, ZielNr.ZielNrBez, ZielNr.Funktion, CAST(ArbPSetT.Wert AS nchar(15)) AS IP
 FROM ArbPSetT
 JOIN ZielNr ON ArbPSetT.ZielNrID = ZielNr.ID
+JOIN Standort ON ZielNr.ProduktionsID = Standort.ID
 WHERE ArbPSetT.ZielNrID > 0
   AND ArbPSetT.Bereich = N'UHFReader'
   AND ArbPSetT.Schluessel = N'IP'
