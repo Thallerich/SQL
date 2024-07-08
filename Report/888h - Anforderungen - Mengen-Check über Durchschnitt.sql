2fa916dedf027a12eb141753cda2d722205a8941
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-07-08 08:29:20 +0200

    IT-84553  - added filter for customer service

--- Report/888h - Anforderungen - Mengen-Check über Durchschnitt.sql
+++ Report/888h - Anforderungen - Mengen-Check über Durchschnitt.sql
@@ -4,7 +4,7 @@
 
 DROP TABLE IF EXISTS #TmpLsKontrolle888h;
 
-SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS VsaBezeichnung, AnfKo.AuftragsNr AS Packzettelnummer, AnfKo.Lieferdatum, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS ArtikelBez, ArtGroe.Groesse AS Größe, AnfPo.Angefordert, COALESCE(IIF(ArtiStan.Packmenge <= 0, NULL, ArtiStan.Packmenge), Artikel.Packmenge) AS Packmenge, VsaAnf.Durchschnitt, AnfPo.ID AS AnfPoID, Vsa.ID AS VsaID, Artikel.ID AS ArtikelID, AnfPo.ArtGroeID, KdBer.BetreuerID, KdBer.VertreterID, KdBer.ServiceID
+SELECT Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.SuchCode AS VsaStichwort, Vsa.Bez AS VsaBezeichnung, AnfKo.AuftragsNr AS Packzettelnummer, AnfKo.Lieferdatum, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS ArtikelBez, ArtGroe.Groesse AS Größe, AnfPo.Angefordert, COALESCE(IIF(ArtiStan.Packmenge <= 0, NULL, ArtiStan.Packmenge), Artikel.Packmenge) AS Packmenge, VsaAnf.Durchschnitt, AnfPo.ID AS AnfPoID, Vsa.ID AS VsaID, Artikel.ID AS ArtikelID, AnfPo.ArtGroeID, VsaBer.BetreuerID, VsaBer.VertreterID, VsaBer.ServiceID
 INTO #TmpLsKontrolle888h
 FROM AnfPo
 JOIN AnfKo ON AnfPo.AnfKoID = AnfKo.ID
@@ -13,6 +13,7 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN KdArti ON AnfPo.KdArtiID = KdArti.ID
 JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+JOIN VsaBer ON VsaBer.VsaID = Vsa.ID AND VsaBer.KdBerID = KdBer.ID
 JOIN ArtGroe ON AnfPo.ArtGroeID = ArtGroe.ID
 JOIN VsaAnf ON AnfKo.VsaID = VsaAnf.VsaID AND AnfPo.KdArtiID = VsaAnf.KdArtiID AND AnfPo.ArtGroeID = VsaAnf.ArtGroeID
 LEFT JOIN ArtiStan ON ArtiStan.ArtikelID = Artikel.ID AND ArtiStan.StandortID = AnfKo.ProduktionID
@@ -54,4 +55,5 @@ JOIN Mitarbei AS Vertrieb ON LSK.VertreterID = Vertrieb.ID
 JOIN Mitarbei AS Kundenservice ON LSK.ServiceID = Kundenservice.ID
 CROSS APPLY dbo.funcGetNextVsaTouren(LSK.VsaID, -1, CAST(GETDATE() AS date), 0, 1, 0, 1, 1, 1, 0, 0) AS NextTour
 WHERE LSK.Lieferdatum = NextTour.NextDate
+  AND LSK.ServiceID IN ($2$)
 ORDER BY LSK.KdNr, LSK.VsaStichwort, Packzettel;
