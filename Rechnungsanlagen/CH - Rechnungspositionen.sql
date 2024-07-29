a0d7917292acc7cf88a419e935751a4e247c249a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-07-29 13:11:44 +0200

    IT-85271  - new: split by calendar week

--- Rechnungsanlagen/CH - Rechnungspositionen.sql
+++ Rechnungsanlagen/CH - Rechnungspositionen.sql
@@ -1,15 +1,28 @@
-SELECT RechPo.Menge,
+SELECT SUM(LsPo.Menge) AS Menge,
+  [Week].Woche AS Kalenderwoche,
   RechPo.Bez AS Positionsbezeichnung,
   Artikel.ArtikelNr,
   KdArti.Variante,
   Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung,
   RechPo.EPreis AS Einzelpreis,
-  RechPo.GPreis AS Positionssumme,
+  SUM(LsPo.Menge) * RechPo.EPreis AS Positionssumme,
   Abteil.Abteilung AS Kostenstelle,
   RechPo.Rabatt,
   RechPo.RabattProz AS Rabattsatz
-FROM RechPo
+FROM LsPo
+JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+JOIN [Week] ON LsKo.Datum BETWEEN [Week].VonDat AND [Week].BisDat
+JOIN RechPo ON LsPo.RechPoID = RechPo.ID
 JOIN Abteil ON RechPo.AbteilID = Abteil.ID
 LEFT OUTER JOIN KdArti ON KdArti.ID = RechPo.KdArtiID
 LEFT OUTER JOIN Artikel ON Artikel.ID = KdArti.ArtikelID
-WHERE RechPo.RechKoID = $RECHKOID$;
+WHERE RechPo.RechKoID = $RECHKOID$
+GROUP BY [Week].Woche,
+  RechPo.Bez,
+  Artikel.ArtikelNr,
+  KdArti.Variante,
+  Artikel.ArtikelBez$LAN$,
+  RechPo.EPreis,
+  Abteil.Abteilung,
+  RechPo.Rabatt,
+  RechPo.RabattProz;
