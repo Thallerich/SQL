bfe42ae148a19d0fcbb92efe5cf86d84e71cbd01
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-08-22 07:33:10 +0200

    optimization  - display username and user short name  - search username and user short name if advantex user is specified

--- Diverse/Arbeitsplatz - Loginlog mit Startparameter.sql
+++ Diverse/Arbeitsplatz - Loginlog mit Startparameter.sql
@@ -1,7 +1,7 @@
 DECLARE @Computer nvarchar(50) = N'';
 
 IF @Computer != N''
-  SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
+  SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.MitarbeiUser AS Benutzer, Mitarbei.UserName AS Kürzel, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
   FROM LoginLog
   JOIN ArbPlatz ON LoginLog.ArbPlatzID = ArbPlatz.ID
   JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
@@ -13,7 +13,7 @@ GO
 DECLARE @WinUser nvarchar(50) = N'';
 
 IF @WinUser != N''
-  SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
+  SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.MitarbeiUser AS Benutzer, Mitarbei.UserName AS Kürzel, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
   FROM LoginLog
   JOIN ArbPlatz ON LoginLog.ArbPlatzID = ArbPlatz.ID
   JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
@@ -25,11 +25,11 @@ GO
 DECLARE @AdvantexUser nvarchar(50) = N'';
 
 IF @AdvantexUser != N''
-  SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.UserName, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
+  SELECT TOP 50 ArbPlatz.ComputerName, Mitarbei.MitarbeiUser AS Benutzer, Mitarbei.UserName AS Kürzel, Mitarbei.Name, LoginLog.LogInZeit, LoginLog.AdvanTexVersion, REPLACE(LoginLog.StartParameter, N'/Exit=Yes /MANDANT:Salesianer', N'') AS StartParameter, LoginLog.WindowsUserName
   FROM LoginLog
   JOIN ArbPlatz ON LoginLog.ArbPlatzID = ArbPlatz.ID
   JOIN Mitarbei ON LoginLog.UserID = Mitarbei.ID
-  WHERE Mitarbei.UserName = @AdvantexUser
+  WHERE (Mitarbei.MitarbeiUser = @AdvantexUser OR Mitarbei.UserName = @AdvantexUser)
   ORDER BY LoginLog.LoginZeit DESC;
 
 GO
