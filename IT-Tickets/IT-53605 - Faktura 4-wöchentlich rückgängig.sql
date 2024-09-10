6279336740e6e328767c89b6ae02fc41ec4c1a9c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-09-10 09:54:44 +0200

    IT-86582  - parameterized the script  - made transaction safe

--- IT-Tickets/IT-53605 - Faktura 4-wöchentlich rückgängig.sql
+++ IT-Tickets/IT-53605 - Faktura 4-wöchentlich rückgängig.sql
@@ -1,67 +1,76 @@
-UPDATE AbtKdArW SET RechPoID = -1
-WHERE RechPoID IN (
-  SELECT RechPo.ID
-  FROM RechPo
-  JOIN RechKo ON RechPo.RechKoID = RechKoID
-  WHERE RechPo.FakLaufID IN (48336, 48338)
-    AND RechKo.RechNr < 0
-);
+DECLARE @rechnr int = -11757538;
+DECLARE @userid int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST');
 
-DECLARE @LsNoFakt TABLE (
-  LsKoID int
-);
+BEGIN TRY
+  BEGIN TRANSACTION;
 
-UPDATE LsPo SET RechPoID = -1
-OUTPUT deleted.LsKoID INTO @LsNoFakt
-WHERE RechPoID IN (
-  SELECT RechPo.ID
-  FROM RechPo
-  JOIN RechKo ON RechPo.RechKoID = RechKoID
-  WHERE RechPo.FakLaufID IN (48336, 48338)
-    AND RechKo.RechNr < 0
-);
+    UPDATE AbtKdArW SET RechPoID = -1, UserID_ = @userid
+    WHERE RechPoID IN (
+      SELECT RechPo.ID
+      FROM RechPo
+      JOIN FakLauf ON RechPo.FakLaufID = FakLauf.ID
+      WHERE RechPo.RechKoID = (SELECT ID FROM RechKo WHERE RechNr = @rechnr)
+        AND FakLauf.Restwerte = 0
+    );
 
-UPDATE LsKo SET [Status] = N'Q'
-WHERE ID IN (
-  SELECT DISTINCT LsKoID
-  FROM @LsNoFakt
-);
+    DECLARE @LsNoFakt TABLE (
+      LsKoID int
+    );
 
-DECLARE @RKoDel TABLE (
-  RechKoID int
-);
+    UPDATE LsPo SET RechPoID = -1, UserID_ = @userid
+    OUTPUT deleted.LsKoID INTO @LsNoFakt
+    WHERE RechPoID IN (
+      SELECT RechPo.ID
+      FROM RechPo
+      JOIN FakLauf ON RechPo.FakLaufID = FakLauf.ID
+      WHERE RechPo.RechKoID = (SELECT ID FROM RechKo WHERE RechNr = @rechnr)
+        AND FakLauf.Restwerte = 0
+    );
 
-DELETE FROM RechPo
-OUTPUT deleted.RechKoID INTO @RKoDel
-WHERE ID IN (
-  SELECT RechPo.ID
-  FROM RechPo
-  JOIN RechKo ON RechPo.RechKoID = RechKoID
-  WHERE RechPo.FakLaufID IN (48336, 48338)
-    AND RechKo.RechNr < 0
-);
+    UPDATE LsKo SET [Status] = N'Q'
+    WHERE ID IN (
+      SELECT DISTINCT LsKoID
+      FROM @LsNoFakt
+    );
 
-DECLARE @KundenChange TABLE (
-  KundenID int
-);
+    DECLARE @RKoDel TABLE (
+      RechKoID int,
+      KdBerID int
+    );
 
-DELETE FROM RechKo
-OUTPUT deleted.KundenID INTO @KundenChange
-WHERE ID IN (
-    SELECT DISTINCT RechKoID
-    FROM @RKoDel
-  )
-  AND NOT EXISTS (
-    SELECT RechPo.*
-    FROM RechPo
-    WHERE RechPo.RechKoID = RechKo.ID
-  );
+    DELETE FROM RechPo
+    OUTPUT deleted.RechKoID, deleted.KdBerID INTO @RKoDel (RechKoID, KdBerID)
+    WHERE ID IN (
+      SELECT RechPo.ID
+      FROM RechPo
+      JOIN FakLauf ON RechPo.FakLaufID = FakLauf.ID
+      WHERE RechPo.RechKoID = (SELECT ID FROM RechKo WHERE RechNr = @rechnr)
+        AND FakLauf.Restwerte = 0
+    );
 
-UPDATE KdBer SET FakBisDat = N'2021-10-10', FakVonDat = N'2021-09-13'
-WHERE KdBer.KundenID IN (
-    SELECT DISTINCT KundenID
-    FROM @KundenChange
-  )
-  AND (KdBer.FakBisDat = N'2021-11-07' OR KdBer.FakBisDat = N'2021-11-08');
+    DELETE FROM RechKo
+    WHERE ID IN (
+        SELECT DISTINCT RechKoID
+        FROM @RKoDel
+      )
+      AND NOT EXISTS (
+        SELECT RechPo.*
+        FROM RechPo
+        WHERE RechPo.RechKoID = RechKo.ID
+      );
 
-UPDATE BrLauf SET LetzterLauf = N'' WHERE ID = 22;
+    UPDATE KdBer SET FakBisDat = DATEADD(week, -4, FakBisDat), FakVonDat = DATEADD(week, -4, FakVonDat)
+    WHERE KdBer.ID IN (SELECT KdBerID FROM @RKoDel);
+  
+  COMMIT;
+END TRY
+BEGIN CATCH
+  DECLARE @Message varchar(MAX) = ERROR_MESSAGE();
+  DECLARE @Severity int = ERROR_SEVERITY();
+  DECLARE @State smallint = ERROR_STATE();
+  
+  IF XACT_STATE() != 0
+    ROLLBACK TRANSACTION;
+  
+  RAISERROR(@Message, @Severity, @State) WITH NOWAIT;
+END CATCH;
