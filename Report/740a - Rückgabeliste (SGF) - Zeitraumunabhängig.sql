4a344bec4c12fe3b95f773479a840ebd317e1e24
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-09-17 10:59:34 +0200

    IT-82333  - added customer service, sales and key account  - added leasing price

--- Report/740a - Rückgabeliste (SGF) - Zeitraumunabhängig.sql
+++ Report/740a - Rückgabeliste (SGF) - Zeitraumunabhängig.sql
@@ -20,7 +20,7 @@ TeileSTatus as (
   FROM [Status]
   WHERE [Status].Tabelle = N'EINZHIST'
 )
-SELECT KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenstatus.StatusBez AS Kundenstatus, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], VsaStatus.StatusBez AS [VSA-Status], VSA.Name1, VSA.Name2, Vsa.GebaeudeBez AS Gebäude, Abteil.Abteilung AS [Stamm-Kostenstelle], Abteil.Bez AS [Stamm-Kostenstellenbezeichnung], Traeger.Traeger AS [Träger-Nr], Traeger.PersNr AS Personalnummer, Traeger.Nachname, Traeger.Vorname, Traeger.Titel, Traegerstatus.StatusBez AS Trägerstatus, IIF(Traeger.BerufsgrKdArtiID < 0, NULL, Berufsgruppe.VariantBez) AS Berufsgruppe, Traeger.SchrankInfo, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Barcode, TeileStatus.StatusBez AS [Status Teil], EinzHist.Abmeldung, RestwertAktuell.RestwertInfo AS Restwert, RestwertAktuell.Alterinfo AS [Alter in Wochen], Standort.Suchcode AS Expedition, EinzHist.Indienst, Einsatz.EinsatzBez$LAN$ AS Ausdienstgrund, Weggrund.WeggrundBez$LAN$ AS Schrottgrund, KdArti.ID AS KdArtiID
+SELECT KdGf.KurzBez AS Geschäftsbereich, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenstatus.StatusBez AS Kundenstatus, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], VsaStatus.StatusBez AS [VSA-Status], VSA.Name1, VSA.Name2, Vsa.GebaeudeBez AS Gebäude, Abteil.Abteilung AS [Stamm-Kostenstelle], Abteil.Bez AS [Stamm-Kostenstellenbezeichnung], Traeger.Traeger AS [Träger-Nr], Traeger.PersNr AS Personalnummer, Traeger.Nachname, Traeger.Vorname, Traeger.Titel, Traegerstatus.StatusBez AS Trägerstatus, IIF(Traeger.BerufsgrKdArtiID < 0, NULL, Berufsgruppe.VariantBez) AS Berufsgruppe, Traeger.SchrankInfo, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Barcode, TeileStatus.StatusBez AS [Status Teil], EinzHist.Indienst as [Erstwoche], EinzHist.Abmeldung, RestwertAktuell.RestwertInfo AS Restwert, RestwertAktuell.Alterinfo AS [Alter in Wochen], Standort.Suchcode AS Expedition, Einsatz.EinsatzBez$LAN$ AS Ausdienstgrund, Weggrund.WeggrundBez$LAN$ AS Schrottgrund, [Service].[Name] AS Kundenservice, Betreuer.[Name] AS Kundenbetreuer, Vertreter.[Name] AS Vertrieb, KdArti.LeasPreis AS Mietpreis, KdArti.ID AS KdArtiID
 FROM EinzTeil
 JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
 CROSS APPLY funcGetRestwert(EinzHist.ID, @curweek, 1) AS RestwertAktuell
@@ -29,6 +29,7 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
 JOIN KdArti ON EinzHist.KdArtiID = KdArti.ID
 JOIN KdBer ON KdArti.KdBerID = KdBer.ID
+JOIN VsaBer ON VsaBer.KdBerID = KdBer.ID AND VsaBer.VsaID = Vsa.ID
 JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON EinzHist.ArtikelID = Artikel.ID
 JOIN KdGf ON Kunden.KdGfID = KdGf.ID
@@ -42,6 +43,9 @@ JOIN StandBer ON Vsa.StandKonID = StandBer.StandKonID AND KdBer.BereichID = Stan
 JOIN Standort ON StandBer.ExpeditionID = Standort.ID
 JOIN WegGrund ON EinzHist.WegGrundID = WegGrund.ID
 JOIN Einsatz ON EinzHist.Ausdienstgrund = Einsatz.Einsatzgrund
+JOIN Mitarbei AS [Service] ON VsaBer.ServiceID = [Service].ID
+JOIN Mitarbei AS Betreuer ON VsaBer.BetreuerID = Betreuer.ID
+JOIN Mitarbei AS Vertreter ON VsaBer.VertreterID = Vertreter.ID
 WHERE Kunden.ID IN ($3$)
   AND KdBer.BereichID IN ($4$)
   AND Standort.ID IN ($5$)
