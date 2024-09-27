88ab1013f15a654dea685b7f0ccf9d6c1685e203
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-09-27 07:59:13 +0200

    include runtime and lock info

--- Troubleshooting and Maintenance/Blocking über WhoIsActive-Logging.sql
+++ Troubleshooting and Maintenance/Blocking über WhoIsActive-Logging.sql
@@ -1,4 +1,4 @@
-SELECT TOP 100 DENSE_RANK() OVER (ORDER BY WhoIsActive.collection_time) AS GroupNr, WhoIsActive.collection_time, WhoIsActive.session_id, WhoIsActive.blocking_session_id, WhoIsActive.sql_text, WhoIsActive.sql_command, WhoIsActive.wait_info, REPLACE(SUBSTRING(WhoIsActive.wait_info, 2, CHARINDEX(N')', WhoIsActive.wait_info, 1) - 2), N'ms', N'') AS wait_time, WhoIsActive.login_name, WhoIsActive.status, WhoIsActive.tran_start_time, WhoIsActive.host_name, WhoIsActive.database_name, WhoIsActive.program_name, WhoIsActive.start_time, WhoIsActive.additional_info
+SELECT TOP 100 DENSE_RANK() OVER (ORDER BY WhoIsActive.collection_time) AS GroupNr, WhoIsActive.collection_time, WhoIsActive.[dd hh:mm:ss.mss], WhoIsActive.session_id, WhoIsActive.blocking_session_id, WhoIsActive.sql_text, WhoIsActive.sql_command, WhoIsActive.wait_info, REPLACE(SUBSTRING(WhoIsActive.wait_info, 2, CHARINDEX(N')', WhoIsActive.wait_info, 1) - 2), N'ms', N'') AS wait_time, WhoIsActive.login_name, WhoIsActive.status, WhoIsActive.tran_start_time, WhoIsActive.host_name, WhoIsActive.database_name, WhoIsActive.program_name, WhoIsActive.start_time, WhoIsActive.additional_info, WhoIsActive.locks
 FROM Salesianer_Archive.dbo.WhoIsActive
 JOIN (
 	SELECT DISTINCT WhoIsActive.collection_time, WhoIsActive.session_id
@@ -9,5 +9,5 @@ JOIN (
 		WHERE blocking_session_id IS NOT NULL
 	) blocked_session ON blocked_session.collection_time = WhoIsActive.collection_time AND (blocked_session.blocking_session_id = WhoIsActive.session_id OR WhoIsActive.blocking_session_id IS NOT NULL)
 ) x ON x.collection_time = WhoIsActive.collection_time AND x.session_id = WhoIsActive.session_id
-WHERE WhoIsActive.collection_time > DATEADD(hour, -1, GETDATE())
+WHERE WhoIsActive.collection_time BETWEEN N'2024-09-27 06:00:00.000' AND N'2024-09-27 06:17:00.000'
 ORDER BY x.collection_time DESC;
