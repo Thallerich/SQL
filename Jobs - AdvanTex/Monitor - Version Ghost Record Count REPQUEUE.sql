05b9cb456eab05eb5b1bde1f9df0d47fee408b52
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-10-31 09:52:52 +0100

    cast message to shorten column length

--- Jobs - AdvanTex/Monitor - Version Ghost Record Count REPQUEUE.sql
+++ Jobs - AdvanTex/Monitor - Version Ghost Record Count REPQUEUE.sql
@@ -6,4 +6,4 @@ SELECT @vgr_count = sum(version_ghost_record_count)
 FROM sys.dm_db_index_physical_stats (@db_id, @object_id, 0, NULL, 'detailed');
 
 IF @vgr_count > 1000000
-  SELECT N'Version ghost record count in REPQUEUE is at ' + FORMAT(@vgr_count, N'##,#', N'de-AT') + N'!' AS [Alert];
+  SELECT CAST(N'Version ghost record count in REPQUEUE is at ' + FORMAT(@vgr_count, N'##,#', N'de-AT') + N'!' AS nvarchar(60)) AS [Alert];
