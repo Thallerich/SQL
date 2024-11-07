926b374d758e87f0e5f20e330fcdaba324e69f02
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-11-07 11:13:53 +0100

    modified due to switch to advantex

--- Report/736a - UHF-Teile verschrottet - Standort.sql
+++ Report/736a - UHF-Teile verschrottet - Standort.sql
@@ -1,14 +1,15 @@
-SELECT Standort.Bez AS Produktionsstandort, Artikel.ArtikelNr, Artikel.ArtikelNr2 AS [BMD-ArtikelNr], Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, WegGrund.WegGrundBez$LAN$ AS Schrottgrund, COUNT(EINZTEIL.ID) AS Menge, (SUM(EINZTEIL.RuecklaufG) / COUNT(EINZTEIL.ID)) AS [Durchschnitt Waschzyklen]
-FROM EINZTEIL, ZielNr, Standort, Artikel, WegGrund
-WHERE EINZTEIL.ZielNrID = ZielNr.ID
-  AND ZielNr.ProduktionsID = Standort.ID
-  AND Standort.ID IN ($3$)
-  AND ZielNr.GeraeteNr IS NOT NULL
-  AND EINZTEIL.ArtikelID = Artikel.ID
-  AND Artikel.EAN IS NOT NULL --nur UHF-Artikel
-  AND EINZTEIL.WegGrundID = WegGrund.ID
-  AND EINZTEIL.WegGrundID IN ($2$)
-  AND EINZTEIL.WegDatum BETWEEN $STARTDATE$ AND $ENDDATE$
-  AND EINZTEIL.Status = 'Z'
-GROUP BY Standort.Bez, Artikel.ArtikelNr, ARtikel.ArtikelNr2, Artikel.ArtikelBez$LAN$, Artikel.EKPreis, WegGrund.WegGrundBez$LAN$
-ORDER BY Produktionsstandort, Artikelbezeichnung;
+DECLARE @startdatetime datetime2 = CAST($STARTDATE$ AS datetime2), @enddatetime datetime2 = CAST(DATEADD(day, 1, $ENDDATE$) AS datetime);
+
+SELECT Standort.Bez AS Produktionsstandort, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, WegGrund.WegGrundBez$LAN$ AS Schrottgrund, COUNT(EINZTEIL.ID) AS Menge, (SUM(EINZTEIL.RuecklaufG) / COUNT(EINZTEIL.ID)) AS [Durchschnitt Waschzyklen]
+FROM Scans
+JOIN EinzTeil ON Scans.EinzTeilID = EinzTeil.ID
+JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
+JOIN WegGrund ON EinzTeil.WegGrundID = WegGrund.ID
+JOIN Mitarbei ON Scans.AnlageUserID_ = Mitarbei.ID
+JOIN Standort ON Mitarbei.StandortID = Standort.ID
+WHERE Mitarbei.StandortID IN ($3$)
+  AND EinzTeil.WegGrundID IN ($2$)
+  AND Scans.[DateTime] >= @startdatetime
+  AND Scans.[DateTime] < @enddatetime
+  AND Scans.ActionsID = 108 /* Schrott */
+GROUP BY Standort.Bez, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, Artikel.EKPreis, WegGrund.WegGrundBez$LAN$;
