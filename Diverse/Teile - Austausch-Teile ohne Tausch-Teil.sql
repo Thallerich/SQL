ac2fcafeac5a185a0200be28866c18627e99e598
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-11-19 08:14:44 +0100

    added zone and main location

--- Diverse/Teile - Austausch-Teile ohne Tausch-Teil.sql
+++ Diverse/Teile - Austausch-Teile ohne Tausch-Teil.sql
@@ -3,13 +3,15 @@ WITH Teilestatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = N'EINZHIST'
 )
-SELECT Firma.SuchCode AS Firma, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS Vsa, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Barcode, Teilestatus.StatusBez AS [Status Teil], EinzHist.AbmeldDat AS [Abmelde-Datum], EinzHist.AusdienstDat AS [Außerdienststellungs-Datum], __EinzHistNachfolgeEinzHist.AustauschScanDateTime AS [Zeitpunkt Austausch-Scan], __EinzHistNachfolgeEinzHist.StopAuftragZeitpunkt AS [Zeitpunkt Teil-Abmeldung]
+SELECT Firma.SuchCode AS Firma, [Zone].ZonenCode AS Vertriebszone, Standort.SuchCode AS Hauptstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Vsa.VsaNr, Vsa.Bez AS Vsa, Traeger.Traeger, Traeger.Vorname, Traeger.Nachname, Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Barcode, Teilestatus.StatusBez AS [Status Teil], EinzHist.AbmeldDat AS [Abmelde-Datum], EinzHist.AusdienstDat AS [Außerdienststellungs-Datum], __EinzHistNachfolgeEinzHist.AustauschScanDateTime AS [Zeitpunkt Austausch-Scan], __EinzHistNachfolgeEinzHist.StopAuftragZeitpunkt AS [Zeitpunkt Teil-Abmeldung]
 FROM __EinzHistNachfolgeEinzHist
 JOIN EinzHist ON __EinzHistNachfolgeEinzHist.AltEinzHistID = EinzHist.ID
 JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
 JOIN Vsa ON EinzHist.VsaID = Vsa.ID
 JOIN Kunden ON EinzHist.KundenID = Kunden.ID
 JOIN Firma ON Kunden.FirmaID = Firma.ID
+JOIN Standort ON Kunden.StandortID = Standort.ID
+JOIN [Zone] ON Kunden.ZoneID = [Zone].ID
 JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON EinzHist.ArtikelID = Artikel.ID
 JOIN Teilestatus ON EinzHist.[Status] = Teilestatus.[Status]
