a8bb8e32402cd0a1653723eaf69e40d8482a70dd
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-11-28 12:31:16 +0100

    added restwert

--- Report/10030 - Lagerbestand - einzelne Barcodes mit Einlager-Zeitpunkt.sql
+++ Report/10030 - Lagerbestand - einzelne Barcodes mit Einlager-Zeitpunkt.sql
@@ -1,12 +1,14 @@
-SELECT EinzHist.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS [Zeitpunkt Einbuchung], Einzteil.Erstdatum as [Erstdatum], EINZTEIL.Ruecklaufg as [Anzahl Wäschen], Lagerart.LagerartBez$LAN$ AS Lagerart, Lagerart.Neuwertig, Lager.Bez AS Lagerstandort, Lagerort.Lagerort, Kunden.KdNr AS [KdNr letzter Kunde], Kunden.SuchCode AS [letzter Kunde]
+SELECT EinzHist.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS [Zeitpunkt Einbuchung], Einzteil.Erstdatum as [Erstdatum], EinzTeil.RuecklaufG AS [Anzahl Wäschen], Lagerart.LagerartBez$LAN$ AS Lagerart, Lagerart.Neuwertig, Lager.Bez AS Lagerstandort, Lagerort.Lagerort, Kunden.KdNr AS [KdNr letzter Kunde], Kunden.SuchCode AS [letzter Kunde], EinzHist.RestwertInfo AS [Restwert in Lagerwährung], Firma.WaeID AS [Restwert in Lagerwährung_WaeID] , EinzTeil.AlterInfo AS [Alter in Wochen]
 FROM EinzTeil
 JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
 JOIN Lagerart ON EinzHist.LagerartID = Lagerart.ID
 JOIN Standort AS Lager ON Lagerart.LagerID = Lager.ID
+JOIN Firma ON Lagerart.FirmaID = Firma.ID
 JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
 JOIN Kunden ON EinzHist.KundenID = Kunden.ID
 JOIN Lagerort ON EinzHist.LagerortID = Lagerort.ID
 WHERE Lager.ID IN ($1$)
+AND Lagerart.ID IN ($2$)
   AND EinzHist.Status IN (N'X', N'XE')
   AND EinzHist.EinzHistTyp = 2;
