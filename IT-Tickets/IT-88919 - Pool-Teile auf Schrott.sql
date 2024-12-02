5647dfdfa72e988db4a8fd4d1122abf7b304525e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-12-02 12:36:42 +0100

    bugfix  - file renamed - only does pool  - fixed setting fields correctly for pool

--- IT-Tickets/IT-88919 - Pool-Teile auf Schrott.sql
+++ IT-Tickets/IT-88919 - Pool-Teile auf Schrott.sql
@@ -17,16 +17,8 @@ CREATE TABLE #Cleanup (
   EinzHistID_Schrott int,
   EinzTeilID int,
   Barcode varchar(33) COLLATE Latin1_General_CS_AS,
-  KundenID int,
-  VsaID int,
-  TraegerID int,
-  TraeArtiID int,
-  KdArtiID int,
   ArtikelID int,
-  ArtGroeID int,
-  Ausdienst varchar(7),
-  AusdienstDat date,
-  AusdienstGrund varchar(1)
+  ArtGroeID int
 );
 
 CREATE NONCLUSTERED INDEX IX_Cleanup ON #Cleanup (EinzTeilID);
@@ -49,13 +41,14 @@ DECLARE @MapTable TABLE (
 SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Teile für Schrott in Temp-Table laden';
 RAISERROR(@msg, 0, 1) WITH NOWAIT;
 
-INSERT INTO #Cleanup (EinzHistID, EinzTeilID, Barcode, KundenID, VsaID, TraegerID, TraeArtiID, KdArtiID, ArtikelID, ArtGroeID, Ausdienst, AusdienstDat, AusdienstGrund)
-SELECT EinzHist.ID, EinzHist.EinzTeilID, EinzHist.Barcode, EinzHist.KundenID, EinzHist.VsaID, EinzHist.TraegerID, EinzHist.TraeArtiID, EinzHist.KdArtiID, EinzHist.ArtikelID, EinzHist.ArtGroeID, EinzHist.Ausdienst, EinzHist.AusdienstDat, EinzHist.AusdienstGrund
+INSERT INTO #Cleanup (EinzHistID, EinzTeilID, Barcode, ArtikelID, ArtGroeID)
+SELECT EinzHist.ID, EinzHist.EinzTeilID, EinzHist.Barcode, EinzHist.ArtikelID, EinzHist.ArtGroeID
 FROM EinzTeil
 JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
 JOIN Kunden ON EinzHist.KundenID = Kunden.ID
-WHERE EinzHist.Barcode IN (SELECT Barcode FROM _IT88919)
-  AND EinzHist.EinzHistTyp = 1;
+WHERE EinzHist.Barcode IN (SELECT Barcode FROM Salesianer.dbo._IT88875)
+  AND EinzHist.EinzHistTyp = 1
+  AND EinzHist.PoolFkt = 1;
 
 SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Anzahl zu verschrottende Teile = ' + FORMAT(@@ROWCOUNT, N'N0', N'de-AT');
 RAISERROR(@msg, 0, 1) WITH NOWAIT;
@@ -67,10 +60,10 @@ BEGIN TRY
     /* ++ Neuen EinzHist-Eintrag (Typ 3 - ausgeschieden) erstellen und in Temp-Table eintragen                                      ++ */
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-    INSERT INTO EinzHist (EinzTeilID, Barcode, EinzHistTyp, [Status], EinzHistVon, KundenID, VsaID, TraegerID, TraeArtiID, KdArtiID, ArtikelID, ArtGroeID, Ausdienst, AusdienstDat, WegGrundID, UserID_, AnlageUserID_)
+    INSERT INTO EinzHist (EinzTeilID, Barcode, EinzHistTyp, PoolFkt, [Status], EinzHistVon, ArtikelID, ArtGroeID, UserID_, AnlageUserID_)
     OUTPUT inserted.EinzTeilID, inserted.ID
     INTO @MapTable (EinzTeilID, EinzHistID)
-    SELECT EinzTeilID, Barcode, CAST(3 AS int) AS EinzHistTyp, CAST('Y' AS varchar(2)) AS [Status], @curdatetime AS EinzHistVon, KundenID, VsaID, TraegerID, CAST(-1 AS int) AS TraeArtiID, KdArtiID, ArtikelID, ArtGroeID, ISNULL(Ausdienst, @ausdienstweek) AS Ausdienst, ISNULL(AusdienstDat, CAST(@curdatetime AS date)) AS AusdienstDat, @weggrundid AS WegGrundID, @userid AS UserID_, @userid AS AnlageUserID_
+    SELECT EinzTeilID, Barcode, CAST(3 AS int) AS EinzHistTyp, CAST(1 AS bit) AS PoolFkt, CAST('Y' AS varchar(2)) AS [Status], @curdatetime AS EinzHistVon, ArtikelID, ArtGroeID, @userid AS UserID_, @userid AS AnlageUserID_
     FROM #Cleanup;
 
     UPDATE #Cleanup SET EinzHistID_Schrott = [@MapTable].EinzHistID
@@ -84,7 +77,7 @@ BEGIN TRY
     /* ++ Alten Umlauf-Datensatz anpassen                                                                                           ++ */
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-    UPDATE EinzHist SET [Status] = N'Y', WegGrundID = @weggrundid, Ausdienst = ISNULL(#Cleanup.Ausdienst, @ausdienstweek), AusdienstDat = ISNULL(#Cleanup.AusdienstDat, CAST(@curdatetime AS date)), AusdienstGrund = ISNULL(#Cleanup.AusdienstGrund, N'Z'), Einzug = ISNULL(EinzHist.Einzug, CAST(@curdatetime AS date)), EinzHistBis = @curdatetime, UserID_ = @userid
+    UPDATE EinzHist SET EinzHistBis = @curdatetime, UserID_ = @userid
     FROM #Cleanup
     WHERE #Cleanup.EinzHistID = EinzHist.ID;
 
@@ -95,7 +88,7 @@ BEGIN TRY
     /* ++ EinzTeil-Datensatz anpassen                                                                                               ++ */
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-    UPDATE EinzTeil SET [Status] = N'Z', CurrEinzHistID = [@MapTable].EinzHistID, LastScanTime = @curdatetime, LastActionsID = 7, ZielNrID = 19, WegGrundID = @weggrundid, WegDatum = CAST(@curdatetime AS date), UserID_ = @userid
+    UPDATE EinzTeil SET [Status] = N'Z', CurrEinzHistID = [@MapTable].EinzHistID, LastScanTime = @curdatetime, LastActionsID = 108, ZielNrID = -1, WegGrundID = @weggrundid, WegDatum = CAST(@curdatetime AS date), UserID_ = @userid
     FROM #Cleanup
     JOIN @MapTable ON #Cleanup.EinzTeilID = [@MapTable].EinzTeilID
     WHERE #Cleanup.EinzTeilID = EinzTeil.ID
@@ -107,12 +100,8 @@ BEGIN TRY
     /* ++ Scans schreiben (Rückgabe und Schrott - jeweils auf alten Umlauf-Datensatz)                                               ++ */
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-    INSERT INTO Scans (EinzHistID, EinzTeilID, [DateTime], ActionsID, ZielNrID, ArbPlatzID, TraegerID, VsaID, AnlageUserID_, UserID_)
-    SELECT [#Cleanup].EinzHistID, [#Cleanup].EinzTeilID, @returntime, CAST(6 AS int) AS ActionsID, CAST(6 AS int) AS ZielNrID, @arbplatzid AS ArbPlatzID, #Cleanup.TraegerID, #Cleanup.VsaID, @userid AS AnlageUserID_, @userid AS UserID_
-    FROM #Cleanup;
-
-    INSERT INTO Scans (EinzHistID, EinzTeilID, [DateTime], ActionsID, ZielNrID, ArbPlatzID, TraegerID, VsaID, AnlageUserID_, UserID_)
-    SELECT [#Cleanup].EinzHistID, [#Cleanup].EinzTeilID, @curdatetime, CAST(7 AS int) AS ActionsID, CAST(19 AS int) AS ZielNrID, @arbplatzid AS ArbPlatzID, #Cleanup.TraegerID, #Cleanup.VsaID, @userid AS AnlageUserID_, @userid AS UserID_
+    INSERT INTO Scans (EinzHistID, EinzTeilID, [DateTime], ActionsID, ZielNrID, ArbPlatzID, GrundID, AnlageUserID_, UserID_)
+    SELECT [#Cleanup].EinzHistID, [#Cleanup].EinzTeilID, @returntime, CAST(108 AS int) AS ActionsID, CAST(-1 AS int) AS ZielNrID, @arbplatzid AS ArbPlatzID, @weggrundid AS GrundID, @userid AS AnlageUserID_, @userid AS UserID_
     FROM #Cleanup;
 
     SELECT @msg = FORMAT(GETDATE(), N'dd.MM.yyyy HH:mm:ss', N'de-AT') + N': Scans geschrieben';
