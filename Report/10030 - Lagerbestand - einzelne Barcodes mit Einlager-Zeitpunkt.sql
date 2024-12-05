1e70e7281a19e7087afce1d0fe1b873f712c5125
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-12-05 08:53:38 +0100

    changes  - changes made by UlriUl:    - Lagerart in WHERE, AlterInfo in SELECT - new column "mit Applikation"

--- Report/10030 - Lagerbestand - einzelne Barcodes mit Einlager-Zeitpunkt.sql
+++ Report/10030 - Lagerbestand - einzelne Barcodes mit Einlager-Zeitpunkt.sql
@@ -1,4 +1,4 @@
-SELECT EinzHist.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS [Zeitpunkt Einbuchung], Einzteil.Erstdatum as [Erstdatum], EinzTeil.RuecklaufG AS [Anzahl Wäschen], Lagerart.LagerartBez$LAN$ AS Lagerart, Lagerart.Neuwertig, Lager.Bez AS Lagerstandort, Lagerort.Lagerort, Kunden.KdNr AS [KdNr letzter Kunde], Kunden.SuchCode AS [letzter Kunde], EinzHist.RestwertInfo AS [Restwert in Lagerwährung], Firma.WaeID AS [Restwert in Lagerwährung_WaeID] , EinzTeil.AlterInfo AS [Alter in Wochen]
+SELECT EinzHist.Barcode, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, EinzHist.Anlage_ AS [Zeitpunkt Einbuchung], Einzteil.Erstdatum as [Erstdatum], EinzTeil.RuecklaufG AS [Anzahl Wäschen], Lagerart.LagerartBez$LAN$ AS Lagerart, Lagerart.Neuwertig, Lager.Bez AS Lagerstandort, Lagerort.Lagerort, Kunden.KdNr AS [KdNr letzter Kunde], Kunden.SuchCode AS [letzter Kunde], EinzHist.RestwertInfo AS [Restwert in Lagerwährung], Firma.WaeID AS [Restwert in Lagerwährung_WaeID] , EinzTeil.AlterInfo AS [Alter in Wochen], CAST(IIF(EXISTS(SELECT TeilAppl.ID FROM TeilAppl WHERE TeilAppl.EinzHistID = EinzHist.ID), 1, 0) AS bit) [mit Applikation]
 FROM EinzTeil
 JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
 JOIN Lagerart ON EinzHist.LagerartID = Lagerart.ID
@@ -8,7 +8,6 @@ JOIN ArtGroe ON EinzHist.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
 JOIN Kunden ON EinzHist.KundenID = Kunden.ID
 JOIN Lagerort ON EinzHist.LagerortID = Lagerort.ID
-WHERE Lager.ID IN ($1$)
-AND Lagerart.ID IN ($2$)
+WHERE Lagerart.ID IN ($2$)
   AND EinzHist.Status IN (N'X', N'XE')
   AND EinzHist.EinzHistTyp = 2;
