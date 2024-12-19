6a96620ed9a666a78d1c2bb78226d82a9737f3cb
Thallerich <20878157+Thallerich@users.noreply.github.com>
2024-12-19 11:02:05 +0100

    IT-89739   - initial version as rewrite from original by MeyeCr

---
+++ Report/1403 - Anzahl Träger pro Kunde_VSA.sql
@@ -0,0 +1,26 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ IT-89739                                                                                                                  ++ */
+/* ++                                                                                                                           ++ */
+/* ++ Author: Stefan THALLER - 2024-12-19                                                                                       ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+SELECT Kunden.KdNr,
+  Kunden.SuchCode AS Kunde,
+  Vsa.VsaNr,
+  Vsa.Bez AS [Vsa-Bezeichnung],
+  Vsa.ID AS VsaID,
+  Touren = STUFF((SELECT DISTINCT N', ' + Touren.Tour FROM VsaTour JOIN Touren ON VsaTour.TourenID = Touren.ID WHERE VsaTour.VsaID = Vsa.ID AND CAST(GETDATE() AS date) BETWEEN VsaTour.VonDatum AND VsaTour.BisDatum FOR XML PATH(N'')), 1, 2, N''),
+  COUNT(DISTINCT Traeger.ID) AS [Anzahl Träger],
+  SUM(TraeArti.Menge) AS [Anzahl Teile],
+  [Anzahl Träger historisch] = (SELECT COUNT(DISTINCT _Umlauf.TraegerID) FROM _Umlauf JOIN Traeger ON _Umlauf.TraegerID = Traeger.ID JOIN Artikel ON _Umlauf.ArtikelID = Artikel.ID WHERE _Umlauf.VsaID = Vsa.ID AND _Umlauf.TraegerID > 0 AND Traeger.Altenheim = 0 AND Artikel.ArtiTypeID = 1 AND _Umlauf.Datum = (SELECT [Week].BisDat FROM [Week] WHERE $3$ BETWEEN [Week].VonDat AND [Week].BisDat)),
+  [Anzahl Teile historisch] = (SELECT SUM(_Umlauf.Umlauf) FROM _Umlauf JOIN Traeger ON _Umlauf.TraegerID = Traeger.ID JOIN Artikel ON _Umlauf.ArtikelID = Artikel.ID WHERE _Umlauf.VsaID = Vsa.ID AND _Umlauf.TraegerID > 0 AND Traeger.Altenheim = 0 AND Artikel.ArtiTypeID = 1 AND _Umlauf.Datum = (SELECT [Week].BisDat FROM [Week] WHERE $3$ BETWEEN [Week].VonDat AND [Week].BisDat))
+FROM TraeArti
+JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
+JOIN Vsa ON Traeger.VsaID = Vsa.ID
+JOIN Kunden ON Vsa.KundenID = Kunden.ID
+WHERE Traeger.Status != N'I'
+  AND Traeger.Altenheim = 0
+  AND Vsa.Status = N'A'
+  AND Kunden.ID IN ($2$)
+GROUP BY Kunden.KdNr, Kunden.SuchCode, Vsa.ID, Vsa.VsaNr, Vsa.Bez
+ORDER BY KdNr, VsaNr;
