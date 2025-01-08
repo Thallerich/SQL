65329f894fc85d0709373fa82de8f86c5b3c5527
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-01-08 11:32:41 +0100

    removed temp table

--- Report/614 - Artikelliste.sql
+++ Report/614 - Artikelliste.sql
@@ -1,27 +1,36 @@
-DROP TABLE IF EXISTS #TmpArtiList;
-
-SELECT Artikel.ID AS ArtikelID, Artikel.ArtikelNr, Artikel.ArtikelNr2 AS [BMD-Nr.], Artikel.SuchCode AS [Artikel-Stichwort], Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ABC.ABC AS [ABC-Klasse], ABC.ABCBez AS [ABC-Klasse Bezeichnung], Bereich.BereichBez$LAN$ AS Bereich, ArtGru.ArtGruBez$LAN$ AS Artikelgruppe, Artikel.PackMenge, Me.MeBez$LAN$ AS Mengeneinheit, Status.StatusBez AS Artikelstatus, Artikel.Anlage_ AS Artikelanlage, ISNULL(Mitarbei.UserName, N'(unbekannt)') AS Anlagebenutzer, CAST(0 AS numeric(18,4)) AS [Liefermenge letzte 6 Monate]
-INTO #TmpArtiList
-FROM Bereich, ArtGru, Me, (SELECT Status.Status, Status.StatusBez$LAN$ AS StatusBez FROM Status WHERE Status.Tabelle = 'ARTIKEL') AS Status, ABC, Artikel
-LEFT OUTER JOIN Mitarbei ON Artikel.AnlageUserID_ = Mitarbei.ID
-WHERE Artikel.BereichID = Bereich.ID
-  AND Artikel.ArtGruID = ArtGru.ID
-  AND Artikel.Status = Status.Status
-  AND Artikel.MeID = Me.ID
-  AND Artikel.AbcID = ABC.ID
-  AND Bereich.ID IN ($1$)
-  AND ArtGru.ID IN ($2$)
+SELECT Artikel.ID AS ArtikelID,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelNr2 AS [BMD-Nr.],
+  Artikel.SuchCode AS [Artikel-Stichwort],
+  Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung,
+  ABC.ABC AS [ABC-Klasse],
+  ABC.ABCBez AS [ABC-Klasse Bezeichnung],
+  Bereich.BereichBez$LAN$ AS Bereich,
+  ArtGru.ArtGruBez$LAN$ AS Artikelgruppe,
+  Artikel.PackMenge,
+  Me.MeBez$LAN$ AS Mengeneinheit,
+  Artikelstatus.StatusBez AS Artikelstatus,
+  Artikel.Anlage_ AS Artikelanlage,
+  ISNULL(Mitarbei.UserName, N'(unbekannt)') AS Anlagebenutzer,
+  [Liefermenge letzte 6 Monate] = (
+    SELECT SUM(LsPo.Menge)
+    FROM LsPo
+    JOIN LsKo ON LsPo.LsKoID = LsKo.ID
+    JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
+    WHERE LsKo.Datum >= CAST(DATEADD(month, -6, GETDATE()) AS date)
+      AND KdArti.ArtikelID = Artikel.ID
+  )
+FROM Artikel
+JOIN Bereich ON Artikel.BereichID = Bereich.ID
+JOIN ArtGru ON Artikel.ArtGruID = ArtGru.ID
+JOIN Me ON Artikel.MeID = Me.ID
+JOIN Abc ON Artikel.AbcID = Abc.ID
+JOIN (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = N'ARTIKEL'
+) AS Artikelstatus ON Artikel.[Status] = Artikelstatus.[Status]
+LEFT JOIN Mitarbei ON Artikel.AnlageUserID_ = Mitarbei.ID
+WHERE Artikel.BereichID IN ($1$)
+  AND Artikel.ArtGruID IN ($2$)
 ORDER BY Bereich, Artikelgruppe, Artikel.ArtikelNr;
-
-UPDATE ArtiList SET ArtiList.[Liefermenge letzte 6 Monate] = x.Liefermenge
-FROM #TmpArtiList AS ArtiList, (
-  SELECT KdArti.ArtikelID, SUM(LsPo.Menge) AS Liefermenge
-  FROM LsPo, LsKo, KdArti
-  WHERE LsPo.LsKoID = LsKo.ID
-    AND LsPo.KdArtiID = KdArti.ID
-    AND LsKo.Datum >= CONVERT(date, DATEADD(month, -6, GETDATE()))
-  GROUP BY KdArti.ArtikelID
-) AS x
-WHERE x.ArtikelID = ArtiList.ArtikelID;
-
-SELECT * FROM #TmpArtiList;
