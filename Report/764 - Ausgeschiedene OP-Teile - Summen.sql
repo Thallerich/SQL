9b8e5efe38785d7579b6a95f64f044aae5b36a6a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-01-21 09:34:58 +0100

    IT-90301  - get last vsa from scans

--- Report/764 - Ausgeschiedene OP-Teile - Summen.sql
+++ Report/764 - Ausgeschiedene OP-Teile - Summen.sql
@@ -1,11 +1,22 @@
 SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, WegGrund.WeggrundBez$LAN$ AS Schrottgrund, COUNT(EinzTeil.ID) AS [Anzahl Ausgeschieden]
-FROM EinzTeil
+FROM (
+	SELECT EinzTeil.ID, EinzTeil.WegGrundID, EinzTeil.ArtikelID, EinzTeil.WegDatum, VsaID = (
+		SELECT TOP 1 Scans.VsaID
+		FROM Scans
+		WHERE Scans.EinzTeilID = EinzTeil.ID
+			AND Scans.LsPoID > 0
+		ORDER BY Scans.ID DESC
+	)
+	FROM EinzTeil
+	WHERE EinzTeil.WegGrundID > 0
+		AND EinzTeil.[Status] = N'Z'
+		AND EinzTeil.LastActionsID IN (SELECT Actions.ID FROM Actions WHERE UPPER(Actions.ActionsBez) LIKE N'%SCHROTT%')
+		AND EinzTeil.WegDatum BETWEEN $STARTDATE$ AND $ENDDATE$
+) AS EinzTeil
 JOIN WegGrund ON EinzTeil.WegGrundID = WegGrund.ID
 JOIN Vsa ON EinzTeil.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
-WHERE EinzTeil.WegGrundID > 0
-	AND EinzTeil.WegDatum BETWEEN $STARTDATE$ AND $ENDDATE$
-	AND Vsa.StandKonID IN ($2$)
+WHERE Vsa.StandKonID IN ($2$)
 	AND Kunden.SichtbarID IN ($SICHTBARIDS$)
 GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, WegGrund.WegGrundBez$LAN$;
