09224a8c00820128d71a9b80424103a9f06c4a7a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-01-22 15:02:17 +0100

    IT-90402  - consolidated reports 923  - added new columns

--- Report/923 - Lagerbestand aktuell.sql
+++ Report/923 - Lagerbestand aktuell.sql
@@ -3,7 +3,8 @@ WITH Artikelstatus AS (
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'ARTIKEL')
 )
-SELECT Standort.Bez AS Lagerstandort, Lagerort.Lagerort, LagSchr.Bez AS Lagerschrank, Lagerart.LagerartBez$LAN$ AS Lagerart, LagerArt.Neuwertig, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikelstatus.StatusBez AS Artikelstatus, ArtGroe.Groesse, BestOrt.Bestand, BestOrt.Reserviert, BestOrt.BestandUrsprung AS [Bestand vom Ursprungsartikel]
+SELECT Standort.Bez AS Lagerstandort, Lagerort.Lagerort, LagSchr.Bez AS Lagerschrank, Lagerart.LagerartBez$LAN$ AS Lagerart, Lagerart.Zustand, LagerArt.Neuwertig, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Bereich.BereichBez as 'Bereich', ARTGRU.ArtGruBez as 'Artikelgruppe',Artikelstatus.StatusBez AS Artikelstatus, ArtGroe.Groesse, ABC.ABCBez as ABC, BestOrt.Bestand, BestOrt.Reserviert, BestOrt.BestandUrsprung AS [Bestand vom Ursprungsartikel],Bestand.Gleitpreis, Bestand.EntnahmeJahr
+, case when Artgroe.id > 0 then artgroe.ekpreis else artikel.ekpreis end as EkPreis
 FROM Lagerort
 JOIN Standort ON Lagerort.LagerID = Standort.ID
 JOIN LagSchr ON Lagerort.LagSchrID = LagSchr.ID
@@ -12,7 +13,10 @@ JOIN Bestand ON BestOrt.BestandID = Bestand.ID
 JOIN Lagerart ON Bestand.LagerartID = Lagerart.ID
 JOIN ArtGroe ON Bestand.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
+JOIN ABC ON ABC.id = Artikel.AbcID
 JOIN Artikelstatus ON Artikel.Status = Artikelstatus.Status
-WHERE Standort.ID = $1$
+JOIN Bereich on Bereich.id = Artikel.BereichID
+JOIN ARTGRU on ARTGRU.id = Artikel.ArtGruID
+WHERE Standort.ID IN ($1$)
   AND BestOrt.Bestand != 0
   AND (($2$ = 1 AND Lagerart.Neuwertig = 1) OR ($2$ = 0));
--- Report/923b - Lagerbestand aktuell ohne Lagerort.sql
+++ Report/923b - Lagerbestand aktuell ohne Lagerort.sql
@@ -2,14 +2,46 @@ WITH Artikelstatus AS (
   SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
   FROM [Status]
   WHERE [Status].Tabelle = UPPER(N'ARTIKEL')
+),
+Bestellt AS (
+  SELECT BKo.LagerArtID, BPo.ArtGroeID, SUM(BPo.Menge - BPo.LiefMenge) AS Bestellt
+  FROM BPo
+  JOIN BKo ON BPo.BKoID = BKo.ID
+  WHERE BKo.[Status] < N'M'
+  GROUP BY BKo.LagerArtID, BPo.ArtGroeID
+  HAVING SUM(BPo.Menge - BPo.LiefMenge) > 0
+),
+Entnahmen AS (
+  SELECT LagerBew.BestandID, COUNT(LagerBew.ID) AS AnzEntnahmen
+  FROM LagerBew
+  WHERE LagerBew.Zeitpunkt >= DATEADD(year, -1, CAST(GETDATE() AS date))
+    AND LagerBew.LgBewCodID IN (SELECT LgBewCod.ID FROM LgBewCod WHERE (LgBewCod.IstEntnahme = 1 OR LgBewCod.Code = N'UMZL'))
+  GROUP BY LagerBew.BestandID
 )
-SELECT Standort.Bez AS Lagerstandort, Lagerart.LagerartBez$LAN$ AS Lagerart, LagerArt.Neuwertig, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikelstatus.StatusBez AS Artikelstatus, ArtGroe.Groesse, Bestand.Bestand, Bestand.Reserviert, Bestand.BestandUrsprung AS [Bestand vom Ursprungsartikel], Bestand.Umlauf
+SELECT Standort.Bez AS Lagerstandort,
+  Lagerart.LagerartBez$LAN$ AS Lagerart,
+  LagerArt.Neuwertig,
+  Artikel.ArtikelNr,
+  Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung,
+  Artikelstatus.StatusBez AS Artikelstatus,
+  ArtGroe.Groesse,
+  Bestand.Bestand,
+  Bestand.Reserviert,
+  Bestand.BestandUrsprung AS [Bestand vom Ursprungsartikel],
+  Bestand.Umlauf,
+  ISNULL(Bestellt.Bestellt, 0) AS Bestellt,
+  Entnahmen.AnzEntnahmen AS [Entnahmen 12 Monate],
+  Bestand.GleitPreis AS GLD
 FROM Bestand
 JOIN Lagerart ON Bestand.LagerartID = Lagerart.ID
 JOIN Standort ON Lagerart.LagerID = Standort.ID
 JOIN ArtGroe ON Bestand.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
 JOIN Artikelstatus ON Artikel.Status = Artikelstatus.Status
-WHERE Standort.ID = $1$
+LEFT JOIN Bestellt ON Bestand.LagerArtID = Bestellt.LagerArtID AND Bestand.ArtGroeID = Bestellt.ArtGroeID
+LEFT JOIN Entnahmen ON Bestand.ID = Entnahmen.BestandID
+WHERE Standort.ID IN ($1$)
+  AND Artikel.BereichID IN ($2$)
   AND Bestand.Bestand != 0
-  AND (($2$ = 1 AND Lagerart.Neuwertig = 1) OR ($2$ = 0));
+  AND Lagerart.ArtiTypeID = 1
+  AND (($3$ = 1 AND Lagerart.Neuwertig = 1) OR ($3$ = 0));
