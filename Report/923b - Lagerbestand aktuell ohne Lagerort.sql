c1076f6e8047514e8fd42fda5b6a7f9fb84eec75
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-01-24 07:54:54 +0100

    IT-90402  - bugfix: Entnahmen richtig berechnen

--- Report/923b - Lagerbestand aktuell ohne Lagerort.sql
+++ Report/923b - Lagerbestand aktuell ohne Lagerort.sql
@@ -1,23 +1,3 @@
-WITH Artikelstatus AS (
-  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
-  FROM [Status]
-  WHERE [Status].Tabelle = UPPER(N'ARTIKEL')
-),
-Bestellt AS (
-  SELECT BKo.LagerArtID, BPo.ArtGroeID, SUM(BPo.Menge - BPo.LiefMenge) AS Bestellt
-  FROM BPo
-  JOIN BKo ON BPo.BKoID = BKo.ID
-  WHERE BKo.[Status] < N'M'
-  GROUP BY BKo.LagerArtID, BPo.ArtGroeID
-  HAVING SUM(BPo.Menge - BPo.LiefMenge) > 0
-),
-Entnahmen AS (
-  SELECT LagerBew.BestandID, COUNT(LagerBew.ID) AS AnzEntnahmen
-  FROM LagerBew
-  WHERE LagerBew.Zeitpunkt >= DATEADD(year, -1, CAST(GETDATE() AS date))
-    AND LagerBew.LgBewCodID IN (SELECT LgBewCod.ID FROM LgBewCod WHERE (LgBewCod.IstEntnahme = 1 OR LgBewCod.Code = N'UMZL'))
-  GROUP BY LagerBew.BestandID
-)
 SELECT Standort.Bez AS Lagerstandort,
   Lagerart.LagerartBez$LAN$ AS Lagerart,
   LagerArt.Neuwertig,
@@ -37,9 +17,26 @@ JOIN Lagerart ON Bestand.LagerartID = Lagerart.ID
 JOIN Standort ON Lagerart.LagerID = Standort.ID
 JOIN ArtGroe ON Bestand.ArtGroeID = ArtGroe.ID
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
-JOIN Artikelstatus ON Artikel.Status = Artikelstatus.Status
-LEFT JOIN Bestellt ON Bestand.LagerArtID = Bestellt.LagerArtID AND Bestand.ArtGroeID = Bestellt.ArtGroeID
-LEFT JOIN Entnahmen ON Bestand.ID = Entnahmen.BestandID
+JOIN (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'ARTIKEL')
+) AS Artikelstatus ON Artikel.Status = Artikelstatus.Status
+LEFT JOIN (
+  SELECT BKo.LagerArtID, BPo.ArtGroeID, SUM(BPo.Menge - BPo.LiefMenge) AS Bestellt
+  FROM BPo
+  JOIN BKo ON BPo.BKoID = BKo.ID
+  WHERE BKo.[Status] < N'M'
+  GROUP BY BKo.LagerArtID, BPo.ArtGroeID
+  HAVING SUM(BPo.Menge - BPo.LiefMenge) > 0
+) AS Bestellt ON Bestand.LagerArtID = Bestellt.LagerArtID AND Bestand.ArtGroeID = Bestellt.ArtGroeID
+LEFT JOIN (
+  SELECT LagerBew.BestandID, ABS(SUM(LagerBew.Differenz)) AS AnzEntnahmen
+  FROM LagerBew
+  WHERE LagerBew.Zeitpunkt >= DATEADD(year, -1, CAST(GETDATE() AS date))
+    AND LagerBew.LgBewCodID IN (SELECT LgBewCod.ID FROM LgBewCod WHERE (LgBewCod.IstEntnahme = 1 OR LgBewCod.Code = N'UMZL'))
+  GROUP BY LagerBew.BestandID
+) AS Entnahmen ON Bestand.ID = Entnahmen.BestandID
 WHERE Standort.ID IN ($1$)
   AND Artikel.BereichID IN ($2$)
   AND Bestand.Bestand != 0
