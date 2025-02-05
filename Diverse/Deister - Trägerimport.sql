4432a8d764ce5ec8d47dd1bb008ff353c51b0c6e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-02-05 07:52:28 +0100

    added output messages

--- Diverse/Deister - Trägerimport.sql
+++ Diverse/Deister - Trägerimport.sql
@@ -10,6 +10,10 @@
 /* ++ Column9 → Ausdienst nchar(8)                                                                                              ++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+GO
+
 DROP TABLE IF EXISTS #ImportTable;
 GO
 
@@ -31,10 +35,14 @@ CREATE TABLE #ImportTable (
 GO
 
 DECLARE @MaxTraegerNr int = (SELECT MAX(CAST(Traeger.Traeger AS int)) FROM Traeger WHERE Traeger.VsaID = 6119607);
+DECLARE @msg nvarchar(max);
+
+SET @msg = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss') + N' - Starte Vorbereitung...';
+RAISERROR(@msg, 0, 1) WITH NOWAIT;
 
 WITH DeisterBHSTraeger AS (
   SELECT LTRIM(RTRIM(PersNr)) AS PersNr, LTRIM(RTRIM(KartenNr)) AS KartenNr, KdNr, LTRIM(RTRIM(KsSt)) AS KsSt, CONVERT(date, Indienst, 112) AS Indienst, CONVERT(date, Ausdienst, 112) AS Ausdienst
-  FROM Salesianer.dbo.__DeisterBHS
+  FROM __DeisterBHS
   WHERE PersNr IS NOT NULL
 )
 INSERT INTO #ImportTable (VsaID, [Status], Traeger, AbteilID, PersNr, Vorname, Nachname, Indienst, IndienstDat, Ausdienst, AusdienstDat)
@@ -51,9 +59,13 @@ UPDATE #ImportTable SET TraegerID = Traeger.ID
 FROM Traeger
 WHERE Traeger.VsaID = #ImportTable.VsaID AND Traeger.PersNr = #ImportTable.PersNr;
 
+SET @msg = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss') + N' - Vorbereitung abgeschlossen. Beginne mit dem Import...';
+RAISERROR(@msg, 0, 1) WITH NOWAIT;
+
 GO
 
 DECLARE @userid int = (SELECT ID FROM Mitarbei WHERE UserName = N'THALST');
+DECLARE @msg nvarchar(max);
 
 BEGIN TRY
   BEGIN TRANSACTION;
@@ -64,11 +76,17 @@ BEGIN TRY
       AND #ImportTable.TraegerID IS NOT NULL
       AND (#ImportTable.Status != Traeger.Status OR #ImportTable.Indienst != Traeger.Indienst OR #ImportTable.IndienstDat != Traeger.IndienstDat OR #ImportTable.Ausdienst != Traeger.Ausdienst OR #ImportTable.AusdienstDat != Traeger.AusdienstDat OR #ImportTable.AbteilID != Traeger.AbteilID);
 
+    SET @msg = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss') + N' - ' + CAST(@@ROWCOUNT AS nvarchar) + N' Träger aktualisiert.';
+    RAISERROR(@msg, 0, 1) WITH NOWAIT;
+
     INSERT INTO Traeger (VsaID, [Status], Traeger, AbteilID, PersNr, Vorname, Nachname, Indienst, IndienstDat, Ausdienst, AusdienstDat, AnlageUserID_, UserID_)
     SELECT VsaID, [Status], Traeger, AbteilID, PersNr, Vorname, Nachname, Indienst, IndienstDat, Ausdienst, AusdienstDat, @userid, @userid
     FROM #ImportTable
     WHERE TraegerID IS NULL;
   
+    SET @msg = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss') + N' - ' + CAST(@@ROWCOUNT AS nvarchar) + N' Träger neu angelegt.';
+    RAISERROR(@msg, 0, 1) WITH NOWAIT;
+
   COMMIT;
 END TRY
 BEGIN CATCH
@@ -81,6 +99,9 @@ BEGIN CATCH
   
   RAISERROR(@Message, @Severity, @State) WITH NOWAIT;
 END CATCH;
+
+SET @msg = FORMAT(GETDATE(), N'yyyy-MM-dd HH:mm:ss') + N' - Import abgeschlossen.';
+RAISERROR(@msg, 0, 1) WITH NOWAIT;
 
 GO
 
