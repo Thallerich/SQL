d12fa42b7e26d4dd90ad70d25468150071f27d64
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-02-14 13:16:48 +0100

    IT-91211  - added "Basisrestwert"  - optimized parameter usage

--- Report/404 - Preisliste.sql
+++ Report/404 - Preisliste.sql
@@ -1,9 +1,4 @@
-WITH KdArtiStatus AS (
-  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
-  FROM [Status]
-  WHERE [Status].Tabelle = UPPER(N'KdArti')
-)
-SELECT Holding.Holding, Holding.Bez AS Holdingbezeichnung, Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS [Varianten-Bezeichnung], Bereich.Bereich AS Produktbereich, KdArti.WaschPreis AS [Bearbeitungs-Preis], LeasPreisWoche.LeasPreisProWo AS [Leasing-Preis], FakFreq.FakFreqBez AS Fakturafrequenz, KdArti.KundenID, KdArti.ID AS KdArtiID
+SELECT Holding.Holding, Holding.Bez AS Holdingbezeichnung, Kunden.KdNr, Kunden.SuchCode AS Kunde, Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, KdArti.Variante, KdArti.VariantBez AS [Varianten-Bezeichnung], Bereich.Bereich AS Produktbereich, KdArti.WaschPreis AS [Bearbeitungs-Preis], LeasPreisWoche.LeasPreisProWo AS [Leasing-Preis], KdArti.BasisRestwert, KdArti.AfaWochen, FakFreq.FakFreqBez AS Fakturafrequenz, KdArti.KundenID, KdArti.ID AS KdArtiID
 FROM KdArti
 CROSS APPLY dbo.advFunc_GetLeasPreisProWo(KdArti.ID) AS LeasPreisWoche
 JOIN Kunden ON KdArti.KundenID = Kunden.ID
@@ -12,8 +7,17 @@ JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
 JOIN KdBer ON KdArti.KdBerID = KdBer.ID
 JOIN Bereich ON KdBer.BereichID = Bereich.ID
 JOIN FakFreq ON KdBer.FakFreqID = FakFreq.ID
-JOIN KdArtiStatus ON KdArti.[Status] = KdArtiStatus.[Status]
-WHERE KdArti.KundenID IN ($2$)
-  AND KdArtiStatus.ID IN ($3$)
+JOIN (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'KdArti')
+) AS KdArtiStatus ON KdArti.[Status] = KdArtiStatus.[Status]
+JOIN (
+  SELECT [Status].ID, [Status].[Status], [Status].StatusBez$LAN$ AS StatusBez
+  FROM [Status]
+  WHERE [Status].Tabelle = UPPER(N'KUNDEN')
+) AS Kundenstatus ON Kundenstatus.[Status] = Kunden.[Status]
+WHERE Kunden.ID IN ($3$)
+  AND KdArtiStatus.ID IN ($4$)
   AND KdArti._HidePrListReport = 0
 ORDER BY Holding, KdNr, Artikelbezeichnung;
