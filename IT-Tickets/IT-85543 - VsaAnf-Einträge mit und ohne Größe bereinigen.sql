de1886550981b80af330ec4fc4c5cf1899a839b8
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-02-17 07:16:31 +0100

    added logic for anfpo

--- IT-Tickets/IT-85543 - VsaAnf-Einträge mit und ohne Größe bereinigen.sql
+++ IT-Tickets/IT-85543 - VsaAnf-Einträge mit und ohne Größe bereinigen.sql
@@ -1,16 +1,25 @@
 DROP TABLE IF EXISTS #VsaAnf;
+DROP TABLE IF EXISTS #AnfPo;
 GO
 
 SELECT VsaAnf.ID, VsaAnf.VsaID, VsaAnf.KdArtiID, VsaAnf.ArtGroeID, Bereich.Bereich, Bereich.VsaAnfGroe
 INTO #VsaAnf
 FROM VsaAnf
-INNER JOIN KdArti ON VsaAnf.KdArtiID = KdArti.ID AND KdArti.Status = 'A'
-INNER JOIN KdBer ON KdArti.KdBerID = KdBer.ID AND KdBer.Status = 'A'
-INNER JOIN Bereich ON KdBer.BereichID = Bereich.ID AND Bereich.VsaAnfGroe = 0
-INNER JOIN Kunden ON KDARTI.KundenID = Kunden.ID AND Kunden.Status = 'A'
-INNER JOIN Vsa ON VSAANF.VsaID = Vsa.ID AND Vsa.Status = 'A'
+JOIN KdArti ON VsaAnf.KdArtiID = KdArti.ID AND KdArti.Status = 'A'
+JOIN KdBer ON KdArti.KdBerID = KdBer.ID AND KdBer.Status = 'A'
+JOIN Bereich ON KdBer.BereichID = Bereich.ID AND Bereich.VsaAnfGroe = 0
+JOIN Kunden ON KdArti.KundenID = Kunden.ID AND Kunden.Status = 'A'
+JOIN Vsa ON VsaAnf.VsaID = Vsa.ID AND Vsa.Status = 'A'
 WHERE VsaAnf.ArtGroeID > -1
 AND VsaAnf.Status < 'I';
+
+SELECT AnfPo.ID, AnfPo.KdArtiID, AnfPo.ArtGroeID
+INTO #AnfPo
+FROM AnfPo
+JOIN AnfKo ON AnfPo.AnfKoID = AnfKo.ID
+JOIN #VsaAnf ON AnfPo.KdArtiID = #VsaAnf.KdArtiID AND AnfPo.ArtGroeID = #VsaAnf.ArtGroeID
+WHERE AnfKo.Lieferdatum >= CAST(GETDATE() AS date)
+  AND AnfKo.LsKoID < 0;
 
 BEGIN TRY
   BEGIN TRANSACTION;
@@ -40,6 +49,19 @@ BEGIN TRY
           AND VsaAnf.ID != #VsaAnf.ID
       )
     );
+
+    UPDATE AnfPo SET ArtGroeID = -1
+    WHERE ID IN (
+        SELECT #AnfPo.ID
+        FROM #AnfPo
+      )
+      AND NOT EXISTS (
+        SELECT a.*
+        FROM AnfPo AS a
+        WHERE a.KdArtiID = AnfPo.KdArtiID
+          AND a.AnfKoID = AnfPo.AnfKoID
+          AND a.ArtGroeID = -1
+      );
   
   COMMIT;
 END TRY
