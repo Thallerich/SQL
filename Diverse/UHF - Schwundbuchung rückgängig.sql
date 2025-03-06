c620db49aaff2ac49c1cea1ef9cd8ecba18df576
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-03-06 15:36:43 +0100

    optimization  - set user on updates  - saftey net for no rollback einzhist

--- Diverse/UHF - Schwundbuchung rückgängig.sql
+++ Diverse/UHF - Schwundbuchung rückgängig.sql
@@ -17,9 +17,9 @@ GO
 /* ++ Parameter-Definition - hier Werte anpassen!                                                                               ++ */
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
-DECLARE @kdnr int = 5154;
-DECLARE @user nchar(6) = N'LAMABE';
-DECLARE @schwundtime datetime2 = N'2023-04-01 06:00:00';
+DECLARE @kdnr int = 20000;
+DECLARE @user nchar(6) = N'ANDRSA';
+DECLARE @schwundtime datetime2 = N'2025-03-06 10:00:00';
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* ++                                                                                                                           ++ */
@@ -62,6 +62,8 @@ WHERE x.EinzTeilID = #SchwundRetour.EinzTeilID;
 
 GO
 
+DECLARE @userid int = (SELECT ID FROM Mitarbei WHERE UserName = UPPER(REPLACE(USER_NAME(), N'SAL\', N'')));
+
 BEGIN TRY
   BEGIN TRANSACTION;
   
@@ -69,16 +71,17 @@ BEGIN TRY
     DELETE FROM Scans WHERE ID IN (SELECT ScanID FROM #SchwundRetour);
     
     /* Restore EinzTeil */
-    UPDATE EinzTeil SET [Status] = N'Q', CurrEinzHistID = #SchwundRetour.RollbackEinzHistID, ZielNrID = #SchwundRetour.ZielNrID, LastActionsID = #SchwundRetour.LastActionsID, LastScanTime = #SchwundRetour.LastScanTime
+    UPDATE EinzTeil SET [Status] = N'Q', CurrEinzHistID = #SchwundRetour.RollbackEinzHistID, ZielNrID = #SchwundRetour.ZielNrID, LastActionsID = #SchwundRetour.LastActionsID, LastScanTime = #SchwundRetour.LastScanTime, UserID_ = @userid
     FROM #SchwundRetour
     WHERE #SchwundRetour.EinzTeilID = EinzTeil.ID
+      AND #SchwundRetour.RollbackEinzHistID IS NOT NULL;
 
     /* Delete Schwund-EinzHist */
-    DELETE FROM EinzHist WHERE ID IN (SELECT SchwundEinzHistID FROM #SchwundRetour);
+    DELETE FROM EinzHist WHERE ID IN (SELECT SchwundEinzHistID FROM #SchwundRetour WHERE SchwundEinzHistID IS NOT NULL) AND NOT EXISTS (SELECT 1 FROM EinzTeil WHERE EinzTeil.CurrEinzHistID = EinzHist.ID);
     
     /* Restore Rollback EinzHist */
-    UPDATE EinzHist SET EinzHistBis = N'2099-12-31 23:59:59.000'
-    WHERE ID IN (SELECT RollbackEinzHistID FROM #SchwundRetour);
+    UPDATE EinzHist SET EinzHistBis = N'2099-12-31 23:59:59.000', UserID_ = @userid
+    WHERE ID IN (SELECT RollbackEinzHistID FROM #SchwundRetour WHERE RollbackEinzHistID IS NOT NULL);
 
   COMMIT;
 END TRY
