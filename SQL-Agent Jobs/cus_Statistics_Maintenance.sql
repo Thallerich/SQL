430b80e6b54dd18ec7f1244e35d6484efc6def1b
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-03-12 08:27:25 +0100

    statistics maintenance job TSA

---
+++ SQL-Agent Jobs/cus_Statistics_Maintenance.sql
@@ -0,0 +1,30 @@
+DECLARE @role tinyint;
+
+SET @role = (
+  SELECT [role]
+  FROM [sys].[dm_hadr_availability_replica_states] AS hars 
+  JOIN [sys].[availability_databases_cluster] AS adc ON hars.[group_id] = adc.[group_id]
+  WHERE hars.[is_local] = 1
+    AND adc.[database_name] = N'Salesianer'
+);
+
+IF @role = 1 
+BEGIN
+
+  EXEC Salesianer_Archive.dbo.IndexOptimize
+    @Databases = N'Salesianer',
+    @Indexes = N'ALL_INDEXES, -Salesianer.dbo.SCANS, -Salesianer.dbo.TRAEARCH',
+    @FragmentationLow = NULL,
+    @FragmentationMedium = N'INDEX_REORGANIZE,INDEX_REBUILD_ONLINE',
+    @FragmentationHigh = N'INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE',
+    @FragmentationLevel1 = 50,
+    @FragmentationLevel2 = 80,
+    @UpdateStatistics = N'ALL',
+    @OnlyModifiedStatistics = N'Y',
+    @StatisticsSample = 100,
+    @StatisticsPersistSample = N'Y',
+    @WaitAtLowPriorityMaxDuration = 300,
+    @WaitAtLowPriorityAbortAfterWait = N'SELF',
+    @LogToTable = N'Y';
+
+END;
