ec50b18954a61bfa29c47a69a8f2cec823dc910e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-03-12 10:14:10 +0100

    ABS is no longer available

--- ABS/Error-Check Bestellung.sql
+++
@@ -1,6 +0,0 @@
-select *
-from abslog a
-where a.systemuser_id = 261
-and a.timestamp >= sysdate-7
-and a.message like '%Error%'
-order by a.timestamp desc
--- ABS/Workorder Message Log.sql
+++
@@ -1,5 +0,0 @@
-select a.message, a.timestamp
-from abslog a
-where a.logsource = 'pack_itm' --wo Schnittstelle
-and a.timestamp >= sysdate - 1  --Einträge der letzten Woche
-ORDER BY a.timestamp DESC;
