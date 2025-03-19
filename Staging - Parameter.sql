e0092f43bb4f32eb7c9c1ac5cc7f002b101b6202
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-03-19 10:02:11 +0100

    for new staging layout

--- Staging - Parameter.sql
+++ Staging - Parameter.sql
@@ -48,31 +48,31 @@ BEGIN
       WHERE [Parameter] = N'INTERNET_ROOT_DIR';
 
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\WebDB_Test\'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\WebDB_Test\'
       WHERE [Parameter] = N'WEB_EXPORT_UPLOAD_PATH';
 
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\Logos\Salesianer_LogoReleasetest.bmp'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\Logos\Salesianer_LogoReleasetest.bmp'
       WHERE [Parameter] = N'LOGO1_PATH_UND_DATEINAME';
 
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\UHFInventur\Testmandant\'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\UHFInventur\Testmandant\'
       WHERE [Parameter] = N'INVENTUR_UHF2_PATH';
       
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\UHFInventur\Testmandant\Archiv\'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\UHFInventur\Testmandant\Archiv\'
       WHERE [Parameter] = N'INVENTUR_UHF2_BACKUP_PATH';
       
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\SALADVTAPP1.salres.com\advantex\Data\Temp\'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\advantex\Data\Temp\'
       WHERE [Parameter] = N'PDF_SPOOL_PATH';
       
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\Export\Inventurliste\Inventurliste.csv'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\Export\Inventurliste\Inventurliste.csv'
       WHERE [Parameter] = N'CSV_FILENAME_INVENTURIMPORT';
       
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\SALADVTAPP1.salres.com\advantex\Data\Temp\'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\advantex\Data\Temp\'
       WHERE [Parameter] = N'REPORT_EXPORT_PATH';
       
     UPDATE Salesianer.dbo.Settings
@@ -88,11 +88,11 @@ BEGIN
       WHERE [Parameter] = N'URL_WS_COUNTIT_UNCLEANSIDE';
       
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\EDI\EDI_Test\'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\EDI\EDI_Test\'
       WHERE [Parameter] = N'PATH_EOFFICE';
     
     UPDATE Salesianer.dbo.Settings
-      SET [ValueMemo] = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\EDI\BMD_Test\'
+      SET [ValueMemo] = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\EDI\BMD_Test\'
       WHERE [Parameter] = N'PATH_BMD';
 
     UPDATE Salesianer.dbo.Settings
@@ -128,43 +128,43 @@ BEGIN
       WHERE Parameter = N'ABS_USERNAME';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\Dms\Document\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\Dms\Document\'
       WHERE Parameter = N'PATH_DMS_DOCUMENT';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\Dms\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\Dms\'
       WHERE Parameter = N'PATH_DMS_MASTERS';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\email\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\email\'
       WHERE Parameter = N'TEMP_ANHANG_PATH';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\Regina\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\Regina\'
       WHERE Parameter = N'EXPORT_REGINA';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\Temp\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\Temp\'
       WHERE Parameter = N'PATH_EXCEL';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\easybackup\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\easybackup\'
       WHERE Parameter = N'INVENTUR_FTP_BACKUP_PATH';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\PflegeBackUp\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\PflegeBackUp\'
       WHERE Parameter = N'OPETIBCIMPORT_BACKUP_PATH';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\Rechnungsarchiv\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\Rechnungsarchiv\'
       WHERE Parameter = N'PATH_RECHARCH';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\Mandant\Salesianer\'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\Mandant\Salesianer\'
       WHERE Parameter = N'DATA_PATH_MANDANT';
 
     UPDATE Salesianer.dbo.Rentomat 
-      SET ExportFile1 = N'\\salshdsvm11_682.salres.com\advsapp_file\DCS\Test\'
+      SET ExportFile1 = N'\\SALADVSAPP1.salres.com\DCS\Test\'
       WHERE Rentomat.Interface <> 'Unimat';
 
     UPDATE Salesianer.dbo.Rentomat 
@@ -176,23 +176,19 @@ BEGIN
       WHERE Rentomat.Interface = N'Unimat';
       
     UPDATE Salesianer.dbo.ExpDef
-      SET ExportFileName = REPLACE(ExpDef.ExportFileName, N'salshdsvm09_681.salres.com', N'salshdsvm11_682.salres.com')
+      SET ExportFileName = REPLACE(ExpDef.ExportFileName, N'salshdsvm09_681.salres.com\advpapp_file\', N'\\SALADVSAPP1.salres.com\')
       WHERE ExportFileName LIKE N'\\salshdsvm09_681.salres.com\advpapp_file\%'
 
     UPDATE Salesianer.dbo.ExpDef
-      SET ExportFileName = REPLACE(ExpDef.ExportFileName, N'\scp\', N'\scq\')
-      WHERE ExportFileName LIKE N'\\tsafile1.sal.co.at%';
-
-    UPDATE Salesianer.dbo.ExpDef
-      SET BackupFolder = REPLACE(ExpDef.BackupFolder, N'salshdsvm09_681.salres.com', N'salshdsvm11_682.salres.com')
-      WHERE BackupFolder LIKE N'%salshdsvm09_681.salres.com%';
+      SET BackupFolder = REPLACE(ExpDef.ExportFileName, N'salshdsvm09_681.salres.com\advpapp_file\', N'\\SALADVSAPP1.salres.com\')
+      WHERE BackupFolder LIKE N'%salshdsvm09_681.salres.com\advpapp_file\%';
 
     UPDATE Salesianer.dbo.RKoOut
-      SET ArchivePath = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Export\Rechnungen_Testmandant\'
+      SET ArchivePath = N'\\SALADVSAPP1.salres.com\AdvanTex\Export\Rechnungen_Testmandant\'
       WHERE ArchivePath IS NOT NULL;
 
     UPDATE Salesianer.dbo.RKoOut
-      SET VersandPath = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Export\Rechnungen_Testmandant\'
+      SET VersandPath = N'\\SALADVSAPP1.salres.com\AdvanTex\Export\Rechnungen_Testmandant\'
       WHERE VersandPath IS NOT NULL;
 
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
@@ -220,11 +216,11 @@ BEGIN
       WHERE Parameter = N'LIVE_URL';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\SSL\Salesianer_Wildcard_aktuell.crt'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\SSL\Salesianer_Wildcard_aktuell.crt'
       WHERE Parameter = N'LIVE_SSL_CERT_FILE';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\SSL\Salesianer_Wildcard_aktuell.key'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\SSL\Salesianer_Wildcard_aktuell.key'
       WHERE Parameter = N'LIVE_SSL_KEY_FILE';
 
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
@@ -232,11 +228,11 @@ BEGIN
     /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\SSL\Salesianer_Wildcard_aktuell.crt'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\SSL\Salesianer_Wildcard_aktuell.crt'
       WHERE Parameter = N'MOBSYNC_SSL_CERTIFICATE';
 
     UPDATE Salesianer.dbo.Settings
-      SET ValueMemo = N'\\salshdsvm11_682.salres.com\advsapp_file\AdvanTex\Data\SSL\Salesianer_Wildcard_aktuell.key'
+      SET ValueMemo = N'\\SALADVSAPP1.salres.com\AdvanTex\Data\SSL\Salesianer_Wildcard_aktuell.key'
       WHERE Parameter = N'MOBSYNC_SSL_KEY';
 
   COMMIT;
