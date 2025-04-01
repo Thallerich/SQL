c28d453aba7cc9fac2fbea971e3fafb21a569b2c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-04-01 07:45:30 +0200

    dynamic dates and only specific bks

--- Diverse/Bekleidungsausgabesystem - Import-Job Dauer je Automat.sql
+++ Diverse/Bekleidungsausgabesystem - Import-Job Dauer je Automat.sql
@@ -5,9 +5,11 @@ JOIN LsPo ON Scans.LsPoID = LsPo.ID
 JOIN LsKo ON LsPo.LsKoID = LsKo.ID
 JOIN Vsa ON EinzHist.VsaID = Vsa.ID
 JOIN Rentomat ON Vsa.RentomatID = Rentomat.ID
-WHERE Scans.[DateTime] > N'2025-04-01 02:00:00.000'
+WHERE Scans.[DateTime] >= DATEADD(day, DATEDIFF(day, 1, GETDATE()), 0)
+  AND Scans.Anlage_ >= DATETIME2FROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), DAY(GETDATE()), 2, 0, 0, 0, 0)
   AND Scans.AnlageUserID_ IN (SELECT ID FROM Mitarbei WHERE MitarbeiUser IN (N'JOBLOG', N'JOB'))
   AND Scans.ActionsID = 65
+  AND Rentomat.ID IN (35, 36, 37, 38, 39, 40, 41, 47, 57, 59, 61, 62, 64, 65, 66, 70, 71, 72, 73, 75, 82, 83, 84, 85, 90, 97, 98, 100, 102)
 GROUP BY Rentomat.ID, Rentomat.Bez, LsKo.Datum
 ORDER BY [Erster Scan] ASC;
 
