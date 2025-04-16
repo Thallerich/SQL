74869182268bf0a2a2afc4c04f3c6c27376ada1c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-04-16 09:23:54 +0200

    added 4 new articles

--- Export-Definition/L2G - Teile.sql
+++ Export-Definition/L2G - Teile.sql
@@ -2,4 +2,4 @@ SELECT EinzTeil.Code AS EPC, Artikel.ArtikelNr AS ARTIKELNR, EinzTeil.Anlage_ AS
 FROM EinzTeil
 JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
 JOIN Mitarbei ON EinzTeil.AnlageUserID_ = Mitarbei.ID
-WHERE Artikel.ArtikelNr LIKE 'L2G%';
+WHERE (Artikel.ArtikelNr LIKE 'L2G%' OR Artikel.ArtikelNr IN ('G10100', 'G20100', 'G30114', 'G30118'));
