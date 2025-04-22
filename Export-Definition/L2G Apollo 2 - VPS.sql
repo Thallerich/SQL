e83f53b003738e0c4705d96e6a4c53168e29f4a1
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-04-22 15:16:29 +0200

    new truck Apollo 2

---
+++ Export-Definition/L2G Apollo 2 - VPS.sql
@@ -0,0 +1,10 @@
+SELECT VPSKo.ID AS STACKID, EinzTeil.Code AS EPC, VPSKo.Anlage_ AS [DATE]
+FROM Scans
+JOIN EinzTeil ON Scans.EinzTeilID = EinzTeil.ID
+JOIN VPSPo ON Scans.VPSPoID = VPSPo.ID
+JOIN VPSKo ON VPSPo.VPSKoID = VPSKo.ID
+JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
+JOIN Mitarbei ON VPSKo.AnlageUserID_ = Mitarbei.ID
+WHERE Artikel.ArtikelNr LIKE N'L2G%'
+  AND VPSKo.Anlage_ >= DATEADD(minute, -180, GETDATE())
+  AND Mitarbei.StandortID = (SELECT ID FROM Standort WHERE SuchCode = N'LEOG');
