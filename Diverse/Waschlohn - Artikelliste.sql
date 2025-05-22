5b46026b455f45673fe2b339e581ab9ea9fcb9ed
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-05-22 15:43:26 +0200

    fixed calculation of leasing

--- Diverse/Waschlohn - Artikelliste.sql
+++ Diverse/Waschlohn - Artikelliste.sql
@@ -16,9 +16,10 @@ WHERE Wochen.Monat1 BETWEEN N'2024-04' AND N'2025-03'
   AND RechKo.[Status] < N'X'
   AND RechKo.[Status] >= N'N'
   AND Kunden.FirmaID = (SELECT Firma.ID FROM Firma WHERE Firma.SuchCode = N'FA14')
+  AND AbtKdArW.RechPoID > 0
 GROUP BY KdArti.ArtikelID, StandBer.ProduktionID;
 
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, Bereich.BereichBez AS Produktbereich, ArtGru.ArtGruBez AS Artikelgruppe, CAST(IIF(UPPER(Artikel.ArtikelBez) LIKE '%HIVIS%', 1, 0) AS bit) AS HIVIS, CAST(IIF(EXISTS(SELECT Normen.* FROM ArtiNorm JOIN Normen ON ArtiNorm.NormenID = Normen.ID WHERE ArtiNorm.ArtikelID = Artikel.ID AND UPPER(Normen.NormenBez) LIKE '"PSA"%'), 1, 0) AS bit) AS PSA, Standort.Bez AS Produktion, SUM(LsPo.Menge) AS Liefermenge, SUM(LsPo.Menge * LsPo.EPreis) AS [Umsatz Bearbeitung netto], SUM(ISNULL(#LeasingUmsatz.Umsatz, 0)) AS [Umsatz Leasing netto], SUM(LsPo.InternKalkPreis * LsPo.Menge) AS [bezahlter Waschlohn]
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, Artikel.SuchCode2 AS [Artikelbezeichnung 2], Bereich.BereichBez AS Produktbereich, ArtGru.ArtGruBez AS Artikelgruppe, CAST(IIF(UPPER(Artikel.ArtikelBez) LIKE '%HIVIS%', 1, 0) AS bit) AS HIVIS, CAST(IIF(EXISTS(SELECT Normen.* FROM ArtiNorm JOIN Normen ON ArtiNorm.NormenID = Normen.ID WHERE ArtiNorm.ArtikelID = Artikel.ID AND UPPER(Normen.NormenBez) LIKE '"PSA"%'), 1, 0) AS bit) AS PSA, Standort.Bez AS Produktion, SUM(LsPo.Menge) AS Liefermenge, SUM(IIF(LsPo.RechPoID > 0, LsPo.Menge * LsPo.EPreis, 0)) AS [Umsatz Bearbeitung netto], ISNULL(#LeasingUmsatz.Umsatz, 0) AS [Umsatz Leasing netto], SUM(LsPo.InternKalkPreis * LsPo.Menge) AS [bezahlter Waschlohn]
 FROM LsPo
 JOIN LsKo ON LsPo.LsKoID = LsKo.ID
 JOIN KdArti ON LsPo.KdArtiID = KdArti.ID
@@ -33,6 +34,6 @@ WHERE LsKo.Datum >= N'2024-04-01'
   AND LsKo.InternKalkFix = 1
   AND LsKo.SentToSAP = 1
   AND Kunden.FirmaID = (SELECT Firma.ID FROM Firma WHERE Firma.SuchCode = N'FA14')
-GROUP BY Artikel.ID, Artikel.ArtikelNr, Artikel.ArtikelBez, Bereich.BereichBez, ArtGru.ArtGruBez, CAST(IIF(UPPER(Artikel.ArtikelBez) LIKE '%HIVIS%', 1, 0) AS bit), Standort.Bez;
+GROUP BY Artikel.ID, Artikel.ArtikelNr, Artikel.ArtikelBez, Artikel.SuchCode2, Bereich.BereichBez, ArtGru.ArtGruBez, CAST(IIF(UPPER(Artikel.ArtikelBez) LIKE '%HIVIS%', 1, 0) AS bit), Standort.Bez, ISNULL(#LeasingUmsatz.Umsatz, 0);
 
 GO
