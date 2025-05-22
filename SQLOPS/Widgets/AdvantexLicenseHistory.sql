1efd69fd8feef243edb5f054cdf7be713d30403e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-05-22 10:09:44 +0200

    10.10: new filter by type needed

--- SQLOPS/Widgets/AdvantexLicenseHistory.sql
+++ SQLOPS/Widgets/AdvantexLicenseHistory.sql
@@ -1,5 +1,6 @@
 SELECT LoginUse.Datum, LoginUse.AnzSessions
 FROM LoginUse
 WHERE LoginUse.Datum BETWEEN CAST(DATEADD(day, -30, GETDATE()) AS date) AND CAST(GETDATE() AS date)
+  AND LoginUse.Type = 0
   /* AND DATEPART(weekday, LoginUse.Datum) NOT IN (7, 1) -- Samstag / Sonntag */
 ORDER BY Datum ASC;
