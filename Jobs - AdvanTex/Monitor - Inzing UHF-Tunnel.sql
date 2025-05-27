73ed09d288fab005234d4c4649d2b467901560cf
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-05-27 10:06:17 +0200

    initial version

---
+++ Jobs - AdvanTex/Monitor - Inzing UHF-Tunnel.sql
@@ -0,0 +1,18 @@
+SELECT Ort,
+  [Anzahl mit Rückmeldung] = SUM(IIF(Rückmeldung = 1, 1, 0)),
+  [Anzahl ohne Rückmeldung] = SUM(IIF(Rückmeldung = 0, 1, 0))
+FROM (
+  SELECT SUBSTRING(SdcTcpL.Message60, 12, 4) AS Ort,
+    [Rückmeldung] = CAST(IIF(EXISTS(
+      SELECT 1
+      FROM [SVATINZSQL1.sal.co.at].Salesianer_Inzing.dbo.SdcTcpL AS SdcTcpL2
+      WHERE SdcTcpL2.TransNr = N'609'
+        AND SdcTcpL2.Stamp BETWEEN SdcTcpL.Stamp AND DATEADD(minute, 1, SdcTcpL.Stamp)
+        AND SdcTcpL2.Chipcode = SdcTcpL.Chipcode
+    ), 1, 0) AS BIT)
+  FROM [SVATINZSQL1.sal.co.at].Salesianer_Inzing.dbo.SdcTcpL
+  WHERE SdcTcpL.TransNr = N'608'
+    AND SdcTcpL.Stamp > DATEADD(minute, -10, GETDATE())
+) AS x
+GROUP BY Ort
+HAVING SUM(IIF(Rückmeldung = 0, 1, 0)) >= 10;
