6a9454d231eb2d73cc32486d78159577e58fd5ee
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-06-03 08:03:40 +0200

    bugfixes

--- IT-Tickets/IT-94573 - Userauswertung.sql
+++ IT-Tickets/IT-94573 - Userauswertung.sql
@@ -4,7 +4,7 @@ SELECT Mitarbei.ID AS MitarbeiID,
   Mitarbei.Vorname,
   Mitarbei.Nachname,
   Mitarbei.LastLogin,
-  [deaktivieren] = CAST(IIF((Mitarbei.LastLogin < N'2025-01-01 00:00:00' OR Mitarbei.LastLogin IS NULL), 1, 0) AS bit),
+  [deaktivieren] = CAST(IIF((Mitarbei.LastLogin < N'2025-01-01 00:00:00' OR Mitarbei.LastLogin IS NULL) AND NOT EXISTS (SELECT UsrInGrp.* FROM UsrInGrp WHERE UsrInGrp.MitarbeiID = Mitarbei.ID AND UsrInGrp.UserGrpID IN (SELECT UserGrp.ID FROM UserGrp WHERE UserGrp.UserGrpBez IN (N'SAL_SDC', N'SAL_ADVSUP_only'))), 1, 0) AS bit),
   [kein Standort] = CAST(IIF(Mitarbei.StandortID < 0, 1, 0) AS bit),
   [keine Firma] = CAST(IIF(Mitarbei.FirmaID < 0, 1, 0) AS bit)
 FROM Mitarbei
--- Rechnungsanlagen/Dienstleistungsübersicht_neu.sql
+++ Rechnungsanlagen/Dienstleistungsübersicht_neu.sql
@@ -1,6 +1,8 @@
-SELECT IIF(ArtGru.Gruppe IN (N'IK1', N'IK2', N'SH12', N'SH13', N'DRY'), N'Inko-Versorgung', RechPo.Bez) AS Positionsbezeichnung, RechKo.RechNr, RechKo.Debitor, RechKo.EffektivBis, RechKo.RechDat, SUM(RechPo.GPreis) AS Positionssumme
+SELECT IIF(ArtGru.Gruppe IN (N'IK1', N'IK2', N'SH12', N'SH13', N'DRY'), N'Inko-Versorgung', IIF(RPoType.StatistikGruppe = N'Restwerte', RechPo.Bez, RPoType.RPoTypeBez$LAN$ + N' ' + Bereich.BereichBez$LAN$)) AS Positionsbezeichnung, RechKo.RechNr, RechKo.Debitor, RechKo.EffektivBis, RechKo.RechDat, SUM(RechPo.GPreis) AS Positionssumme
 FROM RechPo
 JOIN RechKo ON RechPo.RechKoID = RechKo.ID
+JOIN Bereich ON RechPo.BereichID = Bereich.ID
+JOIN RPoType ON RechPo.RPoTypeID = RPoType.ID
 LEFT OUTER JOIN ArtGru ON RechPo.ArtGruID = ArtGru.ID
 WHERE RechKo.ID = $RECHKOID$
-GROUP BY IIF(ArtGru.Gruppe IN (N'IK1', N'IK2', N'SH12', N'SH13', N'DRY'), N'Inko-Versorgung', RechPo.Bez), RechKo.RechNr, RechKo.Debitor, RechKo.EffektivBis, RechKo.RechDat;
+GROUP BY IIF(ArtGru.Gruppe IN (N'IK1', N'IK2', N'SH12', N'SH13', N'DRY'), N'Inko-Versorgung', IIF(RPoType.StatistikGruppe = N'Restwerte', RechPo.Bez, RPoType.RPoTypeBez$LAN$ + N' ' + Bereich.BereichBez$LAN$)), RechKo.RechNr, RechKo.Debitor, RechKo.EffektivBis, RechKo.RechDat;
