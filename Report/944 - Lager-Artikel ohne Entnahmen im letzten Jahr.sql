4adbdbde6aff573d7478c97cd92fdcefb5bea95a
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-06-05 10:26:58 +0200

    IT-94929  - bugfix: do not join lagerbew, leads to multiples of stock amount

--- Report/944 - Lager-Artikel ohne Entnahmen im letzten Jahr.sql
+++ Report/944 - Lager-Artikel ohne Entnahmen im letzten Jahr.sql
@@ -1,19 +1,17 @@
-WITH Lagerbestand AS (
-  SELECT BestandRaw.ArtGroeID, BestandRaw.LagerID, MAX(IIF(BestandRaw.Neuwertig = 1, BestandRaw.Lagerbestand, 0)) AS BestandNeuware, MAX(IIF(BestandRaw.Neuwertig = 0, BestandRaw.Lagerbestand, 0)) AS BestandGebrauchtware, MAX(BestandRaw.LetzteLagerbewegung) AS LetzteLagerbewegung
-  FROM (
-    SELECT Bestand.ArtGroeID, LagerArt.LagerID, LagerArt.Neuwertig, SUM(Bestand.Bestand) AS Lagerbestand, MAX(LagerBew.Zeitpunkt) AS LetzteLagerbewegung
-    FROM Bestand
-    JOIN LagerArt ON Bestand.LagerArtID = LagerArt.ID
-    LEFT OUTER JOIN LagerBew ON LagerBew.BestandID = Bestand.ID
-    WHERE LagerArt.LagerID IN ($1$)
-      AND Bestand.Bestand <> 0
-    GROUP BY Bestand.ArtGroeID, LagerArt.LagerID, LagerArt.Neuwertig
-  ) AS BestandRaw
-  GROUP BY BestandRaw.ArtGroeID, BestandRaw.LagerID
-)
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Standort.Bez AS Lagerstandort, ArtGroe.EntnahmeJahr AS [Jahressumme Entnahmen], Lagerbestand.BestandNeuware AS [Bestand Neuware], Lagerbestand.BestandGebrauchtware AS [Bestand Gebraucht], CAST(Lagerbestand.LetzteLagerbewegung AS date) AS [Letzte Lagerbewegung]
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, ArtGroe.Groesse AS Größe, Standort.Bez AS Lagerstandort, ArtGroe.EntnahmeJahr AS [Jahressumme Entnahmen], Lagerbestand.BestandNeuware AS [Bestand Neuware], Lagerbestand.BestandGebrauchtware AS [Bestand Gebraucht], CAST(Lagerbestand.LetzteLagerbewegung AS date) AS [Letzte Lagerbewegung]
 FROM ArtGroe
 JOIN Artikel ON ArtGroe.ArtikelID = Artikel.ID
-JOIN Lagerbestand ON Lagerbestand.ArtGroeID = ArtGroe.ID
+JOIN (
+  SELECT BestandRaw.ArtGroeID, BestandRaw.LagerID, MAX(IIF(BestandRaw.Neuwertig = 1, BestandRaw.Lagerbestand, 0)) AS BestandNeuware, MAX(IIF(BestandRaw.Neuwertig = 0, BestandRaw.Lagerbestand, 0)) AS BestandGebrauchtware, MAX(BestandRaw.LetzteBewegung) AS LetzteLagerbewegung
+  FROM (
+    SELECT Bestand.ArtGroeID, LagerArt.LagerID, LagerArt.Neuwertig, SUM(Bestand.Bestand) AS Lagerbestand, Bestand.LetzteBewegung
+    FROM Bestand
+    JOIN LagerArt ON Bestand.LagerArtID = LagerArt.ID
+    WHERE LagerArt.LagerID IN ($1$)
+      AND Bestand.Bestand <> 0
+    GROUP BY Bestand.ArtGroeID, LagerArt.LagerID, LagerArt.Neuwertig, Bestand.LetzteBewegung
+  ) AS BestandRaw
+  GROUP BY BestandRaw.ArtGroeID, BestandRaw.LagerID
+) AS Lagerbestand ON Lagerbestand.ArtGroeID = ArtGroe.ID
 JOIN Standort ON Lagerbestand.LagerID = Standort.ID
 WHERE ArtGroe.EntnahmeJahr = 0;
