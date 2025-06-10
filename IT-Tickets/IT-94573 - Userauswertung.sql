2261fbb6a8902a14e26332cf08fac2702fe041ef
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-06-10 13:15:39 +0200

    don't list newly created users as deactivate

--- IT-Tickets/IT-94573 - Userauswertung.sql
+++ IT-Tickets/IT-94573 - Userauswertung.sql
@@ -6,7 +6,7 @@ SELECT Mitarbei.ID AS MitarbeiID,
   Mitarbei.Vorname,
   Mitarbei.Nachname,
   Mitarbei.LastLogin,
-  [deaktivieren] = CAST(IIF((Mitarbei.LastLogin < N'2025-01-01 00:00:00' OR Mitarbei.LastLogin IS NULL) AND NOT EXISTS (SELECT UsrInGrp.* FROM UsrInGrp WHERE UsrInGrp.MitarbeiID = Mitarbei.ID AND UsrInGrp.UserGrpID IN (SELECT UserGrp.ID FROM UserGrp WHERE UserGrp.UserGrpBez IN (N'SAL_SDC', N'SAL_ADVSUP_only'))), 1, 0) AS bit),
+  [deaktivieren] = CAST(IIF((Mitarbei.LastLogin < N'2025-01-01 00:00:00' OR Mitarbei.LastLogin IS NULL) AND Mitarbei.Anlage_ < DATEADD(month, -1, GETDATE()) AND NOT EXISTS (SELECT UsrInGrp.* FROM UsrInGrp WHERE UsrInGrp.MitarbeiID = Mitarbei.ID AND UsrInGrp.UserGrpID IN (SELECT UserGrp.ID FROM UserGrp WHERE UserGrp.UserGrpBez IN (N'SAL_SDC', N'SAL_ADVSUP_only'))), 1, 0) AS bit),
   [kein Standort] = CAST(IIF(Mitarbei.StandortID < 0, 1, 0) AS bit),
   [keine Firma] = CAST(IIF(Mitarbei.FirmaID < 0, 1, 0) AS bit)
 FROM Mitarbei
@@ -14,4 +14,6 @@ JOIN Standort ON Mitarbei.StandortID = Standort.ID
 JOIN Firma ON Mitarbei.FirmaID = Firma.ID
 WHERE Mitarbei.[Status] = N'A'
   AND Mitarbei.IsAdvanTexUser = 1
-  AND (Mitarbei.LastLogin < N'2025-01-01 00:00:00' OR Mitarbei.LastLogin IS NULL OR Mitarbei.StandortID < 0 OR Mitarbei.FirmaID < 0)
+  AND (Mitarbei.LastLogin < N'2025-01-01 00:00:00' OR Mitarbei.LastLogin IS NULL OR Mitarbei.StandortID < 0 OR Mitarbei.FirmaID < 0);
+
+GO
