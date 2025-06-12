e12c083c76d0b8b5af298d93220ad47885c2613c
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-06-12 15:43:51 +0200

    rewrote for new data structures and fields

--- Diverse/Userauswertung - Firmenzugehörigkeit.sql
+++ Diverse/Userauswertung - Firmenzugehörigkeit.sql
@@ -1,30 +1,20 @@
-SELECT Firma =
-  CASE
-    WHEN Firma.ID < 0 AND Standort.ID < 0 THEN N'WOMI'
-    WHEN Firma.ID < 0 AND Standort.SuchCode = N'BUDW' THEN 'SMBU'
-    WHEN Firma.ID < 0 AND Standort.SuchCode = N'UKLU' THEN N'UKLU'
-    WHEN Firma.ID < 0 AND Standort.SuchCode NOT IN (N'BUDW', N'UKLU') THEN N'WOMI'
-    WHEN Firma.SuchCode = N'SAL' AND Standort.SuchCode = N'BUDW' THEN N'SMBU'
-    WHEN Firma.SuchCode = N'SAL' AND Standort.SuchCode = N'UKLU' THEN N'UKLU'
-    WHEN Firma.SuchCode = N'SAL' AND Standort.SuchCode NOT IN (N'BUDW', N'UKLU') THEN N'SAL'
-    WHEN Firma.SuchCode = N'91' THEN N'WOMI'
-    ELSE Firma.SuchCode
-  END,
-  COUNT(Mitarbei.ID) AS AnzahlMA
+SELECT Firma.Bez AS Firma, Standort.Bez AS Standort, COUNT(Mitarbei.ID) AS [Anzahl User]
 FROM Mitarbei
 JOIN Firma ON Mitarbei.FirmaID = Firma.ID
 JOIN Standort ON Mitarbei.StandortID = Standort.ID
-WHERE Mitarbei.Status = N'A'
-  AND Mitarbei.LastLogin >= N'2019-01-01 00:00:00'
-GROUP BY
-  CASE
-    WHEN Firma.ID < 0 AND Standort.ID < 0 THEN N'WOMI'
-    WHEN Firma.ID < 0 AND Standort.SuchCode = N'BUDW' THEN 'SMBU'
-    WHEN Firma.ID < 0 AND Standort.SuchCode = N'UKLU' THEN N'UKLU'
-    WHEN Firma.ID < 0 AND Standort.SuchCode NOT IN (N'BUDW', N'UKLU') THEN N'WOMI'
-    WHEN Firma.SuchCode = N'SAL' AND Standort.SuchCode = N'BUDW' THEN N'SMBU'
-    WHEN Firma.SuchCode = N'SAL' AND Standort.SuchCode = N'UKLU' THEN N'UKLU'
-    WHEN Firma.SuchCode = N'SAL' AND Standort.SuchCode NOT IN (N'BUDW', N'UKLU') THEN N'SAL'
-    WHEN Firma.SuchCode = N'91' THEN N'WOMI'
-    ELSE Firma.SuchCode
-  END;
+WHERE Mitarbei.IsAdvanTexUser = 1
+  AND Mitarbei.[Status] = N'A'
+  AND Mitarbei.LastLogin >= N'2025-01-01'
+GROUP BY Firma.Bez, Standort.Bez;
+
+GO
+
+SELECT Firma.Bez AS Firma, Standort.Bez AS Standort, Mitarbei.[Name], Mitarbei.MitarbeiUser AS Benutzername
+FROM Mitarbei
+JOIN Firma ON Mitarbei.FirmaID = Firma.ID
+JOIN Standort ON Mitarbei.StandortID = Standort.ID
+WHERE Mitarbei.IsAdvanTexUser = 1
+  AND Mitarbei.[Status] = N'A'
+  AND Mitarbei.LastLogin >= N'2025-01-01';
+
+GO
