bdbcf8f521afdd5a20257aee59b46746ad787097
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-07-02 13:59:21 +0200

    performance optimization

--- Report/764 - Ausgeschiedene OP-Teile - Summen.sql
+++ Report/764 - Ausgeschiedene OP-Teile - Summen.sql
@@ -1,18 +1,38 @@
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, WegGrund.WeggrundBez$LAN$ AS Schrottgrund, COUNT(EinzTeil.ID) AS [Anzahl Ausgeschieden]
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Pipeline: prepareData                                                                                                     ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+DROP TABLE IF EXISTS #SchrottTeile, #LastVsaScan;
+
+SELECT EinzTeil.ID, EinzTeil.WegGrundID, EinzTeil.ArtikelID, EinzTeil.WegDatum, CAST(NULL AS int) AS VsaID
+INTO #SchrottTeile
+FROM EinzTeil
+WHERE EinzTeil.WegGrundID > 0
+	AND EinzTeil.[Status] = N'Z'
+	AND EinzTeil.LastActionsID IN (SELECT Actions.ID FROM Actions WHERE UPPER(Actions.ActionsBez) LIKE N'%SCHROTT%')
+	AND EinzTeil.WegDatum BETWEEN $STARTDATE$ AND $ENDDATE$;
+
+SELECT Scans.EinzTeilID, MAX(Scans.ID) AS LastVsaScanID
+INTO #LastVsaScan
+FROM Scans
+WHERE Scans.LsPoID > 0
+	AND Scans.EinzTeilID IN (SELECT ID FROM #SchrottTeile)
+GROUP BY Scans.EinzTeilID
+
+UPDATE #SchrottTeile SET VsaID = LastVsa.VsaID
 FROM (
-	SELECT EinzTeil.ID, EinzTeil.WegGrundID, EinzTeil.ArtikelID, EinzTeil.WegDatum, VsaID = (
-		SELECT TOP 1 Scans.VsaID
-		FROM Scans
-		WHERE Scans.EinzTeilID = EinzTeil.ID
-			AND Scans.LsPoID > 0
-		ORDER BY Scans.ID DESC
-	)
-	FROM EinzTeil
-	WHERE EinzTeil.WegGrundID > 0
-		AND EinzTeil.[Status] = N'Z'
-		AND EinzTeil.LastActionsID IN (SELECT Actions.ID FROM Actions WHERE UPPER(Actions.ActionsBez) LIKE N'%SCHROTT%')
-		AND EinzTeil.WegDatum BETWEEN $STARTDATE$ AND $ENDDATE$
-) AS EinzTeil
+	SELECT Scans.EinzTeilID, Scans.VsaID
+	FROM #LastVsaScan AS LastVsaScan
+	JOIN Scans ON LastVsaScan.LastVsaScanID = Scans.ID
+) AS LastVsa
+WHERE LastVsa.EinzTeilID = #SchrottTeile.ID;
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Pipeline: OPTeile                                                                                                         ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, WegGrund.WeggrundBez$LAN$ AS Schrottgrund, COUNT(EinzTeil.ID) AS [Anzahl Ausgeschieden]
+FROM #SchrottTeile AS EinzTeil
 JOIN WegGrund ON EinzTeil.WegGrundID = WegGrund.ID
 JOIN Vsa ON EinzTeil.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -20,3 +40,9 @@ JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
 WHERE Vsa.StandKonID IN ($2$)
 	AND Kunden.SichtbarID IN ($SICHTBARIDS$)
 GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez$LAN$, WegGrund.WegGrundBez$LAN$;
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++ Pipeline: Datumsbereic                                                                                                    ++ */
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+SELECT $STARTDATE$ AS VonDatum, $ENDDATE$ AS EndDatum;
