6a9266e7f6bf4cfc6066258884ecd0725ac844b2
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-08-25 15:01:12 +0200

    IT-94299  - added "customer care" contact

--- Report/412 - Tourenplan.sql
+++ Report/412 - Tourenplan.sql
@@ -1,8 +1,11 @@
 SELECT DISTINCT Kunden.KdNr, 
   Kunden.SuchCode AS Kunde, 
   Vsa.VsaNr, 
-  Vsa.Bez AS [VSA-Bezeichnung], 
+  Vsa.Bez AS [VSA-Bezeichnung],
+  VSA.Suchcode,
+  ISNULL(VSA.Strasse,'') +' '+ ISNULL(VSA.PLZ,'') + ' ' + ISNULL(VSA.Ort,'')  as Adresse,
   Bereich.BereichBez$LAN$ AS Produktbereich, 
+  Mitarbei.Name AS [Kundenservice-Mitarbeiter],
   Touren.Tour, 
   Touren.Bez AS Tourbezeichnung, 
   Wochentag = 
@@ -28,9 +31,12 @@ JOIN Kunden ON Vsa.KundenID = Kunden.ID
 JOIN Standort ON Touren.ExpeditionID = Standort.ID
 JOIN KdBer ON VsaTour.KdBerID = KdBer.ID
 JOIN Bereich ON KdBer.BereichID = Bereich.ID
+JOIN Mitarbei ON KdBer.ServiceID = Mitarbei.ID
 WHERE VsaTour.ID > 0
 AND VsaTour.VsaID > 0
 AND Kunden.Status = N'A'
 AND Vsa.Status = N'A'
+AND VSATour.VonDatum <= GETDATE() 
+AND VSATour.BisDatum >= GETDATE() 
 AND Kunden.KdGfID IN ($1$)
 AND Vsa.StandKonID IN ($2$);
