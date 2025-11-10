cee23e36d52583f7ffda81e91a9f070f29d1ec3e
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-11-10 13:57:24 +0100

    bugfix eliminate double teilsofa-entries (from a past advantex bug)

--- Report/734 - Teile verschrottet im Zeitraum - BK - Berufskleidung.sql
+++ Report/734 - Teile verschrottet im Zeitraum - BK - Berufskleidung.sql
@@ -1,4 +1,4 @@
-SELECT WegGrund.WegGrundBez$LAN$ AS Grund, EinzHist.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, Einzteil.ErstDatum, EinzHist.PatchDatum, EinzHist.Ausdienst, EinzHist.AusdienstDat, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, ArtMisch.ArtMischBez AS Gewebe, ArtMisch.Gewicht AS [Flächengewicht g/m²], Artikel.StueckGewicht AS [Gewicht kg/Stück], ArtGru.ArtGruBez AS Artikelgruppe, Bereich.BereichBez AS Produktbereich, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS Geschäftsbereich, EinzHist.AusdRestw AS Restwert, Einzteil.AlterInfo as [Alter des Teils in Wochen], Einzhist.AnzRepair as [Anzahl Reparaturen Gesamt], Einzteil.RuecklaufG AS [Anzahl Wäschen gesamt], EinzHist.RuecklaufK AS [Anzahl Wäschen Kunde], EinzHist.Kostenlos, CAST(IIF(ISNULL(TeilSoFa.ID, -1) > 0, 1, 0) AS bit) AS [Restwert berechnet?], RWArt.RWArtBez$LAN$ AS [Restwert-Art]
+SELECT WegGrund.WegGrundBez$LAN$ AS Grund, EinzHist.Barcode, [Status].StatusBez AS [Status], Week.Woche AS ErstWoche, Einzteil.ErstDatum, EinzHist.PatchDatum, EinzHist.Ausdienst, EinzHist.AusdienstDat, Artikel.ArtikelNr, Artikel.ArtikelNr2, Artikel.ArtikelBez$LAN$ AS Artikelbezeichnung, Artikel.EKPreis, ArtMisch.ArtMischBez AS Gewebe, ArtMisch.Gewicht AS [Flächengewicht g/m²], Artikel.StueckGewicht AS [Gewicht kg/Stück], ArtGru.ArtGruBez AS Artikelgruppe, Bereich.BereichBez AS Produktbereich, Vsa.VsaNr, Vsa.SuchCode AS [VSA-Stichwort], Vsa.Bez AS [VSA-Bezeichnung], Produktion.Bez AS Produktionsstandort, Kunden.KdNr, Kunden.SuchCode AS Kunde, Kundenservice.Bez AS [Kundenservice-Standort], KdGf.KurzBez AS Geschäftsbereich, EinzHist.AusdRestw AS Restwert, Einzteil.AlterInfo as [Alter des Teils in Wochen], Einzhist.AnzRepair as [Anzahl Reparaturen Gesamt], Einzteil.RuecklaufG AS [Anzahl Wäschen gesamt], EinzHist.RuecklaufK AS [Anzahl Wäschen Kunde], EinzHist.Kostenlos, CAST(IIF(ISNULL(TeilSoFa.EinzHistID, -1) > 0, 1, 0) AS bit) AS [Restwert berechnet?], RWArt.RWArtBez$LAN$ AS [Restwert-Art]
 FROM EinzHist
 JOIN Vsa ON EinzHist.VsaID = Vsa.ID
 JOIN Kunden ON Vsa.KundenID = Kunden.ID
@@ -14,7 +14,11 @@ JOIN Standort AS Produktion ON StandBer.ProduktionID = Produktion.ID
 JOIN Standort AS Kundenservice ON Kunden.StandortID = Kundenservice.ID
 JOIN EinzTeil ON EinzHist.EinzTeilID = EinzTeil.ID
 JOIN [Week] ON DATEADD(day, Einzteil.AnzTageImLager, Einzteil.ErstDatum) BETWEEN [Week].VonDat AND [Week].BisDat
-LEFT JOIN TeilSoFa ON TeilSoFa.EinzHistID = EinzHist.ID AND TeilSoFa.SoFaArt = N'R' AND (TeilSoFa.RechPoID > 0 OR (TeilSoFa.Status IN (N'L', N'P') AND TeilSoFa.OhneBerechGrund = 0))
+LEFT JOIN (
+  SELECT TeilSoFa.EinzHistID, TeilSoFa.SoFaArt, TeilSoFa.RechPoID, TeilSoFa.[Status], TeilSoFa.OhneBerechGrund, TeilSoFa.RwArtID
+  FROM TeilSoFa
+  GROUP BY TeilSoFa.EinzHistID, TeilSoFa.SoFaArt, TeilSoFa.RechPoID, TeilSoFa.[Status], TeilSoFa.OhneBerechGrund, TeilSoFa.RwArtID
+) TeilSoFa ON TeilSoFa.EinzHistID = EinzHist.ID AND TeilSoFa.SoFaArt = N'R' AND (TeilSoFa.RechPoID > 0 OR (TeilSoFa.Status IN (N'L', N'P') AND TeilSoFa.OhneBerechGrund = 0))
 LEFT JOIN RWArt ON RWArt.ID = TeilSoFa.RwArtID
 WHERE Kunden.KdGfID IN ($4$)
   AND EinzHist.[Status] = N'Y'
