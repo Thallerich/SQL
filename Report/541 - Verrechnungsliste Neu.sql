5c7201c759f3828680602075fe374c8740b685ca
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-11-10 10:44:23 +0100

    IT-100538  - added discount information

--- Report/541 - Verrechnungsliste Neu.sql
+++ Report/541 - Verrechnungsliste Neu.sql
@@ -1,8 +1,9 @@
-SELECT Traeger.PersNr AS Personalnummer, Traeger.Nachname, Traeger.Vorname, Abteil.Bez AS Kostenstelle, KdArti.VariantBez AS Berufsgruppe, Traeger.VormalsNr AS [Sonstige Daten], Traeger.Indienst AS [Indienststellungswoche Träger], 1 AS Menge, KdArti.LeasPreis AS Periodenpreis
+SELECT Traeger.PersNr AS Personalnummer, Traeger.Nachname, Traeger.Vorname, Abteil.Bez AS Kostenstelle, KdArti.VariantBez AS Berufsgruppe, Traeger.VormalsNr AS [Sonstige Daten], Traeger.Indienst AS [Indienststellungswoche Träger], 1 AS Menge, KdArti.LeasPreis AS Periodenpreis, CAST(KdBer.RabattLeasing AS float) AS Rabattsatz, CAST(KdArti.LeasPreis * (1 - (KdBer.RabattLeasing / 100)) AS money) AS [rabattierter Preis]
 FROM TraeArch
 JOIN TraeArti ON TraeArch.TraeArtiID = TraeArti.ID
 JOIN Traeger ON TraeArti.TraegerID = Traeger.ID
 JOIN KdArti ON TraeArti.KdArtiID = KdArti.ID
+JOIN KdBer ON KdArti.KdBerID = KdBer.ID
 JOIN Abteil ON TraeArch.AbteilID = Abteil.ID
 JOIN Wochen ON TraeArch.WochenID = Wochen.ID
 WHERE Wochen.Woche = $2$
