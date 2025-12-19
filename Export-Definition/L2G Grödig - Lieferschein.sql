50762ff22e58aec202aaaeeaf14c266e6550daaa
Thallerich <20878157+Thallerich@users.noreply.github.com>
2025-12-19 07:35:53 +0100

    bugfix can simply use the vps-reference from the delivery scan now

--- Export-Definition/L2G Grödig - Lieferschein.sql
+++ Export-Definition/L2G Grödig - Lieferschein.sql
@@ -20,29 +20,16 @@ WHERE Kunden.KdNr = 10006208
 IF @@ROWCOUNT > 0
 BEGIN
 
-  WITH ScanHistory AS (
-    SELECT Scans.EinzTeilID, Scans.ID AS LastAusgangScanID, LAG(Scans.ID) OVER (PARTITION BY Scans.EinzTeilID ORDER BY Scans.ID) AS BeforeLastAusgangScanID
-    FROM Scans
-    WHERE Scans.ActionsID IN (102, 165)
-      AND Scans.EinzTeilID IN (SELECT EinzTeilID FROM #LsForExport)
-  )
-  SELECT LsKo.LsNr,
+  SELECT DISTINCT
+    LsKo.LsNr,
     Vsa.VsaNr,
-    VpsKoID = (
-      SELECT TOP 1 VPSPo.VPSKoID
-      FROM Scans
-      JOIN VPSPo ON Scans.VPSPoID = VPSPo.ID
-      WHERE Scans.EinzTeilID = EinzTeil.ID
-        AND Scans.ActionsID = 126
-        AND Scans.ID BETWEEN ISNULL(ScanHistory.BeforeLastAusgangScanID, 1) AND ScanHistory.LastAusgangScanID
-      ORDER BY Scans.ID DESC
-    ),
+    VPSPo.VpsKoID,
     EinzTeil.Code,
     LsKo.Datum,
     CAST(Kunden.KdNr AS nvarchar) + N'_' + CAST(Vsa.VsaNr AS nvarchar) AS _SPLIT_
   FROM EinzTeil
   JOIN Scans ON Scans.EinzTeilID = EinzTeil.ID
-  JOIN ScanHistory ON Scans.ID = ScanHistory.LastAusgangScanID
+  JOIN VPSPo ON Scans.VPSPoID = VPSPo.ID
   JOIN LsPo ON LsPo.ID = Scans.LsPoID
   JOIN LsKo ON LsKo.ID = LsPo.LsKoID
   JOIN Vsa ON Vsa.ID = LsKo.VsaID
