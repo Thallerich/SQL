5df4da90decff54ec51a54463184c63dadd0496f
Thallerich <20878157+Thallerich@users.noreply.github.com>
2026-01-15 10:01:05 +0100

    get data from teilsofa

--- VOEST/VOEST - Restwerte zu Artikeln.sql
+++ VOEST/VOEST - Restwerte zu Artikeln.sql
@@ -1,19 +1,15 @@
-SELECT Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, COUNT(EinzTeil.ID) AS [Anzahl Teile], SUM(CAST(EinzHist.RestwertInfo AS decimal(18,4))) AS Restwert
-FROM EinzTeil
-JOIN EinzHist ON EinzTeil.CurrEinzHistID = EinzHist.ID
-JOIN Traeger ON EinzHist.TraegerID = Traeger.ID
-JOIN KdArti ON EinzHist.KdArtiID = KdArti.ID
-JOIN Artikel ON KdArti.ArtikelID = Artikel.ID
+SELECT Artikel.ArtikelNr, Artikel.ArtikelBez AS Artikelbezeichnung, COUNT(EinzTeil.ID) AS [Anzahl Teile], SUM(CAST(TeilSoFa.EPreis AS decimal(18,4))) AS Restwert
+FROM TeilSoFa
+JOIN EinzTeil ON TeilSoFa.EinzTeilID = EinzTeil.ID
+JOIN EinzHist ON TeilSoFa.EinzHistID = EinzHist.ID
+JOIN Artikel ON EinzTeil.ArtikelID = Artikel.ID
 JOIN Vsa ON EinzHist.VsaID = Vsa.ID
 JOIN Kunden ON EinzHist.KundenID = Kunden.ID
 JOIN Holding ON Kunden.HoldingID = Holding.ID
 WHERE Holding.Holding = N'VOES'
   AND Artikel.ArtikelNr IN ('29V1', '29V3', '70V3', '70V4', '70V6', '45V3', '45V4', '45V5', '46V2', '44V2', '15V2', '41V2', '42V2', '46V5', '44V5', '41VB', '42VB', '45VL', '46VL', '44VL', '41VL', '04VC', '70VR', '05VT', '15VT', '04VT', '06VT', '01VT', '08VT', '05LT', '04LT', '01LT', '06LT')
-  AND EinzHist.EinzHistTyp = 1
-  AND EinzTeil.AltenheimModus = 0
-  AND EinzHist.Kostenlos = 0
-  AND ISNULL(EinzHist.Indienst, '1980/01') <= '2026/03'
-  AND ISNULL(EinzHist.Ausdienst, '2099/52') > '2026/03'
-  AND Traeger.Status NOT IN ('K', 'P')
-  AND Vsa.Status = 'A'
+  AND TeilSoFa.Zeitpunkt >= N'2025-01-01 00:00:00.000'
+  AND TeilSoFa.Zeitpunkt <= N'2025-12-31 23:59:59.999'
+  AND TeilSoFa.SoFaArt = N'R'
+  AND TeilSoFa.[Status] >= N'H'
 GROUP BY Artikel.ArtikelNr, Artikel.ArtikelBez;
