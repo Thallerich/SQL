ad1df73dcef2448b60dee432c108d16b17d4813d
Thallerich <20878157+Thallerich@users.noreply.github.com>
2026-01-23 10:54:47 +0100

    new script for added notices to garments old script deleted as it is no longer functional

---
+++ Diverse/Hinweise zu Teilen anlegen (aus importierter Tabelle).sql
@@ -0,0 +1,29 @@
+DECLARE @userid int = (SELECT ID FROM Mitarbei WHERE UserName = UPPER(REPLACE(ORIGINAL_LOGIN(), N'SAL\', N'')));
+
+BEGIN TRY
+  BEGIN TRANSACTION;
+  
+    INSERT INTO Hinweis (EinzHistID, EinzTeilID, Aktiv, Hinweis, BisWoche, Anzahl, EingabeDatum, HinwTextID, EingabeMitarbeiID, AnlageUserID_, UserID_)
+    SELECT EinzHist.ID AS EinzHistID, EinzHist.EinzTeilID, CAST(1 AS bit) AS Aktiv, N'Kundenservice (SM) - UHF-Chip prüfen/zuordnen' AS Hinweis, '2099/52' AS BisWoche, 1 AS Anzahl, GETDATE() AS Eingabedatum, 1000539 AS HinwTextID, @userid AS EingabeMitarbID, @userid AS AnlageUserID_, @userid AS UserID_
+    FROM EinzHist
+    WHERE EinzHist.Barcode IN (SELECT Code FROM Salesianer.._SAWRKsHinweis)
+      AND EinzHist.ID = (SELECT EinzTeil.CurrEinzHistID FROM EinzTeil WHERE EinzTeil.ID = EinzHist.EinzTeilID);
+
+    INSERT INTO Hinweis (EinzHistID, EinzTeilID, Aktiv, Hinweis, BisWoche, Anzahl, EingabeDatum, HinwTextID, EingabeMitarbeiID, AnlageUserID_, UserID_)
+    SELECT EinzHist.ID AS EinzHistID, EinzHist.EinzTeilID, CAST(1 AS bit) AS Aktiv, N'Label erneuern (SM) - UHF-Chip prüfen/zuordnen' AS Hinweis, '2099/52' AS BisWoche, 1 AS Anzahl, GETDATE() AS Eingabedatum, 1000524 AS HinwTextID, @userid AS EingabeMitarbID, @userid AS AnlageUserID_, @userid AS UserID_
+    FROM EinzHist
+    WHERE EinzHist.Barcode IN (SELECT Code FROM Salesianer.._SAWRLableHinweis)
+      AND EinzHist.ID = (SELECT EinzTeil.CurrEinzHistID FROM EinzTeil WHERE EinzTeil.ID = EinzHist.EinzTeilID); 
+  
+  COMMIT;
+END TRY
+BEGIN CATCH
+  DECLARE @Message varchar(MAX) = ERROR_MESSAGE();
+  DECLARE @Severity int = ERROR_SEVERITY();
+  DECLARE @State smallint = ERROR_STATE();
+  
+  IF XACT_STATE() != 0
+    ROLLBACK TRANSACTION;
+  
+  RAISERROR(@Message, @Severity, @State) WITH NOWAIT;
+END CATCH;
